de497386a6248e504acb1abab8d4c64ebcef8ce2
Miško Hevery <misko@hevery.com>
2020-02-24 17:33:58 +0000

    89f6b341a3 docs: add amazon s3 builder package (#34783)

--- fesm2015/localize.js
+++ fesm2015/localize.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+27.sha-bb09cd0
+ * @license Angular v9.0.2+41.sha-89f6b34
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -259,7 +259,8 @@ function translate(translations, messageParts, substitutions) {
                 return message.substitutions[placeholder];
             }
             else {
-                throw new Error(`No placeholder found with name ${placeholder} in message ${describeMessage(message)}.`);
+                throw new Error(`There is a placeholder name mismatch with the translation provided for the message ${describeMessage(message)}.\n` +
+                    `The translation contains a placeholder with name ${placeholder}, which does not exist in the message.`);
             }
         })
     ];
