4ac1c649ef6aaf5fbe4a5235f6eb86a4e784f524
atscott <atscott@google.com>
2020-04-14 19:35:07 +0000

    698b0288be build: reformat repo to new clang@1.4.0 (#36613)

--- fesm2015/localize.js
+++ fesm2015/localize.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+32.sha-5e80e7e
+ * @license Angular v10.0.0-next.1+33.sha-698b028
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -101,7 +101,8 @@ function parseMessage(messageParts, expressions) {
         messageString,
         meaning: metadata.meaning || '',
         description: metadata.description || '',
-        messageParts: cleanedMessageParts, placeholderNames,
+        messageParts: cleanedMessageParts,
+        placeholderNames,
     };
 }
 /**
@@ -195,9 +196,9 @@ function computePlaceholderName(index) {
  */
 function findEndOfBlock(cooked, raw) {
     /************************************************************************************************
-    * This function is repeated in `src/localize/src/localize.ts` and the two should be kept in sync.
-    * (See that file for more explanation of why.)
-    ************************************************************************************************/
+     * This function is repeated in `src/localize/src/localize.ts` and the two should be kept in sync.
+     * (See that file for more explanation of why.)
+     ************************************************************************************************/
     for (let cookedIndex = 1, rawIndex = 1; cookedIndex < cooked.length; cookedIndex++, rawIndex++) {
         if (raw[rawIndex] === '\\') {
             rawIndex++;
