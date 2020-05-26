443e568a1e6f0186977978b64dff04cf8e2c7a5d
Matias Niemelä <matias@yearofmoo.com>
2020-05-26 21:02:02 +0000

    a62c1c441c build: migrate ng-dev config to .ng-dev directory (#37142)

--- src/tools/src/translate/translation_files/translation_parsers/translation_utils.d.ts
+++ src/tools/src/translate/translation_files/translation_parsers/translation_utils.d.ts
@@ -1,7 +1,7 @@
 /// <amd-module name="@angular/localize/src/tools/src/translate/translation_files/translation_parsers/translation_utils" />
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
