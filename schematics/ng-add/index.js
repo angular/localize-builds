22dce40407abda7cec4d3e4a0379e2471e5b6a25
atscott <atscott01@gmail.com>
2020-09-03 19:23:07 +0000

    25afbcc459 docs: add dayjs date adapter to resources page (#38031)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.0+6.sha-29c89c9"
+                version: "~10.1.0+7.sha-25afbcc"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
