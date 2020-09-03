e4fcd49a09b79802c564e24a7b3f4e5fe294cb6f
Andrew Scott <atscott@google.com>
2020-09-03 21:30:18 +0000

    9bf32c4dcb refactor(compiler-cli): make template parsing errors into diagnostics (#38576)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.0+7.sha-25afbcc"
+                version: "~10.1.0+9.sha-9bf32c4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
