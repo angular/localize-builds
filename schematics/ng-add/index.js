05955c14bad46cb622f7bde2e298b92456f9b709
atscott <atscott01@gmail.com>
2020-09-08 17:27:34 +0000

    a29f9a9fb3 fix(dev-infra): change logging of commit message restoration to debug (#38704)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.0+17.sha-3e57ca1"
+                version: "~11.0.0-next.0+23.sha-a29f9a9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
