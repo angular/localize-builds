c26ff6af5252466e1371c6121d0e64725fb082c2
atscott <atscott01@gmail.com>
2020-09-08 21:49:57 +0000

    4360eed9b7 fix(localize): enable whitespace preservation marker in XLIFF files (#38737)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.0+42.sha-c880e39"
+                version: "~11.0.0-next.0+43.sha-4360eed"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
