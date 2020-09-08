8d105d55828644d9b390b5aedf3585fbb7277b40
atscott <atscott01@gmail.com>
2020-09-08 19:10:28 +0000

    7869de6136 fix(ngcc): use aliased exported types correctly (#38666)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.0+27.sha-92ff6d9"
+                version: "~11.0.0-next.0+29.sha-7869de6"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
