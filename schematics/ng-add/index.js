4b0d5dbfd69f891e20ffe285e3d10ecc75e5b36f
atscott <atscott01@gmail.com>
2020-09-08 17:24:28 +0000

    7baa7ebfc4 docs(core): update CONSTS to DECLS (#38731)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.0+13.sha-73001b4"
+                version: "~11.0.0-next.0+15.sha-7baa7eb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
