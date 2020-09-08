e2677af915282c88c5a25501fe571cff1f73d2f2
atscott <atscott01@gmail.com>
2020-09-08 19:09:28 +0000

    92ff6d93eb fix(localize): render location in XLIFF 2 even if there is no metadata (#38713)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.0+28.sha-2c4a98a"
+                version: "~11.0.0-next.0+27.sha-92ff6d9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
