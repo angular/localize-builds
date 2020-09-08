68c209b0cdc64675dd27453af07d1f67f3dfb394
atscott <atscott01@gmail.com>
2020-09-08 19:06:16 +0000

    2c4a98a285 fix(localize): do not expose NodeJS typings in $localize runtime code (#38700)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.0+25.sha-926ffcd"
+                version: "~11.0.0-next.0+28.sha-2c4a98a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
