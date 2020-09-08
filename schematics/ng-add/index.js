6ea2d31d7ab2d9ce79a303b872b3c72b40f3524f
atscott <atscott01@gmail.com>
2020-09-08 17:30:01 +0000

    5863537575 build: upgrade all preview-server JS dependencies to latest versions (#38736)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.0+10.sha-0a277c6"
+                version: "~10.1.0+20.sha-5863537"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
