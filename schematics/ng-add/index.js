ad672590114f20c094f50cfda36da6408c61a004
atscott <atscott01@gmail.com>
2020-09-08 17:42:47 +0000

    926ffcd8ac fix(router): support lazy loading for empty path named outlets (#38379)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.0+21.sha-261f689"
+                version: "~11.0.0-next.0+25.sha-926ffcd"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
