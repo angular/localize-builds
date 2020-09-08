15aa3ad3cb8bb28490acd7f67f379cb5e56dc076
atscott <atscott01@gmail.com>
2020-09-08 17:28:54 +0000

    79d8795633 docs: fix typos in library guide (#38726)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.0+15.sha-7baa7eb"
+                version: "~11.0.0-next.0+18.sha-79d8795"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
