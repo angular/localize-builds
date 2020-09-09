dd33683bac880213391269b09831294f04090443
atscott <atscott01@gmail.com>
2020-09-09 17:07:46 +0000

    3d77b64fc3 docs: add Andrew Grekov to GDE resources (#36690)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.0+50.sha-7997fc5"
+                version: "~11.0.0-next.0+53.sha-3d77b64"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
