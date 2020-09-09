be842567e738e34db3d0e4726f1c1e461291fdd7
atscott <atscott01@gmail.com>
2020-09-09 23:47:36 +0000

    3c4b8b97c1 docs: put docs style guide back in left nav (#38683)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.1+1.sha-c6ebb77"
+                version: "~11.0.0-next.1+3.sha-3c4b8b9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
