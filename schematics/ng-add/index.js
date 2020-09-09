c0cea681049d976c2ad671fd85220b409867b829
atscott <atscott01@gmail.com>
2020-09-09 23:51:25 +0000

    7669bd856f feat(dev-infra): Allow local user ng-dev configuration to disable commit message wizard (#38701)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.1"
+                version: "~10.1.1+5.sha-7669bd8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
