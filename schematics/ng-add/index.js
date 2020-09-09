f9ac453d66b7a80d46a4136a66c79665b055b191
atscott <atscott01@gmail.com>
2020-09-09 16:30:27 +0000

    6d9bfb8368 docs(router): fixed PreloadAllModules comment typo (#38758) PR Close #38758

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.0+47.sha-19a4843"
+                version: "~11.0.0-next.0+51.sha-6d9bfb8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
