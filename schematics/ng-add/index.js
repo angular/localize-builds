9780b7b5d0e80e66d5437261075af0cee92aed66
atscott <atscott01@gmail.com>
2020-09-09 19:48:00 +0000

    396548442e Revert "perf(compiler-cli): only emit directive/pipe references that are used (#38749)" (#38767)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.0+46.sha-f1b355b"
+                version: "~10.1.0+48.sha-3965484"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
