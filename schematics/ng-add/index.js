4c3ad347b58d6baf9b58fdedcbf8c16dc9f2c9e7
atscott <atscott01@gmail.com>
2020-09-09 16:32:20 +0000

    19a484302d docs: Fix component decorator closing brackets (#38754) PR Close #38754

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.0+49.sha-3cb2a79"
+                version: "~11.0.0-next.0+47.sha-19a4843"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
