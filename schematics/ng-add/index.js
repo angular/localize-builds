7843094294cc032f78d07317202fa652f7120aaa
atscott <atscott01@gmail.com>
2020-09-09 16:31:05 +0000

    f645d26e3f docs(docs-infra): add The Deep Dive podcast, update Angular inDepth URL (#37621)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.0+46.sha-ded075a"
+                version: "~11.0.0-next.0+52.sha-f645d26"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
