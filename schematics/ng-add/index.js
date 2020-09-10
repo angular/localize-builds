14c196e019deca9f1d9f029f91303c0224482154
Andrew Kushnir <akushnir@google.com>
2020-09-10 22:58:23 +0000

    6acea54f62 fix(common): mark locale data arrays as readonly (#30397)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.1+22.sha-2d52c80"
+                version: "~11.0.0-next.1+23.sha-6acea54"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
