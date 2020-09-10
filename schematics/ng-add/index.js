05eb5eb13994552dd779496daf57826127719111
Andrew Kushnir <akushnir@google.com>
2020-09-10 17:03:27 +0000

    9078187378 docs: add note about only one label target being allowed to be applied to a PR (#38793)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.1+9.sha-edb7f90"
+                version: "~10.1.1+11.sha-9078187"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
