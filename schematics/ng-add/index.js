9ae273ea25819485c37a5cf28bb907e850cdf118
Andrew Kushnir <akushnir@google.com>
2020-09-10 18:21:35 +0000

    a47383d1e8 fix(localize): ensure that `formatOptions` is optional (#38787)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.1+11.sha-9078187"
+                version: "~10.1.1+12.sha-a47383d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
