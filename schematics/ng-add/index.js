0c13a1844cf1527226100eb0b54c8371220a76ae
Andrew Kushnir <akushnir@google.com>
2020-09-11 21:22:20 +0000

    aa43cbf8c5 fix(dev-infra): correct build setup for dev-infra (#38815)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.1+18.sha-b05d79d"
+                version: "~10.1.1+19.sha-aa43cbf"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
