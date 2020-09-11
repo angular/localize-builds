6e010e137e252ccbf2a0acdc531f228c455b57af
Andrew Kushnir <akushnir@google.com>
2020-09-11 20:45:04 +0000

    b05d79d14a build(router): update symbols for routing app (#38817)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.1+15.sha-8096c63"
+                version: "~10.1.1+18.sha-b05d79d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
