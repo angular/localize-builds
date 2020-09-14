d495cad1703ccc9b64bdbdf52024b336a1cf26e1
Andrew Kushnir <akushnir@google.com>
2020-09-14 21:55:41 +0000

    3934f0a833 docs(elements): convert the `ng add` command to code-example (#38834)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.1+34.sha-297c060"
+                version: "~11.0.0-next.1+35.sha-3934f0a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
