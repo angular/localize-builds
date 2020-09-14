a2d8b2abc1940665aaf12cba0faae6e47bd9cf42
Andrew Kushnir <akushnir@google.com>
2020-09-14 20:30:16 +0000

    ebede67433 perf(compiler-cli): optimize computation of type-check scope information (#38843)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.1+20.sha-c62d1cb"
+                version: "~10.1.1+22.sha-ebede67"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
