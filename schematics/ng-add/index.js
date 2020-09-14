0d009a3b38e548d533b767f5d8d85349d6796de2
Andrew Kushnir <akushnir@google.com>
2020-09-14 21:56:28 +0000

    26deef2d3e feat(dev-infra): allow local ng-dev configuration to error on invalid commit messages (#38784)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.1+22.sha-ebede67"
+                version: "~10.1.1+24.sha-26deef2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
