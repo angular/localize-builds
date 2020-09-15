9ec2e60a69b9335c47c208bcdf938bb8b30ca752
Andrew Kushnir <akushnir@google.com>
2020-09-15 01:26:05 +0000

    593bd594e3 build: create temporary script for symbol extractor tests (#38819)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.1+36.sha-78e1ecb"
+                version: "~11.0.0-next.1+38.sha-593bd59"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
