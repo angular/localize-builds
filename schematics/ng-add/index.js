6a4b39627b43b0a69c1720cf065ff023b97430da
Andrew Kushnir <akushnir@google.com>
2020-09-15 23:27:20 +0000

    d192c87f6a refactor(dev-infra): refactor commit-message files (#38845)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.1+45.sha-3817e5f"
+                version: "~11.0.0-next.1+46.sha-d192c87"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
