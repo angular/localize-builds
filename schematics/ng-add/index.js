37fff6ab103f344dafec87e57e31936cf47496e4
Andrew Kushnir <akushnir@google.com>
2020-09-15 18:54:26 +0000

    3817e5f1df fix(router): Fix arguments order for call to shouldReuseRoute (#26949)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.1+44.sha-171a0d0"
+                version: "~11.0.0-next.1+45.sha-3817e5f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
