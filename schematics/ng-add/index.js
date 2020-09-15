226307e1cf1e9dbe075c935c7d214009bd3f6eac
Andrew Kushnir <akushnir@google.com>
2020-09-15 16:05:30 +0000

    84bd1a233d feat(dev-infra): include CI status check in the caretaker check (#38779)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.1+26.sha-dc4f858"
+                version: "~10.1.1+28.sha-84bd1a2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
