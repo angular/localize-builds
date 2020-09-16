5af665b6885141a0f4f93cae008b1b7566580b1b
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:49:05 +0000

    722699fb0c fix(dev-infra): handle no caretaker config being defined (#38862)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+8.sha-49ee90b"
+                version: "~11.0.0-next.2+11.sha-722699f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
