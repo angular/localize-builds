08d11f5c61260695e589a07966a12076f2a3f7e8
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:42:06 +0000

    7849fdde09 feat(router): add migration to update calls to navigateByUrl and createUrlTree with invalid parameters (#38825)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+6.sha-97adc27"
+                version: "~11.0.0-next.2+7.sha-7849fdd"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
