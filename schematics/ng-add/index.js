2c775bb376e794743b946937f038f39d4df7a52b
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:45:27 +0000

    e62a918542 docs(forms): fix grammar in first sentence of reset function docs (#38872)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+7.sha-7849fdd"
+                version: "~11.0.0-next.2+9.sha-e62a918"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
