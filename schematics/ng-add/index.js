231374eff950af7296eee2a48e42000854a32a95
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:50:56 +0000

    dd8d8c8289 fix(common): add `params` and `reportProgress` options to `HttpClient.put()` overload (#37873)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+11.sha-722699f"
+                version: "~11.0.0-next.2+13.sha-dd8d8c8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
