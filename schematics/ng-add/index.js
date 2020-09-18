c4737a1526e1501970de0c0a6966da8373c694de
Misko Hevery <misko@hevery.com>
2020-09-18 18:42:56 +0000

    2d6105a784 feat(dev-infra): output the number of new and fixed cycles (#38805)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+22.sha-5548571"
+                version: "~11.0.0-next.2+25.sha-2d6105a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
