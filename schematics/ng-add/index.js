fbaf04ddb809575bdcdbb47909ef3e31bae2cb96
Misko Hevery <misko@hevery.com>
2020-09-18 15:31:03 +0000

    02c6bff9cf Update aio/content/marketing/events.json (#38874)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+19.sha-d795a00"
+                version: "~11.0.0-next.2+17.sha-02c6bff"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
