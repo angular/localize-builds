324642521e75051445ffed079d6ec23534248b59
Misko Hevery <misko@hevery.com>
2020-09-18 15:33:58 +0000

    6b05dc432b refactor(core): remove unused imports (#38818)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.2+10.sha-0958a8d"
+                version: "~10.1.2+16.sha-6b05dc4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
