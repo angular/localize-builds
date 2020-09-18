3cc0471c93c4dd0a24fa3e2c3256c815d0df8e18
Misko Hevery <misko@hevery.com>
2020-09-18 15:29:10 +0000

    7fb388f929 docs: updating the text of user input page to reflect real location of the forms page (#38802)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+21.sha-d3169c5"
+                version: "~11.0.0-next.2+18.sha-7fb388f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
