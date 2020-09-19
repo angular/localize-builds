41a4d81d1a0659857e87f19141f44947bbdb6cbf
Misko Hevery <misko@hevery.com>
2020-09-19 00:26:01 +0000

    a91f0f6b82 refactor(compiler): refactor ast spans test to be more human readable (#38902)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+28.sha-b68fab5"
+                version: "~11.0.0-next.2+30.sha-a91f0f6"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
