9e1da6bf42f747920d26fa6f19925755bb350d9c
Misko Hevery <misko@hevery.com>
2020-09-21 19:52:14 +0000

    2b1b7180db fix(forms): type NG_VALUE_ACCESSOR injection token as array (#29723)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+36.sha-fdd4fa0"
+                version: "~11.0.0-next.2+37.sha-2b1b718"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
