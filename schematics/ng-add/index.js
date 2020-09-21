425eb067471e43e722dae9c29f42514702169592
Misko Hevery <misko@hevery.com>
2020-09-21 19:54:00 +0000

    dfbfabc052 fix(docs-infra): remove scrollbar styles for accessibility (#38852)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+35.sha-984ed39"
+                version: "~11.0.0-next.2+33.sha-dfbfabc"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
