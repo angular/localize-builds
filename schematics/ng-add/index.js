c483291878a14a22dddc2ff6f04247be220f0d8e
Misko Hevery <misko@hevery.com>
2020-09-22 22:36:23 +0000

    b1682526dd refactor(dev-infra): simplify runBenchmark (#38941)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+52.sha-ba3f4c2"
+                version: "~11.0.0-next.2+54.sha-b168252"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
