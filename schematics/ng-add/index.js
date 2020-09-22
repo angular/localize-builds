ec0e0eb214d2a6245b04d1b133b9f70a125d4a09
Misko Hevery <misko@hevery.com>
2020-09-22 22:33:20 +0000

    ba3f4c26bb refactor(compiler): make `keySpan` available for `BoundAttributes` (#38898)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+50.sha-a206852"
+                version: "~11.0.0-next.2+52.sha-ba3f4c2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
