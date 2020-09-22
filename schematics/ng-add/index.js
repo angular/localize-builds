dedb2c9515a6cd72c0296d3f563d9eb5187a9ad6
Misko Hevery <misko@hevery.com>
2020-09-22 22:41:43 +0000

    c8f056beb6 fix(core): ensure TestBed is not instantiated before override provider (#38717)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+53.sha-7561050"
+                version: "~11.0.0-next.2+51.sha-c8f056b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
