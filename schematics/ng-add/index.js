7f42c136a6de2bfb24b49ad2e6468d10d4cc2f40
Misko Hevery <misko@hevery.com>
2020-09-22 16:53:50 +0000

    a2068523fd feat(service-worker): add the option to prefer network for navigation requests (#38565)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.2+49.sha-145ab3d"
+                version: "~11.0.0-next.2+50.sha-a206852"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
