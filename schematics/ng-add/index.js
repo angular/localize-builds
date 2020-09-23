d9312a022ec5de1d1e5290c2966b4990a2c2a181
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-23 20:11:02 +0000

    15ea811f05 feat(router): Add `relativeLinkResolution` migration to update default value (#38698)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+4.sha-f979914"
+                version: "~11.0.0-next.3+1.sha-15ea811"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
