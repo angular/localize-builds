8f1967620b46e0ca90bfa727471958e60f1048ea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-24 16:02:01 +0000

    19d543f71e fix(zone.js): disable wrap uncaught promise rejection should handle primitive value (#38476)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+2.sha-ffe89fb"
+                version: "~11.0.0-next.3+5.sha-19d543f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
