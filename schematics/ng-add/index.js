7a79d631b0c9a19f49040bae279bd8980d0b774a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 15:02:46 +0000

    e4f4d18e7e refactor(router): Adjust type of parameter in navigateByUrl and createUrlTree to be more accurate (#38227)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+10.sha-837889f"
+                version: "~11.0.0-next.3+11.sha-e4f4d18"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
