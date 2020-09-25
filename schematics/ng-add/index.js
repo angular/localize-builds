55a0b136ceb0267895678243cf17960c0cf7017c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:59:33 +0000

    323be39297 fix(language-service): hybrid visitor returns parent node of BoundAttribute (#38995)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+20.sha-239968d"
+                version: "~11.0.0-next.3+22.sha-323be39"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
