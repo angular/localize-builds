27576afb95396fd06f8e12a0ef1438ddd5031b01
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:52:58 +0000

    2f6777df18 docs(docs-infra): add upcoming conference (#38956)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+12.sha-7f6f8bb"
+                version: "~11.0.0-next.3+18.sha-2f6777d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
