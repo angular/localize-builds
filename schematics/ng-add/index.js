aaf9c44f434cbb5ec3cea844fd884b732f5e8434
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:58:23 +0000

    e790c8547e test(compiler-cli): load test files into memory only once (#38909)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+17.sha-4ca1c73"
+                version: "~11.0.0-next.3+14.sha-e790c85"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
