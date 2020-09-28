ff54b280d91d30a682fa9e944dcdbd30cdc4dc5a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:49:13 +0000

    cb7164a236 refactor(zone.js): rename BlacklistedStackFrames to InternalZoneJsStackFrames (#38978)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+46.sha-f96dcc5"
+                version: "~11.0.0-next.3+64.sha-cb7164a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
