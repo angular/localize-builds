161e668e83fba18b48532b45b381b1a3a952bdc4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:52:42 +0000

    c5e178997c build(docs-infra): update docs examples to Angular v10 (#38993)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+69.sha-4fd635a"
+                version: "~11.0.0-next.3+65.sha-c5e1789"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
