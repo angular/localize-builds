bf5738b733972c7e0950910aea2168f6a32e3a0f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:54:13 +0000

    3ba97ab391 ci: note Paul and Pawel as OOO in pullapprove (#39028)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+65.sha-c5e1789"
+                version: "~11.0.0-next.3+77.sha-3ba97ab"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
