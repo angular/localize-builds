1d3ae5562e456168e447c48b974548a0710c92a1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:52:30 +0000

    4fd635a03a build(docs-infra): enable AOT in development mode for all docs examples (#39001)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+63.sha-4a9f452"
+                version: "~11.0.0-next.3+69.sha-4fd635a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
