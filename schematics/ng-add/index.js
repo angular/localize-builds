d2686ac3ab3e978b1bc0ae623ac138159c8f17c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 16:47:00 +0000

    4744c229db fix(common): correct typing and implementation of `SlicePipe` (#37447)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+22.sha-323be39"
+                version: "~11.0.0-next.3+29.sha-4744c22"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
