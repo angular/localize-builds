4f46b265243b045f3bf2a82cca89fcf291c5e0a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:55:04 +0000

    06525cfed3 test(compiler-cli): fix tests to have at least one component (#39011)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+77.sha-3ba97ab"
+                version: "~11.0.0-next.3+72.sha-06525cf"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
