a97afeca8c7d90c103f5aea39e5d838eb2cb2b48
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:54:08 +0000

    8f11b516f8 refactor(compiler-cli): API for getting components from a template file (#39002)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+90.sha-393ce55"
+                version: "~11.0.0-next.3+87.sha-8f11b51"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
