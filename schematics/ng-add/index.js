8cb26273abf3c179e2aedafba7740caad26ab9cd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:41:16 +0000

    b16a69c7a4 docs: fix two broken commands (#38960)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+78.sha-1b70dc9"
+                version: "~11.0.0-next.3+81.sha-b16a69c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
