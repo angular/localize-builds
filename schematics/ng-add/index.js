46cbcdbf87f64d2c9e7deb3e3eb42350514a87fd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:48:50 +0000

    8f66540152 test(language-service): Update AppComponent test project to include more fields (#39033)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+82.sha-ded9aeb"
+                version: "~11.0.0-next.3+88.sha-8f66540"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
