20815e99676be863a7500c028b1fcfcd74abde19
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 16:08:49 +0000

    c8d757624b refactor(dev-infra): use common git client for git environment reset (#39051)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+96.sha-31e42f0"
+                version: "~11.0.0-next.3+97.sha-c8d7576"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
