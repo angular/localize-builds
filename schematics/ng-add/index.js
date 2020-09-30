7be31f441a43c740a0feebaa7d893ece486c32d6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:57:27 +0000

    8b7acc4f8f refactor(compiler): Binding parser sets binding span as source span in Ivy (#39036)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+93.sha-e8084ff"
+                version: "~11.0.0-next.3+91.sha-8b7acc4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
