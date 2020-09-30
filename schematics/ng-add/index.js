97258273a1f3bdc2fad023d11e8e4bdb8dd029bd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-30 13:54:40 +0000

    2aeaedc519 docs(docs-infra): replace a comma with a period at the end of a sentence (#39034)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.3+87.sha-8f11b51"
+                version: "~11.0.0-next.3+89.sha-2aeaedc"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
