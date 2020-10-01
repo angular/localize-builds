3bcba09aa9271e8ab6ad73bd65c761b130b89b47
Joey Perrott <josephperrott@gmail.com>
2020-10-01 16:57:32 +0000

    50e83e2566 refactor(docs-infra): leave TODO comments for aligning tslint with CLI (#39018)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.4+7.sha-bb11fd9"
+                version: "~10.1.4+14.sha-50e83e2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
