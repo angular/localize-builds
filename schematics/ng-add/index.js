0eb0865f72fe37db086e9aeec2b20b15ff095fef
Joey Perrott <josephperrott@gmail.com>
2020-10-01 17:39:08 +0000

    d66d82a9b5 Revert "fix(core): use single quotes for relative link resolution migration to align with style guide (#39070)" (#39082)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.4+22.sha-ac54019"
+                version: "~11.0.0-next.4+24.sha-d66d82a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
