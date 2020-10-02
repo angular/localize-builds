1c0c50ef126d5c10006960bae5ea48d2ec16d43e
Joey Perrott <josephperrott@gmail.com>
2020-10-02 18:22:26 +0000

    30433a0710 fix(docs-infra): better distinguish wrapped headings from other entries in TOC (#39092)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.4+18.sha-42f9679"
+                version: "~10.1.4+20.sha-30433a0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
