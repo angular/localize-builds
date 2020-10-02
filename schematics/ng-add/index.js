756e87666fb308ecf909f5da3e906e2cac13f4ec
Joey Perrott <josephperrott@gmail.com>
2020-10-02 18:24:11 +0000

    d3bd00b7bf docs: remove IE9-10 from `.browserslistrc` in docs (#39083)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.4+29.sha-db56cf1"
+                version: "~11.0.0-next.4+31.sha-d3bd00b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
