001e2b004bf5c26516191e98488228074a194a34
Joey Perrott <josephperrott@gmail.com>
2020-10-02 18:22:28 +0000

    904adb72d2 feat(language-service): add quick info for inline templates in ivy (#39060)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.4+31.sha-d3bd00b"
+                version: "~11.0.0-next.4+33.sha-904adb7"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
