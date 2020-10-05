1bae85431977e8b615ccb2255872ccc60b82c7d4
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:46:45 +0000

    82d54fe8c3 feat(zone.js): add jest fakeTimers support (#39016)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.4+40.sha-26afcdd"
+                version: "~11.0.0-next.4+43.sha-82d54fe"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
