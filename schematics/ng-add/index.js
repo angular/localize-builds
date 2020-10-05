1e44e20dd5d6d774a69d8c712ab050e1f3070d23
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:53:23 +0000

    bc69182bdd docs: Migrate section, view encapsulation, from Component Styles topic into its own topic. (#38986)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.4+43.sha-82d54fe"
+                version: "~11.0.0-next.4+44.sha-bc69182"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
