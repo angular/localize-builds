5e28ab2eb51f899e4558ef7832f338b3cebaaa05
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:08:29 +0000

    75e90fa456 docs(common): update docs for HttpClient methods (#38878)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.4+39.sha-f315022"
+                version: "~11.0.0-next.4+41.sha-75e90fa"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
