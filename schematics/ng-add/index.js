203a12803c775d4f8b48a51aec6cdc19842a927c
Joey Perrott <josephperrott@gmail.com>
2020-10-05 15:31:46 +0000

    4c30f5135b docs(upgrade): expand upon change detection implications for downgraded components (#39100)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.4+22.sha-6791cd7"
+                version: "~10.1.4+23.sha-4c30f51"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
