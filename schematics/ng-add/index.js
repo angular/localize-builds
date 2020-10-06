a9a76a676a395474c6d31d430c5e2fc47c5bc83f
Joey Perrott <josephperrott@gmail.com>
2020-10-06 16:06:31 +0000

    9f132d0d93 docs: remove explicit boolean type in examples as TS infers it automatically (#39081)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.4+31.sha-0845d11"
+                version: "~10.1.4+33.sha-9f132d0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
