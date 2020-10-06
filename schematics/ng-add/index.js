521152238a931aa5106249010ae53ba45d158d2c
Joey Perrott <josephperrott@gmail.com>
2020-10-06 16:02:24 +0000

    6e0a8eb268 docs: add info about working with fixup commits (#39110)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.4+51.sha-21dfb33"
+                version: "~11.0.0-next.4+52.sha-6e0a8eb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
