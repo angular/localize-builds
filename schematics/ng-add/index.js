775cb1efec27f4f8aa2d79405d1d201a00e1ec46
Joey Perrott <josephperrott@gmail.com>
2020-10-06 00:33:01 +0000

    3c474ecf56 fix(common): add boolean to valid json for testing (#37893)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.4+49.sha-ee03408"
+                version: "~11.0.0-next.4+50.sha-3c474ec"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
