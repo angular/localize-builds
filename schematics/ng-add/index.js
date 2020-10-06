8e70f6498c696c3e0c2c835e6096b67e76d91a75
Joey Perrott <josephperrott@gmail.com>
2020-10-06 21:19:11 +0000

    f2fca6d58e refactor(compiler-cli): add a global autocompletion API (#39048)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.4+60.sha-aeec223"
+                version: "~11.0.0-next.4+59.sha-f2fca6d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
