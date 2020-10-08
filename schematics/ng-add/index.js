317baacbb80c50a1f047062e3c4aa8117d9f95d3
atscott <atscott01@gmail.com>
2020-10-08 16:08:25 +0000

    4604fe9ed2 feat(language-service): Add module name to directive quick info (#39121)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.5+2.sha-de66818"
+                version: "~11.0.0-next.5+4.sha-4604fe9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
