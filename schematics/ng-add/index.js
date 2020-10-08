dfd98405916ceec292dc2a9072581bf8f7a3bfb9
atscott <atscott01@gmail.com>
2020-10-08 16:59:32 +0000

    dace4aacb6 docs: fix for HttpRequest expansion panel (#39166)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.5+5.sha-63624a2"
+                version: "~11.0.0-next.5+6.sha-dace4aa"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
