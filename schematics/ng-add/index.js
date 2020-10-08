2f27d2b4bcb6f35ca40e5118b3bd2606a5265725
Joey Perrott <josephperrott@gmail.com>
2020-10-08 18:04:14 +0000

    635543e6f6 build: upgrade angular build, integration/bazel and @angular/bazel package to rule_nodejs 2.2.0

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.5+3.sha-b635f25"
+                version: "~11.0.0-next.5+7.sha-635543e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
