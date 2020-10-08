7f1f397459f71bf56d1c506c241b31d4ae0d0266
atscott <atscott01@gmail.com>
2020-10-08 16:08:03 +0000

    de66818cf6 test(compiler-cli): temporarily disable integrationtest (#39168)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.5+3.sha-01ec0f0"
+                version: "~11.0.0-next.5+2.sha-de66818"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
