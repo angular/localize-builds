1847fca4870bebb408f4cc399a025d2502487bba
atscott <atscott01@gmail.com>
2020-10-09 17:28:40 +0000

    952fd8662c fix(elements): detect matchesSelector prototype without IIFE (#37799)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.5+8.sha-52a0c6b"
+                version: "~10.1.5+10.sha-952fd86"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
