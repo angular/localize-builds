812023e615dd860021545caef293812ff2bdef76
atscott <atscott01@gmail.com>
2020-10-09 15:59:30 +0000

    52a0c6b36e fix(compiler): incorrectly encapsulating @import containing colons and semicolons (#38716)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.5+7.sha-8913aee"
+                version: "~10.1.5+8.sha-52a0c6b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
