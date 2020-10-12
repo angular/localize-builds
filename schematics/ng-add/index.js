ceee47fc8150d86288a66c28ca2bf8da653b4d6f
atscott <atscott01@gmail.com>
2020-10-12 19:27:32 +0000

    96dd78e0c6 fix(dev-infra): detect all commit message keywords that can close a PR (#39229)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.5+43.sha-c76dda6"
+                version: "~11.0.0-next.5+45.sha-96dd78e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
