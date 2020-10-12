dd4f715e169247093808d4e9e047143034289d38
atscott <atscott01@gmail.com>
2020-10-12 20:20:05 +0000

    b0b4953fd6 fix(router): Allow undefined inputs on routerLink (#39151)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.5+46.sha-72f4497"
+                version: "~11.0.0-next.5+47.sha-b0b4953"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
