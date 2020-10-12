caad389f2d77b776b97b6025f679499bedd2b6b8
atscott <atscott01@gmail.com>
2020-10-12 16:03:45 +0000

    4cc5982e58 docs: add linkable section about updating commit messages in `CONTRIBUTING.md` (#39215)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.5+19.sha-a84976f"
+                version: "~11.0.0-next.5+20.sha-4cc5982"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
