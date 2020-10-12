b1e8638ce85af312197e30c96c7fa878235697b9
atscott <atscott01@gmail.com>
2020-10-12 20:04:06 +0000

    3487d5a13a docs: move Inputs and Outputs topic to Components section (#38984)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.5+17.sha-1efc75c"
+                version: "~10.1.5+18.sha-3487d5a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
