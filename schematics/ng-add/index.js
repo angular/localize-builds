32d19e7196d6fc11e5d9c34d6e8192ca6a93ae61
atscott <atscott01@gmail.com>
2020-10-13 23:16:19 +0000

    8fd25d9614 docs: remove IE10 references from comments in the code (#39090)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.5+55.sha-584f37c"
+                version: "~11.0.0-next.5+62.sha-8fd25d9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
