1f3db195e4c3e0da8d1ef09715995ef38008e759
atscott <atscott01@gmail.com>
2020-10-13 19:22:22 +0000

    0875fd2360 feat(core): create internal Trusted Types module (#39207)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.5+49.sha-5ce71e0"
+                version: "~11.0.0-next.5+51.sha-0875fd2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
