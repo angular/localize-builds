43007c74275a8587bdb641bea106018d512b5b9f
atscott <atscott01@gmail.com>
2020-10-14 17:01:54 +0000

    898be92f70 perf(ngcc): do not rescan program source files when referenced from multiple root files (#39254)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.5+62.sha-8fd25d9"
+                version: "~11.0.0-next.5+68.sha-898be92"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
