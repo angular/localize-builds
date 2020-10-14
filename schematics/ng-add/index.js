b79f9f4f805b2f15902fff9a4a718a8ee1aac539
atscott <atscott01@gmail.com>
2020-10-14 21:34:58 +0000

    cdf642b609 docs: update marketing home loved-by-millions svg (#39105)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+7.sha-aee2d3f"
+                version: "~11.0.0-next.6+1.sha-cdf642b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
