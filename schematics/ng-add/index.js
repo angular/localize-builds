7182d1a9b96db305082083d95df7904ffa82f26d
Andrew Kushnir <akushnir@google.com>
2020-10-15 18:43:48 +0000

    14f15ecd38 ci: add jessicajaniuk to require-minimum-review reviewers (#39268)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+14.sha-56ab6d5"
+                version: "~11.0.0-next.6+15.sha-14f15ec"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
