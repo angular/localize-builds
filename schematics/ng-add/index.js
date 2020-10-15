c09fc5beae08b925f9bd6b63a3f44a6e33e2e211
Andrew Kushnir <akushnir@google.com>
2020-10-15 23:31:13 +0000

    5bda62c51d refactor(language-service): Return directive defs when input name is part of selector (#39243)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+22.sha-7669827"
+                version: "~11.0.0-next.6+24.sha-5bda62c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
