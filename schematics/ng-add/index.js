c414bbbad8d089e7c905848d23961f53d7440a3e
Andrew Kushnir <akushnir@google.com>
2020-10-16 19:58:52 +0000

    d2ea9b05d8 test(language-service): Make project service a singleton (#39308)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+30.sha-a0fde29"
+                version: "~11.0.0-next.6+32.sha-d2ea9b0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
