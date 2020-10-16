089fa2d9aa6b848d9e1ee91e4b916c8b0a07fa19
Andrew Kushnir <akushnir@google.com>
2020-10-16 23:32:35 +0000

    8a744d5246 refactor(docs-infra): fix `strictTemplates` failures in `accessibility` docs example (#39248)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+33.sha-6c9c70a"
+                version: "~11.0.0-next.6+34.sha-8a744d5"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
