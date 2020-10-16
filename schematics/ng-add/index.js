3af635924cac45f88ae55fd0b2d6358148e6c3e4
Andrew Kushnir <akushnir@google.com>
2020-10-16 17:31:03 +0000

    4dc8d83d84 docs: edit attribute-binding doc and move colSpan note to property binding (#38860)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+34.sha-34b74ce"
+                version: "~11.0.0-next.6+36.sha-4dc8d83"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
