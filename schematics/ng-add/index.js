acc8162874e883652426b27f97489ede4e6684d2
Andrew Kushnir <akushnir@google.com>
2020-10-16 22:14:02 +0000

    b5ec5a7fca ci: separate the windows CI tests into build and test (#39289)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+39.sha-563fb6c"
+                version: "~11.0.0-next.6+42.sha-b5ec5a7"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
