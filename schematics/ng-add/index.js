09ddb9c320fb26aa6fdfbf60a2c934bb06d2353e
Andrew Kushnir <akushnir@google.com>
2020-10-19 15:29:40 +0000

    d30911c4eb docs: add Johannes Hoppe to GDE resources (#34694)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+34.sha-8a744d5"
+                version: "~11.0.0-next.6+36.sha-d30911c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
