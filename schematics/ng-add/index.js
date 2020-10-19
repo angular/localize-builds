b8c97da2f787a18b434994e9c9a6a432cc775ef0
Andrew Kushnir <akushnir@google.com>
2020-10-19 23:49:10 +0000

    cd8ec3d902 docs: add Jessica Janiuk to contributors.json (#39253)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.6+27.sha-02b8750"
+                version: "~10.1.6+28.sha-cd8ec3d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
