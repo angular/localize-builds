0cb98abe8c9ff232bbb11fa0c6d9a9ae65ee8000
Andrew Kushnir <akushnir@google.com>
2020-10-19 21:57:24 +0000

    41c2228d49 docs: clarify grammatical error (#39279)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+53.sha-f5a3e44"
+                version: "~11.0.0-next.6+52.sha-41c2228"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
