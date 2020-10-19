3bf77219228b1ae2d632a343f3e12e7d835c7ea2
Andrew Kushnir <akushnir@google.com>
2020-10-19 21:52:42 +0000

    f5a3e44e9d refactor(compiler): remove support for TypeScript 3.9 (#39313)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+51.sha-767fdcc"
+                version: "~11.0.0-next.6+53.sha-f5a3e44"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
