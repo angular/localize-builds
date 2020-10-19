2ee3e1f0e286633320b96008682d2375a9401c67
Andrew Kushnir <akushnir@google.com>
2020-10-19 19:05:31 +0000

    7e742aea7c refactor(compiler-cli): linker - add Babel plugin, FileLinker and initial PartialLinkers (#39116)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+49.sha-b304bd0"
+                version: "~11.0.0-next.6+50.sha-7e742ae"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
