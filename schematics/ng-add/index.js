5f2be14442f98ff1535fa7da35e6d92db16926c6
Andrew Kushnir <akushnir@google.com>
2020-10-19 18:48:03 +0000

    7827d3ee73 docs: change definition of NgModules in Angular concepts guide (#36179)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.1.6+24.sha-38db976"
+                version: "~10.1.6+25.sha-7827d3e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
