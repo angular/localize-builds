7b974f8bf76790e0cebf660a2a4d970c9502ae73
Andrew Kushnir <akushnir@google.com>
2020-10-20 20:47:27 +0000

    a7964f4eca fix(common): update locales using new CLDR data (#39343)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+57.sha-a67895c"
+                version: "~11.0.0-next.6+59.sha-a7964f4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
