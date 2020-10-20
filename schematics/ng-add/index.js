b683e8ce5ef16b25c5e8306ef60e5b57f1702b38
Andrew Kushnir <akushnir@google.com>
2020-10-20 20:59:07 +0000

    9fb7bdea89 fix(router): incorrect signature for createUrlTree (#39347)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+59.sha-a7964f4"
+                version: "~11.0.0-next.6+60.sha-9fb7bde"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
