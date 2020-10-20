9f3ea1fe7d04b12771eea11373748476c5d6f229
Andrew Kushnir <akushnir@google.com>
2020-10-20 23:55:21 +0000

    cfac8e0764 build: upgrade karma to version 4.4.0 (#39180)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+60.sha-9fb7bde"
+                version: "~11.0.0-next.6+61.sha-cfac8e0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
