dc3aaa788ba608a516ea09e8ec31e0ee4973d4a5
Andrew Kushnir <akushnir@google.com>
2020-10-21 17:02:20 +0000

    b4e8399144 fix(platform-server): Resolve absolute URL from baseUrl (#39334)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+54.sha-d8c9f42"
+                version: "~11.0.0-next.6+55.sha-b4e8399"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
