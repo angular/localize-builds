dd51d4ee45532af1a0087bbd57b76409a531b037
Andrew Kushnir <akushnir@google.com>
2020-10-21 15:54:30 +0000

    ef2d07fdd1 docs: remove `style` commit type from CONTRIBUTING.md (#39357)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+61.sha-cfac8e0"
+                version: "~11.0.0-next.6+64.sha-ef2d07f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
