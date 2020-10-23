220a0f4dc4d4255e240c9e2ab579b2a5ab1d3ed4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-23 18:00:00 +0000

    b989ba2502 fix(core): update expected size for cli-hello-world-ivy-i18n integration test (#39402)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+81.sha-08f3d62"
+                version: "~11.0.0-next.6+95.sha-b989ba2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
