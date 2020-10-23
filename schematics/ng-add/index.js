632d2de79aaf62ffcda1dcd87b5af22ebe25536a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-23 22:48:56 +0000

    6669571884 fix(zone.js): remove global declaration (#37861)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-rc.0+1.sha-7f79e77"
+                version: "~11.0.0-rc.0+29.sha-6669571"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
