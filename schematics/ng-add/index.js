28d912aa285ddd943e2239b1f261860f61fb75e1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:19:10 +0000

    57ead7aa53 fix(localize): serialize all the message locations to XLIFF (#39411)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+105.sha-3236ae0"
+                version: "~11.0.0-next.6+104.sha-57ead7a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
