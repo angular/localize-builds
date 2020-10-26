d9b346020e35bce53096d962a58f16114912b4cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:17:48 +0000

    1db4f212e5 docs: improve DefaultValueAccessor directive docs (#39404)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+104.sha-57ead7a"
+                version: "~11.0.0-next.6+101.sha-1db4f21"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
