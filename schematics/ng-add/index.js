75dc8b59ceda70f54995a733a224d16d686e7eba
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-26 18:13:33 +0000

    35fdc6d92f build(docs-infra): ignore `doc.basePath` when generating keywords for each document (#39409)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+100.sha-cf60e0e"
+                version: "~11.0.0-next.6+103.sha-35fdc6d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
