bb4a48e954f7ff056b650aa3fee69c3a4c1e419c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-27 22:19:09 +0000

    e649f1dda6 ci: bust cache of stored node_modules on CircleCI (#39461)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+102.sha-2b09f5b"
+                version: "~11.0.0-next.6+130.sha-e649f1d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
