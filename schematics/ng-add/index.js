cc6aa1c4cdf61d01f87c99e8be93b38f9b8e7b27
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 18:18:39 +0000

    b9358c2ad5 docs: add carlos caballero to GDE resources (#37976)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+132.sha-e6ca3d3"
+                version: "~11.0.0-next.6+133.sha-b9358c2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
