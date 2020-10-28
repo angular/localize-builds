a2d35f589cff5144fb26eee63a1111986280eaae
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-28 17:28:36 +0000

    e6ca3d3841 refactor(core): add top 10 runtime error codes (#39188)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+131.sha-0723331"
+                version: "~11.0.0-next.6+132.sha-e6ca3d3"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
