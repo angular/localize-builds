52828902e3b1948b3b0c02b49e9f0294b7f9c32a
Joey Perrott <josephperrott@gmail.com>
2020-10-29 23:51:17 +0000

    c83b2ad87f docs: tView.preOrderHooks and tView.preOrderCheckHooks docs update (#39497)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+163.sha-4a8d5ae"
+                version: "~11.0.0-next.6+165.sha-c83b2ad"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
