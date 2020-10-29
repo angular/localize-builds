f094608059ef7851031ddae04ca25a11dc6b3409
Joey Perrott <josephperrott@gmail.com>
2020-10-29 20:06:16 +0000

    e3365724f2 feat(compiler): recover expression parsing in more malformed pipe cases (#39437)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+158.sha-8d324ec"
+                version: "~11.0.0-next.6+159.sha-e336572"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
