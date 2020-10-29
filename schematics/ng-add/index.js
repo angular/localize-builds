6b85b643738cfab6f94b3c04020c644529c81bc5
Joey Perrott <josephperrott@gmail.com>
2020-10-29 21:19:21 +0000

    17070af417 build: update to CLI 11.0.0-rc.1 (#39432)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+159.sha-e336572"
+                version: "~11.0.0-next.6+161.sha-17070af"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
