6bbf64472b47771dc347f539f037130b0c07d7a8
Joey Perrott <josephperrott@gmail.com>
2020-10-29 18:41:07 +0000

    e7f262d691 docs(forms): update ngModel documentation (#39481)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-rc.1+8.sha-8ea0ee9"
+                version: "~11.0.0-rc.1+9.sha-e7f262d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
