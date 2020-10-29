dc8af5af79d4c9e47786132ee429e80f205ac907
Joey Perrott <josephperrott@gmail.com>
2020-10-29 19:48:28 +0000

    ec416d0210 ci: update NgBot config to include more labels that indicate that ticket is triaged (#39494)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-rc.1+9.sha-e7f262d"
+                version: "~11.0.0-rc.1+10.sha-ec416d0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
