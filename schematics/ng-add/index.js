e8d563ce04608e61ae6935fb508135d48592a7d1
Joey Perrott <josephperrott@gmail.com>
2020-10-29 23:05:10 +0000

    170af0740d release: cut the zone.js-0.11.3 release (#39440)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+161.sha-17070af"
+                version: "~11.0.0-next.6+162.sha-170af07"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
