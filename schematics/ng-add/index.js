971a641f6222e18a93266e32fb7ef625f15291fe
Joey Perrott <josephperrott@gmail.com>
2020-10-29 17:20:19 +0000

    8ea0ee918d fix(dev-infra): update generated ng-dev compiled output (#39474)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-rc.1+4.sha-eccf9d6"
+                version: "~11.0.0-rc.1+8.sha-8ea0ee9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
