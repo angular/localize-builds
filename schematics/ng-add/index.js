2913d3b934f8c4fb4482d4f8ac9238b999c650c8
Joey Perrott <josephperrott@gmail.com>
2020-10-30 23:44:51 +0000

    3af585c004 fix(router): Ensure all outlets are used when commands have a prefix (#39456)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+170.sha-f12157c"
+                version: "~11.0.0-next.6+172.sha-3af585c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
