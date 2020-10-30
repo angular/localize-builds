5da6422cbe3f68f4fa5066caad9c5303ffd29736
Joey Perrott <josephperrott@gmail.com>
2020-10-30 20:04:39 +0000

    eb625b993e docs: update CLI workspace config documentation (#39427)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-rc.1+18.sha-a8e0db7"
+                version: "~11.0.0-rc.1+19.sha-eb625b9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
