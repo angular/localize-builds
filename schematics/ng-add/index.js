83159bcb4e6590af0f58a815c6ce749d08cb37f0
Joey Perrott <josephperrott@gmail.com>
2020-10-30 17:04:31 +0000

    3241d922fc refactor(compiler): parse bindings "by hand" rather than via regex (#39375)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+165.sha-c83b2ad"
+                version: "~11.0.0-next.6+166.sha-3241d92"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
