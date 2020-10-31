e81b9327b6b5ba4f5ce56388d883fd10e8b6ec91
Joey Perrott <josephperrott@gmail.com>
2020-10-31 01:26:40 +0000

    71d0063285 fix(compiler-cli): report missing pipes when `fullTemplateTypeCheck` is disabled (#39320)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.2.1+14.sha-85d5242"
+                version: "~10.2.1+15.sha-71d0063"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
