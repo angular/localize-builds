98563a6a7df750d35144f668a32e99a2d4431f0f
Joey Perrott <josephperrott@gmail.com>
2020-11-02 22:02:13 +0000

    3cab823222 docs(bazel): Mention Alex Eagle's article on migration doc (#39507)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+188.sha-5f1e975"
+                version: "~11.0.0-next.6+189.sha-3cab823"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
