7acffc530180da8c35edd966c973088a1a5de2df
Joey Perrott <josephperrott@gmail.com>
2020-11-02 21:01:38 +0000

    5f1e9758f5 build(docs-infra): ensure that deployment works on CI (#39535)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+187.sha-ad62eda"
+                version: "~11.0.0-next.6+188.sha-5f1e975"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
