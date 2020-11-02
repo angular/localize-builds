ace4050c9b8531ed864a3d1e0581a559ff031a58
Joey Perrott <josephperrott@gmail.com>
2020-11-02 16:29:37 +0000

    cf48d508af build(docs-infra): upgrade `firebase-tools` to v8.14.1 (#39470)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+173.sha-3b0b7d2"
+                version: "~11.0.0-next.6+184.sha-cf48d50"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
