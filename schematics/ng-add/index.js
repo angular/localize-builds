0a63566ac18289e08bf19c13eeefb38342d5dcff
Joey Perrott <josephperrott@gmail.com>
2020-11-03 23:16:56 +0000

    7e33cb9626 fix(compiler-cli): generating invalid setClassMetadata call in ES5 for class with custom decorator (#39527)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+190.sha-db1c48f"
+                version: "~11.0.0-next.6+192.sha-7e33cb9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
