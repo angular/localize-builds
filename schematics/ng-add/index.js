cf20196e74248b4322fac0a7de46dd58b32bed04
Joey Perrott <josephperrott@gmail.com>
2020-11-03 01:02:49 +0000

    b6064c2782 fix(dev-infra): run fetches in the git client in quiet mode (#39503)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-rc.1+35.sha-a0b6548"
+                version: "~11.0.0-rc.1+37.sha-b6064c2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
