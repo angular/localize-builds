59ba1dcc14bec85430352855b99a0f006fe04377
Joey Perrott <josephperrott@gmail.com>
2020-11-03 22:12:21 +0000

    aa3bd27cbf build: update dev dependency on @angular/cli to 10.2.0 (#39553)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.2.1+15.sha-71d0063"
+                version: "~10.2.1+29.sha-aa3bd27"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
