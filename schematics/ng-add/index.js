3a407f544ff7118168cd11e46c7477c3e417967a
Joey Perrott <josephperrott@gmail.com>
2020-11-04 19:21:28 +0000

    43b4940c9d fix(forms): remove validators while cleaning up a control (#39234)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-rc.1+42.sha-450830c"
+                version: "~11.0.0-rc.1+44.sha-43b4940"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
