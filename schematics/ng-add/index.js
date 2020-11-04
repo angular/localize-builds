47c08ae556555e794192a3536c7253844026ed53
Joey Perrott <josephperrott@gmail.com>
2020-11-04 00:58:15 +0000

    7ed0f0f7d0 fix(dev-infra): run caretaker checks asyncronously (#39086)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~10.2.1+30.sha-250858a"
+                version: "~10.2.1+32.sha-7ed0f0f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
