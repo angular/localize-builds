8d1a1fdcdabb87e0db8838d3acb1b5ee50af941f
Joey Perrott <josephperrott@gmail.com>
2020-11-04 21:14:27 +0000

    0aa53cdd60 docs: fix typo in the "Property Binding" guide (#39569) PR Close #39569

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+206.sha-3701376"
+                version: "~11.0.0-next.6+208.sha-0aa53cd"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
