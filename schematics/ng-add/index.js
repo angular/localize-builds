f801b11d708359df9d531b3a1790f139bd73d1b7
Misko Hevery <misko@hevery.com>
2020-11-05 19:51:20 +0000

    27358eb60f feat(zone.js): monkey patches queueMicrotask() (#38904)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+209.sha-6138bc2"
+                version: "~11.0.0-next.6+210.sha-27358eb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
