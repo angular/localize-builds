c73828fa1a1b4940353f4adf6d20897167a2ef88
Misko Hevery <misko@hevery.com>
2020-11-06 17:29:00 +0000

    1f956184c4 fix(compiler): skipping leading whitespace should not break placeholder source-spans (#39486)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+210.sha-27358eb"
+                version: "~11.0.0-next.6+214.sha-1f95618"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
