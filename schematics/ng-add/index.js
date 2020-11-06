455607ce63c1dcd000fc2df7d48f952913738569
Misko Hevery <misko@hevery.com>
2020-11-06 17:58:57 +0000

    dd28855455 fix(elements): update the view of an `OnPush` component when inputs change (#39452)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-rc.2+5.sha-b8e9b3d"
+                version: "~11.0.0-rc.2+14.sha-dd28855"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
