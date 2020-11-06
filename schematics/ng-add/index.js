1cd3cad6f0591532841e015cb55874f9ae6906d6
Misko Hevery <misko@hevery.com>
2020-11-06 17:59:15 +0000

    290ea57a93 fix(core): Access injected parent values using SelfSkip (#39464)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+217.sha-b9b9178"
+                version: "~11.0.0-next.6+218.sha-290ea57"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
