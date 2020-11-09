e967507ae941bc1802d016aaffea817e063f9fcf
Misko Hevery <misko@hevery.com>
2020-11-09 16:11:23 +0000

    48d15fc760 docs: update cli deprecations (#39547)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+229.sha-0edf6fc"
+                version: "~11.0.0-next.6+227.sha-48d15fc"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
