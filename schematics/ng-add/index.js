ebccd49e6b9a3ed3a77c4f289b09842c58daf20f
atscott <atscott01@gmail.com>
2020-11-12 20:03:25 +0000

    20db90a41d fix(core): not inserting ViewContainerRef nodes when inside root of a component (#39599)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0+6.sha-050cea9"
+                version: "~11.0.0+7.sha-20db90a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
