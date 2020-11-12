f353753df7953f60cf5532dd1d7271876c4ae703
atscott <atscott01@gmail.com>
2020-11-12 22:25:43 +0000

    ade6da95e4 perf(compiler-cli): reduce filesystem hits during resource resolution (#39604)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+245.sha-8e17dc0"
+                version: "~11.0.0-next.6+247.sha-ade6da9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
