2d4549a1b6d4205d6fc78300fdedddc5e221b003
atscott <atscott01@gmail.com>
2020-11-12 17:28:27 +0000

    d1355caa74 docs: added missing slash to close tag (#39626) PR Close #39626

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+235.sha-3be344c"
+                version: "~11.0.0-next.6+237.sha-d1355ca"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
