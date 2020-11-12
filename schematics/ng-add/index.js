9f69e176b46278b251103d92e11a19132de94f53
atscott <atscott01@gmail.com>
2020-11-12 22:20:31 +0000

    7ce825011b docs: archive di-navtree doc (#39543)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+244.sha-c8ed497"
+                version: "~11.0.0-next.6+243.sha-7ce8250"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
