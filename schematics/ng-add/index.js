9a2edab1d9d6a7e7580aa40e51b6ebcdb00c81f9
atscott <atscott01@gmail.com>
2020-11-12 22:17:18 +0000

    f8956adb73 build: update zone.js version to 0.11.3 (#39317)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+241.sha-a872362"
+                version: "~11.0.0-next.6+242.sha-f8956ad"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
