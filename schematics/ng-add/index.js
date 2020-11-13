bc265b64328d27ac64224caa0f1cba1a9bd7d75a
atscott <atscott01@gmail.com>
2020-11-13 19:52:49 +0000

    9e53c6ee40 docs(docs-infra): remove link to constructor article (#39670)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+257.sha-eea5d6b"
+                version: "~11.0.0-next.6+251.sha-9e53c6e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
