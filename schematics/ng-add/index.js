1ce307016f2743c3e4f3ed30a0bf38e873f7320b
atscott <atscott01@gmail.com>
2020-11-13 22:04:59 +0000

    d68cac69ed ci: update angular-robot config for L2 triage of PRs (#39655)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+261.sha-24e13e0"
+                version: "~11.0.0-next.6+262.sha-d68cac6"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
