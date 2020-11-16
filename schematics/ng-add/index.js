2e1d95e5acd654ad7094206a2e52ce75d667b96f
atscott <atscott01@gmail.com>
2020-11-16 18:01:15 +0000

    e05cfdd367 fix(compiler-cli): incorrectly type checking calls to implicit template variables (#39686)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0+20.sha-7f3314a"
+                version: "~11.0.0+24.sha-e05cfdd"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
