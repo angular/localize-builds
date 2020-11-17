2ca5a0a57dc9a29e15ef3bdd3a6f8f2c0670712e
atscott <atscott01@gmail.com>
2020-11-17 21:37:47 +0000

    39266654e6 fix(http): queue jsonp <script> tag onLoad event handler in microtask (#39512)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+290.sha-49410f8"
+                version: "~11.0.0-next.6+289.sha-3926665"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
