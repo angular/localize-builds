24e0f07cab841cd4311e9b4b8deea1ae00ac12e2
atscott <atscott01@gmail.com>
2020-11-17 18:39:58 +0000

    882ff8fc37 perf(compiler): use raw bytes to represent utf-8 encoded strings (#39694)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0+35.sha-9e87fc8"
+                version: "~11.0.0+37.sha-882ff8f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
