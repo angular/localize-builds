ec833540d618cafb712afd792dd286292311362b
atscott <atscott01@gmail.com>
2020-11-17 23:09:49 +0000

    d39c4bbe37 refactor(language-service): language_service_adapter -> adapters (#39619)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+289.sha-3926665"
+                version: "~11.0.0-next.6+292.sha-d39c4bb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
