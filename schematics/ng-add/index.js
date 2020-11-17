8760adbce787a21b68dea5c878d98c3aadcc23d3
atscott <atscott01@gmail.com>
2020-11-17 20:20:29 +0000

    806d7aa22c docs: add documentation for ngNonBindable (#36560)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+283.sha-209a18c"
+                version: "~11.0.0-next.6+284.sha-806d7aa"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
