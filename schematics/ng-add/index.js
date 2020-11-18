f48b44db93d38d796c2d3e193dc0224c15820f61
atscott <atscott01@gmail.com>
2020-11-18 00:39:48 +0000

    00291ae054 docs: fix dead CSRF link (#35929)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+292.sha-d39c4bb"
+                version: "~11.0.0-next.6+293.sha-00291ae"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
