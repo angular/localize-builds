ba1fdf222a0629aee3f4c9f05a26b74c7c58405d
atscott <atscott01@gmail.com>
2020-11-18 17:40:11 +0000

    7d68f92315 docs(core): fix directive.ts preserving whitespace example comment (#37377)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+297.sha-e4028ae"
+                version: "~11.0.0-next.6+296.sha-7d68f92"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
