e8fff4d1cc1b5ba4fc7627c382eb95f9948d01c0
atscott <atscott01@gmail.com>
2020-11-18 17:40:01 +0000

    e4028ae5c4 build(docs-infra): compute previous version list (#39689)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+295.sha-b33b89d"
+                version: "~11.0.0-next.6+297.sha-e4028ae"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
