ce6fa04441e0cb9c00a3cde3237b1d73bf050260
atscott <atscott01@gmail.com>
2020-11-18 17:33:15 +0000

    0ab833f5c2 docs: meetup url to https (#33713)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0-next.6+293.sha-00291ae"
+                version: "~11.0.0-next.6+294.sha-0ab833f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
