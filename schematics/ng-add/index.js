599e82f73015b6c53fa0742c4a1c296f81025b43
atscott <atscott01@gmail.com>
2020-11-18 17:50:33 +0000

    5d8b5eb97b refactor(core): Replace non-null assertion operator with property initialization (#39730)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.0+42.sha-724dc93"
+                version: "~11.0.0+46.sha-5d8b5eb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
