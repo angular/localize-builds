d70f020adcf26784724ed3c772b50f7c61019c04
Andrew Kushnir <akushnir@google.com>
2020-11-19 00:34:41 +0000

    5b45fab19d build: update to latest "yargs" package (#39749)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+13.sha-d001ecc"
+                version: "~11.1.0-next.0+14.sha-5b45fab"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
