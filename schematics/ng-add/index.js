82fa182c078184a7bb37fd69ff4683555bff92a6
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:36:38 +0000

    935cf433ed fix(docs-infra): support recovering from unrecoverable SW states (#39651)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+22.sha-066126a"
+                version: "~11.1.0-next.0+21.sha-935cf43"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
