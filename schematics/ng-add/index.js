28cab2a4beb3ce2687c88642a118e8ba1da44bdd
Andrew Kushnir <akushnir@google.com>
2020-11-19 00:14:40 +0000

    b521b5eb39 build(docs-infra): update @angular/material to 11.0.0 (#39600)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+5.sha-dc697d3"
+                version: "~11.1.0-next.0+11.sha-b521b5e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
