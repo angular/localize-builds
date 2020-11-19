dfcf1970f013decfe742b4f8be56d07d14a603a1
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:42:37 +0000

    a965589eb8 feat(core): adds get method to QueryList (#36907)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+24.sha-1eb4066"
+                version: "~11.1.0-next.0+31.sha-a965589"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
