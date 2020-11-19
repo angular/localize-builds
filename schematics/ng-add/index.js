93d6cb8742ce10d67a664b1434498ab075878366
Andrew Kushnir <akushnir@google.com>
2020-11-19 20:38:05 +0000

    066126ae2f fix(core): remove duplicated noop function (#39761)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+18.sha-0e85938"
+                version: "~11.1.0-next.0+22.sha-066126a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
