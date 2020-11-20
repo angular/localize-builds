ce90d91958dd42d6857a30ccf55cd59983df4bfb
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:51:15 +0000

    3114b0aa12 fix(core): meta addTag() adds incorrect attribute for httpEquiv (#32531)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.2+28.sha-50c19a2"
+                version: "~11.0.2+31.sha-3114b0a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
