570ed5bac58dcff91a08c4bac455453e062cce08
Andrew Kushnir <akushnir@google.com>
2020-11-20 17:07:55 +0000

    c33a8235d9 fix(router): correctly handle string command in outlets (#39728)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+39.sha-1cba56e"
+                version: "~11.1.0-next.0+43.sha-c33a823"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
