a25184fe73ee3a08523e3f2e3881fb6170aa60bc
Andrew Kushnir <akushnir@google.com>
2020-11-20 16:59:31 +0000

    66d863febe fix(animations): getAnimationStyle causes exceptions in older browsers (#29709)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+36.sha-fe1ada8"
+                version: "~11.1.0-next.0+38.sha-66d863f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
