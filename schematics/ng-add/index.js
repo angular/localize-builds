b4484c3dbb243be2238d4da6eec06ecd29db800e
Andrew Kushnir <akushnir@google.com>
2020-11-20 21:37:14 +0000

    3e1e5a15ba docs: update links to use HTTPS as protocol (#39718)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+46.sha-ff0a90e"
+                version: "~11.1.0-next.0+51.sha-3e1e5a1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
