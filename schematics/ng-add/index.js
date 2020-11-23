de268a3e3bc504f0c9a7bbbe762e07a51cc44f18
Andrew Kushnir <akushnir@google.com>
2020-11-23 23:23:44 +0000

    7bf73d7442 fix(service-worker): correctly handle failed cache-busted request (#39786)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.2+42.sha-7e52be3"
+                version: "~11.0.2+43.sha-7bf73d7"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
