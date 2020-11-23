b02963fe46a9cca3364d310613b98b231155dbe2
Andrew Kushnir <akushnir@google.com>
2020-11-23 17:46:14 +0000

    938abc03bc docs(http): Add mention to default value 'OK' for statusText. #23334 (#23494)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+51.sha-3e1e5a1"
+                version: "~11.1.0-next.0+60.sha-938abc0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
