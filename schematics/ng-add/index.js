470cd870ac542802b22facf04a2321c18d8a9745
Andrew Kushnir <akushnir@google.com>
2020-11-24 21:31:45 +0000

    266959e745 docs: added Srashti Jain to GDE resources (#39150)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+82.sha-e75244e"
+                version: "~11.1.0-next.0+73.sha-266959e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
