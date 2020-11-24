27f306279ad4e37ca58b762563f3a116e1fcdd81
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:07:41 +0000

    b5c0f9d7d1 feat(platform-browser): add doubletap HammerJS support (#26362)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+66.sha-88fbc06"
+                version: "~11.1.0-next.0+69.sha-b5c0f9d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
