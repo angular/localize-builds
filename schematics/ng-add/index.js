9ac5b07c77903f3550ed078c9fc2a4e8d2cd8dd5
Andrew Kushnir <akushnir@google.com>
2020-11-24 23:43:36 +0000

    9dc0a4edf6 test(docs-infra): disable the Selenium Promise Manager in docs examples e2e tests (#39818)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.2+51.sha-75b5369"
+                version: "~11.0.2+56.sha-9dc0a4e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
