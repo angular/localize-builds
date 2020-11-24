d399a59f97ab2345591233fb8e367328da4a7701
Andrew Kushnir <akushnir@google.com>
2020-11-24 22:48:31 +0000

    8758006acc refactor(language-service): create getReferencesAtPosition stubs (#39829)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+83.sha-3a6e7b5"
+                version: "~11.1.0-next.0+84.sha-8758006"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
