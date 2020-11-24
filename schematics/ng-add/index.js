5367a079e74f029e7203727eac21fe863650c03e
Andrew Kushnir <akushnir@google.com>
2020-11-24 17:04:18 +0000

    2c275f6d7a docs: archive template expression operators doc, move pipes precedence section (#39170)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+64.sha-6046419"
+                version: "~11.1.0-next.0+67.sha-2c275f6"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
