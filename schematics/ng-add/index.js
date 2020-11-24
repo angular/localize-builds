0ad70a851638854f1dd6a3cfcb8045dd7f518a50
Andrew Kushnir <akushnir@google.com>
2020-11-24 21:33:12 +0000

    e75244ec00 feat(compiler-cli): support for partial compilation of components (#39707)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+72.sha-39a47c2"
+                version: "~11.1.0-next.0+82.sha-e75244e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
