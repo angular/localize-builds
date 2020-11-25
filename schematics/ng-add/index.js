8e812f1709488d078ffce722b640592614fb9a38
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 19:27:35 +0000

    2bfa8eec7a docs: make MDN link locale-indepenent (#39842)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.2+56.sha-9dc0a4e"
+                version: "~11.0.2+59.sha-2bfa8ee"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
