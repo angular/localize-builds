54c408ea6bd307daa31d8291afb2868c0651a62e
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 19:39:01 +0000

    4def19f39f test(compiler-cli): add TypeScript 4.1 typings integration test (#39571)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+96.sha-2786292"
+                version: "~11.1.0-next.0+99.sha-4def19f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
