e135716f2f2b4d16bf41f527db69122e2561a71f
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 21:19:53 +0000

    ba6945bfa9 docs(docs-infra): display angulararchitects.io in trainings (#37139)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+99.sha-4def19f"
+                version: "~11.1.0-next.0+101.sha-ba6945b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
