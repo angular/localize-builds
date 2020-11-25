eea5d839353f391701f1ba8e12a3f670e3b30af2
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-25 23:11:53 +0000

    68d4a74411 fix(core): Ensure OnPush ancestors are marked dirty when events occur (#39833)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+103.sha-e148382"
+                version: "~11.1.0-next.0+104.sha-68d4a74"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
