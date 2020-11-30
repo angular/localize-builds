762f184ac4a5c3cf876e95e010dae97adeffc9f7
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 23:30:12 +0000

    1de59d2818 docs: edit step 2 of getting started start-routing.md (#39593)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+114.sha-dd3ed3c"
+                version: "~11.1.0-next.0+115.sha-1de59d2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
