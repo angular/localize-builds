22a9daa09b3a919304429249ffc5127d39654e09
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:40:24 +0000

    df97fbc077 docs: added a missing ` in inputs-outputs.md (#39856)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+105.sha-93d689f"
+                version: "~11.1.0-next.0+107.sha-df97fbc"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
