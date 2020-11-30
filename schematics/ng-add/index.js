1926d465a705a189110b3297ed973d760898f039
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:00:03 +0000

    c622b17b0d test(compiler-cli): convert di compliance tests (#39863)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+106.sha-9a2f83b"
+                version: "~11.1.0-next.0+110.sha-c622b17"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
