c375d956d324c0fe2a2c91a9b0fdedf34b042500
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:30:02 +0000

    1539c64fb0 docs(common): change HTTPResponse to HttpResponse (#39860) PR Close #39860

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+110.sha-c622b17"
+                version: "~11.1.0-next.0+112.sha-1539c64"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
