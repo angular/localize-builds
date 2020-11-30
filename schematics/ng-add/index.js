15601f0281ad4b8c20af9b75f3c65bdcedecd131
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 20:33:58 +0000

    288a0a668a docs: fix builder section of generated angular.json file for libs (#39888)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.2+68.sha-20ab5d8"
+                version: "~11.0.2+70.sha-288a0a6"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
