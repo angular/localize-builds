52b603ba3a17dfc5c5f4781f5946d2c84072adac
Jessica Janiuk <jessicajaniuk@google.com>
2020-11-30 19:36:42 +0000

    20ab5d89a0 docs: fix typo in two-way-binding guide (#39859)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.2+63.sha-01c1bfd"
+                version: "~11.0.2+68.sha-20ab5d8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
