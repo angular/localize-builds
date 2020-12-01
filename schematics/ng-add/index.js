239f2271e246bba7cc114729c9a51c41fae16f90
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 18:40:59 +0000

    0b5b24abd7 docs: edit interpolation doc (#38687)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.0+116.sha-11cd37f"
+                version: "~11.1.0-next.0+117.sha-0b5b24a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
