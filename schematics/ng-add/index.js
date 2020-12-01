41c84b9533d3e252f5112808134da98df9121ef0
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-01 00:17:19 +0000

    75e22abe74 fix(core): not invoking object's toString when rendering to the DOM (#39843)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.2+71.sha-24c0897"
+                version: "~11.0.2+72.sha-75e22ab"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
