4c1bb584df471ddbdb9e2b8d34e0aaca9e668a7f
Misko Hevery <misko@hevery.com>
2020-12-02 21:27:12 +0000

    a55e01b89c test(core): verify `onDestroy` callbacks are invoked when ComponentRef is destroyed (#39876)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+12.sha-eba185e"
+                version: "~11.1.0-next.1+20.sha-a55e01b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
