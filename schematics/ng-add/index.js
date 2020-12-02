2891dd790065ff8e674ef153f33ae5e64383716f
Misko Hevery <misko@hevery.com>
2020-12-02 21:20:21 +0000

    75fc89384d refactor(compiler-cli): expose TTC method to determine if file is tracked shim (#39768)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+6.sha-c923aaf"
+                version: "~11.1.0-next.1+18.sha-75fc893"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
