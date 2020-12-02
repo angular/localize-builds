b8f1ecdec4e0aa5d1e260a5c4dc47df166307244
Misko Hevery <misko@hevery.com>
2020-12-02 23:25:07 +0000

    8d613c1d42 feat(compiler): allow trailing comma in array literal (#22277)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+22.sha-744f46c"
+                version: "~11.1.0-next.1+25.sha-8d613c1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
