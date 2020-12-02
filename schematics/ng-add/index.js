a78832de85beeb02ebd8ede30b0c82fc078a94ee
Misko Hevery <misko@hevery.com>
2020-12-02 23:30:24 +0000

    efd0d33391 test(compiler-cli): convert bindings compliance tests (#39862)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+21.sha-790ca09"
+                version: "~11.1.0-next.1+24.sha-efd0d33"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
