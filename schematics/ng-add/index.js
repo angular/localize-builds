91130eebfc91666e6ff85c95a0f1e6a2148cfdb6
Jessica Janiuk <jessicajaniuk@google.com>
2020-12-02 19:44:10 +0000

    57642e80a6 feat(docs-infra): add maskable PWA icon (#39928)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.2+78.sha-7001ef6"
+                version: "~11.0.3+5.sha-57642e8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
