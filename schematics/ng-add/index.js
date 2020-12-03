04786fa4bfe418427c6ddf1cb1c16857917a8ea0
Misko Hevery <misko@hevery.com>
2020-12-03 22:08:19 +0000

    3b700985f0 fix(language-service): do not treat file URIs as general URLs (#39917)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+35.sha-5a3a154"
+                version: "~11.1.0-next.1+37.sha-3b70098"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
