ca5f005111ca0a6861bb820f981c8e36570c3aef
Misko Hevery <misko@hevery.com>
2020-12-03 22:08:38 +0000

    aa8bd733fa ci: update l2 triage label configuration for pull requests (#39954)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+28.sha-76ae874"
+                version: "~11.1.0-next.1+33.sha-aa8bd73"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
