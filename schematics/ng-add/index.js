19a6cc6fba01d2e62b48712dc267d78c251f94a2
Misko Hevery <misko@hevery.com>
2020-12-04 23:35:55 +0000

    d2042a0da2 refactor(compiler-cli): use semver range checking for partial versions (#39847)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+32.sha-c7c5b2f"
+                version: "~11.1.0-next.1+45.sha-d2042a0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
