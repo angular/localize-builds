f48bf3745038dd8002bc71075021783c238cce19
Misko Hevery <misko@hevery.com>
2020-12-04 23:33:11 +0000

    1e3534f74f perf(core): use `ngDevMode` to tree-shake warnings (#39959)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.3+24.sha-4aeb65d"
+                version: "~11.0.3+26.sha-1e3534f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
