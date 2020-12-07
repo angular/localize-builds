7fdbf74247393d22bb28245cdcf0f7db6d052f9c
Misko Hevery <misko@hevery.com>
2020-12-07 18:14:04 +0000

    6d19c16d32 build: remove check-env script (#39980)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+51.sha-d9356f2"
+                version: "~11.1.0-next.1+52.sha-6d19c16"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
