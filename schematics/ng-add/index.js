bc7c365b9832ce1cf0e65dd7f6c15662e9ea5c68
Misko Hevery <misko@hevery.com>
2020-12-07 18:08:33 +0000

    a8aeb74714 test(upgrade): run tests against AngularJS v1.8.x as well (#39972)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+49.sha-7954c8d"
+                version: "~11.1.0-next.1+50.sha-a8aeb74"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
