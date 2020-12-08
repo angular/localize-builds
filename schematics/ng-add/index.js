23c07841a3ec9bea955152e32f9fae9f9995eb28
Misko Hevery <misko@hevery.com>
2020-12-08 17:13:05 +0000

    6e7a57f97a docs: add note that sting based lazy loading is opt-in (#35956)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+63.sha-3ec2807"
+                version: "~11.1.0-next.1+64.sha-6e7a57f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
