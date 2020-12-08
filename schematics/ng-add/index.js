a56697f0f3b478452797b69ba98d8dae6bd2925b
Misko Hevery <misko@hevery.com>
2020-12-08 20:34:32 +0000

    74e42cf7d5 fix(service-worker): handle error with ErrorHandler (#39990)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+85.sha-6dc43a4"
+                version: "~11.1.0-next.1+86.sha-74e42cf"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
