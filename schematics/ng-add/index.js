a14c5d048220185f5d1767a511dbb0c2adb333a2
Misko Hevery <misko@hevery.com>
2020-12-08 20:31:44 +0000

    e69288418c refactor(compiler-cli): reformat directive/pipe metadata extraction (#39961)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+79.sha-5848439"
+                version: "~11.1.0-next.1+83.sha-e692884"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
