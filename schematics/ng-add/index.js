25d178cacb8ef193884220aa95f3e2ee8d1c9a96
Misko Hevery <misko@hevery.com>
2020-12-08 00:50:38 +0000

    5631daa05c feat(compiler): support tagged template literals in code generator (#39122)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.3+41.sha-6bb6dfd"
+                version: "~11.0.3+42.sha-5631daa"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
