881a47644237cf7c11553daeced331c8f7863b83
Misko Hevery <misko@hevery.com>
2020-12-08 19:14:19 +0000

    a694838c41 refactor(compiler-cli): TemplateTypeChecker with checkTypeOfAttributes=false should still work (#39537)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+76.sha-5a2a907"
+                version: "~11.1.0-next.1+73.sha-a694838"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
