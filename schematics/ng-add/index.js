bc6f03ac1f5f87b8db2cdf5e35fa779106bae6ec
Misko Hevery <misko@hevery.com>
2020-12-08 20:37:18 +0000

    65e93ea7b3 ci: add atscott to language-service approvers (#40027)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.3+56.sha-19309b5"
+                version: "~11.0.3+63.sha-65e93ea"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
