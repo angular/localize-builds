12271499971a25d672b6e588460b067ee2b3146d
Misko Hevery <misko@hevery.com>
2020-12-08 00:58:45 +0000

    3ec2807e38 refactor(compiler-cli): add script to update all golden partial files (#39989)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+62.sha-b8714c3"
+                version: "~11.1.0-next.1+63.sha-3ec2807"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
