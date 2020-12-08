d0ed72ce65f3e874b1948f11882c0da3145d0fcd
Misko Hevery <misko@hevery.com>
2020-12-08 17:50:17 +0000

    fbfc7dff48 build: update to use npm_package.pack rule (#39636)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+72.sha-86fdc77"
+                version: "~11.1.0-next.1+75.sha-fbfc7df"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
