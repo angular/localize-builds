d25286709047a687d8a525a6e6897a4e9f59ab28
Misko Hevery <misko@hevery.com>
2020-12-08 20:25:30 +0000

    5848439a48 docs(platform-browser): fix spelling of deserialized (#36102) PR Close #36102

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+78.sha-bc6e669"
+                version: "~11.1.0-next.1+79.sha-5848439"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
