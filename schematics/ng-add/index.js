8c8637b346bd96d3de0b4440f661cf12e14c8d1b
Misko Hevery <misko@hevery.com>
2020-12-08 17:24:56 +0000

    f06a50fc0c docs: fix reference to `routes` array in TOH tutorial (#40023)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+69.sha-8d30551"
+                version: "~11.1.0-next.1+70.sha-f06a50f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
