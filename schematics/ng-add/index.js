c4fb6210fec25d1cc0af7fa3f248d35a1ab3af64
Misko Hevery <misko@hevery.com>
2020-12-09 00:42:27 +0000

    634c393d35 fix(dev-infra): correct the environment variable to skip husky during merge (#40025)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+90.sha-61b9adb"
+                version: "~11.1.0-next.1+91.sha-634c393"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
