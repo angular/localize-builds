5e5b1a6ecc9712e57d1c2d1196b996ab24ca49d9
Misko Hevery <misko@hevery.com>
2020-12-09 00:44:33 +0000

    2f8a42036a refactor(compiler-cli): Return addEventListener symbol for native output bindings (#39312)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+91.sha-634c393"
+                version: "~11.1.0-next.1+92.sha-2f8a420"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
