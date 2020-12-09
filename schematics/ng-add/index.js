aafaa67410a52a9ae54c548f221af4cbd7b61521
Misko Hevery <misko@hevery.com>
2020-12-09 03:37:39 +0000

    cfb094dbaf test(compiler-cli): temporary disable source mapping tests which have errors (#40033)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+92.sha-2f8a420"
+                version: "~11.1.0-next.1+95.sha-cfb094d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
