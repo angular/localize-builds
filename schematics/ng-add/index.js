139d8921a87038df84410b985c2560409d264a4d
Misko Hevery <misko@hevery.com>
2020-12-09 17:32:45 +0000

    13dfef14dc docs: remove cursor: hand (#39918)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+100.sha-e39f923"
+                version: "~11.1.0-next.1+99.sha-13dfef1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
