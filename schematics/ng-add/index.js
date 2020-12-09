d55ec32fbf28d81e90057fd0b80d6b68156ff488
Misko Hevery <misko@hevery.com>
2020-12-09 17:45:40 +0000

    85760cb170 test(compiler-cli): fix and re-enable compliance source-map tests (#40040)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.1+102.sha-42d478b"
+                version: "~11.1.0-next.1+103.sha-85760cb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
