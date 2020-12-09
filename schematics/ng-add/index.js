6a3389962493e18a6edd3ceaa952b02d738359c1
Misko Hevery <misko@hevery.com>
2020-12-09 17:34:14 +0000

    b2090fdf93 docs(common): use table layout for Pre-defined format options (#40036)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.3+66.sha-5a765f0"
+                version: "~11.0.3+73.sha-b2090fd"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
