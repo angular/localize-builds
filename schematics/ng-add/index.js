7b04d0985e4ee8b8689b0c9d3bce153332c8d35d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:19:00 +0000

    3e68b79106 Revert "Revert "ci: update pullapprove config to add comp labels"" (#40059)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.2+18.sha-2a38641"
+                version: "~11.1.0-next.2+19.sha-3e68b79"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
