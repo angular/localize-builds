fb3de2a6a45528dc5e6001d8a96d08ef413988be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:10:48 +0000

    b4b21bdff4 fix(upgrade): fix HMR for hybrid applications (#40045)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.2+10.sha-76e3de2"
+                version: "~11.1.0-next.2+13.sha-b4b21bd"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
