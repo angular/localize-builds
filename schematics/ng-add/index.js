0f37e2beffa57201d51d82cbc5c2195855ae3a71
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 21:52:52 +0000

    1f73af77a7 refactor(compiler-cli): use `ngDevMode` guard for `setClassMetadata` call (#39987)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.2+8.sha-85b07ad"
+                version: "~11.1.0-next.2+9.sha-1f73af7"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
