fd774a7ee3903673f0cb59eb9a3ab8155ebcc82b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:15:20 +0000

    2a386415ce build(language-service): install @angular/language-service in root node_modules (#40058)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.2+17.sha-0817758"
+                version: "~11.1.0-next.2+18.sha-2a38641"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
