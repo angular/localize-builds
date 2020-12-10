220f8f3b55ad9f4a86ec74d98ac6722e303fc53c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:57:08 +0000

    e90a7f7ff1 docs: remove unused CSS rules (#40069)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.4+3.sha-f5aab2b"
+                version: "~11.0.4+15.sha-e90a7f7"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
