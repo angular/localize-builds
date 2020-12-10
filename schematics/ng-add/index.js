9a85c824be8125ecf5b2257616300e65492bbcf9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 22:04:12 +0000

    76e3de253c docs: remove -webkit-box-sizing and -moz-box-sizing (#40039)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.2+9.sha-1f73af7"
+                version: "~11.1.0-next.2+10.sha-76e3de2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
