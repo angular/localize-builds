6e909c78bbfb578df7251c7fb5512abf79033a05
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-10 21:45:43 +0000

    85b07adc67 docs: add Teri Glover to contributor file (#39830)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.2+7.sha-112324a"
+                version: "~11.1.0-next.2+8.sha-85b07ad"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
