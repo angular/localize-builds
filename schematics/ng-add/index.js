9da509ebc6758629692763f5b40c8488cac818fa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:13:34 +0000

    c18a9d5c79 ci: reduce flakiness of docs-infra `deploy-to-firebase.js` tests (#40088)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.2+33.sha-c977e1b"
+                version: "~11.1.0-next.2+36.sha-c18a9d5"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
