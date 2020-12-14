fcc0d7f5987c9e22737703ee1abf9af37ebc0ab2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 20:04:42 +0000

    c977e1b629 test(compiler): use `yarn bazel` instead of `bazel` in error message (#40078)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.2+32.sha-044cd52"
+                version: "~11.1.0-next.2+33.sha-c977e1b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
