86bfe408fd69a61a2a9af3e3343e03920a838898
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-14 19:43:22 +0000

    028e4f7f6d fix(language-service): force compileNonExportedClasses: false in LS (#40092)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.2+27.sha-2b74a05"
+                version: "~11.1.0-next.2+28.sha-028e4f7"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
