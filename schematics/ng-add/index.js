2d2bf9b43993be0981ee2e36e7d08bc2acb91536
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-15 21:57:58 +0000

    dcb3d176a6 docs: update optimization and source map section (#40037)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.2+47.sha-973bb40"
+                version: "~11.1.0-next.2+50.sha-dcb3d17"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
