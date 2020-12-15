fb3e07007e4fe2b7e9ae75bdd14cee7fa48cbb47
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-15 21:59:27 +0000

    a1c82e772d build: update config flags for snapshot builds (#40095)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.4+22.sha-7d9ac37"
+                version: "~11.0.4+24.sha-a1c82e7"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
