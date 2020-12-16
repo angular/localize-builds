db4a9558b28306b19a301eb51b8c3831275abf33
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 21:42:37 +0000

    922f492a3b fix(core): set `ngDevMode` to `false` when calling `enableProdMode()` (#40124)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.4+26.sha-0aa220b"
+                version: "~11.0.4+27.sha-922f492"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
