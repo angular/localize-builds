b66332ae70d52fd3fea331d3bf67fa34e3861eb8
Joey Perrott <josephperrott@gmail.com>
2020-12-17 20:27:20 +0000

    362f45c4bf docs: use an explicit product id in the getting started example (#34934)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+5.sha-475468c"
+                version: "~11.1.0-next.3+6.sha-362f45c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
