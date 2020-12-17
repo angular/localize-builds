33fecd7e19a4ad76adda3aedf4c302b1a054de08
Joey Perrott <josephperrott@gmail.com>
2020-12-17 18:26:37 +0000

    356a256909 refactor(docs-infra): enable tslint rules for the angular.io app (#39307)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+2.sha-c53bae8"
+                version: "~11.1.0-next.3+3.sha-356a256"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
