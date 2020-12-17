03203b42109f8c2f7036b766289f8e6bc02d3a11
Joey Perrott <josephperrott@gmail.com>
2020-12-17 18:06:00 +0000

    c53bae839d ci: fail CI jobs when rebasing on master fails (#40161)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+1.sha-ee6b8a7"
+                version: "~11.1.0-next.3+2.sha-c53bae8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
