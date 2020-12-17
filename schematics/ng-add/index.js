0c1957174cdd51032c41f4fdeb47f797d03ef597
Joey Perrott <josephperrott@gmail.com>
2020-12-17 17:54:48 +0000

    ee6b8a7afb Revert "build: update bazel rules_nodejs to version 2.3.1 (#39636)" (#40174)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.2+57.sha-0fc8466"
+                version: "~11.1.0-next.3+1.sha-ee6b8a7"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
