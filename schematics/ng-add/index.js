1fc1eeb0f4b573d59a89512805909ddf3d6bf39c
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:43:19 +0000

    f7642ceff3 docs: update App Shell guide to reflect its actual behavior (#40210) PR Close #40210

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+7.sha-10a468a"
+                version: "~11.1.0-next.3+9.sha-f7642ce"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
