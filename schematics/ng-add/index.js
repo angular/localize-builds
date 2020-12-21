6b69266511bf5ae77a51d107c49c35cba5f681fd
Joey Perrott <josephperrott@gmail.com>
2020-12-21 19:06:13 +0000

    3f4a1d6154 build(docs-infra): update Lighthouse to v7.0.0 (#40198)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.5+5.sha-2075cf7"
+                version: "~11.0.5+11.sha-3f4a1d6"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
