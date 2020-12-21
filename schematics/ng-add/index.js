211f52d4d86cc60a1d490b6c4db3fe9557c66046
Joey Perrott <josephperrott@gmail.com>
2020-12-21 18:38:50 +0000

    5831d2b685 ci: correctly rebase PRs for branches contain a slash (/) (#40184)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+6.sha-362f45c"
+                version: "~11.1.0-next.3+8.sha-5831d2b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
