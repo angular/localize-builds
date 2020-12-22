a4f9d6a84f7fc6b7ab5eb7268a89df0945e6dc69
Joey Perrott <josephperrott@gmail.com>
2020-12-22 23:20:34 +0000

    ef13e834fe fix(core): error if detectChanges is called at the wrong time under specific circumstances (#40206)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.5+11.sha-3f4a1d6"
+                version: "~11.0.5+23.sha-ef13e83"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
