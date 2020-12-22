68d1bdcffe2188aa305157fd9e0442cab78f4cbf
Joey Perrott <josephperrott@gmail.com>
2020-12-22 23:15:00 +0000

    12cb39c1a4 fix(language-service): shorthand syntax with variables (#40239)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+24.sha-382f906"
+                version: "~11.1.0-next.3+25.sha-12cb39c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
