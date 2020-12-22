1570d8e78ee50ef58bbe8dabc1eec4dfe372df97
Joey Perrott <josephperrott@gmail.com>
2020-12-22 20:10:03 +0000

    382f906948 refactor(compiler-cli): include linker entry-points in NPM package (#40180)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+12.sha-7413cb4"
+                version: "~11.1.0-next.3+24.sha-382f906"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
