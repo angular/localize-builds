737d177e1e02d28f49bf0df956b4980808729690
Joey Perrott <josephperrott@gmail.com>
2020-12-23 18:29:40 +0000

    c22d00547a docs(router): fix typo (#40244)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+31.sha-9186f1f"
+                version: "~11.1.0-next.3+32.sha-c22d005"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
