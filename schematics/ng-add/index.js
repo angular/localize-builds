6404f152e8d7019e11cdd6e8eb2062e8fbbb5fe3
Joey Perrott <josephperrott@gmail.com>
2020-12-23 18:16:26 +0000

    e54261b8d8 docs: update boolean and enumerated CLI section (#40224)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+29.sha-53b64fb"
+                version: "~11.1.0-next.3+30.sha-e54261b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
