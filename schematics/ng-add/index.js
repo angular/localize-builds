f21bdfba16135f1a479bb0878497bc01a661e4c8
Joey Perrott <josephperrott@gmail.com>
2020-12-23 17:01:31 +0000

    9a149d4c49 docs: use kebab-case for CLI commands (#40240)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.5+23.sha-ef13e83"
+                version: "~11.0.5+24.sha-9a149d4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
