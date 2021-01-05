11d07cd25f9c16453c579290c822a8719625925e
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:22:10 +0000

    8cdfd7706d docs(core): fix typo in component overview (#40269)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+52.sha-c1d7805"
+                version: "~11.1.0-next.3+53.sha-8cdfd77"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
