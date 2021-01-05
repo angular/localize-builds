89ef79e953c198450c7a22df4be261da7b5107ad
Joey Perrott <josephperrott@gmail.com>
2021-01-05 19:22:31 +0000

    45838c033e fix(core): take @Host into account while processing `useFactory` arguments (#40122) (#40313)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.5+31.sha-ec5b09b"
+                version: "~11.0.5+32.sha-45838c0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
