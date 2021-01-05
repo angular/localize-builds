f84c92a4494a23f1f61196d8b44da355b1ca8abd
Joey Perrott <josephperrott@gmail.com>
2021-01-05 20:52:39 +0000

    7e583abfe0 ci: remove autolabeling from pullapprove config (#40314)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.5+32.sha-45838c0"
+                version: "~11.0.5+33.sha-7e583ab"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
