39d592978c4872966cfa9d449ed9da86cb002a9f
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:17:58 +0000

    c1d7805acc docs(core): Fix spelling (#40296) PR Close #40296

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+50.sha-e43f7e2"
+                version: "~11.1.0-next.3+52.sha-c1d7805"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
