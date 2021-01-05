8037c1347b1fd3a270fc76994b5d2c365cbd9562
Joey Perrott <josephperrott@gmail.com>
2021-01-05 18:31:46 +0000

    d466db8285 fix(language-service): Do not include $event parameter in reference results (#40158)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+37.sha-4eac7e6"
+                version: "~11.1.0-next.3+38.sha-d466db8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
