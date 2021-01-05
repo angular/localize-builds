c21b4d17ce81d8aceda2082c39adda445b27e90d
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:31:20 +0000

    6abc13330b fix(compiler): don't report parse error for interpolation inside string in property binding (#40267)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+51.sha-6057753"
+                version: "~11.1.0-next.3+55.sha-6abc133"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
