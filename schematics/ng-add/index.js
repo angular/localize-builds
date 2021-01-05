530187b564aa5e6646fc839b615d224d9203cfb1
Joey Perrott <josephperrott@gmail.com>
2021-01-05 22:26:04 +0000

    6a9d7e5969 refactor(core): express trusted constants with tagged template literals (#40082)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+53.sha-8cdfd77"
+                version: "~11.1.0-next.3+54.sha-6a9d7e5"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
