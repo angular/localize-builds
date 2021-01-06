9fd8749cfc9cc81c1d9814ca2db323903aa59d64
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:57:13 +0000

    c130812afe docs: fix `fullTemplateTypeCheck` compiler option description (#40253)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+62.sha-7eadf2e"
+                version: "~11.1.0-next.3+61.sha-c130812"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
