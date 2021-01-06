7cdae70ec8821ee15d0a85cd60841d21daa2aa9e
Joey Perrott <josephperrott@gmail.com>
2021-01-06 19:18:27 +0000

    0264f76e94 fix(language-service): LSParseConfigHost.resolve should not concat abs paths (#40242)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+68.sha-335d6c8"
+                version: "~11.1.0-next.3+70.sha-0264f76"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
