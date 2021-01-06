351bfe6148d96f0b332e241872bcb5d3b2310b75
atscott <atscott01@gmail.com>
2021-01-06 22:16:56 +0000

    48526ccdf1 docs: fix instructions for setting up `ProductDetailsComponent` in `start-routing.md` (#40197)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.5+56.sha-5b9c981"
+                version: "~11.0.6+5.sha-48526cc"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
