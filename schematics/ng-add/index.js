195306f2aedac15af7bd7cf318290d3ccbb5334b
Joey Perrott <josephperrott@gmail.com>
2021-01-06 20:18:39 +0000

    b6a9717597 test(core): fix invalid symbol used in html sanitizier spec (#40325)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.5+51.sha-bad1717"
+                version: "~11.0.5+55.sha-b6a9717"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
