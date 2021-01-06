9bf8226e34bf3836c41bbc60ea6007287dd98781
Joey Perrott <josephperrott@gmail.com>
2021-01-06 16:55:45 +0000

    d4327d51d1 feat(compiler-cli): JIT compilation of component declarations (#40127)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+61.sha-c130812"
+                version: "~11.1.0-next.3+64.sha-d4327d5"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
