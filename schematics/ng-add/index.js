05e75a8090be81e372e0085dcb143bed1ef6d208
atscott <atscott01@gmail.com>
2021-01-06 22:15:36 +0000

    13020f904f fix(router): correctly deactivate children with componentless parent (#40196)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+73.sha-bff48a0"
+                version: "~11.1.0-next.4+1.sha-13020f9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
