fc883b10feb2578832087308e3842b4048578bde
Joey Perrott <josephperrott@gmail.com>
2021-01-06 15:27:23 +0000

    0a317f3bb4 docs: add Aristeidis Bampakos to contributor list (#40165)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.3+56.sha-9e6b3cf"
+                version: "~11.1.0-next.3+58.sha-0a317f3"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
