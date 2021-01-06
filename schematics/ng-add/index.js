f89a0972932327783454a84caf95923ba6427beb
Joey Perrott <josephperrott@gmail.com>
2021-01-06 17:08:09 +0000

    bad171754e docs(forms): typo word "property" was missing (#40323)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.5+50.sha-67ab59b"
+                version: "~11.0.5+51.sha-bad1717"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
