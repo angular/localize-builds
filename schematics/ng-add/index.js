5c4a5902a24fcb487f12bb0df941b0ddf49a7d30
atscott <atscott01@gmail.com>
2021-01-07 21:52:29 +0000

    da6c739bb6 test(core): update test expectation to account for IE11 anonymous function name (#40342)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+30.sha-15b15be"
+                version: "~11.1.0-next.4+31.sha-da6c739"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
