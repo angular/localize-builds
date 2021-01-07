314af322bea34850585ee7c5ce22285248ff85af
atscott <atscott01@gmail.com>
2021-01-07 21:35:25 +0000

    335c1ab37f docs: add Craig Spence to contributor list (#40335)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+10.sha-1437f4c"
+                version: "~11.1.0-next.4+11.sha-335c1ab"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
