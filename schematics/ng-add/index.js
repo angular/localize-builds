9dd9329c832e128114f198f50687e72dbd463f4d
atscott <atscott01@gmail.com>
2021-01-07 20:11:35 +0000

    6429be1c15 fix(router): Remove usage of `Object.entries` to avoid the need for a polyfill (#40340)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.6+5.sha-48526cc"
+                version: "~11.0.6+6.sha-6429be1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
