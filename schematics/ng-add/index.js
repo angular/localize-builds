b3c197b6a1a6343fba0a9fea3a0a0b1e8d0a0214
atscott <atscott01@gmail.com>
2021-01-07 21:55:34 +0000

    104546569e fix(compiler): incorrectly interpreting some HostBinding names (#40233)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+31.sha-da6c739"
+                version: "~11.1.0-next.4+22.sha-1045465"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
