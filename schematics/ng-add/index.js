cb7d4b11dc57e3f0e704678ecc2a58ab1671cfdf
atscott <atscott01@gmail.com>
2021-01-07 21:34:52 +0000

    40a03f0f28 docs: lifecycle-hooks example - extract things (components, directives) into their own files (#40212)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.6+7.sha-64d94e8"
+                version: "~11.0.7+4.sha-40a03f0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
