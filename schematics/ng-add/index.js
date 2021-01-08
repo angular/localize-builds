94299670f0e7b75dbdf565534bab0de100d361e5
atscott <atscott01@gmail.com>
2021-01-08 22:10:07 +0000

    6a9e328432 feat(language-service): log Angular compiler options (#40364)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+44.sha-b36bece"
+                version: "~11.1.0-next.4+46.sha-6a9e328"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
