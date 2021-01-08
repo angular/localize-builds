324158be7384887a6ad6c4b44f1099149954a9f7
Andrew Scott <atscott@google.com>
2021-01-08 18:08:46 +0000

    7a819caef6 fix(dev-infra): delay checking if a closed pr has been merged by 30 seconds (#40181)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+35.sha-e27b920"
+                version: "~11.1.0-next.4+37.sha-7a819ca"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
