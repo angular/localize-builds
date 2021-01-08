1b7c8b19c69d881b2cb4af9152267ef17651c653
Andrew Scott <atscott@google.com>
2021-01-08 18:22:39 +0000

    6ccdb2ddba docs: add @christianliebel to list of GDEs (#39788)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.7+4.sha-40a03f0"
+                version: "~11.0.7+11.sha-6ccdb2d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
