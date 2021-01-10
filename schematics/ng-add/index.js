93bacf5f929809ed57511ee035d7d59f3693d302
Andrew Scott <atscott@google.com>
2021-01-10 01:02:43 +0000

    c44dd84a71 fix(router): Remove usage of `Object.values` to avoid the need for a polyfill (#40370)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.7+11.sha-6ccdb2d"
+                version: "~11.0.7+13.sha-c44dd84"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
