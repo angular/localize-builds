3dbecc0cf4a41ad89095e41ed400f51624987b72
atscott <atscott01@gmail.com>
2021-01-11 18:14:06 +0000

    1e46b73463 ci: add new group to approve contributor list changes (#40368)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+49.sha-94ec027"
+                version: "~11.1.0-next.4+50.sha-1e46b73"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
