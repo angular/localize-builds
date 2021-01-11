6d3f33abfc3bca517842e45aacd8a138e85c1c81
atscott <atscott01@gmail.com>
2021-01-11 18:25:32 +0000

    6e57443acd docs(router): fix minor grammatical issue (#40380)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+51.sha-34d2988"
+                version: "~11.1.0-next.4+54.sha-6e57443"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
