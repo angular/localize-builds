1266a43ec32592abc8c573a0d3a23d2659edbcca
atscott <atscott01@gmail.com>
2021-01-11 23:57:33 +0000

    85af573f81 docs: resolve non-exiting references in contributors.json (#40369)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+69.sha-9105005"
+                version: "~11.1.0-next.4+74.sha-85af573"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
