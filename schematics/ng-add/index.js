2b5dea72c2a64396c0024cb39848f5a306b66431
atscott <atscott01@gmail.com>
2021-01-11 18:21:09 +0000

    3d7e207a25 fix(dev-infra): detect multiple target labels as invalid (#40156)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+50.sha-1e46b73"
+                version: "~11.1.0-next.4+53.sha-3d7e207"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
