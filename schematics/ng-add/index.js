0c0167b9de2a3e7fa912e0e08f946d639a56639f
atscott <atscott01@gmail.com>
2021-01-11 18:27:08 +0000

    3acbec8532 docs: remove invalid style binding example (#40334)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+54.sha-6e57443"
+                version: "~11.1.0-next.4+52.sha-3acbec8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
