57b5591127d6d1f44f229655820a6527ee46afe5
atscott <atscott01@gmail.com>
2021-01-11 23:59:00 +0000

    957332e59f docs: edit gh-pages instructions in deployment guide (#40139)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+74.sha-85af573"
+                version: "~11.1.0-next.4+75.sha-957332e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
