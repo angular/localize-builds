86aad7e18b1f0edb8cafdbf1f390109ecc142a7c
atscott <atscott01@gmail.com>
2021-01-11 23:59:23 +0000

    9105005192 refactor(router): refactor and simplify router RxJS chains (#40290)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+68.sha-4a7a649"
+                version: "~11.1.0-next.4+69.sha-9105005"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
