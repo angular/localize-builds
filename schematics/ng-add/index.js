c02583dd0735ec17c4fe34383a8357c19ac41421
atscott <atscott01@gmail.com>
2021-01-11 19:37:03 +0000

    76954688b1 fix(router): better ngZone checking for warning (#25839)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+65.sha-c9de7da"
+                version: "~11.1.0-next.4+66.sha-7695468"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
