94ae0b946932ae49d11c1a27e07eccf8299f2de5
atscott <atscott01@gmail.com>
2021-01-11 19:20:45 +0000

    f8aad11866 refactor(service-worker): remove work-around for Chrome code highlighting bug (#40234)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+57.sha-27d0e54"
+                version: "~11.1.0-next.4+64.sha-f8aad11"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
