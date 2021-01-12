92d9a7f042b5eb6d3c74dc10f6f415d4f4eafaf1
atscott <atscott01@gmail.com>
2021-01-12 00:04:54 +0000

    66c27ffdfc fix(compiler): incorrectly inferring content type of SVG-specific title tag (#40259)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+77.sha-184d0e5"
+                version: "~11.1.0-next.4+78.sha-66c27ff"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
