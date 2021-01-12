9c3cb46bad52defdeaec78a113d205b964159e95
atscott <atscott01@gmail.com>
2021-01-12 00:05:12 +0000

    bd17610c29 feat(dev-infra): make ts-api-guardian compatible with rules_nodejs version 3 (#40353)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+78.sha-66c27ff"
+                version: "~11.1.0-next.4+79.sha-bd17610"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
