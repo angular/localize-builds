d9520632ee43e5802c079a3a014793c0b9c2606c
atscott <atscott01@gmail.com>
2021-01-13 22:34:48 +0000

    25788106e5 ci: add mgechev to docs-marketing approvers (#40396)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+86.sha-25892c1"
+                version: "~11.1.0-next.4+91.sha-2578810"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
