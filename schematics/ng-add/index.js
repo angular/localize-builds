046a5820c39946ed7e744baea95abb26f5b2fd4e
atscott <atscott01@gmail.com>
2021-01-13 19:23:11 +0000

    4db89f4576 fix(compiler-cli): report non-template diagnostics (#40331)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+81.sha-625d2c2"
+                version: "~11.1.0-next.4+82.sha-4db89f4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
