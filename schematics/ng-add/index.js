f09964cd0573cbd86fd3e6c9c33216a40f77f2c3
atscott <atscott01@gmail.com>
2021-01-13 23:14:55 +0000

    5aa264f17c docs: update identity-revealed.directive.ts to use `AbstractControl` instead of `FormGroup` (#40132)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.9+7.sha-3f6279d"
+                version: "~11.0.9+9.sha-5aa264f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
