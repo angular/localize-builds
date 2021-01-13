9a8b4c1c3766f417723ba4b4956a1c1e94bf448d
atscott <atscott01@gmail.com>
2021-01-13 22:36:28 +0000

    0664d750b6 perf(core): make DI decorators tree-shakable when used for `useFactory` deps config (#40145)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.5+1.sha-ebd94ef"
+                version: "~11.1.0-rc.0+7.sha-0664d75"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
