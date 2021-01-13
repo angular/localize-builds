c9863ac5fff0994c876b9061e5b18ddfdd32ce00
atscott <atscott01@gmail.com>
2021-01-13 17:34:37 +0000

    625d2c252b fix(language-service): diagnostic and definition should work for absolute url (#40406)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+80.sha-b48eabd"
+                version: "~11.1.0-next.4+81.sha-625d2c2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
