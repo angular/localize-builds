747c8752edad5da94c4846051d6754f31f4a6e59
Andrew Kushnir <akushnir@google.com>
2021-01-14 19:57:41 +0000

    10994ae68c docs(core): change URL to shadow dom spec (#39797)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+97.sha-b3b8509"
+                version: "~11.1.0-next.4+98.sha-10994ae"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
