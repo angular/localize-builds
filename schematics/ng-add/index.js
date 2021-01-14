d96a837e97abef44d307db0800ad90b8b69310df
Andrew Scott <atscott@google.com>
2021-01-14 17:41:28 +0000

    c33d2a20ac docs(common): updated differential loading section (#37913)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+94.sha-5013a7e"
+                version: "~11.1.0-next.4+95.sha-c33d2a2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
