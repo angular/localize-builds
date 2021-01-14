d72b3a9c4b616a6517f985295aaa006a346adde5
Andrew Kushnir <akushnir@google.com>
2021-01-14 19:59:49 +0000

    2f7e315157 perf(ngcc): do not copy files that have been processed (#40429)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.9+12.sha-72e8899"
+                version: "~11.0.9+17.sha-2f7e315"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
