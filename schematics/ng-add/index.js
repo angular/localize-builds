8cdd572a1bc92761a889c1b3b6f143f40efcc15a
atscott <atscott01@gmail.com>
2021-01-14 18:46:12 +0000

    968b2bbfc6 docs: Update security.md to include Trusted Types (#40142)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-rc.0+10.sha-f4ed122"
+                version: "~11.1.0-rc.0+12.sha-968b2bb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
