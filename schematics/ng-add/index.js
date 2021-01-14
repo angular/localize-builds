9f20ba5fc09df3eb211559add1664d231731810b
Andrew Kushnir <akushnir@google.com>
2021-01-14 20:02:44 +0000

    63bf613785 docs: remove duplicated the (#40434)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-rc.0+12.sha-968b2bb"
+                version: "~11.1.0-rc.0+19.sha-63bf613"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
