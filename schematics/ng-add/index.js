d795115dc91c2bee4d0d1bfafb09a8c7af02e72f
Andrew Kushnir <akushnir@google.com>
2021-01-15 20:21:23 +0000

    524415ef8a fix(compiler): correct the `KeySpan` for animation events and properties (#40347)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-rc.0+21.sha-d5f696c"
+                version: "~11.1.0-rc.0+28.sha-524415e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
