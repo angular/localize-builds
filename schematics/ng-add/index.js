54c84d7ccd71fd3babcf93e327c2c92124af2e02
Andrew Kushnir <akushnir@google.com>
2021-01-15 19:41:51 +0000

    b37a9eba1a fix(router): lazy loaded modules without RouterModule.forChild() won't cause an infinite loop (#36605)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+108.sha-5457936"
+                version: "~11.1.0-next.4+111.sha-b37a9eb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
