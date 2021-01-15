fb41d3bd4173478927445162b7663b48d1f849df
Andrew Kushnir <akushnir@google.com>
2021-01-15 19:40:08 +0000

    96690ed3a4 feat(docs-infra): add shortcuts for the angular.io PWA (#40393)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+111.sha-b37a9eb"
+                version: "~11.1.0-next.4+110.sha-96690ed"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
