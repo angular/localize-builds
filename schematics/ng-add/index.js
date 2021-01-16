2563e6c58509da2256f718d2dcbb9aeb181d0764
Andrew Kushnir <akushnir@google.com>
2021-01-16 01:09:55 +0000

    4e8198d60f feat(language-service): Implement `getRenameInfo` (#40439)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+114.sha-40e0bfd"
+                version: "~11.1.0-next.4+115.sha-4e8198d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
