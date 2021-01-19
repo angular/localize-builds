d50f17ec82f1f73cd3e905116434d2d03395d3ee
Andrew Kushnir <akushnir@google.com>
2021-01-19 18:41:51 +0000

    afabb83696 refactor(core): add links to top compiler errors (#40326)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+121.sha-0568c73"
+                version: "~11.1.0-next.4+123.sha-afabb83"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
