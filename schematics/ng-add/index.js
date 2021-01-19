ee5c4fb29afb69dcfa832256205c8413d6aaee7c
Andrew Kushnir <akushnir@google.com>
2021-01-19 20:04:54 +0000

    2c5ad5cbe8 fix(forms): error if control is removed as a result of another one being reset (#40462)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-rc.0+34.sha-106df61"
+                version: "~11.1.0-rc.0+35.sha-2c5ad5c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
