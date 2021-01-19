c1d6cc4b9a7420afce911ffa6008b74af0c6a16a
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:52:23 +0000

    724e44a44a docs: add Angular Builders to community resources (#40475)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.0.9+22.sha-e7ed209"
+                version: "~11.0.9+25.sha-724e44a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
