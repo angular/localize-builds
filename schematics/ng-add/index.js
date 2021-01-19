e78a58b0fac920c729495c5d3438ee8ae8f82c4b
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:40:21 +0000

    f49447df20 fix(core): fix fakeAsync() error messages (#40442)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+115.sha-4e8198d"
+                version: "~11.1.0-next.4+116.sha-f49447d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
