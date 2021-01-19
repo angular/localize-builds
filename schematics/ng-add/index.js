e240428d208281359c40cdfa18c13ec846ce8560
Andrew Kushnir <akushnir@google.com>
2021-01-19 22:44:19 +0000

    5e95153d44 docs: Return SVG topic to documentation; light edits for clarity (#40487)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+125.sha-3e97a1e"
+                version: "~11.1.0-next.4+126.sha-5e95153"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
