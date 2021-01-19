5de3eb0785a5e91c7eca8d407ad0f5841f18270a
Andrew Kushnir <akushnir@google.com>
2021-01-19 17:47:08 +0000

    2c95f57266 feat(docs-infra): use Record type instead of key-value (#40463)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+119.sha-b636406"
+                version: "~11.1.0-next.4+120.sha-2c95f57"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
