999990b79dd0d7097cb97c4654810793f8616736
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:28:48 +0000

    2b2a847ad7 refactor(migrations): remove rxjs usage within static queries migration (#38657)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+153.sha-dc06873"
+                version: "~11.1.0-next.4+151.sha-2b2a847"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
