b9043d444512a091a97a4b0c5f9e1fe8ec88ec07
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 00:50:29 +0000

    a8a27efb77 fix(router): always stringify matrix parameters (#25095)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0"
+                version: "~11.1.0+9.sha-a8a27ef"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
