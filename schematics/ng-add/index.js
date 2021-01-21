5c66d8d7f134e630cd15395985b78b0d1aa2fe60
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 01:21:57 +0000

    e995d851cf test(core): verify that token IDs that exceed the bloom filter size are handled correctly (#40489)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0+9.sha-a8a27ef"
+                version: "~11.1.0+11.sha-e995d85"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
