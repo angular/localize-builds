9b03d8f4ae6b628b3e34e3ce1761bc70d0f8d194
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-21 22:14:16 +0000

    5a8e9394df Remove myself from language-service reviews (#40509)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+146.sha-3f92be4"
+                version: "~11.1.0-next.4+147.sha-5a8e939"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
