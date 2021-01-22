b51764d8f63f3225f3f15fc2477fb7b06d8194fd
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 22:40:14 +0000

    3bf6a9908c fix(core): fix possible XSS attack in development through SSR (#40525)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0+23.sha-6298b3e"
+                version: "~11.1.0+24.sha-3bf6a99"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
