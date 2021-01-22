bde2cfa0541c0d6e3bb6822839922c8c2fc2bb10
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 18:51:13 +0000

    b3b80d4e4a build: update to @bazel/bazelisk@1.7.3 (#40519)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0+19.sha-566206b"
+                version: "~11.1.0+22.sha-b3b80d4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
