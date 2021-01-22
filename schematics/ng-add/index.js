1b0ce00fc09e0d130d63a14945708798c19a1dae
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 18:47:27 +0000

    1e4b51e9f7 fix(core): improve injector debug information in `ngDevMode` (#40476)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+154.sha-07b7af3"
+                version: "~11.1.0-next.4+155.sha-1e4b51e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
