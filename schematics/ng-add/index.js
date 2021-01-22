58b95122a4b258c96281a257d35d6b88358a5c59
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-22 21:57:22 +0000

    6298b3e11b docs: fix NG0201 error description (#40500)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0+22.sha-b3b80d4"
+                version: "~11.1.0+23.sha-6298b3e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
