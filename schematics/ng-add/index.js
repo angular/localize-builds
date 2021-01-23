749db7fe3f966f9cc11fc357efe25f05870c3413
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-23 01:09:10 +0000

    fc64fa8e1a Revert "fix(compiler): support multiple `:host-context()` selectors (#40494)" (#40531)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+159.sha-c64a56f"
+                version: "~11.1.0-next.4+160.sha-fc64fa8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
