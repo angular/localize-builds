17c410bff3624e7fa7fa65a50688b61deee3416d
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 19:26:00 +0000

    56612982c7 fix(localize): include meaning in generated ARB files (#40546)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0+30.sha-37710b9"
+                version: "~11.1.0+36.sha-5661298"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
