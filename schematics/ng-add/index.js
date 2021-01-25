b228bbdb548c62ca460151abd8ab20246205958f
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 23:25:11 +0000

    33e0f2bd56 fix(compiler-cli): use `Map` rather than `object` for map of partial linkers (#40563)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0+39.sha-2fab148"
+                version: "~11.1.0+41.sha-33e0f2b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
