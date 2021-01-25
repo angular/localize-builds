ef771e3cccf640abf4fb9e1f4488468d35bb1e75
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-25 18:37:20 +0000

    37710b9be3 fix(service-worker): handle error with console.error (#40236)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0+26.sha-dfe4200"
+                version: "~11.1.0+30.sha-37710b9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
