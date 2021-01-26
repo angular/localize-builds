afa5b071adec1751fb79f3704464ae86d2558f73
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-26 01:01:40 +0000

    a87e90071c docs: fix the docs because the interface does not exist (#40308)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0+41.sha-33e0f2b"
+                version: "~11.1.0+43.sha-a87e900"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
