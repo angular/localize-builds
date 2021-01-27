a9d8f470cfe1383d99ebca6d30e9fba20d796cc0
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 19:11:26 +0000

    e43cba5c82 fix(language-service): implement realpath to resolve symlinks (#40593)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+190.sha-d703d21"
+                version: "~11.1.0-next.4+189.sha-e43cba5"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
