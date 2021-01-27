d04d4965ab15017595521802bd50d30f425b7161
Jessica Janiuk <jessicajaniuk@google.com>
2021-01-27 19:10:13 +0000

    d703d21656 fix(dev-infra): fix substitution string for component_benchmark (#40591)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+186.sha-f1cf875"
+                version: "~11.1.0-next.4+190.sha-d703d21"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
