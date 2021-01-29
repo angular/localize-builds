65c2a9bffc87a44846bfcd1a44257f3f925d30a8
Misko Hevery <misko@hevery.com>
2021-01-29 00:25:12 +0000

    4c5f18dfda fix(common): parse `YYYY-MM` strings as UTC dates (#40620)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.0-next.0+11.sha-552009e"
+                version: "~11.2.0-next.0+15.sha-4c5f18d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
