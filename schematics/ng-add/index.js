1d4fd04afb86d290924fc6ebe2e2a79580217094
Misko Hevery <misko@hevery.com>
2021-01-29 19:42:51 +0000

    bd0d19141b fix(compiler-cli): preserve user line endings in diagnostic template parse (#40597)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+215.sha-4b3106e"
+                version: "~11.1.0-next.4+216.sha-bd0d191"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
