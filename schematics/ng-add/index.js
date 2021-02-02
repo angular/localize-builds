da18efaf3878e1e3a620dc4942da8d73050dbdad
Misko Hevery <misko@hevery.com>
2021-02-02 20:23:59 +0000

    20654c95d8 refactor(docs-infra): remove unused styles for `.heading` CSS class (#40427)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.1+17.sha-00eeebf4"
+                version: "~11.1.1+29.sha-20654c9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
