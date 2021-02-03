d1cdc8607860fea0fcb8c3ccca6eddd81df1e7b6
Misko Hevery <misko@hevery.com>
2021-02-03 00:50:10 +0000

    a3b0864428 refactor(compiler-cli): remove the overrideComponentTemplate API (#40585)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+229.sha-11ca2f0"
+                version: "~11.1.0-next.4+231.sha-a3b0864"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
