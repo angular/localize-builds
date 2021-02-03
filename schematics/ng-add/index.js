d8d064773b29a1b8a80a5d68c34608da2e43b46c
Misko Hevery <misko@hevery.com>
2021-02-03 19:38:13 +0000

    0ee9ea8148 refactor(language-service): pull out interfaces on package toplevel (#40621) (#40695)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.1+29.sha-20654c9"
+                version: "~11.1.1+37.sha-0ee9ea8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
