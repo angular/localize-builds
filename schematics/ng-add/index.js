659d92152df8ebb1d454ac0c4db445bcbf4fac4c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-04 19:20:22 +0000

    5324a62ebe docs: add guide for branching process (#39924)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+240.sha-836b176"
+                version: "~11.1.0-next.4+241.sha-5324a62"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
