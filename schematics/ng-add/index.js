3d65af6e37b36dec00dd9ee23d6a26226d4b6147
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-04 19:31:25 +0000

    c9fa59ac1f fix(docs-infra): switch code font from `Droid Sans Mono` to `Roboto Mono` (#40667)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+245.sha-d067dc0"
+                version: "~11.1.0-next.4+244.sha-c9fa59a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
