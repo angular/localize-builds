f8dcd4be39672307e89a3ef3608610f6b518f934
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-08 17:59:13 +0000

    8fb83ea1b5 feat(forms): introduce min and max validators (#39063)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+244.sha-c9fa59a"
+                version: "~11.1.0-next.4+246.sha-8fb83ea"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
