47c4c093bbf78ff292d4f8df795f14f6fe12f3fe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:13:34 +0000

    4ce44eac33 fix(animations): properly track listeners for a removed element (#40712)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+284.sha-089a634"
+                version: "~11.1.0-next.4+302.sha-4ce44ea"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
