40947157fbf2960423d91f97cabfc066feedb6df
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 23:08:54 +0000

    61a0b6de6d fix(http): complete the request on timeout (#39807)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+311.sha-2a851c0"
+                version: "~11.1.0-next.4+309.sha-61a0b6d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
