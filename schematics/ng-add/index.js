4a5d169b74ddbe6bb041597ebeb7020e7b7ad85c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:10:19 +0000

    089a634053 refactor(docs-infra): use a single CSS class to target the API list container (#40704)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+249.sha-0652b29"
+                version: "~11.1.0-next.4+284.sha-089a634"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
