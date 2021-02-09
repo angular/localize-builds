7b803879b2e7dbf45239334efd52a4f4619b6d39
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:09:23 +0000

    0652b29f62 fix(zone.js): setTimeout patch should clean tasksByHandleId cache. (#40586)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+250.sha-2282069"
+                version: "~11.1.0-next.4+249.sha-0652b29"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
