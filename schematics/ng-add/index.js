b27ddf2a8078f926dde6dc9ee7f2ca5f18f2c540
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 23:06:27 +0000

    3b0c51e045 docs: edit example to fetch hero before sending message (#40419)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.0-rc.0+65.sha-c0d1e34"
+                version: "~11.2.0-rc.0+67.sha-3b0c51e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
