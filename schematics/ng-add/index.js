1d187a84a960f320c7640df21324717202dcb501
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:11:36 +0000

    9dada8a2dd fix(localize): improve invalid syntax in extraction error message (#40724)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+302.sha-4ce44ea"
+                version: "~11.1.0-next.4+303.sha-9dada8a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
