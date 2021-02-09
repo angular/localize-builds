2a77e1fbd21b6d0de4d191a094f81b6a90eb668d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 19:06:59 +0000

    2282069388 docs: clarify vscode integration maintenance (#40647)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+248.sha-d9e4d75"
+                version: "~11.1.0-next.4+250.sha-2282069"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
