a13bdf55959976ca199f4b88fef575d537c05f13
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-09 23:06:47 +0000

    820d95ba40 docs: add redirect from updating-v10 to v11 (#40108)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+308.sha-f76f2eb"
+                version: "~11.1.0-next.4+310.sha-820d95b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
