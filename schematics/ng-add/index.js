ebe78873adee26c3d993c7eec0b85e8e0816c193
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:46:14 +0000

    7a8821c9aa docs: add link to component-less routes description. (#40741)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+324.sha-fface51"
+                version: "~11.1.0-next.4+321.sha-7a8821c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
