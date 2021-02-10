30bff7f8811dff29c5fef26b95157f3bc3b0f029
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-02-10 19:38:34 +0000

    03f0b157c1 fix(common): date is not correctly formatted when year is between 0 and 99 (#40448)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+313.sha-378da71"
+                version: "~11.1.0-next.4+316.sha-03f0b15"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
