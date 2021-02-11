05f2797f84b37edddb0983c1ff9abb2326045eda
Joey Perrott <josephperrott@gmail.com>
2021-02-11 17:22:16 +0000

    7ca244a38b docs(core): add missing dot at the end of the sentence (#40800)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+4.sha-68da05d"
+                version: "~12.0.0-next.0+6.sha-7ca244a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
