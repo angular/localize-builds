eb4c87b5063196b3663320101cb44f3ab15dd50d
Joey Perrott <josephperrott@gmail.com>
2021-02-11 20:56:14 +0000

    fcf00de864 ci: remove BES usages from CI (#40809)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+10.sha-f72626d"
+                version: "~12.0.0-next.0+12.sha-fcf00de"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
