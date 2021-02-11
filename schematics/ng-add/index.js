0b6aa130d509f9cc7dd0e9752be254980646d619
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:33:48 +0000

    4c6d557ca9 build: update browserstacktunnel-wrapper (#40810)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.0+13.sha-925e2b7"
+                version: "~11.2.0+16.sha-4c6d557"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
