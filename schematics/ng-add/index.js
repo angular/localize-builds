b245352dcf8afc27f4eba150f964fb116bad783c
Joey Perrott <josephperrott@gmail.com>
2021-02-11 21:00:48 +0000

    e27787a583 build: remove jpm dependency (#40807)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+12.sha-fcf00de"
+                version: "~12.0.0-next.0+13.sha-e27787a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
