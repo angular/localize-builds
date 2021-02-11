8bc81a88e73b96fdcd936736e92d501e4e73fe50
Joey Perrott <josephperrott@gmail.com>
2021-02-11 00:34:21 +0000

    9cbd9bed55 Revert "ci: Remove language-service from testing group (#40787)" (#40794)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.0-rc.0+80.sha-e3f09ce"
+                version: "~11.2.0+3.sha-9cbd9be"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
