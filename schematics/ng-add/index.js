a8cb4a6f302dea08ff3d06a59c4beff149a1be18
Joey Perrott <josephperrott@gmail.com>
2021-02-11 00:29:46 +0000

    c56ecab515 feat(common): support ICU standard "stand alone day of week" with `DatePipe` (#40766)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+2.sha-6f017a4"
+                version: "~12.0.0-next.0+3.sha-c56ecab"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
