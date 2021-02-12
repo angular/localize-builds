b6a6d1f381ea6196213e506e02362f1a1a0ec49a
Joey Perrott <josephperrott@gmail.com>
2021-02-12 17:12:55 +0000

    43ecf8a77b feat(platform-server): allow shimming the global env sooner (#40559)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+20.sha-80f4ff3"
+                version: "~12.0.0-next.0+24.sha-43ecf8a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
