dee48bf0e65eca4e09c7e45a866fb488312a11df
Joey Perrott <josephperrott@gmail.com>
2021-02-12 00:19:26 +0000

    ca43938d24 docs: copy paste error (#40812)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+19.sha-5eb1954"
+                version: "~12.0.0-next.0+21.sha-ca43938"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
