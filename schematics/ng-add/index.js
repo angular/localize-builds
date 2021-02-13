a0563b5465c679c08a0a4a24e79b0bcfb816c08b
Joey Perrott <josephperrott@gmail.com>
2021-02-13 00:32:34 +0000

    bbbfa3e6bd docs: clarify that support schedule is subject to change (#40770)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+31.sha-ccd10ea"
+                version: "~12.0.0-next.0+32.sha-bbbfa3e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
