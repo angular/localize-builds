07221acc5006e7c195c83c1d363e131742db8f10
Joey Perrott <josephperrott@gmail.com>
2021-02-13 01:10:52 +0000

    5554a3f174 docs: add NgRx book to resources (#39841)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+34.sha-9661873"
+                version: "~12.0.0-next.0+33.sha-5554a3f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
