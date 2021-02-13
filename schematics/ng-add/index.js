298394c7dc5f60712a807060bbab02839bcfd054
Joey Perrott <josephperrott@gmail.com>
2021-02-13 00:30:46 +0000

    ccd10ea315 docs: Clarify new ng workspace options (#40816)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+30.sha-44b8b0f"
+                version: "~12.0.0-next.0+31.sha-ccd10ea"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
