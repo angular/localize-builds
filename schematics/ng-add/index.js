82e872cb4514e6df66c75d21c6faadd939f2b3f6
Joey Perrott <josephperrott@gmail.com>
2021-02-13 01:32:30 +0000

    72a00dcc6b docs: add a style recommendation for initializing inputs (#40698)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+35.sha-980f6a4"
+                version: "~12.0.0-next.0+36.sha-72a00dc"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
