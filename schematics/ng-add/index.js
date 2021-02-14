4a0ebb188f70d21c1690087c24f9eb3b53859504
JoostK <joost.koehoorn@gmail.com>
2021-02-14 15:15:17 +0000

    1646f8da87 perf(compiler-cli): detect semantic changes and their effect on an incremental rebuild

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.1.0-next.4+308.sha-0d8e6b4"
+                version: "~12.0.0-next.0+37.sha-1646f8d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
