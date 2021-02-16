5c594debbc96cb792d3173cf819c8bc45aee3a67
Joey Perrott <josephperrott@gmail.com>
2021-02-16 21:25:21 +0000

    53865c4517 docs(forms): fix link for browser compatibility of using selects in IE (#40855)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.0+37.sha-7f09945"
+                version: "~11.2.0+61.sha-53865c4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
