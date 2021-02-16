0d91d8e6d28345f719fa4387faf775a2137cf67e
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:01:34 +0000

    5f0c219883 docs: fix schematics' package name for `ng generate` sub-commands (#40846)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+36.sha-72a00dc"
+                version: "~12.0.0-next.0+38.sha-5f0c219"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
