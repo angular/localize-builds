d5cc2fa2feefb6d9f92d46d24fafb75a0a6cdd03
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:01:43 +0000

    f2ee9d5679 refactor(docs-infra): use CDK clipboard service (#40840)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+37.sha-67d85b3"
+                version: "~12.0.0-next.0+39.sha-f2ee9d5"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
