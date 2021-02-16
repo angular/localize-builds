2ff40153e1f3a20edece377879b6b62400dbeb6f
Joey Perrott <josephperrott@gmail.com>
2021-02-16 16:22:34 +0000

    7fa881919b docs: Add documentation about the CLI failing (#35385)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+42.sha-19114dc"
+                version: "~12.0.0-next.0+44.sha-7fa8819"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
