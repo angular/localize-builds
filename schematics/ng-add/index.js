79827f024f1e7163051defa7f3ffbcd203f5eb66
Joey Perrott <josephperrott@gmail.com>
2021-02-17 15:14:41 +0000

    a4c00c2148 build: upgrade `webdriver-manager` to v12.1.8 (#40756)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+69.sha-12f7101"
+                version: "~12.0.0-next.0+72.sha-a4c00c2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
