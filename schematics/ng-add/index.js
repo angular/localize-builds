ff402795ef001644f58e17f82407536acc2eeae2
Joey Perrott <josephperrott@gmail.com>
2021-02-17 15:21:19 +0000

    9b26f462d0 docs: fix grammar in angular-compiler-options file (#40879)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.0+74.sha-13d82ce"
+                version: "~12.0.0-next.0+76.sha-9b26f46"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
