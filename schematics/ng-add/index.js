3906e2a537fe67ec924b72c6b5785d04673a69be
Zach Arend <zachzach@google.com>
2021-02-18 01:26:35 +0000

    65baac6055 docs: update attribute-binding.md to correctly describe [attr.binding] rules (#40878)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.1+8.sha-2375b85"
+                version: "~12.0.0-next.1+9.sha-65baac6"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
