87f46a43037d800e4a80ad1cb8b260fb34a754ea
Zach Arend <zachzach@google.com>
2021-02-18 01:29:01 +0000

    170692c18c refactor(compiler): add process title to Angular node binaries (#40648)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.1+3.sha-a970c19"
+                version: "~11.2.1+9.sha-170692c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
