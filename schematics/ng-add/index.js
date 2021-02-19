378c688c4e3245be548d0c1928e00b21a67a59be
atscott <atscott01@gmail.com>
2021-02-19 17:35:03 +0000

    09e1e1935a refactor(docs-infra): convert Sass mixin from camelCase to kebab-case to follow Sass conventions (#40881)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.1+14.sha-b4301c3"
+                version: "~12.0.0-next.1+32.sha-09e1e19"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
