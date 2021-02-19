7c44e5ac4e4725a32caafb53eacd72c98b8d74a4
atscott <atscott01@gmail.com>
2021-02-19 17:39:51 +0000

    a82fddf1ce feat(router): Allow for custom router outlet implementations (#40827)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.1+32.sha-09e1e19"
+                version: "~12.0.0-next.1+16.sha-a82fddf"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
