e620967c587f5f2001d031b8aa3d71ca6bcd40d3
atscott <atscott01@gmail.com>
2021-02-22 17:06:17 +0000

    0d5fbea5dc refactor(zone.js): update Object.create params to match web platform (#34287)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.1+33.sha-ff86f91"
+                version: "~11.2.1+36.sha-0d5fbea"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
