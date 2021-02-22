324fe90d62b8e4f4306562533a50f88e6b9e38df
Andrew Scott <atscott@google.com>
2021-02-22 21:39:14 +0000

    73a3ff11cb perf(language-service): short-circuit LS operations (#40946)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.1+37.sha-8beead1"
+                version: "~11.2.1+39.sha-73a3ff1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
