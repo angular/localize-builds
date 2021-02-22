fbfc2961cab3c906f5b3866be773e6538f395bf6
atscott <atscott01@gmail.com>
2021-02-22 23:38:47 +0000

    09b92d755a refactor(compiler-cli): separate out constant target of g3 patch (#40950)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.1+40.sha-263bbd4"
+                version: "~11.2.2+1.sha-09b92d7"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
