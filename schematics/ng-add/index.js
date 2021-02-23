d8b91733ca5ce26e33f0bf64dcc6030cd33bc2e6
atscott <atscott01@gmail.com>
2021-02-23 19:10:48 +0000

    1f7921be33 docs: improve accessibility of tohpt3 (#40617)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.1+55.sha-1f91672"
+                version: "~12.0.0-next.1+54.sha-1f7921b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
