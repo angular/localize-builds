05bbdf9fda960cdd9d3a88df8cc78bf5b98c7eff
atscott <atscott01@gmail.com>
2021-02-23 16:35:43 +0000

    d3705b3284 fix(common): avoid mutating context object in NgTemplateOutlet (#40360)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.1+45.sha-ddf7970"
+                version: "~12.0.0-next.1+47.sha-d3705b3"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
