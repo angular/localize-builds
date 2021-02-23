3f748d3f592ce05ec2639a1163114dca3f26440c
atscott <atscott01@gmail.com>
2021-02-23 21:44:50 +0000

    db28ed2341 refactor(language-service): add type guard for NgLanguageService (#40954) (#40968)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.2+10.sha-c6e2234"
+                version: "~11.2.2+13.sha-db28ed2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
