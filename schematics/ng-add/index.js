0df3364dacfac9aa5d9f4623f042447c99aac2d7
atscott <atscott01@gmail.com>
2021-02-23 19:07:44 +0000

    4d1299bd82 docs: improve accessibility of toh-pt5 example (#40632)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.1+48.sha-49e02ca"
+                version: "~12.0.0-next.1+52.sha-4d1299b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
