7023de6d303c07d4b79c9b6f53aa7d5df597a66e
Zach Arend <zachzach@google.com>
2021-02-24 22:45:04 +0000

    ca271b84fc test(localize): ensure tests pass on Windows (#40952)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.2+26.sha-d7133a8"
+                version: "~11.2.3+3.sha-ca271b8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
