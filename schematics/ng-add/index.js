496164364d42194e96034a3095c72a1e8d24506e
Zach Arend <zachzach@google.com>
2021-02-24 23:30:29 +0000

    29d8a0ab09 feat(animations): add support for disabling animations through BrowserAnimationsModule.withConfig (#40731)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.2+5.sha-3c24136"
+                version: "~12.0.0-next.2+6.sha-29d8a0a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
