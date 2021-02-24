261edbd5c63bce5abe55f718874113c9050730e5
Zach Arend <zachzach@google.com>
2021-02-24 23:27:02 +0000

    55eb7b5681 fix(compiler): ensure JIT compilation of ɵɵngDeclarePipe() works (#40929)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.3+3.sha-ca271b8"
+                version: "~11.2.3+4.sha-55eb7b5"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
