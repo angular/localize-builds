17e3ef0e642aa8851e98efa6a0ec56b0fc6d9e44
Zach Arend <zachzach@google.com>
2021-02-24 23:51:18 +0000

    6c05c80f19 feat(router): Add more find-tuned control in `routerLinkActiveOptions` (#40303)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.2+6.sha-29d8a0a"
+                version: "~12.0.0-next.2+7.sha-6c05c80"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
