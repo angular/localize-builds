c11b2eafed1e02ed395d9a7e51561fad8ccb3ddf
atscott <atscott01@gmail.com>
2021-02-24 17:13:41 +0000

    3fe759eb6c test(language-service): remove circular dependency (#40966)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.2+15.sha-3551434"
+                version: "~11.2.2+25.sha-3fe759e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
