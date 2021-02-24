f9f717adf2fe8735f6d2ba62e1edf414d492e324
Andrew Scott <atscott@google.com>
2021-02-24 16:20:19 +0000

    aaf9b31fb4 feat(core): drop support for zone.js 0.10.x (#40823)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.1+60.sha-03de2f2"
+                version: "~12.0.0-next.1+63.sha-aaf9b31"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
