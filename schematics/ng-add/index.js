9579d4e3c3ef2f2eb4143987cdfa3ed4353e51a3
Andrew Scott <atscott@google.com>
2021-02-24 16:16:44 +0000

    03de2f2b78 docs: fix ch 4 so it compiles in strict mode (#40970)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.1+59.sha-011a527"
+                version: "~12.0.0-next.1+60.sha-03de2f2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
