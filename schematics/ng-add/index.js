101e228642feb4c228efb1f192e6636e79650c24
Zach Arend <zachzach@google.com>
2021-02-26 16:52:20 +0000

    383d226118 fix(docs-infra): add links to glossary for terms on the "Press kit" page (#40945)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.2+10.sha-284af73"
+                version: "~12.0.0-next.2+13.sha-383d226"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
