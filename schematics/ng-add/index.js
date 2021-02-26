8214728492008e3397ad558b0d01862f4948b141
Zach Arend <zachzach@google.com>
2021-02-26 16:44:54 +0000

    953e98e211 docs: Change grammatically in security guide (#40995)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.2+7.sha-6c05c80"
+                version: "~12.0.0-next.2+9.sha-953e98e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
