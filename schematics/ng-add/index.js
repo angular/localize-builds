2ef6af96d174239d6ef8b42c0378fc5f1da27144
Zach Arend <zachzach@google.com>
2021-02-26 20:57:35 +0000

    91cdc11aa0 fix(common): allow number or boolean as http params (#40663)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.2+13.sha-383d226"
+                version: "~12.0.0-next.2+14.sha-91cdc11"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
