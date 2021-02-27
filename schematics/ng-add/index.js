807387aed3c7fc561a5036b2424cd6d28b0e0913
Zach Arend <zachzach@google.com>
2021-02-27 00:01:21 +0000

    bebdeba5fd docs: improve description, examples of DecimalPipe's digitsInfo parameter (#40714)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.2+29.sha-272b564"
+                version: "~12.0.0-next.2+33.sha-bebdeba"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
