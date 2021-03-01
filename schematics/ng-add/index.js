37cdd5fb9a6ee7d68746d692055aaabbecc357ce
Zach Arend <zachzach@google.com>
2021-03-01 23:53:26 +0000

    6c783c7fcb docs: rewrite structural-directives.md (#40015)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.2+40.sha-356d9a0"
+                version: "~12.0.0-next.2+41.sha-6c783c7"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
