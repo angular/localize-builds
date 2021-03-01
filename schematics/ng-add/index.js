87526808da949610187405b760ef1e28300b7d1c
Zach Arend <zachzach@google.com>
2021-03-01 17:21:57 +0000

    e12d9dec64 fix(core): remove duplicated EMPTY_ARRAY constant (#40991)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.2+34.sha-be8893f"
+                version: "~12.0.0-next.2+35.sha-e12d9de"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
