45c83a0ee536998361512aacd96c876492d82cf5
Zach Arend <zachzach@google.com>
2021-03-02 00:07:05 +0000

    3370ade9a4 refactor(platform-browser): cast response of JSON.parse (#41020)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.2+41.sha-6c783c7"
+                version: "~12.0.0-next.2+43.sha-3370ade"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
