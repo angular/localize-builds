98b02a559fd21841f8d6f58692d948c53a196398
Zach Arend <zachzach@google.com>
2021-03-03 18:07:02 +0000

    21f0deeaa6 build(docs-infra): update Angular framework, Material and CLI to latest methods (#40994)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.2+47.sha-10aa564"
+                version: "~12.0.0-next.2+49.sha-21f0dee"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
