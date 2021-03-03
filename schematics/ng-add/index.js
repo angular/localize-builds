d34ab2e8c34020c379a415322911f1fd6006eb74
Zach Arend <zachzach@google.com>
2021-03-03 18:07:47 +0000

    e9e7c33f3c fix(language-service): Add plugin option to force strictTemplates (#41062)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.2+49.sha-21f0dee"
+                version: "~12.0.0-next.2+50.sha-e9e7c33"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
