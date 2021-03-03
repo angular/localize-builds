268bd04d48bf7da41ac25f404c4e5c6bb1521ac2
Zach Arend <zachzach@google.com>
2021-03-03 17:54:34 +0000

    5eab6e14f1 docs: fix typo in router.md file (#41064) PR Close #41064

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.2+44.sha-96f15f0"
+                version: "~12.0.0-next.2+45.sha-5eab6e1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
