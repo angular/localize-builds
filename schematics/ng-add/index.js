14ffa8211b67180ccbe866e62b07ec3f64c07c28
Zach Arend <zachzach@google.com>
2021-03-03 21:33:51 +0000

    0847a0353b fix(language-service): Always attempt HTML AST to template AST conversion for LS (#41068)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.2+55.sha-7765b64"
+                version: "~12.0.0-next.2+57.sha-0847a03"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
