0a41d339d77ec38be0cdfac2897854e891d3a4d2
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:20:02 +0000

    ad40fcae7a docs: improve documentation on how to pass a stringified parameter list to HttpClient (#41010)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.3+2.sha-9ae0faa"
+                version: "~12.0.0-next.3+3.sha-ad40fca"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
