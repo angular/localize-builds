ab5716b6142886767fee8a7c4314b6e3a8ad5585
Andrew Kushnir <akushnir@google.com>
2021-03-04 19:27:57 +0000

    1d4fc94801 refactor(compiler): remove unused `__BUILD_OPTIMIZER_*` constants (#41040)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.4+3.sha-4f4454d"
+                version: "~11.2.4+11.sha-1d4fc94"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
