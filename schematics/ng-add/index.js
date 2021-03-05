d1147d9d2b01eebb5dcdf0f88778d82fe9c4407b
Andrew Kushnir <akushnir@google.com>
2021-03-05 18:06:32 +0000

    e7453f15d5 docs: fix issue #40941 (#40942)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.3+16.sha-4637df5"
+                version: "~12.0.0-next.3+17.sha-e7453f1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
