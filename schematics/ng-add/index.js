784585956bfdde8019e8601bb87300a62c6591a4
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:51:50 +0000

    03d47d5701 feat(core): manually annotate de-sugarred core tree-shakable providers with @pureOrBreakMyCode (#41096)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.3+33.sha-1735430"
+                version: "~12.0.0-next.3+36.sha-03d47d5"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
