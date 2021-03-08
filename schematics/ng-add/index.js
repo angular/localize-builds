f9c47bb6ac1f943a65d95a53daa32f198343b489
Andrew Kushnir <akushnir@google.com>
2021-03-08 23:52:11 +0000

    2ebe2bcb2f refactor(compiler): move factory out of injector definition (#41022)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.3+40.sha-bdf13fe"
+                version: "~12.0.0-next.3+42.sha-2ebe2bc"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
