2b86dc2afbef78cf0bcda573cfaead6c27f3068d
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:27:31 +0000

    1735430476 feat(localize): add scripts to migrate away from legacy message IDs (#41026)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.3+32.sha-531f0bf"
+                version: "~12.0.0-next.3+33.sha-1735430"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
