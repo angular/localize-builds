fab0174b1baec531275a4a4148b22aa2e7fb5bb8
Andrew Kushnir <akushnir@google.com>
2021-03-08 17:12:07 +0000

    681c3417fe docs: add Discord, YouTube, StackOverflow to the "Community" section in `README.md` (#41086)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.3+19.sha-13681d9"
+                version: "~12.0.0-next.3+25.sha-681c341"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
