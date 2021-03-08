ac08a18a8fa519d82f90ea06dd415a0fbcef9873
Andrew Kushnir <akushnir@google.com>
2021-03-08 20:41:43 +0000

    bdf13fe376 docs(compiler-cli): add README.md for template type checking system (#41004)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.3+37.sha-153e3a8"
+                version: "~12.0.0-next.3+40.sha-bdf13fe"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
