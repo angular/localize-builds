571dee5633f1f74249dfe6e845ee3368b3890de7
Andrew Kushnir <akushnir@google.com>
2021-03-08 18:27:48 +0000

    925746b6d8 fix(bazel): fix incorrect rollup plugin method signature (#41101)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.4+24.sha-e35ecea"
+                version: "~11.2.4+26.sha-925746b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
