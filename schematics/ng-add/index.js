f8b5ed90577bb46184150945f3ae699f3f5e7d2a
Andrew Kushnir <akushnir@google.com>
2021-03-09 00:23:40 +0000

    fa3689f432 test(forms): split Forms example app into Reactive and Template-driven ones (#41108)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.3+42.sha-2ebe2bc"
+                version: "~12.0.0-next.3+43.sha-fa3689f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
