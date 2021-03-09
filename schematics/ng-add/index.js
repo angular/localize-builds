145cabcbb3dd67ac64e47a7cc694d22a21a6d7f2
Andrew Kushnir <akushnir@google.com>
2021-03-09 19:02:27 +0000

    16f90cac4b perf(compiler-cli): ensure module resolution cache is reused for type-check program (#39693)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.4+50.sha-e7ce857"
+                version: "~11.2.4+51.sha-16f90ca"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
