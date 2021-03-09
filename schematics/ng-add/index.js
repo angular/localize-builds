136d8df768cd26a93f2efc1987a479386656bc6b
Andrew Kushnir <akushnir@google.com>
2021-03-09 17:25:16 +0000

    e7ce857bc6 docs(common): change misspelled constant (#41136)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.4+31.sha-c5ead38"
+                version: "~11.2.4+50.sha-e7ce857"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
