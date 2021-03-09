9b69c19473ce7b30762341293f99df699de3de4e
Andrew Kushnir <akushnir@google.com>
2021-03-09 22:16:43 +0000

    41b8f98744 build: update the lock closed sha to the latest sha (#41140)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.3+61.sha-b50d880"
+                version: "~12.0.0-next.3+62.sha-41b8f98"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
