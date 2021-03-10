7da0eed97bc56bad6be7d1bfca5f1dc477dae1d5
Andrew Kushnir <akushnir@google.com>
2021-03-10 19:43:19 +0000

    4c79b8a644 docs: Remove of unnecessary Input (#41150)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.4+2.sha-ba81e8c"
+                version: "~12.0.0-next.4+3.sha-4c79b8a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
