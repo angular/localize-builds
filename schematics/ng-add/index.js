ea9052ed455b9c05b94f478750f07c4481c1bbea
Andrew Kushnir <akushnir@google.com>
2021-03-10 19:25:48 +0000

    ba81e8cc94 build: remove commit message max line length limit (#41157)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.3+64.sha-1fb0dbb"
+                version: "~12.0.0-next.4+2.sha-ba81e8c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
