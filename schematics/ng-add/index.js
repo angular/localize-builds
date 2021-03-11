deab4eacb3d5956745546d865d0fead749a45b3b
Andrew Kushnir <akushnir@google.com>
2021-03-11 20:50:36 +0000

    74aba139ed fix(docs-infra): fix angular.io on IE 11 (#41162) (#41184)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.5+7.sha-ed6c09a"
+                version: "~11.2.5+9.sha-74aba13"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
