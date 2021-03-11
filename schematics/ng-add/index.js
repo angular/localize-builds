7b49a24c2b399312fc25909eac02ef8aa04fcfaf
Andrew Kushnir <akushnir@google.com>
2021-03-11 18:49:15 +0000

    ed6c09ae45 fix(localize): trim extracted `equiv-text` values (#41180)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.5+6.sha-78e6713"
+                version: "~11.2.5+7.sha-ed6c09a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
