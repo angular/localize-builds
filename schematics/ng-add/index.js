6778a671b5abe36216fff56f14e0942baf6cbb80
Andrew Kushnir <akushnir@google.com>
2021-03-11 17:52:46 +0000

    78e67130f7 docs: add stepan suvorov to GDE resources (#41178)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.5+4.sha-2fa231a"
+                version: "~11.2.5+6.sha-78e6713"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
