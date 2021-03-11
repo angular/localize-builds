fb161ded9902b077fa073976aa35286aacfdc840
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-11 23:22:20 +0000

    012a2b55e1 build(language-service): use 'export =' syntax for default export (#41165)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.4+10.sha-69afeb3"
+                version: "~12.0.0-next.4+11.sha-012a2b5"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
