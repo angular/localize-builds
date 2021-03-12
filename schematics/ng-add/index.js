d16868711fa64af09e638a965100c6cbbee07cab
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-12 22:08:51 +0000

    e3ea8630bc ci: remove `CHANGELOG.md` from the `public-api` group (#41135)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.4+19.sha-cfaf425"
+                version: "~12.0.0-next.4+20.sha-e3ea863"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
