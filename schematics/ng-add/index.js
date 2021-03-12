6f970639638aad779ae647c37bf96884d3b0ab84
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-12 22:42:27 +0000

    8a9fe49a2a fix(forms): properly handle the change to the FormGroup shape (#40829)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.4+20.sha-e3ea863"
+                version: "~12.0.0-next.4+21.sha-8a9fe49"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
