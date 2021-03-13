771bed4c936fb6cfbebc7b0942ee0e95effee9e4
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-13 00:13:56 +0000

    937e90cd16 perf(forms): make built-in ControlValueAccessors more tree-shakable (#41146)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.4+21.sha-8a9fe49"
+                version: "~12.0.0-next.4+22.sha-937e90c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
