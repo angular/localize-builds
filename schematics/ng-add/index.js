45d495ccbf146bbe9a6828692293f2aa9c931db3
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 18:33:26 +0000

    da5f5a4910 docs: add youtube to social icons (#40987) (#41202)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.5+19.sha-b448412"
+                version: "~11.2.5+20.sha-da5f5a4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
