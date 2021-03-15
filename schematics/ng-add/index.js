1b52356078d01c8ce36d5e82bd604329c9cc5654
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-15 18:37:18 +0000

    3bd1992218 perf(forms): avoid direct references to the `Validators` class (#41189)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.4+24.sha-1644d64"
+                version: "~12.0.0-next.4+25.sha-3bd1992"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
