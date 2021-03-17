c6f9335875b3197b1ba6cf70d862ace9a028cff2
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:24:51 +0000

    78038027fc docs: update assets configuration to include `followSymlinks` option (#41238)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.5+28.sha-18cd7bb"
+                version: "~11.2.5+30.sha-7803802"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
