1c79255b6b1989e76ed43404358655553f12e9d0
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:32:58 +0000

    eb74a96935 refactor(compiler-cli): separate used components from used directives in partial declaration (#41104)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.4+47.sha-98de4cb"
+                version: "~12.0.0-next.4+48.sha-eb74a96"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
