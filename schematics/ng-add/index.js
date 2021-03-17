3c36196e78cb6868b6b1e095593914c4341a5dd0
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:28:42 +0000

    98de4cb1bf docs: add deprecation note next to IE 11 in browser-support page (#41147)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.4+44.sha-0f0f34f"
+                version: "~12.0.0-next.4+47.sha-98de4cb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
