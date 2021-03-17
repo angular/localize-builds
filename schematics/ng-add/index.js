d5a684b775d6bf4361bd1f5371ce106f87ab850b
Jessica Janiuk <jessicajaniuk@google.com>
2021-03-17 16:37:40 +0000

    fa048948be feat(core): drop support for TypeScript 4.0 and 4.1 (#41158)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.4+48.sha-eb74a96"
+                version: "~12.0.0-next.4+50.sha-fa04894"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
