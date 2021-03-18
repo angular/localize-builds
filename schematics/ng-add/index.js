4761e485c60c3f53bd34cfc693367e14ffcb4705
Misko Hevery <misko@hevery.com>
2021-03-18 17:35:42 +0000

    ac9a250fc4 docs: update link to schematics schema (#41258)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.5+6.sha-ac9c428"
+                version: "~12.0.0-next.5+7.sha-ac9a250"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
