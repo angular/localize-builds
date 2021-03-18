e71667202c1531532bb69a7c31a1e6f1a355cde5
Misko Hevery <misko@hevery.com>
2021-03-18 17:39:24 +0000

    e23994191c docs: make `HeroService#gerHero()` in `toh-pt5` compatible with TS strict mode (#41234)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.5+7.sha-ac9a250"
+                version: "~12.0.0-next.5+5.sha-e239941"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
