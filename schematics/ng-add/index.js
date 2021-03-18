76f59711da3e35ed97d2af0393585cbbb97bc7d4
Misko Hevery <misko@hevery.com>
2021-03-18 17:33:37 +0000

    81d2aabcbb docs: add preston lamb to GDE resources (#41204)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.4+52.sha-02e8901"
+                version: "~12.0.0-next.5+3.sha-81d2aab"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
