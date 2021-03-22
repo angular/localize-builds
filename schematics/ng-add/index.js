03c148445f78e3f79a527e32d599341d9a5276cf
Misko Hevery <misko@hevery.com>
2021-03-22 20:06:58 +0000

    b5551609fe fix(router): fragment can be null (#37336)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.5+23.sha-e7b1d43"
+                version: "~12.0.0-next.5+24.sha-b555160"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
