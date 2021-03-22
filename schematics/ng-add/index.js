55a32cd71ee7e6810288b53268defbc9c254abb0
Misko Hevery <misko@hevery.com>
2021-03-22 16:19:29 +0000

    e7b1d434c8 refactor(compiler): use `ɵɵInjectorDeclaration` rather than `ɵɵInjectorDef` in compiled output (#41119)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.5+19.sha-efe02d8"
+                version: "~12.0.0-next.5+23.sha-e7b1d43"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
