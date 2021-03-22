c95eec60cf9251712e8df9dd88cb97e63d97855c
Misko Hevery <misko@hevery.com>
2021-03-22 16:16:55 +0000

    ed5bfe8173 docs: simplify deleteHero method in tutorial (#41303)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.5+18.sha-5bc0a47"
+                version: "~12.0.0-next.5+16.sha-ed5bfe8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
