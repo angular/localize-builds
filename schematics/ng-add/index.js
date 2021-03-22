819b54a347cd5f00bca74d0212e9b3f289faf510
Misko Hevery <misko@hevery.com>
2021-03-22 16:15:52 +0000

    5bc0a472bd refactor(docs-infra): remove image size exclusion list (#41292)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.5+15.sha-5eb7f34"
+                version: "~12.0.0-next.5+18.sha-5bc0a47"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
