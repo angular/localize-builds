6041264e789993b5a8f17adbdf2a3b12540323d0
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:12:30 +0000

    18cd7a0c69 fix(language-service): show suggestion when type inference is suboptimal (#41072)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.6+17.sha-17c5dc7"
+                version: "~11.2.6+22.sha-18cd7a0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
