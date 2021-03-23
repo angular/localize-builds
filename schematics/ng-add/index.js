e0d65d9b81e658504a3fb599b322f64270c42d4c
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:37:28 +0000

    6a6d332c06 docs: add Alexander Inkin to GDE list (#41310)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.5+30.sha-1eba57e"
+                version: "~12.0.0-next.5+32.sha-6a6d332"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
