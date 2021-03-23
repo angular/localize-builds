88a3d871a825950052e8a78328efe544050c413c
Joey Perrott <josephperrott@gmail.com>
2021-03-23 17:11:41 +0000

    e340d190bd docs: add some boilerplate button, h3, and hr styles (#41052)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.5+26.sha-31dc483"
+                version: "~12.0.0-next.5+27.sha-e340d19"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
