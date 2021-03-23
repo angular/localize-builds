0b275edbc6537902fc2c6f01b2d0be7eef18a3d5
Joey Perrott <josephperrott@gmail.com>
2021-03-23 20:33:27 +0000

    0516fbb180 refactor(dev-infra): use conventional-commits-parser for commit parsing (#41286)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.5+36.sha-eba1289"
+                version: "~12.0.0-next.5+37.sha-0516fbb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
