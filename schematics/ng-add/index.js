99997dc886af5d6533b591a0195f8567a06a1d28
Joey Perrott <josephperrott@gmail.com>
2021-03-23 18:42:10 +0000

    9cb11758c1 docs: add link to fork repo how-to (#41266)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.6+22.sha-18cd7a0"
+                version: "~11.2.6+24.sha-9cb1175"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
