35002b64e85e3c4ae244197d70a73a20a2e60a94
Joey Perrott <josephperrott@gmail.com>
2021-03-23 20:28:14 +0000

    a68f6b0ca6 docs: format browser-support tables for GitHub markdown (#41327)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.6+24.sha-9cb1175"
+                version: "~11.2.6+25.sha-a68f6b0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
