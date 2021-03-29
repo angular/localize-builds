83d06f67e00b74cd02370fce0bc436055742bd11
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-29 22:38:04 +0000

    6dcea34c24 refactor(compiler): option to include html comments in `ParsedTemplate` (#41251)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.6+28.sha-2d183f6"
+                version: "~11.2.7+5.sha-6dcea34"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
