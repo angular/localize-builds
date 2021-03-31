a8bc88252d8cd6ac60fcf38f1b99817fc80ec630
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 00:11:59 +0000

    2d3cd2b969 refactor(compiler): rename `R3FactoryTarget` to `FactoryTarget` (#41231)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.6+10.sha-d35751d"
+                version: "~12.0.0-next.6+18.sha-2d3cd2b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
