7846899bcb203e6b0b9ddd5677cbd80dad0a04b5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-03-31 00:18:11 +0000

    31370f4103 feat(bazel): allow setting `compilationMode` in `ng_module` rule (#41366)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.6+20.sha-9f31cdd"
+                version: "~12.0.0-next.6+21.sha-31370f4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
