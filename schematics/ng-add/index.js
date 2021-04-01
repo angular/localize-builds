ceab99c5d58adcf3c1ba6bab3759e33ba4ca5fec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:20:24 +0000

    a5a3752859 docs: fix misspelled word in VSCode Remote Development guide (#41376)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.6+43.sha-c6d9a19"
+                version: "~12.0.0-next.6+44.sha-a5a3752"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
