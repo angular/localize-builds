eb9f725579410e3798c88c4b7c3f168a028c6833
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:58:02 +0000

    3470ea17d5 fix(docs-infra): increase the max width of the `file-not-found` page (#41275)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.6+38.sha-db55f8e"
+                version: "~12.0.0-next.6+40.sha-3470ea1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
