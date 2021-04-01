47383db3a2f91279486b762ebd5620f6c49c338b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 18:44:57 +0000

    18bc9ffb51 ci: do not try to publish directories that are not packages (#41393)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.6+34.sha-6391a4a"
+                version: "~12.0.0-next.6+36.sha-18bc9ff"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
