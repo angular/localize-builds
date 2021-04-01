52c2ccfe206ddfddb96b6f28afcb99465e1e0d1b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:02:34 +0000

    ff9470b0a0 fix(compiler): include used components during JIT compilation of partial component declaration (#41353)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.6+41.sha-0226a11"
+                version: "~12.0.0-next.6+42.sha-ff9470b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
