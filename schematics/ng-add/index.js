34d50510e7d32e0cb8cd081f8c6777e597bc8593
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-04-01 19:27:56 +0000

    c4af49335c docs(forms): fix the short description for `DefaultValueAccessor` (#41409)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.6+44.sha-a5a3752"
+                version: "~12.0.0-next.6+48.sha-c4af493"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
