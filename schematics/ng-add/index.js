21ff895b3290eb9f2e9ef66123db6d718eeda2ec
atscott <atscott01@gmail.com>
2021-04-02 23:09:05 +0000

    64b979e12b fix(dev-infra): exclude node_modules from circular dependency checks (#41407)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.7+5.sha-03a46af"
+                version: "~12.0.0-next.7+7.sha-64b979e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
