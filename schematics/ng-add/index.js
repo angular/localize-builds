3e2ecfd77f9e884f1fdbb3c9ba2816063323bb64
atscott <atscott01@gmail.com>
2021-04-02 23:10:25 +0000

    dc655262be  refactor(compiler): remove TypeScript 4.0 program reuse check (#41406)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.7+7.sha-64b979e"
+                version: "~12.0.0-next.7+8.sha-dc65526"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
