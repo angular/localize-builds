f36dc7ff5b7e8b398b7bc3f48fa50f52dda8c87f
atscott <atscott01@gmail.com>
2021-04-02 17:49:26 +0000

    dc8a0818e7 fix(dev-infra): require npm login for all publishes via wombat proxy (#41422)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.7+33.sha-154129b"
+                version: "~11.2.8+2.sha-dc8a081"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
