a3b529ef0e58c020577fbb5e12f563e1eb4e25bc
atscott <atscott01@gmail.com>
2021-04-05 16:19:35 +0000

    2cef385e43 feat(dev-infra): prevent `git push` from being called in dryRun mode (#41387)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.7+11.sha-ab47f41"
+                version: "~12.0.0-next.7+14.sha-2cef385"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
