ca5e52ccf929b294bf8d8c59fd2f40f2d01089ae
atscott <atscott01@gmail.com>
2021-04-05 15:37:12 +0000

    103f05ad54 docs: fix grammar mistake in `glossary.md` ("can exported" --> "can be exported") (#41440)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.8+3.sha-d4f739f"
+                version: "~11.2.8+6.sha-103f05a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
