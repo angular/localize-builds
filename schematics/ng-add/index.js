b57bc21d21a9b8d5b0630a7a183b83a514b7de6c
atscott <atscott01@gmail.com>
2021-04-06 20:07:12 +0000

    ff9253b4a6 ci: add cache for windows run (#41467)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.7+23.sha-ba3344d"
+                version: "~12.0.0-next.7+32.sha-ff9253b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
