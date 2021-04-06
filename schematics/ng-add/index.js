b3b34adc7b8443e6e118d5d5589581f419535205
atscott <atscott01@gmail.com>
2021-04-06 20:06:25 +0000

    3f12a0dbfa build: update package.json engines range for nodes in preparation for upgrade (#41472)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.8+23.sha-bc74735"
+                version: "~11.2.8+26.sha-3f12a0d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
