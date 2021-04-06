75ef48185153d3a625b55ac7c36c0baaf38d891a
atscott <atscott01@gmail.com>
2021-04-06 17:29:07 +0000

    f7c294ee0f feat(core): support `forwardRef` in `providedIn` of `Injectable` declaration (#41426)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.7+25.sha-575f537"
+                version: "~12.0.0-next.7+22.sha-f7c294e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
