589cc3a4002331690a09f0f0a841938465a21364
atscott <atscott01@gmail.com>
2021-04-06 08:39:00 +0000

    3a823abcc5 refactor(compiler): remove dependency on fs-extra (#41445)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.7+16.sha-2a76d84"
+                version: "~12.0.0-next.7+18.sha-3a823ab"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
