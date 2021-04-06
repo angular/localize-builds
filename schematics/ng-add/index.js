223bd1517d20dba7a267b96448b4082655333f85
atscott <atscott01@gmail.com>
2021-04-06 18:54:59 +0000

    bc74735644 build: correctly publish `angular-in-memory-web-api` as CI build artifact (#41429) (#41471)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.8+17.sha-0eecbed"
+                version: "~11.2.8+23.sha-bc74735"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
