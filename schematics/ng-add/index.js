d79142c49626049e2460c04f73e07e2b9c88a224
atscott <atscott01@gmail.com>
2021-04-07 17:41:38 +0000

    f8986e1a53 fix(dev-infra): make scopes for `test` and `refactor` commit types optional (#41486)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.8+26.sha-3f12a0d"
+                version: "~11.2.8+30.sha-f8986e1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
