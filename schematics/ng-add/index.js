be10faade6c37be786ef774aac112b8c5152b78a
atscott <atscott01@gmail.com>
2021-04-07 19:11:46 +0000

    5a81490554 docs: use the right tag in the HeroesComponent's template snippet (#41473)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.7+40.sha-4b8bbe2"
+                version: "~12.0.0-next.8+1.sha-5a81490"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
