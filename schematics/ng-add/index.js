240c6899353f2682ca0338e9eacf0f63d32afa90
atscott <atscott01@gmail.com>
2021-04-07 19:15:56 +0000

    d641542587 refactor(core): remove deprecated rxjs signature usage in event emitter and make more minifier-friendly (#41450)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+6.sha-7a49aa5"
+                version: "~12.0.0-next.8+7.sha-d641542"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
