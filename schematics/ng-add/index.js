addb947d7f45df4f1d4cf4cf96e2f5d3fa63a9ef
atscott <atscott01@gmail.com>
2021-04-07 16:58:16 +0000

    dd66da1864 release: bump angular-in-web-memory-api package to 0.12.0 (#41487)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.7+33.sha-4a68e13"
+                version: "~12.0.0-next.7+34.sha-dd66da1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
