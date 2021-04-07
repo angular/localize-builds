82ac1c81f74107985a315d9d00796fb5e7c2ec47
atscott <atscott01@gmail.com>
2021-04-07 18:12:32 +0000

    ae6c195c87 docs: fix link URL in NG0100 error page (#41484)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.7+38.sha-8f12f47"
+                version: "~12.0.0-next.7+39.sha-ae6c195"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
