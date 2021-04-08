e70317dca7bbdeb821b2ed628cce97c120d220b0
Zach Arend <zachzach@google.com>
2021-04-08 17:43:39 +0000

    deacc741e0 fix(compiler-cli): ensure the compiler tracks `ts.Program`s correctly (#41291)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+18.sha-44a7fae"
+                version: "~12.0.0-next.8+17.sha-deacc74"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
