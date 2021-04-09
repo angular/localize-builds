e26f02378d24af6e89c908e2a0deba9fe8c0c3d9
Zach Arend <zachzach@google.com>
2021-04-09 14:53:19 +0000

    e34299af25 fix(router): handle new navigations from a NavigationEnd event (#41262) (#41511)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.9+13.sha-74d1769"
+                version: "~11.2.9+14.sha-e34299a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
