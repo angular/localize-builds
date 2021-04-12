62c3e5c00cb1314607a81c4b3cb4de4e51a6c935
Zach Arend <zachzach@google.com>
2021-04-12 17:52:13 +0000

    419c189ad3 fix(core): error if DebugRenderer2.destroyNode is called twice in a row (#41565)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.9+17.sha-7946f63"
+                version: "~11.2.9+21.sha-419c189"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
