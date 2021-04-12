9888a2a178d74b1d15aa32033fafc22520514d99
Zach Arend <zachzach@google.com>
2021-04-12 17:56:39 +0000

    42e3a5241d test(compiler-cli): add integration test for relative rootDir (#41359)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+32.sha-aa0e54f"
+                version: "~12.0.0-next.8+35.sha-42e3a52"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
