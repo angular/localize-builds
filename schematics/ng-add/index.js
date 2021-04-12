ae92e561104b6476cdb5fefc2a189005ce3a18af
Zach Arend <zachzach@google.com>
2021-04-12 17:55:35 +0000

    6ba67c6fff feat(compiler-cli): mark ability to use partial compilation mode as stable (#41518)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+35.sha-42e3a52"
+                version: "~12.0.0-next.8+33.sha-6ba67c6"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
