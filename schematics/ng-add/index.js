9b028715f6551abe0720d4317b02a4cd0fb93f57
Zach Arend <zachzach@google.com>
2021-04-13 04:28:58 +0000

    0f54d6c4a5 fix(language-service): use 'any' instead of failing for inline TCBs (#41513)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+52.sha-bfbdb8f"
+                version: "~12.0.0-next.8+50.sha-0f54d6c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
