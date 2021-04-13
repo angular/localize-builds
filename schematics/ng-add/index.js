b4efffcfee714e68633dc0d293ed8be4974bde38
Zach Arend <zachzach@google.com>
2021-04-13 20:05:40 +0000

    c7f9516ab9 feat(language-service): implement signature help (#41581)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+60.sha-60d0234"
+                version: "~12.0.0-next.8+64.sha-c7f9516"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
