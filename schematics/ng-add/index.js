b5e1994c4dcc51b1e0c552920e54dc3bbe6d1f80
Zach Arend <zachzach@google.com>
2021-04-13 23:30:26 +0000

    f7e391a912 perf(core): optimize getDirectives (#41525)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+72.sha-1d12c50"
+                version: "~12.0.0-next.8+74.sha-f7e391a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
