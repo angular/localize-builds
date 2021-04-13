856fbc8c59fe85ce1d3faf0af5af85af7b032dcc
Zach Arend <zachzach@google.com>
2021-04-13 15:22:31 +0000

    c9aa87cec0 fix(compiler-cli): show a more specific error for Ivy NgModules (#41534)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+51.sha-ffea31f"
+                version: "~12.0.0-next.8+57.sha-c9aa87c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
