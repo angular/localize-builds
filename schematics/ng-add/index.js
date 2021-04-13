ac0a753ef8c5ad0d9c4d3dd9f09269fd862ae000
Zach Arend <zachzach@google.com>
2021-04-13 04:32:54 +0000

    3a61bb82b3 docs: replaced erroneous instances of `HeroDetailsComponent` with `HeroDetailComponent` (#41569) PR Close #41569

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+53.sha-d27c8b4"
+                version: "~12.0.0-next.8+54.sha-3a61bb8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
