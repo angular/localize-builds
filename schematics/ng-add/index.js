69f7ec963ad3f7f37a5bb3fbfe4940bc14544389
Zach Arend <zachzach@google.com>
2021-04-13 20:29:09 +0000

    78236bfdca fix(language-service): use script versions for incremental compilations (#41475)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+65.sha-71b8c9a"
+                version: "~12.0.0-next.8+69.sha-78236bf"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
