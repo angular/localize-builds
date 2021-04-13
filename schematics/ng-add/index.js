e93729eca6ec51ddfdb5dfb53a47b26d616d702e
Zach Arend <zachzach@google.com>
2021-04-13 20:28:56 +0000

    04235fa079 docs: replace Codelyzer recommendation with Angular-ESLint in accessibility guide (#41294)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.9+36.sha-4847c14"
+                version: "~11.2.9+37.sha-04235fa"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
