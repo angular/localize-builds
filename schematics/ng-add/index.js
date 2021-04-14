187ecada9c1e8bca1cf8bade9830392415993f6d
Zach Arend <zachzach@google.com>
2021-04-14 00:56:35 +0000

    fd9a7ca8c9 build: update to latest version of `rules_nodejs` v3.3.0 (#41599)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+74.sha-f7e391a"
+                version: "~12.0.0-next.8+75.sha-fd9a7ca"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
