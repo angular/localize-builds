07163f5756d917052ac8125a2e1f0798ca840ccf
Zach Arend <zachzach@google.com>
2021-04-14 23:10:32 +0000

    2af894d164 refactor: format renovate configuration with 2 spaces (#41619)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+99.sha-eaeb7ef"
+                version: "~12.0.0-next.8+101.sha-2af894d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
