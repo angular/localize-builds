f309de4a527a1bb76fe143aa0a6575105f01e522
Zach Arend <zachzach@google.com>
2021-04-14 23:18:46 +0000

    dde81ba0cd perf(compiler): reduce amount of generated code for safe accesses and nullish coalescing (#41563)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+107.sha-b649273"
+                version: "~12.0.0-next.8+106.sha-dde81ba"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
