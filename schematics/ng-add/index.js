43644f7c762cf2b90186059123da5429b91f3be5
Zach Arend <zachzach@google.com>
2021-04-14 23:12:33 +0000

    1043aa42ad build: update typescript to version 4.2.4 (#41618)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+101.sha-2af894d"
+                version: "~12.0.0-next.8+102.sha-1043aa4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
