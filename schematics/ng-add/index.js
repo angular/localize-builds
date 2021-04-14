b0ad67ebf65847014e0b299988631ce0ad744348
Zach Arend <zachzach@google.com>
2021-04-14 23:21:34 +0000

    4329ccae2c docs: add rx-angular to resources.json (#39291)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+106.sha-dde81ba"
+                version: "~12.0.0-next.8+108.sha-4329cca"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
