4bf915d500a5969ccd00e183a3b71ab37de7527c
Zach Arend <zachzach@google.com>
2021-04-14 18:05:29 +0000

    5a8bc1bfe1 build: `ng_package` no longer generate minified UMDs (#41425)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+95.sha-beafa22"
+                version: "~12.0.0-next.8+96.sha-5a8bc1b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
