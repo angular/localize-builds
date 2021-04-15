2419b66666cccf0bf0e60c80db516889aa9cbb2d
Andrew Kushnir <akushnir@google.com>
2021-04-15 19:20:44 +0000

    6bceb709df fix(router): Only retrieve stored route when reuse strategy indicates it should reattach (#30263)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+111.sha-dd4c3db"
+                version: "~12.0.0-next.8+114.sha-6bceb70"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
