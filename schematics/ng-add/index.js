dc50ac0f672882435f0580b15ed1223df6c1ee01
Andrew Kushnir <akushnir@google.com>
2021-04-15 20:09:43 +0000

    8d0511d009 ci: disable dependency updates of Angular JS packages (#41642)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+115.sha-a99aa29"
+                version: "~12.0.0-next.8+117.sha-8d0511d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
