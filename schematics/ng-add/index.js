21c450509029d91302c18d65fb94b634ce67de7d
Zach Arend <zachzach@google.com>
2021-04-15 02:13:09 +0000

    5332a4a919 refactor: update `$schema` and `id` keywords (#41574)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+108.sha-4329cca"
+                version: "~12.0.0-next.8+110.sha-5332a4a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
