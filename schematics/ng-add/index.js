8d49886dd3621630d8c9fb5dc184cb6b7006f1b6
Zach Arend <zachzach@google.com>
2021-04-15 02:08:57 +0000

    c4b0753908 test(router): add Router regression test for hash location strategy (#40409)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.10+4.sha-b616f73"
+                version: "~11.2.10+5.sha-c4b0753"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
