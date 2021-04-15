e27caaae70c1e898b3d975d7ccf3aa78551fa31a
Andrew Kushnir <akushnir@google.com>
2021-04-15 21:57:51 +0000

    9684cd36b1 docs: update copy in schematics.md (#41521)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.9+17.sha-df465c6"
+                version: "~12.0.0-next.9+20.sha-9684cd3"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
