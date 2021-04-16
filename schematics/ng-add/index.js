e0084d727a3921486a70f958c38cbd6e3f76c4f2
Andrew Kushnir <akushnir@google.com>
2021-04-16 20:54:06 +0000

    e251ece18d feat(dev-infra): prevent pr merges of features or breaking changes on rc (#41660)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.9+32.sha-ab257b3"
+                version: "~12.0.0-next.9+33.sha-e251ece"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
