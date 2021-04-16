c87124e074b9d1b87209498350658510fc46e340
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:21:41 +0000

    b23850eb1c ci: disable dependency updates of `angular-mocks` packages (#41650)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+125.sha-2bd4363"
+                version: "~12.0.0-next.8+124.sha-b23850e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
