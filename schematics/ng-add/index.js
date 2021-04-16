5b5e04cbbe15b1cacb2cceac4a69993a4a7f57ac
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:24:52 +0000

    d3edc5c0f5 test(docs-infra): run all example tests in IVY runtime (#41638)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+124.sha-b23850e"
+                version: "~12.0.0-next.8+130.sha-d3edc5c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
