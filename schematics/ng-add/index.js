e9a519237ab3bf7fdb7fba9f8152b6525ab82cd0
Andrew Kushnir <akushnir@google.com>
2021-04-16 16:16:41 +0000

    8b2b5ef903 fix(compiler-cli): autocomplete literal types in templates. (#41456) (#41645)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.9+21.sha-c85710d"
+                version: "~12.0.0-next.9+26.sha-8b2b5ef"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
