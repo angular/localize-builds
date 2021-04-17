c684c2d8da124294045a5eafd52e5235b06ca143
GitHub <noreply@github.com>
2021-04-17 10:24:57 +0000

    aa35a1a082 ci: disable automatic updates of rollup

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+133.sha-d5b13ce"
+                version: "~12.0.0-next.8+139.sha-aa35a1a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
