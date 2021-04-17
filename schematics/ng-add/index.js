a00827de995453057af65b47b437755406ef2e42
Andrew Kushnir <akushnir@google.com>
2021-04-17 00:44:42 +0000

    96175fdda2 docs: fix the property name in the example (#41410)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+135.sha-d5a8302"
+                version: "~12.0.0-next.8+136.sha-96175fd"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
