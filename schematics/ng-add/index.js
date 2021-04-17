1c60292dff315c3a3cd65943dd4ad7ad808d69ed
Andrew Kushnir <akushnir@google.com>
2021-04-17 00:47:03 +0000

    de25cdac54 docs: move lazy loading section to the best practices section (#41378)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+136.sha-96175fd"
+                version: "~12.0.0-next.8+138.sha-de25cda"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
