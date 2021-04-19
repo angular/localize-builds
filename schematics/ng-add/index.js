84cf492dea134872099cdd19b9c2128b6950081c
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:28:11 +0000

    b64b6b3944 build: update mocha to version 8.3.2 (#41699)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+154.sha-0c23c8b"
+                version: "~12.0.0-next.8+160.sha-b64b6b3"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
