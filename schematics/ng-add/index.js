64b2d4fcb548c3c6d572535a7b849cbecc6cfc1a
Andrew Kushnir <akushnir@google.com>
2021-04-19 22:43:31 +0000

    b333a1fe5c build: update firebase-tools to version 9.10.0 (#41686)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.9+75.sha-2114a13"
+                version: "~12.0.0-next.9+77.sha-b333a1f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
