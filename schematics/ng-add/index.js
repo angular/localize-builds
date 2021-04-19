1a4b18e05474374049b761b96e26a484cf78598f
Andrew Kushnir <akushnir@google.com>
2021-04-19 23:18:33 +0000

    a76f07bd13 docs: documentation of descendants property of @ContentChildren (#35927)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.10+31.sha-3631934"
+                version: "~11.2.10+35.sha-a76f07b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
