429b8905db40e7e75a8b70f75b4bb1e96a22234a
Andrew Kushnir <akushnir@google.com>
2021-04-19 21:34:02 +0000

    5e343173e8 docs: fix styling issues in example CSS (#41222)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+167.sha-d58747d"
+                version: "~12.0.0-next.8+169.sha-5e34317"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
