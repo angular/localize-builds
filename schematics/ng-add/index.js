dab924c5fb8fd5e394fdee1c2fa32f74d727ccd3
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:22:54 +0000

    6a153a2a6f build: update diff to version 5.0.0 (#41684)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+147.sha-1391323"
+                version: "~12.0.0-next.8+149.sha-6a153a2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
