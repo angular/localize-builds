987c4d31274686557c3c32708a8c62c014c19f35
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:13:38 +0000

    d0b1f99598 docs: fix broken links to CLI wiki in the "Component styles" guide (#41516)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+138.sha-de25cda"
+                version: "~12.0.0-next.8+140.sha-d0b1f99"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
