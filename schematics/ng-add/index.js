fa95ad4a61016908ca37a843e694c9a7a5e96e85
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:26:55 +0000

    0c23c8b0ac build: update image-size to version 1.0.0 (#41691)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+148.sha-185090b"
+                version: "~12.0.0-next.8+154.sha-0c23c8b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
