bdedcfe5f1328de72bb2b6c699dfd807e6ab74d4
Andrew Kushnir <akushnir@google.com>
2021-04-19 16:29:59 +0000

    322db1755a docs: add missing preposition in the animations guide (#41697)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+160.sha-b64b6b3"
+                version: "~12.0.0-next.8+159.sha-322db17"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
