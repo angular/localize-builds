edcde35a70a4a43ea99ff2d071f57d96f769d19e
Andrew Kushnir <akushnir@google.com>
2021-04-20 23:19:43 +0000

    7bd8741109 build: update io_bazel_rules_sass to version 1.32.11 (#41713)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.9+87.sha-87b5026"
+                version: "~12.0.0-next.9+88.sha-7bd8741"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
