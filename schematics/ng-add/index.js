08b0f5dac01df3372ff565abd63865d26a6e5193
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:08:07 +0000

    73824d5337 fix(compiler): not generating update instructions for ng-template inside alternate namespaces (#41669)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+185.sha-364ff96"
+                version: "~12.0.0-next.8+186.sha-73824d5"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
