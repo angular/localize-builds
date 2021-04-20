db9329b568b75c98ba499f4e3cadf8c46923606f
Andrew Kushnir <akushnir@google.com>
2021-04-20 17:01:14 +0000

    e238f2c33a docs: rewrites animation query () method use (#41710)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+179.sha-03f3dcb"
+                version: "~12.0.0-next.8+182.sha-e238f2c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
