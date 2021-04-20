176c97ac346e333edac3da1017353b5cc618832d
Andrew Kushnir <akushnir@google.com>
2021-04-20 18:59:36 +0000

    20e09177b7 docs: add margin to button boilerplate CSS for examples (#41413)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.10+38.sha-7459f6d"
+                version: "~11.2.10+39.sha-20e0917"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
