d9abb601e94b61253dabbb6fd6f71267aabdd01b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-21 23:14:57 +0000

    0830885c95 test(zone.js): should invoke XHR task even onload handler throw error. (#41562)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.9+89.sha-897d5ad"
+                version: "~12.0.0-rc.0+2.sha-0830885"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
