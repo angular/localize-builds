eb45b79cb2aac3caf5fa301fd94fd3da8fc08c23
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:03:12 +0000

    c3a512a9e9 fix(compiler-cli): match string indexed partial declarations (#41747)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+200.sha-6ca6302"
+                version: "~12.0.0-next.8+202.sha-c3a512a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
