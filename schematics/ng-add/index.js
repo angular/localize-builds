ab4718bc21139b9117283bafdca8d7830899d54c
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 00:51:50 +0000

    d061be99a9 fix(dev-infra): verify python3 is available before release (#41753)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.11+2.sha-1d1fa1a"
+                version: "~11.2.11+4.sha-d061be9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
