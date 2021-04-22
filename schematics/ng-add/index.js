afafaa8f07abd1aef57467195cb4bd1d6dcbaa02
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 17:34:48 +0000

    42c64aa376 Revert "build(docs-infra): fix `StackblitzBuilder` after `jsdom` update (#41725)" (#41768)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.11+4.sha-d061be9"
+                version: "~11.2.11+8.sha-42c64aa"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
