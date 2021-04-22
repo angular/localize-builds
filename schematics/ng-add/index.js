b22301b7194fdcca786500be1573989c6a58f00b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:09:07 +0000

    5895ddbcd1 refactor(core): add single type for injector token (#41580)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.0+10.sha-6221227"
+                version: "~12.0.0-rc.0+16.sha-5895ddb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
