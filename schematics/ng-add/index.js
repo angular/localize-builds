a0ba925776d79292164335de87359ad4234978bc
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-22 18:03:29 +0000

    64567d3d39 docs: Remove Protractor test from CI examples (#41733)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+203.sha-e0fb18a"
+                version: "~12.0.0-next.8+204.sha-64567d3"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
