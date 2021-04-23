5cf4fe3cc67fa8c96d14b6014b102949bc4c81dc
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 17:04:33 +0000

    87dc8511cc fix(upgrade): preserve $interval.flush when ngMocks is being used (#30229)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.0+16.sha-5895ddb"
+                version: "~12.0.0-rc.0+18.sha-87dc851"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
