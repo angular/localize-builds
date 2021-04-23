4eaa6ba2971c618c3551fc2ce9da67344009b60b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 18:35:30 +0000

    097fdffc91 build: use platform from shared dev-infra package for remote execution (#41767)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.0+18.sha-87dc851"
+                version: "~12.0.0-rc.0+22.sha-097fdff"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
