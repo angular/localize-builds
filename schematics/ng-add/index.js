e4ebb2129380d00f306f3133a61d1bd23613e043
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-23 19:13:18 +0000

    73105aa7d6 ci: remove trailing comma from renovate config (#41790)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+213.sha-9f88da2"
+                version: "~12.0.0-next.8+214.sha-73105aa"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
