32f672b25212473542da2466a416f9ecadbdba4d
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:31:26 +0000

    70fb3e958b docs: improve accessibility of http example (#41057)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+214.sha-73105aa"
+                version: "~12.0.0-next.8+215.sha-70fb3e9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
