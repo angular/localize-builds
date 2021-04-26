5568681f3c26a640496d735ab881b816494bcd3a
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 16:51:52 +0000

    990067a0c6 perf(core): avoid storing LView in __ngContext__ (#41358)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.0+24.sha-e6ec22f"
+                version: "~12.0.0-rc.0+34.sha-990067a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
