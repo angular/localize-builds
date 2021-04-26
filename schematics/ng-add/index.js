38512fddbae0276310c050858d7925596eed4e65
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 19:02:37 +0000

    295889ed2e ci: prevent CI cache from growing indefinitely (#41814)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+226.sha-a938849"
+                version: "~12.0.0-next.8+228.sha-295889e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
