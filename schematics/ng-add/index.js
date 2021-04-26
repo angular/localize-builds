95a04cb02913cf41c4860a86827deb68828f248e
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 22:39:44 +0000

    f9c1f08957 fix(core): update supported range of node versions to only include LTS versions (#41822)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.0+42.sha-8d4460d"
+                version: "~12.0.0-rc.0+61.sha-f9c1f08"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
