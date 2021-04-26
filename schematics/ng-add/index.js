89014b5ecca3d58a4d6d37e51e3892303d8141f0
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-26 17:04:20 +0000

    21f1466d84 build(docs-infra): fix e2e testing SystemJS-based docs examples (#41796)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.11+10.sha-163c076"
+                version: "~11.2.11+15.sha-21f1466"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
