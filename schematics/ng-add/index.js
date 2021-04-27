240aba644b2e68a216b90223f99ddc8aba4d3261
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 21:24:42 +0000

    3b7d4ebbd6 fix(docs-infra): do not redirect disambiguated URLs (#41842)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+259.sha-d580b27"
+                version: "~12.0.0-next.8+265.sha-3b7d4eb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
