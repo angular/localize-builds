1ec9e3fd8cfc9f4c2d278fc1a7e967207f9a5adf
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 17:45:12 +0000

    84a44a9b0f fix(dev-infra): add github token requirement for discover-new-conflicts (#41825)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.11+20.sha-7756ead"
+                version: "~11.2.11+23.sha-84a44a9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
