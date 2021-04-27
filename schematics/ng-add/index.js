9646a91e370e575c590e7f1d4ca6fef98520e833
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 20:45:48 +0000

    4aafcc06ed Revert "feat(dev-infra): create the prettier formatter for ng-dev format tooling (#41824)" (#41844)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.11+25.sha-eedbb75"
+                version: "~11.2.11+27.sha-4aafcc0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
