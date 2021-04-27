e6b07dd97b21dba086c4457d0674b53e88612289
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 16:39:48 +0000

    da6ed1562e fix(compiler): strip scoped selectors from `@font-face` rules (#41815)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+251.sha-af12d8d"
+                version: "~12.0.0-next.8+252.sha-da6ed15"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
