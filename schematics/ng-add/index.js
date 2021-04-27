0654af0750ed396a280d4f9edf196e6432b149d0
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-27 18:54:33 +0000

    cd004d4a23 ci: add Zach Arend to pullapprove groups (#41839)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+256.sha-b1ae31c"
+                version: "~12.0.0-next.8+257.sha-cd004d4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
