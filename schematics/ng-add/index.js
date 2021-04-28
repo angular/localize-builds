f58fca454a8fc6396fd06c14f89774c81e18454f
GitHub <noreply@github.com>
2021-04-28 19:25:00 +0000

    083ceecc51 ci: enable Renovate for remark packages

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+213.sha-45ffab5"
+                version: "~12.0.0-next.8+272.sha-083ceec"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
