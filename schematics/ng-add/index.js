6edffcebce35dca641937943519399a815a4a3a7
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 16:46:44 +0000

    044b380e5a build: update remark packages (#41721)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+268.sha-1aed2f3"
+                version: "~12.0.0-next.8+269.sha-044b380"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
