5d875ad40c04bd6da02a106c8be2ccd920e86ac5
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 21:52:43 +0000

    fca8d7ae92 docs: remove tslint, codelyzer & protractor from dev deps (#41826)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.12+1.sha-f555a55"
+                version: "~11.2.12+3.sha-fca8d7a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
