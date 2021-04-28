c5665803f7adffc666603f020a6bae4477d42f6f
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 17:59:42 +0000

    336fe9124e Revert "ci: re-enable the `@angular/components` unit tests (#41816)" (#41863)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.11+36.sha-7219d1b"
+                version: "~11.2.11+38.sha-336fe91"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
