97a56a6ac6ce1ad12c126bf9f2576458232f7a5b
Jessica Janiuk <jessicajaniuk@google.com>
2021-04-28 19:56:25 +0000

    42a64f8676 docs: remove mention of scheduleTick in markForCheck (#41845)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+272.sha-bbda73e"
+                version: "~12.0.0-next.8+278.sha-42a64f8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
