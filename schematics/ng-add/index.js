7ec748690c209fa77ca413dd59d493f1e1fa5449
Misko Hevery <misko@hevery.com>
2021-04-29 21:14:07 +0000

    0255a2c631 docs(docs-infra): fix handling of client-side errors in networking (#36608)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.1+16.sha-77b77ff"
+                version: "~12.0.0-rc.1+20.sha-0255a2c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
