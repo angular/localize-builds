b30083caf59e0d50a3adb90d78722e5285eb5308
Misko Hevery <misko@hevery.com>
2021-04-29 19:40:58 +0000

    ca0fcd6c08 ci: only run `publish_snapshot` when not PR (#41877)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+286.sha-05209fa"
+                version: "~12.0.0-next.8+289.sha-ca0fcd6"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
