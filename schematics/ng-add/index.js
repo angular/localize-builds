1d759edd807b8dce17c0401af04b74c9ed4e492e
Misko Hevery <misko@hevery.com>
2021-04-29 17:31:00 +0000

    d80ad951c6 docs: correct toh router tutorial (#41859)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+283.sha-595a75c"
+                version: "~12.0.0-next.8+284.sha-d80ad95"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
