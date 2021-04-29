4fa04977a3ff7d9ae27353e4267a7b8bef7736f2
Misko Hevery <misko@hevery.com>
2021-04-29 17:46:12 +0000

    05209fa397 ci: exclude packages which don't have a major from being batched (#41865)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+288.sha-f3331a8"
+                version: "~12.0.0-next.8+286.sha-05209fa"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
