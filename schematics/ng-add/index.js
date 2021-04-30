22b195338864e967c2ddd501537e22d47c68a10b
Misko Hevery <misko@hevery.com>
2021-04-30 21:39:13 +0000

    72384a3357 fix(docs-infra): switch over to img[loading=lazy] for front page images (#34900)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+296.sha-6581a1b"
+                version: "~12.0.0-next.8+300.sha-72384a3"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
