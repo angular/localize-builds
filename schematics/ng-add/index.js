e2e5561cdcc522b10b35642edeb25fe2545537e8
Misko Hevery <misko@hevery.com>
2021-04-30 20:48:57 +0000

    be9b19e820 Revert "build: update dependency karma to v6 (#41696)" (#41904)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.1+20.sha-0255a2c"
+                version: "~12.0.0-rc.1+21.sha-be9b19e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
