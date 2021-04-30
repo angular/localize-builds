32ca28cd49c7f50c3eee681b5562cdab067f6e4d
Misko Hevery <misko@hevery.com>
2021-04-30 21:59:34 +0000

    74da3724e4 docs: Update link to Testing Angular online book (#41906)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+303.sha-b5134e2"
+                version: "~12.0.0-next.8+307.sha-74da372"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
