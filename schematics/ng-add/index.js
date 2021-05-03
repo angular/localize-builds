d197a1cb34160931f6ec98f404aa99990b82821d
Misko Hevery <misko@hevery.com>
2021-05-03 21:48:00 +0000

    3b589030a8 docs: Remove references to protractor from documentation (#41731)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+312.sha-253ed5d"
+                version: "~12.0.0-next.8+313.sha-3b58903"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
