716d4911a41a253a3fb148034514122dd0e4b80f
Misko Hevery <misko@hevery.com>
2021-05-03 21:50:04 +0000

    741c295129 docs: update file structure document (#41923)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+315.sha-f42796a"
+                version: "~12.0.0-next.8+314.sha-741c295"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
