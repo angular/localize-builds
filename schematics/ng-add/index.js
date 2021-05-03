36f52a9df61dd27c8421d48ad4717941d809b2fe
Misko Hevery <misko@hevery.com>
2021-05-03 21:55:37 +0000

    a75a3175ee Revert "fix(language-service): only provide template results on reference requests (#41041)" (#41930)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.12+12.sha-7e9ef4d"
+                version: "~11.2.12+18.sha-a75a317"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
