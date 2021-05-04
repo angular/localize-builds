6ab5d672d69752485fc22401103479bf381eafcd
Misko Hevery <misko@hevery.com>
2021-05-04 15:37:03 +0000

    6d7d659919 docs: add new migration guide to v12 documentation that describes how to migrate older localization ids to new ones (#41828)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+318.sha-b7db98f"
+                version: "~12.0.0-next.8+319.sha-6d7d659"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
