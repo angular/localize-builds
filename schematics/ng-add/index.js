9bdc7e7ee43f5a3fda80a7342573cd7623ca8727
Misko Hevery <misko@hevery.com>
2021-05-04 15:38:43 +0000

    4dae97de43 docs: update Node.js supported version statement (#41820)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+319.sha-6d7d659"
+                version: "~12.0.0-next.8+320.sha-4dae97d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
