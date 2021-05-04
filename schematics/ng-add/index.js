f068d724263dce5f8b2a7d601ab551223c962590
Misko Hevery <misko@hevery.com>
2021-05-04 15:54:04 +0000

    dac78d1ba1 build: update dependency yargs to v17 (#41920)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+320.sha-4dae97d"
+                version: "~12.0.0-next.8+321.sha-dac78d1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
