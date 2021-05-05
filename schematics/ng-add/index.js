bf3ce6ec27bcb08e8bfb007da2421575c95def13
Misko Hevery <misko@hevery.com>
2021-05-05 21:14:55 +0000

    c472c5b872 docs: initial commit of content projection topic. (#41143)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+336.sha-530e45f"
+                version: "~12.0.0-next.8+339.sha-c472c5b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
