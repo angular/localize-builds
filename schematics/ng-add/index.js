893ac51357330b5b6e2457cdb525bdc9d57f8ec0
Misko Hevery <misko@hevery.com>
2021-05-05 04:25:16 +0000

    fdf2e02bff build: fix crash when validating commit messages (#41949)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+323.sha-e387d22"
+                version: "~12.0.0-next.8+325.sha-fdf2e02"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
