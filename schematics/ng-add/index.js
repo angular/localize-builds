3ffdb60c59c8e2e2f1ef9fccbc87f74d211a93e0
Misko Hevery <misko@hevery.com>
2021-05-05 04:39:35 +0000

    6b8baad940 fix(common): add body as an optional property on the options of HttpClient.delete (#19438) (#41723)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+331.sha-19b12d7"
+                version: "~12.0.0-next.8+332.sha-6b8baad"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
