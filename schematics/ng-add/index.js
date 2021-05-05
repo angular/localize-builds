cc3a20a8b09ddbe398bed09cadf93ebab3709d1b
Misko Hevery <misko@hevery.com>
2021-05-05 21:05:24 +0000

    21c2a06811 fix(dev-infra): spawned child processes messing with tty output (#41948)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+332.sha-6b8baad"
+                version: "~12.0.0-next.8+335.sha-21c2a06"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
