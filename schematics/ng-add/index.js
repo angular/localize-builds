2f2cef49118aeb848cc242dbae377797796af91a
Misko Hevery <misko@hevery.com>
2021-05-05 21:04:29 +0000

    fb38175a40 docs: remove ts-node and tslint form dev deps (#41950)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+333.sha-753fb06"
+                version: "~12.0.0-next.8+334.sha-fb38175"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
