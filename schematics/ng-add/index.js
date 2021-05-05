5b800794217b885df0b5a9c9c6dc0c471eaa1b84
Misko Hevery <misko@hevery.com>
2021-05-05 04:36:45 +0000

    46069ffe50 docs(common): Corrected the wrong formatting (#41945)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.12+22.sha-7a86ebf"
+                version: "~11.2.12+27.sha-46069ff"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
