d7e68b52abbb109b55f915893333f3ec86a06783
Misko Hevery <misko@hevery.com>
2021-05-05 04:35:50 +0000

    5947239f89 fix(dev-infra): do not set lts dist tag on packages from release-candidate train (#41946)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+330.sha-378bb04"
+                version: "~12.0.0-next.8+329.sha-5947239"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
