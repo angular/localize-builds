0618da42a95f96ed35eac1b376e224f5982e0673
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 22:00:21 +0000

    83bb0effc2 docs: use relative path for the feature request process diagram (#41970)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.13+6.sha-e090a6d"
+                version: "~11.2.13+8.sha-83bb0ef"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
