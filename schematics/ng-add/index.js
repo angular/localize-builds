ac9f890221ee3afa9fab0b995122542ae4390d04
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 20:08:43 +0000

    4d7294d9f7 feat(dev-infra): include release notes in Github release entries when publishing (#41964)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+354.sha-640ec78"
+                version: "~12.0.0-next.8+355.sha-4d7294d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
