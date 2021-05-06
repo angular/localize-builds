25e6ff7665d817d572019def21096cd9724630d3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 14:01:08 +0000

    35fbd4f411 build: update dependency fs-extra to v10 (#41937)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+344.sha-abcd4bb"
+                version: "~12.0.0-next.8+345.sha-35fbd4f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
