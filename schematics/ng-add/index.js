f0243b50a7361d8e40f63b7fa2358863d02f127f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-06 19:23:23 +0000

    a4e3ea0311 feat(docs-infra): add support for unformatted code-snippets (#41335)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+348.sha-90d4b22"
+                version: "~12.0.0-next.8+351.sha-a4e3ea0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
