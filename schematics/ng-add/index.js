83c4ccd254251ffc3a0712cd5d3ba101a2f37050
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 20:09:04 +0000

    7a4d9805ea fix(compiler-cli): use '' for the source map URL of indirect templates (#41973)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.2+23.sha-c580fbb"
+                version: "~12.0.0-rc.2+24.sha-7a4d980"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
