096d24a9ab5ef797441ca0852e1573e930e599a1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 18:31:01 +0000

    c580fbb60c fix(dev-infra): merge tool should ensure that token has `workflow` oauth scope (#41989)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.2+22.sha-89c0298"
+                version: "~12.0.0-rc.2+23.sha-c580fbb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
