5cddedbf6ffbe020fbae511635185d686626e7ab
Andrew Scott <atscott@google.com>
2021-05-07 01:31:34 +0000

    51cc14292d docs: reword a few phrases in the GITHUB_PROCESS.md doc (#41977)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.2+15.sha-8dd5216"
+                version: "~12.0.0-rc.2+16.sha-51cc142"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
