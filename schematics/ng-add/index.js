2d9792c7e4c2f767c2289ff5bb40db2f065237f2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 15:06:17 +0000

    4bc3cf216d feat(common): add URLSearchParams to request body (#37852)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+367.sha-cb418bd"
+                version: "~12.0.0-next.8+370.sha-4bc3cf2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
