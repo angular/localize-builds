d2c2f93b7f69faed0656ac2192cec301d4f8bcfe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-07 17:32:00 +0000

    89c02980ae build(docs-infra): ensure that terminal code snippets render correctly (#41986)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.2+21.sha-d6c217b"
+                version: "~12.0.0-rc.2+22.sha-89c0298"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
