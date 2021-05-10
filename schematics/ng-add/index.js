f9f4577da57c6a96e6320a1ed747b52ab5877ad4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 16:31:34 +0000

    388f4056e6 docs: fix a grammatical error in lazy-loading-ngmodules guide (#42008)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.2+38.sha-cffba6f"
+                version: "~12.0.0-rc.2+39.sha-388f405"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
