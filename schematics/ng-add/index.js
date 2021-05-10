356060f2911a6578717ab5122c7a0c5e2833aa52
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 14:59:05 +0000

    089daea98b refactor(compiler-cli): remove redundant `potentialSourceMapUrl` property (#42000)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+387.sha-ddf2a6c"
+                version: "~12.0.0-next.8+385.sha-089daea"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
