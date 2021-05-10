49adca3a569ac574ee14ec353fc79e6f16aa0f00
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 15:03:20 +0000

    cffba6fa0b build: update node version to stop aio from breaking while generating docs (#42009)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.2+24.sha-7a4d980"
+                version: "~12.0.0-rc.2+38.sha-cffba6f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
