eb0f3c677341d3390a5cf254b9c007764ff7c0ec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-10 21:07:26 +0000

    d5555558d0 fix(platform-browser): prevent memory leak of style nodes if shadow DOM encapsulation is used (#42005)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-rc.2+42.sha-d43e351"
+                version: "~12.0.0-rc.2+45.sha-d555555"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
