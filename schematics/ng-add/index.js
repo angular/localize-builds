52855ed7e7a8da342db9055ae0d08af7e3f50611
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-12 21:29:56 +0000

    dad205e502 docs: add release notes for v12.0.0 to CHANGELOG

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+396.sha-a74eb52"
+                version: "~12.0.0-next.8+397.sha-dad205e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
