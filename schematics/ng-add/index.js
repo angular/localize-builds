a5f754218357beac7dcff49e617e3ce86f79aeb4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-05-13 18:26:22 +0000

    04f41554dc build(docs-infra): show a dist-tag when installing the CLI on non-stable versions (#41991) (#42041)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.14"
+                version: "~11.2.14+1.sha-04f4155"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
