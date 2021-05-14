76c1545f50a9e4027be2f98c060d8ea1be57953a
Zach Arend <zachzach@google.com>
2021-05-14 19:31:40 +0000

    678bce2419 ci: update to latest feature request triage bot commit (#42084)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0+12.sha-3a46ad9"
+                version: "~12.0.0+13.sha-678bce2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
