89d688eab1ba00674ee0a679d103081e9896c8b2
Andrew Scott <atscott@google.com>
2021-05-14 00:20:33 +0000

    28ee9864a4 fix(benchpress): update the check for start and end events (#42085)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0+2.sha-7514917"
+                version: "~12.0.0+11.sha-28ee986"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
