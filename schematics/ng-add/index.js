dd0c9c8f2f2784da912c3df325f2ebb93d16d412
Zach Arend <zachzach@google.com>
2021-05-14 21:25:15 +0000

    e89e3cb375 docs: uppercase API word in Ivy Compatibility file (#42089)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+413.sha-7fb8527"
+                version: "~12.0.0-next.8+414.sha-e89e3cb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
