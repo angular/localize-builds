72db790a42bbb064b37efd68f9c5553088b92c1d
atscott <atscott01@gmail.com>
2021-05-17 18:09:57 +0000

    e86a1d3441 docs: make all examples compatible with strict mode (#41999)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+416.sha-5960d4c"
+                version: "~12.0.0-next.8+417.sha-e86a1d3"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
