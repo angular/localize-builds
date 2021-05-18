eeef42745b5e6cfdeac05735b9fbd867fd3ae3af
atscott <atscott01@gmail.com>
2021-05-18 22:55:26 +0000

    b7d161487f build: update dependency esbuild to ^0.12.0 (#42143)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+432.sha-6d14776"
+                version: "~12.0.0-next.8+435.sha-b7d1614"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
