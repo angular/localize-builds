538754453ed993fc120a71b3d9695a477629f079
atscott <atscott01@gmail.com>
2021-05-19 18:39:41 +0000

    ef676bf946 build: reformat yml files (#42136)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.0-next.8+445.sha-4259945"
+                version: "~12.1.0-next.2+2.sha-ef676bf"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.0.0-next.8+445.sha-4259945`
+            version: `~12.1.0-next.2+2.sha-ef676bf`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
