b6ab456716bd0ee0ad858b559b0e64c8e304ccfa
Zach Arend <zachzach@google.com>
2021-05-20 16:31:00 +0000

    04ab5edf65 test(docs-infra): correctly test URL redirects when the destination URL is also redirected (#42018)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.2+2.sha-ef676bf`
+            version: `~12.1.0-next.2+3.sha-04ab5ed`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
