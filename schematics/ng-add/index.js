356f833c532d5e7a9b48f4ca822b0a547f388f07
Zach Arend <zachzach@google.com>
2021-05-24 22:36:13 +0000

    47bb841ed6 build(docs-infra): temporarily serve the DevTools guide at `/devtools` too (#42283)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.2+32.sha-cb48bde"
+                version: "~12.1.0-next.2+34.sha-47bb841"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.2+32.sha-cb48bde`
+            version: `~12.1.0-next.2+34.sha-47bb841`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
