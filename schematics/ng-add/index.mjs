5e2e8fde594cd49bf1a75d2c54f9743c0c9001be
Zach Arend <zachzach@google.com>
2021-05-24 15:04:45 +0000

    c03280fd54 docs: update the intro image in the devtools guide (#42162)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.2+21.sha-3293dd3`
+            version: `~12.1.0-next.2+23.sha-c03280f`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
