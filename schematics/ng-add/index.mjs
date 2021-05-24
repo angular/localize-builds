dca39ace67b9119898696d625812e2cd1536d314
Zach Arend <zachzach@google.com>
2021-05-24 23:01:57 +0000

    a39169851e docs: clarify pathMatch strategy prefix (#42287)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.2+37.sha-1040697`
+            version: `~12.1.0-next.2+38.sha-a391698`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
