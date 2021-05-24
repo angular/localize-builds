5f8b8a2b521827cae0b639d6165db07730a402e4
Zach Arend <zachzach@google.com>
2021-05-24 16:19:33 +0000

    4ac55ca676 fix(dev-infra): use template strings for release note templates (#42224)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.2+22.sha-d7a1da9`
+            version: `~12.1.0-next.2+24.sha-4ac55ca`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
