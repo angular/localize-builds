26ce4218da65c2a98ec9fe6d7c3690ff4fb8aa2d
Zach Arend <zachzach@google.com>
2021-05-25 18:15:26 +0000

    d15a6a3a71 docs: update web-worker syntax (#42306)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.2+59.sha-1955dbb`
+            version: `~12.1.0-next.2+58.sha-d15a6a3`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
