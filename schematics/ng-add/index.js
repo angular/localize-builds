95e204dbd7811902ddb7ef633c3883fec65afa8e
Zach Arend <zachzach@google.com>
2021-05-25 18:15:20 +0000

    1955dbb048 docs: remove references to CheckAlways and CheckOnce (#42300)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.2+57.sha-29e5d50"
+                version: "~12.1.0-next.2+59.sha-1955dbb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
