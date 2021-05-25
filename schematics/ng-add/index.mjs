5d3272b657447808dad60b8bf38bc0787ad2d028
Zach Arend <zachzach@google.com>
2021-05-25 22:33:58 +0000

    4a2cc3e451 docs: add explanation of how to use locally created build artifacts in a local project (#42319)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.2+71.sha-063d178`
+            version: `~12.1.0-next.2+77.sha-4a2cc3e`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
