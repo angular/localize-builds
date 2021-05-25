68fd7bd7e902c781a55f43f28e78e7719e24adcc
Zach Arend <zachzach@google.com>
2021-05-25 22:32:36 +0000

    063d1789c2 docs: move "Browser Support" page in navigation tree (#42326)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.2+72.sha-bdd3cca"
+                version: "~12.1.0-next.2+71.sha-063d178"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
