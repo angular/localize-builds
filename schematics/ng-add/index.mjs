0af6a461a00f94a6f2f8cab17a937117d4018bbf
Zach Arend <zachzach@google.com>
2021-05-25 22:38:31 +0000

    850028d000 docs: remove VS 2015 guide (#42253)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.2+84.sha-b7721aa`
+            version: `~12.1.0-next.2+82.sha-850028d`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
