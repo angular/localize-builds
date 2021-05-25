b95ae16cd6ee9350eb5d2933000a4ecb09da8f03
Zach Arend <zachzach@google.com>
2021-05-25 18:16:27 +0000

    2ff5bfe139 docs: fix documentation of guard execution order (#42288)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.2+60.sha-dc6bff1`
+            version: `~12.1.0-next.2+61.sha-2ff5bfe`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
