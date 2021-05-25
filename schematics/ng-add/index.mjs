47dd0a3334e7e9b7f37089a21ebc26e767015b8a
Zach Arend <zachzach@google.com>
2021-05-25 22:35:14 +0000

    6da0c97423 docs: add note about redirects after absolute redirect (#42277)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.2+78.sha-b0581f3`
+            version: `~12.1.0-next.2+80.sha-6da0c97`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
