802014515feea0e2f7cdb1ba122ab52cda44da4a
Zach Arend <zachzach@google.com>
2021-05-25 18:21:07 +0000

    c3cbfc22e0 fix(dev-infra): format command incorrectly prints outdated files (#42252)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.2+65.sha-f74fd64"
+                version: "~12.1.0-next.2+63.sha-c3cbfc2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
