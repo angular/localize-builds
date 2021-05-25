e59787a22bedf87419d0f014fd02480eb351bb1d
Zach Arend <zachzach@google.com>
2021-05-25 18:09:01 +0000

    9f09c3b9b1 build(core): update supported range of node versions to be less restrictive (#42205)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.2+40.sha-1c3fe69"
+                version: "~12.1.0-next.2+56.sha-9f09c3b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
