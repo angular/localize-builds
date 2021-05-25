a79bfd5f5c50abed7986c145bbec9ad90ad785d7
Zach Arend <zachzach@google.com>
2021-05-25 20:51:04 +0000

    3a6af8e629 fix(platform-browser): update started state on reset (#41608)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.2+68.sha-d7768c6`
+            version: `~12.1.0-next.2+69.sha-3a6af8e`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
