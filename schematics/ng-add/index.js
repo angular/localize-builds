911f477cec9dac7b24a021adc01ed8b0b8150c9c
Zach Arend <zachzach@google.com>
2021-05-25 20:49:45 +0000

    3a6af8e629 fix(platform-browser): update started state on reset (#41608)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.2+68.sha-d7768c6"
+                version: "~12.1.0-next.2+69.sha-3a6af8e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
