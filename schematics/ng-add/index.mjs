175017b72db2c88d2c2541612051e9790c7a07c4
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:38:43 +0000

    4e9b61aaf9 docs: remove/update broken links in resources appearing on aio (#42232)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.3+13.sha-9193ea2`
+            version: `~12.1.0-next.3+14.sha-4e9b61a`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
