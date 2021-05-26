c97d344c26225b3b43539ef6fa93db51ac3ffbf7
Andrew Kushnir <akushnir@google.com>
2021-05-26 22:45:28 +0000

    44027c4e0e build: allow node v15 and above for aio (#42367)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.3+18.sha-516923e"
+                version: "~12.1.0-next.3+19.sha-44027c4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
