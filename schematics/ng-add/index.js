af95929bf4e0c280f5b5e6e9a9b5f399cfcffc20
Zach Arend <zachzach@google.com>
2021-05-26 20:32:40 +0000

    33e1a18530 build(docs-infra): allow only Node.js v14 and above (#42093)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.3+9.sha-de8a6ae"
+                version: "~12.1.0-next.3+12.sha-33e1a18"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
