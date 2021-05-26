2bc888e27e8b4313951662f04f513d5f95f3c6e8
Zach Arend <zachzach@google.com>
2021-05-26 18:55:20 +0000

    5a51884069 fix(dev-infra): build with stamping for release builds (#42354)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.1+84.sha-037b636"
+                version: "~12.0.2+2.sha-5a51884"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
