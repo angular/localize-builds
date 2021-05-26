695c7c428123df5a19415c02f2f4b2da0e0c6284
Zach Arend <zachzach@google.com>
2021-05-26 20:25:45 +0000

    38b0e71cb2 docs: move the schematics under Angular Tools menu section (#42332)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.3+5.sha-40bbcaa`
+            version: `~12.1.0-next.3+6.sha-38b0e71`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
