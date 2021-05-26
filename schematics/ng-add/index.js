78d13b007784aa9bf0543a2f0d10ffd3627f3591
Zach Arend <zachzach@google.com>
2021-05-26 20:30:13 +0000

    de8a6ae9ed docs: add example that exports a part of an animation for reuse (#42321)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.3+6.sha-38b0e71"
+                version: "~12.1.0-next.3+9.sha-de8a6ae"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
