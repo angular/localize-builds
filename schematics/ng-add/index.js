ce1b873c5751d5d7686398393ac7c977b02f2054
Andrew Kushnir <akushnir@google.com>
2021-05-26 21:43:00 +0000

    bcfe4afda9 docs: clarify that `forRoot` can be called in other modules (#42361)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.3+15.sha-4cb11cc"
+                version: "~12.1.0-next.3+17.sha-bcfe4af"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
