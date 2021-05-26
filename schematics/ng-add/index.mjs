8de8740c58a89493e7a866f3bf43f2b3b224bddd
Andrew Kushnir <akushnir@google.com>
2021-05-26 23:08:46 +0000

    74186c36de docs: remove lightbox gif from Use Directives to Watch the DOM section. (#42369) This gif is out of date and causing reader confusion.

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.3+19.sha-44027c4`
+            version: `~12.1.0-next.3+21.sha-74186c3`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
