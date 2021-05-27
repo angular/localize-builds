9b8b10a83128b726f83ea81eef897cb6ba9996ad
Andrew Kushnir <akushnir@google.com>
2021-05-27 18:30:52 +0000

    e73fef4d0b docs: add a note about <select> and nested <option>s in Ivy compat guide (#42338)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.3+30.sha-306f7ac`
+            version: `~12.1.0-next.3+31.sha-e73fef4`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
