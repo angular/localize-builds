2d8e7aa13c533ef44a167ab622a5b95d1a60d1ea
Andrew Kushnir <akushnir@google.com>
2021-05-27 21:28:27 +0000

    b60c9c18f1 docs: clarify behavior of strict templates flags (#42392)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.2+39.sha-a86a6b9"
+                version: "~12.0.2+42.sha-b60c9c1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
