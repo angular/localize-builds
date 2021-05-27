718f57bd629b0ac13e71d1fc9b6b5bd0e1eb4dc9
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:12:41 +0000

    38378ce50a docs: explain what a deploy url is and how it differs from a base href (#42363)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.3+24.sha-d74d130`
+            version: `~12.1.0-next.3+25.sha-38378ce`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
