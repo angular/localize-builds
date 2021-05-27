292dd4d53a1bd25ab18a51ed7bb56526b21dd886
Andrew Kushnir <akushnir@google.com>
2021-05-27 16:20:05 +0000

    deb893095f build(docs-infra): remove unused `spy-directive.gif` image (#42388)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.3+26.sha-e87dbaf"
+                version: "~12.1.0-next.3+29.sha-deb8930"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.3+26.sha-e87dbaf`
+            version: `~12.1.0-next.3+29.sha-deb8930`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
