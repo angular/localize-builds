0dbf663c398d925b72054e3ac5c6c482451855c9
Andrew Kushnir <akushnir@google.com>
2021-05-28 00:22:32 +0000

    62b5a6cb07 docs: add docs for DebugElement (#42352)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.3+46.sha-4cd2cc4`
+            version: `~12.1.0-next.3+48.sha-62b5a6c`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
