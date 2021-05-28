7533a2d0a064191b39be4b88ef1bb8af2db3f566
Andrew Kushnir <akushnir@google.com>
2021-05-28 00:05:15 +0000

    ee0e3fbf89 docs: edits to remove jargon (#42025)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.3+42.sha-635aaaa"
+                version: "~12.1.0-next.3+45.sha-ee0e3fb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
