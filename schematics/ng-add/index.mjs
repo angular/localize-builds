dc735bbee5585be59eacf46ab61bf49ae31a27c5
Andrew Kushnir <akushnir@google.com>
2021-06-01 23:55:59 +0000

    574f64041e docs: specify correct interceptor class type (#42437)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.3+65.sha-6a2e3e2`
+            version: `~12.1.0-next.3+66.sha-574f640`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
