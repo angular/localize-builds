65c7f01b73eb40b8b2a4480a54f7abc89414084b
Andrew Kushnir <akushnir@google.com>
2021-06-01 17:39:59 +0000

    3e192bfa4d build: fix linting against jasmine focus functions (#42415)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.3+61.sha-44b737e"
+                version: "~12.1.0-next.3+60.sha-3e192bf"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
