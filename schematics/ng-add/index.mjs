6039a03c02b30c4f268d137fc81930efa2173e07
Andrew Kushnir <akushnir@google.com>
2021-06-01 19:37:14 +0000

    e039075a28 fix(compiler-cli): better detect classes that are indirectly exported (#42207)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.3+60.sha-3e192bf`
+            version: `~12.1.0-next.3+62.sha-e039075`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
