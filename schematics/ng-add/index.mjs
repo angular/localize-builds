933d8f1fcb23531007490700d2b3f557a0041fd9
Andrew Kushnir <akushnir@google.com>
2021-06-01 17:31:36 +0000

    c54643f00e docs(forms): make the forms validation guide specify more clearly what happens when mixing HTML5 native validators with Angular forms. (#42377)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.3+52.sha-e0381a8`
+            version: `~12.1.0-next.3+53.sha-c54643f`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
