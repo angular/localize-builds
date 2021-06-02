135b3c9164a2e2bfb8665726152823988f6eee24
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:40:23 +0000

    c0b2eeb54c docs: remove unused imports from unit tests (#42441)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.4+6.sha-1fe1dd1`
+            version: `~12.1.0-next.4+8.sha-c0b2eeb`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
