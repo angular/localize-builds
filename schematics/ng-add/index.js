ea86b8c5a332a01aadcc6fd9503847bedd658738
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:45:09 +0000

    0ac193c86c docs: fix typo in the Router guide (#42455) PR Close #42455

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+8.sha-c0b2eeb"
+                version: "~12.1.0-next.4+13.sha-0ac193c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
