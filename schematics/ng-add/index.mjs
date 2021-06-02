6a00a14f73b2e6eced5f7847035ac0458be91933
Andrew Kushnir <akushnir@google.com>
2021-06-02 21:01:39 +0000

    3d1c905c91 refactor(docs-infra): clean up AIO Sass files (#42442)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.4+13.sha-0ac193c`
+            version: `~12.1.0-next.4+14.sha-3d1c905`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
