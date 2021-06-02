eae4de9811ee819412b1707d70374ac239aba023
Andrew Kushnir <akushnir@google.com>
2021-06-02 20:37:11 +0000

    1fe1dd1f19 docs: clarify the wording around live examples for all testing guides (#42406)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+5.sha-4c39869"
+                version: "~12.1.0-next.4+6.sha-1fe1dd1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
