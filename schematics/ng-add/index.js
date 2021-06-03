7324f045034f2400573a1a2b32fded5d1ed6dacd
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 21:03:57 +0000

    a6dec05e82 refactor(dev-infra): always use en-US locale for printing date in merge tool (#42470)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.3+22.sha-50c87e8"
+                version: "~12.0.3+26.sha-a6dec05"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
