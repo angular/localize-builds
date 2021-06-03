2ec115d5562015b6dff4d743d22fa3a6ef17d381
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 18:51:44 +0000

    bd1836b999 fix(compiler-cli): exclude type-only imports from cycle analysis (#42453)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+23.sha-374fa2c"
+                version: "~12.1.0-next.4+24.sha-bd1836b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
