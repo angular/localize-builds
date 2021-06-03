96fc94bf2ae5f7a67715b2ced3545b519c98bd2e
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 19:00:40 +0000

    69e57827e2 refactor(compiler-cli): remove unused return type transform (#41996)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+24.sha-bd1836b"
+                version: "~12.1.0-next.4+30.sha-69e5782"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
