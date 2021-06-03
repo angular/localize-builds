7d4c33229d2979f34e5696d1705a4130675dc88b
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-03 18:14:31 +0000

    0dad375de7 fix(common): initialize currencyCode in currencyPipe (#40505)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.3+11.sha-0a1d8e5"
+                version: "~12.0.3+17.sha-0dad375"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
