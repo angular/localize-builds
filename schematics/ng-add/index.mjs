51d2e251ae1554d2372cadf50016b8296e3b5576
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 01:03:59 +0000

    00b1444d12 Revert "feat(forms): add `ng-submitted` class to forms that have been submitted." (#42474)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.4+39.sha-ae858c0`
+            version: `~12.1.0-next.4+40.sha-00b1444`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
