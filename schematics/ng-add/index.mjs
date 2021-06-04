4af3abc3e28f683ded3723619ce327089d40ad1f
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 20:52:58 +0000

    4fc24addcb build: suppress errors in git hooks (#42484)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.4+43.sha-71e14a7`
+            version: `~12.1.0-next.4+46.sha-4fc24ad`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
