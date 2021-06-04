40aa91b667374752d0c9e8d82385176265c6aecf
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 20:52:36 +0000

    6080db1839 build: suppress errors in git hooks (#42484)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.3+31.sha-beae425"
+                version: "~12.0.3+33.sha-6080db1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
