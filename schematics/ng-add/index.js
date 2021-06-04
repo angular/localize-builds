95289f0f3a1dedda73c6a555f5791642f166f989
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-04 18:39:28 +0000

    71e14a71f5 test: update compiler-cli compliance goldens due to TS 4.3 emit format regression (#42022)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+41.sha-6929625"
+                version: "~12.1.0-next.4+43.sha-71e14a7"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
