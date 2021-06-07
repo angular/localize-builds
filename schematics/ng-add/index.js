8c07d5e0abdf20b12e55d348a782f1fa3670feed
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:52:58 +0000

    cd22c7a1f5 docs: Updated the code snippet path (#42494) PR Close #42494

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+47.sha-3005917"
+                version: "~12.1.0-next.4+48.sha-cd22c7a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
