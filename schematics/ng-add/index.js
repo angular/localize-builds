ef92c4c6cbb2c4c56d07723ce1ac2e41c8705e2c
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 17:22:26 +0000

    cc904b5226 docs(core): clarify deprecation of `entryComponents` (#42248)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+50.sha-b061e5b"
+                version: "~12.1.0-next.4+51.sha-cc904b5"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.4+50.sha-b061e5b`
+            version: `~12.1.0-next.4+51.sha-cc904b5`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
