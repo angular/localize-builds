3b7bf17c819c88ab4a98e90feb1fc8a3bdf97d7c
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:57:57 +0000

    b061e5b2d7 fix(docs-infra): fix search results font color (#42488)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+49.sha-5d70f09"
+                version: "~12.1.0-next.4+50.sha-b061e5b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
