14f6c801ee5f30fd2abb4f2c9445f9b637ebfdca
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 19:16:24 +0000

    8bb6fb7704 docs(router): fix code-example headers (#42507)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.3+41.sha-65d5930"
+                version: "~12.0.3+42.sha-8bb6fb7"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
