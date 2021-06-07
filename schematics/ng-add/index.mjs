24e0d481ffae7afba18153bf83b28756a6e305e1
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:56:24 +0000

    5d70f09032 docs: Corrected spelling (#42493)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.4+48.sha-cd22c7a`
+            version: `~12.1.0-next.4+49.sha-5d70f09`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
