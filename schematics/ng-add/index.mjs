1d1a1d9ad35c6d69874d2b28746b6aa9cfcf1c3f
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-07 16:51:30 +0000

    cd22c7a1f5 docs: Updated the code snippet path (#42494) PR Close #42494

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.4+47.sha-3005917`
+            version: `~12.1.0-next.4+48.sha-cd22c7a`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
