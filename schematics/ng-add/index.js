05ce3847e4dbb02e2d635fe7dda05fa2acefe19f
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 17:28:05 +0000

    66f49c2497 Revert "refactor: remove checked-in locale files" (#42521)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+64.sha-f1d58ef"
+                version: "~12.1.0-next.4+70.sha-66f49c2"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
