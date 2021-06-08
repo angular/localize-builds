0fb70c77f75c2e0c481e418a50eea23e0df5e619
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:15:38 +0000

    2a28a1ddb2 docs: add Angular Rocks podcast (#42078)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+89.sha-0777fac"
+                version: "~12.1.0-next.4+88.sha-2a28a1d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.4+89.sha-0777fac`
+            version: `~12.1.0-next.4+88.sha-2a28a1d`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
