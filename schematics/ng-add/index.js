c85156316d0851d2431954050c90a663b7e69354
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:06:05 +0000

    43b4a8c07c docs: Edited to remove jargon in Reference Errors (#42186)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+86.sha-f3fde4b"
+                version: "~12.1.0-next.4+85.sha-43b4a8c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
