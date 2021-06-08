35f6cde426626703bfe449a5c06a7ef944ee5860
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 19:07:10 +0000

    8fe40026c1 docs: clarify documentation regarding passing multiple configurations to ng build (#41333)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+75.sha-171428e"
+                version: "~12.1.0-next.4+79.sha-8fe4002"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
