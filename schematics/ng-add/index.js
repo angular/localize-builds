08ce18c96ae45ff7b61ff4b0c8290b202af94dbb
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 18:03:23 +0000

    c44ab4f6da fix(router): fix `serializeQueryParams` logic (#42481)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+72.sha-d10c38a"
+                version: "~12.1.0-next.4+76.sha-c44ab4f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
