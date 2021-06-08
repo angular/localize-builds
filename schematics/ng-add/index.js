1f5c8caf7457617654b1898207b88bee98b36e5a
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 17:58:40 +0000

    751f68dcf8 docs: updated collaborators and add angular dev's link (#42516)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+70.sha-66f49c2"
+                version: "~12.1.0-next.4+71.sha-751f68d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
