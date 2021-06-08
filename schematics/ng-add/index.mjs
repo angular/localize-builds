0adde8be1562bb8cebb2ca8ad4359d4b842c0144
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 19:35:15 +0000

    0031c8cf41 docs: improve accessibility of attribute-binding example (#41432)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.4+79.sha-8fe4002`
+            version: `~12.1.0-next.4+81.sha-0031c8c`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
