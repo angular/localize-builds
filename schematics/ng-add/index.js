115d1d224e9cb0f955389cebc7923cd5aec17fb8
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:08:13 +0000

    4975a106dc docs(platform-server): added commands for universal and add prerendering info (#36005)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+85.sha-43b4a8c"
+                version: "~12.1.0-next.4+87.sha-4975a10"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.4+85.sha-43b4a8c`
+            version: `~12.1.0-next.4+87.sha-4975a10`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
