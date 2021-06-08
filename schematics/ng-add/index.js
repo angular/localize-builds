0a37554be2bbd6ce08477b585e715f2c39cf663b
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 01:44:31 +0000

    cba68e39c4 docs: Edits to remove jargon in Reference Observables, Dependency Injection (#42185)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.3+43.sha-34dd3c3"
+                version: "~12.0.3+44.sha-cba68e3"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
