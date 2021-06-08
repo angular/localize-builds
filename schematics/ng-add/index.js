4a5d5adab6291ef03af016cbe8cb4caf594833cc
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-08 20:04:32 +0000

    f3fde4bbb5 docs: Add selector for `PeekABooDirective` (#42146)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+84.sha-5c0541f"
+                version: "~12.1.0-next.4+86.sha-f3fde4b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
