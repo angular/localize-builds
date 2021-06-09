7c77f94ad0cb54375b4b9586a2faeb638d1c48c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 19:34:13 +0000

    785da0f1bf fix(core): ensure that autoRegisterModuleById registration in ɵɵdefineNgModule is not DCE-ed by closure (#42529)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.3+66.sha-dfb072a"
+                version: "~12.0.4+2.sha-785da0f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
