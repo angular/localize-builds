4c31ddbe365aa45023cf4503b7d8010573723326
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-09 17:05:29 +0000

    536c3738ba docs: clarify onSameUrlNavigation behavior (#42275)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.4+94.sha-3c25242`
+            version: `~12.1.0-next.4+95.sha-536c373`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
