3baedcfa9245dba50e9249fddbbb7022dec28541
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 23:34:33 +0000

    a75874c385 docs: CLI users polyfills (#42263)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.4+2.sha-785da0f"
+                version: "~12.0.4+4.sha-a75874c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
