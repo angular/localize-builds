d40b3b8602fa83f2faff09180a859f93afeaf703
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-09 23:30:36 +0000

    22bda2226b fix(compiler-cli): prevent prior compilations from being retained in watch builds (#42537)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.5+2.sha-3961b3c"
+                version: "~12.1.0-next.5+3.sha-22bda22"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
