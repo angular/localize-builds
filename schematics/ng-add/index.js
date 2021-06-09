f287b84d3156047c4f3e7b32f016a81586287c30
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-09 01:14:54 +0000

    3de774e778 docs: add guidance on adding filtering and sorting logic to componetns instead of pipes (#42368)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.4+92.sha-e1a80d4"
+                version: "~12.1.0-next.4+93.sha-3de774e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.4+92.sha-e1a80d4`
+            version: `~12.1.0-next.4+93.sha-3de774e`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
