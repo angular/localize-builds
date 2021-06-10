17523dc15fe8869ef30a392bc588e465d3a221a1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:21:18 +0000

    cddb3a88d3 docs: fix typo in "deploy multiple locales" section (#42541)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.5+7.sha-aef489f"
+                version: "~12.1.0-next.5+9.sha-cddb3a8"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
