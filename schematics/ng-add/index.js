407ba68a86fec62910b86f846cc8f396d0ef541d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 16:43:02 +0000

    e0158a1515 fix(zone.js): only one listener should also re-throw an error correctly (#41868)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.4+4.sha-a75874c"
+                version: "~12.0.4+5.sha-e0158a1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
