e02b229dcd2a5e2e3f5d9e92ee262d8c75bbb851
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-10 21:28:12 +0000

    013d1cb010 refactor(router): compute correct history restoration when navigation is cancelled (#38884)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.4+6.sha-5185795"
+                version: "~12.0.4+11.sha-013d1cb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
