0d213e034b1da74fef6083eb7f461cbe407f554e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:19:17 +0000

    e7c69b8551 docs: Grammatical error (#42571)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.5+20.sha-7180ec9`
+            version: `~12.1.0-next.5+21.sha-e7c69b8`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
