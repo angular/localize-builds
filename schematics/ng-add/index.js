8c195356297b3037804d71e0335f9f05eb574bf4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:18:09 +0000

    7180ec9e7c fix(forms): changes to status not always being emitted to statusChanges observable for async validators. (#42553)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.5+18.sha-828fde6"
+                version: "~12.1.0-next.5+20.sha-7180ec9"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
