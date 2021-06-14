68b679d1739daf08583ee453f8b79217ea442daf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 21:33:28 +0000

    a66dd8834c build: remove unnecessary polyfills from tests (#42567)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.5+31.sha-4001e9d"
+                version: "~12.1.0-next.5+32.sha-a66dd88"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.5+31.sha-4001e9d`
+            version: `~12.1.0-next.5+32.sha-a66dd88`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
