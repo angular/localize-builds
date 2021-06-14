e8c090d37f5e8c6c9b1c0857512d063803b98ed2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 21:32:00 +0000

    4001e9d808 fix(language-service): 'go to defininition' for objects defined in template (#42559)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.5+30.sha-228beea"
+                version: "~12.1.0-next.5+31.sha-4001e9d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.5+30.sha-228beea`
+            version: `~12.1.0-next.5+31.sha-4001e9d`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
