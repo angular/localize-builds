aee346a3b30409b4fb2a76358c310352772a4202
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-14 17:20:33 +0000

    56a0582d79 fix(docs-infra): correctly serve `index.html` with a query string (#42547)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.5+21.sha-e7c69b8`
+            version: `~12.1.0-next.5+19.sha-56a0582`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
