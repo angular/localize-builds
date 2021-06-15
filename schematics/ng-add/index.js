ec5ea907b0f3ad4c45726e3f1cdfcb7411ab628b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-15 20:13:31 +0000

    b52e93543f docs: add Daniel Trevino to contributors file (#42573)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.5+32.sha-a66dd88"
+                version: "~12.1.0-next.5+33.sha-b52e935"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
