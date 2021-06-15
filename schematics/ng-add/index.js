97635aac51f75787c7ae707ee3d8ffed7746433a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-15 21:22:02 +0000

    6bf4cb6d44 refactor(language-service): add missing helper method (#42579)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.4+21.sha-11e0f53"
+                version: "~12.0.4+25.sha-6bf4cb6"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
