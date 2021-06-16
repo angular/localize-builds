bb11d80116d2be18da115b2eabf9051474682f99
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:20:18 +0000

    f3a79878af docs(forms): correct sample code for FormArray.reset (#42477)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.5+41.sha-62aca30`
+            version: `~12.1.0-next.5+42.sha-f3a7987`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
