051e5c0c1a50a2bf7234925a7d1deb5d37c094b1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 23:54:24 +0000

    0e33521705 Revert "release: bump the next branch to v12.2.0-next.0"

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.5+49.sha-7961e2f`
+            version: `~12.1.0-next.5+50.sha-0e33521`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
