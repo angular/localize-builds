474d9742a53cc88bcc81049f07ef58e1deb68473
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:22:13 +0000

    3b81528ddd fix(docs-infra): improve card layout for different browser font sizes (#42533)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.5+42.sha-f3a7987`
+            version: `~12.1.0-next.5+43.sha-3b81528`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
