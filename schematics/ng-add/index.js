1efbaa388a2080f8ef4bdb4678a7d40d37c14fc5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-06-16 21:21:20 +0000

    62aca30286 feat(docs-infra): add support for "special elements" (#41299)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.5+40.sha-2d1347b"
+                version: "~12.1.0-next.5+41.sha-62aca30"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
