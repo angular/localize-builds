6bc84718414e97e5197f8ed24e72073c800dbb07
Dylan Hunn <dylhunn@gmail.com>
2021-06-17 18:23:55 +0000

    873229f24b feat(core): add opt-in test module teardown configuration (#42566)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.5+51.sha-f8e17c8"
+                version: "~12.1.0-next.6+1.sha-873229f"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.5+51.sha-f8e17c8`
+            version: `~12.1.0-next.6+1.sha-873229f`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
