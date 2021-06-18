679fd24462997d2869e54f02acd1df105319a2aa
Dylan Hunn <dylhunn@gmail.com>
2021-06-18 17:52:12 +0000

    0c105c38da fix(docs-infra): make `NotificationComponent` elements non-focusable when hidden (#42584)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.6+8.sha-983c540"
+                version: "~12.1.0-next.6+13.sha-0c105c3"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
