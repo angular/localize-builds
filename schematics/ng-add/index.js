a076cabe9623ee9af63698ae74682346d8fab0fb
Dylan Hunn <dylhunn@gmail.com>
2021-06-21 17:14:58 +0000

    e83d7cb2d3 refactor(compiler-cli): support xi18n in ngtsc (#42485)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.6+25.sha-d77f560"
+                version: "~12.1.0-next.6+27.sha-e83d7cb"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
