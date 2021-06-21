66f6e34a758bf5ed1bbd5088879edab69d45ed45
Dylan Hunn <dylhunn@gmail.com>
2021-06-21 19:12:25 +0000

    6b2a475532 docs: add new i18n formats to i18n guide (#42597)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.6+28.sha-8793d1a"
+                version: "~12.1.0-next.6+33.sha-6b2a475"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
