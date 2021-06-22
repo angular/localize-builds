b217f04b43054757e78c23a91893c7bbf344da45
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 16:47:43 +0000

    81a19e4e65 feat(dev-infra): update pullapprove verification to ensure all groups have reviewers (#42614)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.6+36.sha-75bbcf7"
+                version: "~12.1.0-next.6+37.sha-81a19e4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
