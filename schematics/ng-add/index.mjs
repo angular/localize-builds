1b8e3df58bbcdd66edca5b160bd4b0cacada9884
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 17:03:22 +0000

    9de65dbdce fix(compiler): should not break a text token on a non-valid start tag (#42605)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.6+39.sha-8a67770`
+            version: `~12.1.0-next.6+41.sha-9de65db`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
