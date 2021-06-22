83a2a67731090e561c4f1cf799cbb5c480ba64d8
Dylan Hunn <dylhunn@gmail.com>
2021-06-22 00:02:56 +0000

    cc672f05bf feat(compiler): add support for shorthand property declarations in templates (#42421)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.6+33.sha-6b2a475"
+                version: "~12.1.0-next.6+35.sha-cc672f0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.6+33.sha-6b2a475`
+            version: `~12.1.0-next.6+35.sha-cc672f0`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
