4974f90b89fbd2f20056779e6f73abc71a5eadc2
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 17:18:10 +0000

    cc30dc0713 fix(service-worker): ensure obsolete caches are always cleaned up (#42622)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.6+48.sha-874de59"
+                version: "~12.1.0-next.6+55.sha-cc30dc0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
