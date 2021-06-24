96c732a3ad2c2dc8fc3d389888fe276dc9aeda46
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 15:57:30 +0000

    2938008a20 ci: replace RBE instance name for components-repo-unit-tests job (#42645)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~11.2.14+1.sha-04f4155"
+                version: "~11.2.14+3.sha-2938008"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
