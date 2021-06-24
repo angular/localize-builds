978df0186c37a7a186c9452c5346fdd4dbb5aa2b
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 17:31:41 +0000

    4e46aef17b build: update dependency madge to v5 (#42629)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0-next.6+55.sha-cc30dc0"
+                version: "~12.1.0-next.6+56.sha-4e46aef"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0-next.6+55.sha-cc30dc0`
+            version: `~12.1.0-next.6+56.sha-4e46aef`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
