dfc9b6aadd730c3e6b3389229e89630f1d4acbef
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-24 20:15:23 +0000

    b31e905754 build(docs-infra): disambiguate doc paths for global APIs (#42648)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.0.5+39.sha-4459741"
+                version: "~12.0.5+42.sha-b31e905"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
