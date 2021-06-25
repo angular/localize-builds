10a96a3335537e6212ad935fbb374a3854893c28
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-25 17:21:33 +0000

    37a740c659 fix(compiler-cli): add support for partially evaluating types (#41661)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.0+2.sha-45d24d2`
+            version: `~12.2.0-next.0+3.sha-37a740c`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
