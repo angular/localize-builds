2c87b87bc31ebbedd53fba408f8931fdb60fbcaf
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-25 00:45:26 +0000

    261b060fa1 fix(dev-infra): fetch 100 branches from Github instead of 30 (#42658)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.0`
+            version: `~12.2.0-next.0+1.sha-261b060`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
