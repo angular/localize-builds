7f839d41098d742fc5520fff556419dc346e8164
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 19:19:30 +0000

    c93df7dc6d docs: correct literal string example (#42623)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.0+13.sha-b54e8ae"
+                version: "~12.2.0-next.0+12.sha-c93df7d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
