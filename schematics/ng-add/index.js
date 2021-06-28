3a7c69fc618ca9d9930b2202f8bfaa207fd4574b
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-28 16:54:47 +0000

    4bea630baa docs(core): add context jsdoc param to createEmbeddedView (#42675) PR Close #42675

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.0+9.sha-2a260dc"
+                version: "~12.2.0-next.0+10.sha-4bea630"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
