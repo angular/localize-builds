732cd89e482f945a3fab0fa6c6c34cc30c7fb758
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 16:46:35 +0000

    ace0a50646 docs: remove duplicate link to `guide/inputs-outputs.md` (#42654)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.0+12.sha-0f27f89"
+                version: "~12.1.0+13.sha-ace0a50"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
