b2f78be517ee86f95fcd922f6812fecb8ed2f51c
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 17:53:22 +0000

    f29fe5ced0 fix(dev-infra): use API pagination for retrieving project branches (#42666)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.0+17.sha-0274255`
+            version: `~12.2.0-next.0+19.sha-f29fe5c`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
