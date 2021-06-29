760968be6bf400ea3233ee09898d28d41eedc418
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 17:50:35 +0000

    78cf10b28b refactor(docs-infra): make angular.io and docs examples compatible with RxJS v7 (#42660)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0+13.sha-ace0a50`
+            version: `~12.1.0+15.sha-78cf10b`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
