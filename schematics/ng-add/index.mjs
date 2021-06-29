535f8e6f460cd31a97fcadd7802029a65a5d2b4d
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-29 18:37:46 +0000

    8528073f6c fix(dev-infra): remove bots from special thanks section (#42697)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.0+19.sha-f29fe5c`
+            version: `~12.2.0-next.0+20.sha-8528073`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
