703a00ae4673b3d2cecafcef5fe7f439ff9cb999
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 19:04:34 +0000

    59fe159b78 build: update API goldens to reflect new tool (#42688)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.0+24.sha-0e20b05"
+                version: "~12.2.0-next.0+27.sha-59fe159"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
