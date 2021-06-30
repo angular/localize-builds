df1cd3e4e5960df30857d7b40a60082e93959a25
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 17:54:40 +0000

    234b5edcc7 fix(platform-browser): in `Meta.addTag()` do not add duplicate meta tags (#42703)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.0+21.sha-f3965ff"
+                version: "~12.2.0-next.0+22.sha-234b5ed"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
