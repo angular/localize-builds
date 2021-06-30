8a05b49f7ef545f3950511dc6f76bcc81cd3cd6d
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 21:51:41 +0000

    41c6877c01 fix(core): error in TestBed if module is reset mid-compilation in ViewEngine (#42669)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.0+23.sha-12e0cfa`
+            version: `~12.1.1+1.sha-41c6877`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
