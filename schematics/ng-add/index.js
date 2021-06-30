9bbcad4eb5dcdcf34a58b3233d2a1f9cfcf7dac3
Jessica Janiuk <jessicajaniuk@google.com>
2021-06-30 16:35:51 +0000

    f3965ff834 docs: setting context value inside an interceptor (#42699)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.0+20.sha-8528073"
+                version: "~12.2.0-next.0+21.sha-f3965ff"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.0+20.sha-8528073`
+            version: `~12.2.0-next.0+21.sha-f3965ff`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
