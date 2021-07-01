2ba0adf3b42b4d96559d7415c86f09d2935076c5
Jessica Janiuk <jessicajaniuk@google.com>
2021-07-01 16:57:35 +0000

    74350a5cf1 fix(compiler-cli): return directives for an element on a microsyntax template (#42640)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.1+3.sha-bfa1b5d`
+            version: `~12.2.0-next.1+4.sha-74350a5`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
