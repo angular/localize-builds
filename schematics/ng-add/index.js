fc059d5d2d5827d393b8c47837496d6897008ab6
atscott <atscott01@gmail.com>
2021-07-01 22:42:27 +0000

    97c18f4527 fix(language-service): Do not override TS LS methods not supported by VE NgLS (#42727)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.1+4.sha-df5cc1f"
+                version: "~12.1.1+5.sha-97c18f4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.1+4.sha-df5cc1f`
+            version: `~12.1.1+5.sha-97c18f4`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
