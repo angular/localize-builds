400af91dd46d3c78e46040dc6eb36a99f2c8d813
Jessica Janiuk <jessicajaniuk@google.com>
2021-07-01 16:54:17 +0000

    bfa1b5d9eb fix(docs-infra): move dots inside Usage Notes anchor tag (#42725)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+2.sha-d997303"
+                version: "~12.2.0-next.1+3.sha-bfa1b5d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.1+2.sha-d997303`
+            version: `~12.2.0-next.1+3.sha-bfa1b5d`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
