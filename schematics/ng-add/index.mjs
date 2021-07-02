79ba8f9baf3b1b15bc9540913a5acb6b0d4e7055
atscott <atscott01@gmail.com>
2021-07-02 17:10:15 +0000

    19bcdfb028 fix(docs-infra): correctly configure top-menu nav-item in the sidenav (#42747)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.1+5.sha-ffeea63`
+            version: `~12.2.0-next.1+7.sha-19bcdfb`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
