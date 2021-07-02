fb570efcba026c06893dca566058e06b8449f477
atscott <atscott01@gmail.com>
2021-07-02 17:11:24 +0000

    e81efe6667 docs: convert commit SHAs and PR numbers in changelog to links (#42732)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.1+7.sha-19bcdfb`
+            version: `~12.2.0-next.1+9.sha-e81efe6`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
