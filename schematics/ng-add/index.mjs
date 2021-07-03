6128f5a24e2b9438e5d8d2ce8662bbf0e949eb55
atscott <atscott01@gmail.com>
2021-07-03 00:35:49 +0000

    9557e87ad8 fix(dev-infra): missing `pkg_npm` substitution for top-level targets being accessed (#42754)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.1+12.sha-6da24e6`
+            version: `~12.1.1+16.sha-9557e87`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
