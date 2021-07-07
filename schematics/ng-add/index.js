95a6110d6bb17525690f9a4e96c4d13a4969adfb
atscott <atscott01@gmail.com>
2021-07-07 22:38:16 +0000

    30c82cd177 fix(compiler-cli): inline type checking instructions no longer prevent incremental reuse (#42759)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+25.sha-95ba5b4"
+                version: "~12.2.0-next.1+26.sha-30c82cd"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
