aad50f2f03bdf176b2f7d2682c39b96700d996c1
Andrew Scott <atscott@google.com>
2021-07-07 20:57:26 +0000

    24ac37891c fix(docs-infra): slightly improve top-menu responsiveness (#42753)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.1+18.sha-e064f17`
+            version: `~12.2.0-next.1+19.sha-24ac378`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
