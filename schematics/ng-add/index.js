314c2cf36301160d5e16768ceadbf5888518b2c2
Andrew Scott <atscott@google.com>
2021-07-07 21:00:11 +0000

    95ba5b4edb fix(docs-infra): ensure the sidenav backdrop covers the floating ToC (#42787)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+24.sha-8b95816"
+                version: "~12.2.0-next.1+25.sha-95ba5b4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.1+24.sha-8b95816`
+            version: `~12.2.0-next.1+25.sha-95ba5b4`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
