0b7d4c44ec1ab6260b3fdfaaf42b8da670ec0fb8
atscott <atscott01@gmail.com>
2021-07-08 17:52:43 +0000

    f592a12005 fix(service-worker): avoid storing redundant metadata for hashed assets (#42606)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+39.sha-cb2ca9a"
+                version: "~12.2.0-next.1+28.sha-f592a12"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
