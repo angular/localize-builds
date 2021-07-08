04d7fd650f9369cf003d066dc600d46e8fe16189
atscott <atscott01@gmail.com>
2021-07-08 21:28:54 +0000

    c30502ee1b ci: use issue forms for issue creation (#42099)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.1+40.sha-e26bfc1`
+            version: `~12.2.0-next.1+42.sha-c30502e`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
