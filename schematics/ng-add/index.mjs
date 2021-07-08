279b0c4b8096abe695de2121764e0713f72e7559
atscott <atscott01@gmail.com>
2021-07-08 17:48:41 +0000

    d87917542a fix(service-worker): correctly handle unrecoverable state when a client no longer exists (#42736)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.1.1+26.sha-0a17e98`
+            version: `~12.1.1+39.sha-d879175`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
