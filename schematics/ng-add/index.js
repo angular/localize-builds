32db5a7e5bc152cecdaf5354d6adffbb8f4bb9e3
atscott <atscott01@gmail.com>
2021-07-08 22:13:17 +0000

    89084ffcf9 docs(core): initializeApp method should return function (#42743)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+43.sha-e7832a6"
+                version: "~12.2.0-next.1+44.sha-89084ff"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
