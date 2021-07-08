f8466d3d79016841fef3572a96f14ab9d159175e
atscott <atscott01@gmail.com>
2021-07-08 22:11:50 +0000

    1f3747b583 ci: configure renovate to ignore `@microsoft/api-extractor` (#42797)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.1+44.sha-89084ff`
+            version: `~12.2.0-next.1+45.sha-1f3747b`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
