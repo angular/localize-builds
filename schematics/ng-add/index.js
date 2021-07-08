80df7e06d823b162c39df3356c66b5a4d6e39cff
atscott <atscott01@gmail.com>
2021-07-08 17:47:53 +0000

    ad08b17b05 docs(forms): add diff between add and set control (#42638)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+26.sha-30c82cd"
+                version: "~12.2.0-next.1+29.sha-ad08b17"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.1+26.sha-30c82cd`
+            version: `~12.2.0-next.1+29.sha-ad08b17`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
