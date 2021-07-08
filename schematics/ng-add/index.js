e1448ea550d405a8febcba785e838b3617088cfb
atscott <atscott01@gmail.com>
2021-07-08 22:10:56 +0000

    e7832a6b2f docs(core): fix spelling error (#42783)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+42.sha-c30502e"
+                version: "~12.2.0-next.1+43.sha-e7832a6"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
