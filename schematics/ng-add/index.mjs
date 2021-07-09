9755c3a741000c66656cbe9801c0e48f29f79699
Andrew Kushnir <akushnir@google.com>
2021-07-09 19:36:40 +0000

    9d58ebfcee test(bazel): update `example_package.golden` (#42804)

--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.1+50.sha-3791ae0`
+            version: `~12.2.0-next.1+53.sha-9d58ebf`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
