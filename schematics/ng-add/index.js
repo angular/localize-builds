f2a5ca1fd9605741f72a969cd514c8a2dbd4cbf3
Andrew Kushnir <akushnir@google.com>
2021-07-09 17:39:57 +0000

    3791ae0c95 refactor(router): Adjust behavior for computed navigation restoration (#42751)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+49.sha-5356796"
+                version: "~12.2.0-next.1+50.sha-3791ae0"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
--- schematics/ng-add/index.mjs
+++ schematics/ng-add/index.mjs
@@ -88,7 +88,7 @@ function moveToDependencies(host, context) {
         addPackageJsonDependency(host, {
             name: '@angular/localize',
             type: NodeDependencyType.Default,
-            version: `~12.2.0-next.1+49.sha-5356796`
+            version: `~12.2.0-next.1+50.sha-3791ae0`
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
