b076c2928c872a3c5e8d811137777cfc5145fe69
Andrew Kushnir <akushnir@google.com>
2021-07-12 19:12:06 +0000

    4c482bf3f1 fix(compiler-cli): properly emit literal types when recreating type parameters in a different file (#42761)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+62.sha-40da386"
+                version: "~12.2.0-next.1+63.sha-4c482bf"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
