98e7c49e61c15169224665726811d7b6a296caba
Andrew Kushnir <akushnir@google.com>
2021-07-12 19:25:27 +0000

    e8be045cbd refactor(core): optimize the implementation about finding context from error in ErrorHandler (#42581)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+63.sha-4c482bf"
+                version: "~12.2.0-next.1+67.sha-e8be045"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
