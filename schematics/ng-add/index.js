ea109315a421091601bd10d70bd4900eae314425
Andrew Kushnir <akushnir@google.com>
2021-07-12 21:59:38 +0000

    9456eca7c5 feat(dev-infra): better caching for browser archive contents (#42814)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+93.sha-e6593ad"
+                version: "~12.2.0-next.1+91.sha-9456eca"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
