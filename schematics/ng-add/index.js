b9e1693f7c7260b26aade763ea7336ea2c64fd4b
Andrew Kushnir <akushnir@google.com>
2021-07-12 16:59:51 +0000

    ef15b979cf docs(core): edit and add docs for ng-template (#42704)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+58.sha-762e057"
+                version: "~12.2.0-next.1+59.sha-ef15b97"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
