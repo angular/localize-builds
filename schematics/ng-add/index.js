caec577223094c7f156fdcfeaf027b700e2019d7
Andrew Kushnir <akushnir@google.com>
2021-07-12 17:08:45 +0000

    40da386a4d docs(core): fix `ViewChildren` code examples to avoid TS error (#42816)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+61.sha-4e73b88"
+                version: "~12.2.0-next.1+62.sha-40da386"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
