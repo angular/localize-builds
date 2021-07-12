8b40bf6f6920f398050bcf457a9a05e784fd4880
Andrew Kushnir <akushnir@google.com>
2021-07-12 20:41:48 +0000

    a524af15a5 docs: move start tutorial alert for clarity (#42764)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+89.sha-b5ab7af"
+                version: "~12.2.0-next.1+90.sha-a524af1"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
