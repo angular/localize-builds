aea86aa9df9f058a84cd8c6303fe87b31bc80659
Andrew Kushnir <akushnir@google.com>
2021-07-13 21:33:51 +0000

    51156f3f07 fix(core): allow proper type inference when `ngFor` is used with a `trackBy` function (#42692)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+102.sha-970d7f7"
+                version: "~12.2.0-next.1+103.sha-51156f3"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
