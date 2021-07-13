0f244e7d6c54294741a53c65c7dd52f5d2359a13
Andrew Kushnir <akushnir@google.com>
2021-07-13 16:11:24 +0000

    81dce5c664 fix(compiler-cli): check split two way binding (#42601)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+99.sha-88b15d5"
+                version: "~12.2.0-next.1+100.sha-81dce5c"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
