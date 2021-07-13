205668b950ed9964d40b59c98b605e3c17c80b30
Andrew Kushnir <akushnir@google.com>
2021-07-13 18:55:20 +0000

    d654c7933a fix(common): re-sort output of `KeyValuePipe` when `compareFn` changes (#42821)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.1+92.sha-b101ad0"
+                version: "~12.1.1+93.sha-d654c79"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
