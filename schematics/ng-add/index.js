572234a287b0a238c359f7e6c5216b8eab92b8dc
Andrew Kushnir <akushnir@google.com>
2021-07-13 23:19:50 +0000

    63013546e1 fix(core): associate the NgModule scope for an overridden component (#42817)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.1+95.sha-9ebd41e"
+                version: "~12.1.1+96.sha-6301354"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
