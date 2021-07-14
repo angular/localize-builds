66fec7caa9843797806c07730fcfe4cf47ea4dd8
Andrew Kushnir <akushnir@google.com>
2021-07-14 18:44:51 +0000

    437759ba47 revert: docs: remove references to Trusted Types due to incompatibility with Webpack 5 (#42796)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+2.sha-48c1c81"
+                version: "~12.2.0-next.2+3.sha-437759b"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
