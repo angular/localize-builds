7289fff8660da1d89835be43466421dccfe03035
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:35:27 +0000

    07d7e6034f perf(compiler-cli): optimize cycle detection using a persistent cache (#41271)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+3.sha-437759b"
+                version: "~12.2.0-next.2+4.sha-07d7e60"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
