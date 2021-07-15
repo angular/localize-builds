b03b8d2d4752bafe835c25f74e6e1a8ef673c117
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:56:44 +0000

    74228215a1 build: update dependency @microsoft/api-extractor to v7.18.4 (#42864)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+15.sha-642362a"
+                version: "~12.2.0-next.2+13.sha-7422821"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
