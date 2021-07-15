2f55d8b53ae4b00647b4e3d1c2bb49653aa3b09b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:41:23 +0000

    670300e9ab build: lock file maintenance (#42824)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+5.sha-31593db"
+                version: "~12.2.0-next.2+10.sha-670300e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
