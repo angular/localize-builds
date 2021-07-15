b6769ad5d4d00422d8ad39f863288ff1af4df9d2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:40:32 +0000

    31593db489 refactor(core): expand error logging when the JIT compiler is not available (#42693)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+4.sha-07d7e60"
+                version: "~12.2.0-next.2+5.sha-31593db"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
