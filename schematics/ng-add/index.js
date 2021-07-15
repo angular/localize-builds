918121baac0bd4842fc2d37f92a3c51a56ed1056
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-15 20:54:45 +0000

    97fa1cf544 build: add configuration for caretaker handoff (#42859)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.2+3.sha-e6f01d7"
+                version: "~12.1.2+13.sha-97fa1cf"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
