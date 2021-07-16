3a12273592c70075541eef21ad53e013bf63477b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-16 20:07:32 +0000

    3b2f607cda build: generate closure locale files using hard-coded list of locales (#42230)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+16.sha-3da7146"
+                version: "~12.2.0-next.2+24.sha-3b2f607"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
