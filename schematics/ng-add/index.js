82646fdea1a27c141e3561e9ff782254e2bea5c6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 19:19:25 +0000

    e2aa7652e5 fix(docs-infra): fix keyword color in CLI pages code blocks (#42889)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+44.sha-70c3461"
+                version: "~12.2.0-next.2+45.sha-e2aa765"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
