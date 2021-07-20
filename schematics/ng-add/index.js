57bcd1005f0ef1aa7a34c23c0760a463dd66d200
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 18:50:52 +0000

    282e86ad71 fix(dev-infra): use the version from package.json rather than tags (#42872)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+40.sha-52dc65d"
+                version: "~12.2.0-next.2+42.sha-282e86a"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
