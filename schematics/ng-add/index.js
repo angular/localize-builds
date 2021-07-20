c91c80c5fe3744704938d47fd729b4c724fa19a9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 18:52:30 +0000

    445fee4174 feat(dev-infra): update package version verification to allow for experimental version (#42898)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+42.sha-282e86a"
+                version: "~12.2.0-next.2+41.sha-445fee4"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
