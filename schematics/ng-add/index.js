3cee624a964bcf88694630f0f11b54a0c7e135d2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:56:31 +0000

    722eb5dd45 fix(docs-infra): prevent main content from overlapping with ToC (#42884)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+35.sha-7271ad1"
+                version: "~12.2.0-next.2+38.sha-722eb5d"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
