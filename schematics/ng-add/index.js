0949f8dea22044d497b7c9ef1f97904030c45588
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-07-20 00:32:03 +0000

    0ce8f6e62d refactor(animations): remove unused animation trigger imports (#42763)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+24.sha-3b2f607"
+                version: "~12.2.0-next.2+28.sha-0ce8f6e"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
