fb9bb6dcce10d74321cb69a2135996539ea80f59
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:55:28 +0000

    4d86ea6471 ci: no longer use deprecated `pullapprove_conditions` option (#42895)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+46.sha-23d6133"
+                version: "~12.2.0-next.3+3.sha-4d86ea6.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
