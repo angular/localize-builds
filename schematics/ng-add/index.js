8d0e3a448f7c31e4c5293c083f3307c61d8ec73d
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 23:03:59 +0000

    f589b01672 perf(compiler-cli): skip analysis in incremental builds for files without Angular behavior (#42562)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.2+34.sha-b0854e2"
+                version: "~12.1.3+5.sha-f589b01.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
