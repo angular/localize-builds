dfbc1500285e9cb81a7a7c63281a65a96d2d493c
Dylan Hunn <dylhunn@gmail.com>
2021-07-21 22:57:39 +0000

    a502279592 feat(forms): allow minLength/maxLength validator to be bound to `null` (#42565)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.3+4.sha-eefe168.with-local-changes"
+                version: "~12.2.0-next.3+5.sha-a502279.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
