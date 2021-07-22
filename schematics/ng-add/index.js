f8a15327357c3ec6c03bea03b0f330b9711da3d8
Dylan Hunn <dylhunn@gmail.com>
2021-07-22 16:51:26 +0000

    1d9d02696e feat(forms): add hasValidators, addValidators, and removeValidators methods (for both sync and async) (#42838)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.3+7.sha-b5f1779.with-local-changes"
+                version: "~12.2.0-next.3+10.sha-1d9d026.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
