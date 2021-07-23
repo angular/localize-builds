d495c05ce147f6593f7dcf8209aa71987b794861
Dylan Hunn <dylhunn@gmail.com>
2021-07-23 17:59:30 +0000

    772dbe8a8e feat(dev-infra): include experimental version stamp in env stamps (#42934)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.3+11.sha-c20103c.with-local-changes"
+                version: "~12.1.1+47.sha-12a9d21.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
