8f54c81e3774318249be2172f7dbdf2c728de48a
Dylan Hunn <dylhunn@gmail.com>
2021-07-26 19:23:26 +0000

    72c03a0d4f build: remove ts-api-guardian from repository (#42735)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+78.sha-634ba9c.with-local-changes"
+                version: "~12.2.0-next.1+79.sha-ff87da3.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
