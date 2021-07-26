91a20072a3a308b44d4feeb0269342622b4ad9d3
Dylan Hunn <dylhunn@gmail.com>
2021-07-26 20:37:18 +0000

    ed9cfb674f fix(compiler-cli): use correct module resolution context for absolute imports in .d.ts files (#42879)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+80.sha-22290af.with-local-changes"
+                version: "~12.2.0-next.1+81.sha-c13ccc3.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
