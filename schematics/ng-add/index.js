51b8f349fa816d89f0e9755e0a398264a790aaa0
Dylan Hunn <dylhunn@gmail.com>
2021-07-27 23:53:27 +0000

    0d81b007e4 fix(router): add missing outlet events to RouterOutletContract (#42431) Exposes both activateEvents and deactivateEvents as the original outlet interface did.

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+85.sha-bfc4c3c.with-local-changes"
+                version: "~12.2.0-next.1+86.sha-5b5868d.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
