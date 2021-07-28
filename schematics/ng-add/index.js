565a1ec3c6bbdc5f28306f928fc503fb8b6f72d0
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 20:21:42 +0000

    b075721a49 fix(dev-infra): ensure that building environment stamp fails silently (#42985)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+92.sha-867000e.with-local-changes"
+                version: "~12.2.0-next.1+93.sha-e6593ad.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
