72c8811381103859bde0c24014e8eeedb95a69d2
Dylan Hunn <dylhunn@gmail.com>
2021-07-28 23:16:03 +0000

    da35c0409f docs: Add josmar-crwdstffng to contributors list. (#42972)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.1.1+64.sha-2afaaf0.with-local-changes"
+                version: "~12.1.1+65.sha-c1f9d80.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
