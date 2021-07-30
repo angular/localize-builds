bfc0aaac4277d2a29352bb16034c1070b5e12551
Andrew Kushnir <akushnir@google.com>
2021-07-30 16:52:33 +0000

    0af354ce05 refactor(bazel): extract function to patch fileNameToModuleName on host (#42974)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.1+98.sha-c1c1cda.with-local-changes"
+                version: "~12.2.0-next.1+100.sha-81dce5c.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
