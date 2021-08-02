089db36195814fb8f4f6e824ea60960509c6f140
atscott <atscott01@gmail.com>
2021-08-02 18:02:34 +0000

    5d8759b6a2 build(docs-infra): upgrade cli command docs sources to 129d45cfa (#43004)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+10.sha-670300e.with-local-changes"
+                version: "~12.2.0-next.2+11.sha-4e97514.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
