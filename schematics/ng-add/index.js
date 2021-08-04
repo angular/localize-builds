ab55d51a3aab64b9bc6b600e447a6b57b3f6b453
Andrew Scott <atscott@google.com>
2021-08-04 16:18:29 +0000

    bd7f0d8b70 fix(core): incorrect error reported when trying to re-create view which had an error during creation (#43005)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+23.sha-9d1deb1.with-local-changes"
+                version: "~12.2.0-next.2+24.sha-3b2f607.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
