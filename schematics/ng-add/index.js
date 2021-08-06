3d94f41e95da28fddccdac4255dd3872ffd31fe1
Andrew Kushnir <akushnir@google.com>
2021-08-06 16:13:51 +0000

    d8690d867c fix(dev-infra): search since HEAD on the local branch for version tags when creating stamps (#43065)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0-next.2+36.sha-7eddd12.with-local-changes"
+                version: "~13.0.0-next.0+5.sha-d8690d8.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
