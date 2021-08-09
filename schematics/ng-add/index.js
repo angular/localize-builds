d6f243ccdf7a0d252ef2ecbad983cf2aff804a3b
Andrew Kushnir <akushnir@google.com>
2021-08-09 17:47:45 +0000

    dd3abdb9d9 fix(router): ensure check for match options is compatible with property renaming (#43086)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.0+7.sha-d93427b.with-local-changes"
+                version: "~12.2.0+8.sha-dd3abdb.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
