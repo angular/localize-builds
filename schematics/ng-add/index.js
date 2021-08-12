45a0e01538b75d71380cae046139570df9bbb694
Dylan Hunn <dylhunn@gmail.com>
2021-08-12 22:59:54 +0000

    470371f1b2 docs: fix description: "property decorator" (not parameter) (#43068) (#43128)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.1+5.sha-398be10.with-local-changes"
+                version: "~13.0.0-next.1+6.sha-470371f.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
