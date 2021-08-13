d5a1d31bb6e45b89ce9fc0ff35926d3b932696d5
Dylan Hunn <dylhunn@gmail.com>
2021-08-13 22:19:09 +0000

    2a1aa5867d ci: add breaking changes auto labeling action (#43141)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.1+6.sha-470371f.with-local-changes"
+                version: "~13.0.0-next.1+7.sha-2a1aa58.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
