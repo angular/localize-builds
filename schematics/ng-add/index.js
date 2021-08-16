8a2bfa6b72a0a4b8581993e39c657e726c6a1189
Dylan Hunn <dylhunn@gmail.com>
2021-08-16 18:09:18 +0000

    d743dd9900 build: update dependency http-server to v13 (#43096)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.1+13.sha-30bf472.with-local-changes"
+                version: "~13.0.0-next.1+17.sha-d743dd9.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
