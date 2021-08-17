6c475cff7a94e39308fbbc85ccf9c8d84e3d2471
Dylan Hunn <dylhunn@gmail.com>
2021-08-17 16:43:41 +0000

    545972e5e9 docs(upgrade): add `@extensible` to the `UpgradeComponent` class (#42807)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.1+26.sha-bb65559.with-local-changes"
+                version: "~12.2.1+29.sha-545972e.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
