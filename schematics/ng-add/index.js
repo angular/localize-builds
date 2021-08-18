ed0abc8e850b851b39d0c89941d90dbabf8852de
Dylan Hunn <dylhunn@gmail.com>
2021-08-18 16:45:17 +0000

    c366d1c8a0 docs: tidy up description of NgComponentOutlet (#43159)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.1+40.sha-3db35b3.with-local-changes"
+                version: "~13.0.0-next.1+41.sha-c366d1c.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
