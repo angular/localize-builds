40b2939a1960cc8223d6e915da77dc932cb86009
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-19 19:18:25 +0000

    cb8e51f8cf test(language-service): add extended diagnostics test (#43107)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.2+2.sha-19fa033.with-local-changes"
+                version: "~12.2.2+7.sha-cb8e51f.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
