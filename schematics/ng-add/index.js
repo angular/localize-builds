ac87be7d74482271898e5aba2faa9639694669f9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-08-23 20:49:33 +0000

    6e924313c3 fix(service-worker): NPE if onActionClick is undefined (#43210)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.2+9.sha-ccb09b4.with-local-changes"
+                version: "~13.0.0-next.2+11.sha-6e92431.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
