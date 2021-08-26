04ef34d9813d1cded3d2666b0f45e7b218a163fc
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 17:07:23 +0000

    030ff0d7a1 test(ngcc): use @angular npm packages in integration test (#43234)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.3+3.sha-298d8a4.with-local-changes"
+                version: "~13.0.0-next.3+5.sha-030ff0d.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
