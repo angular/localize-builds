f8aa9c051d2df362454dbf5270ca109059c41237
Jessica Janiuk <jessicajaniuk@google.com>
2021-08-26 17:27:14 +0000

    fdcb420bd7 docs(router): Fix incorrect name for url match options (#43268)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.3+3.sha-fb64f71.with-local-changes"
+                version: "~12.2.3+5.sha-fdcb420.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
