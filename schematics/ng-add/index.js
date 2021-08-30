935fec2fe3e5df7c7ec141f63e373e5d61c32fae
Joey Perrott <josephperrott@gmail.com>
2021-08-30 15:45:58 +0000

    f3c8408b25 ci: update pullapprove config to reflect kyliau's departure (#43230)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.3+16.sha-95cfd0b.with-local-changes"
+                version: "~13.0.0-next.3+18.sha-f3c8408.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
