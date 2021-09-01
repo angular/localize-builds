4b68100470b01076a55778dac4a80a6905b8b3a0
Andrew Scott <atscott01@gmail.com>
2021-09-01 21:40:32 +0000

    a0bd6e90f9 fix(router): add more context to `Unhandled Navigation Error` (#43291)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.3+17.sha-caecd18.with-local-changes"
+                version: "~12.2.4+1.sha-a0bd6e9.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
