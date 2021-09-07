cb15d5180311f338b8176039b9b74d04a04de731
Andrew Scott <atscott01@gmail.com>
2021-09-07 21:49:59 +0000

    d647491679 docs: remove Angular 9 from support table (#43350)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.4+22.sha-2e11be8.with-local-changes"
+                version: "~13.0.0-next.4+24.sha-d647491.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
