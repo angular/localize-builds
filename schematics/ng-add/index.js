d2387ced4377caf3b028d1cffb52be5e7d03122a
Andrew Scott <atscott01@gmail.com>
2021-09-07 19:05:57 +0000

    3b4f121caf docs(common): correct typos in jsdoc of http client (#43352)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.4+10.sha-aa1782c.with-local-changes"
+                version: "~12.2.4+12.sha-3b4f121.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
