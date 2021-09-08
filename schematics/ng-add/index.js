62f9d1a5e169507c68ae35652f46ddd9af504b2b
Andrew Scott <atscott01@gmail.com>
2021-09-08 17:15:39 +0000

    9e039ca68b fix(router): Only trigger router navigation on `popstate` events from `Location` subscription (#43328)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.4+29.sha-93c9932.with-local-changes"
+                version: "~13.0.0-next.5+1.sha-9e039ca.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
