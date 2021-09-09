5f668caa141cb894865fdc43252f3bdf27e66612
Andrew Kushnir <akushnir@google.com>
2021-09-09 16:42:10 +0000

    6c84c5f513 fix(animations): emit pure annotations to static property initializers (#43344)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.5+6.sha-4c307ad.with-local-changes"
+                version: "~13.0.0-next.5+8.sha-6c84c5f.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
