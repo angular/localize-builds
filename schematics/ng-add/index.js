767d6ccaca4957c98cab0b799013c129df2c0215
Andrew Kushnir <akushnir@google.com>
2021-09-09 18:53:57 +0000

    8cc93b723c docs: add Angular Projects book in resources (#43130)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.5+4.sha-2658219.with-local-changes"
+                version: "~12.2.5+8.sha-8cc93b7.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
