a8f3f677266b61b581812f302c8688016d65aaf9
Andrew Kushnir <akushnir@google.com>
2021-09-13 22:00:47 +0000

    79eee5509f docs(router): Add documentation for the router reset rawUrlTree (#43420)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.5+32.sha-59353c6.with-local-changes"
+                version: "~13.0.0-next.5+33.sha-79eee55.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
