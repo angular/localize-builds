f82ef952d3ef6a471172645764a26b514cf96f39
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-16 20:41:08 +0000

    79700822fd refactor(router): remove unnecessary getTransition function (#43436)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.6+27.sha-e55d052.with-local-changes"
+                version: "~13.0.0-next.6+28.sha-7970082.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
