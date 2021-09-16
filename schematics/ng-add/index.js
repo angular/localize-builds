38984e693af19aa3cdb5b77df396685451073bca
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-16 16:20:16 +0000

    7efa47075c docs: remove http example dependency on 3rd party service (#43475)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.6+14.sha-e5d08c1.with-local-changes"
+                version: "~13.0.0-next.6+19.sha-7efa470.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
