a97865e7fa5cd6817f090b7d37d69a66d06784c1
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-16 18:38:15 +0000

    05e1adba48 test(compiler): add a test for parsing multiline expressions in attributes (#43132)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.6+21.sha-b43f21b.with-local-changes"
+                version: "~12.2.6+28.sha-05e1adb.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
