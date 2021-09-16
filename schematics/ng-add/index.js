8b21604c40475b96a4c448ead5259b6fade2bc6e
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-16 17:11:37 +0000

    b43f21bb15 refactor(compiler): improve regular expression for stripping comments (#43456)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.6+15.sha-def6315.with-local-changes"
+                version: "~12.2.6+21.sha-b43f21b.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
