859a230cd1bc355efc2db9bb750a5be674f171d7
Jessica Janiuk <jessicajaniuk@google.com>
2021-09-22 16:38:50 +0000

    9b7cdaefe8 docs(http): get rid of not needed space at the end of the sentence (#43528)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.6+51.sha-3cc8c7e.with-local-changes"
+                version: "~12.2.6+55.sha-9b7cdae.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
