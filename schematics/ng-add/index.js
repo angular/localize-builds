b7a2de6a1b5472c8c0202a7abff3643348d6d2f3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-24 18:19:15 +0000

    d02c8da7bd fix(docs-infra): display deprecation notes for properties (#43566)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.7+40.sha-9d290bd.with-local-changes"
+                version: "~13.0.0-next.7+39.sha-d02c8da.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
