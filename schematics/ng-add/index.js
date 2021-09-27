40bf55538e5866ec8f0ac825ebbfc19a92571ad3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-27 18:07:05 +0000

    81d09d14dc docs: deprecate input setter coercion fields (#43506)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.8+2.sha-3e37e89.with-local-changes"
+                version: "~13.0.0-next.8+3.sha-81d09d1.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
