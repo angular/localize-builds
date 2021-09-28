fd80179dc28cd692a006387286d88323242357b9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-28 21:37:03 +0000

    8d2b6affcd fix(compiler-cli): correctly interpret token arrays in @Injectable `deps` (#43226)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.8+11.sha-de662da.with-local-changes"
+                version: "~13.0.0-next.8+13.sha-8d2b6af.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
