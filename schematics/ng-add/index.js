bb523c5a5693234f47189df4f3d86ed7ef70a772
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 17:20:22 +0000

    d1589604a8 build(docs-infra): ensure all overloads are shown in interfaces (#43614)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.8+25.sha-e0a7285.with-local-changes"
+                version: "~13.0.0-next.8+26.sha-d158960.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
