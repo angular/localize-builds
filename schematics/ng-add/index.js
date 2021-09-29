da5d408e166b0953159f26bfae1e84b0ac3302e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-09-29 18:59:46 +0000

    ab3de40ba3 Revert "refactor(migrations): support use of an ESM `@angular/compiler` package (#43627)"

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.8+29.sha-09d325a.with-local-changes"
+                version: "~13.0.0-next.8+30.sha-ab3de40.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
