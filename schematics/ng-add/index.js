b14bd32dd9a353b22fbda331a4519ab54804b715
Dylan Hunn <dylhunn@gmail.com>
2021-10-01 16:50:53 +0000

    9eba2609f3 test(core): update matcher.jasmineToString() to work with jasmine@3.6.0 (#43411)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.9+6.sha-74ca3c5.with-local-changes"
+                version: "~13.0.0-next.9+10.sha-9eba260.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
