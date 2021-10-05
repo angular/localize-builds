c5b2b85e6e9a0fd8096a974248e0fa7596e3a7ed
Dylan Hunn <dylhunn@gmail.com>
2021-10-05 00:16:22 +0000

    81c7eb813c feat(core): add migration to opt out existing apps from new test module teardown behavior (#43353)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.10+11.sha-6d1c2f7.with-local-changes"
+                version: "~13.0.0-next.10+22.sha-81c7eb8.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
