9d12003dc8e415cf984af8bcd674e286550c030a
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 18:14:39 +0000

    e0a0d05d45 feat(core): update node version support range to support v16 (#43740)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.12+14.sha-841c5ae.with-local-changes"
+                version: "~13.0.0-next.12+16.sha-e0a0d05.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
