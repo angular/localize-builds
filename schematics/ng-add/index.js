2d196947aae39d7156e0d61cc9aba1d249654c2b
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 02:11:10 +0000

    e3d6e97050 docs: deprecate ViewEngine-based JIT Compiler symbols (#43710)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.11+16.sha-7acf3be.with-local-changes"
+                version: "~13.0.0-next.12+1.sha-e3d6e97.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
