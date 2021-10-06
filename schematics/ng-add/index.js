2fca598354dd7f024e4364c86b5fb8215226faf1
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 17:23:51 +0000

    23ac3f7cef ci: test AIO/docs examples against the latest RxJS 7.x version (v7.3.1) (#43683)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.12+5.sha-52587e5.with-local-changes"
+                version: "~13.0.0-next.12+9.sha-23ac3f7.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
