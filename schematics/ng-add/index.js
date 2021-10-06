a11dad0b3bd4e26613a4373d47e9e54c7b09e5ec
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 16:42:23 +0000

    6ac35d0660 fix(docs-infra): convert button-like elements to actual buttons (#43601)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.12+2.sha-fa6aa1b.with-local-changes"
+                version: "~13.0.0-next.12+4.sha-6ac35d0.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
