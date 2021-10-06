4eadc2f31b6d7a8d8203da6b7a6e5f8026074694
Dylan Hunn <dylhunn@gmail.com>
2021-10-06 17:25:04 +0000

    841c5aebbf build(docs-infra): ensure `ngsw.json` generation is correct and deterministic (#43686)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.12+9.sha-23ac3f7.with-local-changes"
+                version: "~13.0.0-next.12+14.sha-841c5ae.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
