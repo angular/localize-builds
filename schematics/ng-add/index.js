b0abb77c4d709b5e77217c4b64a1cf8829f5296a
Andrew Scott <atscott01@gmail.com>
2021-10-11 17:35:59 +0000

    7396021e4b fix(core): avoid duplicating comments in TestBed teardown migration (#43776)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.15+16.sha-2a67227.with-local-changes"
+                version: "~13.0.0-next.15+18.sha-7396021.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
