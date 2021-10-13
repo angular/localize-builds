a4448d0a7c99402b464d13f68b6e0bb069db447f
Andrew Scott <atscott01@gmail.com>
2021-10-13 17:45:16 +0000

    7ba9591a1d ci: remove rollup from renovate ignoreDeps (#43737)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.15+34.sha-ad83290.with-local-changes"
+                version: "~13.0.0-next.15+41.sha-7ba9591.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
