604b1fcc81c16def116dd5fb9b18cd192294c934
Andrew Scott <atscott01@gmail.com>
2021-10-13 19:06:22 +0000

    6113813300 docs: add missing split marker (#43830)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.15+42.sha-b5eccde.with-local-changes"
+                version: "~13.0.0-rc.0+1.sha-6113813.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
