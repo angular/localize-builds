0be3f012f6ff41c6ad7772ef2593f42e70f93642
Andrew Kushnir <akushnir@google.com>
2021-10-15 21:16:02 +0000

    ee2678ca42 build: update bazel integration test to `rules_nodejs` v4.4.1 (#43853)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-rc.0+14.sha-70c14ed.with-local-changes"
+                version: "~13.0.0-rc.0+16.sha-ee2678c.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
