cb03ce1e2e3571de2942a926e9586bacb864ffe7
Andrew Kushnir <akushnir@google.com>
2021-10-15 17:35:08 +0000

    426a3ecae7 fix(compiler-cli): updates `ngc` to pass the build when only warnings are emitted (#43673)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-rc.0+8.sha-91e73c4.with-local-changes"
+                version: "~13.0.0-rc.0+11.sha-426a3ec.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
