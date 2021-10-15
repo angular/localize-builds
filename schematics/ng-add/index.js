8f88bf0484bbd974477ffb6d85e22c8b45aaf27e
Andrew Kushnir <akushnir@google.com>
2021-10-15 18:52:50 +0000

    9a6bd264ab build(docs-infra): add a box around CLI command line reference syntax (#43850)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.14+63.sha-e78c7cc.with-local-changes"
+                version: "~13.0.0-next.14+65.sha-9a6bd26.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
