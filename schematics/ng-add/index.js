cc5a261e3a4447ac9ca03ad4cfcf70d6827c4928
Andrew Kushnir <akushnir@google.com>
2021-10-18 22:40:47 +0000

    b489ae5ecd ci: remove browserstack configuration (#43881)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-rc.0+25.sha-fc6519c.with-local-changes"
+                version: "~13.0.0-rc.0+27.sha-b489ae5.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
