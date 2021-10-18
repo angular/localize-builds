6e5f36d060e1f49dcd93a5f7430557f3ece906c1
Andrew Kushnir <akushnir@google.com>
2021-10-18 16:39:50 +0000

    67a7c48683 fix(docs-infra): move security anchors outside callout elements (#43865)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.14+64.sha-e79e3ca.with-local-changes"
+                version: "~13.0.0-next.14+66.sha-67a7c48.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
