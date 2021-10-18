b58e0cb47cb54066a5b939822cbebde72be92639
Andrew Kushnir <akushnir@google.com>
2021-10-18 20:50:48 +0000

    fc6519c25a Revert "fix(compiler): support i18n interpolated only attribute bindings (#43815)" (#43882)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-rc.0+20.sha-db57e39.with-local-changes"
+                version: "~13.0.0-rc.0+25.sha-fc6519c.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
