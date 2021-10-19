6f8a5db6418118ad8fdb2fc107e2cb63389c902b
Andrew Kushnir <akushnir@google.com>
2021-10-19 17:24:15 +0000

    fbd2e4f0cd ci: mark core/schematics/test:test and compiler-cli/test:perform_watch test as view engine only (#43862)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.14+78.sha-9f40d2a.with-local-changes"
+                version: "~13.0.0-next.14+90.sha-fbd2e4f.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
