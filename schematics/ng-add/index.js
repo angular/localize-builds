40c3ad79e7b73a63bc450dbd9bfba5da7cb63619
Andrew Kushnir <akushnir@google.com>
2021-10-19 18:02:34 +0000

    9d9346c57a docs: add docs about build cache (#43843)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-rc.0+39.sha-812a33d.with-local-changes"
+                version: "~13.0.0-rc.0+40.sha-9d9346c.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
