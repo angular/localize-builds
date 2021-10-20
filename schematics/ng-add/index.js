d4a2d06bd4d5c4dd179a9effe9584924baa84ec8
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-20 20:31:39 +0000

    b8f1f82fe2 docs: clarify meaning of ellipsis in bazel command (#43890)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.14+96.sha-4c9f35b.with-local-changes"
+                version: "~13.0.0-next.14+97.sha-b8f1f82.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
