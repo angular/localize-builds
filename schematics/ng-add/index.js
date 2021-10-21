8e4d9e2a910b643f003a16e1c720d86b989c562e
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 17:10:58 +0000

    45e4e60fd6 fix(router): reuse route strategy fix (#43791)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.14+102.sha-643cdf7.with-local-changes"
+                version: "~13.0.0-next.14+103.sha-45e4e60.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
