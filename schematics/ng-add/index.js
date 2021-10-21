2415e7a1bb29dca9de2732878b3fe7145c5cfa28
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 14:39:44 +0000

    c5d0b76e29 refactor(service-worker): avoid unnecessary writes to cache when version fails (#43680)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.14+98.sha-c78f1cd.with-local-changes"
+                version: "~13.0.0-next.14+99.sha-c5d0b76.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
