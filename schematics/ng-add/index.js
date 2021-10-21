b877caddc591303cccc25c52cda72901c538605c
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-21 19:12:04 +0000

    97fc9de2c4 refactor(core): made comment structure consistent in index.ts (#43684)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-rc.1+8.sha-72f6877.with-local-changes"
+                version: "~13.0.0-rc.1+11.sha-97fc9de.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
