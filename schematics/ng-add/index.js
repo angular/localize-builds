12233a87610bb12f63831ff4141e38f8be58a142
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 22:19:03 +0000

    0873ae2580 Revert "refactor(migrations): remove no longer accessible migrations" (#43943)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.14+115.sha-0b4350c.with-local-changes"
+                version: "~13.0.0-next.14+116.sha-0873ae2.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
