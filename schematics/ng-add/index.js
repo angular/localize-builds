048cc71155f6c688035b763438ba95560f017b41
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-25 18:17:09 +0000

    bf1a2fc2d5 test(platform-browser): complete component bootstrap before switching to the next test (#43933)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.14+113.sha-6e37c93.with-local-changes"
+                version: "~13.0.0-next.14+114.sha-bf1a2fc.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
