150955e892c3a2d50a2f6e282d099ae8aa434d82
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-27 16:37:06 +0000

    515949e7e6 docs: review tag added (#43465)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.11+14.sha-8fcfd87.with-local-changes"
+                version: "~12.2.11+20.sha-515949e.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
