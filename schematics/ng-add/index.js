0c42f2e03cb787c12bd27e0f91be899233f9bd22
Jessica Janiuk <jessicajaniuk@google.com>
2021-10-27 21:58:47 +0000

    8d89b2853c Revert "refactor(forms): update minlength/maxlength to inherit abstractValidator" (#43971)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-rc.1+34.sha-a2ce1da.with-local-changes"
+                version: "~13.0.0-rc.2+1.sha-8d89b28.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
