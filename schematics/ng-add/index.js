f10d70efc05acf405ac00eb8dc68774aefbfed7f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-11-03 22:24:20 +0000

    595a7e3dcd build: update angular (#44024)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0-next.14+200.sha-cbf96c1.with-local-changes"
+                version: "~13.1.0-next.0+1.sha-595a7e3.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
