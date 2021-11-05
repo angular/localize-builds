15ecb7d05ae8fb688d63d9ddbb93d9380b77f379
Andrew Scott <atscott01@gmail.com>
2021-11-05 16:39:01 +0000

    bac03a95d7 ci: do not capture nested bazel integration test workspace within Bazel (#44061)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0-next.0+22.sha-523e8fd.with-local-changes"
+                version: "~13.1.0-next.0+26.sha-bac03a9.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
