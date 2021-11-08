52713a7fe5ba6173bfcdf7b25bb61389e013e87e
Andrew Scott <atscott01@gmail.com>
2021-11-08 17:52:11 +0000

    e5a960b159 fix(compiler): generate correct code for safe method calls (#44088)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0+29.sha-58f453d.with-local-changes"
+                version: "~13.0.0+49.sha-e5a960b.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
