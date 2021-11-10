bca0d7b5506f25877b76e4a688c525c5a7a1d960
Andrew Scott <atscott01@gmail.com>
2021-11-10 18:45:07 +0000

    ee2031d9f4 fix(compiler): ensure that partially compiled queries can handle forward references (#44113)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0+68.sha-30a27ad.with-local-changes"
+                version: "~13.0.0+73.sha-ee2031d.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
