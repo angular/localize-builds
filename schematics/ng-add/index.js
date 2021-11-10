01933c05c0bf1e870d5aa3f6e7ea71ebb1a0e17c
Andrew Scott <atscott01@gmail.com>
2021-11-10 01:39:05 +0000

    30a27adf9a fix(platform-browser): use correct parent in animation removeChild callback (#44033)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.0+54.sha-5eac434.with-local-changes"
+                version: "~13.0.0+68.sha-30a27ad.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
