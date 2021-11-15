b2c82afa93797f5396f25161acf4624243ef8fa2
Andrew Kushnir <akushnir@google.com>
2021-11-15 19:36:25 +0000

    44890437ae build: update to yarn v1.22.17 (#44150)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0-next.0+81.sha-f817e30.with-local-changes"
+                version: "~13.1.0-next.1+1.sha-4489043.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
