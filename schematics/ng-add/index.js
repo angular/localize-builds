e6340c23b4be57e2b4a8a63078cb2f055d99e4c4
Andrew Kushnir <akushnir@google.com>
2021-11-15 19:39:45 +0000

    b7fd9ebf1d ci: remove `cla: yes` from required labels (#44179)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0-next.1+1.sha-4489043.with-local-changes"
+                version: "~13.1.0-next.1+3.sha-b7fd9eb.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
