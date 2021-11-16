e46625dc93afcea1243fce489b2d233506464289
Andrew Kushnir <akushnir@google.com>
2021-11-16 20:24:37 +0000

    5a24f96792 refactor(elements): remove non-existent schematic collection breaking with `ng add` (#44187)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0-next.1+6.sha-00ae55c.with-local-changes"
+                version: "~13.1.0-next.1+8.sha-5a24f96.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
