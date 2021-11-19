43af8f21b027198e8c4d447e2b3b87b56f6b31cd
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-19 19:02:06 +0000

    7e121e4b55 build: update dependency @angular-eslint/builder to v13 (#44222)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0-next.2+18.sha-894aaea.with-local-changes"
+                version: "~13.1.0-next.2+16.sha-7e121e4.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
