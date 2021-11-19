d99ee5b255ee04d87da31f2f9acb788e897d3637
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-19 22:45:38 +0000

    3289aae620 refactor(bazel): always add `strictTemplates` option to `tsconfig.json` (#43674)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.2+19.sha-6e351ed.with-local-changes"
+                version: "~13.0.2+21.sha-3289aae.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
