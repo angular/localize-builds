fa0e8c42f70f8c627a5a16b9a0fb2493d2231e09
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-23 17:29:50 +0000

    40eb2f57f4 build: update dependency source-map-support to v0.5.21 (#44225)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0-next.2+33.sha-a0177c6.with-local-changes"
+                version: "~13.1.0-next.2+34.sha-40eb2f5.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
