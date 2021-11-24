a57c92fcd5efa36395e59f4d27f086cb7ec3f6f7
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 20:26:36 +0000

    dcea04b6b7 test: remove Ivy/ViewEngine switch helpers and obsolete tests (#44120) (#44262)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.2+47.sha-5119be4.with-local-changes"
+                version: "~13.0.2+48.sha-dcea04b.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
