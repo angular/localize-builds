003b1b9b96fe7f51dc13e167aabd72e8bf90a9d0
Jessica Janiuk <jessicajaniuk@google.com>
2021-11-24 17:20:56 +0000

    2bf131a8a0 fix(docs-infra): move aio footer inside mat-sidenav-container (#44236)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0-next.2+40.sha-414d5fe.with-local-changes"
+                version: "~13.1.0-next.2+43.sha-2bf131a.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
