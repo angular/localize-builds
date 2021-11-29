ead8324206f1b57d90f70a4044c19b2f53e7822d
Dylan Hunn <dylhunn@gmail.com>
2021-11-29 22:38:51 +0000

    ef140a76d9 fix(docs-infra): fix the main content layout (#44282)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0-next.2+66.sha-708eb8c.with-local-changes"
+                version: "~13.1.0-next.2+65.sha-ef140a7.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
