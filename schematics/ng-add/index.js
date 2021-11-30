2c3128f3c8a5c692ba017a3a170d6c8bfcd7242a
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 19:06:57 +0000

    c92095c201 refactor(docs-infra): clean up entryComponents usages (#44257)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.2+70.sha-f702c92.with-local-changes"
+                version: "~13.0.2+72.sha-c92095c.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
