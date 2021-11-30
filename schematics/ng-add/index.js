3a999425489238d2d74da0caa42a79e3cd1d920a
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 17:13:22 +0000

    0fcf18d5f4 ci: increase jasmine default timeout for saucelabs legacy tests (#44281)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0-next.2+67.sha-3aafa76.with-local-changes"
+                version: "~13.1.0-next.2+74.sha-0fcf18d.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
