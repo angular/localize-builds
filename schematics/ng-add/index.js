eab88e98f9722962030f73ffe2f2760a6b7e706b
Dylan Hunn <dylhunn@gmail.com>
2021-11-30 17:39:50 +0000

    960068899c build: fix lint errors in build-saucelabs legacy script (#44311)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0-next.2+75.sha-d56e3f4.with-local-changes"
+                version: "~13.1.0-next.2+76.sha-9600688.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
