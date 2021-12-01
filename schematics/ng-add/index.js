7d66c18bd543cf6f64a1fd3a99b272462d9f2322
Dylan Hunn <dylhunn@gmail.com>
2021-12-01 20:13:11 +0000

    0c9d33cb51 refactor(docs-infra): replace use of deprecated `Compiler` with `createNgModuleRef()` (#44293)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.2+78.sha-98430d6.with-local-changes"
+                version: "~13.0.2+81.sha-0c9d33c.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
