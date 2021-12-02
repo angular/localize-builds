44b4614c7156a88a6a8a274c5818e4aa9f0420a1
Andrew Kushnir <akushnir@google.com>
2021-12-02 17:10:48 +0000

    5624dc6574 ci: update pullapprove groups (#44340)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.3+1.sha-0789401.with-local-changes"
+                version: "~13.0.3+3.sha-5624dc6.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
