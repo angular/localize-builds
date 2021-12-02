052b1dcef10c48ac0d3149d55ba52bdc96113b58
Andrew Kushnir <akushnir@google.com>
2021-12-02 20:11:32 +0000

    a8f96e8aaa build(docs-infra): update `.browserslistrc` to align with new CLI apps (#44352)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.3+4.sha-7fb4309.with-local-changes"
+                version: "~13.0.3+5.sha-a8f96e8.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
