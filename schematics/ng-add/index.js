455a60e0b01892005e016ebb18c66372b58dc81c
Andrew Kushnir <akushnir@google.com>
2021-12-02 17:43:38 +0000

    13362972bb perf(common): code size reduction of `ngFor` directive (#44315)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0-next.3+3.sha-99d85cb.with-local-changes"
+                version: "~13.1.0-next.3+4.sha-1336297.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
