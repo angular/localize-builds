02efa4ff82dba5242232b90fc81b012ace424355
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-08 18:41:05 +0000

    86843f8916 refactor(animations): remove unnecessary code (#44378)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.0.3+15.sha-763212d.with-local-changes"
+                version: "~13.0.3+17.sha-86843f8.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
