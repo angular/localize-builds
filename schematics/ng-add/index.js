abd5197c750ae2a571121c7d5980fbd0115b9e2a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-08 18:39:15 +0000

    80ab604fe0 docs(localize): fix typo in i18n-overview.md (#44389)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0-next.3+10.sha-4126591.with-local-changes"
+                version: "~13.1.0-next.3+14.sha-80ab604.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
