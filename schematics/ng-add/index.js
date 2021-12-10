a8f5af83f3cf2830d66e1d320db29a0eda94049b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-10 21:47:06 +0000

    944b6705f8 build: remove unused starlark file that is no longer used (#44430)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0-next.3+28.sha-a7c117a.with-local-changes"
+                version: "~13.2.0-next.0+2.sha-944b670.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
