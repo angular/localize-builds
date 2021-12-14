6a99add6c34497ed4319097847526ca49b3dc3da
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:29:15 +0000

    f56ac0fe83 refactor(animations): improve _finalKeyframe non-null assertion (#44458)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.2.0-next.0+17.sha-5da31d6.with-local-changes"
+                version: "~13.2.0-next.0+18.sha-f56ac0f.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
