b1b30c05277363b0ac64a8177951971527cac169
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:24:33 +0000

    e4839939b0 test(ngcc): don't use the workspace typescript version to generate integration test fixtures (#44448)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.2.0-next.0+12.sha-12f7c85.with-local-changes"
+                version: "~13.2.0-next.0+14.sha-e483993.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
