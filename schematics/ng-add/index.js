2b5b70bd56e32203ead1297e389c4d755be1a66a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:26:26 +0000

    65717999c8 refactor(forms): Make the minimum changes to the forms unit tests in order to support the typed forms PR (#43834). (#44451)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.2.0-next.0+13.sha-ab2248e.with-local-changes"
+                version: "~13.2.0-next.0+16.sha-6571799.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
