bf9d8f2b80eb91ab084c074cbd54a2de9beccfca
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-14 21:24:45 +0000

    ab2248e773 docs(docs-infra): change variable names in route guards examples (#44444)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.2.0-next.0+14.sha-e483993.with-local-changes"
+                version: "~13.2.0-next.0+13.sha-ab2248e.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
