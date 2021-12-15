d07ca52d841d1b5adc50cf824ce4349817666f37
Alex Rickabaugh <alx+alxhub@alxandria.net>
2021-12-15 17:43:40 +0000

    bce108ab49 fix(forms): `_reduceValue` arrow function now has correct types. (#44483)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.0+24.sha-240edcb.with-local-changes"
+                version: "~13.1.0+25.sha-bce108a.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
