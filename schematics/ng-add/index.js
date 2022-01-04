1bdc715d5d36dfdb1ebaddde582cb79211a74f47
Dylan Hunn <dylhunn@gmail.com>
2022-01-04 18:55:23 +0000

    e6beeab7dd ci: remove atscott for unavailable users (#44539)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.1+1.sha-beb1828.with-local-changes"
+                version: "~13.1.1+2.sha-e6beeab.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
