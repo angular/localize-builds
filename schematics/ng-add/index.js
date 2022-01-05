386c2fbdecc57f87897d9ab6125b36c1d9c32be4
Dylan Hunn <dylhunn@gmail.com>
2022-01-05 00:09:38 +0000

    d4015f762f test: remove newlines to make a test more stable (#44616)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.2.0-next.1+31.sha-f2b858d.with-local-changes"
+                version: "~13.2.0-next.1+30.sha-d4015f7.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
