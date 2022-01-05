b25a603aca3ea72621919879af2f1bf78b1b58b3
Andrew Scott <atscott01@gmail.com>
2022-01-05 23:37:06 +0000

    2d14724965 docs(common): url link format updated (#44638)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0-next.1+59.sha-12e0424.with-local-changes`
+                version: `~13.2.0-next.1+61.sha-2d14724.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
