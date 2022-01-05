3fedf0b3914a45425df97b34248d7a1019122026
Andrew Scott <atscott01@gmail.com>
2022-01-05 18:03:20 +0000

    7278a9c584 docs(animations): fix filter-animations docregion (#44544)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.2.0-next.1+50.sha-d5ced77.with-local-changes"
+                version: "~13.2.0-next.1+51.sha-7278a9c.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
