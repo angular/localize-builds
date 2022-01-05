17986912336a7e06a9d269dc416456516c619f28
Andrew Scott <atscott01@gmail.com>
2022-01-05 18:03:05 +0000

    426b6f9472 docs(router): fix code block for IsActiveMatchOptions (#44635)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~13.1.1+46.sha-0ac4c0b.with-local-changes"
+                version: "~13.1.1+48.sha-426b6f9.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
