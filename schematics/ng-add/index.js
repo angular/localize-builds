732ea2f73d918113a1bf1ba214ee78ba5e0137d5
Andrew Scott <atscott01@gmail.com>
2022-01-06 18:56:43 +0000

    5017bf92fe docs(animations): fix grammar (#44607)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.1.1+58.sha-9b9042c.with-local-changes`
+                version: `~13.1.1+62.sha-5017bf9.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
