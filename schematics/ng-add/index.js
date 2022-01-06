2ab74cf7b9e3d3ea85979215af681c375fd24c5a
Andrew Scott <atscott01@gmail.com>
2022-01-06 18:52:33 +0000

    7f51f1dcae docs(docs-infra): improve the content of attribute directives (#44628)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0-next.1+62.sha-b69a514.with-local-changes`
+                version: `~13.2.0-next.1+65.sha-7f51f1d.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
