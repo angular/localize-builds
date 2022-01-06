6b866684e3458c1dae32e4466855ab3d0a999cda
Andrew Scott <atscott01@gmail.com>
2022-01-06 00:40:24 +0000

    9b9042c8d1 docs(animations): fix links to the AnimationBuilder.build method (#44594)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.1.1+57.sha-98592b0.with-local-changes`
+                version: `~13.1.1+58.sha-9b9042c.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
