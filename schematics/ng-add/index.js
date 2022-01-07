6da62fc706b94913880ec5d2df450e453402d61e
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:27:47 +0000

    277d2af007 docs(common): use <em> instead of <i> in examples (#44648)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0-next.1+82.sha-b184f0a.with-local-changes`
+                version: `~13.2.0-next.1+81.sha-277d2af.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
