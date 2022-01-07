1dd6b8c8c5a7ba1db724e214ed283b063c560f8b
Andrew Scott <atscott01@gmail.com>
2022-01-07 18:29:34 +0000

    63c8e56a3a fix(compiler-cli): incorrectly interpreting $any calls with a property read (#44657)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.1.1+73.sha-d252cff.with-local-changes`
+                version: `~13.1.1+78.sha-63c8e56.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
