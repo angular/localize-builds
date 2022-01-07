88fc6d12774a47768f1213c70e0841cdda33dd02
Andrew Scott <atscott01@gmail.com>
2022-01-07 22:32:51 +0000

    abc217b28e fix(animations): retain triggers values for moved tracked list items (#44578)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.1.1+79.sha-919f8ae.with-local-changes`
+                version: `~13.1.1+80.sha-abc217b.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
