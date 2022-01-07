94a68063eee0373f83a581a12b326288f64c8b42
Andrew Scott <atscott01@gmail.com>
2022-01-07 19:33:21 +0000

    919f8ae4e8 build: update dependency google-closure-compiler to v20220104 (#44655)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.1.1+78.sha-63c8e56.with-local-changes`
+                version: `~13.1.1+79.sha-919f8ae.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
