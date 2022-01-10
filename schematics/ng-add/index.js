fb1e654872d55c4457e521a58b2c7f7efaec58b8
Andrew Scott <atscott01@gmail.com>
2022-01-10 20:10:21 +0000

    c82e5411f3 docs: improve the component styling guide (#44234)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0-next.1+87.sha-5a4ddfd.with-local-changes`
+                version: `~13.2.0-next.1+89.sha-c82e541.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
