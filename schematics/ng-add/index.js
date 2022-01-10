14583001bb62655b960928353d5e1900c7532af1
Andrew Scott <atscott01@gmail.com>
2022-01-10 21:38:13 +0000

    1a9121826e fix(compiler-cli): enable narrowing of using type guard methods (#44447)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0-next.1+91.sha-ebf8446.with-local-changes`
+                version: `~13.2.0-next.1+92.sha-1a91218.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
