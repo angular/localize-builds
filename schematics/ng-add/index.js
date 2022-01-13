100e62a1076a454bd63c7e74c9cae5e08d9292e7
Dylan Hunn <dylhunn@gmail.com>
2022-01-13 20:20:30 +0000

    af0a152a2c fix(animations): apply setStyles to only rootTimelines (#44515)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.1.2+10.sha-44ff4fb.with-local-changes`
+                version: `~13.1.2+12.sha-af0a152.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
