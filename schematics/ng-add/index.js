8c79e243acd403fd2772b1a362887360e665844b
Andrew Scott <atscott01@gmail.com>
2022-01-13 18:44:10 +0000

    3c0446cd2d build: update shelljs dependencies to "^0.8.5" (#44697)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0-next.2+5.sha-432e518.with-local-changes`
+                version: `~13.2.0-next.2+7.sha-3c0446c.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
