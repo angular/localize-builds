37e11836bc1b8a8f3a6e0c05eacd76ee5d3c7301
Andrew Scott <atscott01@gmail.com>
2022-01-13 18:43:54 +0000

    432e518931 refactor(elements): remove the createCustomEvent function (#44703)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0-next.2+6.sha-2189588.with-local-changes`
+                version: `~13.2.0-next.2+5.sha-432e518.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
