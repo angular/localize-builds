6241c2a748f08fd9f75ff22fd12a484af07dcbd1
Dylan Hunn <dylhunn@gmail.com>
2022-01-18 17:41:13 +0000

    617610e3b0 docs: add Ankit to GDE resources (#44659)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.1.2+13.sha-f9ca4d8.with-local-changes`
+                version: `~13.1.2+15.sha-617610e.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
