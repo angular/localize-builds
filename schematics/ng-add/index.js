a5c48388ccb033d1d461179bad66782517b0fbc2
Andrew Kushnir <akushnir@google.com>
2022-01-20 22:20:49 +0000

    100091ebf0 fix(compiler-cli): remove leftover `_extendedTemplateDiagnostics` requirements (#44777)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0-rc.0+2.sha-b7ff6f0.with-local-changes`
+                version: `~13.2.0-rc.0+3.sha-100091e.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
