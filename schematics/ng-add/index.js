25ccacc38b8108ae525e48538212eec42cb9921a
Andrew Kushnir <akushnir@google.com>
2022-01-20 19:35:51 +0000

    2b6461ebad refactor(compiler): pass rootDir to tsickle (#44768)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.1.3+2.sha-e534f8e.with-local-changes`
+                version: `~13.1.3+3.sha-2b6461e.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
