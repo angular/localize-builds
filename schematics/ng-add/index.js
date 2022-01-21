c5902e5d91f968cfde68250f71817c958191924d
Andrew Kushnir <akushnir@google.com>
2022-01-21 19:39:03 +0000

    ef5486fab5 build: update all non-major dependencies (#44713)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.1.3+4.sha-a9f5c87.with-local-changes`
+                version: `~13.1.3+7.sha-ef5486f.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
