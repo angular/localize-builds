48f0a450fb9c60309e4228e71e940567c57c81b5
Andrew Kushnir <akushnir@google.com>
2022-01-24 20:39:53 +0000

    a534a78e67 Revert "feat(core): allow for injector to be specified when creating an embedded view (#44666)" (#44807)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0-next.2+56.sha-215db7f.with-local-changes`
+                version: `~13.2.0-next.2+58.sha-a534a78.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
