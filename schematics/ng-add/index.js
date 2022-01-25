75771c24f9a7a9679c4cd7593bfa83a1b2f97f88
Andrew Kushnir <akushnir@google.com>
2022-01-25 22:31:14 +0000

    fe3e4d6865 fix(compiler-cli): Handle `ng-template` with structural directive in indexer (#44788)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0-rc.1+24.sha-6cb7c3e.with-local-changes`
+                version: `~13.2.0-rc.1+25.sha-fe3e4d6.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
