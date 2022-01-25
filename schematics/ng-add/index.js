8ed302e2599a7e0621ac6ae2a064dffc2937f504
Andrew Kushnir <akushnir@google.com>
2022-01-25 17:41:49 +0000

    6cb7c3ebe3 build(docs-infra): update `dgeni-packages` to v0.29.3 (#44810)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0-rc.1+22.sha-7e93513.with-local-changes`
+                version: `~13.2.0-rc.1+24.sha-6cb7c3e.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
