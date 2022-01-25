2daf12b2dc5c8351a246061dbbe1c41545da7e36
Andrew Kushnir <akushnir@google.com>
2022-01-25 01:26:46 +0000

    dd7364fc16 ci: update payload size limit for the Forms test app (#44813)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.1.3+13.sha-a52685b.with-local-changes`
+                version: `~13.1.3+27.sha-dd7364f.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
