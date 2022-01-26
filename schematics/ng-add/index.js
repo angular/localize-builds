5ac5d144ee11b00d5980910dcab4e041c99f5dad
Andrew Kushnir <akushnir@google.com>
2022-01-26 01:19:49 +0000

    894fa759f1 refactor(forms): Temporarily disable the typed forms migration. (#44828)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0-next.2+77.sha-1bce51c.with-local-changes`
+                version: `~13.2.0-next.2+78.sha-894fa75.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
