a01dfc37e2135b2031238775ff04bb2341ee1245
Andrew Kushnir <akushnir@google.com>
2022-01-26 20:52:03 +0000

    e46ac81f54 docs: cleanup CHANGELOG.md file (#44841)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0-next.2+81.sha-04eec90.with-local-changes`
+                version: `~14.0.0-next.0+1.sha-e46ac81.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
