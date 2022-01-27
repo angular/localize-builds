48a14b092c223a8d9a9d7116a22085675a0cdad2
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-27 22:49:31 +0000

    910de8bc33 feat(router): Add `Route.title` with a configurable `TitleStrategy` (#43307)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.0+1027.sha-7b7d644.with-local-changes`
+                version: `~14.0.0-next.0+1029.sha-910de8b.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
