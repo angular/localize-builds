7d7321bc793d646626d6e24b431dc04cb99b6bc8
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 17:16:18 +0000

    f490c2de4e feat(forms): support negative indices in FormArray methods. (#44848)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.0+1033.sha-156fb37.with-local-changes`
+                version: `~14.0.0-next.0+1034.sha-f490c2d.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
