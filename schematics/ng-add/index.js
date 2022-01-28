5f39b0006053ae8245fc02b9250875dcc4739481
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 16:25:10 +0000

    c9abf34d88 docs: fix GitHub broken link in update guide (#44875)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0+10.sha-f6c2934.with-local-changes`
+                version: `~13.2.0+13.sha-c9abf34.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
