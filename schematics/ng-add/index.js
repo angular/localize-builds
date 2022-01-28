f22ca728b082c77bed9a40d6be5ecc1d38c0de73
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-28 16:13:06 +0000

    07f827eec3 docs(core): edit and add docs for ng-container (#44762)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.0+1029.sha-910de8b.with-local-changes`
+                version: `~14.0.0-next.0+1030.sha-07f827e.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
