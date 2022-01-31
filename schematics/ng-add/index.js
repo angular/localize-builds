f0937aafbbc975158fa3334bb59dbe140f96b43b
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:54:12 +0000

    6642e3c8fd perf(animations): remove no longer needed CssKeyframes classes (#44903)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.0+1062.sha-5e099b5.with-local-changes`
+                version: `~14.0.0-next.0+1063.sha-6642e3c.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
