b244f335abf12a902b4c837ca04d55fdd9aa95b4
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:23:58 +0000

    55b180876d docs: update Nx headline (#44910) PR Close #44910

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.0+1051.sha-3d07d1f.with-local-changes`
+                version: `~14.0.0-next.0+1053.sha-55b1808.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
