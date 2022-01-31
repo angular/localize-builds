e099a9217a5b056493efa683a17f5ccb64ba9a23
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:49:06 +0000

    8fb2e3b71f docs: Update deployment docs for Vercel (#44904) PR Close #44904

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0+28.sha-07185f4.with-local-changes`
+                version: `~13.2.0+34.sha-8fb2e3b.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
