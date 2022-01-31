906db143f0a341be67965f781ce1ed21366fd5e5
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 20:23:55 +0000

    3d07d1f772 fix(docs-infra): make it possible to see whole notification text on smaller screens (#44900)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.0+1049.sha-390a653.with-local-changes`
+                version: `~14.0.0-next.0+1051.sha-3d07d1f.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
