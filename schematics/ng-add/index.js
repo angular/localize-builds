2a527fdbe1c40fc63c6a2356a300840dc85ea7f6
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:47:37 +0000

    7155e9e5e2 build: fix `update_all_goldens.js` to use ESM. (#44916)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.0+1060.sha-4c833955.with-local-changes`
+                version: `~14.0.0-next.0+1061.sha-7155e9e.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
