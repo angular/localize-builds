7fd2179629c47b9b7b9cb9bef7970a36613ae50e
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:42:56 +0000

    53a4a03144 build: bump version for `@angular/benchpress` to v0.3.0 (#44893)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.0+1057.sha-366e424.with-local-changes`
+                version: `~14.0.0-next.0+1059.sha-53a4a03.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
