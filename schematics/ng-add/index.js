01ed28be2ad9b5d1e1de524bfe741dec522080e7
Jessica Janiuk <jessicajaniuk@google.com>
2022-01-31 21:55:43 +0000

    9eff8908af refactor(core): strict templates type-checking compatibility for perf tests (#44905)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.0+1065.sha-889de82.with-local-changes`
+                version: `~14.0.0-next.0+1066.sha-9eff890.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
