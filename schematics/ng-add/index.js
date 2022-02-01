c4bde8201d50dcbcdefbad5f01a7258953c23c20
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 19:34:49 +0000

    1cc1588c29 docs: fix incorrect title and tooltip in navigation (#44710)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0+41.sha-2916ac9.with-local-changes`
+                version: `~13.2.0+48.sha-1cc1588.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
