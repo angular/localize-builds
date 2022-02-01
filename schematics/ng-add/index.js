fd741fd1c8a683c77677f52e0e4e6977f94f2d86
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-01 18:38:13 +0000

    6829fc4a4c docs: add colon before introducing the list of items (#44751)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.0+1073.sha-fdfcef5.with-local-changes`
+                version: `~14.0.0-next.0+1074.sha-6829fc4.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
