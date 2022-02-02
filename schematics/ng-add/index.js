e796b32a9bf398a590ebf781b3d85dc78960426c
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 20:22:47 +0000

    b9aab0c87b fix(upgrade): Do not trigger duplicate navigation events from Angular Router (#43441)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.0+58.sha-7924a2e.with-local-changes`
+                version: `~13.2.1+3.sha-b9aab0c.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
