d8edfc7ac213de5356b7c5774ec06bc5c655a00a
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-02 20:20:40 +0000

    39b90b46b6 docs: remove banner after survey expiration (#44949)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.0+1093.sha-0c36f82.with-local-changes`
+                version: `~14.0.0-next.1+1.sha-39b90b4.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
