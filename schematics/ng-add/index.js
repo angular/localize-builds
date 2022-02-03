8485a95f4c47531ace7950f14fdf663b7178dab3
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 20:56:56 +0000

    791bd31424 feat(router): set stricter type for Route.title (#44939)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.1+12.sha-0072eb4.with-local-changes`
+                version: `~14.0.0-next.1+16.sha-791bd31.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
