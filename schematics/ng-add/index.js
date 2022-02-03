ea8dc5d8216857cf532d418683e452b49feb1bff
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 20:56:59 +0000

    48d430d7c2 docs: remove item from changelog that does not work (#44961)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.1+16.sha-791bd31.with-local-changes`
+                version: `~14.0.0-next.1+14.sha-48d430d.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
