32370084819279134292cfe3847119ae0a8e73cb
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 17:10:17 +0000

    1aae414b36 docs: remove duplicate closing tags in View Encapsulation file (#44958)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.1+8.sha-077593f.with-local-changes`
+                version: `~14.0.0-next.1+9.sha-1aae414.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
