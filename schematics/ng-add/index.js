9ff6175c3b718ec007ded0f0020d105a71300447
Dylan Hunn <dylhunn@gmail.com>
2022-02-03 23:38:07 +0000

    59a6ec4396 build: exclude devtools scope from generated changelogs (#44964)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.1+15.sha-1b21629.with-local-changes`
+                version: `~13.2.1+16.sha-59a6ec4.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
