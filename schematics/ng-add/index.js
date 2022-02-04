daf8832ace6397846ab5d10ce79ab0da259f058e
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 18:58:47 +0000

    459cbed33b refactor(dev-infra): convert nullish coalescing operator to plain javascript (#44968)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.1+30.sha-8dd3f82.with-local-changes`
+                version: `~14.0.0-next.1+31.sha-459cbed.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
