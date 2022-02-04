2ee279e27a9ab6ec7f18194992d5c0f69469f76f
Dylan Hunn <dylhunn@gmail.com>
2022-02-04 20:03:16 +0000

    28393031b1 perf(http): remove IE special status handling (#44354)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.1+34.sha-9917152.with-local-changes`
+                version: `~14.0.0-next.1+35.sha-2839303.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
