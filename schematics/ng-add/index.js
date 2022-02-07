2c2e0e7be7c44b1490011331d057f8165958930c
Dylan Hunn <dylhunn@gmail.com>
2022-02-07 18:02:28 +0000

    604a67f0a1 ci: group all scorecard action dependencies (#44998)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.1+21.sha-cd97d87.with-local-changes`
+                version: `~13.2.1+23.sha-604a67f.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
