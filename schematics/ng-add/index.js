fbb5ff77324c002bb3633f727e294bf0130b0fd1
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 05:34:14 +0000

    7bc4dbd7ff docs: fix command to clear cache on Windows (#44988) PR Close #44988

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.1+28.sha-d40ac20.with-local-changes`
+                version: `~13.2.1+29.sha-7bc4dbd.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
