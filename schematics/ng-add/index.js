07f78e36742d81749d10a6a0665ec258959228f6
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 18:14:10 +0000

    1b91e1049e perf(compiler): chain element start/end instructions (#44994)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.2+5.sha-635683a.with-local-changes`
+                version: `~14.0.0-next.2+7.sha-1b91e10.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
