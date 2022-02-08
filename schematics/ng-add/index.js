11d0e0a5052cb9ae82044daf59e4a877ce98a8b0
Dylan Hunn <dylhunn@gmail.com>
2022-02-08 01:56:49 +0000

    28e943634a refactor: one more CHANGELOG fixup. (#45010)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.2+1.sha-4d63b80.with-local-changes`
+                version: `~14.0.0-next.2+2.sha-28e9436.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
