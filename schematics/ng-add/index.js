11877939dc62c6dfd0326f7d19a118fb6f1fbdfc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-09 18:40:02 +0000

    0b8208690c docs: improve try-it-now card focus area (#45002)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.2+7.sha-a01ae48.with-local-changes`
+                version: `~13.2.2+11.sha-0b82086.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
