105bfc8d193c18b328d66ba5f1d5fb193de67160
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-09 18:39:06 +0000

    7d9413e1d1 docs: remove aikidave@ from contributors.json as part of offboarding (#45006)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.2+14.sha-d5565cc.with-local-changes`
+                version: `~14.0.0-next.2+17.sha-7d9413e.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
