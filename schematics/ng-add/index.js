a9a349f2b373f0b77cedd4885407c7ce82c556ea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-11 22:51:11 +0000

    a782be62e9 refactor(docs-infra): replace deprecated toPromise usages (#45045)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.2+13.sha-d8a1871.with-local-changes`
+                version: `~13.2.2+15.sha-a782be6.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
