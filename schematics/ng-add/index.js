2cfd6ac46df7567a8f75b1eb7aa975f3155ad630
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-15 20:48:18 +0000

    aeeb1b55d3 docs(localize): fix angular.json syntax error about i18n (#45048)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.2+22.sha-0050b01.with-local-changes`
+                version: `~13.2.2+24.sha-aeeb1b5.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
