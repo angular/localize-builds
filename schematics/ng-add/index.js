c3dbafa72da1695dd4a3b35ef7c4d1a449fc5f5b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:02:18 +0000

    1eae65ead7 build: update .pullapprove.yml (#45061)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.2+41.sha-81bb0eb.with-local-changes`
+                version: `~14.0.0-next.2+43.sha-1eae65e.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
