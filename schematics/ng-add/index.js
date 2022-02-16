c8a89a58c3ce24f4d1a8d7823eeccc0697b8473a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-16 22:08:01 +0000

    ee381dadc2 test(zone.js): add integration test for zone.js Mocha patch (#45047)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.2+45.sha-6769f94.with-local-changes`
+                version: `~14.0.0-next.2+47.sha-ee381da.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
