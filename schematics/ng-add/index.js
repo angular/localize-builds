d74a47824ac6017fa978d0cd1a174d88b379df1e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-18 21:52:33 +0000

    702e196037 fix(docs-infra): make hamburger menu non focusable when not visible (#45067)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.3+6.sha-be50401.with-local-changes`
+                version: `~14.0.0-next.3+7.sha-702e196.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
