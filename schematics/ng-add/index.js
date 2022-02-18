6f5d0b246335e1346d4d15f2f27c818afab2660c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-02-18 21:49:27 +0000

    66d45b0079 docs: fix test examples to use withContext (#45100)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.3+1.sha-071c8af.with-local-changes`
+                version: `~14.0.0-next.3+2.sha-66d45b0.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
