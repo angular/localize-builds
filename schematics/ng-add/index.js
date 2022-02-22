4d5fecbb8c270395717ebb96d9d52968f57192f8
Andrew Scott <atscott01@gmail.com>
2022-02-22 18:37:33 +0000

    f80c59e4ab ci: update SHA for components-repo-unit-tests job to fix flakiness (#45166)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.3+12.sha-5957ff4.with-local-changes`
+                version: `~14.0.0-next.3+13.sha-f80c59e.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
