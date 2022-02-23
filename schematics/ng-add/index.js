90f7bf83949be91dc0e91b601a2b8a39fc49e922
Andrew Scott <atscott01@gmail.com>
2022-02-23 17:16:04 +0000

    6550c59df5 docs: fix test description to match code example (#45101)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.3+12.sha-0322762.with-local-changes`
+                version: `~13.2.3+13.sha-6550c59.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
