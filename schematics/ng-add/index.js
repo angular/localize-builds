7a48934ee8e75463961cc0566b245fbbc77df6b5
Andrew Scott <atscott01@gmail.com>
2022-02-23 21:19:20 +0000

    2e7e46f1b6 ci: use renovate Github Action tag version pinning (#45178)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.3+23.sha-0c26c93.with-local-changes`
+                version: `~14.0.0-next.4+1.sha-2e7e46f.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
