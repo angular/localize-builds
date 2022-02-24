1badbe0bf71e26276cafce4c3db0d9ed8038750d
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 21:32:15 +0000

    451bc27b96 test(animations): Fix flaky animations e2e test (#45196)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.4+8.sha-7fd416d.with-local-changes`
+                version: `~14.0.0-next.4+9.sha-451bc27.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
