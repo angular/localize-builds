9509aadb028f945048d08e5d30c067e4f93a8fa4
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-24 23:14:32 +0000

    c6e64aa8ad docs: fix style guide example app included as routing (#45148)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.4+9.sha-7379d75.with-local-changes`
+                version: `~13.2.4+10.sha-c6e64aa.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
