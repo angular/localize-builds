228b19a3431cacd3f4c32b9ccb2e2d9bc335321c
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 19:30:29 +0000

    e56472d3c2 ci: fix stamping for builds performed in CI (#45147)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.4+22.sha-e01820c.with-local-changes`
+                version: `~14.0.0-next.4+23.sha-e56472d.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
