53c233500e952da9fa5512e4f34f01d1455ed9b1
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-25 22:29:23 +0000

    76c6dca93f ci: instruct renovate to not update/override merge ready PRs (#45204)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.4+15.sha-8554659.with-local-changes`
+                version: `~13.2.4+16.sha-76c6dca.with-local-changes`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
