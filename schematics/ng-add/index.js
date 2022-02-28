b2dcd9d1c077060bfa7de5907c7bfef83feb4891
Jessica Janiuk <jessicajaniuk@google.com>
2022-02-28 19:48:01 +0000

    9366a3c5f3 docs: improve structure of http example (#44986)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.4+29.sha-4d494d2`
+                version: `~14.0.0-next.4+30.sha-9366a3c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
