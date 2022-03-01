f6427c9aa6ba7b6fc98b17053ced283c051e2ae8
Jessica Janiuk <jessicajaniuk@google.com>
2022-03-01 20:06:06 +0000

    c8e4d62882 refactor(forms): Split up `model.ts`. (#45217)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.4+40.sha-c5df474`
+                version: `~14.0.0-next.4+41.sha-c8e4d62`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
