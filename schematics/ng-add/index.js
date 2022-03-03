24de5f73d9fe65846d5ebf0f08a2496cf9aa515f
Andrew Kushnir <akushnir@google.com>
2022-03-03 17:34:38 +0000

    e16dab82a1 refactor(router): Remove special logic for hybrid apps (#45240)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.4+24.sha-8bf3b53.with-local-changes`
+                version: `~13.2.5+6.sha-e16dab8`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
