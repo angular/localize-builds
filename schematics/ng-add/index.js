f01934071a3114972b42fed43345f51606a8fe80
Andrew Kushnir <akushnir@google.com>
2022-03-03 20:46:41 +0000

    4e1d825d83 docs: change link for devtools to angular repo (#45207) PR Close #45207

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.5+6.sha-e16dab8`
+                version: `~13.2.5+9.sha-4e1d825`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
