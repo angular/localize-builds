5ee330397d5e5921ecfe96cc74fe0b25f3e63323
Andrew Kushnir <akushnir@google.com>
2022-03-04 23:42:20 +0000

    de67c9fd01 refactor(router): Use helper function to throw NoMatch (#45244)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.5+9.sha-4e1d825`
+                version: `~13.2.5+11.sha-de67c9f`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
