bee893b586626427e1d7f71999e70a50842c3960
Andrew Kushnir <akushnir@google.com>
2022-03-07 23:59:30 +0000

    ad9d98146e docs: Change tag name to TD from TR in context of colspan usage (#45257) PR Close #45257

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.5+16.sha-8390e12`
+                version: `~13.2.5+17.sha-ad9d981`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
