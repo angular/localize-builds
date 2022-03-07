a7937702823ee6e4367adc21247ab59105bb9992
Andrew Kushnir <akushnir@google.com>
2022-03-07 23:59:22 +0000

    8390e12bdd docs: update `flush` description to include microtasks (#45237)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.5+15.sha-43bbaf8`
+                version: `~13.2.5+16.sha-8390e12`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
