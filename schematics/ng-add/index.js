70d54722f8bd4c864427609d8b1bbfccf1e2c376
Andrew Kushnir <akushnir@google.com>
2022-03-07 19:53:18 +0000

    d87f66c02f test(compiler): fix deprecation warning (#45285)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.5+15.sha-886cfe8`
+                version: `~14.0.0-next.5+16.sha-d87f66c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
