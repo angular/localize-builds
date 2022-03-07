560db4af43ffeeb8f9f4c0c3f16f8e257e285bb2
Andrew Kushnir <akushnir@google.com>
2022-03-07 20:42:16 +0000

    d336ba96d9 fix(forms): Update the typed forms migration. (#45281)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.5+16.sha-d87f66c`
+                version: `~14.0.0-next.5+17.sha-d336ba9`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
