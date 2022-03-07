66b9c6ac04437b72badb2c14fb84b4409e860b76
Andrew Kushnir <akushnir@google.com>
2022-03-07 22:46:34 +0000

    68eda48670 build: update dependency magic-string to v0.26.1 (#45247)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.5+17.sha-d336ba9`
+                version: `~14.0.0-next.5+18.sha-68eda48`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
