8e841cddea22d15a1436ffa1683ae36646766259
Andrew Kushnir <akushnir@google.com>
2022-03-07 19:50:38 +0000

    aa7b857be8 fix(forms): Property renaming safe code (#45271)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.5+12.sha-8c171da`
+                version: `~14.0.0-next.5+13.sha-aa7b857`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
