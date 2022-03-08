933b2389518e91663ca3f7cf61bb804f7dd56f03
Andrew Kushnir <akushnir@google.com>
2022-03-08 18:42:59 +0000

    4478dff471 docs(service-worker): improve description of `NoNewVersionDetectedEvent` (#45266)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.5+22.sha-9fa6f5a`
+                version: `~14.0.0-next.5+25.sha-4478dff`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
