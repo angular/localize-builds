6e3dcf02ee8e9808e8eb4af80bd3a82785572c81
Andrew Kushnir <akushnir@google.com>
2022-03-08 18:40:44 +0000

    9fa6f5a552 fix(core): incorrectly inserting elements inside <template> element (#43429)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.5+20.sha-0b31c8e`
+                version: `~14.0.0-next.5+22.sha-9fa6f5a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
