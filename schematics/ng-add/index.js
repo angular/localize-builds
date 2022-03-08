f15a822f0024463260884747395187f589987335
Andrew Kushnir <akushnir@google.com>
2022-03-08 21:30:22 +0000

    e78a928d6b refactor(migrations): update `isReferenceToImport` to not use `valueDeclaration` (#45292)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.5+32.sha-ba0efd1`
+                version: `~14.0.0-next.5+33.sha-e78a928`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
