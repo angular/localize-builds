f0452fc56bb7d2f75038778c8a556d60c39af5a1
Andrew Kushnir <akushnir@google.com>
2022-03-08 21:33:31 +0000

    dc6094acdc ci: update payload size goldens to reflect zone.js update (#45289)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.5+33.sha-e78a928`
+                version: `~14.0.0-next.5+35.sha-dc6094a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
