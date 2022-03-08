09e2e14aa4b4e48abbfcd32d92d50ef661fb5230
Andrew Kushnir <akushnir@google.com>
2022-03-08 20:44:15 +0000

    6eaaefd22e feat(core): drop support for Node.js 12 (#45286)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.5+31.sha-6cec8aa`
+                version: `~14.0.0-next.5+30.sha-6eaaefd`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
