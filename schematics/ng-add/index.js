25d5b15a04ba55b588f34821c14d594fcdf96384
Andrew Scott <atscott01@gmail.com>
2022-03-09 22:10:47 +0000

    a08ea3f643 refactor(router): Make a few adjustments to createUrlTree for clarity (#45306)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.6+4.sha-2e2c80a`
+                version: `~14.0.0-next.6+5.sha-a08ea3f`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
