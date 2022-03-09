314d48a909767599013fd8b61b70647565c27bf0
Andrew Scott <atscott01@gmail.com>
2022-03-09 22:10:11 +0000

    2e2c80acd5 docs: update <div> to <p> in lifecycle-hooks.md (#45063)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.6+1.sha-89d71b5`
+                version: `~14.0.0-next.6+4.sha-2e2c80a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
