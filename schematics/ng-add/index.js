eaa2cbd50d39d3663061e1048a5f7296392d6183
Andrew Scott <atscott01@gmail.com>
2022-03-10 20:25:37 +0000

    8848d2dd28 docs: add manual changelog edits from `13.3.x` to main branch (#45323)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.6+6.sha-79580bb`
+                version: `~14.0.0-next.6+7.sha-8848d2d`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
