ad6ab79013b18122454cbc47296450836aa991f9
Andrew Scott <atscott01@gmail.com>
2022-03-10 21:08:28 +0000

    b839aeb2c1 refactor(core): Switch over to new closure LOCALE vs getLocale() (#45302)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.6+4.sha-0f8e748`
+                version: `~13.3.0-rc.0+1.sha-b839aeb`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
