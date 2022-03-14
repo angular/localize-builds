d878f366dd204ffe888c562beb699bdd136d06cc
Andrew Scott <atscott01@gmail.com>
2022-03-14 16:43:18 +0000

    22d1dc0acb build(bazel): add bazel build and test targets for aio build (#45022)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.6+12.sha-0dbd50b`
+                version: `~14.0.0-next.6+13.sha-22d1dc0`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
