a728b558fea094d984937d74762f7b28017814b4
Andrew Scott <atscott01@gmail.com>
2022-03-15 23:48:36 +0000

    9361f091cc docs: add ngx-gantt to "EXPLORE ANGULAR RESOURCES" page (#45351)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.2.6+7.sha-eac94f0`
+                version: `~13.2.6+8.sha-9361f09`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
