d0dbc65bcf45fbe21c49d4d9c05cff6c904e094e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-21 22:07:46 +0000

    aec9abd374 fix(docs-infra): don't wrap subcommands in code element (#45343)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.7+6.sha-c26915c`
+                version: `~14.0.0-next.7+7.sha-aec9abd`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
