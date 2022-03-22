ed0630709b973cafbaec192f088276bfb540452c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-03-22 00:14:01 +0000

    15dbb4dc7d docs(docs-infra): fix TypeScript casing (#45307)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.7+9.sha-e80df0d`
+                version: `~14.0.0-next.7+11.sha-15dbb4d`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
