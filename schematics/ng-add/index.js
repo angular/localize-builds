c2725656f3c56e532b47c5deebee10c9eca75d5e
Joey Perrott <josephperrott.github@gmail.com>
2022-03-23 22:29:41 +0000

    68901bf0f3 fix(docs-infra): prevent framing of AIO with X-Frame-Options

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             dependencies_1.addPackageJsonDependency(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: "~12.2.15+3.sha-b2a081a.with-local-changes"
+                version: "~12.2.16+1.sha-68901bf.with-local-changes"
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
