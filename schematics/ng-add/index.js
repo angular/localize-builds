12e04642089b0fb9b70092c76197a4aad62648ea
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 21:47:14 +0000

    f869b9146a docs: update for 2022 angular events (#45386)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.0+12.sha-aa29fc7`
+                version: `~13.3.0+14.sha-f869b91`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
