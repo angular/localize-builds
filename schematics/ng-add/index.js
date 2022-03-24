8dc39fc8e366edee9cf49ce4f32299e6952d6acd
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:08:26 +0000

    85782e607d docs(router): Fix typo on segments (#45411)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.7+20.sha-fe0e42a`
+                version: `~14.0.0-next.7+21.sha-85782e6`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
