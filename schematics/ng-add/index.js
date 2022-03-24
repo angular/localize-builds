117e6f9b30f9fac82d9473593df7b7846ea600bb
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:14:04 +0000

    dc72f3007a fix(bazel): ng module compilation workers are subject to linker race-conditions (#45393)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.7+25.sha-c9d566c`
+                version: `~14.0.0-next.7+26.sha-dc72f30`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
