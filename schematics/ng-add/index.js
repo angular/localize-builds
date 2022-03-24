0da0e07b8cf782790e10df1f5300029cdf0f8479
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:13:15 +0000

    b481eab39d fix(docs-infra): update (and unpin) dependency versions for preview server (#45390)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.0+6.sha-ee20410`
+                version: `~13.3.0+11.sha-b481eab`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
