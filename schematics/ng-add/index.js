54dc7fc8a83ee979f7cfd7e6858a1be7555545cf
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:24:00 +0000

    71ee417fae refactor(core): remove unused logic from reflection capabilities (#45335)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.7+30.sha-8198bb9`
+                version: `~14.0.0-next.7+31.sha-71ee417`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
