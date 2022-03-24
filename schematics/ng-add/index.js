eb973e92dad47a2feb7d9914466430c27b72c955
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:09:32 +0000

    fc97499676 build(docs-infra): remove `defaultProject` workspace option (#45410)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.7+24.sha-0dca774`
+                version: `~14.0.0-next.7+23.sha-fc97499`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
