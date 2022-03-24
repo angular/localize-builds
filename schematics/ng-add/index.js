e2766548d21871bbac1a8cdc1a84a7acbf05e411
Dylan Hunn <dylhunn@gmail.com>
2022-03-24 18:13:20 +0000

    f19b36f462 fix(zone.js): in TaskTrackingZoneSpec track a periodic task until it is cancelled (#45391)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.7+23.sha-fc97499`
+                version: `~14.0.0-next.7+28.sha-f19b36f`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
