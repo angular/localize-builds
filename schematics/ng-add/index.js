11797bb70c061f6d5f9a9f4601143474e712bc5f
Dylan Hunn <dylhunn@gmail.com>
2022-03-25 21:43:50 +0000

    1177b4e2f8 ci: make payload size tracking script work with CircleCI and Bazel limitation (#45444)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.8+14.sha-7ab7181`
+                version: `~14.0.0-next.9+1.sha-1177b4e`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
