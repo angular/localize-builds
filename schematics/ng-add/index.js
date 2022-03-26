c0a993eb4029af53a1e77076c04b9b091ac57b06
Dylan Hunn <dylhunn@gmail.com>
2022-03-26 00:08:48 +0000

    31434943f1 build: disable payload size uploading within bazel (#45446)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.9+1.sha-1177b4e`
+                version: `~14.0.0-next.9+3.sha-3143494`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
