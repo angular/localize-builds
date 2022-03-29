4a911e20e24c1555c019eeddfd4724986f954d57
Dylan Hunn <dylhunn@gmail.com>
2022-03-29 16:44:05 +0000

    6e1fce529a test: update aio payload size golden to reflect Angular update (#45461)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.9+19.sha-16e635c`
+                version: `~14.0.0-next.9+22.sha-6e1fce5`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
