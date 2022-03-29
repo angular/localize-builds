0b14627324be06d1f5568d81610579bf9d0685e8
Dylan Hunn <dylhunn@gmail.com>
2022-03-29 16:40:22 +0000

    6e4cb48bc5 docs: adjust contributing, developing and overview markdown files (#45464)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.9+13.sha-801d11d`
+                version: `~14.0.0-next.9+14.sha-6e4cb48`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
