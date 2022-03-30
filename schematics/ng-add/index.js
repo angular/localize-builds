bdd27e4abff177f782c2a435befcdbee3dd64417
Dylan Hunn <dylhunn@gmail.com>
2022-03-30 00:28:19 +0000

    82956dcbd3 Update aio/content/marketing/resources.json (#45076)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.9+35.sha-7d94923`
+                version: `~14.0.0-next.9+37.sha-82956dc`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
