33c8427d671bb923947a9deffc702ab8497a671b
Dylan Hunn <dylhunn@gmail.com>
2022-03-30 18:52:42 +0000

    7f53c0f4ac fix(compiler-cli): handle inline type-check blocks in nullish coalescing extended check (#45478)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.0+41.sha-70006da`
+                version: `~13.3.0+50.sha-7f53c0f`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
