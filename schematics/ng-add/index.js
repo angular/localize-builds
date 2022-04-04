72895da69a2e409a789a172a88294b34295917c2
Andrew Kushnir <akushnir@google.com>
2022-04-04 22:11:02 +0000

    fc86fe5dad build: update dependency entities to v4 (#45509)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.1+5.sha-9fcc1b7`
+                version: `~13.3.1+6.sha-fc86fe5`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
