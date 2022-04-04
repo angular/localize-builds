1b9ccbd7720f5db16e6b46ee79c80e56528d97a8
Andrew Kushnir <akushnir@google.com>
2022-04-04 16:52:46 +0000

    5aeaedfa03 refactor(core): validate element is known in JIT mode (#45492)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.10+8.sha-adf4b6d`
+                version: `~14.0.0-next.10+9.sha-5aeaedf`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
