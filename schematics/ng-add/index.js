2db6e95aa557d723323eb4ab538cfdc7d95824d9
Andrew Kushnir <akushnir@google.com>
2022-04-04 16:41:33 +0000

    b2d06527df fix(docs-infra): update Angular packages to latest minor version (#45502)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.10+6.sha-c0c18b7`
+                version: `~14.0.0-next.10+7.sha-b2d0652`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
