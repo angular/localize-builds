66bd1c1b1cbc3ae3e3ad202791a64627d23b0e01
Andrew Kushnir <akushnir@google.com>
2022-04-04 16:35:47 +0000

    c0c18b7113 docs: add animations to readme file (#45488)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.10+5.sha-8b1a2f1`
+                version: `~14.0.0-next.10+6.sha-c0c18b7`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
