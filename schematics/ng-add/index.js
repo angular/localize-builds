4e7319a9faf7eccd356f8e2f30f77baaca147636
Andrew Kushnir <akushnir@google.com>
2022-04-05 16:06:53 +0000

    1c11a57155 fix(router): merge interited resolved data and static data (#45276) in layers

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.10+13.sha-a3f344f`
+                version: `~14.0.0-next.10+14.sha-1c11a57`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
