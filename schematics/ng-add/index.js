fd4895872cce70bb43fcfd2fcb9529fadfb8d3e6
Andrew Kushnir <akushnir@google.com>
2022-04-05 15:59:21 +0000

    a3f344ff6e test(animations): add test for buildAnimationAst() function (#31107)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.10+10.sha-d56a537`
+                version: `~14.0.0-next.10+13.sha-a3f344f`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
