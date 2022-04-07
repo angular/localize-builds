dd07283279d15dd5b8789fb78476af7220219cd5
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-07 21:16:52 +0000

    b57f8bea61 test(docs-infra): ufix e2e test for `{@example}` dgeni tags (#45551)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.10+16.sha-f1a2816`
+                version: `~14.0.0-next.11+2.sha-b57f8be`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
