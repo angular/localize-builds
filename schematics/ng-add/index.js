58fceedde13414ffc0e960bc19e297c18d83839b
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-07 21:19:20 +0000

    5adfe8ef24 docs(forms): remove the incorrect set value from previous commit (#45533)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.11+5.sha-f8f3ab3`
+                version: `~14.0.0-next.11+4.sha-5adfe8e`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
