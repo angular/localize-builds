2385c6ae12b6a202e8dc16346c08bfa02f162a23
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-08 19:53:44 +0000

    e0ac614121 docs: update test threshold \nupdate test threshold to get this content merged. (#45325)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.11+9.sha-7a37fe9`
+                version: `~14.0.0-next.11+12.sha-e0ac614`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
