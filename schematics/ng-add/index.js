5d3a53b31e0b184df6598dd77dbbf980b6e8a02c
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-11 21:19:21 +0000

    0facba5b70 build: update bazel integration test to RNJ v5.4.0 and Sass 1.50.0 (#45470)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.11+23.sha-f1630bb`
+                version: `~14.0.0-next.11+30.sha-0facba5`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
