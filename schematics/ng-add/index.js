1bdba31ff72f1a81ffb90970bd8818b7aa47c494
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-11 22:53:18 +0000

    61894570f1 docs: improve markdown (#45590)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.2+8.sha-09c5c18`
+                version: `~13.3.2+9.sha-6189457`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
