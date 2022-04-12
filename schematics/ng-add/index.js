affbe94e5316d05b8501176bb46077e877510cd0
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 22:48:38 +0000

    fc145d024b refactor(core): make ComponentFactoryResolver lazily instantiated in ApplicationRef (#45507)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.2+15.sha-c49d232`
+                version: `~13.3.2+19.sha-fc145d0`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
