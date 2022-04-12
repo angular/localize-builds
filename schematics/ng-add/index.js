f60e964bc31385336fa288acc461261ddfdac896
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 22:19:40 +0000

    820077df76 build: update dependency glob to v8 (#45598)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.12+5.sha-83920af`
+                version: `~14.0.0-next.12+6.sha-820077d`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
