4465ab434a19ac2cab879eed36235a28f83915be
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 21:44:49 +0000

    ee3cfc8815 docs: fix path typo (#45604) PR Close #45604

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.12+1.sha-79de026`
+                version: `~14.0.0-next.12+3.sha-ee3cfc8`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
