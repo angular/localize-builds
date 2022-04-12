3257a60d870c828f3a161ac305be563f99fa21b4
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 21:47:29 +0000

    c49d23269c docs: new Pipes custom data doc (#45505)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.2+10.sha-5900f0e`
+                version: `~13.3.2+15.sha-c49d232`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
