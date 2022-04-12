f35a26cd9c485ff13bba4c04d8a4c8a165560cf4
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 18:59:01 +0000

    222d8662a2 docs: fix `schematicCollections` option (#45602) PR Close #45602

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.11+35.sha-eb2f449`
+                version: `~14.0.0-next.11+34.sha-222d866`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
