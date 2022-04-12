4f96aaa9eebb05655599bbe21b9777f98d37ddd3
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-12 18:58:27 +0000

    eb2f4492eb docs(docs-infra): add missing period in `aio/README.md` (#45584)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.11+33.sha-89d2991`
+                version: `~14.0.0-next.11+35.sha-eb2f449`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
