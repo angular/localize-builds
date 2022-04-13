b599a221a7288aa462c1c47301fa2e9548485eaf
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-13 17:37:34 +0000

    47e7a2850b refactor(router): Add warning for `relativeLinkResolution: 'legacy'` (#45523)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.12+18.sha-598b759`
+                version: `~14.0.0-next.12+19.sha-47e7a28`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
