5f9129726050b5b0326423aefa3fd79e21a43624
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-13 17:11:07 +0000

    598b759d33 fix(compiler): avoid errors with extremely long instruction chains (#45574)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.12+17.sha-7bf1cf4`
+                version: `~14.0.0-next.12+18.sha-598b759`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
