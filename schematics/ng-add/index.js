cb943c565ec008f4615c1713b691cf1d78603b57
Jessica Janiuk <jessicajaniuk@google.com>
2022-04-13 00:28:10 +0000

    a6ab3f7beb docs: fix grammar (#45455) PR Close #45455

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.12+11.sha-89ed8d8`
+                version: `~14.0.0-next.12+16.sha-a6ab3f7`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
