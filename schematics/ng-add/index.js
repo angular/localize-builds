6a05bcb948045b1e6e20532935907875c5bb51bc
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 00:47:52 +0000

    000363eec5 refactor(router): combine functions for getting loaded config (#45613)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+1.sha-e250db4`
+                version: `~14.0.0-next.13+2.sha-000363e`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
