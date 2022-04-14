250b22fb866646e04ab810fb2925f68553822985
Dylan Hunn <dylhunn@gmail.com>
2022-04-14 22:19:10 +0000

    6444a02620 docs: move old changelog entries to a separate file (#45638)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+6.sha-2f08b80`
+                version: `~14.0.0-next.13+7.sha-6444a02`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
