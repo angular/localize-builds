a6ba7d2127b365cc11d9b737f92406569889dd9b
Dylan Hunn <dylhunn@gmail.com>
2022-04-15 19:47:50 +0000

    96fd29c6d2 fix(router): validate lazy loaded configs (#45526)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+11.sha-f13295f`
+                version: `~14.0.0-next.13+12.sha-96fd29c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
