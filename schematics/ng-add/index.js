e2ddb13ec3bca74f6adf7bccd3b15b098022b015
Dylan Hunn <dylhunn@gmail.com>
2022-04-15 21:39:20 +0000

    0b6407241c fix(docs-infra): fix aio search-results issue with toolbar and notification (#45579)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+13.sha-2e97312`
+                version: `~14.0.0-next.13+18.sha-0b64072`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
