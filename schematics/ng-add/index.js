6ecbf0061bf4d66ff4a912b26b81febe21abf9f4
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 16:44:14 +0000

    1fe255c76f refactor(compiler): add `original_code` to `goog.getMsg()` options (#45606)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+22.sha-8c83f12`
+                version: `~14.0.0-next.13+23.sha-1fe255c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
