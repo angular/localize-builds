895b2e38f432e187bfcb3d9f952df8dfb87011a6
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 21:26:35 +0000

    aa966fd715 refactor(router): Use `EnvironmentInjector` for lazy loading APIs (#45671)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+29.sha-eb7661c`
+                version: `~14.0.0-next.13+30.sha-aa966fd`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
