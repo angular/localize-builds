f9590c6f62c8f7703e17accbac155296f2042abf
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 16:42:32 +0000

    b29b95b0f8 fix(devtools): clean up menu layout (#45665)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+19.sha-6835710`
+                version: `~14.0.0-next.13+20.sha-b29b95b`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
