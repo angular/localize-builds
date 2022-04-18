f5bc68eb027952867f961440cdc0314000db305b
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 17:10:23 +0000

    eb7661c072 refactor(router): unbundle the lazy loaded routes from the lazy loaded module injector (#45593)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+27.sha-3578e94`
+                version: `~14.0.0-next.13+29.sha-eb7661c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
