38159809142ff2867ccce238213b17e140d6837d
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 16:48:00 +0000

    3578e94384 refactor(core): internally support `providedIn: environment` (#45626)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+23.sha-1fe255c`
+                version: `~14.0.0-next.13+27.sha-3578e94`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
