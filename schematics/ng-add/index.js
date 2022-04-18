27a72fec37485e72190579fadd314f022f274273
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 21:27:04 +0000

    ebf98c4695 docs: use language agnostic mdn urls (#45666) PR Close #45666

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.3+13.sha-37b3ac7`
+                version: `~13.3.3+15.sha-ebf98c4`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
