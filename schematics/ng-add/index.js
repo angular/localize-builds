48451d52395b0029e5ecb3aefb177ccbbf664d93
Dylan Hunn <dylhunn@gmail.com>
2022-04-18 16:43:10 +0000

    8c83f12daa ci: add more reviewers to the `docs-packaging-and-releasing` group (#45652)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+20.sha-b29b95b`
+                version: `~14.0.0-next.13+22.sha-8c83f12`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
