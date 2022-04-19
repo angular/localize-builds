71246d491cf7be3bfb1ac435611eca6f76c80e0e
Dylan Hunn <dylhunn@gmail.com>
2022-04-19 16:32:58 +0000

    f5299a3b8f docs: update decorators proposal stage and link in glossary (#45669)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+35.sha-63202b9`
+                version: `~14.0.0-next.13+36.sha-f5299a3`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
