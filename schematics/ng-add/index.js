e9f1e5abc7c7e704fd940ffd6321fc8e2ad61ebb
Dylan Hunn <dylhunn@gmail.com>
2022-04-19 16:37:41 +0000

    ceffdeda69 docs(common): fix links to locales in i18n guides (#45661)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+37.sha-c6feb0a`
+                version: `~14.0.0-next.13+38.sha-ceffded`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
