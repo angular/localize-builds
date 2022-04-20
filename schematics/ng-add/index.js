79044dbab7ebfbdbd218ff45032799204d1f3431
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-04-20 13:04:29 +0000

    b8d3389173 test(compiler-cli): tests for standalone components/directives/pipes (#45672)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+44.sha-989e840`
+                version: `~14.0.0-next.13+54.sha-b8d3389`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
