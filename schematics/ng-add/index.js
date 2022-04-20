073cc27a1b4917e755ddaf7309dd79a26fb4f1fe
Dylan Hunn <dylhunn@gmail.com>
2022-04-20 03:38:21 +0000

    4ad7c2e4df docs: Update aio/content/guide/setup-local.md (#45644)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+41.sha-0231a36`
+                version: `~14.0.0-next.13+43.sha-4ad7c2e`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
