dc7f37cdfcfd2823864958b0c02c4e083e745f78
Dylan Hunn <dylhunn@gmail.com>
2022-04-20 03:36:52 +0000

    0231a363fe build: preparation for primary branch rename in the Angular repos (#45681)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+39.sha-5d15595`
+                version: `~14.0.0-next.13+41.sha-0231a36`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
