c4735ad5513550f23f92bce10141492f3e746b7b
Dylan Hunn <dylhunn@gmail.com>
2022-04-20 20:15:33 +0000

    1d2f5c1101 refactor(router): deprecate no longer needed resolver fields (#45597)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.13+60.sha-4e0957a`
+                version: `~14.0.0-next.13+62.sha-1d2f5c1`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
