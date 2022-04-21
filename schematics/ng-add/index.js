ecc322fe60f1e86947cecebf8a6f4f05d97c4f9e
Andrew Scott <atscott01@gmail.com>
2022-04-21 18:25:10 +0000

    3dee3d107b fix(docs-infra): fix placement of "Edit source" button on errors and diagnostics pages (#45634)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.14+8.sha-9292953`
+                version: `~14.0.0-next.14+9.sha-3dee3d1`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
