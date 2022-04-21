17e68406738623780d6ff6f81560ee21345103a1
Andrew Scott <atscott01@gmail.com>
2022-04-21 18:23:03 +0000

    9292953c56 fix(docs-infra): include `tslib` into SystemJS config in `upgrade-module` example app (#45706)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.14+7.sha-9e4c4bc`
+                version: `~14.0.0-next.14+8.sha-9292953`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
