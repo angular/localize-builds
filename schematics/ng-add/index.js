41c4b719ab1b990f4da82f078865993dbf3fbccf
Andrew Scott <atscott01@gmail.com>
2022-04-22 20:04:52 +0000

    970a3b5c70 fix(bazel): add this_is_bazel marker (#45728)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.14+19.sha-4962a4a`
+                version: `~14.0.0-next.14+20.sha-970a3b5`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
