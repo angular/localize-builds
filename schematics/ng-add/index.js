5c6d52d872f53bd2d5316d0f38450e0b087e02df
Andrew Scott <atscott01@gmail.com>
2022-04-25 18:10:48 +0000

    31d7c3bd71 feat(common): add getState method to LocationStrategy interface (#45648)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.14+20.sha-970a3b5`
+                version: `~14.0.0-next.14+22.sha-31d7c3b`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
