ecb89c87636db239f97e44d687a1ba230b687281
Andrew Scott <atscott01@gmail.com>
2022-04-26 17:30:15 +0000

    3a645c6bfc build: update actions/checkout action to v3.0.2 (#45764)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.4+3.sha-63398e0`
+                version: `~13.3.4+4.sha-3a645c6`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
