cfe406240c6e2136c9d490a6c6925b2a13a2470d
Andrew Scott <atscott01@gmail.com>
2022-04-26 17:26:30 +0000

    4e0784c2f0 refactor(compiler-cli): optimize InjectorDef imports generation (#45701)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.14+23.sha-7992132`
+                version: `~14.0.0-next.14+27.sha-4e0784c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
