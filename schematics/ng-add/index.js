857d511e594ffb37dd65d0b25350dcc87352673d
Dylan Hunn <dylhunn@gmail.com>
2022-04-27 20:58:23 +0000

    f3eb7d9ecb fix(core): Ensure the `StandaloneService` is retained after closure minification (#45783)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.14+43.sha-4d6a1d6`
+                version: `~14.0.0-next.15+1.sha-f3eb7d9`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
