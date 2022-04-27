fd7af0b3a68829c6ce2c1e26c2a39e681196fdce
Andrew Scott <atscott01@gmail.com>
2022-04-27 16:26:22 +0000

    2eb39c0878 fix(devtools): inline loading svg (#45762)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.14+37.sha-882f595`
+                version: `~14.0.0-next.14+41.sha-2eb39c0`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
