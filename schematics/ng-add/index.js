3df7f0f63861f8b91380a00c6c96fc89a0b52e69
Joey Perrott <josephperrott@gmail.com>
2022-04-28 17:42:49 +0000

    839392523e build: updates to account for github primary branch rename

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.4+9.sha-8cfb53d`
+                version: `~13.3.5+1.sha-8393925`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
