221d21796aee9396591041fc4426d5d78d57f265
Dylan Hunn <dylhunn@gmail.com>
2022-04-28 16:55:13 +0000

    4e413d9240 fix(core): support nested arrays of providers in `EnvironmentInjector` (#45789)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.15+4.sha-2f5fd41`
+                version: `~14.0.0-next.15+7.sha-4e413d9`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
