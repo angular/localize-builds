d36a5b77fb7b0910a7ef019cc83e7151e245e158
Dylan Hunn <dylhunn@gmail.com>
2022-04-28 17:09:27 +0000

    29417935de test(core): verify nested array in standalone component imports (#45794)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.15+5.sha-14c4979`
+                version: `~14.0.0-next.15+6.sha-2941793`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
