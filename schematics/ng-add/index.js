a0cb67169d8cf5b9d6ea266e247c4b1db5eab22d
Dylan Hunn <dylhunn@gmail.com>
2022-04-28 16:55:53 +0000

    a097be9497 test(core): verify `importProvidersFrom` work correctly with `ModuleWithProviders` (#45787)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.15+7.sha-4e413d9`
+                version: `~14.0.0-next.15+8.sha-a097be9`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
