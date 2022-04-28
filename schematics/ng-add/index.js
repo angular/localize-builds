e0c963673cf45eb65c9bd8f37a547f5d4afe9ab3
Dylan Hunn <dylhunn@gmail.com>
2022-04-28 21:57:30 +0000

    d4fc12fa19 fix(router): Remove deprecated initialNavigation option (#45729)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.15+sha-3a60063`
+                version: `~14.0.0-next.15+sha-d4fc12f`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
