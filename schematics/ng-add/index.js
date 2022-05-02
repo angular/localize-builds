36eba63bc4cdf6828ed6e3ff96e2c48cc3042b57
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 23:51:02 +0000

    c00786ac3a fix(dev-infra): Fix pullapprove by commenting out the empty availability (#45846)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.5+5.sha-e7cd87d`
+                version: `~13.3.5+6.sha-c00786a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
