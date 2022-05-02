0fb6c231c08c6bbff60900fdb28eae300651a4ee
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 22:53:11 +0000

    e7cd87d237 ci: mark pkozlowski as availabe for PR reviews (#45842)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.5+4.sha-e59ed8c`
+                version: `~13.3.5+5.sha-e7cd87d`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
