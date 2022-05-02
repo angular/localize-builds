85f69f61a946d273d21969d528631a48f2aba45c
Dylan Hunn <dylhunn@gmail.com>
2022-05-02 22:21:25 +0000

    fce50637c5 docs(forms): Add documentation for typed forms. (#45841)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.15+sha-38f2906`
+                version: `~14.0.0-next.15+sha-fce5063`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
