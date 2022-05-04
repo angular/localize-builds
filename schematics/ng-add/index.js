b67c88c16017e16aac913927257efb0182539835
Dylan Hunn <dylhunn@gmail.com>
2022-05-04 00:21:49 +0000

    a2d5358c9b Revert "feat(compiler-cli): exclude abstract classes from `strictInjectionParameters` requirement (#44615)" (#45862)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.15+sha-fa755b2`
+                version: `~14.0.0-next.15+sha-a2d5358`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
