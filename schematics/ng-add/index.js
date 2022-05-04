d708290f41c4ef0b6c490582deec4bb01d987ede
Andrew Kushnir <akushnir@google.com>
2022-05-04 23:54:27 +0000

    8786c609f8 refactor(docs-infra): Redirect old document title guide to new router feature (#45857)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.16+sha-967e934`
+                version: `~14.0.0-next.16+sha-8786c60`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
