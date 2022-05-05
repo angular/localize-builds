dd21bcc2af2d794bc1d48982fd868b342277d04c
Andrew Kushnir <akushnir@google.com>
2022-05-05 22:50:53 +0000

    b7fca64828 build(devtools): Optimize prod build for Angular DevTools (#45886)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-a212fb4`
+                version: `~14.1.0-next.0+sha-b7fca64`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
