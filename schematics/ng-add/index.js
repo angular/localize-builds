e59a9fecee55f65d0602d94ee5d1789d40d3d366
Andrew Kushnir <akushnir@google.com>
2022-05-05 19:08:55 +0000

    719199c678 docs: fix a markdown syntax error in aio/content/marketing/analytics.md (#45898) PR Close #45898

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.6+1.sha-0928025`
+                version: `~13.3.6+2.sha-719199c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
