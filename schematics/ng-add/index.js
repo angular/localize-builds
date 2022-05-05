653e980cc18361e24f356fbc8300269f31702d3e
Andrew Kushnir <akushnir@google.com>
2022-05-05 21:15:13 +0000

    f6543c25aa build: use vendored yarn script for installing Sass repositories (#45892)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.16+sha-3f9faad`
+                version: `~14.0.0-next.16+sha-f6543c2`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
