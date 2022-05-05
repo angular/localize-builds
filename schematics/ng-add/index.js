f012ab11d669b625770174f1b11f1f1536ebbc13
Andrew Kushnir <akushnir@google.com>
2022-05-05 22:07:17 +0000

    c9899637b0 build(docs-infra): remove unused Docker artifacts when updating the preview server (#45895)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.6+2.sha-719199c`
+                version: `~13.3.6+4.sha-c989963`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
