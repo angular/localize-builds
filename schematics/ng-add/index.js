f4c8959e9b881a5e39f8fc8274592208da5c65dd
Andrew Kushnir <akushnir@google.com>
2022-05-05 18:13:44 +0000

    dcea70fab7 refactor(core): rename internal bootstrap function (#45896)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.16+sha-1e4e9ee`
+                version: `~14.0.0-next.16+sha-dcea70f`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
