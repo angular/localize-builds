5e0ef7e49832395d90ba4a1faab12a8f498463f4
Andrew Kushnir <akushnir@google.com>
2022-05-05 17:33:21 +0000

    1e4e9ee9a7 build: update dependency @types/chrome to ^0.0.184 (#45849)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.16+sha-8786c60`
+                version: `~14.0.0-next.16+sha-1e4e9ee`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
