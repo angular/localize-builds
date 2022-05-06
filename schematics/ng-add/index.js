aa49f1e926913bd80d4417e031b64a54585216d3
Andrew Kushnir <akushnir@google.com>
2022-05-06 00:15:46 +0000

    323ef1ebf7 docs(docs-infra): improve clarity of toh-pt2 li>button code (#45858)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.16+sha-86f0213`
+                version: `~14.0.0-next.16+sha-323ef1e`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
