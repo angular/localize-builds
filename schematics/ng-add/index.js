8e4a175fbddccf28f1b2fa3890d86fc8327feb50
Andrew Kushnir <akushnir@google.com>
2022-05-06 00:14:23 +0000

    0ffb19abed test(platform-browser): refactor flaky test (#45906)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-3e7d299`
+                version: `~14.1.0-next.0+sha-0ffb19a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
