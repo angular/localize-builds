7f0d4650a102cfa78aa864fc3a6c033279582b67
Andrew Kushnir <akushnir@google.com>
2022-05-09 20:42:22 +0000

    73bbee2c0d perf(core): allow `checkNoChanges` mode to be tree-shaken in production (#45936)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.6+18.sha-02b99f6`
+                version: `~13.3.6+19.sha-73bbee2`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
