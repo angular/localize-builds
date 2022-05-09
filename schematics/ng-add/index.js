aad9e262a0ec0dc99d866d39dbc7d8d30e6fe9ad
Andrew Kushnir <akushnir@google.com>
2022-05-09 19:51:17 +0000

    91b833a7f4 ci: enable the `aio_preview` CircleCI job (#45935)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-0f0d01b`
+                version: `~14.1.0-next.0+sha-91b833a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
