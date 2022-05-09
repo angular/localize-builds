b4f9aabf8011a26d273360dfa1abbf184c55f28b
Andrew Kushnir <akushnir@google.com>
2022-05-09 19:14:27 +0000

    b5d78f4474 refactor(docs-infra): remove unnecessary types for stemmer dependency (#45831)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-c434097`
+                version: `~14.1.0-next.0+sha-b5d78f4`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
