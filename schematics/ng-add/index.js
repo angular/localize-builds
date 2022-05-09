0760db1647289dac37a6c345b0a66fd5e5ca1765
Andrew Kushnir <akushnir@google.com>
2022-05-09 19:26:53 +0000

    0f0d01b023 docs: remove unnecesarry backslash from pipes documentation (#45916)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-75b3d0f`
+                version: `~14.1.0-next.0+sha-0f0d01b`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
