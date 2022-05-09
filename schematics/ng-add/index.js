c949daac3fe588da30c083709b10c5e3b30f1caf
Andrew Kushnir <akushnir@google.com>
2022-05-09 18:41:55 +0000

    5056cfe839 docs(docs-infra): clarify toh-2 error message (#45878)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.6+8.sha-6644165`
+                version: `~13.3.6+12.sha-5056cfe`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
