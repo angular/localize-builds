70a8784cd2c4d697f60bbe42fa5916cc3c711e84
Andrew Kushnir <akushnir@google.com>
2022-05-10 23:21:29 +0000

    f70b84f755 docs: add NGX-YOUI website to resources list (#45810)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-next.16+sha-1d3cf22`
+                version: `~14.0.0-next.16+sha-f70b84f`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
