6162503e6709fe07517800987327f3ab134d0a9f
Andrew Kushnir <akushnir@google.com>
2022-05-11 18:40:47 +0000

    1c0d0f0579 refactor(bazel): add support for starlark's unused_inputs_list (#45946)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-rc.0+sha-c3c82b6`
+                version: `~14.0.0-rc.0+sha-1c0d0f0`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
