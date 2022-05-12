a7aebf1349bb2dcce7328915e3f22109bb451014
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-12 17:57:09 +0000

    fa754cd9db fix(language-service): Prevent TSServer from removing templates from project (#45965)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-aea7b98`
+                version: `~14.1.0-next.0+sha-fa754cd`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
