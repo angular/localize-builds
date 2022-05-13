1a61711ff5ed1906d333c771c42fdac25f55de55
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 19:36:44 +0000

    77c9508e51 refactor(router): Only create the `Route` injector when the path matches (#45990)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-rc.0+sha-3f7ecec`
+                version: `~14.0.0-rc.0+sha-77c9508`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
