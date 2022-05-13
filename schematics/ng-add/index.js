f05b840b24c6846d7831df6aaf5f5b1c47f11c86
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-13 20:26:18 +0000

    bcef46d83e docs: adds ngx-cookie-service library to resources list (#45955) PR Close #45955

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-rc.0+sha-77c9508`
+                version: `~14.0.0-rc.0+sha-bcef46d`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
