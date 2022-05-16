70c7076232993feef3fab1910e3240fd9c2b1c66
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 23:25:45 +0000

    6952a0a3e6 docs: add template overview doc (#45897)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-a7897ee`
+                version: `~14.1.0-next.0+sha-6952a0a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
