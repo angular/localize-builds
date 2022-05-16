f974bcae6462cde6cb3e292af3b5b7046954ee9a
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 18:22:50 +0000

    39a1364e43 docs(devtools): add firefox extension references (#45985)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-rc.0+sha-9b97b84`
+                version: `~14.0.0-rc.0+sha-39a1364`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
