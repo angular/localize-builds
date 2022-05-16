9308cb9a508d266dee97c48443e05b36d2f797c4
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-16 17:23:00 +0000

    cd04588643 refactor(core): tree-shake away a DI error message string in prod bundles (#45994)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-3242f75`
+                version: `~14.1.0-next.0+sha-cd04588`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
