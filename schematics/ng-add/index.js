5e25e9fdd1375daffcf196aea1bfbc8142483c49
Jessica Janiuk <jessicajaniuk@google.com>
2022-05-17 18:05:23 +0000

    338de1b4cf docs: remove Narco from hero list in code, text, and images (#46008)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-rc.0+sha-79025a6`
+                version: `~14.0.0-rc.0+sha-338de1b`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
