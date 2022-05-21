ea991f515b9a2fb8f084e3dcc8372e11645186fb
Joey Perrott <josephperrott@gmail.com>
2022-05-21 20:38:46 +0000

    183b5f3fde build: update saucelabs key (#46089)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-rc.1+sha-f02ba99`
+                version: `~14.0.0-rc.1+sha-183b5f3`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
