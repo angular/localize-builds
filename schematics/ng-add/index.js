18290c89010f8d8e80a44f30dbfb2877934bf30f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 16:48:02 +0000

    b11a939e21 docs: add demo video for Angular CLI autocompletion doc (#45995)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-rc.1+sha-183b5f3`
+                version: `~14.0.0-rc.1+sha-b11a939`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
