679d48071bef6d7561e4a55a39ea5296d392079d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 16:42:03 +0000

    8704574f7e fix(core): restore NgModule state correctly after TestBed overrides (#46049)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-ae18f00`
+                version: `~14.1.0-next.0+sha-8704574`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
