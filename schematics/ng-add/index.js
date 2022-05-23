0383f7647f1f4136c424438cb8463cb87c59a202
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 17:14:38 +0000

    f6c265deba fix(docs-infra): add member name to search index without ignore list (#46060)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.9+2.sha-1d02c09`
+                version: `~13.3.9+4.sha-f6c265d`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
