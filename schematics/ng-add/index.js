b9dcd05fcdff885bad338264180078c15402d6d1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 17:21:37 +0000

    3172b4cc99 fix(core): produce proper error message for unknown props on `<ng-template>`s (#46068)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-rc.1+sha-b11a939`
+                version: `~14.0.0-rc.1+sha-3172b4c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
