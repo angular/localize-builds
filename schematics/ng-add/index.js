25c8483b84ab882ba696d22e864709627089a292
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-23 21:16:35 +0000

    123782bfb6 docs: move all bazel testing info to a single location (#46084)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.9+4.sha-f6c265d`
+                version: `~13.3.9+13.sha-123782b`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
