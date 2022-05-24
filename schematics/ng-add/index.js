e30cf357bb6ce5f1dab26f657d544587404d7206
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-24 18:12:12 +0000

    8629f2d0af refactor(compiler-cli): fix "for to" typo in code comment (#46113)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-0b6ca5b`
+                version: `~14.1.0-next.0+sha-8629f2d`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
