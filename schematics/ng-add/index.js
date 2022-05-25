b030b6dede5e1463928ef3a110697234f1259ba3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-05-25 19:40:12 +0000

    a1e5aad5dc fix(core): improve TestBed declarations standalone error message (#45999)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-d2ea1ba`
+                version: `~14.1.0-next.0+sha-a1e5aad`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
