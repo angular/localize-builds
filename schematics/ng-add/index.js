12e4cc8efdf45a266ca794e15b865bcc0eb3be0d
Andrew Scott <atscott01@gmail.com>
2022-05-27 01:00:35 +0000

    8c9051eac9 docs: close lightbox div to fix page display (#46155)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.10+1.sha-286f4d6`
+                version: `~13.3.10+3.sha-8c9051e`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
