737ae786c25e9dec905387a1c5a671b59f66db20
Andrew Scott <atscott01@gmail.com>
2022-05-27 16:49:04 +0000

    5f66b55abd docs: change wrong alert to callout (#46145)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.10+3.sha-8c9051e`
+                version: `~13.3.10+4.sha-5f66b55`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
