564dc041581cc7da1db736cbb3087027bc79b5f9
Andrew Scott <atscott01@gmail.com>
2022-05-31 19:20:11 +0000

    00e54e3e94 docs: replace broken  `<br/>` from service worker (#46193)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.10+4.sha-5f66b55`
+                version: `~13.3.10+8.sha-00e54e3`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
