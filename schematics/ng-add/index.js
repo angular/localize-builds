01d46095051f9054fa09d1fb6993cf88bb3ef132
Andrew Scott <atscott01@gmail.com>
2022-06-01 18:25:57 +0000

    94c2cfbe25 docs(router): Adjust example for scrollPositionRestoration (#46201)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.10+10.sha-ce2f149`
+                version: `~13.3.11+2.sha-94c2cfb`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
