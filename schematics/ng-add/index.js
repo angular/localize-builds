2087512cd39b64bf45d8d5f24e84632bcd6c3cf5
Andrew Scott <atscott01@gmail.com>
2022-06-01 18:27:05 +0000

    536939e138 docs(core): improve standalone-related docs and add more usage examples (#46202)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0-rc.3+sha-763c5f8`
+                version: `~14.0.0-rc.3+sha-536939e`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
