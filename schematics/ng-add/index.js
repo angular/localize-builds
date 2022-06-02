18db722ced4cdbe120a96a0059ccb6d9ae3db1a6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-02 21:01:22 +0000

    f74bd1b0f5 perf(bazel): reduce input files for `ng_package` rollup and type bundle actions (#46187)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-0206c10`
+                version: `~14.1.0-next.0+sha-f74bd1b`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
