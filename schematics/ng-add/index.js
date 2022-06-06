2ed6a52454fc65b96d8103bc92f7f5b40fd852d7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-06 19:01:23 +0000

    385b5ec92c docs: removed outdated migration documentation (#46257)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-aa2e8ac`
+                version: `~14.1.0-next.0+sha-385b5ec`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
