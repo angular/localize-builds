4b4048c75db937c55bfb605f6b67f49112d357a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-07 17:35:22 +0000

    862aafbec9 refactor(common): include locale extra data in closure locale file (#46167)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0+sha-b20e470`
+                version: `~14.0.0+sha-862aafb`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
