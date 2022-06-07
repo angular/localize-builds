0e94e81bc1eb9b0e93226ebe5ac1f655ab748512
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-06-07 18:01:48 +0000

    5b7c131658 build: update dev-infra shared code to latest revision (#46291)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.0+sha-862aafb`
+                version: `~14.0.0+sha-5b7c131`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
