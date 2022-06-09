336e28990790910d4f14e35f341f2642f6d82956
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-09 21:05:02 +0000

    d6ee9f29e6 docs: update example hero ID (#46304)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.1+sha-ef5cba3`
+                version: `~14.0.1+sha-d6ee9f2`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
