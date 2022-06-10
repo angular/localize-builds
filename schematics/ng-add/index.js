0f4980c898c0c2dd8b193a409ac51c14b28c222e
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-06-10 22:14:32 +0000

    bf8bc1e4cc refactor(common): properly configure PRECONNECT_CHECK_BLOCKLIST

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-b00b52d`
+                version: `~14.1.0-next.0+sha-bf8bc1e`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
