efc286d4a93fd934a1af517e84380e9260f46c94
Andrew Kushnir <akushnir@google.com>
2022-06-11 00:11:43 +0000

    9dd43f222d refactor(common): update the NgOptimizedImage code to the latest version

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.9+23.sha-872282c`
+                version: `~13.3.11+sha-9dd43f2`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
