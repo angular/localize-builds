0110b5077d566181ad15e35ae7e6d05fae9827ad
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-13 21:17:45 +0000

    b5d5f76c9d docs:  Adding a book "Angular from Scratch" (#46352)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.1+sha-a0f3c81`
+                version: `~14.0.1+sha-b5d5f76`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
