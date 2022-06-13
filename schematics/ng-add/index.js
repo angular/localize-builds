76d93693e9ba806c5e64afecd9637120400a3f15
Jessica Janiuk <jessicajaniuk@google.com>
2022-06-13 17:18:38 +0000

    3fd8948b4a fix(core): Resolve forwardRef declarations for jit (#46334)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.1+sha-b92c1a6`
+                version: `~14.0.1+sha-3fd8948`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
