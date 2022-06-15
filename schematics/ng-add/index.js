e573330b7365a74aeb59355cae96a69f2363e09d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-06-15 06:23:06 +0000

    485c6a7169 Revert "fix(common): sanitize `rawSrc` and `rawSrcset` values in NgOptimizedImage directive"

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.0+sha-8afc623`
+                version: `~14.1.0-next.0+sha-485c6a7`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
