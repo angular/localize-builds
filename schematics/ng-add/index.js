458afb3be12a4be554e7afed09174e12657d6d88
Andrew Kushnir <akushnir@google.com>
2022-06-16 16:53:03 +0000

    8fac2995fc refactor(common): align tree shakable error messages to the new format (#46382) Simplifying the tree shakable error messages with the new format and removing the errorMessage variables

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.2+sha-14b9628`
+                version: `~14.0.2+sha-8fac299`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
