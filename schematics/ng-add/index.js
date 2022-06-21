62ad137d5fcc80b72251e326097fbbfc80888060
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:16:17 +0000

    6ad7b40a6f fix(platform-server): invalid style attribute being generated for null values (#46433)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.2+sha-8fac299`
+                version: `~14.0.2+sha-6ad7b40`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
