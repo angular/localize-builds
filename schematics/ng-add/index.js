120a21c82061a5c6aa6803aa2dce68c1f22d8ce9
Andrew Kushnir <akushnir@google.com>
2022-06-21 21:39:51 +0000

    7a084c8134 docs: add dany paredes to GDE resources (#46411)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.2+sha-c086653`
+                version: `~14.0.2+sha-7a084c8`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
