589362fe37ed24a5e55c0f7cdf618ae3d9dab4c2
Andrew Kushnir <akushnir@google.com>
2022-06-21 19:05:24 +0000

    cdd19bd30e docs(core): add full example for IfLoadedDirective (#46425)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.1+sha-1aa27eb`
+                version: `~14.1.0-next.1+sha-cdd19bd`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
