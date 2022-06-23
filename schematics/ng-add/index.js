97e1508ed7d92e696c6ddc40ad1706e670158784
Dylan Hunn <dylhunn@gmail.com>
2022-06-23 21:12:44 +0000

    4eb1ca11f0 fix(animations): cleanup DOM elements when the root view is removed (#45143)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.2+sha-4a2e733`
+                version: `~14.1.0-next.2+sha-4eb1ca1`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
