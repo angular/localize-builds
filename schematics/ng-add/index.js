cad6f4c0b322d620d69a6ebfb79e6893f5cc88d3
Dylan Hunn <dylhunn@gmail.com>
2022-06-23 23:58:23 +0000

    1da3a051b5 refactor(core): make i18n error messages tree shakable (#46468) Make the error messages tree shakable from the production build to reduce the bundle size.

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.2+sha-4eb1ca1`
+                version: `~14.1.0-next.2+sha-1da3a05`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
