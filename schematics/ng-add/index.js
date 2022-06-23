4ab9b74523f3945706dd4e7242450e58ee639165
Dylan Hunn <dylhunn@gmail.com>
2022-06-23 21:06:25 +0000

    a7db899ade build: speed up `ng-dev` config loading by disabling type-checking (#46476)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.2+sha-8d48549`
+                version: `~14.1.0-next.2+sha-a7db899`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
