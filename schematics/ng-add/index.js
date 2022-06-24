cb6f9f4edd3d74412aab7472f12307ba866e6d2a
Dylan Hunn <dylhunn@gmail.com>
2022-06-24 20:27:57 +0000

    14e24ac1ff ci: attempt to unblock renovate from error state (#46486)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.2+sha-83ef9d2`
+                version: `~14.1.0-next.2+sha-14e24ac`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
