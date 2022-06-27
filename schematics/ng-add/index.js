9a0152fbb5a86402d152b806ffa9ee9d72892b84
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 16:23:21 +0000

    6b8f10ef4b test: update standalone browser tests to not pollute test output (#46510)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.2+sha-0a84898`
+                version: `~14.1.0-next.2+sha-6b8f10e`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
