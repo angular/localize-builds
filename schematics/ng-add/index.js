e4999de2440e734217e9e02d6366f8b32c799ba1
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 23:11:26 +0000

    f355a24a12 test: avoid test fixture affecting zone in all web tests (#46511)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.3+sha-56eca95`
+                version: `~14.0.3+sha-f355a24`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
