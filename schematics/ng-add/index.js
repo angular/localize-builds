9cd3bcf14b550196e096340c161b308df8629959
Dylan Hunn <dylhunn@gmail.com>
2022-06-27 22:34:42 +0000

    acf6dc9031 test(core): update `di_spec.ts` to use TestBed (#46522)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.2+sha-8ed502a`
+                version: `~14.1.0-next.2+sha-acf6dc9`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
