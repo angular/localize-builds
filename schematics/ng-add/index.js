5bc614cab2877429a6438512c8eb9acf9034a0d1
Dylan Hunn <dylhunn@gmail.com>
2022-06-28 18:35:54 +0000

    a322b7d694 refactor(core): disable renderer3 unless explicitly enabled by tests (#46530)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.2+sha-1c5f0cd`
+                version: `~14.1.0-next.2+sha-a322b7d`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
