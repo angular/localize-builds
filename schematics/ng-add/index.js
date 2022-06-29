9862511d45aa4fc6e5f239b0b0d5ddc334de30e7
Dylan Hunn <dylhunn@gmail.com>
2022-06-29 17:49:42 +0000

    926de8c0a4 refactor: remove no longer used prompts utils (#46587)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.2+sha-9d30298`
+                version: `~14.1.0-next.2+sha-926de8c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
