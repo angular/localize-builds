2d10704fe6d635ef65c9e4825196e5407070879f
Andrew Scott <atscott01@gmail.com>
2022-06-29 21:13:35 +0000

    598239c35d build: update dependency rimraf to v3 (#46625)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.3+sha-daa6c24`
+                version: `~14.1.0-next.3+sha-598239c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
