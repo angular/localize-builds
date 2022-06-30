a14255260d763c383bfd7713b897cee8abf73962
Andrew Scott <atscott01@gmail.com>
2022-06-30 20:01:16 +0000

    39c02f1c9d docs(router): mark title strategy as injectable (#46639)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.3+sha-8d2e5e6`
+                version: `~14.1.0-next.3+sha-39c02f1`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
