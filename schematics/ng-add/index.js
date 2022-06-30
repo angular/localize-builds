0360ab3b8e6ea57586f8e95034b115856a575b09
Andrew Scott <atscott01@gmail.com>
2022-06-30 21:26:52 +0000

    6f11a58040 feat(compiler): Add extended diagnostic to warn when text attributes are intended to be bindings (#46161)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.3+sha-5dd9d4a`
+                version: `~14.1.0-next.3+sha-6f11a58`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
