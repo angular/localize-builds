904bea15cb977267a68200927e34897f675985a9
Andrew Scott <atscott01@gmail.com>
2022-06-30 20:03:49 +0000

    1bd0b5665d refactor(core): remove experimental renderComponent function (#46646)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.4+sha-a065b6e`
+                version: `~14.0.4+sha-1bd0b56`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
