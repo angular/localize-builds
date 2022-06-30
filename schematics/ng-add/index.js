39d30147e009b4df48cd727c1c18ec05cd19db86
Andrew Scott <atscott01@gmail.com>
2022-06-30 20:55:56 +0000

    7fe81a9a0c refactor(router): Convert user-visible router errors to RuntimeError (#46630)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.4+sha-1bd0b56`
+                version: `~14.0.4+sha-7fe81a9`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
