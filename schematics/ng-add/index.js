168e6e89c15557e70d5a4d05b59a605d6c127f2a
Andrew Scott <atscott01@gmail.com>
2022-07-01 17:41:22 +0000

    853b1c1bc9 docs: move section on subscribing to observable (#46655)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.4+sha-7fe81a9`
+                version: `~14.0.4+sha-853b1c1`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
