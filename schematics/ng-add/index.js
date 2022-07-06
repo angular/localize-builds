ddd88fb3aaec4af6726dcc404f0b35e6a8df22e5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-07-06 21:05:38 +0000

    818928c70d ci: remove renovate changes group (#46676)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.5+sha-e8c8b69`
+                version: `~14.0.5+sha-818928c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
