ab3006e2066b24f6ec7b586a72b21060752f29ec
Andrew Scott <atscott01@gmail.com>
2022-07-06 14:49:09 +0000

    5fd3bb6a15 docs: replace fix typo in (#46659)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.3+sha-03e810e`
+                version: `~14.1.0-next.3+sha-5fd3bb6`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
