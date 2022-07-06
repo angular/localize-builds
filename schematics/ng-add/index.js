b5ee13034562c9439a61ad563ab9bd2bb2d35822
Andrew Scott <atscott01@gmail.com>
2022-07-06 17:08:25 +0000

    e9b5dac9ec fix(forms): Move all remaining errors in Forms to use RuntimeErrorCode. (#46654)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.4+sha-2972f07`
+                version: `~14.1.0-next.4+sha-e9b5dac`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
