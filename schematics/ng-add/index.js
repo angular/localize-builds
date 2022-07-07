20014551066fd2d49386a1605ae939b4e658e7ee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-07-07 21:09:49 +0000

    b3c20add72 Revert "fix(animations): enable shadowElements to leave when their parent does (#46459)" (#46739)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.4+sha-28b6ec5`
+                version: `~14.1.0-next.4+sha-b3c20ad`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
