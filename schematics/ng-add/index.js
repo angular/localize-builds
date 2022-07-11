43b0d49365d6ee0e021a33b69aeaacecd71aae65
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 17:56:03 +0000

    97491997ff build(docs-infra): update versions of some docs examples dependencies (#46609)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.0.5+sha-ec267b4`
+                version: `~14.0.5+sha-9749199`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
