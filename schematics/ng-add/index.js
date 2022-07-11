6e25b9a76d01654f1e739c65107f374a2cdc2d9c
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-11 17:52:22 +0000

    4190e9a776 build: update dependency jasmine to v4 (#46617)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.4+sha-b9f5829`
+                version: `~14.1.0-next.4+sha-4190e9a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
