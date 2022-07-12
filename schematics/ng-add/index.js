14462501ac569c9a6a3e3f9c0fc60a157770eb8f
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-12 18:17:27 +0000

    55308f2df5 feat(animations): add `provideAnimations()` and `provideNoopAnimations()` functions (#46793)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.4+sha-93c65e7`
+                version: `~14.1.0-next.4+sha-55308f2`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
