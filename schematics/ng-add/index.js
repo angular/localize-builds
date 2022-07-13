28743990f71e62322482dedf099833a1d276a49f
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 22:20:03 +0000

    d36a9e6bf3 refactor(core): improve an error message when `ENVIRONMENT_INITIALIZER` is not a multi provider (#46829)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-rc.0+sha-3f2d16c`
+                version: `~14.1.0-rc.0+sha-d36a9e6`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
