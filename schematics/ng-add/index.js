46affd13a13063486176ce256ca8833c658f30a3
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 00:11:21 +0000

    2c35f8823e build: update cross-repo angular dependencies to e8da4c0 (#46809)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.4+sha-a4ec109`
+                version: `~14.1.0-next.4+sha-2c35f88`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
