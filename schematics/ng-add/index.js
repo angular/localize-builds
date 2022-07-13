cbffe67a422183cd16201d616bbfe600553e9935
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-13 16:50:34 +0000

    336598aedc refactor(docs-infra): update boilerplate to have proper `angular.js` types (#46819)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-next.4+sha-58d2555`
+                version: `~14.1.0-next.4+sha-336598a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
