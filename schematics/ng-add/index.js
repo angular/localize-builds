2a8edf19d139755777df76a5f1dacd5a5be8552f
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-18 20:14:55 +0000

    6b8e60c06a fix(compiler-cli): improve the missingControlFlowDirective message (#46846)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-rc.0+sha-5004b6e`
+                version: `~14.1.0-rc.0+sha-6b8e60c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
