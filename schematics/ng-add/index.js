1ed24366c4b97c88925840a69fded009aa37256d
Jessica Janiuk <jessicajaniuk@google.com>
2022-07-19 16:22:25 +0000

    0e3a2759b0 build: update github/codeql-action action to v2.1.16 (#46885)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.0+sha-6479cdb`
+                version: `~14.2.0-next.0+sha-0e3a275`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
