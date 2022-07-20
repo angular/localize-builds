25e8e288ca2beb2b8f3d4586c36e1212abbdd40e
Andrew Kushnir <akushnir@google.com>
2022-07-20 18:23:19 +0000

    a8e9247e6a refactor(platform-server): make `TransferState` standalone-friendly (#46879)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0+sha-793326d`
+                version: `~14.1.0+sha-a8e9247`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
