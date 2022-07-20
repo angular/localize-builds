9452fd36c0e24d28ce6ee328ae3e77216bb6fe05
Andrew Kushnir <akushnir@google.com>
2022-07-20 16:08:43 +0000

    10d46b4247 refactor(bazel): remove `experimental_extended_template_diagnostics` flag (#46898)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0-rc.0+sha-b431b5c`
+                version: `~14.1.0-rc.0+sha-10d46b4`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
