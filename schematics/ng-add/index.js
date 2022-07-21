f82c2a54d1aca965886f2169005e7188f68ef62a
Pawel Kozlowski <pkozlowski@google.com>
2022-07-21 09:42:11 +0000

    10289f1f6e feat(router): expose resolved route title (#46826)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.0+sha-76790a6`
+                version: `~14.2.0-next.0+sha-10289f1`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
