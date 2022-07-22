d92ca08faf83b8ced49c5e8cc3a3cea5fb283b21
Pawel Kozlowski <pkozlowski@google.com>
2022-07-22 08:43:41 +0000

    1eb77a2f4f docs: add simona cotin to contributors (#46922)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0+sha-55f485b`
+                version: `~14.1.0+sha-1eb77a2`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
