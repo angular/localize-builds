6567a98fa480b68bd511e0864d99442ce9b1a288
Andrew Kushnir <akushnir@google.com>
2022-08-01 16:48:22 +0000

    0a5abf3093 docs: modify callout in TOH part 2 (#46964) PR Close #46964

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.0+sha-bfddfe5`
+                version: `~14.2.0-next.0+sha-0a5abf3`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
