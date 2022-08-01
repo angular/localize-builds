a18708c7497b0e05b104fb83e73ec7a113a10f15
Andrew Kushnir <akushnir@google.com>
2022-08-01 17:03:17 +0000

    996071035b build: update eslint dependencies to v5.31.0 (#46968)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.0+sha-d6899d5`
+                version: `~14.2.0-next.0+sha-9960710`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
