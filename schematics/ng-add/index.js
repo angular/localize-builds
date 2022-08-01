13cc1dbb8831f870ee97147715de993bde1a4156
Andrew Kushnir <akushnir@google.com>
2022-08-01 17:10:25 +0000

    8cd95d4901 refactor(zone.js): remove leftover debugging code using `Error.stack` (#46989)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0+sha-ff7c716`
+                version: `~14.1.0+sha-8cd95d4`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
