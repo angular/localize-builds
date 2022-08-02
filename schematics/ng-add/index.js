5a9e68595cbfa65741e253fc3bf977195bf95008
Andrew Kushnir <akushnir@google.com>
2022-08-02 16:59:04 +0000

    d8cf78ba5e fix(router): Do not call preload method when not necessary (#47007)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.0+sha-b757b13`
+                version: `~14.2.0-next.0+sha-d8cf78b`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
