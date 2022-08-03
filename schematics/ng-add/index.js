64e1fb743ff921bbf3439297ecd9b931eb566024
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2022-08-03 19:55:41 +0000

    6174151cb7 feat(common): define public API surface for NgOptimizedImage directive

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.0+sha-cb0fe50`
+                version: `~14.2.0-next.0+sha-6174151`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
