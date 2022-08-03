0817e8a28cc645e35b7ddfe73ccc60bd6156dac9
Andrew Kushnir <akushnir@google.com>
2022-08-03 18:22:16 +0000

    3ee883e417 fix(docs-infra): do  not require `@ngModule` tag on standalone types (#47024)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.0+sha-efb8e3e`
+                version: `~14.1.1+sha-3ee883e`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
