8ca0cced0cfeebc01364b16be9ab5aa442e0ea41
Dylan Hunn <dylhunn@gmail.com>
2022-08-04 00:53:09 +0000

    ed15427f19 docs: update Vale installation instructions (#47031)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.0+sha-f42a7c5`
+                version: `~14.2.0-next.0+sha-ed15427`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
