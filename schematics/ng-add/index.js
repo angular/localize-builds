c1dfe3cffd1b52a957f262feac7216c373a97fde
Andrew Scott <atscott01@gmail.com>
2022-08-05 17:22:52 +0000

    0920a15139 docs: update dependency injection content (#44466)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.0+sha-c1ad37d`
+                version: `~14.2.0-next.0+sha-0920a15`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
