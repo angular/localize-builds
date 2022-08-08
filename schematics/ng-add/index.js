ba84e528b9f7d5b6988234339b5b510080870cd0
Dylan Hunn <dylhunn@gmail.com>
2022-08-08 18:51:23 +0000

    340fa465ce docs: add more documentation about lint messages (#47019)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.0+sha-621c388`
+                version: `~14.2.0-next.0+sha-340fa46`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
