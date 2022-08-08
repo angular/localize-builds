c25b29fc28d7b377a77c9f8c748ac4fdc27bc9c1
Dylan Hunn <dylhunn@gmail.com>
2022-08-08 16:44:21 +0000

    d14235cfb1 ci: invalidate circleci caches to prune nested `node_modules` (#46707)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.1+sha-387f4a7`
+                version: `~14.1.1+sha-d14235c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
