50edd3ae301006428453300e0b5872e5c0ccf97c
Dylan Hunn <dylhunn@gmail.com>
2022-08-08 18:50:42 +0000

    951d02e1de docs: fix dependecy injection documentation page typos (#47057)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.0+sha-7343ec7`
+                version: `~14.2.0-next.0+sha-951d02e`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
