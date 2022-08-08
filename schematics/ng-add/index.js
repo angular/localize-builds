9a276ebec4b7be1520fa8ab60cd93c494be78494
Dylan Hunn <dylhunn@gmail.com>
2022-08-08 16:41:15 +0000

    3ef4d8a690 refactor(core): drop unused animation-related types (#47054)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.0+sha-561cd84`
+                version: `~14.2.0-next.0+sha-3ef4d8a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
