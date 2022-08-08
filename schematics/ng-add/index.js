81ab950d8501d6427258d5d8cec19da61df08fbe
Dylan Hunn <dylhunn@gmail.com>
2022-08-08 18:48:59 +0000

    4ae8b2e12c docs: fix text formatting for paragraph in Hierarchical injectors (#47067)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.0+sha-7f99232`
+                version: `~14.2.0-next.0+sha-4ae8b2e`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
