39b8bdbd79df133475ad119539abf4512d859586
Dylan Hunn <dylhunn@gmail.com>
2022-08-10 00:20:05 +0000

    91954cf20e fix(zone.js): Fix ConsoleTask interface typo (#47090)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.0+sha-8546b17`
+                version: `~14.2.0-next.0+sha-91954cf`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
