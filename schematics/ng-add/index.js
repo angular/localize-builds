a9d1107212beb8c760e3a72c4ef775f5d8d6f11d
Pawel Kozlowski <pkozlowski@google.com>
2022-08-11 18:26:05 +0000

    063035f86d docs: fix embedded view description issue (#46957)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.2+sha-0a99130`
+                version: `~14.1.2+sha-063035f`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
