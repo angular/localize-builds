e9e6e4e23ee05c28bac9073a8533b6e9be51ea18
Pawel Kozlowski <pkozlowski@google.com>
2022-08-11 16:28:13 +0000

    6a94157d6f docs: Moving a div down in tutorial toh-pt6.md (#47095)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.2+sha-b31f5dd`
+                version: `~14.1.2+sha-6a94157`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
