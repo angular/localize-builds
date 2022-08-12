013e600899cd81c5bf0e3550151752ebf5db3e4c
Pawel Kozlowski <pkozlowski@google.com>
2022-08-12 18:34:51 +0000

    970e68ae38 docs: remove duplicated styles (#47143)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.2+sha-a5ca049`
+                version: `~14.1.2+sha-970e68a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
