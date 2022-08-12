55dffc71765d319bbf05b0b8685f7ba7d6981c36
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-08-12 16:21:40 +0000

    757684b705 docs: set titles to sentence case (#47083)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.1+sha-422323c`
+                version: `~14.2.0-next.1+sha-757684b`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
