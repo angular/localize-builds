30ff31d9753429051fadf4c84d61b7cd8eae312f
Pawel Kozlowski <pkozlowski@google.com>
2022-08-16 16:21:08 +0000

    31429eaccc feat(core): support TypeScript 4.8 (#47038)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.1+sha-75df404`
+                version: `~14.2.0-next.1+sha-31429ea`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
