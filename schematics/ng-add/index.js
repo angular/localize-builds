a78ca7b59b855d7b9cdb18a1f3662658337d2712
Pawel Kozlowski <pkozlowski@google.com>
2022-08-17 07:51:35 +0000

    dc52cef26b fix(compiler): infinite loop in parser assignment expression with invalid left-hand expression (#47151)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-next.1+sha-0ca5eb3`
+                version: `~14.2.0-next.1+sha-dc52cef`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
