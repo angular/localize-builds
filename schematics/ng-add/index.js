9a94ba8a8cffcd73a1ba88a78c184b9af67bb5f9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-18 19:40:30 +0000

    591068afc7 refactor(compiler): /a/.exec() returns RegExpExecArray (#47169)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-rc.0+sha-c83e861`
+                version: `~14.2.0-rc.0+sha-591068a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
