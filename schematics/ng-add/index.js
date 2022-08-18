00658c2851a35a352cfb1698ac4f2f6c3b784146
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-18 21:00:35 +0000

    d14603fdb5 refactor(core): remove obsolete `data:` url validation (#45860)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.0-rc.0+sha-591068a`
+                version: `~14.2.0-rc.0+sha-d14603f`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
