d8cb55006f292f5727331a2993f67618058d66be
Joey Perrott <josephperrott@gmail.com>
2022-08-22 13:22:06 +0000

    107d094083 fix(docs-infra): avoid distorting contributor images (#47215)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.3+sha-9b0fd07`
+                version: `~14.1.3+sha-107d094`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
