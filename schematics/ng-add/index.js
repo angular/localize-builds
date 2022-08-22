3958c40b4233bc637044ea7ad55a8f4049978ca2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 22:11:34 +0000

    791fa9aa53 Revert "fix(animations): fix stagger timing not handling params (#47208)" (#47222)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.1.3+sha-baaf1d1`
+                version: `~14.1.3+sha-791fa9a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
