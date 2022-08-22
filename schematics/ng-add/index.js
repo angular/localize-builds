d0b0e87011baa46ccef73fd2416a8f2ad8cffb0a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 18:12:45 +0000

    2095a08781 docs: fix grammar issues in various markdown files (#47200)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.3.0-next.0+sha-05f5e8a`
+                version: `~14.3.0-next.0+sha-2095a08`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
