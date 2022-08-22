de1407df7ddbe0476b5e20611bbd60745dd42c73
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-22 18:07:49 +0000

    b3879dbf14 fix(common): support density descriptors with 2+ decimals (#47197)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.3.0-next.0+sha-634e6c7`
+                version: `~14.3.0-next.0+sha-b3879db`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
