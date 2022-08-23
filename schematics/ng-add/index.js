fb6f0e5a116f0f1acc80ccc15a59d14a857f2fbb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-23 16:40:29 +0000

    4780f1bf26 ci: do not match dev-infra group for pullapprove owner changes (#47216)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.3.0-next.0+sha-c5b3f1c`
+                version: `~14.3.0-next.0+sha-4780f1b`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
