7cd0bfff64bb99b1061bcd6062cc07e1f7618b0a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-24 19:39:26 +0000

    0afb895f41 fix(common): remove duplicate deepForEach (#47189)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.3.0-next.0+sha-a946ca9`
+                version: `~14.3.0-next.0+sha-0afb895`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
