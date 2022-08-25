b07c5dd63a0dbe539a749d6d46ebc7bfdc9ec083
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-08-25 21:50:47 +0000

    d03742301b docs: release notes for the v14.2.0 release

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.3.0-next.0+sha-3d7deab`
+                version: `~14.3.0-next.0+sha-d037423`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
