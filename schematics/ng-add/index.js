54a1863124fd054286d4a3c4b06bc3ebcf5ed6c8
Andrew Kushnir <akushnir@google.com>
2022-09-06 18:54:43 +0000

    16f96eeabf refactor(compiler-cli): remove `enableIvy` options (#47346)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.0+sha-1edb8e5`
+                version: `~15.0.0-next.0+sha-16f96ee`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
