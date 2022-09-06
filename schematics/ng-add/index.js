05f29b437035730bb6900b7bc19675ed78d3c6af
Andrew Kushnir <akushnir@google.com>
2022-09-06 16:31:51 +0000

    b8fd0da47f docs(docs-infra): improve docs on updating angular.io and docs examples apps (#47250)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.0+sha-28f277e`
+                version: `~15.0.0-next.0+sha-b8fd0da`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
