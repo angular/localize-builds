bbb85e5b22967e91bb5159c03f2f2125562c3360
Andrew Kushnir <akushnir@google.com>
2022-09-06 17:20:40 +0000

    a0c3326485 fix(service-worker): only consider GET requests as navigation requests (#47263)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.0+sha-b9f80d2`
+                version: `~15.0.0-next.0+sha-a0c3326`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
