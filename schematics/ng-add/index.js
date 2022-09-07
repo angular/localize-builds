3ef545cb1ca9a71de5978535eeb788ceb3307196
Andrew Kushnir <akushnir@google.com>
2022-09-07 18:07:01 +0000

    6dec750e4b docs: fix linter errors for accessibility page (#47327)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.1+sha-25c4e4c`
+                version: `~15.0.0-next.1+sha-6dec750`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
