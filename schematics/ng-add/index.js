a95d58ef0eafce8b16b23d17ed564afa2e781050
Dylan Hunn <dylhunn@gmail.com>
2022-09-08 16:51:54 +0000

    bd66f49261 docs: updates es version based on issue #47335 (#47379)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.1+sha-6dec750`
+                version: `~15.0.0-next.1+sha-bd66f49`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
