fb232517f3e19b2ee4c972e6cef3b189f2b6be7a
Dylan Hunn <dylhunn@gmail.com>
2022-09-09 21:48:01 +0000

    a61d553d73 refactor(common): drop unnecessary srcset sanitization (#47302)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.1+sha-403b970`
+                version: `~14.2.1+sha-a61d553`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
