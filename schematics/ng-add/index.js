305081ad0ecb15aa130e82b0615016738dd12f49
Dylan Hunn <dylhunn@gmail.com>
2022-09-12 20:43:17 +0000

    f0f809c561 docs: replace deprecated --outFile option for ng extract-i18n (#47397)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.1+sha-0d4594a`
+                version: `~15.0.0-next.1+sha-f0f809c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
