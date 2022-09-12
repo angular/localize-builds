6f22c9a7b6cc3c6a0b3782e1705054f2eadc3234
Dylan Hunn <dylhunn@gmail.com>
2022-09-12 20:42:18 +0000

    0d4594a53e docs: fix angle brackets in details code block (#47400)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.1+sha-c9241fd`
+                version: `~15.0.0-next.1+sha-0d4594a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
