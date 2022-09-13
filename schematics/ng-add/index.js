fe1e93acc86c444e3d8d8acba8b43075d494e881
Dylan Hunn <dylhunn@gmail.com>
2022-09-13 02:24:05 +0000

    bb06432948 refactor(platform-server): add a marker to specify how a page was rendered (#47103)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.1+sha-4b7b1ae`
+                version: `~14.2.1+sha-bb06432`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
