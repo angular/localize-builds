2612fd6e8c385624d61c4f00b3c09d7f9d68d001
Dylan Hunn <dylhunn@gmail.com>
2022-09-15 16:40:44 +0000

    deaa05a393 refactor(core): generate initial inputs from inputs map (#47228)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.2+sha-8deedc5`
+                version: `~15.0.0-next.2+sha-deaa05a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
