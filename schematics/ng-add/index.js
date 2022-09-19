9c8496a7de9bd6b5a31fdc38c5f5d177386d08d6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 08:00:58 +0000

    3f4cbe4ca7 docs: update contribution docs for serving a local library/project (#47421)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.2+sha-db3b532`
+                version: `~14.2.2+sha-3f4cbe4`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
