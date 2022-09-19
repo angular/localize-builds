1cbd3decde12a78323f148f33723794de6ee519a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-19 11:33:55 +0000

    e563139d30 fix(docs-infra): apply `text-top` instead of `top` for table alignment (#47457)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.2+sha-3f4cbe4`
+                version: `~14.2.2+sha-e563139`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
