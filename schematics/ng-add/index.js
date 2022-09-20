9e4fa48a99b5c4753ae127bf4aa83bd16063b0a2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-20 08:30:35 +0000

    bba2dae812 fix(animations): make sure that the useAnimation function delay is applied (#47468)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.2+sha-fe6d33c`
+                version: `~14.2.2+sha-bba2dae`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
