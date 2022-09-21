291cf4cc5763f265e4a0a5a571de52851adc76db
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-09-21 16:42:14 +0000

    8fcadaad48 perf(compiler-cli): cache source file for reporting type-checking diagnostics (#47471)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.2+sha-8cef5dd`
+                version: `~15.0.0-next.2+sha-8fcadaa`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
