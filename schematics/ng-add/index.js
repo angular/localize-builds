0eb2d1094d3195bdd15ba9be9726c2b34f25f80c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-23 21:18:42 +0000

    aef8d0bb02 fix(core): correctly check for `typeof` of undefined in `ngDevMode` check (#47480)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.3+sha-ddef448`
+                version: `~15.0.0-next.3+sha-aef8d0b`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
