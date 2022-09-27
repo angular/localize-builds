6eeedc7cc31fb1b75b1885367e123848ae40fc8b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-27 17:34:06 +0000

    e7ee53c541 feat(language-service): support to fix invalid banana in box (#47393)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.3+sha-120555a`
+                version: `~15.0.0-next.3+sha-e7ee53c`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
