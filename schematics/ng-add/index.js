e04905903b90fb31d9b612d434d02d3152ab7472
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-27 18:11:56 +0000

    002ee3226c refactor(core): support applying directives to the root component (#47530)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.3+sha-6a88bad`
+                version: `~15.0.0-next.3+sha-002ee32`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
