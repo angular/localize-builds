e00cda7fdfe0569e75a55067e8a7c772c473dff2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-09-27 17:36:40 +0000

    10a3cefdb4 docs: update broken link (#47532) PR Close #47532

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.3+sha-2c3e2d6`
+                version: `~14.2.3+sha-10a3cef`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
