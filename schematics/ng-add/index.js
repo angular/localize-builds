390599feb49c8fad4b84fb0e387b6ca03661d418
Andrew Kushnir <akushnir@google.com>
2022-10-03 16:07:05 +0000

    4ba2d3d6b6 docs: add missing link to canMatch guard (#47602)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -96,7 +96,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.4+sha-400a6b5`,
+                version: `~15.0.0-next.4+sha-4ba2d3d`,
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
