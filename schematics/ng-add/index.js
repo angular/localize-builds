5e1a554bf811e34caa1f8fdca5fd8a3142df17a0
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 19:16:50 +0000

    16c8f55663 feat(router): migrate `RouterLinkWithHref` references to `RouterLink` (#47599)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -96,7 +96,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.5+sha-db28bad`,
+                version: `~15.0.0-next.5+sha-16c8f55`,
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
