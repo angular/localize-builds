d6e4de9bc462f2b45886ed0d438a0b322948c227
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 19:22:18 +0000

    c09c1bb033 docs(http): fix copy-paste error in HttpClientJsonpModule docs (#47502)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -96,7 +96,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.5+sha-16c8f55`,
+                version: `~15.0.0-next.5+sha-c09c1bb`,
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
