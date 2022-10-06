90f7422e4a20c70560f698961f5ef7c1a0ab38e7
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-06 23:43:46 +0000

    4620fbdf04 refactor(docs-infra): add new Input for better description of close button in notification component (#47681)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.5+sha-e3ef401`
+                version: `~14.2.5+sha-4620fbd`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
