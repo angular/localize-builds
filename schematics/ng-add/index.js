35ba168ad148222286a1cd7b674346c318c7841b
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-07 16:51:51 +0000

    9e1465849e build: setup remote execution through shared command (#47537)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.5+sha-4620fbd`
+                version: `~14.2.5+sha-9e14658`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
