9ab10745a87a2bead35bc22639b0e7aa02ca3c52
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-07 18:00:53 +0000

    cfd0cdbd9b docs: remove assorted lint errors (#47693)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.5+sha-9e14658`
+                version: `~14.2.5+sha-cfd0cdb`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
