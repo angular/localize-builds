0e7fcc2189496b9ab57613f5d8f3984fa71aad59
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-07 22:30:11 +0000

    07017a7bd3 feat(router): prevent `provideRouter()` from usage in @Component (#47669)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -96,7 +96,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.5+sha-deb4cab`,
+                version: `~15.0.0-next.5+sha-07017a7`,
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
