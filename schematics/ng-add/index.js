f93e982d67fabb69241def07c372c4697c195c40
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 22:25:38 +0000

    6c9b5a2557 docs: fix linter errors for security.md (#47688)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.5+sha-2fd893d`
+                version: `~14.2.5+sha-6c9b5a2`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
