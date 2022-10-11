153dc711710154909a7c846298a8451ab9fa4269
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-11 01:32:32 +0000

    1b602faee9 Revert "fix(compiler): update element schema" (#47726)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -96,7 +96,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.5+sha-ca2fa7a`,
+                version: `~15.0.0-next.5+sha-1b602fa`,
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
