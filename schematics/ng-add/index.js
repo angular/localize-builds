274f3839c859b368dc532a47ef85ab17ac951b9c
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-12 00:02:07 +0000

    b82da42725 docs: add Joshua Morony to GDE resources (#47704)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.5+sha-b9a0396`
+                version: `~14.2.5+sha-b82da42`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
