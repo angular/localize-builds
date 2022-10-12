f8a814307556a5acde33d93bc566f86b0d01fed7
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-12 16:18:34 +0000

    3fd176a905 fix(compiler-cli): add missing period to error message (#47744)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.5+sha-b82da42`
+                version: `~14.2.5+sha-3fd176a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
