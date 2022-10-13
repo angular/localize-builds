635e7b73b2167e0afafb2bb9cbf5aeef99b4b35c
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-13 16:56:36 +0000

    2ca213747c docs: update of documentation contrib. guide (#47381)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.6+sha-24950dd`
+                version: `~14.2.6+sha-2ca2137`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
