169bb0434d34bf77002dec79373c4c6e599ccc26
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-13 19:30:33 +0000

    0457b58dfe docs: Separated HTML elements with blank lines (#47331)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -96,7 +96,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.6+sha-e13d97c`,
+                version: `~15.0.0-next.6+sha-0457b58`,
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
