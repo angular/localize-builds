6f31f51ed273fff962dc4b5ed5e2633bbe4fd6f9
Jessica Janiuk <jessicajaniuk@google.com>
2022-10-13 21:13:19 +0000

    874f3fc4d3 refactor(compiler-cli): use mkdirSync recursive option instead of custom implementation (#47678)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -96,7 +96,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~15.0.0-next.6+sha-0457b58`,
+                version: `~15.0.0-next.6+sha-874f3fc`,
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
