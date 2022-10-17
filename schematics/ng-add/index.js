332185a664ad1c093dcc822ad9e82aad74df5331
Dylan Hunn <dylhunn@gmail.com>
2022-10-17 08:16:58 +0000

    46d33dd206 docs(docs-infra): fix errors page hover styles (#47770)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.6+sha-2e7e402`
+                version: `~14.2.6+sha-46d33dd`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
