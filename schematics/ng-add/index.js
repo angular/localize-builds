c07c47232e08a7a52b31c5efad1f70789f0060df
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 21:33:08 +0000

    af8afee5bd fix(router): Delay router scroll event until navigated components have rendered (#47563)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.0+sha-38ec156`,
+            version: `~15.1.0-next.0+sha-af8afee`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
