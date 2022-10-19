b6f65f2887fcb6e3c96aae334eb66b8180e2adfc
Dylan Hunn <dylhunn@gmail.com>
2022-10-19 13:44:33 +0000

    b31091b222 docs(common): update image directive docs (#47796)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.0+sha-a471684`,
+            version: `~15.1.0-next.0+sha-b31091b`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
