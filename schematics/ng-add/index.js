ca13afeddb73bac693e5d29f9f9d0a61d088a25d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-21 15:53:08 +0000

    a42c8cfc67 docs: fix minor typo in hierarchical-dependency-injection.md (#47812)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.7+sha-4d18ba1`
+                version: `~14.2.7+sha-a42c8cf`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
