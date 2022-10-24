f23ea3afcbdc08736ea58f8536d2f057a1a7c6e3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-24 09:02:21 +0000

    8161b98069 build: update recommended launch vscode config to use proper bazel dist path (#47831)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~14.2.7+sha-7c4f77a`
+                version: `~14.2.7+sha-8161b98`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
