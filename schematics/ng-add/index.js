9699c4ac219351841ece9e9e4df6cd6fac534676
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-24 12:36:45 +0000

    a99d9d67f3 fix(forms): don't mutate validators array (#47830)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.0-rc.0+sha-6471eaf`,
+            version: `~15.0.0-rc.0+sha-a99d9d6`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
