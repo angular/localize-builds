e1109dd7d8947c613d916e96ab27ab179fc0a561
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-25 13:10:53 +0000

    2908eba59c fix(platform-server): align server renderer interface with base renderer (#47868)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.0-rc.0+sha-a8ce8f7`,
+            version: `~15.0.0-rc.0+sha-2908eba`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
