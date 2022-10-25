d478ea4d3aa1249ef996fb39fb12faf139c0d95a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-25 20:15:08 +0000

    df36537bfd build: update babel dependencies (#47765)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.0-rc.0+sha-ed48887`,
+            version: `~15.0.0-rc.0+sha-df36537`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
