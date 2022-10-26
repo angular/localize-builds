cf4c084a1423a308f99c8f86bff3f44ae26b0f57
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-26 14:01:18 +0000

    d75e3da429 build: update bazel setup to v5.7.0 (#47436)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.0-rc.0+sha-ebd2ddb`,
+            version: `~15.0.0-rc.0+sha-d75e3da`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
