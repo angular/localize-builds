3d34e3905a9ab01a7362c27720367079f62d09bc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2022-10-27 07:48:47 +0000

    96c0e42e61 fix(core): allow readonly arrays for standalone imports (#47851)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.0-rc.1+sha-f24da6b`,
+            version: `~15.0.0-rc.1+sha-96c0e42`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
