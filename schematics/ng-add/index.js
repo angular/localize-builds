ebca7703da87bf6b3cfaab908a14c1e750bcbac9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2022-11-01 12:03:39 +0000

    e63574774e docs: update roadmap as of q4 2022 (#47910)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.0-rc.1+sha-623602f`,
+            version: `~15.0.0-rc.1+sha-e635747`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
