de4f5e832a910c29a2970aa3288dbd45b1118c8a
Andrew Kushnir <akushnir@google.com>
2022-11-03 18:38:10 +0000

    13b863a1bf Revert "fix(core): hardening rules related to the attribute order on iframe elements (#47935)" (#47959)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.0+sha-2d08965`,
+            version: `~15.1.0-next.0+sha-13b863a`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
