0fd9d0a01ec60e493def427124002ca6c7d4a938
Andrew Kushnir <akushnir@google.com>
2022-11-08 18:54:18 +0000

    6438f69f28 docs(common): Merge two ngOptimizedImage docs into one (#47990)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.0+sha-8e6ec72`,
+            version: `~15.1.0-next.0+sha-6438f69`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
