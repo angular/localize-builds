a1ea2b7e7830b07d687a53c85f365976c6e03f61
Andrew Kushnir <akushnir@google.com>
2022-11-09 18:12:02 +0000

    046ce43388 fix(core): add` zone.js` version `0.12.x` as a valid peer dependency (#48002)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.0+sha-2d8d562`,
+            version: `~15.1.0-next.0+sha-046ce43`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
