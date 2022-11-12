02e67f593ccf5ea2956db84131162081d48e26a1
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-12 00:04:08 +0000

    954f700ab4 docs(common): Refine NgOptimizedImage docs (#48035)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.0+sha-ec8b52a`,
+            version: `~15.1.0-next.0+sha-954f700`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
