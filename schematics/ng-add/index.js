e110a1bf3edd1802f56404de29441dc7c8e54401
Jessica Janiuk <jessicajaniuk@google.com>
2022-11-14 17:40:30 +0000

    a1b6610c3d test(common): add e2e tests for the fill mode checks in the NgOptimizedImage directive (#48036)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.0-rc.3+sha-629ebad`,
+            version: `~15.0.0-rc.3+sha-a1b6610`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
