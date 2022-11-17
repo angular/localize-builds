420d383855071ea7ceabd5b206e2ada61571c3f8
Dylan Hunn <dylhunn@gmail.com>
2022-11-17 17:54:26 +0000

    f8f8928210 fix(devtools): prevent devTools to load when not text/html document (#48021)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.0+sha-779a76f`,
+            version: `~15.1.0-next.0+sha-f8f8928`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
