1ddad4362dff3e0768a833403162c5b84cb4d195
Dylan Hunn <dylhunn@gmail.com>
2022-11-18 20:34:36 +0000

    8d81fcbe20 build: add labeled trigger for assistant-to-the-branch-manager (#48119)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.0+sha-57d9296`,
+            version: `~15.1.0-next.0+sha-8d81fcb`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
