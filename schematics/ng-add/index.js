3212d9c6cb2fb9fe4b57653c9c3c8e5975fe1ae8
Dylan Hunn <dylhunn@gmail.com>
2022-11-21 20:40:02 +0000

    4ea399a6d7 docs(core): add an error details page for unsafe <iframe> bindings (#48027) (#48029)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -100,7 +100,7 @@
             (0, dependencies_1.addPackageJsonDependency)(host, {
                 name: '@angular/localize',
                 type: dependencies_1.NodeDependencyType.Default,
-                version: `~13.3.11+sha-3b91101`
+                version: `~13.3.11+sha-4ea399a`
             });
             // Add a task to run the package manager. This is necessary because we updated
             // "package.json" and we want lock files to reflect this.
