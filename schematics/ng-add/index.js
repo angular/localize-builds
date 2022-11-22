b80cf97ac0de70867b397098dbeae2150f62e81e
Dylan Hunn <dylhunn@gmail.com>
2022-11-22 19:58:13 +0000

    8ab50a13d7 docs: fix broken link in v15 deprecations table (#48157)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.0+sha-cc8b76e`,
+            version: `~15.0.0+sha-8ab50a1`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
