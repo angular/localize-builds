e91d067342455141969ca12ba3389b7cb1e0bc92
Joey Perrott <josephperrott@gmail.com>
2022-11-23 14:18:11 +0000

    6948f0f0f3 build(bazel): fix AIO deployment script (#48188)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.0+sha-332461b`,
+            version: `~15.1.0-next.0+sha-6948f0f`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
