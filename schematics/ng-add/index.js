675d9af9f34d500eba845cb3b2bc9bb0dd628972
Andrew Scott <atscott01@gmail.com>
2022-11-28 20:13:31 +0000

    ea114a0034 refactor(router): Move navigationId handling to the transition manager (#48257)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.1+sha-458d62b`,
+            version: `~15.0.1+sha-ea114a0`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
