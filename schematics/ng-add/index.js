4b9fecfa0f411e8561c73aa78916f12bbbbc3b0b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-04 16:33:13 +0000

    38701ce07b build: check out project before setting AIO preview targets (#48347)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.2+sha-09388eb`,
+            version: `~15.0.2+sha-38701ce`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
