1496725292c010f25e44db8961c3771473505c41
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-05 11:59:59 +0000

    d2dcf95467 build(bazel): set ci steps that build aio to 2xlarge+ (#48342)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.1+sha-192df71`,
+            version: `~15.1.0-next.1+sha-d2dcf95`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
