3685bb521d3e29e61e5648ff572464df96951e7d
Andrew Kushnir <akushnir@google.com>
2022-12-05 20:23:49 +0000

    0beeee4304 build: add more debug information for occasional windows flakiness (#48351)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.1+sha-d2dcf95`,
+            version: `~15.1.0-next.1+sha-0beeee4`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
