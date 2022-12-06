6debcba3714c04047d7e9a65f8d40f139f84640f
Andrew Kushnir <akushnir@google.com>
2022-12-06 18:38:16 +0000

    39b6cbcf4b build: update dependency karma-jasmine-html-reporter to v2 (#48369)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.2+sha-40d6a06`,
+            version: `~15.0.2+sha-39b6cbc`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
