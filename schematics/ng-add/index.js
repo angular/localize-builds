beaef8428bc3d07f06076b7dfd3627411dff2ed0
Andrew Kushnir <akushnir@google.com>
2022-12-06 21:11:02 +0000

    c0d04175ff refactor(core): simplify array flatten logic (#48358)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.2+sha-5e2cd07`,
+            version: `~15.0.2+sha-c0d0417`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
