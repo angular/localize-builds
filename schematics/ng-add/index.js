f2c98470a31acedf2845b53e293af06ce31afe56
Andrew Kushnir <akushnir@google.com>
2022-12-06 00:16:50 +0000

    ac51e5c107 build: update dependency jasmine-core to v4 (#48366)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.2+sha-45cc5f4`,
+            version: `~15.0.2+sha-ac51e5c`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
