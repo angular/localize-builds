672c5c648368ef9fa4692f1aa689266deb02e8dd
Andrew Kushnir <akushnir@google.com>
2022-12-07 17:46:26 +0000

    bf44dc234a fix(common): Update `Location` to support base href containing `origin` (#48327)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.2+sha-c0d0417`,
+            version: `~15.0.2+sha-bf44dc2`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
