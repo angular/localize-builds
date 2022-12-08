fc151ae1981a42cb905fdd6f01e42763b198d457
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-08 23:58:57 +0000

    e7707dbf56 docs: move marketing demo out of tested examples (#48404)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.3+sha-5af7ec2`,
+            version: `~15.0.3+sha-e7707db`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
