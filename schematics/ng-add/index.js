45ca24c6cead952f5e53dc48294e839dea35c455
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-09 21:37:04 +0000

    126e798edf docs(common): Add note to location about investigating base href handling (#48422)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.3+sha-85d3f6f`,
+            version: `~15.0.3+sha-126e798`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
