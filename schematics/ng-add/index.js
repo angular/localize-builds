8f27884b39cd11859103cb44f04e1ab706b1f532
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-10 00:23:20 +0000

    6279fecd52 docs: update of landing page and backup pages (#47047)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.0.3+sha-c4d17b9`,
+            version: `~15.0.3+sha-6279fec`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
