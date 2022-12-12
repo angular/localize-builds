cf9ddd3b93c2c3fa5344f0c663f42d568cddb3c7
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-12 22:08:24 +0000

    febfa15c2f docs: make "app" a suggested replacement for "application" (#48456)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.2+sha-a6d953e`,
+            version: `~15.1.0-next.2+sha-febfa15`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
