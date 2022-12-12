4cf54e8d3d6adbf857d3a4277844a024f8649f29
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-12 18:03:45 +0000

    caa1ad000e docs(router): Update ROUTES docs to not point to provideRoutes (#48417)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.2+sha-e362214`,
+            version: `~15.1.0-next.2+sha-caa1ad0`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
