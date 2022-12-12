8575f5c2ca1d5ba90c83603b4ade5610213473e1
Jessica Janiuk <jessicajaniuk@google.com>
2022-12-12 17:58:17 +0000

    10b1c2d1a6 docs: updated docs removed unnecessary imports and empty methods (#48428)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.2+sha-b5bb4c1`,
+            version: `~15.1.0-next.2+sha-10b1c2d`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
