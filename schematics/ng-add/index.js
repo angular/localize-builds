e682cb8b2f0ba5eb191d69ddc35afdfe1a06b1b3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-15 15:40:54 +0000

    e59406f5e7 build: enable new merge ready status action (#48494)

--- schematics/ng-add/index.js
+++ schematics/ng-add/index.js
@@ -76,7 +76,7 @@
         (0, dependencies_1.addPackageJsonDependency)(host, {
             name: '@angular/localize',
             type: dependencies_1.NodeDependencyType.Default,
-            version: `~15.1.0-next.3+sha-63debcc`,
+            version: `~15.1.0-next.3+sha-e59406f`,
         });
         // Add a task to run the package manager. This is necessary because we updated
         // "package.json" and we want lock files to reflect this.
