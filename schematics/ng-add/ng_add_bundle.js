55cc9d79a612f5ccf2f020140209ece144444f1c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2022-12-24 11:44:58 +0000

    27da7338ea ci: remove redundant aio dependencies installation step (#48574)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -96,7 +96,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.0-next.3+sha-74ba302`
+    version: `~15.1.0-next.3+sha-27da733`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
