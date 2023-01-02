c5838e704ad3992c41ef342de808470510897141
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-02 12:38:07 +0000

    d7ff276bce docs: hide state of js survey (#48578)

--- fesm2015/init.mjs
+++ fesm2015/init.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v15.0.4+sha-125b3df
+ * @license Angular v15.0.4+sha-d7ff276
  * (c) 2010-2022 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -7,13 +7,6 @@
 import { ɵ_global, ɵ$localize } from '@angular/localize';
 export { ɵ$localize as $localize } from '@angular/localize';
 
-/**
- * @license
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
 // Attach $localize to the global context, as a side-effect of this module.
 ɵ_global.$localize = ɵ$localize;
 //# sourceMappingURL=init.mjs.map
