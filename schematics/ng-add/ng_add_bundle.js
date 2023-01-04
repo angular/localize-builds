435f221b2b87711d928438633df6dd1560d2b2b6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 20:45:51 +0000

    2d3c98ddf3 docs: missing closing parenthesis in withHashLocation() example (#48507)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.0-next.3+sha-33f35b0`
+    version: `~15.1.0-next.3+sha-2d3c98d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
