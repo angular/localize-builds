23ded3a3043097a374dd033da23d799dae32d52f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 20:36:53 +0000

    2fb6df281b docs: updated docs removed unnecessary imports and empty methods (#48546)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.0-next.3+sha-c51e3e3`
+    version: `~15.1.0-next.3+sha-2fb6df2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
