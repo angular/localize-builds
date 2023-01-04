8f7d9fd57195d59629488d519715abe2576510f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 20:50:00 +0000

    fc1c03e16e refactor(compiler): Remove unnecessary assignment (#48478)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.0-next.3+sha-2d3c98d`
+    version: `~15.1.0-next.3+sha-fc1c03e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
