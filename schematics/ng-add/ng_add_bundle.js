e4bf80798f1658ec0f98c3329ccf18de079f642b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 20:25:41 +0000

    b3fca32a79 refactor(common): remove unnecessary parseFloat from ng_optimized_image directive. (#48527)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.0-next.3+sha-687ab04`
+    version: `~15.1.0-next.3+sha-b3fca32`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
