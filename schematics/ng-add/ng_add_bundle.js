24069b6acceb0d83f531f6c3402acbb0a29332d9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-04 20:36:38 +0000

    a532d71975 feat(compiler): allow self-closing tags on custom elements (#48535)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.0-next.3+sha-c8310a8`
+    version: `~15.1.0-next.3+sha-a532d71`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
