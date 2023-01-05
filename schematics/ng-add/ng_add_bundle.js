700662d93f8a053eb57bbea5c01089b45a9acee0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 23:06:55 +0000

    43ced455d6 docs: remove empty constructors & ngOnInit() (#48576)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.0+sha-d887d69`
+    version: `~15.2.0-next.0+sha-43ced45`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
