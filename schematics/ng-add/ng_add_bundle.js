926676689bc622eae799544aaf4bc09224e0e33e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 23:04:49 +0000

    d887d69f24 refactor(common): remove non-null assertions (#48476)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.0+sha-0c5db1a`
+    version: `~15.2.0-next.0+sha-d887d69`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
