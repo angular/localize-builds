aa8db9fa0997d4e14cca53d50beb9f5202320020
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-01-05 16:55:59 +0000

    5d1fbbbf83 ci: fix snapshot publish github credential preparation (#48648)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.0-next.3+sha-fc1c03e`
+    version: `~15.1.0-next.3+sha-5d1fbbb`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
