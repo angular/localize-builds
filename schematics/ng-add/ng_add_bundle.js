d112c39df58a5d26ef4a597666fdf09b27fa1a05
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-05 23:08:00 +0000

    10de92c68a refactor(forms): cleanup type any in forms tests (#48624)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.0.4+sha-c001528`
+    version: `~15.0.4+sha-10de92c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
