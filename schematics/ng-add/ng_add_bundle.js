17244a3717a0104fa05ca950f41f399b0cc9ddd7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-09 18:28:00 +0000

    e615b598ba fix(language-service): ship `/api` entry-point (#48670)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.0-rc.0+sha-8aa8b4b`
+    version: `~15.1.0-rc.0+sha-e615b59`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
