8e8248ecf0f77c6d65b6354a4d202ed3927db3f8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 16:27:15 +0000

    111e2c7876 test(docs-infra): increase jasmine default timeout for e2e specs (#48671)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.0+sha-579cbb2`
+    version: `~15.2.0-next.0+sha-111e2c7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
