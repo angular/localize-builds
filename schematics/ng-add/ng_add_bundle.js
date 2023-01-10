c12f5e95855323c77f12838531d0b97980483836
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 16:35:14 +0000

    d4eaaefe30 fix(docs-infra): correctly read example type (#48665)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.0+sha-111e2c7`
+    version: `~15.2.0-next.0+sha-d4eaaef`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
