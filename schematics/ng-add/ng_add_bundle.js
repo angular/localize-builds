07591c485c930324d75b7512967fdcf91b8bdbfc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-01-10 22:43:15 +0000

    1aef7a8848 ci: expose `ANGULAR_CLI_BUILDS_READONLY_GITHUB_TOKEN` secret as env (#48687)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.0+sha-df77bbc`
+    version: `~15.1.0+sha-1aef7a8`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
