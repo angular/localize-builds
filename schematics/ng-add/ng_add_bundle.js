7e40fdd7d6a21a9e2f23ad603938d5c66f0531a5
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:19:50 +0000

    4dcbb6aef9 refactor(forms): replace type any for the providers (#48647)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.0+sha-926c35f`
+    version: `~15.2.0-next.0+sha-4dcbb6a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
