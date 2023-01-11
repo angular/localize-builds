4b425f38f7b34e848c3ae30b7aebec82644e4cf0
Andrew Kushnir <akushnir@google.com>
2023-01-11 23:18:04 +0000

    926c35f4ac docs: Deprecate class and InjectionToken and resolvers (#47924)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.0+sha-7f68a70`
+    version: `~15.2.0-next.0+sha-926c35f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
