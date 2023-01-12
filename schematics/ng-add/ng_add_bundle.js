c2ac8b95d335be46c9664a32976db4a10efba1ff
Andrew Kushnir <akushnir@google.com>
2023-01-12 20:04:35 +0000

    8802b4aab9 fix(common): Update `Location` to get a normalized URL valid in case a represented URL starts with the substring equals `APP_BASE_HREF` (#48489)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.0+sha-0b57e24`
+    version: `~15.2.0-next.0+sha-8802b4a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
