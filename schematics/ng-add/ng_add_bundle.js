148c4308d198c7728acbaae187b038cb8181c997
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:47:25 +0000

    5b1c7c10a0 build: update dependency @rollup/plugin-commonjs to v24 (#48530)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.0+sha-d47bd48`
+    version: `~15.2.0-next.0+sha-5b1c7c1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
