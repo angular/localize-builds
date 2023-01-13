89d2dbb0da399be3047ed6fa6471ee8123880977
Andrew Kushnir <akushnir@google.com>
2023-01-13 18:58:18 +0000

    5f21c6d627 perf(common): avoid excessive DOM mutation in NgClass (#48433)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.0+sha-5bb1223`
+    version: `~15.2.0-next.0+sha-5f21c6d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
