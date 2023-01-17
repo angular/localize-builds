26efbc7407bbe1b448197aa2fb3ee7b8f7343bdc
Andrew Kushnir <akushnir@google.com>
2023-01-17 17:42:54 +0000

    6686eacb90 docs: Fix example in breaking changes to apply to correct sentence (#48758)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.0+sha-032b2bd`
+    version: `~15.1.0+sha-6686eac`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
