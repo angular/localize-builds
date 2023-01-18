d1bb9c89aac4d163196c1d8ebd8fd6a684c7409f
Andrew Kushnir <akushnir@google.com>
2023-01-18 19:03:43 +0000

    42ca9c0b02 docs: update documentation to use `ng generate config` and `ng generate environments` (#48757)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.1+sha-8377acd`
+    version: `~15.2.0-next.1+sha-42ca9c0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
