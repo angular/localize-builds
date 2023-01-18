b4ca26885333dd53aca0d43f2242787484b00ebd
Andrew Kushnir <akushnir@google.com>
2023-01-18 19:05:18 +0000

    fe6a2bd9e2 build: update dependency rimraf to v4 (#48726)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.1+sha-3a688cb`
+    version: `~15.2.0-next.1+sha-fe6a2bd`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
