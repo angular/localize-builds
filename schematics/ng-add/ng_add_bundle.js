8e182cc4377760cea995118d41d6c1c761389e24
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-19 21:46:41 +0000

    bca76ed43a docs: Document extracted from the original Introduction document, which is to be retired. Linting suggested corrections made.  Restructuring or rewording sentences to be shorter and clearer. (#47906)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.1+sha-08b72b7`
+    version: `~15.2.0-next.1+sha-bca76ed`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
