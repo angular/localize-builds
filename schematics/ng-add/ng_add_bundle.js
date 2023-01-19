755c1dad70cb052a3956da63754af2d1f1d6b8e0
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-19 17:53:12 +0000

    08b72b77e3 refactor(bazel): Extract the helper patchNgHostWithFileNameToModuleName to its own file for 1P use (#48739)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.1+sha-9e1e5cc`
+    version: `~15.2.0-next.1+sha-08b72b7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
