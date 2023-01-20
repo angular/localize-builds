1b1378a54f734f7d296b277b01884df01930cd0f
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-20 17:25:57 +0000

    840530ffd8 docs: update supported Chrome version description (#48782)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.1+sha-2d0ca73`
+    version: `~15.2.0-next.1+sha-840530f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
