21e78b8b58b7aa08fce315f443b905fa2ff0cbcf
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 17:06:15 +0000

    c03e6a6097 docs: Use of $any() has no runtime incidence. (#48785) Some users are woried about `$any()` as function calls are to be avoided in templates. This sentence clarifies the situation.

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.1+sha-a154db8`
+    version: `~15.2.0-next.1+sha-c03e6a6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
