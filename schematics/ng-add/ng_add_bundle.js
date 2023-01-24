ed1129d58fc8e1c1237b48fe1b9155fe62745ef5
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 16:51:56 +0000

    82c8b7ae9e build(docs-infra): replace base64 images for stackblitz by svgs (#48774)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.1+sha-a63983d`
+    version: `~15.2.0-next.1+sha-82c8b7a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
