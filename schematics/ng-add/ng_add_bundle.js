adb46677a8c60f3b688f5a1198981955f1503d35
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 21:26:41 +0000

    552cd3fb5c refactor: remove unnecessary test support check utilities (#47543)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.1+sha-067c4ce`
+    version: `~15.1.1+sha-552cd3f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
