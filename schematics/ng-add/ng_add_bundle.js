d01f2f81b0b802b507e4cc315354ba19db9ef4af
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 18:07:03 +0000

    98ccb57117 fix(compiler): handle css selectors with space after an escaped character. (#48558)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.1+sha-64416a6`
+    version: `~15.1.1+sha-98ccb57`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
