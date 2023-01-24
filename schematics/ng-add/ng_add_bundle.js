83ce33d778a029c053258fc3eb14526bc618d8d7
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 16:51:28 +0000

    a63983d1ba build: Fix version of images in recommended-Dockerfile. node:14-browsers doesn't exist anymore in Dockerhub (#48803)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.1+sha-0ad0136`
+    version: `~15.2.0-next.1+sha-a63983d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
