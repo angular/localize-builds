c880ebb9a534ad350c05bfc9f7e25161297f68b3
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-24 17:06:23 +0000

    4da1f2948c fix(compiler-cli): resolve deprecation warning (#48812)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.1+sha-c03e6a6`
+    version: `~15.2.0-next.1+sha-4da1f29`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
