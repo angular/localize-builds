ef948f09586be078f2f3c0fef074f0f20119cea6
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 19:51:46 +0000

    dedac8d3f7 feat(router): Add test helper for trigger navigations in tests (#48552)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.2+sha-a055196`
+    version: `~15.2.0-next.2+sha-dedac8d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
