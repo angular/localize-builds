03d6282765ed3ddeed4d79b08c352246156d4c0e
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 18:52:01 +0000

    06e161f2dd fix(compiler): incorrect code when non-null assertion is used after a safe access (#48801)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.2+sha-5915c72`
+    version: `~15.2.0-next.2+sha-06e161f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
