0f99a94c795d1d3565c5d915584de236eb77ba1c
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 18:51:41 +0000

    5915c7236e refactor(upgrade): rewrite the downgrade component adapter (#48715)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.2+sha-fd539a2`
+    version: `~15.2.0-next.2+sha-5915c72`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
