432559b829d1cb98417d81cacda233ab0ce01d45
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-25 18:47:20 +0000

    14aefa1b03 docs: add new files for HTTP content (#47684)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.2+sha-c91606b`
+    version: `~15.2.0-next.2+sha-14aefa1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
