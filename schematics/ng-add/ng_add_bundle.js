1151f8f6125e7c0c05d4f720edf70de9e05fe003
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-26 17:00:31 +0000

    9e86dd231b fix(migrations): Fixed file format issue with lint (#48859)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.2+sha-af31f98`
+    version: `~15.1.2+sha-9e86dd2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
