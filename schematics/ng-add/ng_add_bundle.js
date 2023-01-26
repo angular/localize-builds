45e9c691dd8f4b69c6f281b1afd0200be1d6ab4f
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-26 16:11:33 +0000

    af31f98b00 fix(migrations): migration host incorrectly reading empty files (#48849)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.2+sha-a334e4e`
+    version: `~15.1.2+sha-af31f98`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
