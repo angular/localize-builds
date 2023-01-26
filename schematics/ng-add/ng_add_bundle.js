f74495aa6a2296b887baab8a04c1bddd3f0a6369
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-26 16:05:19 +0000

    2796230e95 fix(migrations): add `enum` in `mode` option in `standalone` schema (#48851)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.2+sha-dedac8d`
+    version: `~15.2.0-next.2+sha-2796230`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
