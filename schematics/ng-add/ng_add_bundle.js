c44e6048fe40595cd27da49188997df9ec8567f2
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-27 17:12:12 +0000

    f82bdc4b01 fix(migrations): don't delete classes that may provide dependencies transitively (#48866)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.2+sha-869c9ac`
+    version: `~15.2.0-next.2+sha-f82bdc4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
