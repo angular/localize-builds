6e1384006ff50e5ac0dae8fd96c4805521bd4f93
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-27 16:38:58 +0000

    2352f8dca3 docs(docs-infra): in navigation put prerequisites page 'guide/property-binding' before 'guide/attribute-binding' (#48766)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.2+sha-e7a4fd3`
+    version: `~15.2.0-next.2+sha-2352f8d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
