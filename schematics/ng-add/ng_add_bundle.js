55f7b2f56af33d454ea7a9e8b3ea03c75af9991a
Jessica Janiuk <jessicajaniuk@google.com>
2023-01-27 16:41:32 +0000

    ef6e0b8f65 docs: add this is angular to Community Curations (#48618)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.2+sha-d36dfd4`
+    version: `~15.1.2+sha-ef6e0b8`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
