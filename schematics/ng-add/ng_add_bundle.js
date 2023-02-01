6499b7a30f2e57d3cdf14f460fc5d55bff2d1cf0
Dylan Hunn <dylhunn@gmail.com>
2023-02-01 12:16:47 +0000

    2fc5b70fce fix(platform-server): insert transfer state `script` before other `script` tags (#48868)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.2+sha-d3a14d9`
+    version: `~15.2.0-next.2+sha-2fc5b70`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
