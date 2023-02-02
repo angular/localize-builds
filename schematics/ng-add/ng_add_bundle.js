dac435d58976cf4db579029b105ea7e0b7a6784d
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 21:58:10 +0000

    c2bcf0b475 refactor(compiler): Remove strictStyling option for ShadowCss (#48824)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.3+sha-3a9bbad`
+    version: `~15.2.0-next.3+sha-c2bcf0b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
