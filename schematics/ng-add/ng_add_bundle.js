1686cb6e46eda03aafe89ea4692e4d51736d2a60
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 19:10:05 +0000

    65329a93f4 Revert "build: pin dependency aspect_bazel_lib to de8d9ca (#48887)" (#48928)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.2+sha-584976e`
+    version: `~15.2.0-next.2+sha-65329a9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
