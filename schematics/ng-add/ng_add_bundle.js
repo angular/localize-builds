1c4501b1927846056b7e92b929857da2a3e25892
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 21:57:04 +0000

    f5a7e02529 docs(router): Update functional guard and resolver docs (#48873)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.3+sha-617a010`
+    version: `~15.2.0-next.3+sha-f5a7e02`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
