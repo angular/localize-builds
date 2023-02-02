2b98856c4f0b68ad89e908941034f2c929247405
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 17:47:38 +0000

    584976e6c8 fix(migrations): support --defaults in standalone migration (#48921)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.2+sha-8a9907c`
+    version: `~15.2.0-next.2+sha-584976e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
