cec2e15a7995dca9619a2222d10bdb9e5e339839
Dylan Hunn <dylhunn@gmail.com>
2023-02-02 21:57:23 +0000

    3a9bbada5d docs: making footer copyright year dynamic (#48811)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.3+sha-f5a7e02`
+    version: `~15.2.0-next.3+sha-3a9bbad`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
