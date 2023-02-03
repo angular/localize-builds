707be2457330e3ba740e00f1115f8f156ca865f4
Andrew Scott <atscott@google.com>
2023-02-03 17:59:09 +0000

    2ceff3f0c2 refactor(core): remove IE11 only sanitization code (#48438)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.3+sha-ba38178`
+    version: `~15.2.0-next.3+sha-2ceff3f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
