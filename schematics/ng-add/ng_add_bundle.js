5c35eb025e001b2a63e19a80ea43f23bf642a1bb
Andrew Scott <atscott@google.com>
2023-02-03 20:01:20 +0000

    2de6dae16d fix(migrations): migrate RouterModule.forRoot with a config object to use features (#48935)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.3+sha-2ceff3f`
+    version: `~15.2.0-next.3+sha-2de6dae`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
