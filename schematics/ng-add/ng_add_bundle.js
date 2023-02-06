d8f55d0487ba2c13df2214bc5f615c9cb31c4893
Andrew Scott <atscott@google.com>
2023-02-06 20:55:59 +0000

    431ec6c8be refactor(forms): removing a workaround comment (#48904)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.3+sha-c4e5840`
+    version: `~15.2.0-next.3+sha-431ec6c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
