27ba441236563ad3425800b6eeb0a8441cce5935
Andrew Scott <atscott@google.com>
2023-02-06 20:58:09 +0000

    03a946dda3 docs: Fixing typo error (#48891)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.3+sha-21b15e8`
+    version: `~15.1.3+sha-03a946d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
