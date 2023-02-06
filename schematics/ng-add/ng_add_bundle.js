de1f56ab607c9186afe21af4dc13d16c271715b0
Andrew Scott <atscott@google.com>
2023-02-06 18:28:15 +0000

    54b24eb40f feat(common): Add loaderParams attribute to NgOptimizedImage (#48907)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.3+sha-759db12`
+    version: `~15.2.0-next.3+sha-54b24eb`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
