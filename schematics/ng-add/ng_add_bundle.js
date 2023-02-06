c2285b36403435917e6e9f99d62f927fb9bad383
Andrew Scott <atscott@google.com>
2023-02-06 20:48:36 +0000

    73394126b6 docs(core): standalone-migration schematics typos fix and properties addition (#48961)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.3+sha-32cf4e5`
+    version: `~15.2.0-next.3+sha-7339412`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
