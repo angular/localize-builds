81eeb91aed6ebdee77416198c692c5d9377d832d
Andrew Scott <atscott@google.com>
2023-02-06 20:45:25 +0000

    32cf4e5cb9 fix(migrations): avoid internal modules when generating imports (#48958)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.3+sha-54b24eb`
+    version: `~15.2.0-next.3+sha-32cf4e5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
