ca58ee675881db8e822bcea3591270f542d8c4d1
Andrew Scott <atscott@google.com>
2023-02-06 18:05:11 +0000

    759db12e0b fix(migrations): duplicated comments on migrated classes (#48966)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.3+sha-2de6dae`
+    version: `~15.2.0-next.3+sha-759db12`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
