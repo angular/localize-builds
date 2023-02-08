43d5922fec73852fca2aaeb4ba4a0c6e62c32514
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-08 18:33:27 +0000

    47387d495e docs: release notes for the v15.1.4 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.3+sha-bdbf21d`
+    version: `~15.2.0-next.3+sha-47387d4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
