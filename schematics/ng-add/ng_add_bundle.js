2fed80be4c97ea93d14ed371aaefa128abdc02cf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-08 18:44:17 +0000

    642cc1c349 release: cut the v15.2.0-next.4 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.3+sha-47387d4`
+    version: `~15.2.0-next.4+sha-642cc1c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
