e1fb5a1727c774e5fcb54a70b916f52f671cc68b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-08 14:20:58 +0000

    ff0d48ad92 build: update github/codeql-action action to v2.2.2 (#48889)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.3+sha-110300e`
+    version: `~15.1.3+sha-ff0d48a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
