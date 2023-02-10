ab7efd7d7b037a01bdf1473c6c1bdf935d4b48b4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 16:38:28 +0000

    c9e2728c85 build: update actions/cache digest to 6998d13 (#49020)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.4+sha-5f2a3ed`
+    version: `~15.1.4+sha-c9e2728`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
