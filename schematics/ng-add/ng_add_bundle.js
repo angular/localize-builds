40631c1a08e0538af099a0415738914d40599cd7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 16:50:02 +0000

    993a43f87c test: update size goldens to reflect devkit update and terser (#48996)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.4+sha-7af4ea6`
+    version: `~15.2.0-next.4+sha-993a43f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
