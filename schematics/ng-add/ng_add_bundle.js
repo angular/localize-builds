df10babaed6edfe1b58df7cf344730fb3b3a1f2d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 09:35:51 +0000

    c19486702a test: typos in standalone test names (#49014)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.4+sha-0331473`
+    version: `~15.2.0-next.4+sha-c194867`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
