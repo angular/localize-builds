337e8346b4778dd9f345342f2edbe192573638c7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 08:50:14 +0000

    0331473e3a docs(platform-browser): Sanitize method has more explicit documentation (#48765)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.4+sha-84ad4d0`
+    version: `~15.2.0-next.4+sha-0331473`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
