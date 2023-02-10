14c24139a9496119de748fed61058b7f8af4bb26
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 07:42:28 +0000

    865e67fcb4 build: remove unused webdriver-manager code (#49009)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.4+sha-4ac25b2`
+    version: `~15.2.0-next.4+sha-865e67f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
