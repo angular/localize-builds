f53d41a38e71c9fc6247a8414c02318f095d8afa
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-02-10 19:34:17 +0000

    42619bd670 build: remove puppeteer, remaining usages of `webdriver-manager` and migrate AIO production test to Bazel (#49025)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.4+sha-993a43f`
+    version: `~15.2.0-next.4+sha-42619bd`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
