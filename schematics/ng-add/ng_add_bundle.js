272421beb087f0b036248b6d7a5fe206b304eb46
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-10 07:55:30 +0000

    a28cf2438b docs(router): fix error handler deprecation (#49015)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.4+sha-865e67f`
+    version: `~15.2.0-next.4+sha-a28cf24`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
