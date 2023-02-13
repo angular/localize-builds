8a1ea35bfbca2686e5baf0b3ac237840302f039a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-13 19:16:55 +0000

    3ec2e2da0c build: never cache monitoring bazel test target (#49039)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.4+sha-ebae506`
+    version: `~15.2.0-next.4+sha-3ec2e2d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
