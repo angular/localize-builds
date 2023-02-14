2289dca9c2b9a34b04bffe36b0c5fdce609d1166
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 19:37:58 +0000

    2ca49726ca refactor(devtools): consolidate devtools global styles into 1 file (#49001)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.4+sha-7321154`
+    version: `~15.2.0-next.4+sha-2ca4972`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
