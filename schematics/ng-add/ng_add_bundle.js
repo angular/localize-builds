189b2f37c957375d650bb3b68e34639b2b0ef979
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 14:04:05 +0000

    1c5ae63811 refactor(docs-infra): Drop deprecated rxjs multicasting operators (#48969)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.4+sha-31a2e21`
+    version: `~15.2.0-next.4+sha-1c5ae63`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
