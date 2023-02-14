87b6d6707d07c157903673de4136c98321321936
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 18:03:12 +0000

    6534a5e3ca docs: remove former team members from bios (#48999)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.1.4+sha-f3e3f6e`
+    version: `~15.1.4+sha-6534a5e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
