b1ff0c2f253d3124b642e41b544deaaf6eebf056
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-02-14 17:16:11 +0000

    1f1a312e2e build: update dependency json5 to v2.2.2 [security] (#48989)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-next.4+sha-a4fdc8f`
+    version: `~15.2.0-next.4+sha-1f1a312`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
