d5b24a2b5fb25147e0bf8e150ed8658527a1c0c4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-16 23:50:41 +0000

    2a4d9879db build: avoid unnecessary re-evaluation of starlark code (#49106)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-06605cc`
+    version: `~16.0.0-next.0+sha-2a4d987`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
