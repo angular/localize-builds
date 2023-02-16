f8973d5cc24ebddfcf82474c54d7c2901917caa2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-16 23:50:53 +0000

    0c7b9aab34 docs: close the developer survey (#49092)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-2a4d987`
+    version: `~16.0.0-next.0+sha-0c7b9aa`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
