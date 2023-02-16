cdb76d8f24c7a19f2f34bffaeb490081883de6b8
Andrew Kushnir <akushnir@google.com>
2023-02-16 19:05:13 +0000

    6977403a9b release: bump version to 16.0.0-next.0 (#49084) PR Close #49084

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.3.0-next.0+sha-be21eaa`
+    version: `~16.0.0-next.0+sha-6977403`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
