5e530bf03dec50bc4a07ed479a937d8e675bc67d
Andrew Kushnir <akushnir@google.com>
2023-02-17 19:27:31 +0000

    f12ba1c6dd refactor: remove unnecessary file (#49042)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-2d51b5f`
+    version: `~16.0.0-next.0+sha-f12ba1c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
