1480ac4a68b8984f3c576d0fe8fc7af547452f60
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-17 00:17:26 +0000

    25e220a23a fix(platform-server): avoid duplicate TransferState info after renderApplication call (#49094)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0-rc.0+sha-440b51b`
+    version: `~15.2.0-rc.0+sha-25e220a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
