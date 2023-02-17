2403c1e303d7a37e708294885d5edb4f584cecb9
Andrew Kushnir <akushnir@google.com>
2023-02-17 20:07:07 +0000

    c872426fa9 ci: update AIO payload size tracking (#49130)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-9b9c818`
+    version: `~16.0.0-next.0+sha-c872426`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
