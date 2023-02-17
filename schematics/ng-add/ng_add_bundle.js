ab63ab382e0ab0769dad45d5989c1562e5e5fbfb
Andrew Kushnir <akushnir@google.com>
2023-02-17 19:24:11 +0000

    68f67d6835 docs: New doc HTTP client - Test requests (#48917)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-e6a622d`
+    version: `~16.0.0-next.0+sha-68f67d6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
