89f7789f9b93a150c5d108a029c32617436f5a80
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-02-20 15:28:11 +0000

    f9b6a3fecc ci: attempt to fix node windows job flakiness (#49142)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-8ba8e0d`
+    version: `~16.0.0-next.0+sha-f9b6a3f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
