e1c8b8ffaea79edb32e39cbd154236531c8ed31b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-21 22:24:37 +0000

    02cd490115 refactor(core): prototype of signals, a reactive primitive for Angular (#49091)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-b0c995a`
+    version: `~16.0.0-next.0+sha-02cd490`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
