bc55f1ab15c2c3e53812354df397f83ae5f6b102
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-02-22 19:45:43 +0000

    bc5ddabdcb feat(core): add Angular Signals to the public API (#49150)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-1e32709`
+    version: `~16.0.0-next.0+sha-bc5ddab`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
