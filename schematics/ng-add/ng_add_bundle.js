031af75335364b8226d26adf97a2af4d1c1ca346
Andrew Kushnir <akushnir@google.com>
2023-02-23 18:46:44 +0000

    daae4d87b3 docs(core): Improved README.md (#49171) PR Close #49171

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-3d589d8`
+    version: `~16.0.0-next.0+sha-daae4d8`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
