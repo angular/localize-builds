affe05f18e08201f5380e502888e52b4c20c7967
Andrew Kushnir <akushnir@google.com>
2023-02-23 18:48:14 +0000

    eea29a7104 build: update Angular FW and CLI packages to version 16.0.0-next (#49173)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-760cf4c`
+    version: `~16.0.0-next.0+sha-eea29a7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
