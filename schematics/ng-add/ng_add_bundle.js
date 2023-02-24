c52cdda770bbae9ed4d96d95eb6e5eab65a8c827
Andrew Kushnir <akushnir@google.com>
2023-02-24 16:34:12 +0000

    7dd19570e8 fix(migrations): delete barrel exports in standalone migration (#49176)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-79cdfeb`
+    version: `~16.0.0-next.0+sha-7dd1957`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
