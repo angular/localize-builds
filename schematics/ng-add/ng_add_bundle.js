e764a821139aa7e99f1866f651b7f55d084801b8
Andrew Kushnir <akushnir@google.com>
2023-02-27 18:21:11 +0000

    3c9d49a4d7 fix(common): make Location.normalize() return the correct path when the base path contains characters that interfere with regex syntax. (#49181)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-b4703a7`
+    version: `~16.0.0-next.0+sha-3c9d49a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
