6df433e19e82923d028e579144c1cc884545c177
Andrew Kushnir <akushnir@google.com>
2023-02-27 18:11:52 +0000

    b4703a7686 refactor(core): add previous sibling node reference to TNode (#49223)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-1d08a93`
+    version: `~16.0.0-next.0+sha-b4703a7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
