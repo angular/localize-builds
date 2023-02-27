97079571beca0fab9333237f911b1e2ecac6e650
Andrew Kushnir <akushnir@google.com>
2023-02-27 16:22:58 +0000

    a852862ff9 build: cleanup unused dependencies (#49227)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0+sha-8542593`
+    version: `~15.2.0+sha-a852862`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
