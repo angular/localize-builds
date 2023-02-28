a17411d70b7d168832ec21daa038e054c98eb17c
Andrew Kushnir <akushnir@google.com>
2023-02-28 18:19:49 +0000

    87affadb87 fix(migrations): avoid migrating the same class multiple times in standalone migration (#49245)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-dba747f`
+    version: `~16.0.0-next.0+sha-87affad`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
