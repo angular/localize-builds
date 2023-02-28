362e41bc84cd613ceca68699f0daf8e8fe56e1d1
Andrew Kushnir <akushnir@google.com>
2023-02-28 17:46:31 +0000

    557ae661ed docs: New doc extract from original HTTP doc to be retired. (#48908)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-a51b5cf`
+    version: `~16.0.0-next.0+sha-557ae66`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
