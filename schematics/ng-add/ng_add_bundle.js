e1bd38d200a3d1630d4f84c7cf3dc1aef022c479
Andrew Kushnir <akushnir@google.com>
2023-02-28 16:43:23 +0000

    6d30f607a0 docs(router): Improving linking (#49203)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-31f210b`
+    version: `~16.0.0-next.0+sha-6d30f60`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
