124b1b617212090bfcc146826129c3cc9a242584
Andrew Kushnir <akushnir@google.com>
2023-02-28 16:41:27 +0000

    31f210bf2c fix(router): Router.createUrlTree should work with any ActivatedRoute (#48508)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-8869289`
+    version: `~16.0.0-next.0+sha-31f210b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
