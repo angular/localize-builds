eb73d947d395d043a5824f1a06cd047c3580a8e9
Andrew Kushnir <akushnir@google.com>
2023-02-28 18:22:35 +0000

    7b9a082c92 refactor(core): Drop Symbol.iterator shim (#49207)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-87affad`
+    version: `~16.0.0-next.0+sha-7b9a082`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
