505a5df561856f1826e353bcf2f096f01ae299de
Andrew Kushnir <akushnir@google.com>
2023-02-28 20:23:16 +0000

    d60ea6ab5a fix(core): update zone.js peerDependencies ranges (#49244)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0+sha-dafb765`
+    version: `~15.2.0+sha-d60ea6a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
