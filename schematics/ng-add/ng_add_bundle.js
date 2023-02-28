0170c1037b841fee179afec17a9ee6b8f555bf91
Andrew Kushnir <akushnir@google.com>
2023-02-28 19:17:37 +0000

    f594725951 refactor(core): remove Node.js v14 support (#49255)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-0c805f1`
+    version: `~16.0.0-next.0+sha-f594725`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
