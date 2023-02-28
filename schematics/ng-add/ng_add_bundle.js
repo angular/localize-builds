1f3070974697409ebfcb5a663e780ec63fb2e696
Andrew Kushnir <akushnir@google.com>
2023-02-28 16:47:07 +0000

    99d874fe3b feat(core): add support for TypeScript 5.0 (#49126)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.0+sha-6d30f60`
+    version: `~16.0.0-next.0+sha-99d874f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
