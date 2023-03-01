3b97700f1fa54254ff0e3e08da8950b7bc352811
Andrew Kushnir <akushnir@google.com>
2023-03-01 20:51:19 +0000

    17abe6dc96 refactor(platform-server): remove deprecated `renderModuleFactory` (#49247)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.1+sha-030060e`
+    version: `~16.0.0-next.1+sha-17abe6d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
