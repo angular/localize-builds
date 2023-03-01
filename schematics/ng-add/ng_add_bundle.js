fb6927f7ca9ad407a6dea27d999d97d62a4ef4f5
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-01 22:57:21 +0000

    8f7fbddb51 refactor: remove duplicate key from component metadata (#49065)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.1+sha-bca3aed`
+    version: `~15.2.1+sha-8f7fbdd`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
