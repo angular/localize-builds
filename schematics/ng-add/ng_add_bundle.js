8e9d478d3096a750d132572b628f1ec302233d91
Andrew Kushnir <akushnir@google.com>
2023-03-01 19:37:01 +0000

    bca3aed9e4 build: remove core-js-bundle from dependencies (#49259)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.1+sha-1749971`
+    version: `~15.2.1+sha-bca3aed`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
