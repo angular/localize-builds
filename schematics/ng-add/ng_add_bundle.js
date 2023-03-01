a3956b652271404cd6e668928ac2b4467462014b
Andrew Kushnir <akushnir@google.com>
2023-03-01 20:51:57 +0000

    c41a21658c refactor(common): remove deprecated `XhrFactory` export from `http` entrypoint (#49251)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.1+sha-17abe6d`
+    version: `~16.0.0-next.1+sha-c41a216`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
