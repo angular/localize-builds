19c84d27d9924cf937b87d5dc2682119810f2dab
Andrew Kushnir <akushnir@google.com>
2023-03-01 19:28:31 +0000

    2fbaee3cbe fix(migrations): add protractor support if protractor imports are detected (#49274)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.1+sha-3673ea0`
+    version: `~16.0.0-next.1+sha-2fbaee3`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
