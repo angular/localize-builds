84e0b74fa49409710632a45eec4552fd286f0358
Andrew Kushnir <akushnir@google.com>
2023-03-01 01:29:52 +0000

    439b4d3a89 docs: clarify ActivatedRouter injection location (#49270)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.0+sha-377d0c9`
+    version: `~15.2.0+sha-439b4d3`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
