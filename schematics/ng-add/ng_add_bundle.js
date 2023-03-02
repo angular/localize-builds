97611a02faef03a46c82f3493cc904b3f6043710
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 22:09:08 +0000

    ea32c3289a feat(router): Expose information about the last successful `Navigation` (#49235)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.1+sha-10c8eae`
+    version: `~16.0.0-next.1+sha-ea32c32`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
