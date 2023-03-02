10d05dde4186c5a100fe23d0401b58a507052d2d
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 21:34:08 +0000

    81e32f4283 docs(router): Update component testing to user `RouterTestingHarness` (#48553)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.1+sha-d42032a`
+    version: `~15.2.1+sha-81e32f4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
