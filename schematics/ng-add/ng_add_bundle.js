cf3cf09969e566c90b24babb71cde930436886f5
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-02 20:13:17 +0000

    ffc684381f docs: Add NG8107 entry : Optional chain not nullable extended diagnostic (#49287)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.1+sha-8f7fbdd`
+    version: `~15.2.1+sha-ffc6843`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
