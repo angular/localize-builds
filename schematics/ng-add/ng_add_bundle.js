a6bcfafb0a74d3c1c3317906ea349ceb29217565
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-03 19:59:10 +0000

    594e68cc4b refactor(core): Remove ɵivyEnabled (#49296)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.1+sha-47dda6e`
+    version: `~16.0.0-next.1+sha-594e68c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
