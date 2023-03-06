56e80ef9b58da94ea6f487589800a27939fca3e6
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-06 19:32:31 +0000

    fc5100986b release: cut the zone.js-0.13 release (#49240)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.1+sha-477237a`
+    version: `~16.0.0-next.1+sha-fc51009`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
