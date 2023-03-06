a9e69f284df4fe5aac943d058a52247cf01c35c5
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-06 17:18:08 +0000

    b13e079bc7 refactor(core): Remove `ChangeDetectorStatus` & `isDefaultChangeDetectionStrategy` (#49299)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.1+sha-ac59054`
+    version: `~15.2.1+sha-b13e079`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
