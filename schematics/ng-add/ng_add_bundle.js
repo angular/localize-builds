758ea7bdf86643a469cf0d78122708be96c225c5
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-06 17:16:38 +0000

    27379d004e refactor(platform-browser): remove ununsed functions. (#49302)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.1+sha-06b0003`
+    version: `~16.0.0-next.1+sha-27379d0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
