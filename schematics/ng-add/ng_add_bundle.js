02c89a9115006a13154365f257c02e1101c0bb67
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-07 19:33:57 +0000

    88a0ff74ad docs(docs-infra): Split Error by categories & sort by code. (#49343)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.1+sha-e0be553`
+    version: `~15.2.1+sha-88a0ff7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
