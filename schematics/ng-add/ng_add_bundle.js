d9a81a470fc432595958488bef19228ee006bfa2
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-07 18:08:41 +0000

    ed23974931 Revert "fix(platform-browser): KeyEventsPlugin should keep the same behavior (#49330)" (#49357)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.1+sha-8551631`
+    version: `~16.0.0-next.1+sha-ed23974`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
