462dcdebf3e6f94e8fcf9af2258cb6d4ef54f65c
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-08 23:55:02 +0000

    addd7f6249 refactor(dev-infra): remove obsolete todos (#49362)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.2+sha-0e070dc`
+    version: `~16.0.0-next.2+sha-addd7f6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
