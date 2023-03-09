5a85f988e84e026b1f5bb3471bb1950b8862d336
Jessica Janiuk <jessicajaniuk@google.com>
2023-03-09 17:39:28 +0000

    1535ab90da docs: New doc: Understanding communicating with backend services using HTTP (#49294)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.2+sha-7e23883`
+    version: `~15.2.2+sha-1535ab9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
