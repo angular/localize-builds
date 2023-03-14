86f45f2524569ee0b0dcdbfa9a1a4f14ff78c0a1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 18:16:19 +0000

    ed110a05f0 docs: remove deprecation notes for removed APIs (#49239)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.2+sha-f07533f`
+    version: `~16.0.0-next.2+sha-ed110a0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
