275349dba41240d325e3d90cb063b9708bdc2101
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 17:03:30 +0000

    e4516c75d2 docs(upgrade): mark lifecycle methods as nodoc (#49416) PR Close #49416

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.2+sha-6c189ed`
+    version: `~15.2.2+sha-e4516c7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
