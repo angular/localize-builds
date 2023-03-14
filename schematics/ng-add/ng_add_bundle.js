6ff27663f3228443603dde5dd45a1512c03a9344
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-14 16:37:49 +0000

    4e098fa8a7 refactor(core): move Zone providers to a single provider function (#49373)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.2+sha-5a2b622`
+    version: `~16.0.0-next.2+sha-4e098fa`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
