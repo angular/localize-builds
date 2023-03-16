16772cb79b696b350071611d4892ae0aabfed267
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-03-16 00:10:44 +0000

    2d7f48cf74 test: fix misconfiguration in tests (#49391)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.2+sha-ed110a0`
+    version: `~16.0.0-next.2+sha-2d7f48c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
