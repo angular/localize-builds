1fbf65bd93a810421dcb6f8aea31f251c8cc2935
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-17 07:54:19 +0000

    c807a324d1 build: update actions/checkout digest to 24cb908 (#49436)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.3+sha-3d71d9d`
+    version: `~15.2.3+sha-c807a32`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
