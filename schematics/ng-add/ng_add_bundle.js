175dc5ea3380f513e130395ce2188e942efa6f55
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-17 17:40:32 +0000

    8d99ad0a39 Revert "feat(compiler): add support for compile-time required inputs (#49453)" (#49467)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.3+sha-c934a8e`
+    version: `~16.0.0-next.3+sha-8d99ad0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
