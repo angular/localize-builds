a7352e3b69fb1037914f20636dc4ce7f5ae26683
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-17 13:20:14 +0000

    17e9862653 feat(core): add API to provide CSP nonce for inline stylesheets (#49444)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.3+sha-13dd614`
+    version: `~16.0.0-next.3+sha-17e9862`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
