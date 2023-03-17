5c8393dd17c857b599adef3cd5beab19c5119dda
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-17 07:50:30 +0000

    82102c1852 docs: Intercept requests - insert updated link. (#49446)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.3+sha-e05f5ba`
+    version: `~16.0.0-next.3+sha-82102c1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
