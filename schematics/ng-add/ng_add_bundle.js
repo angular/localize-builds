2ebdf1555b25baf5c633b786ca6e1a6b70df7fb6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-20 08:05:24 +0000

    686499aa95 docs: Send data to server - insert updated links. (#49447)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.3+sha-8d99ad0`
+    version: `~16.0.0-next.3+sha-686499a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
