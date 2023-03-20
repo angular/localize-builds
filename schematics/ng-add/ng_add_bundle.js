223f27a47161fcf27c9821544889b36ac01e4087
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-20 09:23:57 +0000

    bae6b5ceb1 fix(core): Allow `TestBed.configureTestingModule` to work with recursive cycle of standalone components. (#49473)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.3+sha-868dcab`
+    version: `~15.2.3+sha-bae6b5c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
