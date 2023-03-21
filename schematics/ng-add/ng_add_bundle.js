c88bcee1bb8b977b993c8f3d04ebc882f2d80dc3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 15:52:40 +0000

    24b3e8f042 build: exclude draft PRs from triage queue (#49515)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.3+sha-f0f1e7d`
+    version: `~16.0.0-next.3+sha-24b3e8f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
