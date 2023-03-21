cb079dba2ef09bd34b3ea90e25aba01ebc2040e7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 19:41:12 +0000

    c4d2a55c44 test: increase size tracking threshold to 5K / 5% (#49520)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.3+sha-504bd99`
+    version: `~16.0.0-next.3+sha-c4d2a55`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
