38681e57e2e8bf1e0ebeb2c4b6c812812606eb3b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 14:29:29 +0000

    e2d3da5c71 test: update i18n size golden to reflect `main` (#49514)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.3+sha-81b9e85`
+    version: `~16.0.0-next.3+sha-e2d3da5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
