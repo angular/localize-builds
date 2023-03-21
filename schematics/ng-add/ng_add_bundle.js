7c143058cfe391d1b0feeb94148d9531ecd1bd4c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-21 16:59:20 +0000

    ced237693a refactor(core): adding hydration for content projection (#49454)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.3+sha-d1617c4`
+    version: `~16.0.0-next.3+sha-ced2376`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
