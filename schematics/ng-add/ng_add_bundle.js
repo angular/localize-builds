ed929212c77497159916d436b9583b0260f41c83
Andrew Scott <atscott01@gmail.com>
2023-03-21 21:24:44 +0000

    83f53aca1c refactor(core): Remove VE/Render3 aliases from ViewRef code (#49499)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.3+sha-6287153`
+    version: `~16.0.0-next.3+sha-83f53ac`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
