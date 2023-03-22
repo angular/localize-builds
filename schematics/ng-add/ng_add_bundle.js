4d16b09d868448b244f02cdf3ebd64b00cdc958e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-22 15:59:22 +0000

    41f27ad086 refactor(platform-server): remove `renderApplication` overload that accepts a component (#49463)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.3+sha-546b285`
+    version: `~16.0.0-next.3+sha-41f27ad`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
