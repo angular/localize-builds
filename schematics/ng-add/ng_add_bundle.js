247c88a6b6328a311c7848b3bafe8ceac4a429ce
Andrew Scott <atscott01@gmail.com>
2023-03-22 22:07:03 +0000

    a844435514 fix(router): fix #49457 outlet activating with old info (#49459)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.4+sha-901353e`
+    version: `~15.2.4+sha-a844435`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
