1a2a884cdfae7e28538a800399d1280d36f24c67
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-22 15:18:29 +0000

    ab0b3622a5 docs: remove ticks in code sample to generate components (#49525)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.3+sha-69a6e09`
+    version: `~16.0.0-next.3+sha-ab0b362`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
