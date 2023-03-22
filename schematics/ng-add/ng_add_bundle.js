cd7c050d5e86df9647037549e88072810094dac9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-03-22 16:03:42 +0000

    ff9d3b0f02 refactor(router): Remove legacy url creation code (#49360)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.3+sha-41f27ad`
+    version: `~16.0.0-next.3+sha-ff9d3b0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
