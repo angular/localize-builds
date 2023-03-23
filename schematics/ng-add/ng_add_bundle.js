ecc88e8e4594ff2252e2f601199d426db9771000
Andrew Scott <atscott01@gmail.com>
2023-03-23 19:03:18 +0000

    3f8e4fb724 docs: add missing word (#49565)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.4+sha-a844435`
+    version: `~15.2.4+sha-3f8e4fb`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
