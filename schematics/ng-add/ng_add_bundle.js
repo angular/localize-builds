5afaf07f8a19864df46fa0ed81e0eeecf15a105d
Andrew Scott <atscott01@gmail.com>
2023-03-23 21:12:23 +0000

    03d1d00ad9 feat(compiler-cli): Add an extended diagnostic for `nSkipHydration` (#49512)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.4+sha-d43367b`
+    version: `~16.0.0-next.4+sha-03d1d00`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
