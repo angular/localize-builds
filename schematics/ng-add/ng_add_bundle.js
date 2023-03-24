947ff4e052eac77058962e010337fb220fef4980
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-03-24 13:05:20 +0000

    6db2fed403 ci: update github.com known host given recent RSA update (#49580)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.4+sha-f1d3be3`
+    version: `~16.0.0-next.4+sha-6db2fed`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
