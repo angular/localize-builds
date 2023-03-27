8e3680c770c264b0af5ae46596d95d19393efba6
Andrew Scott <atscott01@gmail.com>
2023-03-27 20:11:51 +0000

    22688b8ff3 refactor(core): handle <ng-content> with *ngIf during hydration (#49590)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.4+sha-f2dd3bd`
+    version: `~16.0.0-next.4+sha-22688b8`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
