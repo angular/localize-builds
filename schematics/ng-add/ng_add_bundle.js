a54e447f4281dd397a83387b580581630b74642f
Andrew Scott <atscott01@gmail.com>
2023-03-27 15:48:50 +0000

    a1578de8fa build(zone.js): remove legacy browsers support (#49327)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.4+sha-2dbf3e0`
+    version: `~16.0.0-next.4+sha-a1578de`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
