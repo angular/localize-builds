1331f4864d395a9db68265808e083d0357f42d6d
Andrew Scott <atscott01@gmail.com>
2023-03-27 15:54:27 +0000

    1d63183bec ci: update pullapprove config to reflect current availability  (#49592)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.4+sha-6499f5a`
+    version: `~16.0.0-next.4+sha-1d63183`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
