ac9a706099746999042f174915f9c1cddefddd56
Andrew Scott <atscott01@gmail.com>
2023-03-27 23:03:41 +0000

    23f4619238 build: update cross-repo angular dependencies (#49553)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.4+sha-5e5dac2`
+    version: `~16.0.0-next.4+sha-23f4619`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
