a0941750e54621b113aca4223a5377d566310d68
Andrew Scott <atscott01@gmail.com>
2023-03-27 18:39:36 +0000

    9b65b84cb9 feat(core): Mark components for check if they read a signal (#49153)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.4+sha-1d63183`
+    version: `~16.0.0-next.4+sha-9b65b84`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
