93500e942669514fcfc0b4854d48565db551599a
Andrew Scott <atscott01@gmail.com>
2023-03-28 17:38:21 +0000

    6d6fc12c88 refactor(core): Remove usage of deprecated `Injector.create()` (#49606)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.4+sha-0fecbab`
+    version: `~15.2.4+sha-6d6fc12`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
