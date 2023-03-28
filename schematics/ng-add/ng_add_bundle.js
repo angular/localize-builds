aca2c7ec8dfa59386ca1fdce7041a72b9efa73b8
Andrew Scott <atscott01@gmail.com>
2023-03-28 18:53:53 +0000

    1829542aea fix(compiler): do not unquote CSS values (#49460)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.4+sha-74adf81`
+    version: `~16.0.0-next.4+sha-1829542`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
