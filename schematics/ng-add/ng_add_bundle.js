6c7dde2129171efb3e579f5bc77c9a8a2a500bcb
Andrew Scott <atscott01@gmail.com>
2023-03-28 18:57:45 +0000

    465c20cb09 refactor(migrations): remove migrations from previous versions (#49603)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.4+sha-1829542`
+    version: `~16.0.0-next.4+sha-465c20c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
