5c28397a3b8a8e0040133a6f560048194d75e391
Andrew Scott <atscott01@gmail.com>
2023-03-29 01:58:19 +0000

    ef149defb9 refactor(core): expose `ReactiveNode` privately on signal functions (#49529)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.4+sha-84a0fa3`
+    version: `~16.0.0-next.4+sha-ef149de`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
