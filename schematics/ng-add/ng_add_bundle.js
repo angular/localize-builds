252a03dbae6d316da818b794a0d5d50c9a46f4c7
Andrew Scott <atscott01@gmail.com>
2023-03-29 16:43:48 +0000

    e8e36811d5 fix(platform-browser): set nonce attribute in a platform compatible way (#49624)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.4+sha-e994608`
+    version: `~16.0.0-next.4+sha-e8e3681`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
