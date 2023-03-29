d712bc93ef06b0d5c3087330450925e2c95f3166
Andrew Scott <atscott01@gmail.com>
2023-03-29 17:29:04 +0000

    c024574f46 feat(core): expose `makeStateKey`, `StateKey` and  `TransferState` (#49563)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.4+sha-e8e3681`
+    version: `~16.0.0-next.4+sha-c024574`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
