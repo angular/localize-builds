392e08895ba41867c4f24a4eb98c01b6b51aac3a
Andrew Scott <atscott01@gmail.com>
2023-03-29 22:44:46 +0000

    cbe6807a46 refactor(core): Add default implementation for WeakRef (#49639)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.4+sha-3c7e637`
+    version: `~16.0.0-next.4+sha-cbe6807`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
