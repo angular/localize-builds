32865ee74b4ee699c02d3b9fdd58b47c9388cf0c
Dylan Hunn <dylhunn@gmail.com>
2023-03-30 23:29:31 +0000

    1dc8480bf4 refactor(router): Remove dead code (#49652)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.5+sha-e9dd7f0`
+    version: `~16.0.0-next.5+sha-1dc8480`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
