5c6556489193d2c9e7480ec949cd64d32ce5a07b
Dylan Hunn <dylhunn@gmail.com>
2023-03-30 19:05:54 +0000

    07a1aa3004 feat(forms): Improve typings form (async)Validators (#48679)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.5+sha-39a648f`
+    version: `~16.0.0-next.5+sha-07a1aa3`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
