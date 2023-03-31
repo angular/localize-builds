caf870a9e6340c1ad4ad411f66eca74096eab87a
Dylan Hunn <dylhunn@gmail.com>
2023-03-31 19:06:03 +0000

    12b2caad24 build: update scorecard action dependencies (#49609)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.5+sha-6c1895f`
+    version: `~16.0.0-next.5+sha-12b2caa`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
