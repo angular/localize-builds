2c906667e2f76aa30751d1865b636aa01e5b7060
Dylan Hunn <dylhunn@gmail.com>
2023-03-31 19:17:22 +0000

    acff3c6d86 docs: fix typo in toh-pg6.md (#49141)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.5+sha-fda3f67`
+    version: `~16.0.0-next.5+sha-acff3c6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
