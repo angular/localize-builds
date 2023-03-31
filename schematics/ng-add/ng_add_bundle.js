6f52a328425c216f62fbe60a5172dfe47a334709
Dylan Hunn <dylhunn@gmail.com>
2023-03-31 19:10:10 +0000

    78a3298f65 refactor(router): Remove internal use of guard and resolve interfaces (#49548)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.5+sha-01d4a13`
+    version: `~16.0.0-next.5+sha-78a3298`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
