6bad7d00562824567e3da78258f2e931a6812fdd
Dylan Hunn <dylhunn@gmail.com>
2023-04-01 00:24:49 +0000

    9b61379096 fix(router): Ensure initial navigation clears current navigation when blocking (#49572)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.5+sha-08979e4`
+    version: `~15.2.5+sha-9b61379`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
