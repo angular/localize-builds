d1e0a386e5220c3fe9dc3a98eda7f29f12879229
Dylan Hunn <dylhunn@gmail.com>
2023-04-03 05:35:34 +0000

    a6def648f5 docs: fix typo (#49664) PR Close #49664

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.5+sha-9b61379`
+    version: `~15.2.5+sha-a6def64`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
