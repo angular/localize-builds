0335b2e154e5442bbf83bf0385e6e49d664c2d47
Dylan Hunn <dylhunn@gmail.com>
2023-04-03 22:17:49 +0000

    a5f1737d1c feat(core): expose onDestroy on ApplicationRef (#49677)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.6+sha-de6dc5f`
+    version: `~16.0.0-next.6+sha-a5f1737`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
