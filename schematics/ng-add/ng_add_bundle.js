c8da06c1aecde26f829945972f1525ab7ef7aeeb
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 22:32:34 +0000

    34b2d34d5b refactor(router): Remove RouterTestingModule in favor of RouterModule.forRoot (#49427)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.6+sha-1aef29c`
+    version: `~16.0.0-next.6+sha-34b2d34`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
