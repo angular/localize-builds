51e2ffe7f2108d29380aabd90c127398fb614170
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 20:29:28 +0000

    e7463dc80f docs: tweak hydration guide from feedback (#49705)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.6+sha-edc3bb1`
+    version: `~16.0.0-next.6+sha-e7463dc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
