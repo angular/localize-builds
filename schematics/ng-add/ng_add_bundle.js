9c445c769b8b8e0df0045af3fdd4f571b43908fe
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:33:05 +0000

    aff1512950 feat(http): allow `HttpClient` to cache requests (#49509)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.6+sha-ba3e9ea`
+    version: `~16.0.0-next.6+sha-aff1512`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
