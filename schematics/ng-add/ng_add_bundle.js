4d260cef2baf4db6686420aa02f63f5a18b65369
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:52:18 +0000

    b2327f4df1 feat(core): Allow typeguards on QueryList.filter (#48042)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.6+sha-6ca1a53`
+    version: `~16.0.0-next.6+sha-b2327f4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
