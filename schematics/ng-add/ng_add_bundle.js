db13f4bb5541aae314bb936c7a2e16661a93cd46
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 18:39:41 +0000

    edc3bb180f Revert "feat(forms): Improve typings form (async)Validators" (#49706)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.6+sha-761e02d`
+    version: `~16.0.0-next.6+sha-edc3bb1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
