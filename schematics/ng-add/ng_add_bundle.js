7f60d3837c085efda5a5fd473853d273e0d4fd6f
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:38:07 +0000

    61bedaf930 refactor(core): handle a case when ngSkipHydration is applied to the root node of an application (#49675)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.6+sha-4b673d5`
+    version: `~16.0.0-next.6+sha-61bedaf`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
