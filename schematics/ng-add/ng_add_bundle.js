3ffeb8e7fb0efa812178e72fc2be7e60b0d70bbd
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 05:30:25 +0000

    761e02d912 feat(platform-browser): add a public API function to enable non-destructive hydration (#49666)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.6+sha-b2327f4`
+    version: `~16.0.0-next.6+sha-761e02d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
