44164e3313a5592c63479e0cf4035467ba363e85
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 02:37:34 +0000

    4b673d5ed7 docs: Add error page for NG0504 error (#49689)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.6+sha-c912294`
+    version: `~16.0.0-next.6+sha-4b673d5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
