ca8bbbb75bacf317f0931e5d62b6a234eb54b14d
Dylan Hunn <dylhunn@gmail.com>
2023-04-04 22:19:51 +0000

    b35fa73968 feat(core): change the URL sanitization to only block javascript: URLs (#49659)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.6+sha-f0978ad`
+    version: `~16.0.0-next.6+sha-b35fa73`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
