70e04acc7482858e0c9b6fa698dfd9f93e739ab2
Dylan Hunn <dylhunn@gmail.com>
2023-04-05 00:00:14 +0000

    3b863ddc1e refactor(core): Remove `ReflectiveInjector` symbol (#48103)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.6+sha-34b2d34`
+    version: `~16.0.0-next.6+sha-3b863dd`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
