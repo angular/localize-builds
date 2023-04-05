0935f0b1f8d517ff439fb15128b3c5f9c05c4f77
Dylan Hunn <dylhunn@gmail.com>
2023-04-05 17:05:15 +0000

    d497be7aca refactor(core): Use the nullish coalescing assignment in render3 functions (#49698)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.6+sha-136ffbc`
+    version: `~16.0.0-next.6+sha-d497be7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
