8e61257c4a3b193df9eeee4a19e44775ed7ac0dd
Dylan Hunn <dylhunn@gmail.com>
2023-04-05 18:11:47 +0000

    1c16be4728 build: update domino digest to aa8de34 (#49716)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.6+sha-df1dfc4`
+    version: `~16.0.0-next.6+sha-1c16be4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
