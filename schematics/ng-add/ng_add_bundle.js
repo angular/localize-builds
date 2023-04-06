d1966d942a642c4365bb5701032a10abf9995933
Andrew Kushnir <akushnir@google.com>
2023-04-06 18:24:27 +0000

    9c07a42fbe docs: Improve previous version navigation (#49063) In archive mode the link to the current doc will now point to same page. If the page doesn't exist anymore, it will show a contextualized warning message.

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.6+sha-9a37a77`
+    version: `~15.2.6+sha-9c07a42`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
