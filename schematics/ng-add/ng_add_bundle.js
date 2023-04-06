f8cf02d894d102c9b4b11eb5ed33ae0f037c6dfe
Joey Perrott <josephperrott.github@gmail.com>
2023-04-06 17:57:41 +0000

    ec0b44da8c docs: release notes for the v13.4.0 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-726f09c`
+    version: `~16.1.0-next.0+sha-ec0b44d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
