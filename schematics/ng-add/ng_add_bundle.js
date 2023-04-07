f45e316ea254e8f02b285cfc90ebb8d27021caf7
Andrew Kushnir <akushnir@google.com>
2023-04-07 16:52:19 +0000

    fe34de47bf refactor(core): add a warning when hydration annotation is missing in server response (#49743)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-8ad31d5`
+    version: `~16.1.0-next.0+sha-fe34de4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
