0c0bb0d4856fdc74ac34cd1eb3c80f929591a6fa
Andrew Kushnir <akushnir@google.com>
2023-04-07 16:09:24 +0000

    8ad31d53b0 ci: remove lock-closed action (#49650)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-ed8a595`
+    version: `~16.1.0-next.0+sha-8ad31d5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
