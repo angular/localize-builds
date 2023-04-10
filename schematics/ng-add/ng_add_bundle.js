8fe2b539844d4668dd7e72faa2bd591ddff3e0d5
Andrew Kushnir <akushnir@google.com>
2023-04-10 16:19:44 +0000

    83262dc0f9 refactor(core): do not enable hydration when server response was incorrect (#49750)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-fe34de4`
+    version: `~16.1.0-next.0+sha-83262dc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
