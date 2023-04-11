7dce86dad4441b61a5a12928f47edc18c239d0a8
Andrew Kushnir <akushnir@google.com>
2023-04-11 21:30:18 +0000

    f9b821f07d fix(http): delay accessing `pendingTasks.whenAllTasksComplete` (#49784)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-e949548`
+    version: `~16.1.0-next.0+sha-f9b821f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
