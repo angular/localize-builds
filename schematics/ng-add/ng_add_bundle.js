3ed64d72388101e60c0c12baa6495fc1f90cba19
Andrew Kushnir <akushnir@google.com>
2023-04-11 21:50:56 +0000

    b0c1a90f55 fix(compiler): Produce diagnositc if directive used in host binding is not exported (#49792)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.6+sha-9c07a42`
+    version: `~15.2.6+sha-b0c1a90`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
