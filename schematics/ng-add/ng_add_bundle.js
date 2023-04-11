a4857ab03f9cae9f2086ea0bb39e162f319b14ef
Andrew Kushnir <akushnir@google.com>
2023-04-11 21:03:06 +0000

    2eb9b8b402 fix(http): wait for all XHR requests to finish before stabilizing application (#49776)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.7+sha-f8e2586`
+    version: `~16.0.0-next.7+sha-2eb9b8b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
