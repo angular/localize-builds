35003914ae44b801b0edaaf02825624f928f0dc1
Andrew Kushnir <akushnir@google.com>
2023-04-11 16:33:39 +0000

    a46b76132e revert "fixup! fix(router): fix = not parsed in router segment name" (#47332)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.7+sha-bab36d0`
+    version: `~16.0.0-next.7+sha-a46b761`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
