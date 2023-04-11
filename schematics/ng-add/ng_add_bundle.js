cc640eeb67c48d32e19d12133ed9ba4fec03488f
Andrew Kushnir <akushnir@google.com>
2023-04-11 18:57:52 +0000

    4e41d127db build(compiler): add a flag to conditionally build with the template pipeline (#48580)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-cf46dfb`
+    version: `~16.1.0-next.0+sha-4e41d12`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
