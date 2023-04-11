28966ceec103c73629c4be5fed58cdac459a8180
Andrew Kushnir <akushnir@google.com>
2023-04-11 20:06:46 +0000

    f8e25864e8 fix(core): allow async functions in effects (#49783)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-next.7+sha-d980af6`
+    version: `~16.0.0-next.7+sha-f8e2586`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
