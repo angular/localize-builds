0df917978b6db97d5ccad64439991230b03ca322
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-13 21:30:00 +0000

    c34d7e0822 fix(core): onDestroy should be registered only on valid DestroyRef (#49804)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-rc.0+sha-6ade3ec`
+    version: `~16.0.0-rc.0+sha-c34d7e0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
