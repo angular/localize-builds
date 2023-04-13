ad2e2668aec89a60ca21f9cb4d6f07ed96175a60
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-13 14:18:21 +0000

    54f18c832d refactor(core): rename internal DI token that indicates whether hydration is enabled (#49800)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-ab5e2d9`
+    version: `~16.1.0-next.0+sha-54f18c8`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
