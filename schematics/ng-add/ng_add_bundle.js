6a4eb22eb4c08871ee834367d7b9ef002d9e77cd
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-13 14:23:48 +0000

    ddf0d4eabe fix(http): HTTP cache was being disabled prematurely (#49826)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-294fae0`
+    version: `~16.1.0-next.0+sha-ddf0d4e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
