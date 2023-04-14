3a5be2ed21e103a0494352cbd2f4014b4a4eebd9
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-14 14:30:40 +0000

    31e1264faf ci: Remove environment files and EnableProdMode on integration tests (#49746)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-rc.0+sha-c34d7e0`
+    version: `~16.0.0-rc.1+sha-31e1264`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
