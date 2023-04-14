9c6787095f536b76e3067b73e49bb19a7925d238
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-14 18:25:21 +0000

    e594d1ca81 test(router): enable a subset of bootstrapping test to ran on node (#49843)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-rc.1+sha-98223c1`
+    version: `~16.0.0-rc.1+sha-e594d1c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
