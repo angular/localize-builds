0b2162b6388ebcf77c033fbb344c951b4de0b73a
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-14 19:39:26 +0000

    8c3b92cfb3 refactor(platform-server): simplify `_render` method (#49840)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-9e9b424`
+    version: `~16.1.0-next.0+sha-8c3b92c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
