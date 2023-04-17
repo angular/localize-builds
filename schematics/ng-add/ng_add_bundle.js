a0589b7d7d5d8ff1ed019ead17d72818f07c2cfd
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 13:44:41 +0000

    96a4e74069 refactor(platform-server): refactor internal `_render` function (#49851)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-3fcf8bd`
+    version: `~16.1.0-next.0+sha-96a4e74`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
