2b60c5592beb02efc484c9005df92dc10118c93b
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 18:47:49 +0000

    1d4cf95a94 docs(docs-infra): add lara newsom to GDE resources (#49620)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.7+sha-79a8b3c`
+    version: `~15.2.7+sha-1d4cf95`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
