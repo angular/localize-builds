9a7a4367795ad3149bf9a05825932fe75c508cd4
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 14:25:05 +0000

    3659e035f7 test: refactor platform-server tests to use async/await (#49855)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-288f2c8`
+    version: `~16.1.0-next.0+sha-3659e03`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
