87ada2405164e80a20fb7ecd73c6af8253fc65d9
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 15:11:32 +0000

    78c76cecf9 perf(compiler-cli): optimize NgModule emit for standalone components (#49837)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-a7dbb23`
+    version: `~16.1.0-next.0+sha-78c76ce`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
