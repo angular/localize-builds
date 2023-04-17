2774bfbe12324377dcfafb0fbe0ee7a07c61e2fb
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 13:54:07 +0000

    d994f8520c fix(core): include inner ViewContainerRef anchor nodes into ViewRef.rootNodes output (#49867)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-96a4e74`
+    version: `~16.1.0-next.0+sha-d994f85`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
