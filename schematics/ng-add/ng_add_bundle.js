457d31add1ebf8e0c466e0c1a25e0a08a1dd3ea2
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 15:04:42 +0000

    0a9d1060b9 refactor(router): Do not access browserPageId if not computed resolution (#49850)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.7+sha-1017630`
+    version: `~15.2.7+sha-0a9d106`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
