a1d8a5d300510bb132a0d461c1e68579211f5bd4
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 15:11:18 +0000

    3ba5dcc150 refactor(compiler-cli): remove unused integration tests. (#49862)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-14b5a5d`
+    version: `~16.1.0-next.0+sha-3ba5dcc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
