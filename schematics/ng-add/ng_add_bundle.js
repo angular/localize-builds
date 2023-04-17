5ce6089036e6ec4df636a4c1b3900492d6f56c05
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 14:22:48 +0000

    1017630d04 refactor(docs-infra): cleanup deprecated code (#49671)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.7+sha-a63d9a3`
+    version: `~15.2.7+sha-1017630`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
