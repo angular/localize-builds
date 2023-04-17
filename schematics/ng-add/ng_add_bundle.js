dbb938c73dd07f84652f00f00d49a73185a952ad
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-17 17:47:38 +0000

    49386de19f docs: Specify when an app is debuggable by the DevTools. (#48970)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-2aa6d6d`
+    version: `~16.1.0-next.0+sha-49386de`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
