c957891c71b70a4b5ca56b9aea926647ec1bc473
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 19:57:21 +0000

    ded7ede1d4 test: update platform-server integration tests to use the Angular CLI (#49900)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-rc.1+sha-5e4797c`
+    version: `~16.0.0-rc.1+sha-ded7ede`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
