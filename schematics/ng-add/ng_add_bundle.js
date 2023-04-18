41295f730a3f9662005a96b0c98e2ee75329517d
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 14:18:47 +0000

    8219857821 refactor(platform-browser): cleanup references to dart (#49858)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-rc.1+sha-003e7d1`
+    version: `~16.0.0-rc.1+sha-8219857`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
