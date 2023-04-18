bf4d33d74c021f0a73367121a2092fe8bacca69c
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 16:37:13 +0000

    3ce1c98b1d refactor: fix typo in windows-chromium-path.js (#49676)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-36ed47f`
+    version: `~16.1.0-next.0+sha-3ce1c98`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
