46acc49be4f94c6037a2bfa13f2b28c0b28d0ee5
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 16:32:26 +0000

    36ed47fdf4 docs: fix errors in toh-pt6.md (#49129)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-09332e7`
+    version: `~16.1.0-next.0+sha-36ed47f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
