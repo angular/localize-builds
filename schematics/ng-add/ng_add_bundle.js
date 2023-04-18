f65abbcd57d262fbce33af32c8a996a85f4d6ea2
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 22:20:15 +0000

    bf8041fdfc build: update angular/dev-infra digest to 1564c4e (#49916)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-1afef35`
+    version: `~16.1.0-next.0+sha-bf8041f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
