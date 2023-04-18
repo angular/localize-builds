e4beb2b1281e3abb8b3ab7853c6450c2de04401d
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 16:49:05 +0000

    d4ef20736f refactor(compiler): handle #24571 todos. (#49220)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-34b796f`
+    version: `~16.1.0-next.0+sha-d4ef207`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
