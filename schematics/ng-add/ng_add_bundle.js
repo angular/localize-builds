96b65c5623d9bc4001af0c8b1e5ecde59eb25246
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-18 21:15:49 +0000

    b1f7aa35d6 ci: update renovate.json (#49909)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-59e332c`
+    version: `~16.1.0-next.0+sha-b1f7aa3`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
