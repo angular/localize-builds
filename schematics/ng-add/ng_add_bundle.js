beeca98b24cb1f81e5b5268ab601505729b9f560
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 15:48:24 +0000

    195aaa6b11 refactor(core): Create TestBed injector without a module (#49864)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-38661a6`
+    version: `~16.1.0-next.0+sha-195aaa6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
