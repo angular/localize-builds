119b5af49d22625104924549daa8e2418e7a2a60
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 19:02:44 +0000

    3d7b8cdb4d docs: add information on how to use index option in configuration (#49813)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.7+sha-2fff8fa`
+    version: `~15.2.7+sha-3d7b8cd`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
