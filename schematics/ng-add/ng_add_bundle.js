4634ef842d06ab24850ad601d344dc158d84c308
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 16:46:38 +0000

    2fff8fadbe fix(core): handle invalid classes in class array bindings (#49924)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.7+sha-0454158`
+    version: `~15.2.7+sha-2fff8fa`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
