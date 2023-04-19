e832e49cc6da99e19e990c8e72042f3c8ad090c2
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 18:30:11 +0000

    1dc919a3df fix(core): execute query setters in non-reactive context (#49906)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-726d8a8`
+    version: `~16.1.0-next.0+sha-1dc919a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
