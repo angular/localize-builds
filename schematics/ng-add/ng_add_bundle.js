27f50f128460110a7c1f2334dfc9c6a2667a3b14
Jessica Janiuk <jessicajaniuk@google.com>
2023-04-19 20:14:14 +0000

    c029c678d9 fix(core): ensure takeUntilDestroyed unregisters onDestroy listener on unsubscribe (#49901)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-856a0ec`
+    version: `~16.1.0-next.0+sha-c029c67`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
