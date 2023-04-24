5985fe1694a4f3b1e1b1695ed5ad46adda363f75
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 17:08:01 +0000

    c17b84a33b docs: applicability of NgModule providers (#49966)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.8+sha-898acfa`
+    version: `~15.2.8+sha-c17b84a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
