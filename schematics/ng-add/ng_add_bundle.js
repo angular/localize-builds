f9b5eab3b0e45986b3608dd62e0cd0d407a90bf7
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 19:09:59 +0000

    5f7d929d9e docs(core): correct description of actively supported versions (#49945)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-a5428a7`
+    version: `~16.1.0-next.0+sha-5f7d929`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
