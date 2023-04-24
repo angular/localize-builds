7b1c451d497fde68fd917935fc799dd3062e2433
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 19:08:09 +0000

    b9c53658a4 refactor(compiler-cli): remove unecessary type assertion (#49971)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-92897b4`
+    version: `~16.1.0-next.0+sha-b9c5365`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
