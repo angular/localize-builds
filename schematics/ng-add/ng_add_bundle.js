96b7054b76ffa207ba13c3971084f7e03e250922
Dylan Hunn <dylhunn@gmail.com>
2023-04-24 01:43:25 +0000

    898acfa4df docs: remove the warning about shadowDom support (#49961)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.8+sha-0f38b98`
+    version: `~15.2.8+sha-898acfa`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
