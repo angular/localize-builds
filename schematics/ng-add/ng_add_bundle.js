0dac6067bdaf364b344c16f9f7c070981bcb64ff
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:48:40 +0000

    bdf2aa1957 build: bump license year (#49984)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.8+sha-1ef6205`
+    version: `~15.2.8+sha-bdf2aa1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
