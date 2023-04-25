0c375c79f88965595a950d12a91fea8b54c65af4
Dylan Hunn <dylhunn@gmail.com>
2023-04-25 16:38:26 +0000

    5607e0f529 fix(core): typing of TestBed Common token. (#49997)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-e40a640`
+    version: `~16.1.0-next.0+sha-5607e0f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
