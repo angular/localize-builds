16d74c6206cf913f46b178947d6169aaba60fbb8
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 16:37:25 +0000

    08949c7c6c refactor(core): Error code for Component Id collision (#49986)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-2d1691a`
+    version: `~16.1.0-next.0+sha-08949c7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
