d2dfd157916868afd1374e6d8fe43791ebd3225d
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 18:46:06 +0000

    a8cf5962ef refactor(core): Rename 'transplanted' view refresh flag and counters to be generic (#50000)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-60f6672`
+    version: `~16.1.0-next.0+sha-a8cf596`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
