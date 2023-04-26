ce9928ee7b926a49e8d0e6be690a4bb3a54ab9c6
Dylan Hunn <dylhunn@gmail.com>
2023-04-26 18:40:41 +0000

    3599214526 build: update dependency patch-package to v7 (#50002)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.8+sha-dd4fb18`
+    version: `~15.2.8+sha-3599214`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
