9101379fe6747f8be5cdee0a5cebdad06d26702b
Dylan Hunn <dylhunn@gmail.com>
2023-04-27 07:49:53 +0000

    567123499a refactor(core): Add signal LView flag (#49988)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-720ad5f`
+    version: `~16.1.0-next.0+sha-5671234`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
