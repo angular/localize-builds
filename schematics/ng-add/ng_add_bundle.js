f1407fab7fb56b5e55055cd486552288f25a98a0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-04-27 15:37:08 +0000

    80ed0a4868 fix(common): fix incorrectly reported distortion for padded images (#49889)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-rc.3+sha-3feb9ae`
+    version: `~16.0.0-rc.3+sha-80ed0a4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
