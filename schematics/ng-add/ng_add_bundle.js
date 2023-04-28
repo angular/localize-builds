91a25b0fb40b2d0729af963f2715cb967eb7d1e8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-04-28 23:52:34 +0000

    6dc3add921 docs(core): primary Angular Signals documentation (#50053)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-rc.3+sha-d5fb3ad`
+    version: `~16.0.0-rc.3+sha-6dc3add`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
