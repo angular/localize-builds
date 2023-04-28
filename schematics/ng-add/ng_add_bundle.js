2dca403da6e48f52bdf033a7d7b3cb97817d8ae9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-04-28 23:19:39 +0000

    cc89766cee refactor(core): finalize rxjs-interop options & docs (#50071)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-bfa6e6a`
+    version: `~16.1.0-next.0+sha-cc89766`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
