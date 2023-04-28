16b068a70b146d2190ab082a02c729cb5adbf9bb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-04-28 16:36:09 +0000

    cfab3ad706 refactor(compiler-cli): add back ngcc as a no-op with a warning (#50045)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-ce00738`
+    version: `~16.1.0-next.0+sha-cfab3ad`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
