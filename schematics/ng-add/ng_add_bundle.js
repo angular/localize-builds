a9a42046a7065c73a36bcab7676600c190fa5e48
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 15:12:58 +0000

    d6d347fec5 docs: add developer preview guide for esbuild-based build system (#50068)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-rc.3+sha-6dc3add`
+    version: `~16.0.0-rc.3+sha-d6d347f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
