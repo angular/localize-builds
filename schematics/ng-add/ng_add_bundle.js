8071971d0aecb9241d8fa9282b5ba7cf59927da3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-01 14:52:56 +0000

    08b664d3c8 docs: Introduce RxJS and Signals interop guide (#50039)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-50a4a7c`
+    version: `~16.1.0-next.0+sha-08b664d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
