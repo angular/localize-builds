5b879519f8cce87f922616c36a536b3b4cdc1887
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 11:35:26 +0000

    8080bf0076 docs: Make links out of @see tags (#50098)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-rc.4+sha-c9d4a69`
+    version: `~16.0.0-rc.4+sha-8080bf0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
