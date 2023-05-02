8bd324a040f3dcba12db7b6b13fe00ac216ed1e9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 10:31:10 +0000

    72ac1a0eab docs(docs-infra): hide `Debugging the error`section. (#50074)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-149e0c0`
+    version: `~16.1.0-next.0+sha-72ac1a0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
