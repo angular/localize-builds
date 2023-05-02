99b95afa3e383b6eb85fb38b7195fb26acf8197a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-02 10:21:06 +0000

    f0bef4c438 docs: archiving older changelog entries (#49957)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-c68545e`
+    version: `~16.1.0-next.0+sha-f0bef4c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
