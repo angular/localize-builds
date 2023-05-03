1ab975754115e5e35efedc28c27ad02b1f977f6a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-03 08:57:51 +0000

    b381ad7d8a docs(docs-infra): Remove unused annotation template (#50114)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0-rc.4+sha-796273d`
+    version: `~16.0.0-rc.4+sha-b381ad7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
