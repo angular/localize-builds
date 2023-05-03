c342d1d52f7b976156802d27acf5a3c0091b6d4c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-03 16:28:06 +0000

    2d68de5bb1 release: cut the v15.2.9 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~15.2.8+sha-8b34482`
+    version: `~15.2.9+sha-2d68de5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
