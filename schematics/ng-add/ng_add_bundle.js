a4c11a06e37bcfe5be0caa847372e7c7ea836c18
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 06:57:23 +0000

    504f55d4c1 docs: add jens to contributors (#50122)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-3c4b307`
+    version: `~16.1.0-next.0+sha-504f55d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
