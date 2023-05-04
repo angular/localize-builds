a0c3f191f8c68ea522082093c74436a6cbac64d2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-05-04 07:24:44 +0000

    2419c486ad build: remove the webworker platform. (#50059)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-cb8cdad`
+    version: `~16.1.0-next.0+sha-2419c48`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
