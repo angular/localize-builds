be85a69e9789cd0c4cae51ce80ebf74ae74c415b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 21:52:39 +0000

    59837f4749 refactor(platform-server): import `xhr2` dynamically in the ServerXhr class (#50095)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-e60b3d4`
+    version: `~16.1.0-next.0+sha-59837f4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
