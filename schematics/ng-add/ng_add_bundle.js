f8df414e825d737d48f4f825a9def0d1e5cdde70
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 21:52:07 +0000

    e60b3d45b9 build: remove unused deps (#50116)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-f780d6f`
+    version: `~16.1.0-next.0+sha-e60b3d4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
