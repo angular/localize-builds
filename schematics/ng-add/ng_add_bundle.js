d38c32e8a30461c0057dec7ee4d919aa537482bd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-08 22:09:07 +0000

    fe653c2141 docs: fix filename in first-app (#50207)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0+sha-da7ff37`
+    version: `~16.0.0+sha-fe653c2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
