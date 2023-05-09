14b1d84d8af8da26dbd379e911ef7f2ca11782f2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-09 22:06:31 +0000

    6636e8321b fix(core): add additional component metadata to component ID generation (#50203)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-1c7f690`
+    version: `~16.1.0-next.0+sha-6636e83`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
