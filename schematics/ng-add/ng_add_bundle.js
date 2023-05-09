cb577244974c55f80fa7fd7101428f8fac5b2dc8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-09 22:07:20 +0000

    822b3079ae fix(core): handle projection of hydrated containters into components that skip hydration (#50199)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-2ef473c`
+    version: `~16.1.0-next.0+sha-822b307`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
