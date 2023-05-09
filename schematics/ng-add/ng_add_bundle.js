a715fb3e60ee1feca5902e6007c64a876feb374f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-05-09 22:26:36 +0000

    e4ad0672c3 docs: fix link label. (#50209)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-69dadd2`
+    version: `~16.1.0-next.0+sha-e4ad067`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
