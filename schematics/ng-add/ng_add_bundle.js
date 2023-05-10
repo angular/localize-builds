6aa93fb9b773ff8cd0cc7bce56f1354d1f2eaa65
Andrew Kushnir <akushnir@google.com>
2023-05-10 18:42:02 +0000

    02437224f5 refactor(core): simplify state transfer escaping (#50201)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-ea3b5f1`
+    version: `~16.1.0-next.0+sha-0243722`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
