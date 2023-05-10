b96e82b08f4db2a071f02ec22794dba1445b963f
Andrew Kushnir <akushnir@google.com>
2023-05-10 18:01:06 +0000

    c9657bdb88 refactor(core): avoid using `def.signal` during component id computation (#50238)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.0+sha-fe653c2`
+    version: `~16.0.0+sha-c9657bd`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
