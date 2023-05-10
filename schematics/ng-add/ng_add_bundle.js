c9b3eb8c6e2b0e4a60ba5c313a13b5d497f69aea
Andrew Kushnir <akushnir@google.com>
2023-05-10 21:40:20 +0000

    143b85184d refactor: remove benchpres tests for other frameworks (#50108)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-cd5946b`
+    version: `~16.1.0-next.0+sha-143b851`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
