e48d237a38e1e82556f6a78115fa9f88ffc00f33
Andrew Kushnir <akushnir@google.com>
2023-05-11 21:48:06 +0000

    4a7ebf4037 build: lock file maintenance (#49879)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.1+sha-d5be23e`
+    version: `~16.0.1+sha-4a7ebf4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
