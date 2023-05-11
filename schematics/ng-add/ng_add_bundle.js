c319a5f9b482e9ae12e787865b19186dd5d50dd9
Andrew Kushnir <akushnir@google.com>
2023-05-11 17:33:57 +0000

    cfff5cb2bd docs: split unsupported versions table in two. (#50243)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-03f1e24`
+    version: `~16.1.0-next.0+sha-cfff5cb`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
