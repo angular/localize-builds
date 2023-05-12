c0b10cf20b57eae3c8e0948eb8d4783eecc88c74
Andrew Kushnir <akushnir@google.com>
2023-05-12 17:44:04 +0000

    21199f8086 fix(docs-infra): labels with links should have the same font weight (#50258)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.1+sha-4a7ebf4`
+    version: `~16.0.1+sha-21199f8`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
