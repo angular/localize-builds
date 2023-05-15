f0c0b33d9502da4b1795a873c2ef0e75a85def23
Andrew Kushnir <akushnir@google.com>
2023-05-15 17:11:31 +0000

    2d87aeecc1 docs: Glossary link to N (#50294)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.1+sha-37acd37`
+    version: `~16.0.1+sha-2d87aee`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
