12e6f271b5fe14b189e6ee0965961ad5fe25ca46
Andrew Kushnir <akushnir@google.com>
2023-05-16 16:59:52 +0000

    5849287a7a docs: fix typo in dependency injection guide (#50323) PR Close #50323

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-efa92ab`
+    version: `~16.1.0-next.0+sha-5849287`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
