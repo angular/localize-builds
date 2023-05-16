a4f8a14884631272b4ebe1b52e508e94e76bb489
Andrew Kushnir <akushnir@google.com>
2023-05-16 17:02:52 +0000

    b75625ba49 docs: fix typo in security guide (#50324) PR Close #50324

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.1+sha-bd4f64d`
+    version: `~16.0.1+sha-b75625b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
