5564b669e6292d3ed91151942e475877198ef80e
Andrew Kushnir <akushnir@google.com>
2023-05-16 16:41:46 +0000

    cd86eb4606 refactor(common): Reduce the precision to 2 digits in the ngOptimizedImage distortion warning message (#50276)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-165b8b6`
+    version: `~16.1.0-next.0+sha-cd86eb4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
