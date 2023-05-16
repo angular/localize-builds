b280d4b806fb32459883152aa6a8b52e6ac7e578
Andrew Kushnir <akushnir@google.com>
2023-05-16 16:39:02 +0000

    165b8b647c fix(core): allow passing value of any type to `isSignal` function (#50035)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.0+sha-fab7f39`
+    version: `~16.1.0-next.0+sha-165b8b6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
