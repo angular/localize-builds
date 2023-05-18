e9b08731b7a728edcc7f0355bb9ebe4ae2c29e9e
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-18 14:22:21 +0000

    87b8aba238 docs(docs-infra): Drop `ngcli-compat` (#50345)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.1+sha-70099a3`
+    version: `~16.1.0-next.1+sha-87b8aba`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
