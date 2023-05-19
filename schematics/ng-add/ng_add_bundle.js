2563b957e958334921e1fa9c0f380a1d27af32c6
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 14:36:06 +0000

    00b4924bad docs: remove unused oninit method (#50353)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.1+sha-b68306e`
+    version: `~16.1.0-next.1+sha-00b4924`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
