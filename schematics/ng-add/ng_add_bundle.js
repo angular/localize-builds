5e0805cab4b973e8a794225a97d6decf9ff86876
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 14:37:11 +0000

    b89fae173e docs(language-service): correction of the demonstration translation (#50359)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.2+sha-2d3dc4a`
+    version: `~16.0.2+sha-b89fae1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
