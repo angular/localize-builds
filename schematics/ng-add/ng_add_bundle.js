2a581a0cc7acb6dfe2d3cef93356df11180963f6
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 14:35:31 +0000

    b68306e616 build: update dependency google-closure-compiler to v20230502 (#50153)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.1+sha-87b8aba`
+    version: `~16.1.0-next.1+sha-b68306e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
