9686c715aca149cd950f8369e4048c907c61838a
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 15:26:10 +0000

    14588ab10d docs: forwardref usage for standalone components (#50366)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.2+sha-b89fae1`
+    version: `~16.0.2+sha-14588ab`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
