1e6c5d63e82a41600bc61ffa3fc80a6c64fc93ae
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 16:41:51 +0000

    d097844da9 docs: replace blossom emoji with tulip (#50349)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.1+sha-24d803b`
+    version: `~16.1.0-next.1+sha-d097844`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
