668f9ef9238b0e3871e2f3eaffc77a9c8d1b7d85
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-19 16:56:18 +0000

    d963ecdf50 docs: added clarity in lifecycle event docs (#50145)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.2+sha-14588ab`
+    version: `~16.0.2+sha-d963ecd`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
