d6e3987d17fffb56d03e16f0ae5771bbf707c4d8
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-22 15:03:05 +0000

    5574e47752 refactor: remove unnecessary array copying (#50370)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.2+sha-d963ecd`
+    version: `~16.0.2+sha-5574e47`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
