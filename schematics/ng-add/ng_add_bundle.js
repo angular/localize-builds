e312c5e43778537c2be1bcd3e5d8b181604b5580
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-22 17:51:55 +0000

    203b55a8cd docs: replaced the html tags with the updated ones (#50403)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.1+sha-6b02242`
+    version: `~16.1.0-next.1+sha-203b55a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
