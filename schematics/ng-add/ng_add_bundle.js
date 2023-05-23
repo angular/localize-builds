086c584e8c866912c7d2e63ee95736500a429c0c
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 17:47:07 +0000

    4dc528d402 build: remove unused bash utilities from legacy build (#50427)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.1+sha-f7b8fb3`
+    version: `~16.1.0-next.1+sha-4dc528d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
