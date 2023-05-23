c24266609e87e429f335ffa5a0f3d6af55fe9483
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 17:33:55 +0000

    f7b8fb3c97 build: remove unused gulp helper script (#50428)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.1+sha-f4393f7`
+    version: `~16.1.0-next.1+sha-f7b8fb3`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
