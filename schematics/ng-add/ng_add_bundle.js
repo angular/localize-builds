34b2b3c9e5832a92d9a02abac8b710ed82862bf0
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 19:11:13 +0000

    c11041e372 fix(core): adds missing symbols for animation standalone bundling test (#50434)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.2+sha-f40ecc0`
+    version: `~16.0.2+sha-c11041e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
