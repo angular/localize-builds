0319701cfab5d0f79f68c5882e928bf70f74ee8b
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 14:31:02 +0000

    f79a5bc71c docs: fix code-example header path in dependency-injection-in-action-md (#50397)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.1+sha-e516556`
+    version: `~16.1.0-next.1+sha-f79a5bc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
