afc3e0e22c404b576ff58e2326d3c29882f84f03
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 14:34:24 +0000

    ea560a7098 test(core): correct typo in signal_spec.ts (#50392)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.1+sha-e8c51a1`
+    version: `~16.1.0-next.1+sha-ea560a7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
