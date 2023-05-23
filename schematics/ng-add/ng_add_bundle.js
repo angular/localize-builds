4037eda5796db8357f666b591477235474906721
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-23 14:28:13 +0000

    3152c4de92 fix(docs-infra): remove extra slash from JSON-LD data (#50140)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.1+sha-3c093d7`
+    version: `~16.1.0-next.1+sha-3152c4d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
