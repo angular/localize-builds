644ca291a5cf8a3ca5d3a51e39e0e5fcc8fa1b4d
Dylan Hunn <dylhunn@gmail.com>
2023-05-25 20:42:07 +0000

    1c37a83e1b Revert "fix(http): create macrotask during request handling instead of load start (#50406)" (#50475)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.3+sha-df65c4f`
+    version: `~16.0.3+sha-1c37a83`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
