1c2c672c1c6ecea8f0dfd8cee1e58feb0408b381
Dylan Hunn <dylhunn@gmail.com>
2023-05-25 18:58:47 +0000

    df65c4fc8f fix(animations): Trigger leave animation when ViewContainerRef is injected (#48705)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.3+sha-fc6b10c`
+    version: `~16.0.3+sha-df65c4f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
