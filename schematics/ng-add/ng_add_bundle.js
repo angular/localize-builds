86b51d53fdcc4d131c200567305150725a397b23
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 14:59:37 +0000

    3a05e4cca1 refactor(platform-server): exclude `provideHttpClient()` call from `provideServerRendering()` (#50459)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.2+sha-721bc72`
+    version: `~16.1.0-next.2+sha-3a05e4c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
