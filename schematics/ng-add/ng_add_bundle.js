d36831bd4cefe447e72def4050cafb926ae815b1
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 15:01:11 +0000

    4f5f4969c5 refactor(core): Remove unused import from the graph.ts (#50372) This commit updates the code to remove unused import of the `throwInvalidWriteToSignalError` symbol.

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.2+sha-3a05e4c`
+    version: `~16.1.0-next.2+sha-4f5f496`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
