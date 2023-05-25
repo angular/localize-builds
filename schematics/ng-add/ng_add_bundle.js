afb471d7b65bdc93a3c6d64fe173220ce88b0fa2
Jessica Janiuk <jessicajaniuk@google.com>
2023-05-25 14:57:10 +0000

    04553b9016 build: update dependency https-proxy-agent to v7 (#50456)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.2+sha-258c773`
+    version: `~16.1.0-next.2+sha-04553b9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
