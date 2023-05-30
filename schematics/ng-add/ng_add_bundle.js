ea60afe34569eca127e037c7f51813d5d7e6d5cb
Dylan Hunn <dylhunn@gmail.com>
2023-05-30 16:37:20 +0000

    9acc0e32c4 docs: refactored the markup tags (#50481)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.2+sha-62e76d4`
+    version: `~16.1.0-next.2+sha-9acc0e3`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
