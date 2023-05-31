a6f43f77538716700d9325d0179739879453756b
Dylan Hunn <dylhunn@gmail.com>
2023-05-31 17:07:30 +0000

    7567348c54 Revert "fix(zone.js): enable monkey patching of the `queueMicrotask()` API in node.js (#50467)" (#50529)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.2+sha-069498c`
+    version: `~16.1.0-next.2+sha-7567348`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
