7ef46aa330cb4cb643f596118d73c6a9df929586
Dylan Hunn <dylhunn@gmail.com>
2023-05-31 19:55:47 +0000

    7e468dffce docs: correct Signals documentation (#50518)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.2+sha-ac447ed`
+    version: `~16.1.0-next.2+sha-7e468df`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
