c291483ad80e86b9adf9971076e6a0d15ed342fc
Dylan Hunn <dylhunn@gmail.com>
2023-05-31 19:48:47 +0000

    7ca301acd5 build: update dependency eslint-plugin-jsdoc to v45 (#50523)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.2+sha-7567348`
+    version: `~16.1.0-next.2+sha-7ca301a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
