bda67177229774d1e19cea7a270ae66eaff12daf
Dylan Hunn <dylhunn@gmail.com>
2023-06-01 16:04:29 +0000

    540e643347 fix(compiler): do not remove comments in component styles (#50346)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.3+sha-9847085`
+    version: `~16.1.0-next.3+sha-540e643`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
