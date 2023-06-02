f788d3bca4b8c526f7d15d333a36fe46e16074b0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-02 21:24:20 +0000

    9d7991a216 fix(zone.js): patch entire promise in node (#50552)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.4+sha-703b8fc`
+    version: `~16.0.4+sha-9d7991a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
