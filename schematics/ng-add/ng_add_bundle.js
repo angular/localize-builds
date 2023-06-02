20d90c5b71567c451e42a0dbc5b967628204b562
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-02 21:16:45 +0000

    cf15c290f0 build: update to TypeScript 5.1 final (#50548)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.3+sha-540e643`
+    version: `~16.1.0-next.3+sha-cf15c29`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
