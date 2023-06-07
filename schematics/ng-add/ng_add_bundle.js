23d57a46f492e3b585d285d1eed51026fd84542b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-07 19:59:30 +0000

    0875b519b9 fix(platform-server): surface errors during rendering (#50587)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.3+sha-b3d2295`
+    version: `~16.1.0-next.3+sha-0875b51`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
