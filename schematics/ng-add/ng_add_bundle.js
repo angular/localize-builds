8c14740aa9e025f976711e0471a9efd18d2d3c6d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-07 20:04:36 +0000

    4e663297c5 fix(compiler): error when reading compiled input transforms metadata in JIT mode (#50600)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.3+sha-3044822`
+    version: `~16.1.0-next.3+sha-4e66329`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
