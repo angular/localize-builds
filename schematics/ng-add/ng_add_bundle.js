8e2ebc4ea8461932df59f7e76b21faf88e616988
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-08 17:44:55 +0000

    55d6147d29 Revert "feat(common): add component input binding support for NgComponentOutlet (#49735)"

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-next.3+sha-f386759`
+    version: `~16.1.0-next.3+sha-55d6147`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
