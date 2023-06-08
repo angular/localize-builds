f5b9878968d502e1e3b83bbad017945e2f0ce4b4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-08 21:34:59 +0000

    b7c710e722 docs: release notes for the v16.0.5 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-e1a715d`
+    version: `~16.2.0-next.0+sha-b7c710e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
