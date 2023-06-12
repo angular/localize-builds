f69f593a27a76aebde2b24e7508bb5303edb9d4d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 10:54:36 +0000

    89a089bdeb docs: add missing navigation item (#50113)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.5+sha-8e9f920`
+    version: `~16.0.5+sha-89a089b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
