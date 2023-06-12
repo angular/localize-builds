03960056d458e143f05a12c96817c04ea531259d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 09:35:46 +0000

    9860acecf0 docs(packaging): Clarify the necessity of dev-dependencies (#50619)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-0370905`
+    version: `~16.2.0-next.0+sha-9860ace`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
