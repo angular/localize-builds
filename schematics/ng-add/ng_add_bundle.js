4899b53469e75f680bfed367a8636ed497212cdb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 16:37:11 +0000

    ecbdb24694 test(compiler): Prevent back-sliding on already passing template pipeline tests (#50582)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-rc.0+sha-ffdc130`
+    version: `~16.1.0-rc.0+sha-ecbdb24`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
