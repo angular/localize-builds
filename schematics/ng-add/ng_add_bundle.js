96787b4318b60a0dc138ef82b28e755707e7e565
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-12 12:41:45 +0000

    9a172a9345 docs: update first-app-lesson-11.md to include info about routerLink (#50348)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-dbadfea`
+    version: `~16.2.0-next.0+sha-9a172a9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
