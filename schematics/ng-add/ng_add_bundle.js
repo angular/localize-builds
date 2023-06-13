8c1cf992f5105a30bbff4e2bcc40ea5bd5b90141
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 11:48:06 +0000

    cefa3de3de refactor(common): use `transform` functions in NgOptimizedImage inputs (#50580)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-307f8ee`
+    version: `~16.2.0-next.0+sha-cefa3de`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
