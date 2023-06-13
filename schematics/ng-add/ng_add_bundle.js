f43d7d16941916e9b78529e8f15a9754cd7f534b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 11:06:36 +0000

    50e185874b build: update all non-major dependencies (#50583)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.0.5+sha-db84fa5`
+    version: `~16.0.5+sha-50e1858`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
