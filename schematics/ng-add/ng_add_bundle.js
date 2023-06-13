360cd4293a8faebd1bfe3b7d3a2e442f1cacdd2d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 17:00:56 +0000

    15ab146e6c refactor(compiler): improve handling of bindings and attributes (#50664)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-1f0d529`
+    version: `~16.2.0-next.0+sha-15ab146`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
