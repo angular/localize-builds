f9ad86dfc5f08b90169ca1f18c0ba7d387c8148e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 10:02:55 +0000

    7abb71609b build: lock file maintenance (#50532)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-rc.0+sha-57f9efe`
+    version: `~16.1.0-rc.0+sha-7abb716`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
