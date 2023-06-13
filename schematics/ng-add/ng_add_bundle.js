d1467f9f1f9da3b7e429ced771e5f5ae128805e7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 17:10:11 +0000

    88962b72a9 refactor(compiler): Support safe property reads and keyed reads. (#50594) Angular's null-safe access operators differ from Javascript's built-in semantics, in that they short-circuit to `null` instead of `undefined`. This necessitates providing a custom transformation, instead of relying on Typescript or Javascript itseld.

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-15ab146`
+    version: `~16.2.0-next.0+sha-88962b7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
