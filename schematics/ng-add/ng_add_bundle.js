72177363f4a4af8447bf17dd72272cb5376e65cf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-13 12:59:52 +0000

    fa3b7332a9 build: update actions/checkout action to v3.5.3 (#50689)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0-rc.0+sha-b90f2b1`
+    version: `~16.1.0-rc.0+sha-fa3b733`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
