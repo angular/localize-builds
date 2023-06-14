b70831163fd866e1abc9ca36ffa723118da72ad6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 10:53:51 +0000

    9ff5572b9f refactor(core): handle #24571 todos. (#49221)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0+sha-0a72df6`
+    version: `~16.1.0+sha-9ff5572`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
