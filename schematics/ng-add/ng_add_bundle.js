4462d2856a6d6b4c69412a163ada67445299551c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-14 14:04:03 +0000

    5e1d8444ae fix(migrations): Prevent a component from importing itself. (#50554)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.0+sha-e94f387`
+    version: `~16.1.0+sha-5e1d844`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
