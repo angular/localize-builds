2eb85fa4a96120a68beecdbf58106ec6ec8cc46b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-15 13:45:29 +0000

    1d3e96b7d9 test: mark ngtsc test target as flaky (#50718)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.1+sha-fd519b0`
+    version: `~16.1.1+sha-1d3e96b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
