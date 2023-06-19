4e6b7447e5c8bd25bd8fe3a06e65fc00e8b260de
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-06-19 13:48:51 +0000

    254023a03c build: resolve benchmark compare ref from upstream if rev-parse fails

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-619c318`
+    version: `~16.2.0-next.0+sha-254023a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
