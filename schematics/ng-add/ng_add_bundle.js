b761340b77daa9ce049cc9210fa324a1afd42cdc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-20 15:31:44 +0000

    f77bd0a2bd refactor: fix lint warning in compiler code by adding explicit `override` (#50772)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-a88ae41`
+    version: `~16.2.0-next.0+sha-f77bd0a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
