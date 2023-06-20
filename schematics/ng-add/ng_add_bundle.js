12a6163af2d8be6b36405cfb06ef8810ecc5ed6a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-20 09:19:12 +0000

    19913ccbb7 feat(docs-infra): add option to filter docs with developer preview status (#50142)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-076d449`
+    version: `~16.2.0-next.0+sha-19913cc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
