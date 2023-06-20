4ef4235f480fd83f6937ec79e0aea31a319e9b07
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-20 13:08:06 +0000

    a88ae41504 build: remove AngularJS benchmarks (#50730)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-7f4a7b4`
+    version: `~16.2.0-next.0+sha-a88ae41`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
