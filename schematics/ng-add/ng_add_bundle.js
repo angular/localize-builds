ff5b6a611d94dd8667444aeb4f5fbde927fc2ff4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-20 12:39:18 +0000

    135167fe8e fix(http): Send query params on fetch request (#50740)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-d381b74`
+    version: `~16.2.0-next.0+sha-135167f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
