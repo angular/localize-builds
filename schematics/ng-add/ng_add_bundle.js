ed7e545895466b9d1e516ab4e9a396c0f84fcded
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-06-20 16:16:36 +0000

    4af49106aa build: speed up remote execution by allowing more concurrent jobs (#50734)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-f77bd0a`
+    version: `~16.2.0-next.0+sha-4af4910`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
