f2f5b21cca2f54392977ea81afcdef7729346b19
Andrew Kushnir <akushnir@google.com>
2023-06-22 17:04:34 +0000

    e1bbe47c23 fix(elements): support input transform functions (#50713)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.2+sha-174f669`
+    version: `~16.1.2+sha-e1bbe47`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
