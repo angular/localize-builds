2db40bc5baba385bfbb5da354b8483a3c911c9e8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2023-06-22 11:07:38 +0000

    ba0ca42b8c docs: fix typos

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-764c7fb`
+    version: `~16.2.0-next.0+sha-ba0ca42`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
