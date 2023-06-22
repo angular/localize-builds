c232dfcf3b817bcd2840ec3078cb6f9687b0b141
Andrew Kushnir <akushnir@google.com>
2023-06-22 17:01:45 +0000

    30154ae6dc docs: improved standalone and workspace config guide (#50672)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-0b90701`
+    version: `~16.2.0-next.0+sha-30154ae`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
