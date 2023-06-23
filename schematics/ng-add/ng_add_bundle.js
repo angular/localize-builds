76d50cb441a0d6452ab87f654b580367dc136937
Andrew Kushnir <akushnir@google.com>
2023-06-23 00:32:00 +0000

    ea38aee82a docs: update invalid links to the new http guides (#49456)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.2+sha-e1bbe47`
+    version: `~16.1.2+sha-ea38aee`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
