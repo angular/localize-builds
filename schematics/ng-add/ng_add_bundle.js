ab13c766d8ef082d30f9f2de43b02ba3cfb5ef6d
Andrew Kushnir <akushnir@google.com>
2023-06-23 16:58:03 +0000

    232a78630e ci: re-enable RBE for http tests (#50741)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-e93708b`
+    version: `~16.2.0-next.0+sha-232a786`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
