ba6bbb201a38d39651bd8a60548c85f58739d4d3
Andrew Kushnir <akushnir@google.com>
2023-06-23 21:29:29 +0000

    bdd8813451 build: print benchmark results at the end of `yarn benchmarks run` invocation (#50816)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-232a786`
+    version: `~16.2.0-next.0+sha-bdd8813`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
