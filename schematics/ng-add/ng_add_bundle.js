2d1a6a0efa14c8905f993951ce8607cd7788980a
Andrew Kushnir <akushnir@google.com>
2023-06-26 20:50:41 +0000

    e699f1a75d build: allow for compliance specs only using template pipeline (#50835)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-d6b1fed`
+    version: `~16.2.0-next.0+sha-e699f1a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
