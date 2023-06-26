0ebc8ea06f13530f1b9b05676b1d3f32578f1760
Andrew Kushnir <akushnir@google.com>
2023-06-26 19:28:37 +0000

    9b7bc801f6 Revert "fix(platform-browser): wait until animation completion before destroying renderer (#50677)" (#50857)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.2+sha-21ced23`
+    version: `~16.1.2+sha-9b7bc80`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
