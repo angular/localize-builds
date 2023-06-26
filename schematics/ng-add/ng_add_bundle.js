fb03be873da366e4ac3e12b14f4a01b24571b475
Andrew Kushnir <akushnir@google.com>
2023-06-26 20:28:38 +0000

    a844249c7f docs: fix minor typo in tutorial (#50795)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.2+sha-9b7bc80`
+    version: `~16.1.2+sha-a844249`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
