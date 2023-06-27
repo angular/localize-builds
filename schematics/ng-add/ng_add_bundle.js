9a896f0ceb253c195f4e4a5da3ae3b3df5ba2bc2
Andrew Kushnir <akushnir@google.com>
2023-06-27 23:23:56 +0000

    520a1b5fe1 build: update eslint dependencies to v5.60.1 (#50859)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.2+sha-52f1dd9`
+    version: `~16.1.2+sha-520a1b5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
