a59e8711415acd2849cacb24006ccca1a043ee42
Andrew Kushnir <akushnir@google.com>
2023-06-27 23:08:12 +0000

    7aa913e512 build: update actions/checkout digest to c85c95e (#50640)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-0522f1b`
+    version: `~16.2.0-next.0+sha-7aa913e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
