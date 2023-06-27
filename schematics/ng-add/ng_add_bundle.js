21467e525265c08f9233a356a5bf5e91335c1634
Andrew Kushnir <akushnir@google.com>
2023-06-27 13:42:03 +0000

    52f1dd9aaa refactor(animations): remove redundant castings (#50860)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.2+sha-ec1f6e3`
+    version: `~16.1.2+sha-52f1dd9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
