be2101507faea7e01c2f5301d92af4d9727ec23b
Andrew Kushnir <akushnir@google.com>
2023-06-27 23:14:40 +0000

    ad1265e225 docs: clarify lesson 3, step 1 (#50849)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.0+sha-77f0a22`
+    version: `~16.2.0-next.0+sha-ad1265e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
