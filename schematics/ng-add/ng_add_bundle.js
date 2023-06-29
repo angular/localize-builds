091810c878c19b26deac9c4e8acaa9044a5f8a5a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-29 20:14:33 +0000

    29bf476bfe refactor(compiler): Generate temporary variable assignments when function calls appear in a safe-access expression. (#50688)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-a5bd5dd`
+    version: `~16.2.0-next.1+sha-29bf476`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
