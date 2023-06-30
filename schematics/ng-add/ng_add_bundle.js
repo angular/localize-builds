1d2e65a18639866ce7419f757905afd8fbf042c6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-30 18:52:03 +0000

    a19a87df6c fix(upgrade): allow for downgraded components to work with component-router (#50871)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-57c9399`
+    version: `~16.2.0-next.1+sha-a19a87d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
