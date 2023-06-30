436ab3ba2ef1632e6b32d9ff5148942948fbc697
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-30 19:02:04 +0000

    0bfd3f537c docs: Use id instead of name for anchor targets (#50885)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-4782336`
+    version: `~16.2.0-next.1+sha-0bfd3f5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
