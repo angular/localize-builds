d7c0322e9686523757e9f8ac3c75d411bfcbf121
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-30 18:47:44 +0000

    b66a16ec4c fix(core): use `setTimeout` when coalescing tasks in Node.js (#50820)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-00f0149`
+    version: `~16.2.0-next.1+sha-b66a16e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
