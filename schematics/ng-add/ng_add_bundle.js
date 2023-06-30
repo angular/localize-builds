c3f2db6878fa58c0aee76ce358a3753c64b4ee72
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-06-30 19:04:52 +0000

    b9c28ca997 refactor(platform-browser-dynamic): Fix JSDoc comment (#50893)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.3+sha-4073cc0`
+    version: `~16.1.3+sha-b9c28ca`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
