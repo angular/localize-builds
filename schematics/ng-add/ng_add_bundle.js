865726ebf3d94968f9cba784158d1f0a14a9cf99
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-07-05 12:15:10 +0000

    5d11844a7c docs: update link to the TS config. (#50933)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-941659f`
+    version: `~16.2.0-next.1+sha-5d11844`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
