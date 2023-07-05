4b3ff721cb82b6223d78201afc89191d96ecb1b8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-07-05 12:10:13 +0000

    f9175dd3b3 docs: updated the b tag with the strong tag (#50921)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-6538e67`
+    version: `~16.2.0-next.1+sha-f9175dd`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
