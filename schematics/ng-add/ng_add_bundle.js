7c1098cdaea0d57ad3d2eb0006600f60132229e1
Dylan Hunn <dylhunn@gmail.com>
2023-07-10 14:33:41 +0000

    e35cc07a1e docs: updated the i tag with the em tag (#50923)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.4+sha-1bf99e2`
+    version: `~16.1.4+sha-e35cc07`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
