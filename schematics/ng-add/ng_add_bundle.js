634f7b81de7be173618aba63af20b8ddac20a3fd
Dylan Hunn <dylhunn@gmail.com>
2023-07-10 14:26:25 +0000

    394ca3b49a docs: Improve documentation about Injection context / inject (#49782)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-c4103cf`
+    version: `~16.2.0-next.1+sha-394ca3b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
