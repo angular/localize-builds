7088981334527f82662b44f1bdcaf99d278f52c7
Dylan Hunn <dylhunn@gmail.com>
2023-07-10 14:34:53 +0000

    875851776c refactor(compiler): Generate `attribute` and `attributeInterpolate` instructions in template pipeline (#50818)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-daea7dc`
+    version: `~16.2.0-next.1+sha-8758517`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
