f8afdc6af1ef7445848b4d0817d0a7f64b7dab05
Joey Perrott <josephperrott@gmail.com>
2023-07-11 18:14:08 +0000

    d9f832d55c ci: remove GHA lint job from required statuses (#51011)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-f1b6299`
+    version: `~16.2.0-next.1+sha-d9f832d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
