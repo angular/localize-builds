35fa9efe88d80596acde11ba1121fa8d6b3b9477
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 15:42:24 +0000

    5828eb94a4 docs: fix typos in tutorial lesson 1 (#50986) PR Close #50986

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-16ce6e7`
+    version: `~16.2.0-next.1+sha-5828eb9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
