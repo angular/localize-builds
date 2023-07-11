1253e748e4259d5fb74e2d6be615d1307014078f
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 17:27:46 +0000

    f1b629961f perf(platform-browser): do not remove renderer from cache when `REMOVE_STYLES_ON_COMPONENT_DESTROY` is enabled. (#51005)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-6c76a83`
+    version: `~16.2.0-next.1+sha-f1b6299`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
