c01a0fcb9be3cf43b04bf94f6f7ce0b22d958125
Joey Perrott <josephperrott@gmail.com>
2023-07-11 19:15:41 +0000

    25153e94f6 ci: use frozen-lockfile in CI (#51012)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-d9f832d`
+    version: `~16.2.0-next.1+sha-25153e9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
