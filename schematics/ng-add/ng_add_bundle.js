b5f7ceb8f293a2c9d7aaf4e4bba73bc0a44fee20
Dylan Hunn <dylhunn@gmail.com>
2023-07-11 15:42:16 +0000

    16ce6e7e13 docs: fix styling in tutorial lesson 3 (#50999)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-19fb3e1`
+    version: `~16.2.0-next.1+sha-16ce6e7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
