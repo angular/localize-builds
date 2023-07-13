47645c632eadbe31513a7c8642c16a77ff738cc1
Dylan Hunn <github@dylanhunn.com>
2023-07-13 18:23:35 +0000

    55d412c5b1 release: cut the v16.2.0-next.2 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-9d06fdf`
+    version: `~16.2.0-next.2+sha-55d412c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
