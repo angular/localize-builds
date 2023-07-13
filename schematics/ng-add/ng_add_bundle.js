fed6e695c35ae24278bcfad9118987741b89ec05
Dylan Hunn <dylhunn@gmail.com>
2023-07-13 16:51:11 +0000

    3b78d068ea refactor(compiler-cli): basic local compilation for components (#50545)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.1+sha-ec07131`
+    version: `~16.2.0-next.1+sha-3b78d06`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
