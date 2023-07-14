158a77e41c5eafd1b75eee839e94cda912394070
Dylan Hunn <dylhunn@gmail.com>
2023-07-14 17:03:54 +0000

    451d6c0c44 docs(docs-infra): http-server-communication typo fix (#51024)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.2+sha-8d636ad`
+    version: `~16.2.0-next.2+sha-451d6c0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
