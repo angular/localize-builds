58871445e543b9bdf59c0b2ee0b6cb97366db360
Dylan Hunn <dylhunn@gmail.com>
2023-07-14 16:56:16 +0000

    5a5bcd2565 ci: migrate devtools tests to GHA (#51008)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.2+sha-55d412c`
+    version: `~16.2.0-next.2+sha-5a5bcd2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
