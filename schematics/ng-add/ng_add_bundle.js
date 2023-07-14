279a3a567474c0985586931c14047798bfe1bc38
Dylan Hunn <dylhunn@gmail.com>
2023-07-14 16:59:37 +0000

    8d636ad6a7 docs: updated the http tag with https (#50927)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.2+sha-5a5bcd2`
+    version: `~16.2.0-next.2+sha-8d636ad`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
