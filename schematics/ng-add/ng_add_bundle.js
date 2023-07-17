9e3b3c1779d324c30dbf2372446b83947e9d9e96
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 21:18:22 +0000

    3da03cacae docs: add mention of paramsInheritanceStrategy in the router doc (#51051)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.5+sha-dea8dc0`
+    version: `~16.1.5+sha-3da03ca`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
