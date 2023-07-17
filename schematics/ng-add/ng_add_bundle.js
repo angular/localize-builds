78a38938a1dee27a6f3b566f4d2f8acab1b80b00
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 15:04:33 +0000

    c5608e5ca9 fix(http): Run fetch request out the angular zone (#50981)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.2+sha-57ea887`
+    version: `~16.2.0-next.2+sha-c5608e5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
