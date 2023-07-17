3a4dd48f6bf8ed6de5cbdf57e2f88b78c0669220
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 21:51:56 +0000

    7f71d89c9a refactor: add dylhunn to pullapprove (#51069)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.2+sha-76d22ae`
+    version: `~16.2.0-next.2+sha-7f71d89`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
