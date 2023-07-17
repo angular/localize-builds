bcb6b45ae94a1710b9dfd2a06f4ea12f2f97a7ad
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-17 21:11:55 +0000

    0e8ca7bef5 docs: add privacy policy link at the bottom of the page (#51013)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.2+sha-f1cb971`
+    version: `~16.2.0-next.2+sha-0e8ca7b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
