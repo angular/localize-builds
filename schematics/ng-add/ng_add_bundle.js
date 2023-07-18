4a8162071e031d0921773ee7180b4af9a929ef70
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-18 21:16:34 +0000

    dd43615202 build: update all non-major dependencies (#50994)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.2+sha-109737b`
+    version: `~16.2.0-next.2+sha-dd43615`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
