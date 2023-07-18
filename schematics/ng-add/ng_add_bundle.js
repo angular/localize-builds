0c5bdb18d5562b59d10c60c4b798f53be9181aa9
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-18 14:19:08 +0000

    d2b0677688 docs: fix typos and styling in lessons 7-14 (#51004)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.5+sha-ed1900e`
+    version: `~16.1.5+sha-d2b0677`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
