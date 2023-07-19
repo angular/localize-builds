0b776b8e1d9f394175fe07fb4a3ad36265b81bb3
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-19 17:48:59 +0000

    385adbbc53 build: update dependency core-js to v3 (#51099)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.2+sha-ddf1769`
+    version: `~16.2.0-next.2+sha-385adbb`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
