9784db202fe39085cd8b1a53d6daaa9154de32ad
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-20 20:14:14 +0000

    33acf4f6ea docs(docs-infra): fix rendering of `Exported from` section on Firefox. (#51116)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.3+sha-6fed684`
+    version: `~16.2.0-next.3+sha-33acf4f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
