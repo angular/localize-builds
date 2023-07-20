e6816c2bae309742064b3a92fc983742de676c83
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-20 20:36:46 +0000

    e0946b5189 docs: remove the links to docs.angular.lat (#51117)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.6+sha-47e51f6`
+    version: `~16.1.6+sha-e0946b5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
