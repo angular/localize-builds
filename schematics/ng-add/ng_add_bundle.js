738bb6f03202ea87ffe6d3a025d889e02580939f
Jessica Janiuk <jessicajaniuk@google.com>
2023-07-21 17:45:59 +0000

    7cfc36567f docs: fix typos in first app tutorial (#51115) PR Close #51115

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.6+sha-bfc6e48`
+    version: `~16.1.6+sha-7cfc365`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
