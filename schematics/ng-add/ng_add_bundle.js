c35e4faded6e30d9ec0622bf8acf9984b443c15f
Andrew Kushnir <akushnir@google.com>
2023-07-24 15:34:07 +0000

    b5cf5d22e5 docs: remove orphan migration docs (#51147)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.3+sha-8b32f89`
+    version: `~16.2.0-next.3+sha-b5cf5d2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
