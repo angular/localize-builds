811502187c92a1d7089c737240e8dfab9093fdc3
Andrew Kushnir <akushnir@google.com>
2023-07-25 17:19:29 +0000

    d886887360 refactor(core): Better use of Object.keys() (#51153)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.3+sha-daaa0a4`
+    version: `~16.2.0-next.3+sha-d886887`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
