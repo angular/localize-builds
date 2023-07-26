ba60ea9de081b2006f357dfdd1396932c317667d
Andrew Kushnir <akushnir@google.com>
2023-07-26 17:16:53 +0000

    29a41031e1 docs: fixed grammar error in vulnerability reporting (#47326)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.4+sha-d9a4ca6`
+    version: `~16.2.0-next.4+sha-29a4103`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
