4014a167099d06b9476e1c3d1f971b00a630e230
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2023-07-26 16:17:54 +0000

    07969d1dc7 release: cut the v16.1.7 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.6+sha-03f9c93`
+    version: `~16.1.7+sha-07969d1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
