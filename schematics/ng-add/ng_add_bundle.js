56b66517338e9bc024933fbe30775633546b2430
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-07-27 22:18:58 +0000

    1c553ee915 refactor(compiler): Delete some redundant types in template pipeline. (#50899)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.4+sha-bb0f3bc`
+    version: `~16.2.0-next.4+sha-1c553ee`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
