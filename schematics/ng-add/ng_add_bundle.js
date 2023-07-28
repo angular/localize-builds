3acfdd03557ad9ee8be55fabc7d46f603c1413c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-07-28 18:59:24 +0000

    50613117bd refactor(compiler): Ingest host attribute bindings in template pipeline (#51188)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.4+sha-1c553ee`
+    version: `~16.2.0-next.4+sha-5061311`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
