4e4c86b099783f159230ce0b943d66e5ad968031
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 20:18:28 +0000

    8ffa85fb5d docs: fix warning about an unknown decorator (#51237)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.7+sha-7d579b9`
+    version: `~16.1.7+sha-8ffa85f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
