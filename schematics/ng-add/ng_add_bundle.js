0047737011f3e4c55bb9320f8a5d7f705bfd91a4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-08-01 19:08:41 +0000

    63b7f52008 docs: update the html tags (#51198)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-next.4+sha-71e803a`
+    version: `~16.2.0-next.4+sha-63b7f52`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
