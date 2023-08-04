9a21b02c3669310ade15b85c2b6043fdf8f458a4
Dylan Hunn <dylhunn@gmail.com>
2023-08-04 15:44:31 +0000

    f066fec36a docs: add title tag to index file (#51266)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.8+sha-938d7a6`
+    version: `~16.1.8+sha-f066fec`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
