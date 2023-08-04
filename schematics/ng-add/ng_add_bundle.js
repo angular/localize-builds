8cdc7abe924b483000947de861f6ec7a8ac7c6e7
Dylan Hunn <dylhunn@gmail.com>
2023-08-04 20:16:37 +0000

    d4ffef215f refactor(core): introduce LView and LContainer utility functions (#51191)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.1.8+sha-f066fec`
+    version: `~16.1.8+sha-d4ffef2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
