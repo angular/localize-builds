86998dead3047dba05fc142d3d9c4b982eb3d8e8
Dylan Hunn <dylhunn@gmail.com>
2023-08-07 16:39:46 +0000

    55965cbf8c fix(core): handle hydration of view containers for root components (#51247)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.3.0-next.0+sha-107be11`
+    version: `~16.3.0-next.0+sha-55965cb`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
