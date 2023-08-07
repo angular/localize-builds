686a12e2fbd128f5a1a858c57ab384d28a934c1e
Dylan Hunn <dylhunn@gmail.com>
2023-08-07 16:41:52 +0000

    0ba2b5e5c3 refactor(animations): remove unecessary interface (#50662)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-rc.0+sha-e01549b`
+    version: `~16.2.0-rc.0+sha-0ba2b5e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
