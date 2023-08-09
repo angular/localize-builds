8bbd2d01dfcb4a8d0700b6f51daf00b14ebd488a
Dylan Hunn <dylhunn@gmail.com>
2023-08-09 17:34:52 +0000

    9998bee5f4 refactor: add `dgp1130` to DevTools PullApprove config (#51301)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0-rc.0+sha-899e51a`
+    version: `~16.2.0-rc.0+sha-9998bee`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
