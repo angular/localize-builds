bf938b4c7c668cf9bd1e3242d19e87abd92103c4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-10 00:33:24 +0000

    af0c8b8fa8 docs: add legend tag to fieldset (#51259)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0+sha-096fdd0`
+    version: `~16.2.0+sha-af0c8b8`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
