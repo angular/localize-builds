f0c587a0f897dedfceb43220c5b5b8ee02a55972
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-10 16:36:17 +0000

    232a8c1b8d fix(router): Apply named outlets to children empty paths not appearing in the URL (#51292)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0+sha-8f944e0`
+    version: `~16.2.0+sha-232a8c1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
