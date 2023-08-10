51bcf3b5dce719c1ed3da1df1819bfaa62672db1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-10 21:03:51 +0000

    36b180ade4 refactor(compiler): implement conditional block AST (#51299)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.3.0-next.0+sha-4e22a39`
+    version: `~16.3.0-next.0+sha-36b180a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
