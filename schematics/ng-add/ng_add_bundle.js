6f569918b5d9a14a05d5d5fbf1f040c818f9cb86
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-13 23:48:22 +0000

    198834b731 docs: update @self() decorator explanation (#51342)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0+sha-6a8c030`
+    version: `~16.2.0+sha-198834b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
