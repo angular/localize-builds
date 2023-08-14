5b28a1df6184f7c661a309af53d55598f7592251
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-14 16:06:07 +0000

    12dbe1a6cf ci: migrate aio jobs to GHA (#51071)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.0+sha-8693627`
+    version: `~17.0.0-next.0+sha-12dbe1a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
