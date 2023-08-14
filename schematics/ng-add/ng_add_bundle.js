de11cf85da2f31bbb1c5b788b06a23f40ba18e0a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-14 22:23:04 +0000

    074fd4a5ec build: update pullapprove (#51360)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0+sha-094d733`
+    version: `~16.2.0+sha-074fd4a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
