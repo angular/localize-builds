cb068e92c0fa09b55530161ab5074074e025c55c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 17:21:52 +0000

    422d0d5ca3 refactor(compiler): handle deferred when trigger with a pipe (#51368)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.0+sha-17c6ad7`
+    version: `~17.0.0-next.0+sha-422d0d5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
