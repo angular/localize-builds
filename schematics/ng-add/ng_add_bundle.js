b5e6a4dcf29b485039fd80f0bb6e1d695b4ddaed
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 22:53:29 +0000

    da68bead56 docs(router): Clarify how to clear secondary routes (#51376)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0+sha-543b98e`
+    version: `~16.2.0+sha-da68bea`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
