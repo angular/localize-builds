d58d90b63e9bac1ccfe0683f1f4bf8f58a864645
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 16:48:07 +0000

    9f977e0f34 docs(docs-infra): fix minor typo in standalone-migration.md (#51370) PR Close #51370

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0+sha-3e19890`
+    version: `~16.2.0+sha-9f977e0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
