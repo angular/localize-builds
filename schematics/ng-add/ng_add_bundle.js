eaf32c51ea0e83a18e8b04dc79991ca2f50541d6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 17:23:10 +0000

    03619ada72 docs(common): revert format date_pipe (#51359)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0+sha-252b111`
+    version: `~16.2.0+sha-03619ad`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
