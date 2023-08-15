1230d31900f50f9cd04f893485cad8f3a98e827d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 00:26:39 +0000

    93675dc797 feat(core): conditional built-in control flow (#51346)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.0+sha-6a60a29`
+    version: `~17.0.0-next.0+sha-93675dc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
