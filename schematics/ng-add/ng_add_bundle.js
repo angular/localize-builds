4e14d9b7d79c3d263ff41382abb7a69890c8910e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-08-15 17:06:19 +0000

    252b111d1e docs: Improve host binding docs (#51326)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.0+sha-9f977e0`
+    version: `~16.2.0+sha-252b111`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
