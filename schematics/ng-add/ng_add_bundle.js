39625027aaf7b49365345806973af823e3069d72
Andrew Kushnir <akushnir@google.com>
2023-08-17 21:10:54 +0000

    273d8e93ad docs: add instruction how to run aio locally (#51402)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.0+sha-552ea77`
+    version: `~17.0.0-next.0+sha-273d8e9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
