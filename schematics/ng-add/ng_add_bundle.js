d4a71772d31a9e256c95baed6df456170f8dfe47
Andrew Kushnir <akushnir@google.com>
2023-08-17 17:33:04 +0000

    a43c0772ea fix(common): Allow safeUrl for ngSrc in NgOptimizedImage (#51351)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.1+sha-4b46819`
+    version: `~16.2.1+sha-a43c077`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
