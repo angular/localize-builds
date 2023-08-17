eafa39a45d85ceb5deae5344d2d802c3a4f5f540
Andrew Kushnir <akushnir@google.com>
2023-08-17 17:27:29 +0000

    5fb5f087e3 docs(docs-infra): fix `docs-watch` script (#51316)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.0+sha-77504f1`
+    version: `~17.0.0-next.0+sha-5fb5f08`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
