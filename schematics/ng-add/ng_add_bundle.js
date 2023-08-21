6b6fb97ea2a7a53fa8d68bde5a3cb1ab53c17cfd
Andrew Kushnir <akushnir@google.com>
2023-08-21 23:46:16 +0000

    9977727f73 docs: fix typos in inputs-outputs guide (#51428) PR Close #51428

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.0+sha-b7d25bb`
+    version: `~17.0.0-next.0+sha-9977727`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
