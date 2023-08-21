68512634fadf533af0d04ff8750705f246e448ac
Andrew Kushnir <akushnir@google.com>
2023-08-21 20:11:33 +0000

    38671f7c14 docs(animations): update animation docs to be standalone first (#51390)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.0+sha-3a68647`
+    version: `~17.0.0-next.0+sha-38671f7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
