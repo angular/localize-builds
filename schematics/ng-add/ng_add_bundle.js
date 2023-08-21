3aa885717a711ff8985d1864c4d1ea6613aae580
Andrew Kushnir <akushnir@google.com>
2023-08-21 23:43:40 +0000

    119eaa5651 docs: add lang attribute in html files (#51269)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.0+sha-803fc7f`
+    version: `~17.0.0-next.0+sha-119eaa5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
