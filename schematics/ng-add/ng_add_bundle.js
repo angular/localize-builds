cde5b10a2eea82fba17b6088bd030e0994b8cf97
Andrew Kushnir <akushnir@google.com>
2023-08-21 15:34:33 +0000

    3a686478ac docs(service-worker): Miss a comma in sample (#3) (#51336)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.0+sha-a9b3c00`
+    version: `~17.0.0-next.0+sha-3a68647`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
