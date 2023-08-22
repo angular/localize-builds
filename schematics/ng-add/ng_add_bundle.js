c8a2f9d3f14a1023b6cce125f5febd90b0de2a48
Andrew Kushnir <akushnir@google.com>
2023-08-22 15:16:33 +0000

    157415831c refactor(router): Have router subscribe to transition events to update state (#48427)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.1+sha-4fe88cf`
+    version: `~16.2.1+sha-1574158`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
