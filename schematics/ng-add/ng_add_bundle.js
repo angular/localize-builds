8624fc5babdc9807cd3bdc3caea52dba1c412184
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-24 16:19:05 +0000

    26ad6d50cc build: add AIO_PAYLOAD_TOKEN env variable (#51489)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.1+sha-fa72384`
+    version: `~17.0.0-next.1+sha-26ad6d5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
