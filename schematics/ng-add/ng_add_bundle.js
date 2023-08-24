ac68050dcea8474b93774a49e1f964aa0b9576d2
Joey Perrott <josephperrott@gmail.com>
2023-08-24 16:54:55 +0000

    6d29ab7818 docs(docs-infra): set meta description per page (#51487)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.1+sha-26ad6d5`
+    version: `~17.0.0-next.1+sha-6d29ab7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
