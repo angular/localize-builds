4f033487cd3159530c268e0dea6275e86f44adf9
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 22:03:22 +0000

    c2d859241b refactor(compiler): Enable additional passing tests (#51498)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.1+sha-cdcfa09`
+    version: `~17.0.0-next.1+sha-c2d8592`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
