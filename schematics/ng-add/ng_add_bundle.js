7b2427a3a6610dc548e9e585ea0ee06cb8f3ba2e
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 16:55:54 +0000

    0ad738658e docs: remove MessageService information from HeroService section (#51501) PR Close #51501

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.2+sha-b7732f7`
+    version: `~16.2.2+sha-0ad7386`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
