81b4ab5141c8a158e95a2df0ce8a01586009b63b
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-28 18:46:30 +0000

    9753347f8a docs: Add standalone edits to "Testing Components" (#51451)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.1+sha-3dc74f3`
+    version: `~17.0.0-next.1+sha-9753347`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
