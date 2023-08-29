94758f2cf9ba79d0aff8d1799b98bfc303db5773
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 19:18:57 +0000

    6e5b865710 build: use pin for devcontainer base image (#51556)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.2+sha-0c7c852`
+    version: `~16.2.2+sha-6e5b865`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
