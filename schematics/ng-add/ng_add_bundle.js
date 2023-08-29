c28ccd1ae5f7cdbfe79b15c70da549d1fd67fce8
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 16:57:37 +0000

    20d62603c2 fix(core): handle hydration of view containers that use component hosts as anchors (#51456)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.2+sha-14941b9`
+    version: `~16.2.2+sha-20d6260`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
