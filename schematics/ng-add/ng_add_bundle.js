f1e950d2ee4a593afc77c79298a2875a6c0d16b0
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 21:24:15 +0000

    1f7e7dff50 fix(docs-infra): escape the `.` character in regex (#51555)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.1+sha-8ef5cc6`
+    version: `~17.0.0-next.1+sha-1f7e7df`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
