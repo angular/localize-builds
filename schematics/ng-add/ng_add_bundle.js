66afb087572ea3dce07b61e28e3efc27b5023aa1
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 20:37:25 +0000

    97a40958b5 build: update security related package version for aio (#51562)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.2+sha-dbd761f`
+    version: `~16.2.2+sha-97a4095`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
