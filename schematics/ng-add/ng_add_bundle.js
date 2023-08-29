d45643f8400c30c22d4603b11d5575767cf16066
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 23:39:49 +0000

    629f0146b8 docs: fix typos in search results component (#51433) PR Close #51433

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.1+sha-e027a59`
+    version: `~17.0.0-next.1+sha-629f014`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
