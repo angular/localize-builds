06678fa4035ccd97e348616702b26ab7958c3101
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 23:42:59 +0000

    fa29eb84a3 docs: fix casing of a11y labels in main page (#51431) PR Close #51431

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.1+sha-8c77240`
+    version: `~17.0.0-next.1+sha-fa29eb8`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
