2b0f978bdcc46739c81a3e92f55c8eb686b9a016
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-29 20:00:50 +0000

    dbd761f528 fix(compiler-cli): correct incomplete escaping (#51557)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.2+sha-6e5b865`
+    version: `~16.2.2+sha-dbd761f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
