6da65e3c70ae5a71d8cbbfd045c7aa35d784ffcb
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-30 19:53:55 +0000

    2c11b83487 ci: migrate zonejs tests to gha (#51352)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.2+sha-e6235ac`
+    version: `~17.0.0-next.2+sha-2c11b83`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
