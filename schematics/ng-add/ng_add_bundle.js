4784743e64eed61a3e71b7f07444bca39fbb93bd
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-30 19:52:09 +0000

    e6235accb1 docs: remove unreachable/incorrect contributor links (#51350)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.1+sha-23996ce`
+    version: `~17.0.0-next.2+sha-e6235ac`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
