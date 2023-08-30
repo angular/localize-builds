8f298c55a6ea220f7d1acf59b124b9dd7933efe2
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2023-08-30 18:10:19 +0000

    587093c3a5 release: cut the v16.2.3 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.2+sha-dc4f77c`
+    version: `~16.2.3+sha-587093c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
