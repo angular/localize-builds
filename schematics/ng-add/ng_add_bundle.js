0a19fa7744d03809f7c26a442cf299442671a8f0
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 20:35:35 +0000

    05e4dbb859 refactor(core): Use intersections on branded types. (#49702)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.2+sha-3c28506`
+    version: `~17.0.0-next.2+sha-05e4dbb`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
