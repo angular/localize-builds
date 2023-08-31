9b0b19bcc3b7cc13f91ac9dae970c91b3a9f4162
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 17:25:28 +0000

    055448e9c6 fix(docs-infra): fix incomplete escaping (#51604)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.3+sha-3fd41a1`
+    version: `~16.2.3+sha-055448e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
