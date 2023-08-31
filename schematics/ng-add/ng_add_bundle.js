6523ab75a198b05908a9f732f1a8913370d3ea24
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 16:36:42 +0000

    544c778b98 ci: fix permissions issue in zonejs ci test (#51600)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.2+sha-eab9216`
+    version: `~17.0.0-next.2+sha-544c778`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
