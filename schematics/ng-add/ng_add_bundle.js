dc47788c7940a9f17161074a1c67ee474b003045
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 17:42:25 +0000

    23db70f2d0 build: update dependency archiver to v6 (#51605)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.3+sha-055448e`
+    version: `~16.2.3+sha-23db70f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
