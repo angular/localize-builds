ea5c561d260cf7b98dd7487c65b51f0fd249877f
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 16:45:12 +0000

    2b518bb899 build: update actions/checkout digest to f43a0e5 (#51576)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.2+sha-544c778`
+    version: `~17.0.0-next.2+sha-2b518bb`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
