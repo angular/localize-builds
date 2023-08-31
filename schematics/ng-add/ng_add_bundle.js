96f09df6a18ae9fa9e81a5ab01644158440f3482
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 18:47:58 +0000

    a1bad49703 refactor(animations): cleanup ahead of animation lazy loading (#51249)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.2+sha-78afe88`
+    version: `~17.0.0-next.2+sha-a1bad49`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
