54a60e5d5f451ed6cc400809e61d3d9382beed60
Jessica Janiuk <jessicajaniuk@google.com>
2023-08-31 16:55:21 +0000

    f464e39364 fix(router): Ensure title observable gets latest values (#51561)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.2+sha-e4d2018`
+    version: `~17.0.0-next.2+sha-f464e39`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
