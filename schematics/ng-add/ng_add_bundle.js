9d2242ac8ef60581ce90dfc210d2561718cee621
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 15:22:13 +0000

    fe1a50c63f docs(docs-infra): mixin generated same multiple css (#51528)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.2+sha-dc4b4aa`
+    version: `~17.0.0-next.2+sha-fe1a50c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
