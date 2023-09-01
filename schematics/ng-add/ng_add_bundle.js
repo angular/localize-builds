bffcfd731035509b5b82940bba3c1f7fe8b03d37
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 16:28:03 +0000

    5a323265cb docs: fix missing information in createComponent (#51493)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.2+sha-4555290`
+    version: `~17.0.0-next.2+sha-5a32326`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
