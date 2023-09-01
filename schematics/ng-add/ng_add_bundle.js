ce6fcacc0d935d4053acef614eac6353aebf0990
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 14:42:20 +0000

    f56b6555bd refactor(core): switch signals to a refcounting algorithm (#51226)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.3+sha-7842d06`
+    version: `~16.2.3+sha-f56b655`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
