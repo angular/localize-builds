76545c1245d358a3493c26a1175c2e7ff3149aae
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-01 17:19:58 +0000

    dcf6b01f0d docs: update the sentence to express its meaning (#51622)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.3+sha-8ce22eb`
+    version: `~16.2.3+sha-dcf6b01`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
