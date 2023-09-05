9ea4b7f2b8af75da53a04b4239a97d2324d1185c
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 14:26:51 +0000

    fcfa173270 docs: add Connie Leung to GDE contributors list (#51476)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.3+sha-dcf6b01`
+    version: `~16.2.3+sha-fcfa173`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
