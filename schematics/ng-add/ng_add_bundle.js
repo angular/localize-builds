bd3fa5e3354c826d542a42a84555a4a89601fcfc
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 18:26:44 +0000

    4b54947c97 fix(devtools): use the __ignore_ng_zone__ flag in devtools message bus' to prevent CD loop (#51339)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.2+sha-2d96dc2`
+    version: `~17.0.0-next.2+sha-4b54947`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
