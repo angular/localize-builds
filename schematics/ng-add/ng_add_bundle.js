6e265835e8f8ab2962444f8a4e1908ec4d92a7c2
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 18:27:34 +0000

    7c068861b7 refactor(compiler): output arrow functions for deferred dependencies functions (#51650)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.2+sha-4b54947`
+    version: `~17.0.0-next.2+sha-7c06886`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
