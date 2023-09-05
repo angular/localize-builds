9d9db98507ce2f986bf946f993357a4e169d2a52
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-05 20:19:46 +0000

    b6844a9056 docs: Added missing explanation of implements OnInit (#51634)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.2+sha-ca1dac2`
+    version: `~17.0.0-next.2+sha-b6844a9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
