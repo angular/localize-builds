2b576218f6cbe6a32d0f04b0749ac6f308d19598
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-06 15:43:41 +0000

    fe0b793fdc refactor(router): Share more code in the route matching utilities (#51543)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.2+sha-88a0af6`
+    version: `~17.0.0-next.2+sha-fe0b793`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
