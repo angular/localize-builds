185d3f33675e322432e6c6513cfe17b2cf0991c1
Jessica Janiuk <jessicajaniuk@google.com>
2023-09-06 15:42:30 +0000

    8ad21ccfb8 docs: Add info about hydration caching. (#51648)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.2+sha-970d68f`
+    version: `~17.0.0-next.2+sha-8ad21cc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
