b381ff523c1d67678f1d9745fc01cd5cf680ec80
Andrew Kushnir <akushnir@google.com>
2023-09-07 20:59:29 +0000

    2579592be6 build: migrate `project_id` option to use `bes_instance_name` (#51696)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.4+sha-7ce89a2`
+    version: `~16.2.4+sha-2579592`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
