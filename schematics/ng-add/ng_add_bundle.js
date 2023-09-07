25edd65becf2998f68cdf3f51b7248683fea8277
Andrew Kushnir <akushnir@google.com>
2023-09-07 17:14:14 +0000

    3e0e6ed60f docs: update Dynamic Component loader docs to rely on `ngComponentOutlet` (#49915)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.3+sha-3f2d783`
+    version: `~17.0.0-next.3+sha-3e0e6ed`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
