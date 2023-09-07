8c02cf45bb6e3a5e40578eef940fbf1b61a5b9cd
Andrew Kushnir <akushnir@google.com>
2023-09-07 17:17:23 +0000

    d14560fa06 docs: migrate dynamic-form guide and code to standalone (#51540)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.3+sha-763314f`
+    version: `~17.0.0-next.3+sha-d14560f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
