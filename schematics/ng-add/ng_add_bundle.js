715cfe08472519d813e54bb4ad7da234d2a6bfe4
Andrew Kushnir <akushnir@google.com>
2023-09-07 17:14:05 +0000

    3f2d783f65 docs: migrate toh-pt3 to standalone (#51581)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.3+sha-8cd9663`
+    version: `~17.0.0-next.3+sha-3f2d783`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
