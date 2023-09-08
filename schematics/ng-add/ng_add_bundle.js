d9f9d731a11b28495770aa7e60f90bf438e381fa
Andrew Kushnir <akushnir@google.com>
2023-09-08 22:01:20 +0000

    05762b9fff refactor(localize): ignore code duplication error with compiler package (#51500) (#51558)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.3+sha-0d9fd6d`
+    version: `~17.0.0-next.3+sha-05762b9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
