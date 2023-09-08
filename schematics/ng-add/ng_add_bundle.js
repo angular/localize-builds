33dbbf5951a9d3f1695c5053ab676e901d40450d
Andrew Kushnir <akushnir@google.com>
2023-09-08 16:38:19 +0000

    b3edcda9e6 build: attempt to deflake windows tests (#51701)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.3+sha-eb0137d`
+    version: `~17.0.0-next.3+sha-b3edcda`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
