7e3c675a3f20cc222e5b68b1fb99b7c9c26307e2
Andrew Kushnir <akushnir@google.com>
2023-09-11 16:32:36 +0000

    ac56efa410 release: cut the zone.js-0.13.2 release (#51689)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.3+sha-2d5b6fa`
+    version: `~17.0.0-next.3+sha-ac56efa`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
