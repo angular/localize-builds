a18ef0a16d4655b979f24db13c51cbb371b9edd4
Andrew Kushnir <akushnir@google.com>
2023-09-11 16:32:00 +0000

    2d5b6fad4e fix(docs-infra): Ensure experimental tag shows up on docs (#51712)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.3+sha-05762b9`
+    version: `~17.0.0-next.3+sha-2d5b6fa`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
