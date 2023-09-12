bc54976c56b9f46eb532c41b6e00a38b1b625cd2
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:32:17 +0000

    b08323747e docs: Migrate 2nd set of template guide pages and code to Standalone (#51632)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.3+sha-f6ccb16`
+    version: `~17.0.0-next.3+sha-b083237`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
