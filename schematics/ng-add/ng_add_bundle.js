e45e3df30a60c67ab37201ac3ca410a4752f0f45
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:33:17 +0000

    7b0bfc1dd8 docs: migrate testing to standalone (#51684)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.3+sha-b083237`
+    version: `~17.0.0-next.3+sha-7b0bfc1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
