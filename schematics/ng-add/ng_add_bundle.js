aee6f12ae73c8639d1ddc53b5b976a48596588d6
Andrew Kushnir <akushnir@google.com>
2023-09-12 15:24:09 +0000

    38c9f08c8d refactor(core): decouple effects from change detection (#51049)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.3+sha-e86d6db`
+    version: `~17.0.0-next.3+sha-38c9f08`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
