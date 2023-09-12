bcb51d52a0a76c1b8b1fd5b45c3054891b10b4e8
Andrew Kushnir <akushnir@google.com>
2023-09-12 20:05:58 +0000

    635318fd61 refactor: add readonly to public InjectionToken types (#51407)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.3+sha-92113d7`
+    version: `~17.0.0-next.3+sha-635318f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
