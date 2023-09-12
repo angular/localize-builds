6d93bee24b4801f467f0b7952113ac1c76ac65db
Andrew Kushnir <akushnir@google.com>
2023-09-12 19:36:00 +0000

    f6f3783a3d docs: Fix misspelling in note of `git commit --all` (#51735)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.4+sha-38c6fa1`
+    version: `~16.2.4+sha-f6f3783`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
