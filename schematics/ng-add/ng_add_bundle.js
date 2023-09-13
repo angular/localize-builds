c3387b0f5224931b2b69c3ca6ce1599873047ea9
Andrew Kushnir <akushnir@google.com>
2023-09-13 17:58:52 +0000

    2a1723c945 test(compiler): fix broken integrity check (#51751)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.4+sha-06bbc2f`
+    version: `~17.0.0-next.4+sha-2a1723c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
