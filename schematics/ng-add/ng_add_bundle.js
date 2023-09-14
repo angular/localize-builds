ac2431b0bbf7c16069cc9521d3461b2d0bcba20d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-14 10:20:27 +0000

    70e88f8a13 refactor(compiler): address missing override modifier tslint failure (#51752)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.4+sha-3a1c57d`
+    version: `~17.0.0-next.4+sha-70e88f8`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
