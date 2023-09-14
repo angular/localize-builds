74ce0e38e77d02f6f5ab99db3985b54496356845
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-14 11:00:55 +0000

    7b40d74fb4 refactor(core): let reactive node prototypes conform to their type (#51722) (#51772)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.5+sha-65d6915`
+    version: `~16.2.5+sha-7b40d74`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
