8d4c9f706258cd7baad43e57e125fe124bec1fd2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-15 07:12:55 +0000

    06bf80dd04 ci: use head_ref for concurrency group (#51784)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.4+sha-a2681c7`
+    version: `~17.0.0-next.4+sha-06bf80d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
