94e10b9ddbd435a29550cbf1851b8e33ed382be1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-15 07:17:52 +0000

    88ac76fb34 build: update dependency conventional-changelog to v5 (#51606)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.4+sha-d79db4a`
+    version: `~17.0.0-next.4+sha-88ac76f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
