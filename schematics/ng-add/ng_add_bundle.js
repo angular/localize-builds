3fcce2ecf3f609e96d8529ad81d77b5dc2aa6283
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-15 09:53:30 +0000

    cb7acdf84d build: update actions/cache digest to 704facf (#51787)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.4+sha-c6b9a3e`
+    version: `~17.0.0-next.4+sha-cb7acdf`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
