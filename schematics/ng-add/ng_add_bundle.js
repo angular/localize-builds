8b229a0ba81a51a3b9974ef51028d3890133403e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-15 14:03:34 +0000

    3b46066ff0 refactor: ensure top-level symbols are marked as side-effect free (#51776)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.5+sha-b0b004e`
+    version: `~16.2.5+sha-3b46066`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
