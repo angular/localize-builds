e94935250616d0f03eab7e2f727b8fcea0387e98
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-18 08:56:27 +0000

    bf1f9a0897 test(compiler): attempt to deflake windows tests (#51804)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.5+sha-3b46066`
+    version: `~16.2.5+sha-bf1f9a0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
