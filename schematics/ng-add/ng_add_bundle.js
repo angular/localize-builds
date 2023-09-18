34656523909bcab9f2e0c361c07b8a34c70b753c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-18 13:46:42 +0000

    03127fc870 refactor(core): ensure reactive node constants are considered pure (#51809) (#51813)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.5+sha-bf1f9a0`
+    version: `~16.2.5+sha-03127fc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
