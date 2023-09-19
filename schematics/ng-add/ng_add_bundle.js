011b51bc820d93290a63e8b53995227a9b2c35a4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-19 10:27:26 +0000

    5810827de4 build: update scorecard action dependencies (#51817)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.5+sha-03127fc`
+    version: `~16.2.5+sha-5810827`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
