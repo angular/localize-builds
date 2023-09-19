d700ea415a27f373e1bce3b12f0f974a819e8960
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-19 10:30:56 +0000

    8a2dc0f2e0 build: update actions/checkout action to v4 (#51793)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.4+sha-e5a0116`
+    version: `~17.0.0-next.4+sha-8a2dc0f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
