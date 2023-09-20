e2e3ca0d513c2d514a0da02a34656f82df7d62a8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-20 18:00:56 +0000

    6e6452d604 release: cut the v16.2.6 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.5+sha-4ccd845`
+    version: `~16.2.6+sha-6e6452d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
