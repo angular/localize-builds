b3d875dbbb490b171d719c9611d2e88e8e0bb1d5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-09-20 18:16:01 +0000

    0a4f18a419 release: cut the v17.0.0-next.5 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.4+sha-bc296a1`
+    version: `~17.0.0-next.5+sha-0a4f18a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
