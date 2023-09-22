a0e0cb7bdd256fcf0921d226e1c970a5d41dd4cb
Dylan Hunn <dylhunn@gmail.com>
2023-09-22 19:25:16 +0000

    0598613950 refactor(animations): deprecation of `AnimationDriver.NOOP` (#51843)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.5+sha-e5d327d`
+    version: `~17.0.0-next.5+sha-0598613`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
