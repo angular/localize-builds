88dd90bc67613685c4cd16889ad62b22cf887a74
Dylan Hunn <dylhunn@gmail.com>
2023-09-22 16:58:39 +0000

    3c0577f991 perf(platform-browser): disable styles of removed components instead of removing (#51808)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.5+sha-86e9146`
+    version: `~17.0.0-next.5+sha-3c0577f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
