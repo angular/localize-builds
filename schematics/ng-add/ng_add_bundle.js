e40400586c6f0f67d0fe51dd58da6be69b0f4dd1
Dylan Hunn <dylhunn@gmail.com>
2023-09-22 19:28:09 +0000

    3cbb2a8ecf feat(core): implement deferred block interaction triggers (#51830)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.5+sha-0598613`
+    version: `~17.0.0-next.5+sha-3cbb2a8`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
