47fa9d08b41267bc37518ee48a17a9cc8248d04f
Dylan Hunn <dylhunn@gmail.com>
2023-09-22 16:58:14 +0000

    86e91463af feat(router): Add option to skip the first view transition (#51825)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.5+sha-917203d`
+    version: `~17.0.0-next.5+sha-86e9146`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
