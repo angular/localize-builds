8a77130b086cc7b6860d92ec065dcb6e1e98c73b
Dylan Hunn <dylhunn@gmail.com>
2023-09-22 17:01:03 +0000

    663703946e fix(bazel): allow setting `_enabledBlockTypes` angular compiler option (#51862)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.6+sha-6e6452d`
+    version: `~16.2.6+sha-6637039`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
