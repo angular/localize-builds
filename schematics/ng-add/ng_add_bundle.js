7cf51d8b05e9d5f3c8ec2d6c77a836a0626fab43
Dylan Hunn <dylhunn@gmail.com>
2023-09-26 16:19:49 +0000

    b771539eb3 build: improve results logging in perf script (#51902)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.5+sha-5f6cc7c`
+    version: `~17.0.0-next.5+sha-b771539`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
