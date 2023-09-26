214c69e518b0da1d5ba3b35a8379671f3d882bce
Dylan Hunn <dylhunn@gmail.com>
2023-09-26 22:31:12 +0000

    4b38e9a1ab refactor(core): add batching for defer blocks with `on idle` conditions (#51750)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.5+sha-ce1b915`
+    version: `~17.0.0-next.5+sha-4b38e9a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
