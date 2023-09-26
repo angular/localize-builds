c7e85896634be74fae9123a3ea2e2957b757cd4b
Dylan Hunn <dylhunn@gmail.com>
2023-09-26 18:09:40 +0000

    c3cb26527e refactor(compiler): Add source maps in template pipeline for expressions and additional ops (#51877)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.5+sha-f91f222`
+    version: `~17.0.0-next.5+sha-c3cb265`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
