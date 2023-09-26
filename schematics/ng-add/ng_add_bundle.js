de415133c6d9c1747cb8f6737f77e1a01039653f
Dylan Hunn <dylhunn@gmail.com>
2023-09-26 16:21:00 +0000

    dcaad169ec fix(service-worker): throw a critical error when `handleFetch` fails (#51885)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.5+sha-8be2c48`
+    version: `~17.0.0-next.5+sha-dcaad16`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
