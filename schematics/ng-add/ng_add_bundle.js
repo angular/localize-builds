52c8a541d1dde17e5d7839e94c9d07fa19062623
Dylan Hunn <dylhunn@gmail.com>
2023-09-26 22:33:33 +0000

    c7127b98b5 feat(core): add schematic to escape block syntax characters (#51905)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.5+sha-ac1afd8`
+    version: `~17.0.0-next.5+sha-c7127b9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
