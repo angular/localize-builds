790a2e435e1f1e220a4e8eb1ec89314329e5af86
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-09-29 21:16:22 +0000

    408d3b44c7 refactor(compiler): Implement `if` aliases in template pipeline (#51931)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.6+sha-8d09e9e`
+    version: `~17.0.0-next.6+sha-408d3b4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
