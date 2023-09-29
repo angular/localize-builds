fe47466aa7ea4f40f8f14fee815653e7381a2457
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-09-29 18:00:32 +0000

    81e7f5be03 test(animations): update the integration test (#50738)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.6+sha-e1728a2`
+    version: `~17.0.0-next.6+sha-81e7f5b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
