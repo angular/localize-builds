0c032962959a1c398279bec70cfd8beb4b5cac7b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-09-29 18:04:40 +0000

    8d09e9e013 test: Update golden partial file (#51876)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.6+sha-47e841c`
+    version: `~17.0.0-next.6+sha-8d09e9e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
