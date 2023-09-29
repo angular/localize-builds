9b83b7431d51ea604141e8f7a77df7d4e4a17a2c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-09-29 22:32:56 +0000

    a54713c831 feat(core): implement ɵgetInjectorMetadata debug API (#51900)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.6+sha-6b6a44c`
+    version: `~17.0.0-next.6+sha-a54713c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
