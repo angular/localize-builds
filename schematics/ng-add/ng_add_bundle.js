a36de1adade50223851f7ae4c91aeef6333e21ff
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-02 15:29:22 +0000

    781bb91f43 build: lock file maintenance (#51838)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.7+sha-82e8e5e`
+    version: `~16.2.7+sha-781bb91`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
