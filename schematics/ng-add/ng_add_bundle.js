443364587984c3855fa4f6322cda1d49f0d49ff3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-02 15:45:04 +0000

    e368d81896 refactor(core): invoke `@defer (on idle)` callback in NgZone (#51975)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.6+sha-c4d77fd`
+    version: `~17.0.0-next.6+sha-e368d81`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
