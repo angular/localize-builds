27ef50a099d1979af0d8f907c021f404e0754b02
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-02 15:49:39 +0000

    96141c6ad6 ci: add slack messages for failed aio deployments (#51956)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.7+sha-781bb91`
+    version: `~16.2.7+sha-96141c6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
