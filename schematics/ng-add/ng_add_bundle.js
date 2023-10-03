41e686383c05fcdd0f91548be92b10dac4c1637d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-03 18:58:42 +0000

    1beef49d80 fix(compiler): update the minVersion if component uses block syntax (#51979)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.6+sha-e9c3790`
+    version: `~17.0.0-next.6+sha-1beef49`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
