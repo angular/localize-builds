d0351735d0bdefe10a1eee85323c455aeba12bfd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 14:39:11 +0000

    8e7feefb5b build: align schematics tsconfig with rest of the project (#51969)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.6+sha-650ab4f`
+    version: `~17.0.0-next.6+sha-8e7feef`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
