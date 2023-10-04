9e8cb3f04f37d59301dd30cfe587003afa86b59d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 18:38:22 +0000

    08482f2c7d fix(language-service): Retain correct language service when `ts.Project` reloads (#51912)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.6+sha-422a3db`
+    version: `~17.0.0-next.6+sha-08482f2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
