de9ae0a83c8076c6213e234600cd9eb7da7b53cb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 15:25:27 +0000

    7426948ff6 refactor(common): update `NgTemplateOutlet` to no longer rely on context swapping (#51887)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.6+sha-d77c4b2`
+    version: `~17.0.0-next.6+sha-7426948`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
