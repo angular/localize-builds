7651ca09e2e7bd1e01f120607fb7b1270ce76287
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 18:48:11 +0000

    dde3fdabbd feat(common): upgrade warning to logged error for lazy-loaded LCP images using NgOptimizedImage (#52004)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.6+sha-4611b70`
+    version: `~17.0.0-next.6+sha-dde3fda`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
