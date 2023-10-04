cf7582306b5be712131449ddbe8fece3b4321876
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-04 14:42:11 +0000

    334fe60c17 refactor(core): Update view flags so only one number needs adjustment (#51839)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.7+sha-96141c6`
+    version: `~16.2.7+sha-334fe60`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
