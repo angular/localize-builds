162d791c236a22092875b2f652e2d9746df408ea
Andrew Scott <atscott01@gmail.com>
2023-10-05 20:16:09 +0000

    40c53577b8 refactor(compiler): introduce unknown block node (#52047)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-ced66d4`
+    version: `~17.0.0-next.7+sha-40c5357`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
