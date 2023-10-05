0ec381347f7bcd52fb1018fadcdf9a7caf8c13e5
Andrew Scott <atscott01@gmail.com>
2023-10-05 20:41:46 +0000

    caa8eb28d5 refactor(core): add `after` and `minimum` parameter support to `@defer` blocks (#52009)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-40c5357`
+    version: `~17.0.0-next.7+sha-caa8eb2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
