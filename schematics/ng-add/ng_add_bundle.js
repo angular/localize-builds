ddfd5e182ade89f3a4bf715eb75919be70f75c66
Andrew Scott <atscott01@gmail.com>
2023-10-05 18:01:21 +0000

    2a10050bea ci: migrate snapshot publishing from CircleCI to GHA (#51957)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.8+sha-5122729`
+    version: `~16.2.8+sha-2a10050`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
