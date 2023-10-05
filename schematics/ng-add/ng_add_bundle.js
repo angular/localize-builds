db41d0d0dfa3a4b62bcb3653b7b0167d8b8e3a74
Andrew Scott <atscott01@gmail.com>
2023-10-05 18:01:50 +0000

    2da3551a70 feat(benchpress): report gc and render time spent in script (#50771)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-a3b0891`
+    version: `~17.0.0-next.7+sha-2da3551`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
