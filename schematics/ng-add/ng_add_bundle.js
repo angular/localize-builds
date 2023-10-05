00994f9d48672a2523a9400c34940f9e94ca823f
Andrew Scott <atscott01@gmail.com>
2023-10-05 18:14:25 +0000

    ced66d4007 revert: fix(core): allow `toSignal` in reactive contexts (#52049)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-2da3551`
+    version: `~17.0.0-next.7+sha-ced66d4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
