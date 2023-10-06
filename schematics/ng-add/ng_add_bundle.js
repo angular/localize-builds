859839c7c1a2b2ba9e96ba8cc054262b4b47618e
Andrew Scott <atscott01@gmail.com>
2023-10-06 15:40:53 +0000

    f06d97e86d test(core): await all async work to be completed before checking test results (#52064)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-22fa9fe`
+    version: `~17.0.0-next.7+sha-f06d97e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
