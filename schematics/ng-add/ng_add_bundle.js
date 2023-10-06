a9caf1b0c1f468a3dbe9eb1fa257f39e5c319117
Andrew Scott <atscott01@gmail.com>
2023-10-06 20:10:52 +0000

    2e16b0e0ea ci: update `@defer` symbol test golden file (#52088)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-048f400`
+    version: `~17.0.0-next.7+sha-2e16b0e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
