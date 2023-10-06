89d477738feb62e1bd7793519cdb27f94a31de30
Andrew Scott <atscott01@gmail.com>
2023-10-06 00:03:10 +0000

    22fa9fe488 docs: recommend enabling `fetch` support in HttpClient when using SSR (#52056)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-05d1fac`
+    version: `~17.0.0-next.7+sha-22fa9fe`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
