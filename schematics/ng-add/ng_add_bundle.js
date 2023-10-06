a5c6d2cd96eb4352a03cf07ec579cce3f0b3f38c
Andrew Scott <atscott01@gmail.com>
2023-10-06 17:38:50 +0000

    b6b9eae7e7 Revert "fix(core): drop mutate function from the signals public API (#51821)" (#52081)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-17078a3`
+    version: `~17.0.0-next.7+sha-b6b9eae`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
