c83f54bb2205b95064e42a13eac7cf0540f3b440
Andrew Scott <atscott01@gmail.com>
2023-10-06 19:21:33 +0000

    048f400efc feat(core): add warnings for oversized images and lazy-lcp (#51846)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-b6b9eae`
+    version: `~17.0.0-next.7+sha-048f400`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
