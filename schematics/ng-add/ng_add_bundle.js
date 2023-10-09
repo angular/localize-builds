c09e682e91abc648335ab97853e3ed919cd88c3c
Andrew Scott <atscott01@gmail.com>
2023-10-09 19:10:30 +0000

    e63e3db80a build: update all non-major dependencies (#52024)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.8+sha-5f433f6`
+    version: `~16.2.8+sha-e63e3db`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
