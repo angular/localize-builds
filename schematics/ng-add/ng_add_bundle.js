e217b87d92ed1279a39bd0a7b0ceb66bcb10cce8
Andrew Scott <atscott01@gmail.com>
2023-10-10 19:43:42 +0000

    427705e6c4 docs: remove the developer survey 2023 banner (#52148)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.8+sha-2bfd28c`
+    version: `~16.2.8+sha-427705e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
