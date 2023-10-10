0059b3b294056bf4f5f329beac956a8421da7cae
Andrew Scott <atscott01@gmail.com>
2023-10-10 18:44:06 +0000

    21b085d refactor(compiler): capture unknown blocks inside switch (#52136)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-9bf6495`
+    version: `~17.0.0-next.7+sha-21b085d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
