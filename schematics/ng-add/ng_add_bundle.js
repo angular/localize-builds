86664d272fc79e10dd0d8d909db197b5dc62c8de
Andrew Scott <atscott01@gmail.com>
2023-10-10 19:08:27 +0000

    75d610d fix(platform-browser): set animation properties when using async animations. (#52087)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-21b085d`
+    version: `~17.0.0-next.7+sha-75d610d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
