20ba0357023e687152a8c650c9a66599cea663a0
Andrew Scott <atscott01@gmail.com>
2023-10-10 22:41:24 +0000

    51a5baace3 fix(forms): reset() call with null values on nested group (#48830)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.8+sha-7d8b3f4`
+    version: `~16.2.8+sha-51a5baa`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
