23b6ccaadbd24d6c236fb7b31da1fa5f480e6f5f
Andrew Scott <atscott01@gmail.com>
2023-10-10 16:46:12 +0000

    503e67dca2 refactor(compiler): clean up compatibility code for old TS versions (#52099)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-229331e`
+    version: `~17.0.0-next.7+sha-503e67d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
