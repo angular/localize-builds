f9635283bdc7dfa45373e55989fff23ca9537e45
Andrew Scott <atscott01@gmail.com>
2023-10-10 20:19:13 +0000

    965ce5a feat(migrations): Schematics for `TransferState`, `StateKey` and `makeStateKey` migration. (#49594)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-1934524`
+    version: `~17.0.0-next.7+sha-965ce5a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
