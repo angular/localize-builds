1cabbb7a34ca575c96d3a7b5cdce903c64cba497
Andrew Scott <atscott01@gmail.com>
2023-10-10 21:15:27 +0000

    11588a1 build: update dependency @types/jasmine to v5 (#52060)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.7+sha-965ce5a`
+    version: `~17.0.0-next.7+sha-11588a1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
