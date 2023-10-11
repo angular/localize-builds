8c3076cbd8128e28b39b6928f3377989493e6383
Andrew Scott <atscott01@gmail.com>
2023-10-11 18:38:46 +0000

    073ebfe fix(compiler): apply style on :host attributes in prod builds. (#49118)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.8+sha-926db6d`
+    version: `~17.0.0-next.8+sha-073ebfe`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
