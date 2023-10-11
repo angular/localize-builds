9e7c9d0d2a01fb3cf7e57acdcc838a78c616b8cc
Andrew Scott <atscott01@gmail.com>
2023-10-11 19:43:11 +0000

    2e5ee1c refactor(compiler-cli): remove `MethodIdentifier` type (#49611)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.8+sha-371e26f`
+    version: `~17.0.0-next.8+sha-2e5ee1c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
