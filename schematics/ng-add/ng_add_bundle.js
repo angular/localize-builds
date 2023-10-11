57a2da2d67cc2d5c4424a2978a931488f9e22f92
Andrew Scott <atscott01@gmail.com>
2023-10-11 17:04:23 +0000

    fb10abcf3e release: cut the v16.2.9 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.8+sha-a3136e0`
+    version: `~16.2.9+sha-fb10abc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
