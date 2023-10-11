d4589ebd80795c039b0ecaa791c8b9f877de534d
Andrew Scott <atscott01@gmail.com>
2023-10-11 16:30:57 +0000

    a3136e00f5 build: update io_bazel_rules_sass digest to 9a327e5 (#52119)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.8+sha-fcdae57`
+    version: `~16.2.8+sha-a3136e0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
