41e2f978f7101cfd791f6012ae3d996d998e6d95
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-12 15:50:44 +0000

    25788b2919 docs: add Nicolas Frizzarin to GDE resources (#52003)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.9+sha-f52eee8`
+    version: `~16.2.9+sha-25788b2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
