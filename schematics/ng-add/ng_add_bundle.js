25d578c8cb717c642fcd456677287147bb33f96d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-12 17:34:27 +0000

    20e7e21 fix(core): do not remove used ng-template nodes in control flow migration (#52186)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-37c8fd7`
+    version: `~17.1.0-next.0+sha-20e7e21`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
