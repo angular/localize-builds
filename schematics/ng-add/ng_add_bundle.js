bb123468619ce8ade537c60b41802a69835a878b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-13 12:16:06 +0000

    2d98726 refactor(core): remove unused helper for setting `LView` for reactive consumer (#52192)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.8+sha-030a68b`
+    version: `~17.0.0-next.8+sha-2d98726`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
