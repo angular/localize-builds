e158c1b5efbfc81b2a9cd037bedf21e19d12fdf8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 15:49:48 +0000

    65c2f02 refactor(core): remove hack on render3 (#51238)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.8+sha-a6be2e2`
+    version: `~17.0.0-next.8+sha-65c2f02`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
