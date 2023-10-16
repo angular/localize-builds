9b03741f590f7f74d8d01e67cae295ef761a052d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-16 16:28:48 +0000

    81a287a fix(compiler): avoid error in template parser for tag names that can occur in object prototype (#52225)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.8+sha-65c2f02`
+    version: `~17.0.0-next.8+sha-81a287a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
