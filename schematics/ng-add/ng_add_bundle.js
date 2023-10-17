55cf9cb9ed270b8a5659ccd667922ce7b8e771d8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-17 09:23:41 +0000

    c9cde3a perf(platform-browser): only append style element on creation (#52237)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-next.8+sha-35fd10a`
+    version: `~17.0.0-next.8+sha-c9cde3a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
