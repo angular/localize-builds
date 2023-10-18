344d473ccbd3e807ab01e9a677231f2314b8a895
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-18 09:36:35 +0000

    06ce49aeeb docs: fix typo (#52247)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.9+sha-d6070af`
+    version: `~16.2.9+sha-06ce49a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
