3a4817196e8f4c508b6b9fb1c4c7172bdf3b3f9f
Dylan Hunn <dylhunn@gmail.com>
2023-10-18 19:19:03 +0000

    b4c1ffb refactor(forms): Log a warning when `FormGroup` keys include a dot. (#50642)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-rc.0+sha-866d0f4`
+    version: `~17.0.0-rc.0+sha-b4c1ffb`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
