0d05f1d20ed5e05b802ea3bd1f8f0ce538b91f2d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-10-18 14:03:41 +0000

    2820425e38 build: update babel dependencies to v7.23.2 (#52255)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.9+sha-06ce49a`
+    version: `~16.2.9+sha-2820425`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
