ecaa8b49402d2efab0d2b786441e3abd4968cafa
Dylan Hunn <dylhunn@gmail.com>
2023-10-18 18:58:48 +0000

    866d0f4 refactor(core): remove test.ts files in integration tests (#52109)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-rc.0+sha-edd060e`
+    version: `~17.0.0-rc.0+sha-866d0f4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
