9de0d544a9b6e418f9947cafb015fe72ce7ae27a
Dylan Hunn <dylhunn@gmail.com>
2023-10-19 16:34:47 +0000

    dcf9086 refactor(core): check "skip hydration" condition only when hydration info is present (#52221)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-43115da`
+    version: `~17.1.0-next.0+sha-dcf9086`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
