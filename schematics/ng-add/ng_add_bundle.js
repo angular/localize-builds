3e0ad26b344d9c6d2bf219e6b04ed1b5c1c4d4ba
Dylan Hunn <dylhunn@gmail.com>
2023-10-19 16:33:23 +0000

    fbf49b5e9c build: update all non-major dependencies (#52259)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.10+sha-bf70bf6`
+    version: `~16.2.10+sha-fbf49b5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
