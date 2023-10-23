fd572748d473697107b7a1c5e93da41002c1b3c5
Dylan Hunn <dylhunn@gmail.com>
2023-10-23 20:48:01 +0000

    8239a8f19e docs: add event banner (#52334) PR Close #52334

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.10+sha-fbf49b5`
+    version: `~16.2.10+sha-8239a8f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
