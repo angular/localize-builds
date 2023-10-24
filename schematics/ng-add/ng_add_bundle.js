1ea7b92569434f29f376cf871112ab0cf05b5718
Dylan Hunn <dylhunn@gmail.com>
2023-10-24 16:30:16 +0000

    36873596be docs: remove test.ts file in aio (#52327)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.10+sha-8239a8f`
+    version: `~16.2.10+sha-3687359`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
