47471f098581ed99342053a495f2d3d67929d933
Dylan Hunn <dylhunn@gmail.com>
2023-10-24 21:42:08 +0000

    c993e9a fix(migrations): handle nested classes in control flow migration (#52309)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-d82d586`
+    version: `~17.1.0-next.0+sha-c993e9a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
