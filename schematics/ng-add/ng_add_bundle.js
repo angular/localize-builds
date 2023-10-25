f7bfbbe3e86e3435cb93da03c31483f3bb637a24
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 16:46:45 +0000

    4c878f9 fix(migrations): Add support for nested structures inside a switch statement (#52358)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-rc.0+sha-1dd8558`
+    version: `~17.0.0-rc.0+sha-4c878f9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
