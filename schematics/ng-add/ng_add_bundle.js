260b96298c079f1b4aeab100e75f7eb38698b34e
Dylan Hunn <dylhunn@gmail.com>
2023-10-25 21:47:30 +0000

    16bf04b release: cut the zone.js-0.14.1 release (#52374)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-4ca2cea`
+    version: `~17.1.0-next.0+sha-16bf04b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
