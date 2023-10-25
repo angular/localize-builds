2d3a82e3b4d78729f36b62d9861eb83212330db8
Dylan Hunn <github@dylanhunn.com>
2023-10-25 21:35:36 +0000

    4ca2cea docs: release notes for the v16.2.11 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-0037c21`
+    version: `~17.1.0-next.0+sha-4ca2cea`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
