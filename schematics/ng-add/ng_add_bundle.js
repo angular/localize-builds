4e30cb3618aee133b7b6ef414a50446a11bd0668
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-26 21:18:30 +0000

    612986b build: update cross-repo angular dependencies (#52191)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-rc.1+sha-da1b71e`
+    version: `~17.0.0-rc.1+sha-612986b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
