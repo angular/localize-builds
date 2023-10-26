84b708ad6b5ff4f8442b2a55b9df276e7e315eb1
Dylan Hunn <dylhunn@gmail.com>
2023-10-26 16:45:37 +0000

    6a01d62 fix(migrations): fix broken migration when no control flow is present (#52399)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-rc.1+sha-576338a`
+    version: `~17.0.0-rc.1+sha-6a01d62`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
