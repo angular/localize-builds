8b23e8e403985b50668d8853cb783b00e380e46b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-26 19:37:33 +0000

    cb8a741 docs: update SSR doc to reflect version 17 (#52398)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-f1a020b`
+    version: `~17.1.0-next.0+sha-cb8a741`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
