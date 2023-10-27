c1ed814dd5ef32530f9be3db8035e47122404dd4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 19:38:20 +0000

    becbcdc docs: add warning for localhost proxy (#51948) PR Close #51948

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-rc.1+sha-612986b`
+    version: `~17.0.0-rc.1+sha-becbcdc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
