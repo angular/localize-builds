6f8c2b363655cfdc1ae5b7184a0f541735230134
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 23:24:54 +0000

    4461cef fix(core): handle trackBy and aliased index in control flow migration (#52423)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-rc.1+sha-8e17dd7`
+    version: `~17.0.0-rc.1+sha-4461cef`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
