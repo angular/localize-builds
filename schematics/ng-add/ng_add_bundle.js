f1b2ce0b59689079b4f145b3293704716da7a327
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 20:07:56 +0000

    fa03f0a fix(migrations): Ensure control flow migration ignores new block syntax (#52402)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-e06b73f`
+    version: `~17.1.0-next.0+sha-fa03f0a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
