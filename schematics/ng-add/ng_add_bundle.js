35f6cde94075aff4cddc23088f6245e689390393
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 23:12:27 +0000

    8e17dd7 refactor(localize): mark `TargetMessage` and `MessageId` as public. (#52418)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-rc.1+sha-57b9798`
+    version: `~17.0.0-rc.1+sha-8e17dd7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
