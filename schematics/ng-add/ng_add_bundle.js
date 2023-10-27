e553c1ac4366160dc0c35bd57d41abdbb71d7eed
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-27 19:40:15 +0000

    5ff99f2f9e docs: add warning for localhost proxy (#51948) PR Close #51948

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.11+sha-38b4602`
+    version: `~16.2.11+sha-5ff99f2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
