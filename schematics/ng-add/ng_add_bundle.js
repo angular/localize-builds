fb45414e977eec85c2f000b17878cd0bce33abb6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-31 20:05:51 +0000

    bf5bda4 refactor(language-service): Add quick info for built in control flow/blocks (#52386)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-73c5d1c`
+    version: `~17.1.0-next.0+sha-bf5bda4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
