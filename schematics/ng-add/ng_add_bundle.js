41cc356c2dca415e7a172ef6144dd53e4c0ba435
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-31 20:30:22 +0000

    a3028e2 refactor(core): Remove RootViewRef<T> because it is the same as ViewRef<T> (#52430)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-bf5bda4`
+    version: `~17.1.0-next.0+sha-a3028e2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
