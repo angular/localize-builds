5f4de21f944f1d08d581f322f8c6b82184e35cab
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-10-31 21:27:54 +0000

    078ebea fix(core): mutation bug in `getDependenciesFromInjectable` (#52450)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-c5980d6`
+    version: `~17.1.0-next.0+sha-078ebea`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
