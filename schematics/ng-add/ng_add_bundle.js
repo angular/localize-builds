2a43ad06da7457043b020e09b9f37fcdbdfe9aa3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-01 23:30:20 +0000

    4e4c678 docs: update examples to use `buildTarget` instead of `browserTarget` (#52361)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-rc.1+sha-7c95079`
+    version: `~17.0.0-rc.1+sha-4e4c678`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
