47a227513f04d48f29db6fd56bd9ae63952bd498
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 17:49:46 +0000

    64e4835f56 build: update peter-evans/create-or-update-comment digest to 23ff157 (#52279)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -109,7 +109,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~16.2.11+sha-03f4050`
+    version: `~16.2.11+sha-64e4835`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
