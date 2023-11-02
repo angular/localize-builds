14b85fa187163c801ff836fba9a1e02ebc37429a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 17:39:03 +0000

    0754122 build: update actions/checkout digest to b4ffde6 (#52252)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-rc.1+sha-356da9c`
+    version: `~17.0.0-rc.1+sha-0754122`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
