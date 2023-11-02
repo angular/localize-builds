21a1b965ad1d31b896a060873cdad5f398369878
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-11-02 20:29:21 +0000

    32f417f refactor(core): Do not refresh view if producers did not actually change (#52476)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-rc.1+sha-0d88e25`
+    version: `~17.0.0-rc.1+sha-32f417f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
