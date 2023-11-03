b66f580c6003dda51856a3100ed4f87ae23d0120
Andrew Scott <atscott01@gmail.com>
2023-11-03 14:56:00 +0000

    2560037 docs: omit repetitions (#52413)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-1f528f1`
+    version: `~17.1.0-next.0+sha-2560037`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
