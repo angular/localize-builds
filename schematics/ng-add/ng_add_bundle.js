7e25f4991853622c8f1e3992d5c2a390136d4851
Andrew Scott <atscott01@gmail.com>
2023-11-03 16:57:02 +0000

    95accb0 docs: update ssr docs to add information about `CommonEngine` (#52506)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-rc.2+sha-a2ba548`
+    version: `~17.0.0-rc.2+sha-95accb0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
