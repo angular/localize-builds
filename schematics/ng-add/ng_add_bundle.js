3250a3d4677239ac5d6eb96af07d896f8f9dcc72
Andrew Scott <atscott01@gmail.com>
2023-11-04 00:42:50 +0000

    63777d2 test(devtools): fix transformInjectorResolutionPathsIntoTree tests (#52489)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-1a33a2c`
+    version: `~17.1.0-next.0+sha-63777d2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
