381f67a59d82cf77b6e89816ef429bdd10ebc11e
Andrew Scott <atscott01@gmail.com>
2023-11-06 19:09:49 +0000

    8bbec83 docs: add angular.dev banner (#52535) PR Close #52535

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-89cfadd`
+    version: `~17.1.0-next.0+sha-8bbec83`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
