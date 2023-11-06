d1dd30c68a4e6fd8ae40cea8a2fe9561c900640f
Andrew Scott <atscott01@gmail.com>
2023-11-06 23:58:39 +0000

    d4c20ac refactor(core): Add warning when signal equality is false for object.is (#52532)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0-rc.2+sha-a86661f`
+    version: `~17.0.0-rc.2+sha-d4c20ac`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
