05664e00b8582264c4feab5f64c3ff29a74abffe
Andrew Scott <atscott01@gmail.com>
2023-11-06 19:48:49 +0000

    699ae60 refactor(compiler): Fix two-way binding source maps (#52479)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-651c347`
+    version: `~17.1.0-next.0+sha-699ae60`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
