03de85fa31f2a6dc43585381b309fe9529bf7f0c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-08 16:10:53 +0000

    c30dde2 build: bump in-memory-web-api to v17 (#52676)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0+sha-da429c5`
+    version: `~17.0.0+sha-c30dde2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
