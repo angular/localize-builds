1231f81c575e731dd23f1c019fa141f9d3ab29e2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-08 15:35:57 +0000

    da429c5 Revert "fix(http): Don't override the backend when using the InMemoryWebAPI (#52425)" (#52630)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0+sha-fc6b2f2`
+    version: `~17.0.0+sha-da429c5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
