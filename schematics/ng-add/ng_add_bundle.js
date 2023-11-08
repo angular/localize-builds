acbc88af4ef8abd4979549ba2659c9f88ddeeae2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-08 15:27:48 +0000

    fc6b2f2 docs: add the v17 blog post link to the changelog (#52675)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.0+sha-fecd141`
+    version: `~17.0.0+sha-fc6b2f2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
