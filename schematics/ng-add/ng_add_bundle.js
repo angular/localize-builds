45cf1047957e7a59a5c3a61cbde08d667087e17c
Andrew Scott <atscott01@gmail.com>
2023-11-08 16:58:04 +0000

    c745ca2 refactor(core): sanitize reactivity tests (#52632)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-3f2501e`
+    version: `~17.1.0-next.0+sha-c745ca2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
