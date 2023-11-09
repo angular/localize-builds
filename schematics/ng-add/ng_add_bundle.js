f2f7ecb75d822d4000e9a4603ab385fe039014c4
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 20:04:11 +0000

    b16fc26 fix(core): limit rate of markers invocations (#52742)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.1+sha-cfb42ee`
+    version: `~17.0.1+sha-b16fc26`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
