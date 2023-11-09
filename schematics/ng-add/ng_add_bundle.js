30d5c031728a88008f8867c95c1f86da99c48bbb
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 19:11:41 +0000

    f212736 docs: removed the broken link (#52743)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-029edea`
+    version: `~17.1.0-next.0+sha-f212736`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
