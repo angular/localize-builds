3116375eeff3a1e633713956b174f48802655147
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 15:07:32 +0000

    3d98a44 docs: fix incorrect tag reference (#52586)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-9cac4ee`
+    version: `~17.1.0-next.0+sha-3d98a44`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
