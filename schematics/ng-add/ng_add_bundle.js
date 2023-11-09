3834a10969612def2bb43feef5b9402203546efd
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 19:46:18 +0000

    cfb42ee docs: add extended diagnostic NG8109 (#52721)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.1+sha-90018f9`
+    version: `~17.0.1+sha-cfb42ee`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
