0d3ea9c94406168968b2dbdbd4e662daa6125d14
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 19:37:29 +0000

    90018f9 docs: add missing backtick on title (#52711)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.1+sha-158b198`
+    version: `~17.0.1+sha-90018f9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
