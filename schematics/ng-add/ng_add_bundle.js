21df768131b366ad8ded0361f4e66320ee0d8a94
Joey Perrott <josephperrott@gmail.com>
2023-11-09 16:03:22 +0000

    b651648 docs: make homepade example accessible (#52578) PR Close #52578

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.1+sha-a548c03`
+    version: `~17.0.1+sha-b651648`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
