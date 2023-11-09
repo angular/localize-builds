1593e301a7185ee55f4e509d7f82d8718fd58ab9
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-09 17:05:56 +0000

    55d2c42 refactor(core): delay applying component metadata until it's needed in tests (#52708)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-8c8ff63`
+    version: `~17.1.0-next.0+sha-55d2c42`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
