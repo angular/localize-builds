125e0634b17244797e64edddb7bf7b3046f96c0b
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-10 18:42:59 +0000

    54a7b33 fix(devtools): check for all new DI debug APIs before trying to determine resolution path providers (#52791)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.2+sha-38aab6a`
+    version: `~17.0.2+sha-54a7b33`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
