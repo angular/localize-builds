f4266ae9a91745aae0793579b970a429fef3b1bb
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 19:03:52 +0000

    a00beec docs(forms): replace fb with formBuilder (#52795)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.2+sha-76aba9e`
+    version: `~17.0.2+sha-a00beec`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
