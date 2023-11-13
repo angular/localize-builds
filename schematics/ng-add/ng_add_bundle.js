de76b68e99bc191c74e37de6b5aa9aefe9749e54
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 15:28:25 +0000

    e1f84a3 fix(migrations): passed in paths will be respected in nx workspaces (#52796)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.2+sha-54a7b33`
+    version: `~17.0.2+sha-e1f84a3`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
