8710ed90213e767255e434da73866bec3a490694
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-13 22:38:27 +0000

    d033540 fix(migrations): Add support for bound versions of NgIfElse and NgIfThenElse (#52869)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.2+sha-c8ff0be`
+    version: `~17.0.2+sha-d033540`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
