7b8dae726d7e9eff4124b3639a8ec6049f7d9d18
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-14 21:11:59 +0000

    290e26a refactor(core): Remove detectChanges private export (#52875)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.2+sha-95e4806`
+    version: `~17.0.2+sha-290e26a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
