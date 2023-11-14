ff6b1fec05b24b6636f07ea4fe8596af0ebbb978
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-14 19:21:40 +0000

    95e4806 docs: fix and improve @defer testing docs for adev (#52874)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.2+sha-64d9e65`
+    version: `~17.0.2+sha-95e4806`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
