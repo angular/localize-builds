d52c8fc2bfd6e6503a6756bda35b4da81413e81f
Jessica Janiuk <jessicajaniuk@google.com>
2023-11-15 21:21:02 +0000

    98376f2 fix(compiler): changed after checked error in for loops (#52935)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.3+sha-f5307bf`
+    version: `~17.0.3+sha-98376f2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
