e64f18e09b8179480efc6ac41025188dcf3d89a6
Andrew Kushnir <akushnir@google.com>
2023-11-16 17:38:41 +0000

    f763d26 refactor(migrations): Add optional template reformatting after migration (#52939)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.3+sha-98376f2`
+    version: `~17.0.3+sha-f763d26`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
