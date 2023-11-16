d28c8c842c223ece79c2c9cff280e9d11d010430
Andrew Kushnir <akushnir@google.com>
2023-11-16 20:25:52 +0000

    1ce31d8 fix(core): handle local refs when `getDeferBlocks` is invoked in tests (#52973)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-81e080e`
+    version: `~17.1.0-next.0+sha-1ce31d8`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
