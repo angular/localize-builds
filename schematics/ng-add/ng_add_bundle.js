50cb1c540b79f26cad64f57b6d861b151f97e0ac
Andrew Kushnir <akushnir@google.com>
2023-11-17 16:18:54 +0000

    31a047f docs(core): replace RefreshTransplantedView with RefreshView (#52982)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.3+sha-f3b8a35`
+    version: `~17.0.3+sha-31a047f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
