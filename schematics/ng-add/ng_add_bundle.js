ec28a38fbd58fc3109253a2d55159d599f2764d2
Andrew Kushnir <akushnir@google.com>
2023-11-17 20:23:29 +0000

    6c68f4c docs: fix broken link for view encapsulation (#52995)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.3+sha-6df5f0e`
+    version: `~17.0.3+sha-6c68f4c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
