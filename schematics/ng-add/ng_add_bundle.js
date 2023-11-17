87a67976bdd27be445473431d522be1b9c4cc1a6
Andrew Kushnir <akushnir@google.com>
2023-11-17 19:56:36 +0000

    f63c0f6 Revert "fix(compiler-cli): add diagnostic for control flow that prevents content projection (#52726)" (#53012)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.0+sha-c3e9a2b`
+    version: `~17.1.0-next.0+sha-f63c0f6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
