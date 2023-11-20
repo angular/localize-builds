66e4acd306c03b2f7459195508b77864cba8a088
Andrew Kushnir <akushnir@google.com>
2023-11-20 21:22:16 +0000

    c7c7ea9 fix(core): inherit host directives (#52992)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.1+sha-29e0834`
+    version: `~17.1.0-next.1+sha-c7c7ea9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
