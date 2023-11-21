5982a6115b615d9843a7ed98b5c86e84ba2f8824
Andrew Kushnir <akushnir@google.com>
2023-11-21 18:50:13 +0000

    c62b2da build: update to TypeScript 5.3 final (#53087)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.1+sha-cb2db54`
+    version: `~17.1.0-next.1+sha-c62b2da`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
