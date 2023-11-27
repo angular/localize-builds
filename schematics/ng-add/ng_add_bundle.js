497128690e94a7e446326f76cebd848cd1862006
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-27 09:52:48 +0000

    6e1bf29 build: update dependency karma-jasmine-html-reporter to v2 (#52544)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.1+sha-c62b2da`
+    version: `~17.1.0-next.1+sha-6e1bf29`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
