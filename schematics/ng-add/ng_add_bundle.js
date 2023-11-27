e289dd5abd229882ec01e57eeffea827997eb18c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-27 14:54:31 +0000

    b76b3c6 build: upgrade node version for .devcontainer example file (#53157)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.1+sha-79ff91a`
+    version: `~17.1.0-next.1+sha-b76b3c6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
