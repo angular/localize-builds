fc1ceecb7b437f36a6f96f84f16a34b9cf79bc79
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-27 10:04:40 +0000

    4604be2 refactor(platform-browser): clean up legacy way of getting a relative path (#53097)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.4+sha-6d61f2d`
+    version: `~17.0.4+sha-4604be2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
