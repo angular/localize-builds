33f1e29934571805594801642c694e3dbeca4261
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 10:11:54 +0000

    92a28a7 build: update dependency jsdom to v23 (#53194)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.4+sha-115dc23`
+    version: `~17.0.4+sha-92a28a7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
