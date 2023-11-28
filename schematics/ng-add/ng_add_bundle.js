f29844684d46185c23f340048de5aaaa504fc9bb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-28 13:08:13 +0000

    53912fd fix(migrations): allows colons in ngIf else cases to migrate (#53076)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.1+sha-4c1d69e`
+    version: `~17.1.0-next.1+sha-53912fd`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
