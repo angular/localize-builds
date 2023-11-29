353caab148dfefbad9dbec0ae91cddc1758de181
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-29 09:36:43 +0000

    c9f8e75 test(compiler): Update golden partial file (#53209)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.1+sha-58cf389`
+    version: `~17.1.0-next.1+sha-c9f8e75`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
