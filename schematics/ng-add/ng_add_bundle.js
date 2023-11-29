c7687ab14bc577f46f95b0fef74af47d980efb23
Joey Perrott <josephperrott@gmail.com>
2023-11-29 20:45:31 +0000

    6462ddd build: set up initial adev guide (#53208)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.1+sha-5d15804`
+    version: `~17.1.0-next.2+sha-6462ddd`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
