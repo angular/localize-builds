cfbc27e5fcd94993f93d7c758846e8a144ba9be6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2023-11-29 14:20:26 +0000

    fadfee4 fix(migrations): cf migration fix migrating empty switch default (#53237)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.1+sha-c9f8e75`
+    version: `~17.1.0-next.1+sha-fadfee4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
