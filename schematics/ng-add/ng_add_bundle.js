eb89469ea375ed66d124be86471fd818f935e9ab
Dylan Hunn <dylhunn@gmail.com>
2023-11-30 17:50:33 +0000

    1940280 fix(router): Ensure canMatch guards run on wildcard routes (#53239)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.2+sha-6462ddd`
+    version: `~17.1.0-next.2+sha-1940280`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
