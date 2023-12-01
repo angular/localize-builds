268d53d598b39780d51064f3d4b69d0fa3c9695b
Dylan Hunn <dylhunn@gmail.com>
2023-12-01 18:42:37 +0000

    9423c19 docs: fix the colspan and colSpan link in the property binding page (#53180)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.2+sha-2998d48`
+    version: `~17.1.0-next.2+sha-9423c19`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
