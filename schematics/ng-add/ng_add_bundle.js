e933604e199f4c844000ecd1c591c98ab6dc5a1c
Dylan Hunn <dylhunn@gmail.com>
2023-12-04 22:29:27 +0000

    12dfa9b test(compiler): Update golden partial file (#53327)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.2+sha-d4b4236`
+    version: `~17.1.0-next.2+sha-12dfa9b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
