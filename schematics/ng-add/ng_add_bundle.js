6eae91b8422aed97fdeeb5a0f617722daa4fd693
Dylan Hunn <dylhunn@gmail.com>
2023-12-05 05:55:32 +0000

    2d2eaec docs: fixed wrong template string placeholder (#53286) PR Close #53286

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.5+sha-dc27bea`
+    version: `~17.0.5+sha-2d2eaec`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
