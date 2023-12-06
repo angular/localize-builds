461878ecae58d0f033d2fffd99f2e7f3c8cb6eb1
Dylan Hunn <dylhunn@gmail.com>
2023-12-06 01:24:56 +0000

    7c863d7 refactor(devtools): run control flow migration. (#53353)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.5+sha-20ea149`
+    version: `~17.0.5+sha-7c863d7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
