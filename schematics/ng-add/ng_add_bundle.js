bc8dbb101be91139455fa36355c4bf1f02580ffa
Dylan Hunn <dylhunn@gmail.com>
2023-12-06 01:28:58 +0000

    e620b3a fix(compiler-cli): add compiler option to disable control flow content projection diagnostic (#53311)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.2+sha-05f9c7b`
+    version: `~17.1.0-next.2+sha-e620b3a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
