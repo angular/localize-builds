8768b200215fbd8b56a7d3962885bb5ba1262027
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-06 15:28:22 +0000

    580af8e Revert "refactor(core): output an error guide link in prod mode (#53324)" (#53392)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.2+sha-e620b3a`
+    version: `~17.1.0-next.2+sha-580af8e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
