7dbde86b90d7eebba7f60db96295297602a148de
Dylan Hunn <dylhunn@gmail.com>
2023-12-06 17:53:51 +0000

    e8d01f9 refactor(migrations): Update comments to be more accurate (#53396)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.5+sha-d4fdba5`
+    version: `~17.0.5+sha-e8d01f9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
