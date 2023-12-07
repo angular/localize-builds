e528eb79e8dd2685f9f2fe3ced8870817a2b6de2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-07 17:42:00 +0000

    543df3d fix(migrations): ensure we do not overwrite prior template replacements in migration (#53393)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.6+sha-049e4ab`
+    version: `~17.0.6+sha-543df3d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
