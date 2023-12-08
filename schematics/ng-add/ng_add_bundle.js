00eb8825569b3e252488251f2d7d144e12ed0a75
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-08 23:06:34 +0000

    e3a6bf9 perf(core): optimize memory allocation when reconcilling lists (#52245)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.3+sha-f2245d1`
+    version: `~17.1.0-next.3+sha-e3a6bf9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
