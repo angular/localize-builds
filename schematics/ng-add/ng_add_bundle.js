557a5a760b03b913ca2e4bfa7900d14ca6ce79fe
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-08 17:45:39 +0000

    a398c55 refactor(compiler): Fix out-of-order i18n issue (#53405)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.6+sha-17dbf8b`
+    version: `~17.0.6+sha-a398c55`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
