6a3daa1f939be6c8cae70a18a12cf24ef59cc0b8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-11 19:00:46 +0000

    24d8aa6 fix(zone.js): Promise.resolve(subPromise) should return subPromise (#53423)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.6+sha-044cb55`
+    version: `~17.0.6+sha-24d8aa6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
