8f909297c08e06ff74eb73a89a856b1e11718e6d
Definitely not me <something@something.com>
2023-12-11 21:41:21 +0000

    9bb34a2 build: drop rxjs_umd_modules as it is unused (#53506)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.6+sha-1ea8a14`
+    version: `~17.0.6+sha-9bb34a2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
