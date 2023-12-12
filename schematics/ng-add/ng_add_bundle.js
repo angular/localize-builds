05041e82bdce991c169d27f3a1fe0d2a7a800690
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-12 17:15:04 +0000

    44f9f01 refactor(core): Update flaky test with wait condition (#53437)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.3+sha-f9731ee`
+    version: `~17.1.0-next.3+sha-44f9f01`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
