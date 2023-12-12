a2ef6ed1edb567960af2d42264317c3574dbe917
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-12 22:40:06 +0000

    9a91e0f docs: add state of js survey link (#53537)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.6+sha-659b88d`
+    version: `~17.0.6+sha-9a91e0f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
