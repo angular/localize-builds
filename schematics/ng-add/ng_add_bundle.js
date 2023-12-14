92be6d28bc3b90f6b907d070a16fe64c71b692bc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2023-12-14 01:18:35 +0000

    be7beca release: cut the v17.1.0-next.4 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.3+sha-bb93f6c`
+    version: `~17.1.0-next.4+sha-be7beca`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
