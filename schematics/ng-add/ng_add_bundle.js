c1ecab33cbabeba5029364256e370ed29d72de50
Andrew Scott <atscott01@gmail.com>
2023-12-14 17:39:34 +0000

    e8f0042 refactor(compiler): Ensure correct context kind on ICU contexts (#53557)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.4+sha-606de51`
+    version: `~17.1.0-next.4+sha-e8f0042`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
