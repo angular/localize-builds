070ccbc4c59d6a5fe9ff2f79b06f91cce3d21303
Andrew Scott <atscott01@gmail.com>
2023-12-14 17:40:07 +0000

    512a19b refactor(compiler): Ensure correct context kind on ICU contexts (#53557)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.7+sha-be58dba`
+    version: `~17.0.7+sha-512a19b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
