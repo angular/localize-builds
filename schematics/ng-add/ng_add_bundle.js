b1243c68783e40ff84b20f1af3e1a920c1e473a8
Jessica Janiuk <jessicajaniuk@google.com>
2023-12-15 20:04:19 +0000

    1892904 refactor(migrations): update cf migration to handle module imports better (#53575)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.4+sha-7bb312f`
+    version: `~17.1.0-next.4+sha-1892904`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
