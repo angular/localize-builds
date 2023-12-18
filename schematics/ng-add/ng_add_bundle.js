65b701c2e0c1b45cb4de53a0ce78974db16283e3
Jessica Janiuk <jessicajaniuk@google.com>
2023-12-18 17:25:29 +0000

    b394125 build: update rxjs build version to v7 (#53621)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.7+sha-de5c9ca`
+    version: `~17.0.7+sha-b394125`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
