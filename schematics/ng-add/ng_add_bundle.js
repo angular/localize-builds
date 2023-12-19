5a98f4441113dcd9e94dc8e1c1a84293e6d1db76
Andrew Scott <atscott01@gmail.com>
2023-12-19 20:27:12 +0000

    ed936ba fix(migrations): cf migration - detect and error when result is invalid i18n nesting (#53638) (#53639)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.7+sha-eb7c29c`
+    version: `~17.0.7+sha-ed936ba`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
