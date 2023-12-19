9cbec9d90264d9a8ae8ebafb33473265cb52b8da
Andrew Scott <atscott01@gmail.com>
2023-12-19 18:22:03 +0000

    817dc1b fix(migrations): cf migration - fix bug in attribute formatting (#53636)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.7+sha-c99491e`
+    version: `~17.0.7+sha-817dc1b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
