1ed207da5b22b3065f5c8697adf886b9ed16d748
Andrew Scott <atscott01@gmail.com>
2023-12-19 17:26:54 +0000

    502f300 fix(router): Should not freeze original object used for route data (#53635)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.4+sha-00cbdc9`
+    version: `~17.1.0-next.4+sha-502f300`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
