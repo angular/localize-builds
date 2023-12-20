227df3d44ac8ee8b0bd75b8d04c50d16d48f687d
Andrew Scott <atscott01@gmail.com>
2023-12-20 22:52:24 +0000

    257536e refactor(docs-infra): build adev application using local generated assets (#53666)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.7+sha-8a77a5a`
+    version: `~17.0.7+sha-257536e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
