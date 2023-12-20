dc571c50aad8714549876575c2f80735d4da68e0
Andrew Scott <atscott01@gmail.com>
2023-12-20 16:00:23 +0000

    8d58595 refactor(core): Add scheduler abstraction and notify when necessary (#53499)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.4+sha-cc74ebf`
+    version: `~17.1.0-next.4+sha-8d58595`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
