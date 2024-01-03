acd431228811cec33e263b16ea345922c47b6eb2
Andrew Scott <atscott@google.com>
2024-01-03 18:34:44 +0000

    bd9f89d fix(common): server-side rendering error when using in-memory scrolling (#53683)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.8+sha-b87324d`
+    version: `~17.0.8+sha-bd9f89d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
