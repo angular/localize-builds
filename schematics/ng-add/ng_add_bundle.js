5a67efa34e8564a6b5f51e913ac960e80c1c6e64
Andrew Scott <atscott@google.com>
2024-01-03 21:34:16 +0000

    5f73737 refactor(router): remouve unused (#53750)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.8+sha-5fb199b`
+    version: `~17.0.8+sha-5f73737`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
