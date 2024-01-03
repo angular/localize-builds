e4870f9859e329cfc89dabc649aa36a0c62bb071
Andrew Scott <atscott@google.com>
2024-01-03 21:07:58 +0000

    58f2b74 refactor(core): Remove `LContainerFlags.HasChildViewsToRefresh` (#53715)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.5+sha-c59a4dc`
+    version: `~17.1.0-next.5+sha-58f2b74`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
