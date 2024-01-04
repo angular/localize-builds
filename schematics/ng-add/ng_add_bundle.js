202f81a3ff92707804ce43dfe1172a3c10250cbd
Andrew Scott <atscott@google.com>
2024-01-04 20:12:09 +0000

    36318db refactor(compiler-cli): reference `InputFlags` enum directly for full compiler output (#53571)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.5+sha-1c63edd`
+    version: `~17.1.0-next.5+sha-36318db`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
