b9081f3aea3fa703bbff652bbcc4defe34e3074b
Andrew Scott <atscott@google.com>
2024-01-05 19:34:10 +0000

    a5a9b40 feat(router): Add transient info to RouterLink input (#53784)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.5+sha-5e84d9c`
+    version: `~17.1.0-next.5+sha-a5a9b40`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
