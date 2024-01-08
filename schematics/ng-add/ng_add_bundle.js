7a1edc3daf8bfbf9c65fcfc4479719be02e1b072
Andrew Scott <atscott@google.com>
2024-01-08 22:44:08 +0000

    c22b513 fix(common): remove unused parameters from the ngClass constructor (#53831)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.8+sha-8e49e38`
+    version: `~17.0.8+sha-c22b513`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
