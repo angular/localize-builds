2f908423600453cd703d348db987173417a9470f
Andrew Scott <atscott@google.com>
2024-01-09 16:56:48 +0000

    9c58717 refactor(core): node removal should notify the scheduler (#53812)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.8+sha-c22b513`
+    version: `~17.0.8+sha-9c58717`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
