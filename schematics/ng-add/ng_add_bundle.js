fe963056917c68abc7c13c8a5c118e11bfd89af3
Andrew Scott <atscott@google.com>
2024-01-09 20:33:27 +0000

    92fd6cc fix(compiler): generate less code for advance instructions (#53845)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.8+sha-c4b266c`
+    version: `~17.0.8+sha-92fd6cc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
