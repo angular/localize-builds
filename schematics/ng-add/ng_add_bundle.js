e2a65f01528c0721e19a81c06cf3dcfa46c5a0fb
Andrew Scott <atscott@google.com>
2024-01-09 20:23:21 +0000

    c4b266c refactor(core): type-safe global ng (#53439)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.8+sha-9c58717`
+    version: `~17.0.8+sha-c4b266c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
