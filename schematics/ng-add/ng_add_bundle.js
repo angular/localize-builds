969e4de93cf10bfba8ddf54ad62c3e6e1bc49322
Andrew Scott <atscott01@gmail.com>
2024-01-10 22:13:18 +0000

    37e2769 release: cut the v17.0.9 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.8+sha-74094e1`
+    version: `~17.0.9+sha-37e2769`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
