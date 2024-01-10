fa116af0f44b3970be7ed6ef9baba3616c191568
Andrew Scott <atscott@google.com>
2024-01-10 20:38:46 +0000

    863be4b feat(core): expose new `input` API for signal-based inputs (#53872)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-next.5+sha-f6a32c0`
+    version: `~17.1.0-next.5+sha-863be4b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
