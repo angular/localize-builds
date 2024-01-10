a852956830ed9b969bcc13b98cdafa79bd9003f4
Andrew Scott <atscott@google.com>
2024-01-10 23:36:49 +0000

    79fbc4e refactor(compiler-cli): add tests for defer blocks and local compilation (#53591)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-9bbc415`
+    version: `~17.2.0-next.0+sha-79fbc4e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
