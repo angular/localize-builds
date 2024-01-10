bdb3d7aa3f5265e11a3c459aed4e3ef218803ff1
Andrew Scott <atscott@google.com>
2024-01-10 00:10:57 +0000

    b7cddf4 refactor(core): Move change detection scheduler implementation to core (#53579)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.8+sha-f4b1e64`
+    version: `~17.0.8+sha-b7cddf4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
