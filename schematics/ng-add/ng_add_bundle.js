8826bb9d020a4ae8c61764875acb8d87ad65e951
Andrew Scott <atscott@google.com>
2024-01-11 20:12:23 +0000

    198e64a fix(core): TestBed should still use the microtask queue to schedule effects (#53843)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.9+sha-425a8d3`
+    version: `~17.0.9+sha-198e64a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
