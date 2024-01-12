35a183b6e1eeafdce76d9dbb09f282ab6b412cb1
Dylan Hunn <dylhunn@gmail.com>
2024-01-12 18:32:51 +0000

    2d76a0d docs: remove repeated sentence (#53853)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.9+sha-198e64a`
+    version: `~17.0.9+sha-2d76a0d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
