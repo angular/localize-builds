45210f51e8584635aeab0119b2111a5554eb9ac1
Dylan Hunn <dylhunn@gmail.com>
2024-01-16 17:43:11 +0000

    f843b78 docs: fix-build-path-in-http-server-command (#53226)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.9+sha-2d76a0d`
+    version: `~17.0.9+sha-f843b78`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
