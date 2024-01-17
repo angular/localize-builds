a009c856e12a22cba6b32197924995bc92a63497
Dylan Hunn <github@dylanhunn.com>
2024-01-17 22:49:55 +0000

    51d53fc release: cut the v17.1.0 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0-rc.0+sha-be882c3`
+    version: `~17.1.0+sha-51d53fc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
