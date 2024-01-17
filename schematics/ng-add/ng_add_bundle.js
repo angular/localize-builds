bfa5f22a69eafb2dd8ae54c7ecd7e79ffd542913
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 17:19:19 +0000

    23f19f8 docs: add `platform-browser/animations/async` entry to AIO (#53914)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.9+sha-eb6389e`
+    version: `~17.0.9+sha-23f19f8`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
