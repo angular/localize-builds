42c2305e3a19c10f1f44e36721e1c246d8eb517f
Dylan Hunn <dylhunn@gmail.com>
2024-01-17 19:24:08 +0000

    4373889 ci: use ng-dev-previews project for previews (#53958)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.0.9+sha-3363295`
+    version: `~17.0.9+sha-4373889`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
