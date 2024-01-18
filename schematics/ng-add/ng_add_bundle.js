22bc4168e1ea9aaccfe73f9f9de28633125c7a5e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-18 09:21:35 +0000

    dc228c6 docs: fix broken code example (#53531)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0+sha-9ff4893`
+    version: `~17.1.0+sha-dc228c6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
