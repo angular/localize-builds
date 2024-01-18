a971acbb268256659ad3632da42c6fa600ecf426
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-18 09:27:55 +0000

    8737544 build(devtools): prevent underscores from being added at build time (#53921)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-a566c5c`
+    version: `~17.2.0-next.0+sha-8737544`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
