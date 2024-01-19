e7a02986f6aa8d738c8f0368619bb0b76a78df64
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 16:41:07 +0000

    c0f0baa refactor(devtools): improving type safety (#53436)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0+sha-0fe9331`
+    version: `~17.1.0+sha-c0f0baa`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
