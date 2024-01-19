831dc3027a802ee75d7026a429af17cb9384bf0b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 18:14:44 +0000

    656549d refactor(devtools): migrate devtools to prettier formatting (#53945)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0+sha-c0f0baa`
+    version: `~17.1.0+sha-656549d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
