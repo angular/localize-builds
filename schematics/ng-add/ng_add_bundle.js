dff42475f54fe65a899a74335f100572def05991
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-19 09:27:14 +0000

    cc34e5f refactor: migrate scripts to prettier formatting (#53976)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-1d1c413`
+    version: `~17.2.0-next.0+sha-cc34e5f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
