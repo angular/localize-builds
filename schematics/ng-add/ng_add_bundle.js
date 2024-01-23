a5a212d04adb818ffbb38c0ad5af488c6d960412
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-23 08:59:03 +0000

    ae145d1 refactor(devtools): migrate to standalone (#53998)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0+sha-20811aa`
+    version: `~17.1.0+sha-ae145d1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
