ea64c2f3e6e0766f28da95b98b29b675c495b9cb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-23 09:29:56 +0000

    f81a436 test: add compliance tests for signal-based queries (#53978)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-9384537`
+    version: `~17.2.0-next.0+sha-f81a436`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
