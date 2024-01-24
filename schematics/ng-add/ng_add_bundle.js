b613a12d68f2dc29ceb535b00c0527471b84c642
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-24 14:37:49 +0000

    09f9423 docs: fix typo in Custom directives section (#54044)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-25f91e3`
+    version: `~17.2.0-next.0+sha-09f9423`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
