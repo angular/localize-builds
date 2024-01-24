00f1dbb2f0d3457422dfd764111e37c61292bf7d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-01-24 10:00:16 +0000

    f222bee fix(router): revert commit that replaced `last` helper with native `Array.at(-1)` (#54021)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.0+sha-9a9a00e`
+    version: `~17.1.0+sha-f222bee`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
