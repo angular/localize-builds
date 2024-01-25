c562b01ca8dfe67a5dba556d9a61f54cbce4ef97
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 23:53:53 +0000

    ecd0daf docs(docs-infra): move comma in Angular Language Service section (#53759)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-d8d6478`
+    version: `~17.1.1+sha-ecd0daf`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
