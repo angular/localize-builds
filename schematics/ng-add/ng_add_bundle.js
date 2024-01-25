8af8901ae3a7fe7be633accd86b648a8d9f19687
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 20:23:41 +0000

    a4923f1 ci: add ben to reviewers for adev files (#54078)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-e603450`
+    version: `~17.1.1+sha-a4923f1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
