f0a75ad7b4d0a083481be31231efde13edb64d98
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 22:34:16 +0000

    3e2cd83 docs: add Trung Vo to GDE resources (#53599)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-9f18227`
+    version: `~17.2.0-next.0+sha-3e2cd83`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
