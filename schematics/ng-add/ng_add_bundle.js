db2e6ae24c845915276750f1764e3e5b8e5d9d09
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 17:16:19 +0000

    656bc28 fix(core): add toString implementation to signals (#54002)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-75aeae4`
+    version: `~17.2.0-next.0+sha-656bc28`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
