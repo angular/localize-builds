570263d1ae27c127f9662b0f065d9472f790dd5c
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-25 22:53:45 +0000

    ccddacf fix: cta clickability issue in adev homepage. (#52905) PR Close #52905

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-b216e76`
+    version: `~17.1.1+sha-ccddacf`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
