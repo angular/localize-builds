b389a29fd3fb6f5100e4494366962de49ce0d69d
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 19:18:45 +0000

    9f9dd90 docs: Update update-to-version-16.md (#51857)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-4a5f37d`
+    version: `~17.1.1+sha-9f9dd90`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
