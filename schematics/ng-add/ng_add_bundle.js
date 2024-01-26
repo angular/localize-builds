33f88fa5205f375032e46e3062510ee2ba68be99
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 16:08:37 +0000

    271755d docs(docs-infra): cleanup repeated scss from typography (#51579)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-1c49160`
+    version: `~17.2.0-next.0+sha-271755d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
