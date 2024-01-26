9159e6a6532851890eb2dbaa55e6ed3b7292c4fe
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 17:43:21 +0000

    74567f1 docs: update i18n-common-merge.md (#52400)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-64399b2`
+    version: `~17.1.1+sha-74567f1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
