c48eb29cf8593fd50a371bf5f4107df8fd9b568a
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 15:52:05 +0000

    1c49160 docs(docs-infra): add if cases in templates to avoid internal marked parameters or methods to appear in the documentation (#52020)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-c3b0095`
+    version: `~17.2.0-next.0+sha-1c49160`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
