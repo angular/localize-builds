820ac1e686f9dda81f43bb0809c42c88a6475f47
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-26 19:07:32 +0000

    4a5f37d docs(router): fix variable reference in text (#54099) PR Close #54099

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-74567f1`
+    version: `~17.1.1+sha-4a5f37d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
