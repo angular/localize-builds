e5cc660b499f0750c549c4357a0095e9af2ab6f8
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 16:02:49 +0000

    030d770 docs: update TypeScript version support details for Angular v15 and v16 (#54131)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-96ca49d`
+    version: `~17.1.1+sha-030d770`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
