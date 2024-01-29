a21179ecc823fbdccfaeda1a1cedf590da18f5a9
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 21:17:12 +0000

    7b4d275 fix(compiler): Fix the template pipeline option (#54148)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-3ca34e6`
+    version: `~17.2.0-next.0+sha-7b4d275`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
