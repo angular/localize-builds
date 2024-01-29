d65164b5b3cb34dbf11951971061c9b2b961b037
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 16:46:39 +0000

    9571287 docs: correct grammar error on start page (#53993)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-f5c520b`
+    version: `~17.2.0-next.0+sha-9571287`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
