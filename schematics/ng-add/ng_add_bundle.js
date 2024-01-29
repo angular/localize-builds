fe31a56ebfd946e4f55db072f02fca7810f360b3
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 20:26:26 +0000

    3ca34e6 refactor(core): Update `ComponentFixture` behavior when using zoneless scheduler (#54024)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-098543f`
+    version: `~17.2.0-next.0+sha-3ca34e6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
