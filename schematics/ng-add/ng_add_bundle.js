1b48209ae6c9aa7f576303d7281041c9996d73b4
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-29 15:48:40 +0000

    96ca49d docs: update input/output guide links (#54135)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-5cd00f1`
+    version: `~17.1.1+sha-96ca49d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
