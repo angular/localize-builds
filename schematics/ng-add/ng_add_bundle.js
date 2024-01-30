4e0785e7cdb2fac4d69ba7623d44e45756eca6c5
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-30 20:14:01 +0000

    c4b880a refactor: migrate docs, examples, private, service worker and upgrade to prettier formatting (#54163)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-274a489`
+    version: `~17.2.0-next.0+sha-c4b880a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
