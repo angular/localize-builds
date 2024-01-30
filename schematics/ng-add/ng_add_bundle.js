2c687d4a84f3d92779b03453e9fa734d4d7359ac
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-30 16:14:30 +0000

    4c2b0d9 refactor: migrate common to prettier formatting (#54159)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-57174bb`
+    version: `~17.1.1+sha-4c2b0d9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
