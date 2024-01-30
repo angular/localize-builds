5929ce4fced9c5c9dfaffd4846d09b77307b74fc
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-30 21:32:42 +0000

    6ec2f07 docs: focus on the respective card of the current active line while scrolling (#53733)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-c4b880a`
+    version: `~17.2.0-next.0+sha-6ec2f07`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
