24b0fb069ccdaef8cb39697029402bf4e52d9195
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 17:41:55 +0000

    0bd668c refactor(router): RouterLink read urlTree only once (#53817)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-948fb5f`
+    version: `~17.1.1+sha-0bd668c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
