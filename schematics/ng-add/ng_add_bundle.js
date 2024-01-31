09178f66e361d86f48d21af62e56156b21e316fa
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:35:50 +0000

    a297012 docs(docs-infra): add tab titles to home, playground and tutorial (#53026)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-2e2e8aa`
+    version: `~17.2.0-next.0+sha-a297012`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
