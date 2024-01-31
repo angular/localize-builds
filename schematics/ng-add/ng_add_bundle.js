fb75e5ce734e97777a4116121b583d092a1545c9
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:09:40 +0000

    34feb4f docs(docs-infra): @input getter setter syntax change (#51023)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-b02cf14`
+    version: `~17.2.0-next.0+sha-34feb4f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
