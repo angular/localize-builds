a8d6976d28c700b3e7838e5b457b7ca4901c9af3
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:49:37 +0000

    b02cf14 fix(docs-infra): sort api reference items (#54116)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-b9aa25e`
+    version: `~17.2.0-next.0+sha-b02cf14`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
