674f9139728042364cec77f0f5d5bba62d437ab4
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2024-01-31 19:08:55 +0000

    57cb448 release: cut the v17.1.2 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-0bd668c`
+    version: `~17.1.2+sha-57cb448`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
