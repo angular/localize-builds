e742c4ec8f03bd602bc9be2b7ab903d8ed372776
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 20:24:27 +0000

    7df133d fix(core): `afterRender` hooks should allow updating state (#54074)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.2+sha-4883ae6`
+    version: `~17.1.2+sha-7df133d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
