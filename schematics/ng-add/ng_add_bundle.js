4d532044b8d72e1366aae7541cede2683d80732c
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:52:25 +0000

    948fb5f build: update slackapi/slack-github-action digest to 2a8087d (#52000)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-ed8e7c7`
+    version: `~17.1.1+sha-948fb5f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
