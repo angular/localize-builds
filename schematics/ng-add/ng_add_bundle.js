9baaf3539659e9dd594c2a555c455a8042a84d0c
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 16:19:39 +0000

    e08cc50 test(docs-infra): unit tests in the reference pages (#53529)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-ca7456e`
+    version: `~17.1.1+sha-e08cc50`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
