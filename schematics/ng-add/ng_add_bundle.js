f2f178e4392f85d1dd645a6f77d133af4293c674
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:28:11 +0000

    3b218a7 docs(core): fix example sections (#54121) PR Close #54121

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.1+sha-4367c44`
+    version: `~17.1.1+sha-3b218a7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
