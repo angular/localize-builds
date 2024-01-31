87f3900a6fb605629f2368bd2405fa4c81f4c5a2
Jessica Janiuk <jessicajaniuk@google.com>
2024-01-31 15:40:17 +0000

    b9aa25e docs: Fix minor grammar mistake (#54094)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.0+sha-d8f417b`
+    version: `~17.2.0-next.0+sha-b9aa25e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
