90f1be92e55a8f9ede220d31491078bd6e215f9b
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 20:23:09 +0000

    908baec [aio/src/styles/custom-themes/{dark,light}-theme.scss] Change theme definitions to resolve warning "Angular Material themes should be created from a map" (#53717)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.1+sha-0e150c4`
+    version: `~17.2.0-next.1+sha-908baec`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
