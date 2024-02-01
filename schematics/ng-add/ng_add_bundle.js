5dec266fde832ccf70b4a1cc601166fc9ccffa7f
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 20:12:23 +0000

    99a0b90 docs: correct TypeScript version range for 17.0.x (#53213)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.2+sha-52a31d0`
+    version: `~17.1.2+sha-99a0b90`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
