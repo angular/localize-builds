638025f6ee2e6abc677212c84a580e80611147c4
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 18:59:51 +0000

    f933a38 docs(docs-infra): Remove the edit button from AIO (#54204)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.2+sha-46a2c5f`
+    version: `~17.1.2+sha-f933a38`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
