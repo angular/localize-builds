5aa84362f6e6f1a707b4187217be86f09493bfc6
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 19:35:06 +0000

    52a31d0 refactor: standardize doctype declaration to <!DOCTYPE html> (#51052)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.2+sha-f933a38`
+    version: `~17.1.2+sha-52a31d0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
