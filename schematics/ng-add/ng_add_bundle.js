d0bb459d081951fe9c4fddba98e8ee8d6f02fdfe
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 21:56:39 +0000

    afc72b0 ci: fix size tracking for aio (#54207)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.2+sha-0fb1142`
+    version: `~17.1.2+sha-afc72b0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
