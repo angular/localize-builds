fd4b2d1129191e825eadfefe09fd7fcd5403a947
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 17:50:05 +0000

    5d98a02 ci: Add framework team to adev approvers (#54082)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.1+sha-36a9bf3`
+    version: `~17.2.0-next.1+sha-5d98a02`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
