97c49c27eb5e41cc72f331c68e11b8a8f75a621d
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 16:08:23 +0000

    36a9bf3 build: update actions/checkout action to v4 (#54183)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.1+sha-3b892e9`
+    version: `~17.2.0-next.1+sha-36a9bf3`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
