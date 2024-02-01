c642db2b82971cb005e9ffbede6b3f1eb7aed84b
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-01 20:53:21 +0000

    3cf612c fix(core): update imports to be compatible with rxjs 6 (#54193)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.1+sha-908baec`
+    version: `~17.2.0-next.1+sha-3cf612c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
