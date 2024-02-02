b5ceb368a75a05610596b3cf0095279b6470ec75
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-02 14:56:07 +0000

    74aa8a3 test(core): ExpressionChanged... error does not happen with signals (#54206)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.1+sha-bdf6927`
+    version: `~17.2.0-next.1+sha-74aa8a3`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
