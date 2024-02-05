0b1a39c81a40d2f643c2fb6239b3d58dd3432c38
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-05 15:20:19 +0000

    1090570 refactor(core): avoid reusing argument name as a local const (#54239)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.1+sha-2ab536e`
+    version: `~17.2.0-next.1+sha-1090570`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
