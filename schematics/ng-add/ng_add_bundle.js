7b95c49bb8c6d231f9aea07260be9725fa2ea1ed
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-05 16:45:30 +0000

    d339941 docs: add Jason Warner to GDE resources (#54237)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.1+sha-1090570`
+    version: `~17.2.0-next.1+sha-d339941`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
