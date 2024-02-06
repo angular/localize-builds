85850311fb15957ec1a4689d1c7638d91fc77e82
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 17:41:38 +0000

    bc4a6a9 fix(compiler-cli): do not error due to multiple components named equally (#54273)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.2+sha-4297005`
+    version: `~17.1.2+sha-bc4a6a9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
