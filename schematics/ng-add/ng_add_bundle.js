19cdec25ae996516d0f1de4b54fddaa002adf818
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 18:43:15 +0000

    4d29174 refactor: address prototype polution concern with better typing (#54286)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.1+sha-4287ca8`
+    version: `~17.2.0-next.1+sha-4d29174`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
