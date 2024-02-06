196e78e77dc2a3d66e0de99e849ed2b4b4c5b4f1
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-06 20:40:02 +0000

    db78666 docs: add alejandrocuba.jpg file to images/bios/ (#53842)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.2+sha-2de7679`
+    version: `~17.1.2+sha-db78666`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
