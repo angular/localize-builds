92bddfc139f5acbe68754ac7e80560796a3cdf42
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-07 19:00:34 +0000

    38c0084 test: verify signal-based queries in CLI integration test (#54314)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.1+sha-a08863e`
+    version: `~17.2.0-next.1+sha-38c0084`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
