4e7f0b24dbf91c7b03496f87f3f6eb9307375da7
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 16:50:45 +0000

    744e206 fix(core): Fix possible infinite loop with `markForCheck` by partially reverting #54074 (#54329)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.2+sha-63a9027`
+    version: `~17.1.2+sha-744e206`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
