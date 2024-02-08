0060da488331ca903c31613bc0b0601d4538b0b5
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 16:34:49 +0000

    63a9027 fix(compiler-cli): interpolatedSignalNotInvoked diagnostic for model signals (#54338)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.2+sha-ef55d48`
+    version: `~17.1.2+sha-63a9027`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
