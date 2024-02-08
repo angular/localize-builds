f4d1830d8ced53f8827b3d9287bdeb19191e00bc
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 16:24:28 +0000

    03c3b3e feat(common): add Netlify image loader (#54311)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.1+sha-9c2bad9`
+    version: `~17.2.0-next.1+sha-03c3b3e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
