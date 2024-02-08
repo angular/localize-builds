06655872340afcc9552cdafa6e6850e67d59d348
Jessica Janiuk <72768744+jessicajaniuk@users.noreply.github.com>
2024-02-08 18:47:51 +0000

    d02fcb1 docs: release notes for the v17.2.0-rc.0 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.1+sha-c47bf63`
+    version: `~17.3.0-next.0+sha-d02fcb1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
