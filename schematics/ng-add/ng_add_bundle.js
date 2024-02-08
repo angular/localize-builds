c65f50ef1fa88bc836af2a36521de0120e94e5d2
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 15:06:47 +0000

    ef55d48 refactor(compiler-cli): avoid naming conflict with built-in global variable (#54333)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.2+sha-2e3fa28`
+    version: `~17.1.2+sha-ef55d48`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
