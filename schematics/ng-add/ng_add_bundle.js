750c5756cd9bca5c3de741956805a348cf5de24c
Jessica Janiuk <jessicajaniuk@google.com>
2024-02-08 16:54:23 +0000

    8dea3b5 test: tidy up signal signal input/query/model signature tests (#54334)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-next.1+sha-adfc3f0`
+    version: `~17.2.0-next.1+sha-8dea3b5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
