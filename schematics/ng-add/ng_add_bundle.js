9ad4bc8d9a96562a8cc23be73026b8bb24e8987c
Andrew Kushnir <akushnir@google.com>
2024-02-09 19:04:18 +0000

    58365e0 refactor(docs-infra): create fast mode (#54364)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.3+sha-2435291`
+    version: `~17.1.3+sha-58365e0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
