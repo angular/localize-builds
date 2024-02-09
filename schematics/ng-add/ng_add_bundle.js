30e0dc5850d8c504f4de9383d6d20b3f6d847e5a
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-02-09 10:58:41 +0000

    7aa92fa release: cut the v17.2.0-rc.1 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-rc.0+sha-4b96f37`
+    version: `~17.2.0-rc.1+sha-7aa92fa`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
