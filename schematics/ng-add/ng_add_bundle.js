8f708e7119ddfe777bc42601af052e3735b09356
Andrew Kushnir <akushnir@google.com>
2024-02-12 22:37:02 +0000

    277540c build: reuse fake core in type checking tests (#54344) (#54398)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0-rc.1+sha-6d00115`
+    version: `~17.2.0-rc.1+sha-277540c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
