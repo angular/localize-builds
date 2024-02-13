d32cf547cfc19b304b5728e0e46c55f31e68a6f0
Andrew Kushnir <akushnir@google.com>
2024-02-13 19:59:30 +0000

    9c486c9 fix(http): Use string body to generate transfer cache key. (#54379)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.1.3+sha-da93046`
+    version: `~17.1.3+sha-9c486c9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
