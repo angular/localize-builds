21aebc07c81f33267cff4dab7796c39cc1a2a5a6
Andrew Kushnir <akushnir@google.com>
2024-02-14 20:53:02 +0000

    7234824 fix(compiler-cli): fix broken version detection condition (#54443)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.0+sha-0b95d14`
+    version: `~17.2.0+sha-7234824`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
