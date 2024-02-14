5fdcd3ac37a8e43961787317a15b529d641ff192
Andrew Kushnir <akushnir@google.com>
2024-02-14 23:49:35 +0000

    da7fbb4 fix(compiler-cli): detect when the linker is working in unpublished angular and widen supported versions (#54439)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.0+sha-12d4b8e`
+    version: `~17.3.0-next.0+sha-da7fbb4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
