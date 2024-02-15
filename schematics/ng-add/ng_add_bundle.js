3cb5a1e14f2301fb1549be12a1c108044c74a18b
Andrew Kushnir <akushnir@google.com>
2024-02-15 18:38:52 +0000

    03acdbe fix(docs-infra): process mermaid code blocks (#54462)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.0+sha-6c2d4b6`
+    version: `~17.3.0-next.0+sha-03acdbe`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
