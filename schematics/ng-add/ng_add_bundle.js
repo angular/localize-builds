697b906282802393033651af2e0dff3cf9c7e82f
Andrew Kushnir <akushnir@google.com>
2024-02-15 20:26:34 +0000

    3bd5860 fix(core): properly execute content queries for root components (#54457)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.1+sha-ba686cc`
+    version: `~17.2.1+sha-3bd5860`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
