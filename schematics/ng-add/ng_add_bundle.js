f2cc0c8070320f2dff8fe9e215543fafa17cb28b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-20 17:38:13 +0000

    a9741e6 docs(router): deprecate RouterTestingModule (#54466)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.0+sha-70345b1`
+    version: `~17.3.0-next.0+sha-a9741e6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
