0d017556a21c3c0232a049d14d5743edc37eadf0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-20 17:55:12 +0000

    d34e329 fix(common): image placeholder not removed in OnPush component (#54515)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.1+sha-f8a5b40`
+    version: `~17.2.1+sha-d34e329`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
