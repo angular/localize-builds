d492dbdcf7d2896a4a601a80d5a1ea8867083b55
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-02-21 22:16:44 +0000

    3e31f1a fix(router): Clear internal transition when navigation finalizes (#54261)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.1+sha-d411ee7`
+    version: `~17.2.1+sha-3e31f1a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
