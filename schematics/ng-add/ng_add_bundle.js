e84215f205fcddd3c04dede6d059ebd5d9e6f23c
Joey Perrott <josephperrott@gmail.com>
2024-02-21 16:04:19 +0000

    d411ee7 docs: typos in signal queries guide (#54540)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.1+sha-d34e329`
+    version: `~17.2.1+sha-d411ee7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
