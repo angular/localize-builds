633a7b123ff5d1fe4c9f7b83b01f4a4b7b1d1e3c
Joey Perrott <josephperrott@gmail.com>
2024-02-21 16:57:43 +0000

    e9246f7 refactor(docs-infra): correct location of hosted images (#54547)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.0+sha-554628e`
+    version: `~17.3.0-next.0+sha-e9246f7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
