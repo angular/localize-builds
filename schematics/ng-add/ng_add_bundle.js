2d4f4bed097317b6c8ee52c8b2ed35bd4c088405
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 20:36:32 +0000

    dcb9deb fix(core): collect providers from NgModules while rendering `@defer` block (#52881)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.0+sha-0d95ae5`
+    version: `~17.3.0-next.0+sha-dcb9deb`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
