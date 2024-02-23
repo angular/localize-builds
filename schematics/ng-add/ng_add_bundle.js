28110e70b58fc4e4714f134c1a9b2c9c61f10faa
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 20:23:44 +0000

    0d95ae5 refactor(core): use performance API for signals (#54521)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.0+sha-4efcc74`
+    version: `~17.3.0-next.0+sha-0d95ae5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
