c81434483a42a9cc93a1493070a8eadd4ed6eaaf
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 19:54:36 +0000

    4efcc74 docs: add a guide for model inputs (#54514)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.0+sha-1a6beae`
+    version: `~17.3.0-next.0+sha-4efcc74`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
