97cba2d91fa3232a7c388f0101e5d36fea8dbe90
Dylan Hunn <dylhunn@gmail.com>
2024-02-23 18:05:27 +0000

    3f95829 refactor(core): Allow dirty views to be refreshed in a loop internally (#54572)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.0+sha-707bfc9`
+    version: `~17.3.0-next.0+sha-3f95829`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
