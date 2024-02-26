4f318432efba04a5e2c9bcbf8095cf2919ef51b7
Dylan Hunn <dylhunn@gmail.com>
2024-02-26 17:09:28 +0000

    69948e1 refactor(dev-infra): Import angular LS package in g3 (#54608)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.0+sha-d4343b5`
+    version: `~17.3.0-next.0+sha-69948e1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
