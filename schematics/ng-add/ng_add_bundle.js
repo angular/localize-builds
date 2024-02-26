9d52831c4b589e77cff3542a8d4a7f889f2a8351
Joey Perrott <josephperrott@gmail.com>
2024-02-26 18:30:07 +0000

    54b5a2f ci: correct @jeanmeche to @JeanMeche in pullapprove (#54610)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.0+sha-69948e1`
+    version: `~17.3.0-next.0+sha-54b5a2f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
