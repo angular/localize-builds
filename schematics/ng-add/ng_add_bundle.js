18c42b3266cba6d267a74286446377890ec7954f
Dylan Hunn <dylhunn@gmail.com>
2024-02-27 22:12:06 +0000

    6470e33 build: update io_bazel_rules_sass digest to 888dd9c (#54615)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.0+sha-3e0b3a1`
+    version: `~17.3.0-next.0+sha-6470e33`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
