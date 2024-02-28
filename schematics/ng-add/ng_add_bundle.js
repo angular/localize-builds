fb2e08f36f78f74822518a0d689a9b231795f2de
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 17:15:00 +0000

    c1c7384 feat(router): Add reusable types for router guards (#54580)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.1+sha-d7c6865`
+    version: `~17.3.0-next.1+sha-c1c7384`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
