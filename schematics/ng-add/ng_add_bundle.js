9338c4835210fc9781d4e158af173423ec57bacf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 13:44:14 +0000

    2f875d0 docs: update versioning and deprecation policies (#51423)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.1+sha-52970e3`
+    version: `~17.3.0-next.1+sha-2f875d0`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
