a162fa5efeb80ac2f8813ce94e03e4b707419b38
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 14:21:08 +0000

    b0f434a docs: fix broken markdown syntax (#53567)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.3+sha-c05bef0`
+    version: `~17.2.3+sha-b0f434a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
