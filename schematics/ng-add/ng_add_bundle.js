7fa8e17b83d649300c9a85c22c348ae0297a2694
Dylan Hunn <dylhunn@gmail.com>
2024-02-28 00:33:58 +0000

    e923545 docs: release notes for the v17.2.3 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.1+sha-ccda258`
+    version: `~17.3.0-next.1+sha-e923545`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
