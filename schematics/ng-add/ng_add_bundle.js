9118eb911942454df2c2bf651631331fcf0959fe
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 17:11:19 +0000

    917b9bd fix(compiler-cli): unwrap expressions with type parameters in query read property (#54647)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.3+sha-7ca08aa`
+    version: `~17.2.3+sha-917b9bd`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
