958397a18ed96b27a8be6d88202a35f05ef87365
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-28 16:20:09 +0000

    c19c44d docs: fix broken link in AIO (#54649)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.3+sha-b0f434a`
+    version: `~17.2.3+sha-c19c44d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
