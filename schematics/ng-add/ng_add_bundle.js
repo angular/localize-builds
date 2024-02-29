91e20a70eb50315e878ff991c6d51d56d7c822a7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-02-29 10:45:18 +0000

    ffbafc7 fix(core): untrack various core operations (#54614)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.3+sha-917b9bd`
+    version: `~17.2.3+sha-ffbafc7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
