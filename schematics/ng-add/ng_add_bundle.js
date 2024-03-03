69db1eb9b1e2dab97e93756c2a0f67ed417b65d3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-03 13:03:35 +0000

    3e703f5 docs: fix broken link on the tutorial page (#54669)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.1+sha-66d78a7`
+    version: `~17.3.0-next.1+sha-3e703f5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
