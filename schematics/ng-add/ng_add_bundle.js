c812c1005c155038f8be682f3319c73f77f2975c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-03 13:04:20 +0000

    0767adc docs: fix broken link on the tutorial page (#54669)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.3+sha-37dcf8d`
+    version: `~17.2.3+sha-0767adc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
