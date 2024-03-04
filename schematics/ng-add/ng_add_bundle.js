cb3b247f653542832057b87b1a1f2589afd75963
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 18:13:25 +0000

    b558a01 fix(core): generic inference for signal inputs may break with `--strictFunctionTypes` (#54652)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.3+sha-2fc1971`
+    version: `~17.2.3+sha-b558a01`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
