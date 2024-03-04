ae903490d01e071692b4b43ee3fd08d17130f389
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 09:41:33 +0000

    07173fd refactor(core): Pending effects should make the application unstable (#53835)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.1+sha-0d98972`
+    version: `~17.3.0-next.1+sha-07173fd`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
