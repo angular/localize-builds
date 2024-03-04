1439fd0611c4a8501080c2d04aab9f7cff27693b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 16:46:39 +0000

    6531e4c ci: Add primitives configurations for ng-dev tools (#54662)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.1+sha-07d5e42`
+    version: `~17.3.0-next.1+sha-6531e4c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
