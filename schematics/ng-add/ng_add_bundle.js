7de4d321fbb16c2a984f7179a8cb7a1af891b803
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-04 17:21:41 +0000

    5e32a77 refactor(compiler): cleanup unused code in template builder pipeline (#54654)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.1+sha-6531e4c`
+    version: `~17.3.0-next.1+sha-5e32a77`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
