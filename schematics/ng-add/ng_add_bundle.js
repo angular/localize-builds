5765b9ba8dba158586930a2423a0a3708d9b8fb7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-05 09:33:24 +0000

    b40f1e5 refactor(compiler): Remove deep imports in the language service (#54695)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.1+sha-33a6fab`
+    version: `~17.3.0-next.1+sha-b40f1e5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
