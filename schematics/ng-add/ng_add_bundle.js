8b16dbe76894fa28b4fab7e6493f39bc0aa1a3e7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-05 17:40:59 +0000

    d2ec5b6 docs: fix clipped `docs-code` block in `docs-reference-api-tab` (#54621)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.1+sha-0ecf515`
+    version: `~17.3.0-next.1+sha-d2ec5b6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
