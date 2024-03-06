011514a113d7c7f8197955c51a26134d0d59ca68
Andrew Scott <atscott@google.com>
2024-03-06 21:40:25 +0000

    d9c0a16 refactor(core): Fix internal test util types (#54736)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.0+sha-fc0f47c`
+    version: `~18.0.0-next.0+sha-d9c0a16`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
