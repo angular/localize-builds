3684d4173bc5a0a8ab95774039dbb1534abb0d42
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-03-06 19:49:38 +0000

    781dbe2 docs: release notes for the v17.3.0-rc.0 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-next.1+sha-4be9632`
+    version: `~17.4.0-next.0+sha-781dbe2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
