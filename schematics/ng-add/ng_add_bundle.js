b1336a97eab745b6e71bf65df40b50db78e0fc07
Andrew Scott <atscott@google.com>
2024-03-06 23:49:33 +0000

    ad045ef fix(core): Ensure views marked for check are refreshed during change detection (#54735)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.0+sha-d9c0a16`
+    version: `~18.0.0-next.0+sha-ad045ef`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
