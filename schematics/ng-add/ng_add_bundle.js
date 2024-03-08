a4e3bc794abeffb5581f20f3b0d8f58c9691d848
Andrew Scott <atscott@google.com>
2024-03-08 00:19:02 +0000

    8180c39 build: fix size failure (#54756)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.0+sha-115ee88`
+    version: `~18.0.0-next.0+sha-8180c39`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
