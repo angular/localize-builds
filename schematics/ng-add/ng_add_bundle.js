a255c0b0b8e58eea0f6a136a7dc775b2c8e76c51
Andrew Scott <atscott@google.com>
2024-03-11 19:52:35 +0000

    2909e98 fix(core): prevent infinite loops in clobbered elements check (#54425)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-rc.0+sha-619f3c8`
+    version: `~17.3.0-rc.0+sha-2909e98`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
