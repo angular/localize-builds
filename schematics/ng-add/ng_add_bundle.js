dc4d45760e538708f30592fe52b373b04bbeef24
Andrew Scott <atscott@google.com>
2024-03-11 18:18:13 +0000

    eff06ea refactor(core): add `i18nNodes` in preparation for i18n hydration (#54750)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.2.4+sha-4db321c`
+    version: `~17.2.4+sha-eff06ea`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
