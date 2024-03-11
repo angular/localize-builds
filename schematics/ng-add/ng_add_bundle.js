9498e263f748a9eab692ad99c92a7f4f9c3e6efe
Andrew Scott <atscott@google.com>
2024-03-11 16:26:52 +0000

    81ccf5d fix(compiler): not catching for loop empty tracking expressions (#54772)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.0+sha-d269c88`
+    version: `~18.0.0-next.0+sha-81ccf5d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
