130eddfaf304a514ec73e2c1f315df3bb7571fc9
Andrew Scott <atscott@google.com>
2024-03-12 21:10:56 +0000

    edc1740 refactor(core): restructure logic in `isCssClassMatching` function (#54800)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.0+sha-0f7bbd0`
+    version: `~18.0.0-next.0+sha-edc1740`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
