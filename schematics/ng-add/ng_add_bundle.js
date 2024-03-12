bc37c124c1e0baeab2963587427a2e4c4b3982bb
Andrew Scott <atscott@google.com>
2024-03-12 18:38:13 +0000

    db2f9a9 refactor(compiler-cli): cleanup unused code (#54775)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.0+sha-4ff94aa`
+    version: `~18.0.0-next.0+sha-db2f9a9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
