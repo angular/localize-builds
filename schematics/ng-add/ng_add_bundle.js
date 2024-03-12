5845f355fe0afac1e45a6fe1c57883d9cbbb7de8
Andrew Scott <atscott@google.com>
2024-03-12 20:17:52 +0000

    e8badec docs: fix missing security guide in navigation (#54830)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0-rc.0+sha-cdfcb77`
+    version: `~17.3.0-rc.0+sha-e8badec`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
