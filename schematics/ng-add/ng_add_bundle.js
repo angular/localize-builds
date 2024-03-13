bf93e68b7201ae2ea3f4cfcd74e665d47cd2637e
Andrew Scott <atscott@google.com>
2024-03-13 18:41:21 +0000

    2d47329 build: update symbol goldens (#54843)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.0+sha-0dd441d`
+    version: `~18.0.0-next.0+sha-2d47329`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
