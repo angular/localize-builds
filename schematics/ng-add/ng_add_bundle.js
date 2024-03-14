44c4cf774fda82b69cb1afc38a529aa74ea6e175
Andrew Scott <atscott@google.com>
2024-03-14 16:00:47 +0000

    b71a323 docs: update Angular CLI help [main] (#54861)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.0+sha-77a397b`
+    version: `~18.0.0-next.0+sha-b71a323`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
