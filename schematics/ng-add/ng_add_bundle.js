f878697b31ceb58b6e701042b8f2452fb0bd6773
Joey Perrott <josephperrott@gmail.com>
2024-03-15 18:37:36 +0000

    33dc072 build: update dependency google-closure-compiler to v20231112 (#54893)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0+sha-209632e`
+    version: `~17.3.0+sha-33dc072`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
