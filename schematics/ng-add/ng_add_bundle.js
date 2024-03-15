300623cbc6e7fec92f5d4520442ad7580f0f35b7
Joey Perrott <josephperrott@gmail.com>
2024-03-15 16:48:02 +0000

    a5cc82e docs: update events (#54749)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0+sha-c22c5c4`
+    version: `~17.3.0+sha-a5cc82e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
