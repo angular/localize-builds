eefc0e9388d98efca88735d11b8efc9dd3cb3d4b
Joey Perrott <josephperrott@gmail.com>
2024-03-15 18:06:48 +0000

    9574eda build: lock file maintenance (#54890)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.0+sha-e56c86f`
+    version: `~18.0.0-next.0+sha-9574eda`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
