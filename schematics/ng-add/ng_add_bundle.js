a73f4e3840813e15e200274377751708cf86a979
Joey Perrott <josephperrott@gmail.com>
2024-03-17 00:45:55 +0000

    f50afb1 build: update to latest angular/dev-infra/github-actions/create-pr-for-changes (#54904)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.0+sha-5328be6`
+    version: `~18.0.0-next.0+sha-f50afb1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
