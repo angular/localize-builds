ebab2e012bac655010b2988202c0fad8cba053f3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-03-18 23:43:54 +0000

    1f5ab96 refactor(core): allow passing an environment injector while creating a view (#54903)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.0+sha-860ecce`
+    version: `~17.3.0+sha-1f5ab96`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
