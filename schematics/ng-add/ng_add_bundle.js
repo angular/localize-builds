1bc060e5ef40a8407c28e4bea3d26ac475f172f3
Andrew Scott <atscott@google.com>
2024-03-20 20:36:26 +0000

    78d4ad2 ci: temporarily disable failing test (#54970)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.1+sha-c2149df`
+    version: `~17.3.1+sha-78d4ad2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
