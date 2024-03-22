51eb5b915b24d28c4e95eae40b7ebfaf7051aafe
Dylan Hunn <dylhunn@gmail.com>
2024-03-22 05:23:54 +0000

    5bd188a feat(compiler-cli): add partial compilation support for deferred blocks (#54908)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.1+sha-0461bff`
+    version: `~18.0.0-next.1+sha-5bd188a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
