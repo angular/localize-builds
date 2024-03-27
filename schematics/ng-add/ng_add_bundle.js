37098d0097edd195f9692603d5e61486e0fcd72b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-03-27 12:04:36 +0000

    9aff43b ci: fix build failures in patch branch (#55064)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.1+sha-fa46844`
+    version: `~17.3.1+sha-9aff43b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
