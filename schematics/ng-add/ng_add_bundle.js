2a03c21c0043611a2d0dd3692444d896ff3f4ecd
Dylan Hunn <dylhunn@gmail.com>
2024-03-27 18:06:55 +0000

    658cf8c fix(core): `ComponentFixture` stability should match `ApplicationRef` (#54949)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.1+sha-e44b077`
+    version: `~18.0.0-next.1+sha-658cf8c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
