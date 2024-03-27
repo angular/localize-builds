995cf2fc1d266d391edd37ca36bd21835a8cf12a
GitHub <noreply@github.com>
2024-03-27 14:52:10 +0000

    2ad5dcf [17.3.x]: build: follow-up fixes for AIO lighthouse checks (#55065)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.1+sha-9aff43b`
+    version: `~17.3.1+sha-2ad5dcf`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
