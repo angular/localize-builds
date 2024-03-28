8c6c4f835bee5cc0b56074b72f7d5476ba5f73b3
Dylan Hunn <dylhunn@gmail.com>
2024-03-28 16:29:11 +0000

    708ba81 fix(core): establish proper injector resolution order for `@defer` blocks (#55079)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.1+sha-fb5a288`
+    version: `~17.3.1+sha-708ba81`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
