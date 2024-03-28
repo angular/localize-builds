74f8922f2d2268ef283858c4ccb6f24084bbc270
Andrew Scott <atscott@google.com>
2024-03-28 22:11:52 +0000

    878f581 docs: remove broken link from CONTRIBUTING.md (#54953)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.2+sha-9cef0d2`
+    version: `~17.3.2+sha-878f581`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
