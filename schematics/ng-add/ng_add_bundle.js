df413994606bfa916c335ca9f07f867e84c36c76
Dylan Hunn <dylhunn@gmail.com>
2024-03-28 16:49:55 +0000

    2f9d94b fix(migrations): account for variables in imports initializer (#55081)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.1+sha-708ba81`
+    version: `~17.3.1+sha-2f9d94b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
