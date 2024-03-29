c6070ffd44fcd675ecb0cd38834a3a201f61ec5f
Andrew Scott <atscott@google.com>
2024-03-29 16:45:16 +0000

    e53d02d docs(http): Update the Setup for testing in Http Client to include provideHttpClient() (#53716)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.2+sha-109df12`
+    version: `~17.3.2+sha-e53d02d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
