46e13a3885cdd31ae67c69972c80adbf91f2a6b9
Andrew Scott <atscott@google.com>
2024-03-29 17:48:04 +0000

    3974c21 refactor(core): All views attached to application are treated as OnPush in zoneless (#55099)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.2+sha-a122bd1`
+    version: `~18.0.0-next.2+sha-3974c21`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
