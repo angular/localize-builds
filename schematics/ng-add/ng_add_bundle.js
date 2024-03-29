b1a2cbdce9a5be20caa473776482c444c6ea73e8
Andrew Scott <atscott@google.com>
2024-03-29 16:27:23 +0000

    0bcea6c docs: document why `undefined` is usefull and does not remove classes (#55112)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.2+sha-2c325d4`
+    version: `~17.3.2+sha-0bcea6c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
