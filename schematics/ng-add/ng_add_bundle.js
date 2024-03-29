239ed8cb1bf4421cd310b5511a705645cbd3f1e4
Andrew Scott <atscott@google.com>
2024-03-29 20:15:56 +0000

    949dec2 fix(migrations): avoid conflicts with some greek letters in control flow migration (#55113)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.2+sha-d10dc5c`
+    version: `~17.3.2+sha-949dec2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
