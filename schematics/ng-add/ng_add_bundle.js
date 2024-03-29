d1599131da919913921f8524ffd1ee00525d957e
Andrew Scott <atscott@google.com>
2024-03-29 19:03:34 +0000

    1ae460c docs: remove unused examples (#55110)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.2+sha-e4fde98`
+    version: `~18.0.0-next.2+sha-1ae460c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
