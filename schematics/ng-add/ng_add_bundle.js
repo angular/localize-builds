cb861793cafa3e0e1535eae0461098f0fd1e62c2
Andrew Scott <atscott@google.com>
2024-03-30 17:56:49 +0000

    87f3f27 feat(router): Allow resolvers to return `RedirectCommand` (#54556)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.2+sha-8a8181a`
+    version: `~18.0.0-next.2+sha-87f3f27`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
