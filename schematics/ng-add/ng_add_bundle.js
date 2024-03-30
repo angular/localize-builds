5c7fe4c38bca4cf6905869c231c40e1e777ec8ae
Andrew Scott <atscott@google.com>
2024-03-30 02:10:37 +0000

    a84beaa fix(docs-infra): scrolling experience in API Reference (#55133)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.2+sha-fd45d03`
+    version: `~17.3.2+sha-a84beaa`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
