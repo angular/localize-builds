9bca62a5eb05095f0f78e5d94d8dd38ac59eda6a
Andrew Scott <atscott@google.com>
2024-04-01 17:04:55 +0000

    bc429ba ci: Update workflows to use latest dev infra hash (#55128)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.2+sha-a84beaa`
+    version: `~17.3.2+sha-bc429ba`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
