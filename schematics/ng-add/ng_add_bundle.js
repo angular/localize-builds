672e0a5e20d6459ebfd5ae94528846d4205adb17
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-02 18:38:00 +0000

    1991508 ci: bump ng-dev hashes (#55172)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.2+sha-58942db`
+    version: `~18.0.0-next.2+sha-1991508`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
