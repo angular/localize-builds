6fde2a04e857ab780fc44e0fd9f4d3bb7ebcf88f
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-02 16:26:12 +0000

    7b67ad2 refactor(language-service): support showing tags info in the completion (#51140)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.2+sha-129c973`
+    version: `~17.3.2+sha-7b67ad2`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
