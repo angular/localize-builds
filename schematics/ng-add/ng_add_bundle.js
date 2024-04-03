f1760e6b3cf9cf39fa33068cda5b91516a8740db
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 18:50:32 +0000

    4ac78fb ci: remove event-dispatch in pullapprove (#55200)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.2+sha-5ba2a48`
+    version: `~17.3.2+sha-4ac78fb`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
