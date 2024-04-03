9a8dbafb1ab11c1360bf28386e38ec73183755f8
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 17:16:54 +0000

    1c736dc feat(forms): Unified Control State Change Events (#54579)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.2+sha-2ff74dc`
+    version: `~18.0.0-next.2+sha-1c736dc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
