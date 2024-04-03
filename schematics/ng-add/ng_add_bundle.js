b8a9b389a13939b81299b969e49546874454492a
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 15:32:53 +0000

    45ae7a6 feat(platform-browser): add withI18nSupport() in developer preview (#55130)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.2+sha-f577319`
+    version: `~18.0.0-next.2+sha-45ae7a6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
