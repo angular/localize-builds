8bd59415da04eb54e1e04c3a1b0d71eafd927b07
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 17:11:45 +0000

    eb9a5f5 ci: Add primitives groups to pullapprove (#53404)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.2+sha-158ceaf`
+    version: `~17.3.2+sha-eb9a5f5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
