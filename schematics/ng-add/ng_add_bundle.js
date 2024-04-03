32a06087737160f225425d6d7df264c707652bdc
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 18:13:43 +0000

    88f0545 refactor(core): remove unnecessary folder (#55197)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.2+sha-eb9a5f5`
+    version: `~17.3.2+sha-88f0545`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
