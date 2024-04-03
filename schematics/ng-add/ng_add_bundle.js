fa4475cc3a1d3af34610541124dc55ce3a7bbbc9
Jessica Janiuk <jessicajaniuk@google.com>
2024-04-03 00:19:31 +0000

    f577319 docs: fix typo in first-app/steps/06-property-binding/README.md (#55181)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.2+sha-894c1c5`
+    version: `~18.0.0-next.2+sha-f577319`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
