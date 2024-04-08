7e0226b5c3871281726f616f2a9c002bf44a226c
Andrew Scott <atscott@google.com>
2024-04-08 21:38:31 +0000

    5587dde docs: add back dynamic form image to fix adev broken link (#55247)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.3+sha-650101e`
+    version: `~17.3.3+sha-5587dde`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
