79d56a88b7790d087b5ef64957fca0e69244ca3f
Andrew Scott <atscott@google.com>
2024-04-08 18:54:54 +0000

    87cdfaf docs: replace `code-example` with `docs-code` (#55186)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.3+sha-f8d5921`
+    version: `~18.0.0-next.3+sha-87cdfaf`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
