b187acfea63a16efcc3bfe4f8a447080183bbc3a
Andrew Scott <atscott@google.com>
2024-04-09 19:22:36 +0000

    545fb0d docs(forms): validate email field with proper validator (#55142)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.3+sha-d0ae556`
+    version: `~17.3.3+sha-545fb0d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
