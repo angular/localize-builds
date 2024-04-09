e84247214a940253bb5fa77d86bc1fec5898e620
Andrew Scott <atscott@google.com>
2024-04-09 17:38:38 +0000

    9d6effc docs: update readme link (#55155)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.3+sha-5587dde`
+    version: `~17.3.3+sha-9d6effc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
