705b7123d6b17f9f1c6d5d812062d1d8d081b614
Andrew Scott <atscott@google.com>
2024-04-09 18:26:46 +0000

    d0ae556 docs: fix input name (#55141)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.3+sha-9d6effc`
+    version: `~17.3.3+sha-d0ae556`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
