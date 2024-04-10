7107879aac4f7518b7d0d91be18897d3769457ae
Andrew Scott <atscott@google.com>
2024-04-10 18:04:32 +0000

    ce231fb ci: Merge primitives-shared and primitives-event-dispatch groups (#55273)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.3+sha-888360a`
+    version: `~17.3.3+sha-ce231fb`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
