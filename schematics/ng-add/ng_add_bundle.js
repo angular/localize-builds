3f332bdf63942b1da35ab8da25e85138839d8840
Andrew Scott <atscott@google.com>
2024-04-10 20:11:34 +0000

    ca8969b Revert "docs(docs-infra): move CLI docs to ADEV (#55242)" (#55291)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.3+sha-ce231fb`
+    version: `~17.3.3+sha-ca8969b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
