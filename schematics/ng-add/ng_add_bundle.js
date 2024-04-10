91c9542dc47a2b8c288d88df3fa9f18bbd4a9985
Andrew Scott <atscott01@gmail.com>
2024-04-10 23:23:21 +0000

    aae062e release: cut the v18.0.0-next.4 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.3+sha-69927ce`
+    version: `~18.0.0-next.4+sha-aae062e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
