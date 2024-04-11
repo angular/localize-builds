b5f369bfaf4e4c36dbaf6c927e5b8be84bcce421
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-11 15:30:53 +0000

    c77ebb6 docs(core): add missing space (#55287)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.4+sha-b955999`
+    version: `~17.3.4+sha-c77ebb6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
