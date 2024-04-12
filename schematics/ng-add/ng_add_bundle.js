2a824e779d004991eb68d66c50410535c8d6afcf
Andrew Scott <atscott@google.com>
2024-04-12 17:48:01 +0000

    c9abe77 docs: Simply redirect function example for router (#55317)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.4+sha-11523c1`
+    version: `~18.0.0-next.4+sha-c9abe77`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
