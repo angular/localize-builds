dfb634ef77b535197fd6961b5435487b4ce82964
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-12 05:44:28 +0000

    f2f2c2a test(core): fix expectations that accidentally used incorrect matchers (#55294)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.4+sha-ab910e2`
+    version: `~18.0.0-next.4+sha-f2f2c2a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
