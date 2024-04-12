1d5b1b789bf87c03c7610a7de6cb473ba37cf524
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-12 08:39:09 +0000

    8a670e6 build: update io_bazel_rules_sass digest to c3f163f (#55307)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.4+sha-afdcff3`
+    version: `~17.3.4+sha-8a670e6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
