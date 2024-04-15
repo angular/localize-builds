1a5c89485285bfdc8a6f0f8eba9a88ae8051d161
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-15 14:41:41 +0000

    3471c41 build(docs-infra): fix playground lezer parsing (#55349)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.4+sha-9f7b9ed`
+    version: `~18.0.0-next.4+sha-3471c41`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
