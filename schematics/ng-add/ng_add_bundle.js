19061d42fecc418ad25e210ec8d83051868e6cb6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-15 14:16:36 +0000

    9f7b9ed refactor(core): warn about duplicated keys when using built-in @for (#55243)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.4+sha-cfed97d`
+    version: `~18.0.0-next.4+sha-9f7b9ed`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
