91d635d931e103c5881f910cac128f1f46191929
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-16 23:35:49 +0000

    e6425f7 refactor(core): use patched timers in root zone for zoneless scheduler (#55367)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.4+sha-f09c5a7`
+    version: `~18.0.0-next.4+sha-e6425f7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
