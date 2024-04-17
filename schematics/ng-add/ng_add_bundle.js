dc4e1c2802217b051bc2d9310e9d81495dfb185e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-17 08:42:07 +0000

    9f5e90e build: make JSA contract binary accessible (#55361)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.4+sha-ff686f3`
+    version: `~18.0.0-next.4+sha-9f5e90e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
