918199777e2a789354f4b19734f7a568f3d48e1c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-17 00:15:29 +0000

    a5623dc Revert "refactor(core): warn about duplicated keys when using built-in @for (#55243)" (#55373)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.4+sha-e6425f7`
+    version: `~18.0.0-next.4+sha-a5623dc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
