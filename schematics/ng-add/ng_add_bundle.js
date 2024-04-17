76998007a30f9a35da703be1c09c52357e8a3360
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-17 05:57:46 +0000

    5f85972 release: bump Angular DevTools version to 1.0.13 (#55371)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.4+sha-de7447d`
+    version: `~18.0.0-next.4+sha-5f85972`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
