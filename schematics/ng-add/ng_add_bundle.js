7e03476c38d6c9de29979962dc4fbb2d2078850b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-04-17 09:34:16 +0000

    a34267b refactor(core): Switch stopPropagation default to false (#55351)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.4+sha-9f5e90e`
+    version: `~18.0.0-next.4+sha-a34267b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
