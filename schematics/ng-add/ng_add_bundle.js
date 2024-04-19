a4305e1199035a5e4a689d807d46a411dab2138e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-19 20:16:50 +0000

    a730f09 feat(core): Add a public API to establish events to be replayed and an attribute to mark an element with an event handler. (#55356)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.5+sha-b67e11a`
+    version: `~18.0.0-next.5+sha-a730f09`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
