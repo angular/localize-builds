520bbde162dc8e015deb0730ad0fd2d2a49922a3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-22 18:31:56 +0000

    a19e214 build: update actions/cache digest to 0c45773 (#54950)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.5+sha-39b7bda`
+    version: `~17.3.5+sha-a19e214`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
