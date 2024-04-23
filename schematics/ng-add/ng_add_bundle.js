c59562e8a00376eee6674351c9ada02c0466c60c
Andrew Kushnir <akushnir@google.com>
2024-04-23 22:23:26 +0000

    5083d89 build: update scorecard action dependencies (#55473)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.5+sha-9cb003a`
+    version: `~17.3.5+sha-5083d89`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
