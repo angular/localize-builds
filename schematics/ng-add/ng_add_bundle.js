1dc0ed77c55d06238a4fb874f4d0be6786e33500
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-23 19:17:27 +0000

    f914f6a feat(migrations): Migration schematics for `HttpClientModule` (#54020)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.5+sha-3d84c98`
+    version: `~18.0.0-next.5+sha-f914f6a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
