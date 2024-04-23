602a92bca765aebe2da97b754cb6385bffdb4190
Andrew Kushnir <akushnir@google.com>
2024-04-23 23:13:14 +0000

    811fe00 refactor(core): Replay events from the event contract using the dispatcher. (#55467)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.5+sha-2e2ca5e`
+    version: `~18.0.0-next.5+sha-811fe00`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
