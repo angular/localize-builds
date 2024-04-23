1960c483069330321418a56d4ba0b822f50dc054
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-23 19:28:09 +0000

    d34c033 refactor(common): Deprecate Local Data API functions (#54483)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.5+sha-f914f6a`
+    version: `~18.0.0-next.5+sha-d34c033`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
