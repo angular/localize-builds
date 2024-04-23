f3b93b215b78a9dc0f75b54e76c052cb280035a5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-04-23 18:10:26 +0000

    3d84c98 refactor(core): Make some minor changes to facilitate testing and usage of event-dispatch. (#55411)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.5+sha-f84d7dd`
+    version: `~18.0.0-next.5+sha-3d84c98`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
