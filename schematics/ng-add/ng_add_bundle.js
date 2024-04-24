31ae351f297daf10eaf45db246b5cd35a552248f
Andrew Kushnir <akushnir@google.com>
2024-04-24 17:39:53 +0000

    9ab36cf refactor(animations): Ensure async animations applies changes when loaded in zoneless (#55132)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.5+sha-4bf3d89`
+    version: `~18.0.0-next.5+sha-9ab36cf`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
