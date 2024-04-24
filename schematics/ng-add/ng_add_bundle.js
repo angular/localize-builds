844490d08aa6eff571a15f53d52c6553adfba5d8
Andrew Kushnir <akushnir@google.com>
2024-04-24 23:40:23 +0000

    e3d5607 Revert "refactor(core): Ensure DOM removal happens when no app views need refresh (#55132)" (#55524)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.5+sha-617bc33`
+    version: `~18.0.0-next.5+sha-e3d5607`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
