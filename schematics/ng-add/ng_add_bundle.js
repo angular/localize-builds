2fa03b3b1dc50c1512a553d62f280ce050d3105e
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-04-25 15:40:24 +0000

    2a3db67 release: cut the v18.0.0-next.6 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.5+sha-e478180`
+    version: `~18.0.0-next.6+sha-2a3db67`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
