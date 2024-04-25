f9b65fad9e426a4b44bb6cdae28b9bfae9de1655
Andrew Kushnir <akushnir@google.com>
2024-04-25 20:00:38 +0000

    9258160 docs: Update di-in-action.md (#55489) PR Close #55489

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.6+sha-af0eb84`
+    version: `~17.3.6+sha-9258160`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
