ded07e684b1ebdad062346c3f9e6f50ad2459e06
Andrew Kushnir <akushnir@google.com>
2024-04-25 16:12:04 +0000

    a177abe fix(core): render hooks should not specifically run outside the Angular zone (#55399)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.6+sha-5ad2f5f`
+    version: `~18.0.0-next.6+sha-a177abe`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
