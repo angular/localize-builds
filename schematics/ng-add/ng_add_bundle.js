334eff30fdabb1e490c1c0612b1fa3c72e0fe8a0
Andrew Kushnir <akushnir@google.com>
2024-04-25 21:36:34 +0000

    9ec0136 refactor(core): Do not duplicate change detection with run coalescing (part 2) (#55403)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.6+sha-a80fa8d`
+    version: `~18.0.0-next.6+sha-9ec0136`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
