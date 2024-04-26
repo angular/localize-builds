e182d8501116cfd1bfe17d12801e29b959e27987
Andrew Kushnir <akushnir@google.com>
2024-04-26 16:48:04 +0000

    b5ec30a refactor(core): Permit disabling autoDetect for zoneless fixture (#55494)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-next.6+sha-f93e518`
+    version: `~18.0.0-next.6+sha-b5ec30a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
