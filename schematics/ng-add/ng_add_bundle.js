79aa775815236dbab681da9d6766bfb9bf052556
Andrew Kushnir <akushnir@google.com>
2024-04-26 19:41:27 +0000

    ac4e8f7 docs: remove non existing link (#55562)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.6+sha-51ac883`
+    version: `~17.3.6+sha-ac4e8f7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
