77b376e1b1de1476ee1cb42d1bd3a2b5a5ea12a9
Andrew Kushnir <akushnir@google.com>
2024-05-01 20:48:10 +0000

    bf25dfa refactor(core): Add a boostrapCustomEvent method. (#55603)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.0+sha-b9a997c`
+    version: `~18.1.0-next.0+sha-bf25dfa`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
