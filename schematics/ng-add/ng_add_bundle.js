79376be3ce8a36dac27835e86fc396a04a1989c3
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-05-01 18:46:32 +0000

    874d332 docs: release notes for the v17.3.7 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.0+sha-7cea897`
+    version: `~18.1.0-next.0+sha-874d332`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
