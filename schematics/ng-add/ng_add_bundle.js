5f612fb778c1e29c384358f25edd9a2ec9cf1dc8
Andrew Kushnir <akushnir@google.com>
2024-05-01 18:58:29 +0000

    e58ff4a docs(core): Add event-dispatch README.md (#55521)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-rc.0+sha-92a1e59`
+    version: `~18.0.0-rc.0+sha-e58ff4a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
