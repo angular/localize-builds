cb00884041a888cd38bdbdfe2af74d6688da4bc6
Andrew Kushnir <akushnir@google.com>
2024-05-01 22:15:48 +0000

    3818436 fix(core): don't schedule timer triggers on the server (#55605)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.7+sha-4c50e1e`
+    version: `~17.3.7+sha-3818436`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
