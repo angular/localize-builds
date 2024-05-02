eae6b1f6ff497cf6e322d6f19d4dda4141faf0da
Andrew Kushnir <akushnir@google.com>
2024-05-02 21:51:33 +0000

    eb91efa refactor(devtools): set `key` in `manifest.chrome.json` (#55639)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-rc.0+sha-73de8b6`
+    version: `~18.0.0-rc.0+sha-eb91efa`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
