893fa0e5d0b611c4cdf8b8b6e19a61d5dc39a945
Andrew Kushnir <akushnir@google.com>
2024-05-02 20:43:35 +0000

    c21b459 fix(compiler): add math elements to schema (#55631) (#55645)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.7+sha-f978e9c`
+    version: `~17.3.7+sha-c21b459`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
