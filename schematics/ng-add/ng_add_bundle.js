e960c963c1720e9f276603128f0a0994dfc9fed6
Andrew Kushnir <akushnir@google.com>
2024-05-02 20:43:04 +0000

    73de8b6 release: bump Angular DevTools to 1.0.15 (#55643)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-rc.0+sha-662bca4`
+    version: `~18.0.0-rc.0+sha-73de8b6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
