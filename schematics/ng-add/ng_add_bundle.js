56643272ec831a40064091a34ee6d108b398563c
Andrew Kushnir <akushnir@google.com>
2024-05-02 18:12:58 +0000

    f978e9c build: update io_bazel_rules_sass digest to 9f8be59 (#55602)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.7+sha-760e284`
+    version: `~17.3.7+sha-f978e9c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
