a5d345ea5e0627faf99c102f839dce85407e9a5c
Andrew Kushnir <akushnir@google.com>
2024-05-03 15:20:49 +0000

    a733525 build: remove tslint and codelyzer deps from integration tests (#55658)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-rc.0+sha-eb91efa`
+    version: `~18.0.0-rc.0+sha-a733525`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
