c222aa3c73c3b926080cbc35536a4f2b74998d4f
Andrew Kushnir <akushnir@google.com>
2024-05-06 23:08:26 +0000

    77ae5a3 refactor(devtools): remove git sha stamping from devtools build (#55694)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.0+sha-8795374`
+    version: `~18.1.0-next.0+sha-77ae5a3`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
