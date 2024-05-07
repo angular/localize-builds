365b0edc425047fff60145cafa9f414e291e1b03
Andrew Kushnir <akushnir@google.com>
2024-05-07 15:13:09 +0000

    2646c40 fix(zone.js): remove `abort` listener on a signal when actual event is removed (#55339)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-rc.0+sha-a5f29f9`
+    version: `~18.0.0-rc.0+sha-2646c40`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
