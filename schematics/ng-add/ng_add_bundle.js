2bbb0f2f8178053849f4f048e4ab97d6daece5f4
Andrew Kushnir <akushnir@google.com>
2024-05-07 15:21:21 +0000

    67bb310 refactor(core): Fix timing of removal of jsaction attribute to be after event replay. (#55696)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.0+sha-a9460d0`
+    version: `~18.1.0-next.0+sha-67bb310`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
