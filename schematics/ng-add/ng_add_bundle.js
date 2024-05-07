18102145de37f24fb2e0c118e77d8ad3a3a15e25
Andrew Kushnir <akushnir@google.com>
2024-05-07 20:45:48 +0000

    1296393 build: trigger error when size-tracking is enabled but not in limit file (#55708)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-rc.0+sha-1b631ae`
+    version: `~18.0.0-rc.0+sha-1296393`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
