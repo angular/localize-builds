d579801ec3f1dc3a2297b1c83f08a017487ec43e
Andrew Kushnir <akushnir@google.com>
2024-05-07 22:02:21 +0000

    e701672 docs: uwu (#55666)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-rc.0+sha-1296393`
+    version: `~18.0.0-rc.0+sha-e701672`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
