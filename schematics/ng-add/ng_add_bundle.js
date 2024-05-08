a3a11f7a23cae09931d8894013f822d223813f52
Andrew Scott <atscott@google.com>
2024-05-08 22:51:57 +0000

    28a24b6 docs: fix visible code snippets in first-app tutorial (#55742)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-rc.1+sha-f9aac83`
+    version: `~18.0.0-rc.1+sha-28a24b6`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
