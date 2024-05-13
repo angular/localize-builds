ecd21f54e972eced5300727c5bd6a511223ce9ff
Andrew Scott <atscott@google.com>
2024-05-13 16:19:12 +0000

    7c9d37d refactor(core): Remove enums from event-dispatch. (#55421)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-rc.1+sha-897e8db`
+    version: `~18.0.0-rc.1+sha-7c9d37d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
