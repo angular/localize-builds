100ebd4a1998829fca910625d30ba753d77e046a
Andrew Scott <atscott@google.com>
2024-05-14 22:21:55 +0000

    4352598 refactor(core): Add an ActionResolver option to Dispatcher. (#55757)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-rc.1+sha-3b1b4e2`
+    version: `~18.0.0-rc.1+sha-4352598`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
