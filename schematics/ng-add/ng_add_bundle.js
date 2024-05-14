3d4742d9b1cc9a7a0df3e04c5ab3b193f08b5e73
Andrew Scott <atscott@google.com>
2024-05-14 18:53:35 +0000

    1fd63e9 refactor(core): deprecate `@Component.interpolation` (#55778)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-rc.1+sha-e205dcb`
+    version: `~18.0.0-rc.1+sha-1fd63e9`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
