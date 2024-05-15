ef2ece0cec49c080781c4c39564d7bad942698f9
Andrew Scott <atscott01@gmail.com>
2024-05-15 22:55:18 +0000

    e6d8f74 release: cut the v18.0.0-rc.2 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-rc.1+sha-e76bebd`
+    version: `~18.0.0-rc.2+sha-e6d8f74`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
