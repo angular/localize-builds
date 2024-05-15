e28503b3c57944e104a7c982d1e31cc944c14662
Andrew Scott <atscott01@gmail.com>
2024-05-15 20:47:18 +0000

    45badde release: cut the v17.3.9 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~17.3.8+sha-d0d8396`
+    version: `~17.3.9+sha-45badde`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
