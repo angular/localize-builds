a6758a7afe1c430ca001384354dccee5d83b28b0
Andrew Scott <atscott01@gmail.com>
2024-05-15 21:01:18 +0000

    7a172f7 docs: release notes for the v17.3.9 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.0+sha-84b2351`
+    version: `~18.1.0-next.0+sha-7a172f7`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
