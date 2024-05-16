2803eee1f1a1972f2f29e6ecc8466715a45489ed
Andrew Scott <atscott@google.com>
2024-05-16 16:20:43 +0000

    f3b0e2f refactor(zone.js): remove `tslib` from `dependencies` (#55827)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.0+sha-3d5c3d9`
+    version: `~18.1.0-next.0+sha-f3b0e2f`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
