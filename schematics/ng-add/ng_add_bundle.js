4ce646cb0f26f39c2b88e25ece68148dbdb7da1e
Andrew Scott <atscott@google.com>
2024-05-16 16:08:12 +0000

    69085ea fix(core): error about provideExperimentalCheckNoChangesForDebug uses wrong name (#55824)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -115,7 +115,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.0-rc.2+sha-e6d8f74`
+    version: `~18.0.0-rc.2+sha-69085ea`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
