2302a8ae3e0ed09cc93fbf23f2f14c1f2cc91aad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 20:18:16 +0000

    995b7fc build: update babel dependencies to v7.24.7 (#56373)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.2+sha-e49a81b`
+    version: `~18.0.2+sha-995b7fc`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
