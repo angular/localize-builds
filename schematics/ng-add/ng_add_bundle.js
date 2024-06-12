f22881f046a008f4e182367be748dfd58a94fb43
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-06-12 20:42:23 +0000

    f4b86bd docs: fix configure routes link (#56415)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.1+sha-5cc9e80`
+    version: `~18.1.0-next.1+sha-f4b86bd`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
