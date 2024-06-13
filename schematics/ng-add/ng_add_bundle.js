74444378b2c6d68568cab2f717526a8a3070731c
Andrew Kushnir <akushnir@google.com>
2024-06-13 16:29:31 +0000

    c698668 docs: language-service - Fix quick info URLs (#56416)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.2+sha-ef2ad1b`
+    version: `~18.1.0-next.2+sha-c698668`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
