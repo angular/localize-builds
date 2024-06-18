ccf909da08197a3e45fc8987819e8398cba74762
Andrew Kushnir <akushnir@google.com>
2024-06-18 16:37:04 +0000

    775f451 docs: add `ng generate directive` page (#56417)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.2+sha-67b2c33`
+    version: `~18.1.0-next.2+sha-775f451`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
