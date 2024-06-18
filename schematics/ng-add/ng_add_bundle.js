8ff31603ee662ae2d71f32c826efb6cd50be6915
Andrew Kushnir <akushnir@google.com>
2024-06-18 19:33:12 +0000

    cc6cd08 refactor(docs-infra): complete removal of aio directory (#56496)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.3+sha-2eaf04d`
+    version: `~18.0.3+sha-cc6cd08`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
