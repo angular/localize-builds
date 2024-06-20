bdcf9f600622020332060e1c95257de33f561cc0
Andrew Kushnir <akushnir@google.com>
2024-06-20 17:49:28 +0000

    5f8f18d refactor(docs-infra): use an older version of the `afterRender` API (#56526)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.3+sha-cc6cd08`
+    version: `~18.0.3+sha-5f8f18d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
