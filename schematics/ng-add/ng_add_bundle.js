3ecf4fe49ae65e516fc2306934843b70f5dccbb2
Andrew Kushnir <akushnir@google.com>
2024-06-20 16:05:42 +0000

    0d52c6b fix(router): Delay the view transition to ensure renders in microtasks complete (#56494)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.2+sha-0bd55a6`
+    version: `~18.1.0-next.2+sha-0d52c6b`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
