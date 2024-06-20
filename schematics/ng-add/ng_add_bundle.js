1c56786ec2aacee9f89f51515f099258e6657bff
Andrew Kushnir <akushnir@google.com>
2024-06-20 22:06:57 +0000

    be9e489 fix(core): improve support for i18n hydration of projected content (#56192)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.4+sha-2e58620`
+    version: `~18.0.4+sha-be9e489`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
