14557cca12fdf4ff25f1ea13cbaef88003b8bb21
Andrew Kushnir <akushnir@google.com>
2024-06-21 17:11:10 +0000

    49b2e65 docs: fix file structure link (#56542)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.3+sha-29ca6d1`
+    version: `~18.1.0-next.3+sha-49b2e65`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
