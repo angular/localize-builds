c810d2f889f881ccc22428a39f0fbce93ca69a77
Dylan Hunn <dylhunn@gmail.com>
2024-06-25 16:35:20 +0000

    ccc8c80 build: update to TypeScript 5.5 stable (#56358)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.3+sha-5be16d0`
+    version: `~18.1.0-next.3+sha-ccc8c80`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
