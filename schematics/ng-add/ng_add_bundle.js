eb618da63b69744865fcdec48d97d14572f4ff88
Dylan Hunn <dylhunn@gmail.com>
2024-06-26 16:01:48 +0000

    f49a2a4 docs: Move the background image migration to its own section. (#56588)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.4+sha-cfe1f38`
+    version: `~18.0.4+sha-f49a2a4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
