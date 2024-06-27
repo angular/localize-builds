a8c57e4da5b543c87a4287efeb16f8f301e6887c
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-06-27 20:01:31 +0000

    ed2665a docs: fix indentation on control flow documentation page (#56748)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.4+sha-040d861`
+    version: `~18.1.0-next.4+sha-ed2665a`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
