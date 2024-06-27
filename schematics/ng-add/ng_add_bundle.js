2c5b827e58a8129dafdf1963448bf5e1d1cf923e
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-06-27 18:35:09 +0000

    be6d9c5 refactor(core): Remove dom.ts and use native Element.prototype.contains. (#56591)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.5+sha-62970ca`
+    version: `~18.0.5+sha-be6d9c5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
