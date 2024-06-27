7d3666bef7e9845b53814d0a33bcfe5f65a11853
Dylan Hunn <dylhunn@gmail.com>
2024-06-27 00:11:38 +0000

    5dc0de1 release: cut the v18.1.0-next.4 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.3+sha-033dfc3`
+    version: `~18.1.0-next.4+sha-5dc0de1`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
