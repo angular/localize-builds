d452a92f3d6c5fb4422b2fd6fd01ad0e7927d582
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 14:45:48 +0000

    46b0292 refactor(core): export additional methods from primitives/signals (#56759)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.4+sha-2a1291e`
+    version: `~18.1.0-next.4+sha-46b0292`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
