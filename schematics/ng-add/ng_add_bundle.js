baf786de1187c8568f6e5c0b1dd8b7923b632fe8
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 17:37:38 +0000

    551027e refactor(core): Set the jsaction cache directly instead of using jsaction attribute. (#56747)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.4+sha-46b0292`
+    version: `~18.1.0-next.4+sha-551027e`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
