03b58d7b6512cf6f4c7a4401b99c8e19d3391f7b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-01 18:19:33 +0000

    f407e3c refactor(common): allow string urls in NgOptimizedImage input (#54901)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-next.4+sha-551027e`
+    version: `~18.1.0-next.4+sha-f407e3c`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
