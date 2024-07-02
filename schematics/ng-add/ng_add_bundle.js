4954cae446338720a55206ca43613ab5d21ba2b1
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 16:51:14 +0000

    0479c35 docs: Update interceptors.md to replace `public class` with `export class` in DI section (#56807)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.5+sha-99de3b7`
+    version: `~18.0.5+sha-0479c35`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
