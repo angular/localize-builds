248bb96f065a4031bcf2a2870b49666dcb50c4a7
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 17:16:26 +0000

    c4384bf fix(router): routes should not get stale providers (#56798)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-rc.0+sha-52852ab`
+    version: `~18.1.0-rc.0+sha-c4384bf`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
