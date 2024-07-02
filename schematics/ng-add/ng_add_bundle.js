487dbaf6e8bbef8ff1db2f5a5a927b39bf2912fd
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-02 14:22:42 +0000

    8de9aca refactor(common): remove remaining code for NgSwitch equality patch (#56806)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.5+sha-fdd8fb0`
+    version: `~18.0.5+sha-8de9aca`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
