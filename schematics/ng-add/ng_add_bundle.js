88124b259064ce2e26836c7f328184e7ca120e2e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-07-03 19:41:47 +0000

    4ceb853 release: cut the v18.0.6 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.5+sha-4909844`
+    version: `~18.0.6+sha-4ceb853`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
