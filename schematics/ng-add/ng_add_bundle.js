525145d6fe8a0144dce5374f37db131c893b9fa5
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-03 14:14:59 +0000

    4909844 fix(core): establish proper defer injector hierarchy for components attached to ApplicationRef (#56763)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.5+sha-19c70d8`
+    version: `~18.0.5+sha-4909844`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
