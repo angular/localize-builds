9292d8c3099368e82f7e4be35159f43e69407198
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 12:35:11 +0000

    97d718d docs: add window title to reference pages (#56840)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.2.0-next.0+sha-6e1f78f`
+    version: `~18.2.0-next.0+sha-97d718d`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
