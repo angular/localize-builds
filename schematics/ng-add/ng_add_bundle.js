1a254694c8b0451330ce8c1e1bf00186f023736d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 10:15:37 +0000

    ecc5644 perf(http): execute `fetch` outside of Angular zone (#56820)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.1.0-rc.0+sha-85da4b3`
+    version: `~18.1.0-rc.0+sha-ecc5644`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
