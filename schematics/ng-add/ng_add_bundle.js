10109f2d878ca359b49a02dcd3be0b082db7c0bf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-04 09:38:10 +0000

    66bf467 docs: update app-shell docs to reflect v17 (#56836)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.6+sha-b5500f8`
+    version: `~18.0.6+sha-66bf467`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
