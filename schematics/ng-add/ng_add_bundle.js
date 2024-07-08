abbd68606baaf75d0fbdd1638277672a87aff5d2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 16:09:39 +0000

    d5ce323 docs(docs-infra): add missing context with link (#56896)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.0+sha-f370f64`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.0+sha-d5ce323`);
 }
 function ng_add_default(options) {
   var _a;
