4a4652e378e85ccb5a3f3d43e30153dbe9864b20
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-08 14:33:00 +0000

    86478e4 refactor(compiler-cli): do not truncate/reduce types in API docs (#56572)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.6+sha-0d3ea33`
+    version: `~18.0.6+sha-86478e4`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
