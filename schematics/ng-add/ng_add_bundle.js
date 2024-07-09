90c238140347041177ba04c5e78ae88632d7190c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 11:19:58 +0000

    260531d refactor(compiler-cli):  escape missing decorators. (#56794)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.0+sha-fe52d8f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.0+sha-260531d`);
 }
 function ng_add_default(options) {
   var _a;
