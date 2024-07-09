0bb7f55d9922ffbe587fcfb567ef46193b1676eb
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 11:11:25 +0000

    ae9505b docs: update dependency-injection-context.md (#56736)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.0-rc.0+sha-0f4219c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.0-rc.0+sha-ae9505b`);
 }
 function ng_add_default(options) {
   var _a;
