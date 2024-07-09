af3fff6c9a0181500489cf01d6fccc6b3b8e2a83
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-09 07:53:01 +0000

    85f77b5 fix(compiler): fix CSS animation rule scope (#56800)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -150,7 +150,7 @@ function moveToDependencies(host, context) {
   (0, import_dependencies.addPackageJsonDependency)(host, {
     name: "@angular/localize",
     type: import_dependencies.NodeDependencyType.Default,
-    version: `~18.0.6+sha-fd6cf36`
+    version: `~18.0.6+sha-85f77b5`
   });
   context.addTask(new import_tasks.NodePackageInstallTask());
 }
