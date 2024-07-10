e478f9162a63f61f6d92485d39286dce09affeee
Andrew Scott <atscott@google.com>
2024-07-10 20:59:06 +0000

    d011c9f refactor(core): Refactor `EarlyEventContract` to prepare for using it as a container. (#56900)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.0+sha-8df13e1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.0+sha-d011c9f`);
 }
 function ng_add_default(options) {
   var _a;
