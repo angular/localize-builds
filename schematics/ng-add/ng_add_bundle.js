1186060847258729080c5adf5a97d3a716281eaf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 13:47:04 +0000

    b6fb5b7 release: cut the v18.2.0-next.0 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.0+sha-8fc2605`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.0+sha-b6fb5b7`);
 }
 function ng_add_default(options) {
   var _a;
