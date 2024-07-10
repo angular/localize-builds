53c2f302c44890da6605fae6c698d7ccf64ba120
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 15:33:53 +0000

    e076eb2 build: update actions/upload-artifact action to v4.3.4 (#56906)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.0+sha-4b82b98`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.0+sha-e076eb2`);
 }
 function ng_add_default(options) {
   var _a;
