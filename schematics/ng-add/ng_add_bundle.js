dbaf2cdf1a372e222b3b97ef1cf3b6836393b377
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-07-10 07:39:41 +0000

    aa8a479 docs: update version table for 18.1 (#56920)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.0+sha-9fd2ef9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.0+sha-aa8a479`);
 }
 function ng_add_default(options) {
   var _a;
