f5cc4220a10a4a184550fcfe877bc363ea447398
Andrew Scott <atscott@google.com>
2024-07-15 18:30:29 +0000

    c0855f0 build: update material / cdk (#56960)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.0+sha-634d55a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.0+sha-c0855f0`);
 }
 function ng_add_default(options) {
   var _a;
