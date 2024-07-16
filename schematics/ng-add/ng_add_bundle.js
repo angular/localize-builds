c22203141809874a11a14b6d1d1c51766a7117f0
Andrew Scott <atscott@google.com>
2024-07-16 16:59:35 +0000

    a1cb9df fix(common): Don't run preconnect assertion on the server. (#56213)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.0+sha-f02f351`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.0+sha-a1cb9df`);
 }
 function ng_add_default(options) {
   var _a;
