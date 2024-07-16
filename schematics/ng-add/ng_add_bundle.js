e3172e1712ad4b3a6364bb556e7a7af561cc6595
Andrew Scott <atscott@google.com>
2024-07-16 19:55:04 +0000

    4f11d6e fix(zone.js): support `Timeout.refresh` in Node.js (#56852)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.0+sha-a1cb9df`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.0+sha-4f11d6e`);
 }
 function ng_add_default(options) {
   var _a;
