3e5a248371f7cafb190ffb49ccca996f514f74b9
Andrew Scott <atscott01@gmail.com>
2024-07-17 20:11:03 +0000

    4bfe707 release: cut the v18.1.1 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.0+sha-4f11d6e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.1+sha-4bfe707`);
 }
 function ng_add_default(options) {
   var _a;
