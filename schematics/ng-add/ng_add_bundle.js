0b7a9d275b8278e5cabdf9199f95c75876e79673
Andrew Scott <atscott@google.com>
2024-07-18 15:47:48 +0000

    edbb403 docs(docs-infra): add open in StackBlitz button to code editor (#57018)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.1+sha-4bfe707`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.1+sha-edbb403`);
 }
 function ng_add_default(options) {
   var _a;
