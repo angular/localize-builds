b403a8a4a5fc11dfd52ad756436eab0a63aac401
Andrew Scott <atscott@google.com>
2024-07-19 18:58:29 +0000

    e904f34 fix(compiler-cli): add warning for unused let declarations (#57033)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.1+sha-04c504a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.1+sha-e904f34`);
 }
 function ng_add_default(options) {
   var _a;
