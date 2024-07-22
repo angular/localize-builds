df3978f4d472c9d2e24fb4b61a65b7e6cab3d4c4
Andrew Scott <atscott@google.com>
2024-07-22 22:34:46 +0000

    915b0e0 Revert "fix(compiler-cli): add warning for unused let declarations (#57033)" (#57088)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.1+sha-4639450`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.1+sha-915b0e0`);
 }
 function ng_add_default(options) {
   var _a;
