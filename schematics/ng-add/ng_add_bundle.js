c756d00d4203ef5fe674c65bb6765ee6a286e7fb
Andrew Scott <atscott@google.com>
2024-07-23 21:56:35 +0000

    889a02b test(docs-infra): update adev tests to be zoneless (#57100)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.1+sha-23fa43d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.1+sha-889a02b`);
 }
 function ng_add_default(options) {
   var _a;
