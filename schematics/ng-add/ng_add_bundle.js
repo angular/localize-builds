fc22b7cea03850df870f3450e70f4e9597ff0d43
Andrew Scott <atscott@google.com>
2024-07-24 17:33:34 +0000

    3d0ee73 docs(docs-infra): Use shiki for code highlighting (#57059)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.1+sha-8bb5937`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.1+sha-3d0ee73`);
 }
 function ng_add_default(options) {
   var _a;
