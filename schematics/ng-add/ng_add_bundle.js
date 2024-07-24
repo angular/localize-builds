22799308202e769ec6b47a91dbe21e52495df684
Andrew Scott <atscott@google.com>
2024-07-24 17:23:40 +0000

    d5fbe11 docs(docs-infra): disable experimental decorators (#56257)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.1+sha-0e439d7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.1+sha-d5fbe11`);
 }
 function ng_add_default(options) {
   var _a;
