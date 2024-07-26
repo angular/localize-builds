d481003b41d42dff49c09a782b7fb65fbcd567de
Dylan Hunn <dylhunn@gmail.com>
2024-07-26 01:48:45 +0000

    ca89ef9 fix(core): handle shorthand assignment in the inject migration (#57134)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.2+sha-6609a94`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.2+sha-ca89ef9`);
 }
 function ng_add_default(options) {
   var _a;
