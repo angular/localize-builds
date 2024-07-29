afc612bfa9f82b5489678c77ea069df1450cda55
Dylan Hunn <dylhunn@gmail.com>
2024-07-29 21:00:08 +0000

    9304c45 docs: fix invalid link formatting in sw config (#57170)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.2+sha-481092d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.2+sha-9304c45`);
 }
 function ng_add_default(options) {
   var _a;
