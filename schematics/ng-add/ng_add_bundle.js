8e27cb7eb261aefefef0136cabb50ebc54542395
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-30 17:01:50 +0000

    03553c4 docs: broken HammerLoader reference fix (#55836)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.2+sha-165973b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.2+sha-03553c4`);
 }
 function ng_add_default(options) {
   var _a;
