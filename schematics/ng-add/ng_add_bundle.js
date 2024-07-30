7bed3c077f16291738408721e9620958b8ebcb7c
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-30 18:11:39 +0000

    62ade76 refactor(core): Update callback schedulers to cancel pending timers (#57186)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.2+sha-0fbcd6e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.2+sha-62ade76`);
 }
 function ng_add_default(options) {
   var _a;
