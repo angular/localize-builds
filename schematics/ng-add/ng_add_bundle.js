6956f0915ded6fceded166b878dd8ed9065e42c6
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-30 15:57:32 +0000

    81b30dc refactor(docs-infra): migrate @angular/docs from dev-infra into adev directory (#57132)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.2+sha-9304c45`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.2+sha-81b30dc`);
 }
 function ng_add_default(options) {
   var _a;
