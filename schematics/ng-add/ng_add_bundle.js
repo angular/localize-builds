16b1b36aa4fcdb8dfa0053a8a71fced80d285336
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-07-31 14:06:54 +0000

    3afd7f0 build: update scorecard action dependencies (#57150)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.2+sha-147eee4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.2+sha-3afd7f0`);
 }
 function ng_add_default(options) {
   var _a;
