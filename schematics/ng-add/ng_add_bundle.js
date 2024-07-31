fc7d9510cf0cd3b8e937853211873ca86cab2cd4
Dylan Hunn <dylhunn@gmail.com>
2024-07-31 17:23:31 +0000

    be6c246 release: cut the v18.1.3 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.2+sha-69b5a31`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.3+sha-be6c246`);
 }
 function ng_add_default(options) {
   var _a;
