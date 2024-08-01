0e8a3129bca7e57c69735939a560c01c82157b54
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-01 19:22:53 +0000

    f125cd8 fix(docs-infra): replace the uses of replaceAll with replace using regex with g flag (#57232)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.3+sha-f5960d0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.3+sha-f125cd8`);
 }
 function ng_add_default(options) {
   var _a;
