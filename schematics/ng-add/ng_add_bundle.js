333f996b4936c8de0f44baa53954d33c86e45f23
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-01 19:40:11 +0000

    ad29621 Revert "fix(compiler): reduce chance of conflicts between generated factory and local variables (#57181)" (#57230)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.3+sha-e8794dd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.3+sha-ad29621`);
 }
 function ng_add_default(options) {
   var _a;
