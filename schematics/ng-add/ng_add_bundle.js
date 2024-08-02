5aeace4192deedacf436d1a45a7cd66df488ba5f
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-02 15:59:31 +0000

    827070e fix(core): Do not run image performance warning checks on server (#57234)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.3+sha-6c76c91`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.3+sha-827070e`);
 }
 function ng_add_default(options) {
   var _a;
