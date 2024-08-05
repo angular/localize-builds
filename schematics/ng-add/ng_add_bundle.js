3e9c2fcc51ad34a12ec5bc3c05cb468f1636c61c
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-05 14:53:25 +0000

    afb05ff fix(compiler-cli): support JIT transforms before other transforms modifying classes (#57262)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.3+sha-523f639`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.3+sha-afb05ff`);
 }
 function ng_add_default(options) {
   var _a;
