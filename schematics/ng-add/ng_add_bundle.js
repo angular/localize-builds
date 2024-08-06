cb33ac88172d4f9f1d86f2af2207da1b32e9534f
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-06 13:55:54 +0000

    f9a97c7 build: update actions/upload-artifact action to v4.3.5 (#57271)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.4+sha-9109f31`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.4+sha-f9a97c7`);
 }
 function ng_add_default(options) {
   var _a;
