9a3163dc2318c94269ab3e455efa2cb189741c76
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-07 16:53:30 +0000

    0b1dd39 perf(compiler-cli): improve performance of `interpolatedSignalNotInvoked` extended diagnostic (#57291)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.4+sha-7919982`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.4+sha-0b1dd39`);
 }
 function ng_add_default(options) {
   var _a;
