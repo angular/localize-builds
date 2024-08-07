7e46b8a37a285e5b1e46c692ce89d489db54ae09
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-08-07 19:32:12 +0000

    02d613e release: cut the v18.2.0-rc.0 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.4+sha-ccbce8f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-rc.0+sha-02d613e`);
 }
 function ng_add_default(options) {
   var _a;
