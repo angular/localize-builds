c5953f16769e5dae7ee0e33e32f01252c49e6b1f
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-08-07 18:18:41 +0000

    e1240c6 fix(zone.js): Add support for addition jest functions. (#57280)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.4+sha-d9d68e7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0-next.4+sha-e1240c6`);
 }
 function ng_add_default(options) {
   var _a;
