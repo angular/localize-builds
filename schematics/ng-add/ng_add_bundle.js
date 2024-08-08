9ae707aeb5b50f4ddeeda11d2845ca03007bdd74
Andrew Kushnir <akushnir@google.com>
2024-08-08 15:39:38 +0000

    e39b22a fix(core): Account for addEventListener to be passed a Window or Document. (#57282)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.4+sha-bdf8450`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.4+sha-e39b22a`);
 }
 function ng_add_default(options) {
   var _a;
