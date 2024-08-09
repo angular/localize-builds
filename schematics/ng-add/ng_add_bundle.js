08a22bca0bc56d3639b17738d7be814385ab5f70
Andrew Kushnir <akushnir@google.com>
2024-08-09 21:00:38 +0000

    37bf7de docs: reorder afterView and afterContent related lifecycle hooks (#57307)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-f271021`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-37bf7de`);
 }
 function ng_add_default(options) {
   var _a;
