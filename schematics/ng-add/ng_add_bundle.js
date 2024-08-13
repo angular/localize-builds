d278bfcf95c77632b2895f27f03543bda0889b4c
Andrew Kushnir <akushnir@google.com>
2024-08-13 19:17:04 +0000

    f99bb20 refactor(core): Add experimental support to have one event contract when there are multiple apps on the page. (#57355)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.1.4+sha-822db64`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.1.4+sha-f99bb20`);
 }
 function ng_add_default(options) {
   var _a;
