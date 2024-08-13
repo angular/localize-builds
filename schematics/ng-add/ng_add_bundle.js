1612dc35ec9476711d7dbffe8280de5991044bb2
Andrew Kushnir <akushnir@google.com>
2024-08-13 16:59:55 +0000

    e4a6198 fix(migrations): avoid duplicating comments when generating properties (#57367)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-84827d5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-e4a6198`);
 }
 function ng_add_default(options) {
   var _a;
