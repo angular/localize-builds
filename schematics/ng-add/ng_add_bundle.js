f93cb25e415898e0b55c15df9ac2791ca71aebab
Andrew Kushnir <akushnir@google.com>
2024-08-13 22:59:16 +0000

    9857570 docs(docs-infra): fix markdown link rendering (#57377)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-6882cc7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-9857570`);
 }
 function ng_add_default(options) {
   var _a;
