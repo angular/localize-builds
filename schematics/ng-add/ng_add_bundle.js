6ffcc01ee139af760c5133054b8af6d231320d8b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2024-08-14 16:48:43 +0000

    c1f1c02 docs: release notes for the v18.2.0 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-c5b598e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-c1f1c02`);
 }
 function ng_add_default(options) {
   var _a;
