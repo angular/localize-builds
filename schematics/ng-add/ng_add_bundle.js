ff51188cbe8a0c7bfc859107e07c3bdfa402214e
Dylan Hunn <dylhunn@gmail.com>
2024-08-15 15:26:51 +0000

    d4449fc fix(core): handle hydration of components that project content conditionally (#57383)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-751880a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-d4449fc`);
 }
 function ng_add_default(options) {
   var _a;
