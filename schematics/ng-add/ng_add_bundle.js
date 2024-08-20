9f22ad4871c723d421b0397069521e1301114d9f
Dylan Hunn <dylhunn@gmail.com>
2024-08-20 05:52:05 +0000

    b063468 feat(core): support TypeScript 5.6 (#57424)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-f47901a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-b063468`);
 }
 function ng_add_default(options) {
   var _a;
