829c80d0233a063d093a6d0860c95a1f76bc979f
Dylan Hunn <dylhunn@gmail.com>
2024-08-21 20:22:41 +0000

    5cc63c2 build: update zone.js peer dependency for core package to 0.15.0 (#57431)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-d1d4adc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.0+sha-5cc63c2`);
 }
 function ng_add_default(options) {
   var _a;
