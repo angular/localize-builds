4dd8b5edf82bf6c4ef183a19ef53c6c4932802a6
Dylan Hunn <dylhunn@gmail.com>
2024-08-21 18:32:40 +0000

    69b20d8 docs(docs-infra): fix regressions around members & deprecations (#57465)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0+sha-1a5f5ee`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0+sha-69b20d8`);
 }
 function ng_add_default(options) {
   var _a;
