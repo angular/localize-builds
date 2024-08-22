7927a6f7ccd2de2c9034a29ad0e5b8dc1677795d
Dylan Hunn <dylhunn@gmail.com>
2024-08-22 16:18:00 +0000

    09deb24 docs: fix indentation of file tree in app-shell (#57436)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.0+sha-69b20d8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.1+sha-09deb24`);
 }
 function ng_add_default(options) {
   var _a;
