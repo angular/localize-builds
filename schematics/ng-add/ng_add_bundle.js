ccaa55e3a705df789cc03d020880befb3d7d62e8
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-08-23 12:07:26 +0000

    4e803ae refactor(migrations): add support for simpler variant of tsurge migration (#57484)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.1+sha-ed920c3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.1+sha-4e803ae`);
 }
 function ng_add_default(options) {
   var _a;
