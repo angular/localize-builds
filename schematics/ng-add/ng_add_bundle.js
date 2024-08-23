a86347cacaa377420a7ad8b4c7f18bfb53c149c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-23 21:01:34 +0000

    0cebfd7 fix(elements): switch to `ComponentRef.setInput` & remove custom scheduler (#56728)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.1+sha-aacef07`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.1+sha-0cebfd7`);
 }
 function ng_add_default(options) {
   var _a;
