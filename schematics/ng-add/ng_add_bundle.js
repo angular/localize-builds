ad4be2d91af3ace2db84f41016ec45240dfdadc0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-26 16:10:46 +0000

    03ec620 fix(upgrade): Address Trusted Types violations in @angular/upgrade (#57454)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.1+sha-6d3a2af`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.1+sha-03ec620`);
 }
 function ng_add_default(options) {
   var _a;
