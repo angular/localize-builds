c96218390d1a29510f27a53b7d1d5eb690c4d8cd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-27 21:08:22 +0000

    76b9e2b Revert "refactor(migrations): initial migration logic for converting to signal queries (#57525)" (#57555)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.1+sha-21445a2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.1+sha-76b9e2b`);
 }
 function ng_add_default(options) {
   var _a;
