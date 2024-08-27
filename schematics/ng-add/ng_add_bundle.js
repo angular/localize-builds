7f009790df6d6ffcf89ad0fe5dd29e3e917e7cd1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-08-27 20:03:56 +0000

    564a8d5 docs(docs-infra): Add query as an url query param to the api reference (#57062)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.1+sha-969dadc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.1+sha-564a8d5`);
 }
 function ng_add_default(options) {
   var _a;
