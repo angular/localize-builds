540cf521bb3eabbf9ab2ff83a773930a9135ca2f
Andrew Kushnir <akushnir@google.com>
2024-08-29 14:59:16 +0000

    d9c1004 refactor(migrations): ensure control flow analysis of signal input migration works with all TS versions (#57566)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.2+sha-1853bbb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.2+sha-d9c1004`);
 }
 function ng_add_default(options) {
   var _a;
