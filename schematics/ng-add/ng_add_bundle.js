5a77b5bed53972176702fb595e0ec0e034ef9e6a
Andrew Kushnir <akushnir@google.com>
2024-08-29 22:25:14 +0000

    1d79cbd docs: update common-router-tasks to include sample dynamic fragment (#57590)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.2+sha-2ad9609`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.2+sha-1d79cbd`);
 }
 function ng_add_default(options) {
   var _a;
