e983ec0b90d15c696b91e3ededea3de7540eee78
Andrew Kushnir <akushnir@google.com>
2024-08-29 21:30:45 +0000

    5007970 docs: revert changes on API entries urls (#57589)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.2+sha-c068045`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.2+sha-5007970`);
 }
 function ng_add_default(options) {
   var _a;
