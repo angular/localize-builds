ae026f44ca90221b3b4bc632c600b3347b95eee6
Andrew Kushnir <akushnir@google.com>
2024-08-29 19:12:21 +0000

    0760771 ci: use version 13.15.1 of firebase-tools for deployment of doc sites (#57583)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.2+sha-93bdbbc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.2+sha-0760771`);
 }
 function ng_add_default(options) {
   var _a;
