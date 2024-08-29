35eed09b6397e98406031203b53e554045380693
Andrew Kushnir <akushnir@google.com>
2024-08-29 23:26:46 +0000

    001f319 Revert "refactor(compiler-cli): extract function overload separatly (#56489)" (#57594)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.2+sha-1d79cbd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.2+sha-001f319`);
 }
 function ng_add_default(options) {
   var _a;
