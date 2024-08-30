c989c767d55de718a07d679fa8665dac6448840e
Andrew Kushnir <akushnir@google.com>
2024-08-30 18:17:31 +0000

    3bafda2 docs(docs-infra): fix navigation between API pages (#57492)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.2+sha-5e42769`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.2+sha-3bafda2`);
 }
 function ng_add_default(options) {
   var _a;
