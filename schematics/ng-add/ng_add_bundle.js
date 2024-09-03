46e7ee905a98a39d1c7c001a174b3727cba2ed1a
Andrew Kushnir <akushnir@google.com>
2024-09-03 16:16:57 +0000

    de68e04 fix(http): Dynamicaly call the global fetch implementation (#57531)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.2+sha-2324d9b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.2+sha-de68e04`);
 }
 function ng_add_default(options) {
   var _a;
