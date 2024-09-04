c08d4c60839b3e204bddd42b8462c70e53e96017
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-04 21:05:19 +0000

    7a4199a refactor(devtools): use signal apis for directive forest (#56998)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.3+sha-f6c40f1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.3+sha-7a4199a`);
 }
 function ng_add_default(options) {
   var _a;
