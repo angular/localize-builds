d0c2c0055fc442ec963520b51e499dd09841395f
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-05 19:38:11 +0000

    227d13b refactor(migrations): preserve brace spacing when applying imports (#57672)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.3+sha-4fa25cf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.3+sha-227d13b`);
 }
 function ng_add_default(options) {
   var _a;
