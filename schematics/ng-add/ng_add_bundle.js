3fd454d8cfd9ac07d0c71f35bda98d632b86199a
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-05 16:11:05 +0000

    74d3fbf docs: clarify behavior of ng serve with server.ts (#57658)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.3+sha-004110c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.3+sha-74d3fbf`);
 }
 function ng_add_default(options) {
   var _a;
