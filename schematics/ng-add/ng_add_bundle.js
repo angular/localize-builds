70be1d2783c15fc218d48e78a707854de5831bc8
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-06 15:15:56 +0000

    6ea8e1e feat(core): Add a schematics to migrate to `standalone: false`. (#57643)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.3+sha-fc95a9a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.3+sha-6ea8e1e`);
 }
 function ng_add_default(options) {
   var _a;
