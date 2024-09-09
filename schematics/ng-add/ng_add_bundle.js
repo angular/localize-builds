415bab9789691e4ed1cfe57e93cdd45a9b8b5f15
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-09 14:46:05 +0000

    8d27839 docs: fix broken link in roadmap page (#57722)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.3+sha-b619d69`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.3+sha-8d27839`);
 }
 function ng_add_default(options) {
   var _a;
