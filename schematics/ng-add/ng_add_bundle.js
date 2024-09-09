f7940f57514a3dcb8df16b59da4f497a7d6bbc20
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-09 14:23:04 +0000

    b619d69 fix(compiler): produce less noisy errors when parsing control flow (#57711)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.3+sha-0843f31`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.3+sha-b619d69`);
 }
 function ng_add_default(options) {
   var _a;
