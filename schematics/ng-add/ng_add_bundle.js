29d74eb5ba114913ea42f1ee9f947f8e32820107
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-10 20:53:01 +0000

    0ebb2be build: bump dev-infra dependencies (#57761)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.4+sha-bc03f53`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.4+sha-0ebb2be`);
 }
 function ng_add_default(options) {
   var _a;
