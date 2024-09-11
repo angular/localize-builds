5b9760431b2d13853c1a433fdce9f4bc64ea8090
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-09-11 19:01:13 +0000

    ada67a3 docs: fix link of DEVELOPER.md in CONTRIBUTING.md (#57775)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.4+sha-73feb0e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.4+sha-ada67a3`);
 }
 function ng_add_default(options) {
   var _a;
