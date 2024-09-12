18918d2835a4a5c126f0ec2800e13dc4c929160f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-12 09:01:04 +0000

    fab6462 docs: add section on removing Zone.js imports/dependency from the build (#57784)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.4+sha-9bf60b0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.4+sha-fab6462`);
 }
 function ng_add_default(options) {
   var _a;
