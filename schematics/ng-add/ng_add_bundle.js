838f34b2ae1b37a07f712de5d487defe9c27c472
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-13 11:34:05 +0000

    1b0a6a0 docs: Update guide for version 18 - unclear (possibly incorrect?) instruction for opting out of caching HTTP requests that require authentication (#57799)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.5+sha-3ebe6b4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.5+sha-1b0a6a0`);
 }
 function ng_add_default(options) {
   var _a;
