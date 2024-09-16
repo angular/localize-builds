6900d17a7f7edb729401ea905a2e3a5c4aa8bfb0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-16 09:32:14 +0000

    e16b8a3 docs: correct key usage in sectionStyles and update template binding (#57820) PR Close #57820

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.4+sha-d5a27b8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.4+sha-e16b8a3`);
 }
 function ng_add_default(options) {
   var _a;
