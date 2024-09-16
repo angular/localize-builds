1bf47cfe97ebebaffb68c52ec5ec8b75b5e4badf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-16 16:13:52 +0000

    59fe9bc feat(migrations): introduce signal input migration as `ng generate` schematic (#57805)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.5+sha-8e01734`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.5+sha-59fe9bc`);
 }
 function ng_add_default(options) {
   var _a;
