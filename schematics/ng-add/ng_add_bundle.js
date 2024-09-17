d89ccc63a5de8b623a7e9cb88ce4f9616039d8d6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-17 13:53:46 +0000

    87e025a build: update github/codeql-action action to v3.26.7 (#57843)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.5+sha-607ea4b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.5+sha-87e025a`);
 }
 function ng_add_default(options) {
   var _a;
