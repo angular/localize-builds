afb62b8c53fa823f63982843838cc320bee738d1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-18 14:58:52 +0000

    029163c docs: adapt the NgModule to the new default : standalone (#57532)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.5+sha-f611faa`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.5+sha-029163c`);
 }
 function ng_add_default(options) {
   var _a;
