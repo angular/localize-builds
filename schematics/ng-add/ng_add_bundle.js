27f048e18016f59571b0fde029bfbe37b6995b1e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-18 15:43:32 +0000

    538dd29 release: cut the v19.0.0-next.6 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.5+sha-f8666a9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-538dd29`);
 }
 function ng_add_default(options) {
   var _a;
