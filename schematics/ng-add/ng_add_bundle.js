c8551862ba556940a3b3251a57e909b5065e5846
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-18 15:38:32 +0000

    f8666a9 docs: release notes for the v18.2.5 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.5+sha-029163c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.5+sha-f8666a9`);
 }
 function ng_add_default(options) {
   var _a;
