0cc71259974a2a1b2e96eecfd60f0309adb54b93
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-19 12:23:39 +0000

    9020a50 docs: Update Making HTTP requests best practices example (#57876)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-e598b08`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-9020a50`);
 }
 function ng_add_default(options) {
   var _a;
