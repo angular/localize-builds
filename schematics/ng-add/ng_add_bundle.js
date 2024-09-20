9f67f4f5b6ccc3536fabe4f2bf095b7a50d87f5a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-20 13:34:37 +0000

    3ac0a78 docs(http): heads up about provider ordering (#56518)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-ef5412c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-3ac0a78`);
 }
 function ng_add_default(options) {
   var _a;
