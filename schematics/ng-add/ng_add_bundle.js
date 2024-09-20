12993d9a571458b551756145fd8eb6f80fc128a3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-20 10:25:11 +0000

    88f2fe4 test(migrations): fix batch test for signal input migration (#57883)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-4112e6f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-88f2fe4`);
 }
 function ng_add_default(options) {
   var _a;
