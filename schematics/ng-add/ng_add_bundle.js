8169b4eff5c2d313684a460515b5e789d0c2fc50
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-23 14:30:27 +0000

    6c9abd3 refactor(forms): lift circular deps (#55881)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-9f4063b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-6c9abd3`);
 }
 function ng_add_default(options) {
   var _a;
