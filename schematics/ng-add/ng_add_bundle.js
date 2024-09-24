89a6a90593b1ae411208ae30ecb0cf9999c93604
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 12:52:31 +0000

    b035cae docs: use X link instead of twitter (#57608) PR Close #57608

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-b124e50`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-b035cae`);
 }
 function ng_add_default(options) {
   var _a;
