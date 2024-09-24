f82788ed9a3a14174db731e3405d9d537803bbc0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 13:07:07 +0000

    7afa978 docs: fix image src in feature modules (#57607)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-b035cae`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-7afa978`);
 }
 function ng_add_default(options) {
   var _a;
