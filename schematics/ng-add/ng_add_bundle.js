de1872d69e236194954a2fb883df2672cb5e7613
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-09-24 10:59:34 +0000

    97e2841 refactor(core): avoid producing zone-related warnings during hydration when in zoneless mode (#57911)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.5+sha-b7b0052`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.5+sha-97e2841`);
 }
 function ng_add_default(options) {
   var _a;
