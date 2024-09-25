0ba126cd4bff7507481e5840fe361b55a2b63c42
Andrew Scott <atscott01@gmail.com>
2024-09-25 17:20:06 +0000

    1549afe release: cut the v19.0.0-next.7 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.6+sha-95e8fb4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.7+sha-1549afe`);
 }
 function ng_add_default(options) {
   var _a;
