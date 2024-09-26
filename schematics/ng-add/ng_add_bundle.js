cdc43d4b914a6a60f3978c4e4d768ac468025aae
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-09-26 21:37:39 +0000

    0aae371 build: update @angular/build-tooling to latest version (#57979)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.7+sha-1549afe`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.7+sha-0aae371`);
 }
 function ng_add_default(options) {
   var _a;
