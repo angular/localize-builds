2b482206fd49e73df1edfcae66f8919ef74eebdf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-09-30 20:47:01 +0000

    b9d846d fix(migrations): delete constructor if it only has super call (#58013)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.6+sha-add5c25`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.6+sha-b9d846d`);
 }
 function ng_add_default(options) {
   var _a;
