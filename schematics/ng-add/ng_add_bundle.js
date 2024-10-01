392fe3e6b79b5155d7859aaf1fcba9055e012021
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 08:38:58 +0000

    8d8fb44 docs: update the name of the sub-navigation item at error encyclopedia menu (#57997)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.6+sha-6b67cb5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.6+sha-8d8fb44`);
 }
 function ng_add_default(options) {
   var _a;
