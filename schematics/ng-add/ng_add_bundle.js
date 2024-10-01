7f4331ae873a67d3dc7210eb12decf8d121b686b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 13:57:42 +0000

    a21867d fix(docs-infra): allow tutorial component to be GCed (#58028)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.6+sha-8d8fb44`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.6+sha-a21867d`);
 }
 function ng_add_default(options) {
   var _a;
