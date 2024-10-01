b30255622fb17914931be69159b75f519fbcddb3
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 07:13:39 +0000

    7bfe62a docs: add deferrable views tutorial (#57934)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.7+sha-f2bea3b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.7+sha-7bfe62a`);
 }
 function ng_add_default(options) {
   var _a;
