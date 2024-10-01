c0dbee93a30a74c89ad2657114d442e68c6c9df5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-01 08:31:59 +0000

    6b67cb5 build: update scorecard action dependencies (#58024)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.6+sha-0e62b30`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.6+sha-6b67cb5`);
 }
 function ng_add_default(options) {
   var _a;
