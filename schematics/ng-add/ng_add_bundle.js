387d01680069f6dc4d68039686f7f2451b58e375
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-02 12:38:39 +0000

    46b088a release: cut the v18.2.7 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.6+sha-46a2ad3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.7+sha-46b088a`);
 }
 function ng_add_default(options) {
   var _a;
