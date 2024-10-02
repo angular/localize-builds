c64ad6fc258465322f8b60449ea4ae2cbf686489
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-02 18:09:00 +0000

    dc911c6 refactor(docs-infra): cleanup `AfterRenderSequence` for reference list (#58030)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.7+sha-46b088a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.7+sha-dc911c6`);
 }
 function ng_add_default(options) {
   var _a;
