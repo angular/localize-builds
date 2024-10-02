7e58fe45bd5ee40cab86d9f6993106a239dedceb
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-02 15:15:48 +0000

    326cca2 refactor(migrations): eagerly migrate `Partial<T>` references in google3 (#58049)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-d48aac8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-326cca2`);
 }
 function ng_add_default(options) {
   var _a;
