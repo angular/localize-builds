70aa7eceab405bf7f37517fbabfbc9089b48adb9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-02 13:31:05 +0000

    d48aac8 release: cut the v19.0.0-next.8 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.7+sha-4461a58`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-d48aac8`);
 }
 function ng_add_default(options) {
   var _a;
