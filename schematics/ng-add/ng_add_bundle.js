2de9c1b4a2d87ca159d222d4119d1caa56cebca9
Andrew Kushnir <akushnir@google.com>
2024-10-04 18:06:22 +0000

    39a851f refactor: change security issue redirect to angular.dev (#58070)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.7+sha-de03e93`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.7+sha-39a851f`);
 }
 function ng_add_default(options) {
   var _a;
