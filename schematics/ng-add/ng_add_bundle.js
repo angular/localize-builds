259f16b810b1a22c8a725ddc090cb9ea409d81fe
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-04 13:39:59 +0000

    58bfb4a build: update ts target to ES2022 and module to ES2022 (#58022)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-9213216`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-58bfb4a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
