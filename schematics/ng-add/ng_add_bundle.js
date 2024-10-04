baefb3e6c59797d8908eefe9294ff4a1e9d0218e
Andrew Kushnir <akushnir@google.com>
2024-10-04 21:55:18 +0000

    10081a9 docs: move JSDoc before functions (#58087)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.7+sha-ccf2c7f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.7+sha-10081a9`);
 }
 function ng_add_default(options) {
   var _a;
