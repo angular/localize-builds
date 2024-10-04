d6bb4aacb5945d72e15513445c16db8fa8f9c0cc
Andrew Kushnir <akushnir@google.com>
2024-10-04 21:45:23 +0000

    ccf2c7f docs(common): Minor typo in code example (#58085)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.7+sha-39a851f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.7+sha-ccf2c7f`);
 }
 function ng_add_default(options) {
   var _a;
