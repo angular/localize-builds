ed09ec2514f3e3d06968d6d26c4a4e4d01e1e8da
Andrew Kushnir <akushnir@google.com>
2024-10-07 16:31:28 +0000

    837af44 refactor(core): introduce debugName optional arg to ReactiveNode (#57710)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-6976349`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-837af44`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
