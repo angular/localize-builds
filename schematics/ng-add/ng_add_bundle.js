f7c604f0ee2c2c19cb30976f94833e450dcbe89f
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-08 16:08:14 +0000

    09f589f fix(compiler): `this.a` should always refer to class property `a` (#55183)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-bc83fc1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-09f589f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
