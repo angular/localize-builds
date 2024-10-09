e67249bd4d0146fda2d2342fd1a86300451fafc9
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-09 13:57:23 +0000

    31fb9d0 docs: mention autoDetectChanges parameter default value (#58092)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.7+sha-59394ee`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.7+sha-31fb9d0`);
 }
 function ng_add_default(options) {
   var _a;
