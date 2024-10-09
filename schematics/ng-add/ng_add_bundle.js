dda9bd6175d4ad5097117590e5982be2fbcefb45
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-09 15:47:00 +0000

    2d11314 refactor(migrations): improve statistic tracking of signal input migration (#58137)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-e00775a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-2d11314`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
