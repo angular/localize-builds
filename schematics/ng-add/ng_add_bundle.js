1d4834135ad8a9d79bb4073f62f8855e81d61e5c
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-09 16:39:34 +0000

    95bee15 refactor(service-worker): remove backward compatibility code (#51246)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-2d11314`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-95bee15`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
