380b63a13961a596518ae9fe7e38c8ffab797afd
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-09 12:21:16 +0000

    9e82559 fix(platform-server): destroy `PlatformRef` when error happens during the `bootstrap()` phase (#58112)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-2213263`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-9e82559`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
