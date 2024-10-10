7277c6730f7e6918924e934f30d97155ddbeeadc
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-10 10:22:24 +0000

    6530c5e release: cut the v19.0.0-next.9 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-dff03d2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.9+sha-6530c5e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
