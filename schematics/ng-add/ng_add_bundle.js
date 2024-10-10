c3de934ae83be2ff4121926c31823e90712bb1a0
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-10 10:31:14 +0000

    da0e6b1 docs(core): fix description about signal value equality (#56472)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.8+sha-2532ffa`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.8+sha-da0e6b1`);
 }
 function ng_add_default(options) {
   var _a;
