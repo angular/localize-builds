88edc5fde97ddb67b4dcd6b463547fd7730b0c06
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-10 10:11:43 +0000

    dff03d2 docs: release notes for the v18.2.8 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-46a6324`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.8+sha-dff03d2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
