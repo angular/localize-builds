1d48333533caa71c98c96d3a3a934fe91411145d
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-11 14:16:00 +0000

    6cd2028 ci: disable bazel-saucelabs job (#58162)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.8+sha-d9bb74f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.8+sha-6cd2028`);
 }
 function ng_add_default(options) {
   var _a;
