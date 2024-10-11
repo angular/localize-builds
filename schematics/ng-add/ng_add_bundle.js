7ca4fdcae4d0793e23155276503e7c2c52b500a1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-11 07:10:57 +0000

    fc6c76a refactor(core): add internal utility to resolve the component name of a node (#58148)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.9+sha-08b4a8a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.9+sha-fc6c76a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
