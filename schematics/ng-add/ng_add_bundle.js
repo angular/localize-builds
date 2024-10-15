a4e780571aeef7a31acdf8f5e131e0864a444e3b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-15 12:57:57 +0000

    97c44a1 feat(core): Promote `takeUntilDestroyed` to stable. (#58200)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.9+sha-b4de7ac`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.9+sha-97c44a1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
