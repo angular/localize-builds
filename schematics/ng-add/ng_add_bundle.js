595f363631b6eb0b82371a77aad8ec34c55abbb5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-16 14:54:59 +0000

    532bd11 docs(language-service): fix link URLs (#58143)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.8+sha-608a151`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.8+sha-532bd11`);
 }
 function ng_add_default(options) {
   var _a;
