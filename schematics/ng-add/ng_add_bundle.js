ee9102b0c7a2d04a4dee7d5fe2191afd1ba46dd7
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-17 13:32:17 +0000

    141f310 refactor(core):  standalone by default for JIT compiled directives (#58236)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.10+sha-94bc69a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.10+sha-141f310`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
