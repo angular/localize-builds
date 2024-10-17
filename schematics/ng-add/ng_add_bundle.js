7253c5b28761eaa12c4220b37c61f38f37a5db01
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-17 18:15:58 +0000

    dd407a7 refactor(core): expose producerMarkClean utility (#58228)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.10+sha-141f310`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.10+sha-dd407a7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
