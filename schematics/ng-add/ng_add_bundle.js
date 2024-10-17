a903eeb999a55b99d25a756da6415cd1b5fd73a1
Andrew Kushnir <akushnir@google.com>
2024-10-17 23:04:30 +0000

    e603e56 test(core): fix flaky hydration test (#58252)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.10+sha-dd407a7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.10+sha-e603e56`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
