2ab25b1b33bc0bbe321043c5412dc18290baf21b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-17 11:45:55 +0000

    37b921d refactor(core): test EventEmitter completion on destroy with outputToObservable (#58239)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.8+sha-d330f94`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.8+sha-37b921d`);
 }
 function ng_add_default(options) {
   var _a;
