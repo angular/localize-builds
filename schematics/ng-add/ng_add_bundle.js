92e758642e6837ee14406bfde9f7b35c09251d8b
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-18 10:20:46 +0000

    d1cfbb1 docs: removed typos of withHttpTransferCacheOptions description (#58244)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.8+sha-b0ab653`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.8+sha-d1cfbb1`);
 }
 function ng_add_default(options) {
   var _a;
