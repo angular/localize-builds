e7cbf1c563c5da998d5c7dca1d1a23223b2f6bcd
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-18 08:18:23 +0000

    8311f00 feat(core): introduce the reactive linkedSignal (#58189)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.10+sha-e603e56`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.10+sha-8311f00`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
