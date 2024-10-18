ceda8976fac5bc410c4c615b9505965f4704e8fc
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-18 11:58:42 +0000

    dff4de0 feat(migrations): add a combined migration for all signals APIs (#58259)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.10+sha-b542f15`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.10+sha-dff4de0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
