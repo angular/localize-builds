99df0db1e7de9514ea6603e306b4c64ce8301fcc
Andrew Kushnir <akushnir@google.com>
2024-10-22 16:50:44 +0000

    1e9328e build: update actions/cache digest to 6849a64 (#58305)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.10+sha-8ddce80`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.10+sha-1e9328e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
