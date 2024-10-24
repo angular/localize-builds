9e9f49b886695bcef155be5f1723dc2878a4b665
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-24 23:30:58 +0000

    2f1f525 Update queries.md (#58330)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-8af71c0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-2f1f525`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
