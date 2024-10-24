7fa4cf1f4acd854529f28d8d9b0a1f1d621963d7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-24 19:50:56 +0000

    aadcfda refactor(language-service): support standalone by default (#58238)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.11+sha-f418b49`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.11+sha-aadcfda`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
