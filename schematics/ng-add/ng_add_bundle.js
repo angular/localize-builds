9a5f1089e80611c09a40fabafbc4b82ffe62465d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-24 23:48:45 +0000

    3b989ac fix(localize): Adding  arb format to the list of valid formats in the localization extractor cli (#58287)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.9+sha-e638b29`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.9+sha-3b989ac`);
 }
 function ng_add_default(options) {
   var _a;
