9698531116baf4eb08fa8a465c7eefbe73d73f18
Paul Gschwendtner <paulgschwendtner@gmail.com>
2024-10-25 18:53:35 +0000

    00b79f8 test: support parallel tsurge unit combining in batch test infra (#58280)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.11+sha-d7283bf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.11+sha-00b79f8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
