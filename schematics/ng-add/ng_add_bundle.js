0d9f59bb814ce4e76b4b953c3a1d609d4c29eb89
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-25 00:55:31 +0000

    2aa9f8b refactor(core): allow passing passive option to addEvent (#58316)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-7ed5e6c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-2aa9f8b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
