ac60aeb212ae72abee14e4acbd7ba386a92a04ee
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-29 12:18:54 +0000

    929db81 build: update scorecard action dependencies (#58400)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-7d9b38e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-929db81`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
