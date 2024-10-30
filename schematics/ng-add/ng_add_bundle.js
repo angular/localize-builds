105a7896f689095e0d88e527836fa68ad78d0ec9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-30 16:29:52 +0000

    d504452 fix(migrations): properly replace imports across files (#58414)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.11+sha-3d918de`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.11+sha-d504452`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
