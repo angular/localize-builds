7f21807d9f5b64334ec330b4e1b1cf0df1d9b73a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-30 17:19:42 +0000

    3230d78 refactor(core): introduce `ngServerMode` as global (#58386)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.11+sha-616b411`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-next.11+sha-3230d78`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
