f0ad114e637d99ee21ac52aaaf3feb62ed1ac6c2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-10-30 16:32:40 +0000

    69dce38 Revert fix(compiler): transform pseudo selectors correctly for the encapsulated view. (#58417)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.9+sha-1f13a5e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.9+sha-69dce38`);
 }
 function ng_add_default(options) {
   var _a;
