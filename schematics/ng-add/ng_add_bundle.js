c528b09f489c3de87e64b30a6e8f851562f8e327
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-10-31 10:34:16 +0000

    32a5388 refactor(core): More cleanup of incremental hydration code (#58430)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-94eae78`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-32a5388`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
