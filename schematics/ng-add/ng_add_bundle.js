f30aa1a96ff545040ad886325ea361b0e7cfecc8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-10-31 18:12:50 +0000

    127023f build: update `yarn` to `1.22.22` (#58448)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.10+sha-5828585`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.10+sha-127023f`);
 }
 function ng_add_default(options) {
   var _a;
