ea35359f6a9f6886721487a6c13f4fdc56156c5c
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-01 14:48:15 +0000

    9d9abf2 build: Update publish-build-artifacts to honor ORG (#58408)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.10+sha-a249d0b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.10+sha-9d9abf2`);
 }
 function ng_add_default(options) {
   var _a;
