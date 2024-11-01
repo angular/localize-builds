da2ef2dc77ba29112e6070a690c4214592b7e4f1
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-01 16:56:47 +0000

    5278f19 ci: update renovate `postUpgradeTasks` executionMode to `branch` (#58470)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.0+sha-efa7baa`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.0+sha-5278f19`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
