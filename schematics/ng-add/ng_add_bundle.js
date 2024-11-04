08661387278c2bcbb1eaa0564084508aab6b9f0e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-04 14:53:04 +0000

    36f4457 refactor(migrations): useFactory in provide-initializer migration (#58493)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.0+sha-61565ea`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.0+sha-36f4457`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
