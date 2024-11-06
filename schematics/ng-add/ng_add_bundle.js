716604c9f01f85cd13e899a70709eac42c11e718
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-06 15:36:31 +0000

    8919c80 refactor(migrations): speed up code refactoring action for queries (#58525)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-121b340`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-8919c80`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
