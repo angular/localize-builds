8672eba6c028cdc94e95b36741cae40a0b4c87b4
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-07 18:58:15 +0000

    ab701ad ci: add myself to the `renovate-changes` PullApprove group (#58537)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.1+sha-65a0a1d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.1+sha-ab701ad`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
