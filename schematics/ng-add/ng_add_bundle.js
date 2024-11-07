87e1b3ecbd18c63d6316989b1f5dbe754b4c59fc
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-07 21:36:45 +0000

    fda8f70 build: lock file maintenance (#58450)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.1+sha-3aa0263`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.1+sha-fda8f70`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
