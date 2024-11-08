2a3a638be74638e6e2aca34a24cd562a4f62cfe7
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-08 19:02:52 +0000

    9395aae docs: update hybrid rendering docs to remove `RenderMode.AppShell` (#58564)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.1+sha-e1c7327`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.1+sha-9395aae`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
