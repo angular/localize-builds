cf780d665c2747da35becb03c9be40e779f9d6a0
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-11 19:17:27 +0000

    42c9976 refactor(core): reorganize defer codebase (#58598)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-3b34073`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-42c9976`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
