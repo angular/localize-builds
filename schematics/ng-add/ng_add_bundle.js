76ee004ac25ff6f4df5385aa96e7ade4f4b8c045
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-12 18:45:50 +0000

    26602fc ci: remove circleci as we no longer rely on it (#58615)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.1+sha-560282a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.1+sha-26602fc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
