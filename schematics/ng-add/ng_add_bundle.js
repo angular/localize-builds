1611be4f62cc0f72c76196b4b9646eb5a18c9dce
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-13 20:10:18 +0000

    84c543a docs: update NgModule overview for standalone default (#58606)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-4f7ad3a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-84c543a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
