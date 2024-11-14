4b3d6e9807949ec52787ada4fbe865ab207bf961
Andrew Kushnir <akushnir@google.com>
2024-11-14 22:28:03 +0000

    91a616f refactor(core): add `REQUEST`, `RESPONSE_INIT` and `REQUEST_CONTEXT` tokens (#58669)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.2+sha-ee49947`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0-rc.2+sha-91a616f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
