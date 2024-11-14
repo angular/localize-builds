9533eff65066e71c799e9810c9385a80e8522037
Andrew Kushnir <akushnir@google.com>
2024-11-14 22:24:24 +0000

    6fef9b5 docs: fix typo in build system migration doc (#58668)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.12+sha-7672145`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.12+sha-6fef9b5`);
 }
 function ng_add_default(options) {
   var _a;
