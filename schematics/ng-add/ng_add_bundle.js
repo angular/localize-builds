a518e8218afd1ab1ab7b29075be8998ec36f6ba4
Andrew Kushnir <akushnir@google.com>
2024-11-18 20:55:44 +0000

    7d161b9 docs: add v19 developer event banner (#58721)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-92f30a7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-7d161b9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
