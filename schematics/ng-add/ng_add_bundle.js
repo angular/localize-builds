db3d4b3a65c21b7179062cf1902198bd307d0e1e
Andrew Kushnir <akushnir@google.com>
2024-11-19 20:25:49 +0000

    a983865 fix(language-service): add fix for individual unused imports (#58719)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0+sha-e4dc7f1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0+sha-a983865`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
