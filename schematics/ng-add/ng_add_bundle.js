83d0fb861b528f3eab904234d7e878f19c61157a
Andrew Kushnir <akushnir@google.com>
2024-11-19 18:01:54 +0000

    b69b16c docs: update standalone migration guide (#58703)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-284771d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-b69b16c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
