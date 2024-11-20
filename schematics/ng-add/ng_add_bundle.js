45b39b92687a97fb489b8dbccaa2b233db7d8132
Andrew Kushnir <akushnir@google.com>
2024-11-20 00:41:07 +0000

    f4e2777 docs: add details on how `@defer` loads deps (#58763)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0+sha-a983865`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0+sha-f4e2777`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
