2a4293ac75fd75d1d0de45be10fdf3bcea481781
Andrew Kushnir <akushnir@google.com>
2024-11-20 16:19:54 +0000

    6ad3059 docs(core): add descriptive usage line for projectableNodes param in createComponent docs (#58727)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-466d557`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-6ad3059`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
