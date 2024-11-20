daf7a1631a0c361abf91dfe9fb574bb93a9e1e8e
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-20 21:26:50 +0000

    28254ca build: update io_bazel_rules_sass digest to 35b1c32 (#58674)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0+sha-7c9f425`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0+sha-28254ca`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
