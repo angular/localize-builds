68a9489abb230a45362c0b61b21878fb9f5daccf
Joey Perrott <josephperrott@gmail.com>
2024-11-21 15:08:56 +0000

    d646d8b ci: update to latest actions from angular/dev-infra (#58799)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-7cb97fb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-d646d8b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
