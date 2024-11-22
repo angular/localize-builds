7d9a82a1234dc2b647d3170c3b957362ff8affbc
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-22 16:20:55 +0000

    671fdce docs: fix RxJS interop typo (#58823) PR Close #58823

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0+sha-6bee546`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0+sha-671fdce`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
