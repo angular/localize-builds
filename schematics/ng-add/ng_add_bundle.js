a26d5a58cde2dee97212d3acb3e1d12ce34f90fe
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-22 20:37:44 +0000

    1bdc3f1 docs(docs-infra): changed default type to object (#58289)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-834c7c3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-1bdc3f1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
