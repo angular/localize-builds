60bb95c64d3cb2fcda05169f63051055372af7f1
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-22 15:43:59 +0000

    843c34f build: update bazel setup (#58102)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-7fd034e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-843c34f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
