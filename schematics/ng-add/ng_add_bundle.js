c5ed7167e7a167f86c0406f89a91fd2e6adf0f6c
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-25 17:19:28 +0000

    83d2a09 build: pin google-github-actions/auth action to 6fc4af4 (#58879)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0+sha-03a14b5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0+sha-83d2a09`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
