f4cc769b11b178ed9ce0cca2f69c43754698b32b
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-26 14:56:43 +0000

    0c86d3e refactor(router): remove unused default error handler function (#58819)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0+sha-f0ecbe6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0+sha-0c86d3e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
