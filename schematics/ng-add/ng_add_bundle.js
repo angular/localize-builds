b9987af125bca22b9f61fbabfb8534843fe4609c
Jessica Janiuk <jessica.janiuk@gmail.com>
2024-11-26 18:18:57 +0000

    552092d refactor(core): remove private `whenStable` (#58891)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-18091e7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-552092d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
