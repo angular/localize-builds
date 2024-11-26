197b76fd1f85237ebae2a8ae67b130dfbb953bfb
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2024-11-26 17:47:05 +0000

    18091e7 release: cut the v19.0.1 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.0+sha-4c1abc4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-18091e7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
