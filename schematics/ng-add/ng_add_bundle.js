da3c0835afa56f807327268cd210b5febdac5c9e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-27 17:17:28 +0000

    3b76536 fix(core): Explicitly manage TracingSnapshot lifecycle and dispose of it once it's been used. (#58929)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-30891d8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-3b76536`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
