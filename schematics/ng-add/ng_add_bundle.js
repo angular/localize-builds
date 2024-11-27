0d0f8bd9ff007cee9d44e177c1e60af6e005f1e6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-27 16:05:41 +0000

    30891d8 refactor(core): Consolidates shouldTrigger* methods down to one (#58833)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-4a874bf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-30891d8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
