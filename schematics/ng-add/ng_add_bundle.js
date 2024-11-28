5a57d097ebfad7bb05a0daf3319519267a756e10
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-28 15:48:40 +0000

    66abff6 docs(docs-infra): don't use `URL` to check for external links (#58955)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-a4b86b2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-66abff6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
