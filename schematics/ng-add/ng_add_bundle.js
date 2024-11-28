b8b6b785932aa7b41f06b71bcb3298fbd4f7e73b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-28 16:46:48 +0000

    e76ddcc fix(docs-infra): do not focus h1 from docs-top-level-banner (#57698)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-66abff6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-e76ddcc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
