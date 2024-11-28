e5f9995f9a9385b552d4d580ee3982104e46a159
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-28 14:24:34 +0000

    e86b179 refactor(core): preventing resolving renderer factory every tick (#58618)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-293dce9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-e86b179`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
