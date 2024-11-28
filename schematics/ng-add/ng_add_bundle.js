193b676e0d4e475c4f3fbfa094158f6b64a21350
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-11-28 16:59:15 +0000

    357166e docs: fix `IMPORTANT` for "Animating the items of a reordering list" (#58938)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-02970f0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-357166e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
