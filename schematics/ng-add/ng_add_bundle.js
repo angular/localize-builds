be150d9bf0271b78e73cf0a9180361b0f57b3021
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-02 14:20:04 +0000

    f72a1b1 ci: mark devversion as unavailable (#59007)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-c453f5d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-f72a1b1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
