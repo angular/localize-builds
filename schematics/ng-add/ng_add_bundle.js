d613a59ee844e64afa0cb8233094641bc4da3854
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-02 15:22:39 +0000

    d0ea622 docs(docs-infra): read jsdoctags from function overloads (#58994)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-7c628f9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-d0ea622`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
