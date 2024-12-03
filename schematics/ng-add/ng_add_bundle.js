d5fab9e68b306a804f6e8660fbbf68df56d9fd24
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-03 17:38:46 +0000

    ad927bc ci: add kirjs to pullapprove groups (#59025)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-5cd35cb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-ad927bc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
