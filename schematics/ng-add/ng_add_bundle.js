495b28435d4373dd97a25a95eecd2b3ba86f6722
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-03 09:29:17 +0000

    55c3cd3 build: update io_bazel_rules_sass digest to fe48516 (#59014)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-42b1662`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.0+sha-55c3cd3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
