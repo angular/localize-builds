72e0849f2e91b418945d4524a9190eaba70dc6be
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 14:32:55 +0000

    7d2308f feat(docs-infra): move members descriptions section to the center of the page (#58610)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-0df7b1e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-7d2308f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
