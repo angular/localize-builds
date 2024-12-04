3738d39ae557ac1ead443c0b05a649653f4d1d42
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 10:10:07 +0000

    b8a2ae0 docs: fix missing alert block styles in the API reference (#59020)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-ad927bc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-b8a2ae0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
