591d2dccb18aa6d05d84facbefa7239fb4136313
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2024-12-04 11:48:50 +0000

    0df7b1e refactor(common): remove standalone: true (#58949)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-b8a2ae0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.1+sha-0df7b1e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
