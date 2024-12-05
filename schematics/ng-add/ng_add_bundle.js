5f83523ca2b3d530f7c0adcf051580e20ce0aa59
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-12-05 23:32:05 +0000

    e2c8c6f refactor(core): Remove event_contract_multi_container (#59059)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.3+sha-47e314f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.3+sha-e2c8c6f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
