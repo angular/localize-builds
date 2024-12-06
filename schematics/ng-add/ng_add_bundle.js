b2003fdcb9738a83a6a49f04f972179c24b61009
Alex Rickabaugh <alx+alxhub@alxandria.net>
2024-12-06 22:43:55 +0000

    46f00f9 build: update actions/cache digest to 1bd1e32 (#59077)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.2+sha-30e6760`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.2+sha-46f00f9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
