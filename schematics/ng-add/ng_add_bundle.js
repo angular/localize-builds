aa35a835b458ab6ab086a39515df748bdfca6b5a
Andrew Kushnir <akushnir@google.com>
2024-12-12 23:05:28 +0000

    8230388 build: simplify symbol tests format (#59147)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.2+sha-cfc1679`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.3+sha-8230388`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
