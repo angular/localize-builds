b0e17ccf966e16b610b86bccfe6502db6d1aa8dd
Andrew Kushnir <akushnir@google.com>
2024-12-12 23:06:09 +0000

    898e77e build: update babel dependencies (#59127)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-ec91da0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-898e77e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
