c1afb1a703baffe0bf0b552b378525b7d903de56
Andrew Kushnir <akushnir@google.com>
2024-12-13 00:26:14 +0000

    fa4419d docs(docs-infra): make font styles consistent in the API code ToC (#59072)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-898e77e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-fa4419d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
