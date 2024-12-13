bc93c8d24233a024e7ae493f3d2e0750bfd67fa7
Andrew Kushnir <akushnir@google.com>
2024-12-13 00:25:47 +0000

    9203276 docs: update i18n deployment docs to use subPath option (#59166)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.3+sha-8230388`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.3+sha-9203276`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
