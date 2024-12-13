c8582d1a2728764fe18cd9e281f9e522509cd07b
Andrew Kushnir <akushnir@google.com>
2024-12-13 01:34:25 +0000

    4c2cdd2 docs(core): remove redundant reference-manifests handwritten entries (#59169)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.3+sha-9203276`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.3+sha-4c2cdd2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
