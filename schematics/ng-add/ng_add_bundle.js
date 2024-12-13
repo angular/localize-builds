9778b1902a1c2cda1cfcc05497d6a5f5103ec7d7
Andrew Kushnir <akushnir@google.com>
2024-12-13 18:17:30 +0000

    3793218 fix(core): avoid triggering `on timer` and `on idle` on the server (#59177)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-c8b1926`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-3793218`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
