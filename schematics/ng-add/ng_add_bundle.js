73364c4794b8a15adcf5cee6ee52decf184273d7
Andrew Kushnir <akushnir@google.com>
2024-12-16 19:40:43 +0000

    6f66b04 docs: Fix Dependency Injection Readme guide link (#59193) PR Close #59193

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.3+sha-9e0b50b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.3+sha-6f66b04`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
