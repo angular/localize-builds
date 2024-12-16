843726bcc6be457270948eb0eb32bb911d95b4cf
Andrew Kushnir <akushnir@google.com>
2024-12-16 18:34:51 +0000

    9e0b50b refactor(core): add `ENABLE_ROOT_COMPONENT_BOOTSTRAP` token (#59133)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.3+sha-1f4ff2f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.3+sha-9e0b50b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
