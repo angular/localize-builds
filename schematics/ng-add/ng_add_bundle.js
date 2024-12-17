3d6bdcb22e2ee01cd279b71ca955d5d0ed9303f7
Andrew Kushnir <akushnir@google.com>
2024-12-17 21:01:19 +0000

    dd76f4e docs: Fix Code of Conduct Readme link (#59217) PR Close #59217

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-1b7b8ad`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-dd76f4e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
