48f0fe65744df41f0637d204ff56fac42b7ae35a
Andrew Kushnir <akushnir@google.com>
2024-12-17 17:45:48 +0000

    f1ab35c refactor(compiler-cli): fix instanceof for error not working (#59219)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-f0df010`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-f1ab35c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
