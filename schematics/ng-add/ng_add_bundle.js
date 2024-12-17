62aa50a78e49261f1d7bf5ec5d33fcb022db4d2c
Andrew Kushnir <akushnir@google.com>
2024-12-17 23:00:12 +0000

    fd968e8 refactor(core): reduce generated code (#59220)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-dd76f4e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-fd968e8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
