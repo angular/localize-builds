1143394504751e2c8a444bb9f1027958a91599c9
Andrew Kushnir <akushnir@google.com>
2024-12-17 00:38:56 +0000

    f0df010 docs(docs-infra): replace newline with <br> (#59156)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-b476537`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-f0df010`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
