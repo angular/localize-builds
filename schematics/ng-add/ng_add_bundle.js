bd2198ece81027911c210487d6adfb82d2039138
Andrew Kushnir <akushnir@google.com>
2024-12-18 17:44:32 +0000

    e0362b2 docs: fix typo in code example (#59235) PR Close #59235

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-fd968e8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.4+sha-e0362b2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
