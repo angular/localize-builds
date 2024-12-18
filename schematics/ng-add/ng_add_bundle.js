85f6c78a415f6d43107be512ab172327acbc2666
Andrew Kushnir <akushnir@google.com>
2024-12-18 21:46:18 +0000

    ae68df7 docs: fix dynamic template tutorial (#59234)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.5+sha-22aacba`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.5+sha-ae68df7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
