461519d35a44695df49d91b7f69f9095d1090ccc
Joey Perrott <josephperrott@gmail.com>
2024-12-19 21:43:52 +0000

    2c3630a ci: consolidate pullapprove groups (#59080)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.4+sha-b97bc5b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.4+sha-2c3630a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
