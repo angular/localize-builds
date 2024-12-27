f73f90ba17f98f53516a50190d4ffa1f1451b313
Joey Perrott <josephperrott@gmail.com>
2024-12-27 15:03:18 +0000

    b98416c fix(bazel): allow `ng_package` to work with `rules_js` dependencies (#59316)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.5+sha-dbb8980`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.5+sha-b98416c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
