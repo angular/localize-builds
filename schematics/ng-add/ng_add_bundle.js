41a731699cf70e67bbf85ebcf2ac6f099b4d9704
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-06 17:36:34 +0000

    cc7634f fix(bazel): handle module names and ES output in hybrid `ts_project` mode (#59325)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.4+sha-0bf6f76`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.4+sha-cc7634f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
