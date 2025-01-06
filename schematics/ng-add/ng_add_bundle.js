b8bc9e7dab9f5922c67234b9679b441b0f1125cc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-06 20:04:32 +0000

    6aeda99 docs: update copyright year (#59359)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.4+sha-5ac6f06`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.4+sha-6aeda99`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
