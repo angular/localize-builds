206e2edfd7bba01c354efea56610b7f028516fca
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-07 15:39:00 +0000

    5c010a5 ci: remove `devversion` from unavailable pullapprove list (#59391)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.5+sha-d3eff34`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.5+sha-5c010a5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
