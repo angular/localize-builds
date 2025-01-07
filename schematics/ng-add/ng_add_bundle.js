697d801ecfdff61240165c02c9ac0648e4aa5081
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-07 15:50:40 +0000

    adc56b7 build: update dependency @types/diff to v7 (#59403)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.4+sha-97039e8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.4+sha-adc56b7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
