b5cf771f27219a925d12f27b22c5d96fb7230bae
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-07 20:21:33 +0000

    7353054 refactor(core): drop `DEFER_BLOCK_DEPENDENCY_INTERCEPTOR` in production (#59381)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.5+sha-c82ad87`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.5+sha-7353054`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
