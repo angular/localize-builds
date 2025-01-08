f917ae84034fbc06b761b97da981bc3d4d91fd85
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-08 15:31:10 +0000

    731dc3a fix(bazel): support setting `type: module` in source `package.json` (#59406)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.5+sha-7353054`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.5+sha-731dc3a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
