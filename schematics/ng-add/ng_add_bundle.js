5cda028638c5ee7e695273f2b0d61d0d2a4ac629
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-08 16:03:09 +0000

    74ce629 refactor(platform-browser): improve error message for UNEXPECTED_SYNTHETIC_PROPERTY (#58983)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.5+sha-731dc3a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.5+sha-74ce629`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
