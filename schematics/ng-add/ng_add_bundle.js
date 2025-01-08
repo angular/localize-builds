f227f1ffd32d62b01b7617360153812c37a01821
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-01-08 16:17:50 +0000

    dd1d69b refactor(common): drop `PRELOADED_IMAGES` name in production (#59425)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.4+sha-00cc923`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-next.4+sha-dd1d69b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
