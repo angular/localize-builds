b5c19f16d55d44201d40f8e2a2fbcb656afb5210
kirjs <kirjs@users.noreply.github.com>
2025-01-09 14:27:07 +0000

    13a51b6 refactor(forms): drop `CALL_SET_DISABLED_STATE` name in production (#59430)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-rc.0+sha-77b24cd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-rc.0+sha-13a51b6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
