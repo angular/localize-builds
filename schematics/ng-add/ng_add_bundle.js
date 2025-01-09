88484bb61b06e3f72b79d204386758664cf88d8b
kirjs <kirjs@users.noreply.github.com>
2025-01-09 23:27:40 +0000

    aed49dd fix(compiler): use chunk origin in template HMR request URL (#59459)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-rc.0+sha-2ec7664`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-rc.0+sha-aed49dd`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
