24e9a75ef9538bff2b496d1ab544b1ee7a60fd2e
kirjs <kirjs@users.noreply.github.com>
2025-01-09 15:02:53 +0000

    28a5a89 build: clean up pipeline-specific tests (#59450)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.6+sha-21a9f53`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.6+sha-28a5a89`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
