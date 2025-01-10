2a3e887082d5961e54a358cadc82450ba2e324c6
kirjs <kirjs@users.noreply.github.com>
2025-01-10 16:58:45 +0000

    eb2fcd1 fix(migrations): incorrect stats when migrating queries with best effort mode (#59463)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.6+sha-534368c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.6+sha-eb2fcd1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
