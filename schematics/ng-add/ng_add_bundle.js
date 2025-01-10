4d9ff22f62600241ac5fd9cfafb74a665397da40
kirjs <kirjs@users.noreply.github.com>
2025-01-10 19:09:05 +0000

    c2b77b5 docs(docs-infra): prevent the host class from being replaced (#59460)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-rc.0+sha-f90dc4c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-rc.0+sha-c2b77b5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
