fa61be623c974d589d688101fa869bfaffa46e81
kirjs <kirjs@users.noreply.github.com>
2025-01-13 17:44:17 +0000

    5d63581 refactor(common): prevent duplicating `Accept` header (#59467)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-4491704`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-5d63581`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
