e2e942ab34e72d2f9428c29459901f11b54cc6f2
kirjs <kirjs@users.noreply.github.com>
2025-01-13 19:54:05 +0000

    065e118 fix(devtools): remove property tab css that is hiding directive metadata (#59493)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-368d69e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-065e118`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
