5ebe94a36a62e8f6b0c6d2a4016c9c09cd6b5924
kirjs <kirjs@users.noreply.github.com>
2025-01-13 16:04:19 +0000

    ff38671 refactor(compiler): fix typo in method name (#59479)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-rc.0+sha-8091a4a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-rc.0+sha-ff38671`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
