180824f7b9152adce4b9b2555e71b18353363119
kirjs <kirjs@users.noreply.github.com>
2025-01-13 15:44:02 +0000

    b745d8d docs: fix typo example code in pipes documentation (#59312)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-rc.0+sha-c2b77b5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0-rc.0+sha-b745d8d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
