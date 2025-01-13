81f8ceafc7f111432c61966a4c3412c8527ffc4f
kirjs <kirjs@users.noreply.github.com>
2025-01-13 15:58:06 +0000

    435d31e refactor(core): add profiler calls for tne newly introduced events (#59233)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-cb30fec`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-435d31e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
