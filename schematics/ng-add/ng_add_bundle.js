6210fc4ab37f2096ea2e36cb9d4e5cc16fd5f80e
kirjs <kirjs@users.noreply.github.com>
2025-01-14 20:04:18 +0000

    e44bf24 docs(docs-infra): fix scrolling issues in the API reference (#59207)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.6+sha-fb3aeba`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.6+sha-e44bf24`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
