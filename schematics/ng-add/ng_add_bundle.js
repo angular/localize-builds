7815a5c043d65f050db15cde26e19bc0bc43976b
kirjs <kirjs@users.noreply.github.com>
2025-01-14 16:20:18 +0000

    803358f docs(docs-infra): change API reference list items order to top-down (#58655)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-d038b3c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-803358f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
