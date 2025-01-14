7d4a6e95d365e06bf38151cb5db0e5bc202cc960
kirjs <kirjs@users.noreply.github.com>
2025-01-14 21:12:47 +0000

    8830537 build: update cross-repo angular dependencies (#59520)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-b9155b5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-8830537`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
