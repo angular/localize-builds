37017ab3dcac41754d428bc0f97a5c3e0a89713a
kirjs <kirjs@users.noreply.github.com>
2025-01-14 22:55:29 +0000

    990ce59 Revert "refactor: initialize headers map directly in HttpHeaders class (#59268)" (#59523)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.6+sha-601994d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.6+sha-990ce59`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
