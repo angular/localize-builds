61c96e7ffd70970cf46c8d65a94ca0d61edcd3d5
kirjs <kirjs@users.noreply.github.com>
2025-01-14 21:34:34 +0000

    601994d docs: fix property wrong usage inside afterNextRender in lifecycle.md (#59521)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.6+sha-5a2d0ed`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.6+sha-601994d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
