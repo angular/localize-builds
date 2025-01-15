132612a7ad0e835902a600495c966b5bb2d55bcb
kirjs <kirjs@users.noreply.github.com>
2025-01-15 17:36:43 +0000

    681f5e4 refactor(common): drop error message in production (#59471)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.0.6+sha-9bd5b4a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.0.6+sha-681f5e4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
