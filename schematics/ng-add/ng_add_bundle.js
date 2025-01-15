8d39435f9f254efcd39926dab01e17fba504c7bb
Andrew Kushnir <akushnir@google.com>
2025-01-15 20:28:33 +0000

    810ed6a refactor(common): prevent duplicating `Content-Type` header (#59518)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0+sha-e36e812`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0+sha-810ed6a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
