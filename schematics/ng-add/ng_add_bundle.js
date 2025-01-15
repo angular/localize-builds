ad789b54053a40e8bd5d1b02c065dc215721641d
kirjs <kirjs@users.noreply.github.com>
2025-01-15 17:06:24 +0000

    c38ea07 docs: clarify that `@for` doesn't support break/continue (#59533)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-3e31438`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-c38ea07`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
