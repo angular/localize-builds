fae7fc03e9af7074642fc1704765909911e30ea6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-16 11:33:29 +0000

    2fe54a1 fix(platform-browser): roll back HMR fix (#59557)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-db745cd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-2fe54a1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
