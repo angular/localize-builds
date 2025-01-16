8e22afde0df6a1df66f71b3a20d176841ac802e7
Andrew Kushnir <akushnir@google.com>
2025-01-16 16:16:23 +0000

    d12a186 fix(core): treat exceptions in `equal` as part of computation (#55818)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.1+sha-5c9d739`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.1+sha-d12a186`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
