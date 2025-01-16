46c430fa69c51958c1ddd0122e84fa0a3b3944be
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-16 11:43:00 +0000

    357795c fix(core): run HMR replacement in the zone (#59562)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0+sha-eb0b185`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.0+sha-357795c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
