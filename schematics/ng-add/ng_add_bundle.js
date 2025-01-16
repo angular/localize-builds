312247d2a68061f4144c5fcc937e68cfab885fd2
Andrew Kushnir <akushnir@google.com>
2025-01-16 00:53:18 +0000

    c9eaf53 release: bump Angular DevTools version to 1.0.21 (#59548)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-d0ad030`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-c9eaf53`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
