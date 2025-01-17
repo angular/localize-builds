8c93642535b121dabc1d4bfe31b9d7254c71e051
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-17 09:36:22 +0000

    8ea691a test(platform-server): fix flaky hydration test (#59579)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-f6e9776`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-8ea691a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
