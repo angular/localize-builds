161ac9d8e5489c7df2dff299c8de33fe6754cc47
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-20 08:17:05 +0000

    8dcd889 fix(compiler): update `@ng/component` URL to be relative (#59620)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.1+sha-95a05bb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.1+sha-8dcd889`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
