8f6a2b3da2d8c58a517d76c45d9ad320114555c6
Andrew Kushnir <akushnir@google.com>
2025-01-21 18:04:50 +0000

    26f6d4c fix(core): skip component ID collision warning during SSR (#59625)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.2+sha-67fe0b9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.2+sha-26f6d4c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
