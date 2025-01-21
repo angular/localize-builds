55be500f3354efc02ac03c000c554efa3036efbd
Andrew Kushnir <akushnir@google.com>
2025-01-21 21:26:30 +0000

    89b391c test(zone.js): reduce flakiness of a timer-related test (#59653)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.2+sha-0de5049`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.2+sha-89b391c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
