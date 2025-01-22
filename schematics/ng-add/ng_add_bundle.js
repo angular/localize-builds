c212a796f731c08b50e30a95db6a642a9a734ff9
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-01-22 19:55:32 +0000

    2ed633a release: cut the v19.2.0-next.0 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-61302cb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-2ed633a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
