c0099cc2ab035fb9eb2bb04748efc7c05c8b8335
Andrew Kushnir <akushnir@google.com>
2025-01-22 21:41:41 +0000

    1de822c refactor(devtools): select the root element by default (#59626)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-c243670`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-1de822c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
