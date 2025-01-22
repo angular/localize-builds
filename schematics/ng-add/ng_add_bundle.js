93a857a4c30a3c1b9c5b5f40f7a51c656ffd53d7
Andrew Kushnir <akushnir@google.com>
2025-01-22 16:04:14 +0000

    61302cb docs: add Bluesky links (#59661)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-6c96d79`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-61302cb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
