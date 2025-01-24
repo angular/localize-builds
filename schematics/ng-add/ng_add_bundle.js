324d7ff33a11d7a4c796562d5511eca5c9198221
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-24 08:34:13 +0000

    4867b64 test: update golden files (#59693)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-cdb439b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-4867b64`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
