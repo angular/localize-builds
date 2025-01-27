500c8f76eea627d672ea266771b2c11e8a49d349
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-27 15:47:54 +0000

    0a9c03c refactor(service-worker): drop platform checks (#59475)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-1c84cbc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-0a9c03c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
