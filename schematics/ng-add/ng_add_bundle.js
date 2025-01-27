3bd6b1b33549b216e3941974cb393e9b8e0c958b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-27 09:02:28 +0000

    170c052 docs: fix typo in signals-interop.md (#59724) PR Close #59724

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-7a328ba`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-170c052`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
