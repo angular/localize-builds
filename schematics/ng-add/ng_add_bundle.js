2ff166189eb5563688798ae7cdafb4e7b6da8321
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-27 15:56:29 +0000

    068cf29 refactor(core): remove unused LifecycleHooksFeature (#59678) (#59735)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-0a9c03c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-068cf29`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
