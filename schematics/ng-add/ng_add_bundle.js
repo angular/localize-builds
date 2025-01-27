5b8fc5b3ae11ac2018c82099457f3720e7ad35e8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-27 09:35:41 +0000

    1c84cbc fix(platform-browser): Update pseudoevent created by createMouseSpecialEvent to populate `_originalEvent` property (#59690)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-170c052`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-1c84cbc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
