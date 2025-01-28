3c7d824ebd5d217f94030b2b3b7c49c6b9e9d1b9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 08:46:31 +0000

    873870f refactor(common): drop error messages in production (#59545)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-d0dc8f7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-873870f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
