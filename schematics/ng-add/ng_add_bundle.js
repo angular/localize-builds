bd2c214de849a6848909785fc4f9696a7e7bf25d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 18:23:24 +0000

    ccebbba ci: prevent renovate from updating @bazel/ibazel (#59761)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-3c3620e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-ccebbba`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
