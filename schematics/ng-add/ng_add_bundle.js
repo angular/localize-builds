cdf047bb456c799212096c6482364d084ea0d5ad
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 12:15:22 +0000

    e2987a1 fix(service-worker): add missing `rxjs` peer dependency (#59747)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-a8139f7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.0+sha-e2987a1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
