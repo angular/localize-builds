daf1a39271d1e8d2d35b4ff6123b9c34571d9646
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-01-28 10:39:00 +0000

    cb1cb0c refactor(core): reuse host directive resolution logic (#59685)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-873870f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-cb1cb0c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
