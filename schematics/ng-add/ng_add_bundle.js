16fa8bb54b0fd8cc9688a09c0bfe39d6684a44fa
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-01-29 23:15:18 +0000

    5d89ffc release: cut the v19.1.4 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-67db7f7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.4+sha-5d89ffc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
