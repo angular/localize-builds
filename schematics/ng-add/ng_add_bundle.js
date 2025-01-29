8cf687e451273938fa005ccf68340022ee82cafb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-01-29 16:57:23 +0000

    4418064 fix(elements): not setting initial value on signal-based input (#59773)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-68dbaf5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.3+sha-4418064`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
