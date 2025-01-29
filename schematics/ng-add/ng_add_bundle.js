1b7546a08ba5a0650e9dd2db6a6e258299b9e6ef
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-01-29 12:36:18 +0000

    3cbc8a5 ci: update to latest version of dev-infra actions

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.13+sha-e1e419a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.13+sha-3cbc8a5`);
 }
 function ng_add_default(options) {
   var _a;
