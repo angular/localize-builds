03f6e6934d49ee45127c6a69b81724f4007eeb25
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-02-03 22:13:01 +0000

    146ab9a feat(core): support TypeScript 5.8 (#59830)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.1+sha-9723f03`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.1+sha-146ab9a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
