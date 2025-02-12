d2edf99fc11cbff6d0df53d43c84f91a51945641
Andrew Scott <atscott@google.com>
2025-02-12 17:14:03 +0000

    b0266bd fix(core): invalidate HMR component if replacement throws an error (#59854)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.2+sha-a7f20eb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.2+sha-b0266bd`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
