71859e127f4c12b443b29ab61d92b0ca1a439c85
Andrew Scott <atscott@google.com>
2025-02-12 18:06:21 +0000

    01f669a fix(compiler): handle tracking expressions requiring temporary variables (#58520)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.5+sha-b41a263`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.5+sha-01f669a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
