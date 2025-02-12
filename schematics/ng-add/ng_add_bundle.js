98f1baea3a0f4dfc54a163a77ec503c840286ce9
Andrew Scott <atscott@google.com>
2025-02-12 17:29:27 +0000

    28dd04a fix(devtools): regressions in component tree (#59873)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.2+sha-b0266bd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.2+sha-28dd04a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
