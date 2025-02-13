44946631c65b914d95f0039a93ce54e3eccfb656
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-02-13 16:36:19 +0000

    fb39fe9 release: cut the v19.2.0-next.3 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.2+sha-f34aeae`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.3+sha-fb39fe9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
