e0718d7952f9c5bd59c5ba90e87ec8cdabe3a6a9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-13 18:04:15 +0000

    2588985 feat(core): pass signal node to throwInvalidWriteToSignalErrorFn (#59600)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.3+sha-973033a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.3+sha-2588985`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
