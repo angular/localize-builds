602d897fd82a4dbe55890553f90c300eaa156435
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-14 19:38:25 +0000

    be32d63 docs: update `zip` command and link to recent DevTools commits (#59792)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.3+sha-751dc4a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.3+sha-be32d63`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
