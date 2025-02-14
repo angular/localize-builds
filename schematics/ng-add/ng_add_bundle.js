f3408db1e2b078b39436e9cac177be068a81a65d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-14 18:47:22 +0000

    3e39da5 feat(common): introduce experimental `httpResource` (#59876)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.3+sha-47d5e1e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.3+sha-3e39da5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
