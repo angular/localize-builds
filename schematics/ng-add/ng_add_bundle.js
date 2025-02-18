0f76dc8a6309c5ed057c3f675bed1b7341d8a038
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-18 17:40:43 +0000

    1cd3a7d feat(migrations): add migration to convert templates to use self-closing tags (#57342)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.3+sha-b6fa69f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.3+sha-1cd3a7d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
