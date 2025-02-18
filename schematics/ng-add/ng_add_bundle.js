02bed2e7ce9c0972c8f60c011e10858ed4c4ae24
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-18 16:57:12 +0000

    b6fa69f build: update dependency shiki to v3 (#59986)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.3+sha-c076b57`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.3+sha-b6fa69f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
