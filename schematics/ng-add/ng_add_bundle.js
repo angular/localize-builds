0198586fbed60faad6b275274ea69524eb763227
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-18 15:52:08 +0000

    6d8c2e2 refactor(core): move LContainer manipulation logic to its own file (#59989)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.6+sha-26c64a8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.6+sha-6d8c2e2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
