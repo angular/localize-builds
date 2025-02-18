fadc105d6b16e448909cee62f2d7e053bc623223
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-18 19:39:29 +0000

    ec1e4c3 fix(forms): Fix typing on `FormRecord`. (#59993)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.6+sha-6d8c2e2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.6+sha-ec1e4c3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
