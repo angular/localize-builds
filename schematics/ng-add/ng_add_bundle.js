507cd8f0de1d64874b019e0697df0b1c1670b30b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-02-19 19:11:11 +0000

    8c1b196 docs: release notes for the v19.2.0-rc.0 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-next.3+sha-497028c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.3.0-next.0+sha-8c1b196`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
