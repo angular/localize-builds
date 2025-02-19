e61698d0ed95905c0d4cf8a522b0144d851b516b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-02-19 17:09:33 +0000

    164facb docs(docs-infra): disable CodeEditor tests (#60015)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.6+sha-e16394a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.6+sha-164facb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
