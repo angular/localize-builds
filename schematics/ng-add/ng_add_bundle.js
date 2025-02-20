7ae6077416a4753e053f24523f78f13d73f4c928
kirjs <kirjs@users.noreply.github.com>
2025-02-20 17:02:53 +0000

    c513e5d refactor(devtools): fix component inspector highlighting (#59995)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.3.0-next.0+sha-7a8e8f3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.3.0-next.0+sha-c513e5d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
