12da5ecca6a7685c8533208c54860c2f3cac560f
kirjs <kirjs@users.noreply.github.com>
2025-02-20 16:11:59 +0000

    da25071 docs: Fix typo ')' in banner-initial.component.spec.ts (#60032)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.7+sha-c2102c5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.7+sha-da25071`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
