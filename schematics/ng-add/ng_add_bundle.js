46f23f730ce395bfab2a9d54ca7962117dabfe31
kirjs <kirjs@users.noreply.github.com>
2025-02-21 16:23:18 +0000

    3fd6551 docs: remove todo from jsdoc (#60042)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-rc.0+sha-2174084`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-rc.0+sha-3fd6551`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
