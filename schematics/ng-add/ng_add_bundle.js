ec961785cb8c55a411003bcd26f6317f05f01d51
kirjs <kirjs@users.noreply.github.com>
2025-02-24 17:03:41 +0000

    c611c8d fix(core): capture stack for HMR errors (#60067)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.7+sha-3b0b493`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.7+sha-c611c8d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
