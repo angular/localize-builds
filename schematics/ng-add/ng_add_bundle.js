e082900bcc892e80ff101b98d1d14f2a9a0c86cc
kirjs <kirjs@users.noreply.github.com>
2025-02-25 19:19:27 +0000

    c4126c7 refactor(forms): tree-shake `_checkParentType` in production (#60041)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-rc.0+sha-7d66360`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-rc.0+sha-c4126c7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
