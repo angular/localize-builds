fb40a8a66a62c3b8659dfc113e2dc44d604415d1
kirjs <kirjs@users.noreply.github.com>
2025-02-25 18:07:54 +0000

    7d66360 fix(common): make types for HttpClient more readable (#59901)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-rc.0+sha-ce3a9a2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-rc.0+sha-7d66360`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
