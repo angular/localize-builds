974ab6d337f4cbccc244be037388c900797f2708
kirjs <kirjs@users.noreply.github.com>
2025-02-25 16:17:42 +0000

    69b6d65 refactor(core): remove performance mark feature for signals (#59991)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.7+sha-94117f1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.7+sha-69b6d65`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
