d378b8a8ffa32f26e24616321f116aa116d3e6cc
Kirill Cherkashin <kirill@kcherkashin.org>
2025-02-26 19:18:32 +0000

    a8a3534 release: cut the v19.1.8 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.7+sha-41ef1cb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.8+sha-a8a3534`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
