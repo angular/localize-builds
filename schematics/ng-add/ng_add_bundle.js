d4b473e413f2b903b066fc0b733697f31a2afe55
kirjs <kirjs@users.noreply.github.com>
2025-02-26 16:24:20 +0000

    f76fd48 refactor(core): tree-shake `REF_EXTRACTOR_REGEXP` (#60081)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-rc.0+sha-5de4b0c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0-rc.0+sha-f76fd48`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
