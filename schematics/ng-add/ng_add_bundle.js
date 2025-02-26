669ab8d2c154a198ea3c4fe41b35a39a5cd30986
kirjs <kirjs@users.noreply.github.com>
2025-02-26 16:08:40 +0000

    d85ebb4 docs: move release date for v20 by one week (#60111)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.0+sha-db2f2d9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.0+sha-d85ebb4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
