9e818841ed4db52f3b0d953bf98ab421aa2199df
kirjs <kirjs@users.noreply.github.com>
2025-02-26 17:08:54 +0000

    41ef1cb docs(docs-infra): remove explicit `dectectChanges()` (#60098)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.1.7+sha-567d48f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.1.7+sha-41ef1cb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
