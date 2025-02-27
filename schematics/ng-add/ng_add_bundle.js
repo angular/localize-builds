037dcc3653d55b91b4b23fd3558f47ea147f444a
Miles Malerba <mmalerba@google.com>
2025-02-27 18:31:48 +0000

    b09f4a5 docs(docs-infra): add a tooltip to show the full text when nav item text is lengthy (#59038)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-e549bde`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-b09f4a5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
