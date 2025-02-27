ebff69e309d4b79717e017ae4713b515cc9b2c12
Miles Malerba <mmalerba@google.com>
2025-02-27 17:59:51 +0000

    e549bde refactor(animations): tree-shake regular expressions (#60095)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-16ca676`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-e549bde`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
