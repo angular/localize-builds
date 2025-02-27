d8913f62ccf9bbea59916b10df13779713e5d7e4
Miles Malerba <mmalerba@google.com>
2025-02-27 00:33:21 +0000

    4078895 docs: fix mermaid event node text visibility in light mode (#60124) PR Close #60124

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.0+sha-ab2ee22`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.0+sha-4078895`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
