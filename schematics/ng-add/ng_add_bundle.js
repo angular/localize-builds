39d93d09b92ae3b91f4758394dbbb03ec56fec15
Miles Malerba <mmalerba@google.com>
2025-02-28 16:38:46 +0000

    1b3b05b fix(core): cache ComponentRef inputs and outputs (#60156)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-b09f4a5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-1b3b05b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
