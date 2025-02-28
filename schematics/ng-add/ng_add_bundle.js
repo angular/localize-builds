509135b458a22688a53ca21d379886aa036aaf34
Miles Malerba <mmalerba@google.com>
2025-02-28 18:54:32 +0000

    b81ea4f refactor(core): Move getCurrentInjector/setCurrentInjector to primitives package. (#60090)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-1b3b05b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-b81ea4f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
