767474ec6422aa9993b0651763a6df24375e5673
Miles Malerba <mmalerba@google.com>
2025-02-28 20:00:24 +0000

    1dd9447 fix(compiler-cli): ensure template IDs are not reused if a source file changes (#60152)

--- fesm2022/localize.mjs
+++ fesm2022/localize.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v19.2.0+sha-b81ea4f
+ * @license Angular v19.2.0+sha-1dd9447
  * (c) 2010-2024 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -694,7 +694,7 @@ function describeMessage(message) {
  * These messages are processed and added to a lookup based on their `MessageId`.
  *
  * @see {@link clearTranslations} for removing translations loaded using this function.
- * @see {@link $localize} for tagging messages as needing to be translated.
+ * @see [$localize](api/localize/init/$localize) for tagging messages as needing to be translated.
  * @publicApi
  */
 function loadTranslations(translations) {
@@ -715,7 +715,7 @@ function loadTranslations(translations) {
  * All translations that had been loading into memory using `loadTranslations()` will be removed.
  *
  * @see {@link loadTranslations} for loading translations at runtime.
- * @see {@link $localize} for tagging messages as needing to be translated.
+ * @see [$localize](api/localize/init/$localize) for tagging messages as needing to be translated.
  *
  * @publicApi
  */
