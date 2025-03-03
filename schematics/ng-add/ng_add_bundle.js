f73ab93423e545d17ba0e9b706465811c2c5e72e
Miles Malerba <mmalerba@google.com>
2025-03-03 22:27:06 +0000

    a58b038 docs: change broke link (#60189)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.0+sha-809b5b4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.0+sha-a58b038`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
