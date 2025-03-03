7b90afad301a49d46fe5de212bcd04d5e1a3c2aa
Miles Malerba <mmalerba@google.com>
2025-03-03 18:47:34 +0000

    2717416 refactor(core): Update push/replace navigation to not trigger popstate (#60028)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-c2de5f6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-2717416`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
