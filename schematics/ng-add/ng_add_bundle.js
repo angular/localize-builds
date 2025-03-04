627569cd9aa1b9e5f0c735edb1350c4e72782c16
Miles Malerba <mmalerba@google.com>
2025-03-04 20:04:22 +0000

    26b3a8f refactor(http): add `context` property to `httpResource` (#60188)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-8579ff8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-26b3a8f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
