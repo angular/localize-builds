ef86cb1a00521f06f91f885b7ce990857530b148
Miles Malerba <mmalerba@google.com>
2025-03-04 19:44:33 +0000

    6f6d9f1 docs: Remove misleading default value DEFAULT_CURRENCY_CODE (#59977)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.0+sha-bec6e56`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.0+sha-6f6d9f1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
