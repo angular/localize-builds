bc9a7d202aede672249f07600c7f4c410a42c288
Miles Malerba <mmalerba@google.com>
2025-03-04 22:27:30 +0000

    a64a575 refactor(docs-infra): AppComponent cleanups (#58897)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-fbbe5d5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-a64a575`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
