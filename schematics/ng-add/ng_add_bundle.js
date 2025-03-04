069e9ab0bae786cab1a96644c035a81630f2bc3b
Miles Malerba <mmalerba@google.com>
2025-03-04 17:20:29 +0000

    c615bdc docs(docs-infra): Improve support for @link within the jsdoc. (#60201)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-cd31bfc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.0+sha-c615bdc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
