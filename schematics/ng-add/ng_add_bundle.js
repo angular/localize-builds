6bc247244eaf24ad0a2fa513dfa58ed70e3f0b61
Andrew Kushnir <akushnir@google.com>
2025-03-05 21:54:15 +0000

    8d770ec refactor(core): Remove the optional flag from Injector and all casts. (#60154)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-699733f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-8d770ec`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
