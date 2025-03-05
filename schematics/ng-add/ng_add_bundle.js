8f358cd424e04f4282140d227ce8fc95478745ec
Andrew Kushnir <akushnir@google.com>
2025-03-05 23:40:24 +0000

    7bcdf7c fix(router): update symbols (#60233)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-484e0cf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-7bcdf7c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
