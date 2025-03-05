e40a84f68ba5853fb074f4e9beaae8eef96aa2b6
Andrew Kushnir <akushnir@google.com>
2025-03-05 22:55:45 +0000

    a8ec7be refactor(platform-browser): drop unsupported event target error message in production (#60016)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.1+sha-ac94604`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.1+sha-a8ec7be`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
