3b5c584a3392c08aad97786a1c27873dd8db11a9
Andrew Kushnir <akushnir@google.com>
2025-03-06 16:08:53 +0000

    2668bd9 refactor(http): Make sur to pass `context` & `transferCache` from `httpResource` to the underlying request. (#60228)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.1+sha-cae1fe5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.1+sha-2668bd9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
