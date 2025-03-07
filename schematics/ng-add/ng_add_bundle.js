ac70ee75643a65fef4c33ae34de44e7dea991e96
Andrew Kushnir <akushnir@google.com>
2025-03-07 19:09:45 +0000

    bde1b37 build: properly compile tests in `core` with Angular compiler (#60268)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-70fb3bb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-bde1b37`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
