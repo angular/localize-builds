1d436cda5b9471f957412e778760028d1a4c144f
Andrew Kushnir <akushnir@google.com>
2025-03-10 15:23:53 +0000

    338818c fix(core): Surface errors from `ApplicationRef.tick` to callsite (#60102)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.1+sha-a02e270`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.1+sha-338818c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
