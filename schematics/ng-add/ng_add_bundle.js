ffc45bc2de7d16c1c130a31ddcd954bf7891044b
Andrew Kushnir <akushnir@google.com>
2025-03-10 16:53:32 +0000

    3089ab4 build: update minimum development Node.js versions (#60080)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.1+sha-07dc806`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.1+sha-3089ab4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
