79ec77b70391e8a39827d5de2d15d67f7fc1b2c3
Andrew Kushnir <akushnir@google.com>
2025-03-10 23:17:33 +0000

    30f6db3 refactor(core): log a warning instead of throwing error on `OutputRef.emit` when the directive is destroyed. (#60293)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-4e87b58`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-30f6db3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
