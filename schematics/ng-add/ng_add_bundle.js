49c6315bc462969b1a75700bebd42926689afc91
Andrew Kushnir <akushnir@google.com>
2025-03-11 20:11:56 +0000

    b6f22a7 refactor(core): inline standalone check to be dropped (#60305)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-f9b6bf9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-b6f22a7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
