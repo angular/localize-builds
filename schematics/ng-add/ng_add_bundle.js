2d68dcf67bc443f65e5252806584eaff26ea84fa
Andrew Kushnir <akushnir@google.com>
2025-03-11 18:16:58 +0000

    f9b6bf9 docs: fix commit message guideline link (#60296)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-e59432c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-f9b6bf9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
