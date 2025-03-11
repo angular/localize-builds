92356275fba7a44b79dc7d7f8b28a43d3be2802e
Andrew Kushnir <akushnir@google.com>
2025-03-11 21:33:41 +0000

    705fb0d docs: add mention of GH issue for devPreview feedback (#60010)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-b6f22a7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.1+sha-705fb0d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
