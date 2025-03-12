c1a765d6e52694fccea0300aa049cb12eb55df21
Andrew Kushnir <akushnir@google.com>
2025-03-12 00:19:25 +0000

    0b6084e refactor(router): split remainder of describes in integration test file (#60313)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.1+sha-0a9403e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.1+sha-0b6084e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
