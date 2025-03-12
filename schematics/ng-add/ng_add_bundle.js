ca53c4b0581ed7dd1738c3fe5d0e0a64ca4e3758
Andrew Kushnir <akushnir@google.com>
2025-03-12 17:45:33 +0000

    817021b refactor(core): add equal option to the signal and computed creation (#60300)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-bc472b6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-817021b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
