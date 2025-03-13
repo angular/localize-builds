b2244a722886f990c1db3b6c5b6105cf7bd6a8a0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-13 16:11:02 +0000

    55ba1ed docs: added short explanation about event reply (#60349)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-7a39585`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-55ba1ed`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
