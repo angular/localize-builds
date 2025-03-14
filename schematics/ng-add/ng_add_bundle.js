f227440039653ae03fbaf67886154cb012dc0daa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-14 18:57:52 +0000

    eb867c3 build: upload grpc remote logs for debugging (#60393)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-b8daf47`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-eb867c3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
