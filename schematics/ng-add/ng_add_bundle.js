5b8f8892c814ca94833de8f2f009363ed0362365
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-14 13:51:02 +0000

    c147a0d refactor(migrations): simplify integration of tsurge migrations into the CLI (#60386)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-2845906`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-c147a0d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
