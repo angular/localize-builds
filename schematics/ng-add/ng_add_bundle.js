a7999970239f39e4ca0cf4a5796f2b254acfba7e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-14 12:50:20 +0000

    24c5398 refactor(core): Support and document manually provided internal injections (#60347)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.2+sha-f030409`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.2+sha-24c5398`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
