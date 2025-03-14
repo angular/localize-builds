d0912cebc24e7fc366e6ac23f31958c71d578e26
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-14 10:24:15 +0000

    f030409 refactor(router): Clean up the transition subject (#60357)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.2+sha-8551cab`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.2+sha-f030409`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
