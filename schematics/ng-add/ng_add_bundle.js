d85ffa9e53a577b5f63cc0ac026750debc51fbf0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-14 07:44:51 +0000

    8551cab fix(devtools): eliminate unnecessary scrollbars on injector tree (#59966)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.2+sha-9e1a103`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.2+sha-8551cab`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
