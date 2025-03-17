7262f2be0b0672b39d56fcbc609a4c530bcc0666
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-17 17:41:28 +0000

    450c1fc docs: revise and consolidate server rendering guides (#60383)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-b154fb3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-450c1fc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
