63624f68ca6097fe91bf9acc2a406a2777a00662
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-18 14:17:36 +0000

    22d3f05 feat(core): add hook for producer creation side effects (#60333)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-48974c3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-22d3f05`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
