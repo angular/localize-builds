6af91e0c925967849c772e660b0de25b679c213b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-18 09:38:25 +0000

    bf5d995 refactor(platform-server): expose internal `render` method for extensibility (#60416)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-4930ed3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-bf5d995`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
