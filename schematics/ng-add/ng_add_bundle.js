94050334267e081580297016cb11c82021c4cdc8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-18 09:29:32 +0000

    4930ed3 refactor(devtools): add support for mutating objects inside signals (#60381)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-c27511e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-4930ed3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
