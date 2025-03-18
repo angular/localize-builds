8a981e2f23a2546c36c242496c239d4be5a628f9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-18 09:18:29 +0000

    c27511e docs: fix typo (#60431) PR Close #60431

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-2ca72fa`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-c27511e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
