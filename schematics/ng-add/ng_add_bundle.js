36d0f7f77b2b311d9be14ffd33c57c2af1816650
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-19 15:00:37 +0000

    13a8709 fix(core): catch hydration marker with implicit body tag (#60429)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.2+sha-9fea601`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.2+sha-13a8709`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
