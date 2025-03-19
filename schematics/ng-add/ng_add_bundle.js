6ec803e2a2d19fe0930852e3d1954c4a3b3d9df2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-19 17:46:34 +0000

    c7cacbf ci: fix flakey defer test (#60461)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-7e65b9f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-c7cacbf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
