6788ab57b6a3647be3a64fc3462bae148d112f2e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-19 19:50:38 +0000

    f901a4c docs: update Angular CLI help [main] (#60471)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.2+sha-0362665`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.3+sha-f901a4c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
