29aae362f3a827cd1d7b4232f59c67120b084568
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-03-20 07:48:07 +0000

    9426e83 build: disable RBE for compliance partial goldens (#60473)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.3+sha-6df6aa1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.3+sha-9426e83`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
