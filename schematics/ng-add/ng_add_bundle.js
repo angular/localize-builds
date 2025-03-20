e8a03bcb0d5ae12b69f42f5590a3c236eb6a2d7c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-20 19:42:04 +0000

    8e8ccc7 fix(platform-browser): ensure `platformBrowserTesting` includes `platformBrowser` providers (#60480)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.3+sha-c13c53f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.3+sha-8e8ccc7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
