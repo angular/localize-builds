99fc3306362cca91ad219e387e680fd11ea52273
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-20 19:04:09 +0000

    911ad40 build: fix type checking issues in test code (#60481)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.3+sha-164bb3f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.3+sha-911ad40`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
