fe491a2486b3bc1d160938dbd7121b758fd4a487
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-03-21 12:19:01 +0000

    877456e refactor(core): share code between ComponentRef and targeted instructions (#60463)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.3+sha-c667d54`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.3+sha-877456e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
