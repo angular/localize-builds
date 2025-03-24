c423c7ffe928771bc84442da7b82b07242755b5d
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-03-24 07:06:52 +0000

    d6a1a3a Revert "fix(localize): handle @angular/build:karma in ng add (#60513)" (#60523)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.3+sha-877456e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.3+sha-d6a1a3a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
