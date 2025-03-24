1fcf99a9023ab0247c11841de733d002da6d38fc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-24 14:13:54 +0000

    997836e refactor(core): specify reactive node kind for linked signal (#60451)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.3+sha-dcfa8a0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.3+sha-997836e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
