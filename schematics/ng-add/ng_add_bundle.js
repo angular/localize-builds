d696f3bb5526e59b753b22dad75792c6036cdd63
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-24 16:15:38 +0000

    7499b74 docs: fix templates bindings link (#60521) PR Close #60521

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.3+sha-997836e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.3+sha-7499b74`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
