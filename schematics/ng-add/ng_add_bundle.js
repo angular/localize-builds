d175651979e94356db60f1be698299a1c1d30c61
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-03-26 14:12:53 +0000

    7a65b06 refactor(core): updates to golden files (#60227) (#60556)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.3+sha-00a9331`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.3+sha-7a65b06`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
