8a7a4c740963ea48af2453e8a11de6f2b7ab1253
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-27 20:36:12 +0000

    5948cd0 fix(compiler-cli): Produce fatal diagnostic on duplicate decorated properties (#60376)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-76c60a6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-5948cd0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
