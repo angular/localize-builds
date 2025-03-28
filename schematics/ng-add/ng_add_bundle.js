69a6c0e3c2bddded269fb10944807715636ee6fe
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 19:55:30 +0000

    43d03f8 ci: fix over extraction of init symbols in symbol tests (#60493) (#60614)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-882f96f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-43d03f8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
