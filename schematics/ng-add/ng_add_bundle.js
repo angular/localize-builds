f7d83574d19f366ccb914400e494d5b753fab088
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 20:04:21 +0000

    0252da9 build: remove obsolete bundling test apps (#60591) (#60615)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-43d03f8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-0252da9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
