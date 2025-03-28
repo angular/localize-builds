bd17f6afa2f36c4c1d9b5f2397d5bc773bfc108b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 17:31:44 +0000

    d15fda3 Revert "refactor(core): allow multiple DI profilers (#60562)" (#60611)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-e40b5c9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-d15fda3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
