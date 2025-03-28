df025e341eb5ba1722468d92ccf71530bef28741
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-28 15:53:44 +0000

    e40b5c9 refactor(core): run the control flow migration during `ng update` (#60492)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-152261c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-e40b5c9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
