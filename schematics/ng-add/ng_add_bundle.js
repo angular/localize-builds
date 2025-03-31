a175db0bafa0aba8b1d66c7277dd4a19d1ca5f70
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 22:31:32 +0000

    a4bad8d feat(core): export signalGetFn from signal primitives (#60497)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-ea62a4f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-a4bad8d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
