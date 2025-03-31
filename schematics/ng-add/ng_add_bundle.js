7ff66c0547103556ef586da7c7879e83b9998d0f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 20:54:16 +0000

    4387b37 refactor(router): drop special handling of the OutletInjector (#58351)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-3855730`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-4387b37`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
