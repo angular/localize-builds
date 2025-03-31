74cdd2770a883adb48053b0856bb2921e778b15c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 17:17:07 +0000

    c5d841a fix(devtools): catch firefox non-top level frame error case when using utilities APIs (#60430)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-8b529de`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-c5d841a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
