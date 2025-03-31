8b474f1a0ec35e1a6f5030059a0d49c08ed0215f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-03-31 18:17:48 +0000

    f3361fb refactor(platform-browser): remove empty `deps` lists (#60312)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-a689415`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-f3361fb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
