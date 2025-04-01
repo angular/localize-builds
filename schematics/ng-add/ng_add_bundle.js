80928db90bcf208aa7273f245c1426f766552cd8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 12:11:23 +0000

    e816d2d refactor(core): add `ApplicationRef.prototype.bootstrapImpl` with an `injector` parameter (#60622)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-0e9e034`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-e816d2d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
