9491c7b5701eaa4274da39c7392983ffcfb78043
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 14:13:55 +0000

    bd1a755 refactor(router): Update StateManager base class with common concrete implementations (#60617)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-9503722`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-bd1a755`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
