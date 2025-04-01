b9b7a58df9cf5ca03e3c0c06254aac28539cca99
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 20:26:17 +0000

    916f768 build: update @angular/ng-dev digest to 10732a8 (#60678)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-e9c5e46`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-916f768`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
