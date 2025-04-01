4ffa87678d6dc75ebeaee94d41c824d00cfe85a4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-01 12:26:12 +0000

    b3c7282 Revert "refactor(core): add `ApplicationRef.prototype.bootstrapImpl` with an `injector` parameter (#60622)" (#60669)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-e816d2d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-b3c7282`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
