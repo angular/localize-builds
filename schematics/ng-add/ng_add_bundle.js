4358c7a6fbc5a216e1c5e29b12bef94312975978
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 12:44:55 +0000

    670dc2a docs(docs-infra): fix adev (#60695)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-9604ec6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-670dc2a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
