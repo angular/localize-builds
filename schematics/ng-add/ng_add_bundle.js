3014c25f424a6638f01e783882580052ae355d21
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 11:48:58 +0000

    9604ec6 refactor(compiler): prevent object methods being recognised as entities (#58100)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-da9f509`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-9604ec6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
