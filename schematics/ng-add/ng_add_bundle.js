16a422da57856ed86d846cc9433e6b4040e61c65
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 16:26:33 +0000

    4f86f3f docs(docs-infra): fix algolia icon in the search dialog on safari (#60702)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-5f7f046`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.4+sha-4f86f3f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
