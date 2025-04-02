6d8ce87fa05a0db4aa777aea06739c59c2a12df3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-04-02 15:08:21 +0000

    635fb00 refactor(service-worker): pull less RxJS symbols (#60657)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-8f68d1b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.4+sha-635fb00`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
