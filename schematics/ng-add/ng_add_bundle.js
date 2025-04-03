c458b09cc7da5183af504f40def5bcd3532fab75
Andrew Scott <atscott@google.com>
2025-04-03 17:43:50 +0000

    0e82d42 fix(language-service): Do not provide element completions in end tag (#60616)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.5+sha-8d183ae`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.5+sha-0e82d42`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
