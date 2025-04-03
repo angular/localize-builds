c0638bc1d6f67b903c97e9588400ecce7558893d
Andrew Scott <atscott@google.com>
2025-04-03 18:00:18 +0000

    1232034 refactor(core): add `ApplicationRef.prototype.bootstrapImpl` with an `injector` parameter (#60622)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.5+sha-0e82d42`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.5+sha-1232034`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
