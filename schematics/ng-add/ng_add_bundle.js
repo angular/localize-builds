d941f22f4a5f51ea96b9f40c3dedb529f9f9285a
Andrew Scott <atscott@google.com>
2025-04-03 19:59:02 +0000

    5795e03 fix(http): Delay stabilization until next app synchronization (#60656)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.5+sha-2348f26`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.5+sha-5795e03`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
