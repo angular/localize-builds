0f0b4b4a3b1ab05310268948e6b9ea51209e0440
Andrew Scott <atscott@google.com>
2025-04-03 17:17:52 +0000

    866cea9 feat(core): Stabilize PendingTasks Injectable (#60716)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.5+sha-4e88e18`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.5+sha-866cea9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
