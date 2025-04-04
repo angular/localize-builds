b5430b935d25c76e93bc51e6b656318654ed392a
Andrew Scott <atscott@google.com>
2025-04-04 00:03:44 +0000

    1899358 refactor(common): Add hashchange event for traversals (#60682)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.5+sha-1765ebf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.5+sha-1899358`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
