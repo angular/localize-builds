5a7e74d6bed7163cc606f372047f020e0fae7d40
Andrew Scott <atscott@google.com>
2025-04-04 17:41:56 +0000

    8d050b5 feat(core): stabilize linkedSignal API (#60741)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.5+sha-3ea0163`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.5+sha-8d050b5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
