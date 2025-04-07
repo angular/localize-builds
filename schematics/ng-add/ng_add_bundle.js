9f9e4424d7d17e3fa10b537d8f55d64a87a2d03a
Andrew Scott <atscott@google.com>
2025-04-07 16:33:09 +0000

    00bbd9b fix(core): fix docs for output migration (#60764)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.5+sha-e9a719e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.5+sha-00bbd9b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
