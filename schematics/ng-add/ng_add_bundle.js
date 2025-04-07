b4262c83cee716269fe9b24675addde348e3db75
Andrew Scott <atscott@google.com>
2025-04-07 20:20:27 +0000

    3d85d93 fix(core): reduce total memory usage of various migration schematics (#60774)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.5+sha-0ae1889`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.5+sha-3d85d93`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
