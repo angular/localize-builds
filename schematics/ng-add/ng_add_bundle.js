bb4e5e67c665df8bf4c157c7948225943a3f4e78
Andrew Scott <atscott@google.com>
2025-04-07 20:41:44 +0000

    35de6f6 build: update io_bazel_rules_sass digest to b4800f3 (#60737)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.5+sha-3441f7b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.5+sha-35de6f6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
