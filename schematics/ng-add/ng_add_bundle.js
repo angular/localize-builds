2bdc999e3f98964b72142c992d15b6a5a6c5bc9f
Andrew Scott <atscott@google.com>
2025-04-08 17:27:24 +0000

    56db79a refactor(core): use stream in rxResource instead of loader (#59910)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.5+sha-8b9df44`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.5+sha-56db79a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
