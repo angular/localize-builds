29f8562d58d8254f7c8f606d0676dce95e0d9ab7
Andrew Scott <atscott@google.com>
2025-04-08 23:33:48 +0000

    ae8a4c4 build: update github/codeql-action action to v3.28.15 (#60780)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.5+sha-9241615`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.5+sha-ae8a4c4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
