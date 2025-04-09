ad7f6d4d0a28696f53b4fae221a8072882053b1f
Andrew Scott <atscott@google.com>
2025-04-09 22:48:51 +0000

    36ae560 refactor(core): remove the `phase` prop from `AfterRenderOptions` (#60641)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.6+sha-9f31947`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.6+sha-36ae560`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
