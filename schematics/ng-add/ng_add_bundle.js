f921158ce2f8a772d0f565ba1c3ba620425f766c
Andrew Scott <atscott@google.com>
2025-04-09 21:33:15 +0000

    d39e09d fix(http): Include HTTP status code and headers when HTTP requests errored in `httpResource` (#60802)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.6+sha-54f9269`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.6+sha-d39e09d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
