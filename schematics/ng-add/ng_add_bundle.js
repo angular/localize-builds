906c6f90f5d4c1e7f3040e0dc4277a6a3c4cb0c0
Andrew Scott <atscott01@gmail.com>
2025-04-09 19:33:31 +0000

    54f9269 release: cut the v19.2.6 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.5+sha-fc946c5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.6+sha-54f9269`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
