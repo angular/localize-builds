39c3be6c7bec396bcefa16f8a9d02bf2d37cf9d3
kirjs <kirjs@users.noreply.github.com>
2025-04-10 17:38:24 +0000

    11d441f fix(core): inject migration: replace param with this. (#60713)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.6+sha-5c575a8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.6+sha-11d441f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
