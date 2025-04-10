1d47a5875d710faf34d3a2466f6297a93a359457
kirjs <kirjs@users.noreply.github.com>
2025-04-10 16:00:37 +0000

    5c575a8 build: ensure material dependencies are properly linked for devtools (#60822)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.6+sha-6aa0bbd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.6+sha-5c575a8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
