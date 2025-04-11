6d2ffd39e89e7d204a12f59db2780bf0bb3297d9
kirjs <kirjs@users.noreply.github.com>
2025-04-11 21:27:38 +0000

    4fec563 docs: Restructure example build rules (#60778)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.6+sha-b144126`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.6+sha-4fec563`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
