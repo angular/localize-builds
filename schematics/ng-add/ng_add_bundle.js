1c12b0ac02727dc63ac83b5b816c83a0f66d7dcc
kirjs <kirjs@users.noreply.github.com>
2025-04-14 13:30:00 +0000

    b210d3c refactor(devtools): switch to components tab whenever the inspector is used (#60694)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.6+sha-4fec563`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.6+sha-b210d3c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
