0dd0ed9d85e0b64dcd0576875d32a4d2ab2c06fe
kirjs <kirjs@users.noreply.github.com>
2025-04-14 15:19:23 +0000

    c1bcae9 feat(core): expose performance data in Chrome DevTools (#60789)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.6+sha-84d359c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.6+sha-c1bcae9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
