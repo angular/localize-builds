a43ce527567d29fbab22d43e12f8c6d923409ef8
kirjs <kirjs@users.noreply.github.com>
2025-04-16 14:30:42 +0000

    f22c72b docs(docs-infra): first-app tutorial - step-14-http src-final correction (#60738)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.6+sha-e90daf3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.6+sha-f22c72b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
