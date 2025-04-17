44826a2e8f17af22851f66812282ed3f28a933c1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-17 09:33:50 +0000

    763e3ca release: bump Angular DevTools version to 1.0.28 (#60893)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.7+sha-2c1f348`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.7+sha-763e3ca`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
