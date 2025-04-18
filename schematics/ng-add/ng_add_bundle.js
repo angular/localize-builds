c029927c4cf3f6734c97ff3cb0b3afa2b8408ba0
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-18 06:43:18 +0000

    f838726 refactor(devtools): typography (#60531)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.7+sha-2e87130`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.7+sha-f838726`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
