e0ddd48cc96cbf4dfe54e188510c4bc1d81d900c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-18 13:42:42 +0000

    3a6121f docs: add new routing in-depth guide (#60006)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.7+sha-1694739`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.7+sha-3a6121f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
