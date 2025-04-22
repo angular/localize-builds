0702464ed47f118888c1a1d734d45485acda650b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 19:35:51 +0000

    cebb9d2 refactor(devtools): disable unsupported features (#60585)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.7+sha-6c2fbad`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.7+sha-cebb9d2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
