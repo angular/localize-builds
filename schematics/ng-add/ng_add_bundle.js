19a568ea6f5a1e5918e62bba0330578a23f32ea8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 09:21:45 +0000

    b43fe29 docs: update examples for i18n tokens (#60938)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.7+sha-0162ceb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.7+sha-b43fe29`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
