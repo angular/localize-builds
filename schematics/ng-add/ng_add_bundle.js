9073c8c7dfc02351b3fa481951c6af614725e08a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-22 11:45:19 +0000

    2c463a4 build: lock file maintenance (#60942)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.7+sha-be99562`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.7+sha-2c463a4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
