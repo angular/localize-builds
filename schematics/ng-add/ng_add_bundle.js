f48f79c89edc5a725653eda5c6276352937bb840
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-23 06:58:12 +0000

    7d29809 docs(docs-infra): Improve doc support for pipes. (#60926)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.7+sha-3824519`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.7+sha-7d29809`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
