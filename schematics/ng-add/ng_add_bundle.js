019b20d5a9663e4a341f9bc500f8c72110cc0e79
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-04-23 15:33:59 +0000

    5696451 docs: remove gitter reference (#60970)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.8+sha-4d7eb05`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.8+sha-5696451`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
