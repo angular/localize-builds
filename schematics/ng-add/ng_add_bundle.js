ee3cd15e2125a2c3c97871f21e1f0c07ae90f6a4
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-04-25 11:00:04 +0000

    d5fd734 feat(core): introduce TestBed.tick() (#60993)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.8+sha-ccc5cc0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.8+sha-d5fd734`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
