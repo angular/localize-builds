cbc08436da11f82006b3fe3f99ec7dbf5c63a7cd
Miles Malerba <mmalerba@google.com>
2025-04-28 15:15:22 +0000

    aca1f45 docs: fix non-working link (#61011)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.8+sha-7d133c0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.8+sha-aca1f45`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
