f1ba86382202e8d42c8978dc9fc932403c1c17f1
Miles Malerba <mmalerba@google.com>
2025-04-28 21:42:51 +0000

    2445946 refactor(core): Update FakeNavigation to extract some Angular quirks (#60961)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.8+sha-7680a27`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.8+sha-2445946`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
