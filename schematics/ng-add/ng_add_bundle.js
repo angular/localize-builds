838c4df3602d5b0537bc65a35069f027f085fb47
Miles Malerba <mmalerba@google.com>
2025-04-29 21:16:06 +0000

    adaecba refactor(compiler-cli): split up large method (#61018)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.8+sha-1003308`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.8+sha-adaecba`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
