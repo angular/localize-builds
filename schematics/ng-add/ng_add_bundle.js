0ee252d52d597907e5a10767d47bbb80a22b77c1
Miles Malerba <mmalerba@google.com>
2025-04-29 16:59:15 +0000

    888f9c2 docs: Changed word optimally to optionally (#61041) PR Close #61041

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.8+sha-42cad28`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.8+sha-888f9c2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
