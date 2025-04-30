89381320cf632a030b18f16e74e7e97b30b012ef
Andrew Kushnir <akushnir@google.com>
2025-04-30 19:50:08 +0000

    9b96fcc refactor(service-worker): drop error messages in production (#59702)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-c1e46c5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-9b96fcc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
