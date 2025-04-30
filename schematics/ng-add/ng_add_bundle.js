c92c01f44fd0801cafa0a467dd55788d1245ce54
Miles Malerba <mmalerba@google.com>
2025-04-30 15:33:13 +0000

    2bddce2 docs: update Angular CLI help [main] (#61048)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.8+sha-867f389`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.8+sha-2bddce2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
