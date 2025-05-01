2c16c9346b0c437ad77c1953bbdf4591f6712e6a
Andrew Kushnir <akushnir@google.com>
2025-05-01 15:53:30 +0000

    d2cb0b9 refactor(core): tree-shake `getDirectiveDef` error (#61075)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-33a336f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-d2cb0b9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
