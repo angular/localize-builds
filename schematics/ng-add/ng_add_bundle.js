d5eea28d0eba153ec4a13f7e219ce875ecaebc85
Andrew Kushnir <akushnir@google.com>
2025-05-01 22:07:31 +0000

    8828a84 ci: add a script to copy cdk api files to adev (#61081)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-5f31c95`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-8828a84`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
