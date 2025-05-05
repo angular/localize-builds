1717fb456f0c662e8ba859d7790c76cd69324876
Andrew Kushnir <akushnir@google.com>
2025-05-05 19:02:57 +0000

    63d10dc build: fix the lockfiles (#61127)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.9+sha-782c6e1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.9+sha-63d10dc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
