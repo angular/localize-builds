c8ca9e190f062122e2a70e34e1378fec10a07918
Andrew Kushnir <akushnir@google.com>
2025-05-05 16:31:56 +0000

    310e5ff build: update rules_angular digest to 3ba9d67 (#61124)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-e7f5aa2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-310e5ff`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
