58ad6a9e451101e29f92e331c4b1b90b565b138c
Andrew Kushnir <akushnir@google.com>
2025-05-06 16:19:01 +0000

    453b1d9 build: update github/codeql-action action to v3.28.17 (#61134)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.9+sha-8772236`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.9+sha-453b1d9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
