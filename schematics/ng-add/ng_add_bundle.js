748d20f5d9658031c06058eac2dd2c4aadf308cc
Andrew Kushnir <akushnir@google.com>
2025-05-06 21:19:41 +0000

    4b365c7 docs: fix non-working link  (#61131)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.9+sha-6cd5d8e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-next.9+sha-4b365c7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
