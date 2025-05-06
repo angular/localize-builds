81a33839363fc6facf6ef8289d9355e4588e41fe
Andrew Kushnir <akushnir@google.com>
2025-05-06 17:34:23 +0000

    267bc0e docs: fix whitespace after a link (#61136)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.9+sha-453b1d9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.9+sha-267bc0e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
