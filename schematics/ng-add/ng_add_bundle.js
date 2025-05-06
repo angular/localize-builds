ec872aa3bb9c47845501238f18e7d19c0e9fca4f
Andrew Kushnir <akushnir@google.com>
2025-05-06 18:13:34 +0000

    c39e799 Revert "docs(docs-infra): fix first-app intro example (#61042)" (#61145)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-c3ff01b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-c39e799`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
