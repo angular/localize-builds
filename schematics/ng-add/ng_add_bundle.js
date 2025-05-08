402a9896b387ea9e8b57a0323670daaeb6e5f834
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 11:37:34 +0000

    1dece6c ci: add workflow to sync non-default branches (#61201)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.0+sha-9f43232`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.0+sha-1dece6c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
