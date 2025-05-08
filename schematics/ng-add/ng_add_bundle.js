70d38eb25ea3129d50350e8310405fde827de2f9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 17:05:40 +0000

    5b60442 docs:  update the description of example (#61205) PR Close #61205

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.10+sha-684af21`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.10+sha-5b60442`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
