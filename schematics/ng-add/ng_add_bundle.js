3279f2f16d21c6f98b1cf834c6285013fc15caa2
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 14:44:49 +0000

    bad2870 ci: use bash syntax for `CURRENT_BRANCH` in GitHub Actions (#61212)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.0+sha-2f7e4eb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.0+sha-bad2870`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
