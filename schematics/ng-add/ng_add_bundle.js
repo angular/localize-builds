1c09ff86708e7c424d66e2f64783639726167c00
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-08 12:50:30 +0000

    6255954 ci: remove redundant dash in workflow file (#61206)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.10+sha-e9c9cfd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.10+sha-6255954`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
