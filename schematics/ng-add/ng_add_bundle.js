f69e6af1c763b72714b116a8e23c377b07709a62
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-08 16:36:00 +0000

    655954a ci: fix step name (#61213)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.10+sha-3b9440b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.10+sha-655954a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
