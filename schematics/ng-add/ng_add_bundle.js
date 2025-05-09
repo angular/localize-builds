9628e2118b23d4125dca5b4509679724e7181273
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-09 10:01:22 +0000

    c8dfe6d ci: ensure pnpm-lock file and aspect lock files are up-to-date (#61224)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-db06708-with-local-changes`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-c8dfe6d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
