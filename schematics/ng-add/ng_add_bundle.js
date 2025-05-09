2d5b0aa9998452ea12df65bbded5eda133baa38c
Joey Perrott <josephperrott@gmail.com>
2025-05-09 16:41:11 +0000

    84b66f3 build: migrate adev shared-docs package to use ts_project (#61217)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.10+sha-6cd2df0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.10+sha-84b66f3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
