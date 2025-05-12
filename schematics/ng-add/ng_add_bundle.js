239745f46deed9b59513356ad49c0e30c3c35302
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-12 22:49:39 +0000

    8d90626 ci: refactor renovate config to use dev-infra preset (#61268)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.0+sha-3d9cb63`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.0+sha-8d90626`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
