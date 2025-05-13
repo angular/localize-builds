574deb3a5275b864887d6e96c4146652a4255c10
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-13 09:04:02 +0000

    ed9a1d4 build: do not auto install peer dependencies (#61295)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.0+sha-8d90626`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.0+sha-ed9a1d4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
