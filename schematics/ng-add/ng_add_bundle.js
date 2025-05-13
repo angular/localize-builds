0560e94d9563a8047b0b305d07c9be8e0c212b30
Joey Perrott <josephperrott@gmail.com>
2025-05-13 22:56:00 +0000

    1b4b44e refactor: add explicit types for exports relying on inferred call return type (#61312)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.0+sha-c1b9d3e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.0+sha-1b4b44e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
