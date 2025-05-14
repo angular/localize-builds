a59ab11736a18a270920e196b221b87a80c11121
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 16:42:31 +0000

    d835a44 build: migrate upgrade to use ng_project instead of ng_module (#61320)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-a07413a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-d835a44`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
