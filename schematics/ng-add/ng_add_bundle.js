009e0bdd3d9bbad52b5ea04ecb255caed022795b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-14 17:53:53 +0000

    dfd068d build: use common macro to define tsconfig for service worker (#61341)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.10+sha-a4e3c12`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.10+sha-dfd068d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
