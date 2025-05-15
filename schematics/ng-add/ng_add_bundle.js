673dd98735cb2205944bf3b3399f13f69d69df56
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-15 17:25:19 +0000

    ca6295e fix(core): handle different DI token types in Chrome DevTools integration (#61333)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-bc2cab7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-ca6295e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
