a86aa42d13d121ad65174542dfd480c05b44c506
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-15 17:34:10 +0000

    f699476 build: update dependency node to v22.15.1 (#61358)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-ca6295e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-f699476`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
