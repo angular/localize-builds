69d1a4ccdf17d15ef056328001b5b34278896f78
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-05-15 15:35:09 +0000

    b2a74aa docs: release notes for the v19.2.11 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-ff62ff1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-b2a74aa`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
