7f03a069e841d31288766310995876bb20efee38
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-15 12:58:50 +0000

    f2d6e59 refactor: explicitly ensure `ngDevMode` types are available (#61365)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.10+sha-dfd068d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.10+sha-f2d6e59`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
