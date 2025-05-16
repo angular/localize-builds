6c0cd9074e0d6443dfe23876c501ef200db0c069
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 08:01:38 +0000

    648d42f fix(core): Testing should not throw when Zone does not patch test FW APIs (#61376)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-7034b74`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-648d42f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
