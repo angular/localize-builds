5e00803d429f647c586a5a384bbdb79181cb868f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 07:21:56 +0000

    1ffe37d ci: replace usage of `create-pr-for-changes` with `create-pull-request` (#61388)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-2699dd6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-1ffe37d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
