10f819b2c08e7c35b2a19883ffd60b4f2fc458c8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 10:28:57 +0000

    b2ade23 docs: fix typos (#61257)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-db815e9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-b2ade23`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
