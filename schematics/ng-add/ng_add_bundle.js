d9a25a8e03aa1a3313eb89b7e2848fa9d76bfdef
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 09:23:00 +0000

    85e0baf docs: fix Routed components example (#61278)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-8edafd0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-85e0baf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
