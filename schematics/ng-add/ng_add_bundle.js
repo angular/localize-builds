885cb0de64164cef922decea22fc163d60e5b7bc
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 10:18:17 +0000

    f0e961c docs: fix note formatting about optionam chaining (#61251)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-095fa2f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-f0e961c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
