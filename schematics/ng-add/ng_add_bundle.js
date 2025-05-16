0846a8130e41d7245075f69712dfcab52116ae4c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 07:49:14 +0000

    bd2a5b9 docs: update Angular CDK apis [19.2.x] (#61347)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-82e5449`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-bd2a5b9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
