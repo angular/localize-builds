b7a28b377c5dc8446b1d90bf118a9a416d5ac316
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-16 09:13:01 +0000

    fb65ad1 perf(platform-server): speed up resolution of base (#61392)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-333a0f3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-fb65ad1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
