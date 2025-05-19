a5859cdbee9523656eb8c251be588c2df0e077a9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 11:29:57 +0000

    1582610 ci: replace `yarn ng-dev misc update-generated-files` with separate update commands for specific targets (#61467)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-5d35288`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-1582610`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
