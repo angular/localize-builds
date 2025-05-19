f7d008e55da48a3e86c18f7ffde32b81bb8330f1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 11:13:41 +0000

    bd2b454 refactor(migrations): gracefully proceed if reference cannot be resolved (#61426)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-9aedd30`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-bd2b454`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
