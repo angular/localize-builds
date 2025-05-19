4f0c6965a80dd7ed3164c2bcfb7bc32a3c39a32d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 14:34:17 +0000

    feb571c refactor(core): expose enableProfiling() on from @angular/core (#61465)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-2b3c89d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-feb571c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
