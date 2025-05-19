8b7ef5218eeb72b76517ef647026f4ed44224416
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-19 16:12:30 +0000

    1360e4b docs: point cdk drag-drop doc links to adev api pages (#61437)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-0a25aba`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-1360e4b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
