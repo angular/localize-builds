3a3d403ab1556a8c6a9c9c870394ef710017d90f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 20:15:34 +0000

    b0ee7ff docs: update Angular CLI help (#61526)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-bb220c0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-b0ee7ff`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
