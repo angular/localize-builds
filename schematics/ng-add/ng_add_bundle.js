895eec41899d14430f29976aa00a5be36bc4fb91
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 09:02:31 +0000

    dbd66ec refactor(devtools): switch default font to material symbols (#61281)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-1360e4b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-dbd66ec`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
