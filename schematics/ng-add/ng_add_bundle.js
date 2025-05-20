1d49dce9612355589079f1207095df6581d19af9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 20:14:55 +0000

    9ce79be docs: update v17 as EOL for current documentation (#61532)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-bb31f6f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.11+sha-9ce79be`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
