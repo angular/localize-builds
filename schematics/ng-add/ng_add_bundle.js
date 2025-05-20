c6322ebcb0ed8654a1bcf86d7bfd1ffa2217e607
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-20 10:30:33 +0000

    f702f0e build: update dependency conventional-changelog to v7 (#61491)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-b55fa4e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-f702f0e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
