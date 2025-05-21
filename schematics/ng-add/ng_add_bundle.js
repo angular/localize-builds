058fc428a61b96e5004b9db970a1a567a6eaf101
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 11:44:00 +0000

    00f34ed refactor(core): drop injection context assertion in production (#61560)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-edc694d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-00f34ed`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
