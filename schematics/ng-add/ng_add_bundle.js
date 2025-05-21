8e68f82a8c42f65c7170e8a2c703bc9fdb0c6e0a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 17:13:02 +0000

    b37fd97 build: migrate language service to use rules_js (#61568)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-f68001d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-b37fd97`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
