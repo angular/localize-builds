fe2fd1628a5683ed8c9e130f662b4c4575630dc7
Andrew Scott <atscott01@gmail.com>
2025-05-21 20:13:45 +0000

    09f79e0 release: cut the v20.0.0-rc.2 release

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-6e79eaf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.2+sha-09f79e0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
