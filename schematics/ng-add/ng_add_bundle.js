17e2b5cf3889909a4a0783226e26dfe14888f04f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 09:24:56 +0000

    3cc0910 build: update compiler-cli to not be stamped when used for the compiler in ng_project (#61535)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-0a82e58`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-3cc0910`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
