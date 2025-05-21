670f3cffecd32e574dbd58fa57bafde4d702fb9d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 12:38:38 +0000

    e74266b fix(compiler): avoid conflicts between HMR code and local symbols (#61550)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-b90160d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-e74266b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
