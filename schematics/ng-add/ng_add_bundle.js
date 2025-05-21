1d07960dfcba972233e247efa7dc71edcfcb3bdd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 11:15:37 +0000

    edc694d ci: temporarily disable saucelabs for push jobs (#61558)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-4e5d6de`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-edc694d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
