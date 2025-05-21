c9ef2417f4ed26ecf7e9dd69bd62e767ef4664e7
Andrew Scott <atscott@google.com>
2025-05-21 23:12:07 +0000

    2f5912e docs(docs-infra): fix nav items active status is shown for multiple items (#61597)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.12+sha-ac76549`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.12+sha-2f5912e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
