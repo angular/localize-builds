7d2d006b679ff0d743b56baabd55221f1ef1fd39
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 12:15:10 +0000

    b90160d fix(core): cleanup testability subscriptions (#61261)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-c68e9d0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-b90160d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
