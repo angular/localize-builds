b121ad7b32747e7bfc6b9a72e1c81a9215ba90a1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 15:31:02 +0000

    d97892a docs: include bun in packageManager value types (#60907)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-abdade9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.1+sha-d97892a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
