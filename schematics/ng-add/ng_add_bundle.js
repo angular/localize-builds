50e779a0cde0268f7b308ef6409b841bd9acc28d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-05-21 13:59:51 +0000

    1123b31 refactor(core): consolidate TNode creation checks (#61563)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-e7656b8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-1123b31`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
