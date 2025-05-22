c61485711ad38e5568b25f1b6fb5e12480704e1b
Andrew Scott <atscott@google.com>
2025-05-22 18:01:32 +0000

    0a51f36 release: bump Angular DevTools to version 1.0.32 (#61620)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -147,7 +147,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~19.2.12+sha-b15bddf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~19.2.12+sha-0a51f36`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
