e0569cf85d91fd02ac94038e6f6ae6ef78c76238
Andrew Scott <atscott@google.com>
2025-05-22 17:10:43 +0000

    959258d fix(service-worker): do not register service worker if app is destroyed before it is ready to register (#61101)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.2+sha-1a3313a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.2+sha-959258d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
