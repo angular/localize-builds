ffc6ef1649affed1f5419499c973dd41d4088416
Andrew Scott <atscott@google.com>
2025-05-22 19:23:19 +0000

    396b543 feat(devtools): update get signal graph (#61541)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-687e018`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-396b543`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
