e345b135a738adde7df12ce5e10155952e7158dc
Andrew Scott <atscott@google.com>
2025-05-25 21:34:51 +0000

    2e5362a feat(common): accept undefined inputs in NgTemplateOutlet (#61404)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -148,7 +148,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.2+sha-f1568ef`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.0-rc.2+sha-2e5362a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
