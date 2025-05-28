8603f109d9bc8650246dcbc701d190dc28058de9
GitHub <noreply@github.com>
2025-05-28 14:25:11 +0000

    62be7fa docs: cherrypick v20 release notes to main (#61724)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -721,7 +721,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, dependencies_1.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, utility_1.addDependency)("@angular/localize", `~20.1.0-next.0+sha-1fd59c0`);
+  return (0, utility_1.addDependency)("@angular/localize", `~20.1.0-next.0+sha-62be7fa`);
 }
 function default_1(options) {
   const projectName = options.project;
