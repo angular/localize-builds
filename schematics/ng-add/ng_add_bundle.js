48b037975ddca6d916d959c268eacc4cb9715238
GitHub <noreply@github.com>
2025-05-28 17:44:00 +0000

    bf7816e build: bump peer deps for angular-in-memory-web-api (#61730)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -721,7 +721,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, dependencies_1.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, utility_1.addDependency)("@angular/localize", `~20.0.0+sha-55306bd`);
+  return (0, utility_1.addDependency)("@angular/localize", `~20.0.0+sha-bf7816e`);
 }
 function default_1(options) {
   const projectName = options.project;
