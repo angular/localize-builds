91632bb39393547f47aaf384801a5c79fc95d383
kirjs <kirts@google.com>
2025-05-29 22:28:57 +0000

    6113fae docs: fix copy-paste error on defer page in interaction section (#61751) PR Close #61751

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -721,7 +721,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, dependencies_1.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, utility_1.addDependency)("@angular/localize", `~20.0.0+sha-1802d36`);
+  return (0, utility_1.addDependency)("@angular/localize", `~20.0.0+sha-6113fae`);
 }
 function default_1(options) {
   const projectName = options.project;
