6927c526de15b3e0ccf3401fecefeec38bdf7ac8
kirjs <kirts@google.com>
2025-05-29 22:09:47 +0000

    1802d36 docs: remove ngIf/ngFor/ngSwitch from the directive overview (#61708)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -721,7 +721,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, dependencies_1.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, utility_1.addDependency)("@angular/localize", `~20.0.0+sha-f7d33ee`);
+  return (0, utility_1.addDependency)("@angular/localize", `~20.0.0+sha-1802d36`);
 }
 function default_1(options) {
   const projectName = options.project;
