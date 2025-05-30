cdb3efe00daddb5c214bdd166b6ed2b4e811e698
kirjs <kirts@google.com>
2025-05-30 17:59:02 +0000

    a70f03a fix(compiler): move defer trigger assertions out of parser (#61747)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -721,7 +721,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, dependencies_1.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, utility_1.addDependency)("@angular/localize", `~20.1.0-next.0+sha-5ccc654`);
+  return (0, utility_1.addDependency)("@angular/localize", `~20.1.0-next.0+sha-a70f03a`);
 }
 function default_1(options) {
   const projectName = options.project;
