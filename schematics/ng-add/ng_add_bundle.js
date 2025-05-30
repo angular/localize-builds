f08fd6509f7346373d2c12fe7cc714154e639eb2
kirjs <kirts@google.com>
2025-05-30 17:45:13 +0000

    5ccc654 docs: fix component harness indentation and comments (#61760)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -721,7 +721,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, dependencies_1.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, utility_1.addDependency)("@angular/localize", `~20.1.0-next.0+sha-194eb11`);
+  return (0, utility_1.addDependency)("@angular/localize", `~20.1.0-next.0+sha-5ccc654`);
 }
 function default_1(options) {
   const projectName = options.project;
