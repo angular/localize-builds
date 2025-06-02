0e12c647eee571106fda036880828f70bb9ab065
kirjs <kirts@google.com>
2025-06-02 14:04:30 +0000

    5a76826 fix(compiler): only report parser errors on invalid expression (#61793)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -721,7 +721,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, dependencies_1.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, utility_1.addDependency)("@angular/localize", `~20.1.0-next.0+sha-744a1fa`);
+  return (0, utility_1.addDependency)("@angular/localize", `~20.1.0-next.0+sha-5a76826`);
 }
 function default_1(options) {
   const projectName = options.project;
