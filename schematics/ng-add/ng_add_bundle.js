86c0d928db6c106885001c50c004490eecacdb47
kirjs <kirts@google.com>
2025-06-02 19:59:26 +0000

    fd5a049 fix(compiler): recover invalid parenthesized expressions (#61815)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -721,7 +721,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, dependencies_1.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, utility_1.addDependency)("@angular/localize", `~20.1.0-next.0+sha-0e58e1f`);
+  return (0, utility_1.addDependency)("@angular/localize", `~20.1.0-next.0+sha-fd5a049`);
 }
 function default_1(options) {
   const projectName = options.project;
