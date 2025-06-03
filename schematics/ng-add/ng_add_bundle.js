4acaad693b5ed68dbe35c77ba837e971a0ecf1c7
kirjs <kirts@google.com>
2025-06-03 14:45:52 +0000

    9a24a2b build: update rules_angular digest to 8c799e8 (#61841)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -721,7 +721,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, dependencies_1.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, utility_1.addDependency)("@angular/localize", `~20.0.0+sha-b5d21b0`);
+  return (0, utility_1.addDependency)("@angular/localize", `~20.0.0+sha-9a24a2b`);
 }
 function default_1(options) {
   const projectName = options.project;
