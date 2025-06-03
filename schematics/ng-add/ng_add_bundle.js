12496919b63fa1ee80ec2f60e39e02ae7a933274
kirjs <kirts@google.com>
2025-06-03 18:07:25 +0000

    939ca07 docs(docs-infra): fix tutorial tooltip (#61782)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -721,7 +721,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, dependencies_1.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, utility_1.addDependency)("@angular/localize", `~20.0.0+sha-9a24a2b`);
+  return (0, utility_1.addDependency)("@angular/localize", `~20.0.0+sha-939ca07`);
 }
 function default_1(options) {
   const projectName = options.project;
