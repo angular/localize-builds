712b459ee4cdf5956e8dc7054e68c9b49a9c8049
kirjs <kirts@google.com>
2025-06-04 21:31:33 +0000

    49e273a build: update all non-major dependencies (#61701)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -721,7 +721,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, dependencies_1.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, utility_1.addDependency)("@angular/localize", `~20.0.1+sha-66e9924`);
+  return (0, utility_1.addDependency)("@angular/localize", `~20.0.1+sha-49e273a`);
 }
 function default_1(options) {
   const projectName = options.project;
