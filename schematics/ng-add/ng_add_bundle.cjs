280aa43d8c405e25cce770ecfe967a097dc3ecfd
Kirill Cherkashin <kirill@kcherkashin.org>
2025-06-04 21:28:45 +0000

    586be0e release: cut the v20.1.0-next.0 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-b839d08`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-586be0e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
