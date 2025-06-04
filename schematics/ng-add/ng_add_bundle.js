b8f485377000fd21b7c9f1c836befc256851abb2
kirjs <kirts@google.com>
2025-06-04 18:32:45 +0000

    ddd22be fix(core): unregister `onDestroy` in `ResourceImpl` when `destroy()` is called (#61870)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -721,7 +721,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, dependencies_1.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, utility_1.addDependency)("@angular/localize", `~20.0.0+sha-bdf882c`);
+  return (0, utility_1.addDependency)("@angular/localize", `~20.0.0+sha-ddd22be`);
 }
 function default_1(options) {
   const projectName = options.project;
