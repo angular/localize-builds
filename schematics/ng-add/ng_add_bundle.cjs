98d23400751ee82af497ccf69628662c5c760f7f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 11:43:06 +0000

    fdcb14d ci: atomatically add target labels based on base branch (#61894)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.1+sha-f562fd4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.1+sha-fdcb14d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
