cb9af9cbfae957a80ce61731b7ef6b1621471a4e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-05 09:24:38 +0000

    e81ea0c fix(core): unregister `onDestroy` in `outputToObservable` (#61882)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-967c8fe`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-e81ea0c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
