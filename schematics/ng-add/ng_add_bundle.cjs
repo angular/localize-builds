698affae0903dea7e5137b4d1ceb9445e8c69beb
Andrew Kushnir <akushnir@google.com>
2025-06-05 16:21:23 +0000

    9354efc build: remove unnecessary zone.js dep from various build targets (#61901)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-4f89f6e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-9354efc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
