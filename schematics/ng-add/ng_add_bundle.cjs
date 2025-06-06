aff32d15cebcdc8e28b1675e3232b5eca3cba395
Andrew Kushnir <akushnir@google.com>
2025-06-06 00:54:17 +0000

    cfb0dc2 fix(docs-infra): Fix import name for house location (#61914)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-9354efc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-cfb0dc2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
