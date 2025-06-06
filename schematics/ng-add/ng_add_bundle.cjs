bff6a283a28bcd4418fc7d37212db4efe6553d63
Andrew Kushnir <akushnir@google.com>
2025-06-06 01:58:58 +0000

    71017f2 fix(docs-infra): fix wrong `visibleLines` numbers in `docs-code` blocks (#61909)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.1+sha-33bd215`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.1+sha-71017f2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
