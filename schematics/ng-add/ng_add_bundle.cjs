5ab8c27493b6fc91d5891315d5284e6c23dacf9f
Andrew Kushnir <akushnir@google.com>
2025-06-06 19:50:19 +0000

    e557313 docs: Add error handling doc to the menu (#61945)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-bbd16f5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-e557313`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
