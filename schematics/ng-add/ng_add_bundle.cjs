989b8e9399a238fc5419ba869d297683cc060cda
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-06 08:39:15 +0000

    9ae8027 build: update cross-repo angular dependencies (#61912)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.1+sha-71017f2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.1+sha-9ae8027`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
