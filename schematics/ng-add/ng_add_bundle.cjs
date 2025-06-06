ddcea6a0a274eb8f1f97574c4152360d554cdcee
Andrew Kushnir <akushnir@google.com>
2025-06-06 14:56:30 +0000

    29e892a Revert "refactor(service-worker): make `SwPush` and `SwUpdate` tree-shakable (#61670)" (#61923)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-6097184`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-29e892a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
