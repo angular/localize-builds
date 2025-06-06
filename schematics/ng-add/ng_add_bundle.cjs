b04270de5847ad427d581d800f15dcbbd42e4835
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-06 11:56:04 +0000

    6097184 refactor(core): Delete createSignalTuple (#61907)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-8ab0028`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-6097184`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
