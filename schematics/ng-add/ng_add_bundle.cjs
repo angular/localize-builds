c543ac94549c3d8ac3d2a8b643bebe440decaf21
Andrew Kushnir <akushnir@google.com>
2025-06-06 22:27:32 +0000

    21fc93b test: use zone from npm instead of the locally built package for modules testing (#61947)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-e557313`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-21fc93b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
