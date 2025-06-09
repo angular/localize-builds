a6d125dcfc554e4cc9db1207b0269df942e07c0d
Andrew Kushnir <akushnir@google.com>
2025-06-09 19:05:34 +0000

    ee4acc2 build: update dependency google-closure-compiler to v20250603 (#61960)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-85e9505`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-ee4acc2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
