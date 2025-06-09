fb8e83c257d121cc67305243f4e9423090636e4e
Andrew Kushnir <akushnir@google.com>
2025-06-09 18:30:47 +0000

    85e9505 fix(zone.js): waitForAsync should pass args to the test function (#61755)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-74b2aa6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-85e9505`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
