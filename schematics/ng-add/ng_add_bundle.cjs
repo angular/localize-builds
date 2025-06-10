1b984aa689d9da0ae560ecc52a59708a24d89c2b
Andrew Kushnir <akushnir@google.com>
2025-06-10 21:51:28 +0000

    b9141da refactor(devtools): use import type to improve performance (#61966)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-4f0221e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-b9141da`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
