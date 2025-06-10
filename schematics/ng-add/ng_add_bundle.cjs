e282f9b32945f812ef89b54e5156fbfadf05e94b
Andrew Kushnir <akushnir@google.com>
2025-06-10 23:02:43 +0000

    eef6faf ci: update to latest version of dev-infra actions (#61990)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-9d6db41`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-eef6faf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
