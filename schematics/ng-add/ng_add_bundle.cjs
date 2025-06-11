8758cfeb14231b28ccd5d9c324bcf3e5ba454605
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 09:47:21 +0000

    9a81a0f docs: navigate-to-routes.md to include RouterLink in imports. (#61965)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-ce7f2e3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-9a81a0f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
