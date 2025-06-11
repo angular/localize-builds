eab340e60697654b181c47ba3e98d48f781ae10a
Andrew Kushnir <akushnir@google.com>
2025-06-11 00:39:30 +0000

    229c6b4 build: update dependency typescript to v5.8.3 (#61958)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-eef6faf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-229c6b4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
