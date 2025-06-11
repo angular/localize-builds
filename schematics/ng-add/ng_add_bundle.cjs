8c6a0c209dbc20492c39202689d43c4a1a6c0f56
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 10:17:16 +0000

    ff27f21 build: update all non-major dependencies (#61954)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-06979c6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-ff27f21`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
