c847b58b27e03d21bdf91b72444c63b508fd5df4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 10:30:17 +0000

    5053637 build: update dependency google-closure-compiler to v20250608 (#61996)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-9a81a0f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.0+sha-5053637`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
