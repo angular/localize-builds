52583b8ff53ff0b9492513b23f5ff79c39aa106a
Andrew Kushnir <akushnir@google.com>
2025-06-11 01:37:52 +0000

    ca478a2 Revert "docs(docs-infra): retain the results in the search dialog while typing (#61935)" (#61995)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-229c6b4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-ca478a2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
