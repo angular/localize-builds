9bd22615cd26a081ff213c3667be5d8b43bcb0c3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-11 11:46:55 +0000

    554b81e docs(docs-infra): retain the results in the search dialog while typing (#61935)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-ff27f21`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.2+sha-554b81e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
