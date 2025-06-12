0d704f63b427f6d965cdc5cd54aa870110d77dd6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-12 10:28:59 +0000

    61fb6e7 build: fix hermetic execution of `packages/core/test/...` (#62027)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.1+sha-6f779b2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.1+sha-61fb6e7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
