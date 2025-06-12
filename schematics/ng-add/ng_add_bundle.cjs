295dceae8cbb98c711197f66a825a0d64fb3aa61
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-12 11:06:06 +0000

    e343cdf fix(core): Fixes template outlet hydration (#62012)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.3+sha-43ef2ea`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.3+sha-e343cdf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
