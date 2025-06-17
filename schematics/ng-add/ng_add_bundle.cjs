f42be52d8456aa7635045cf3a9d189b376802ad3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 17:43:27 +0000

    39237e8 docs: add update AI instructions (#62089)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.1+sha-18a7ce8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.1+sha-39237e8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
