6d34c0698b8b993c36a715001b6455743f6bbc84
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-17 09:03:33 +0000

    734c13a docs: remove mention of `ngModel` in directives overview (#62088)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.3+sha-deaa292`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.3+sha-734c13a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
