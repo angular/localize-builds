68c2d7177f27969b44c689a39d46f782f36a0fca
Joey Perrott <josephperrott@gmail.com>
2025-06-17 23:53:55 +0000

    04e0d4f build: update the ts_integrity expected for the typescript version in WORKSPACE (#62109)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.3+sha-1c73f54`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.3+sha-04e0d4f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
