ce68f3e2b28d875380fb2734215bbfd535ae6fe2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 18:00:52 +0000

    085e29f release: cut the v20.1.0-next.2 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.1+sha-81d918a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-085e29f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
