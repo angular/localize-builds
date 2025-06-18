529aa83b856f43fef4aac0a2682551a9f6ae4377
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 07:42:23 +0000

    fecfb89 build: update dependency aspect_rules_js to v2.3.8 (#62091)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.3+sha-24b4484`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.3+sha-fecfb89`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
