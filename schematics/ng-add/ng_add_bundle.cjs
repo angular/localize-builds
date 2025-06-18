bf2da96f3c291df42e3b7bd45194695ebcdf7df5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 07:19:19 +0000

    5f6e336 docs: add dedicated redirecting routes guide (#62005)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.1+sha-3a0cfd5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.1+sha-5f6e336`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
