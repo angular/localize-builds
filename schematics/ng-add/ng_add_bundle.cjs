bfef3a181cb08a3ee0af6f8fb1460f7944c90f73
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-06-18 07:54:02 +0000

    2e1e9aa docs: remove duplicate entry for tagged template literals (#62082)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.3+sha-fecfb89`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.3+sha-2e1e9aa`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
