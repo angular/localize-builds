d90c87fd11a2596ec5ad1cb8599173f63f4ed9ab
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-19 10:49:07 +0000

    a7e4b3e docs(docs-infra): fix code-editor flakiness (#62143)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-f30bd26`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-a7e4b3e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
