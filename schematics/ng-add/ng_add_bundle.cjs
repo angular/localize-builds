8c86e8c0902f7573683003d2e06d8e453576890d
Andrew Kushnir <akushnir@google.com>
2025-06-20 18:17:45 +0000

    3ecfa9c docs: add JetBrains Junie guidelines (#62161)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-ed5062a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-3ecfa9c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
