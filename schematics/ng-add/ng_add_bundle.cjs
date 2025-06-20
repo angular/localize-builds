2015c3b943d4b8796f9fc517a620091fb15dccde
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-20 08:42:13 +0000

    ed5062a fix(compiler-cli): add suggestion when pipe is missing (#62146)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-fa5ae92`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-ed5062a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
