90ae1a9dc61d3365171afbe0ccfb765d8dd5c45e
Andrew Kushnir <akushnir@google.com>
2025-06-20 19:16:26 +0000

    dff1417 build: revert incompatible build changes in patch branch (#62168)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.4+sha-144c429`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.4+sha-dff1417`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
