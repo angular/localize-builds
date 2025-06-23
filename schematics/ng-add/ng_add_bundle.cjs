23ad0651a63c5fe0a68cd4cd6c981f4d1e308f09
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-06-23 12:38:30 +0000

    3eb5a79 fix(compiler-cli): handle initializer APIs wrapped in type casts (#62203)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.4+sha-e909287`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.4+sha-3eb5a79`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
