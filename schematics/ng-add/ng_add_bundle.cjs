ff453ebbd6e0913f70df60eb29368ed45c351f63
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-24 12:37:06 +0000

    7600bec fix(router): handle scrollRestoration error in restricted environments (#62186)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-8d049f5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-7600bec`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
