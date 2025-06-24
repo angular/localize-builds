6a29164bb60b0c195884f9ecc632d281cc2f1d32
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-24 11:01:49 +0000

    8d049f5 build: update jest monorepo to v30 (#62234)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-07df4e1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-8d049f5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
