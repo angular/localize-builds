27f80896b040ce21ec514dc1bba569e9cdb1c29f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 13:32:58 +0000

    a2e6f31 fix(core): allow to set a resource in an error state (#62253)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.4+sha-17a8ca3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.4+sha-a2e6f31`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
