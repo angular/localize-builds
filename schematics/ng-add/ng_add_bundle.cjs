83063ddd21a6e491f390e8dea9d67fa5cf41a2eb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 11:34:49 +0000

    4f0126e docs: fix deprecation message on ngIf/ngSwitch (#62149)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-b489f9f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-4f0126e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
