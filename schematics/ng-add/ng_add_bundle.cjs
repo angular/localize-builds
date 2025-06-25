78c8147b04d7aeec85f746c0724bddb69cc85500
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-25 09:58:59 +0000

    b489f9f refactor(language-service): add methods for decoding classifications to public api (#62251)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-73269cf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.2+sha-b489f9f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
