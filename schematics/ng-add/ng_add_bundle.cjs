1201e6ffe95132ace2892fc5e014e09a80c3b978
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 09:35:07 +0000

    f1deaba ci: temp add flamegraph dep to ignoredDeps (#62287)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-3a002c2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-f1deaba`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
