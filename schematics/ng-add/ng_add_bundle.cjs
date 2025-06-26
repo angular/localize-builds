f5a084672d0ad3e86e50e728b5c615fa6db59f88
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 14:46:19 +0000

    402eaa1 docs: fix deferrable views doc links (#62293)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-13875d0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-402eaa1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
