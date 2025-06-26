cf732969d16ddab26acc13b7a01b1edce8677c11
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 09:10:39 +0000

    3a002c2 docs: Update documentation httpResource with keepalive feature (#62278)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-c5c9b0b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-3a002c2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
