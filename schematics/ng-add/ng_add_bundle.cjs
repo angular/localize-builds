d33c22472a07f18ee52bc7f7f6cff77fa46ad22d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 10:36:45 +0000

    cd30c4d build: update dependency rollup to v4.44.1 (#62285)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-f1deaba`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-cd30c4d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
