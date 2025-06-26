0c2d81948e2f01f158534017fa85631d4d932494
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 12:15:19 +0000

    a3c246a refactor(common): remove redundant `ɵloadImpl` check in client bundles (#62191)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.5+sha-8b861e2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.5+sha-a3c246a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
