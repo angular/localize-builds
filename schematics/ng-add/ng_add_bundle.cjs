9048026fc819589932b85b7fd64d08c09b70436d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-26 17:30:42 +0000

    b848590 build: migrate to use web test runner rules (#62292)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-ec01599`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-b848590`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
