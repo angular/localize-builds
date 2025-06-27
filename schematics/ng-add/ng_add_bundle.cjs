6f47e4ad4a4dddc4733f23bc4df54c900a9c47ce
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 15:10:10 +0000

    27b7ec0 feat(http): add mode & redirect for fetch request in httpResource (#62337)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-b2afa11`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-27b7ec0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
