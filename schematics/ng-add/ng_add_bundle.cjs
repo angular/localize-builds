c47748a3d6ff46660e70e97016478edb68addffb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 12:02:11 +0000

    7077520 docs: add page NG8115 (uninvokedTrackFunction) (#61962)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-599baa2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-7077520`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
