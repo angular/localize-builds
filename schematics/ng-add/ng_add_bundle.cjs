b3d6d84257df7ca422334a816776e5f6c988ce6f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 13:15:48 +0000

    f943505 refactor(devtools): main nav (#62242)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-6ec2900`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-f943505`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
