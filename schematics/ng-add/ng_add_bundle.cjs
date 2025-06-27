f2de8f040cce4ef088c17c97b919d861b9c13db0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 10:39:35 +0000

    74004f6 build: update rules_sass digest to cc1e845 (#62328)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.5+sha-e3103fe`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.5+sha-74004f6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
