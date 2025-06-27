fcbdf2c31922a1b22185caed01b3df231efc9f87
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 09:07:45 +0000

    5ccb81e build: update dependency marked to v16 (#62320)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-df1a61f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-5ccb81e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
