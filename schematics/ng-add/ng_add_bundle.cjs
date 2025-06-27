f5f162bbf8f6f1217a9fbdb833062dfd859254fe
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 07:54:39 +0000

    ac7fe2c docs: remove list of APIs with a providers array. (#62296)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.5+sha-5c2a942`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.5+sha-ac7fe2c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
