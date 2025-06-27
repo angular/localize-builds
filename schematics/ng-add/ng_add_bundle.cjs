558db8d1d94397bd39a011167f1a03f8aaf462c3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-27 18:44:21 +0000

    0866077 ci: add jkrems to pullapprove (public-api) (#62340)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.5+sha-c0532f0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.5+sha-0866077`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
