43f50364acf753aeecfb8467235d3edb66d49f9f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 08:13:54 +0000

    e145a83 docs: update router read-route-state.md file (#62367)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.5+sha-5edbb54`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.5+sha-e145a83`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
