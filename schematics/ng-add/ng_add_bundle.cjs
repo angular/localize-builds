4564461e323cd5161b134f442f9e4dadd8fe6364
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 10:14:34 +0000

    f0965c7 feat(http): Add support for fetch credentials options in HttpClient (#62354)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-d283595`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-f0965c7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
