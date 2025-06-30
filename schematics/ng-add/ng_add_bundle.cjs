163044ae880015e1f13177e2c5c009426aa963da
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 07:51:26 +0000

    08e2d9d build: update rules_browsers digest to 800d91c (#62358)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-059fb06`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-08e2d9d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
