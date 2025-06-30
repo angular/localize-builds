2438bed9cb0456af18a3f30018d9b2c96b7de845
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-06-30 08:31:19 +0000

    493f25b build: update all non-major dependencies (#62355)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-664ed95`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-next.3+sha-493f25b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
