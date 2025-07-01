8d54d13732d711067b0bb1373cf7708c43e7f18c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-01 08:33:43 +0000

    bf881cc build: update github/codeql-action action to v3.29.2 (#62394)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.5+sha-7c82f26`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.5+sha-bf881cc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
