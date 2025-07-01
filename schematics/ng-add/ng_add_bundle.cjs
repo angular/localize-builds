f5b410b12d16f8d94fe1b671c5305320c8bd0012
Andrew Kushnir <akushnir@google.com>
2025-07-01 19:28:47 +0000

    fb1bc99 ci: fix incorrect base branch in renovate config (#62406)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-4ac0147`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-fb1bc99`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
