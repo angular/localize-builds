d0234a3424088fd8857d0cce2faeee1ba481562b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 17:04:57 +0000

    567597a docs: fix formatting (#62400)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-85b3ae2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-567597a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
