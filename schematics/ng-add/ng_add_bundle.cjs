97877390e80e5cfe14eff7068f4db4b6d8d48c3f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 14:56:27 +0000

    42a3dbb docs: Update documentation httpResource with credentials option (#62415)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-rc.0+sha-56769de`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-rc.0+sha-42a3dbb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
