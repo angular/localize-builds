aa98858745658f8b6f93abbb16a31872e8568afe
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 16:18:06 +0000

    85b3ae2 build: migrate devtools to use packaged version of @angular/* packages (#62413)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-0cdb02a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-85b3ae2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
