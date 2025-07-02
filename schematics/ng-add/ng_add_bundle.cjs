e3fe4d57a7d0d573da85d5b6e6bece2604310577
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-02 17:15:32 +0000

    87056c3 refactor(compiler-cli): do not rely on `url` when loading file system logic (#62431)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-rc.0+sha-d5210f0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-rc.0+sha-87056c3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
