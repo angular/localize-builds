20fce6ff04a754585afc9bb567b4872dcf02976e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-03 11:59:57 +0000

    5c54cb8 docs: improve support for decorator APIs (#60411)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-902121e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-5c54cb8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
