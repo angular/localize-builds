ffbc79951618eee853083e526c12de9a611f9a2b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-04 08:56:10 +0000

    9ba5903 refactor(devtools): remove the standalone demo (#62455)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-720717e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-9ba5903`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
