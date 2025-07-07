8369fe14661923d91ceb1fde1bd56a393828e1e4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 20:52:07 +0000

    22b8e22 build: migrate modules to use dependencies from //modules:package.json (#62506)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-ce8156a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-22b8e22`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
