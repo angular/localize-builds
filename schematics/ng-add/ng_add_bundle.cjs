3eac5ab0d6dab74b3579f5551efb68cb3a07a6e1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 21:03:32 +0000

    efc6405 build: update devinfra digest to 6f54d14 (#62508)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-rc.0+sha-aee83d1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0-rc.0+sha-efc6405`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
