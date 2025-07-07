572f25b90f2d3c9a5528a9b89699e338807a732c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-07 17:01:55 +0000

    65b9adf docs: update comment (#62482) PR Close #62482

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-633d01b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-65b9adf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
