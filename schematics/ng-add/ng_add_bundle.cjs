cc47d14342f0c0da873eb106ccd171cb33e5eace
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-07-07 11:09:52 +0000

    5fca07c refactor(compiler-cli): ensure mock file system can be loaded in the browser (#62493)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.6+sha-434508f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.6+sha-5fca07c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
