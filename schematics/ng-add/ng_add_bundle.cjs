18247f73663c385e689e2586663e43a12d9d2bab
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-07-08 11:40:18 +0000

    ce958e0 refactor(compiler): fix import manager to be closure property renaming-compatible (#62517)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.6+sha-403ba91`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.6+sha-ce958e0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
