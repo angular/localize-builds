d00db27ff1d6794e9658d7700f2bf702769433bb
Andrew Kushnir <akushnir@google.com>
2025-07-08 18:38:47 +0000

    d6765d9 fix(docs-infra): fix tutorial formatting issues (for v20) (#62457)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-acdd5d9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-d6765d9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
