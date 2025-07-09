dcb4b0398b35f2307f6f81de9443646bc3b696b4
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-07-09 19:07:30 +0000

    b2076fa release: cut the v20.0.7 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -126,7 +126,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.0.6+sha-ce958e0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.0.7+sha-b2076fa`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
