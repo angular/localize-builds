ba3038cc07783b151b772e10d8f91dd7c11798c7
Andrew Kushnir <akushnir@google.com>
2025-07-10 20:00:33 +0000

    aea0a70 ci: adjust renovate teams (#62578)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0+sha-6549418`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0+sha-aea0a70`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
