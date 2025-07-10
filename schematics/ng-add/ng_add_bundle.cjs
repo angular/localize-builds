d507939bb235035bf4c7b441a6de021cc4785ac6
Andrew Kushnir <akushnir@google.com>
2025-07-10 00:01:19 +0000

    b4ae55a build: use rollup directory instead of rollup through @bazel/rollup (#62556)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0+sha-6274ea7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0+sha-b4ae55a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
