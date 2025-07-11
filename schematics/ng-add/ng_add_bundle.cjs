cc578c22dad3c8e3251ea88937ee8912df476db3
Andrew Kushnir <akushnir@google.com>
2025-07-11 23:16:07 +0000

    1fa10c0 docs: update Angular CDK apis (#62601)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0+sha-db11cdd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0+sha-1fa10c0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
