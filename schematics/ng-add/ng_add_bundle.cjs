310e1177cc721fd9e08487c3bfdafa9d7abf72ae
Andrew Kushnir <akushnir@google.com>
2025-07-14 17:11:30 +0000

    0ce086e docs: update forgotten change to input signals (#62612) PR Close #62612

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0+sha-1fa10c0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0+sha-0ce086e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
