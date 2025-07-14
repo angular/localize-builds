94d369a175c83934ed39cb50602b92aff6c8fffd
Andrew Kushnir <akushnir@google.com>
2025-07-14 23:01:20 +0000

    e8c5603 build: move all rule/macro loading into adev/defaults.bzl and fix intellisense loading (#62627)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-eca2334`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-e8c5603`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
