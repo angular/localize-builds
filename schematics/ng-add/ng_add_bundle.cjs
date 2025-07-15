f103568d030a024f13679db974b38ce60a449711
Andrew Kushnir <akushnir@google.com>
2025-07-15 16:47:39 +0000

    4574095 build: update all non-major dependencies (#62642)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-80da073`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-4574095`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
