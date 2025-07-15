b245fd2756ead7ec9a5a9a6d85320d9207f74939
Andrew Kushnir <akushnir@google.com>
2025-07-15 15:07:20 +0000

    80da073 docs: fix missing 'of' in Style Guide sentence (#62628)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-e8c5603`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.0+sha-80da073`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
