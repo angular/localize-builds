7be8e9180fd00ebb29c23ef1c5bf0d3acf329c45
Kirill Cherkashin <kirjs@google.com>
2025-07-16 20:48:34 +0000

    0db3613 build: remove rules_nodejs based protractor tooling (#62649)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.1+sha-4b9385f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.1+sha-0db3613`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
