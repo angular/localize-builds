7d35df40c11f9b5d2aeda105bcb1ed3564e9005d
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-16 10:52:39 +0000

    9506cdf fix(compiler-cli): infer type of event target for void elements (#62648)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0+sha-70c8780`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.0+sha-9506cdf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
