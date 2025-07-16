86f1810332085a71ce911a647d5d2a55cf9b039f
Kirill Cherkashin <kirjs@google.com>
2025-07-16 20:20:45 +0000

    f5f8f76 release: bump Angular DevTools version to 1.0.39 (#62670)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-a484a11`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-f5f8f76`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
