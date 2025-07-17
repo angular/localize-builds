0420a597e66e3a95d1b3da5fd2946003fb34707b
Kirill Cherkashin <kirjs@google.com>
2025-07-17 18:40:12 +0000

    e7e2dea docs: fixes binding box typo in httpResource.value() template example (#62661)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.1+sha-8ad10fd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.1+sha-e7e2dea`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
