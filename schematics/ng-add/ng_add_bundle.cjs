f3c4d1b2be49000b479df8b7e5b6ec40affe8c12
Kirill Cherkashin <kirjs@google.com>
2025-07-18 11:28:57 +0000

    448a7f7 refactor(core): Use promise.finally for task removal (#62674)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-d3b770d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-448a7f7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
