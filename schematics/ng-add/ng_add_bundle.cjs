6a09c85edbd04ef5f05b4d20bee894c490899e03
Kirill Cherkashin <kirjs@google.com>
2025-07-18 14:16:17 +0000

    409287a docs: split devtools doc to prepare for upcomming updates (#62597)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-ba8a5d8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-409287a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
