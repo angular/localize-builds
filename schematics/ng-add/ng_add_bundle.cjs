43968f9ef1c853968d9f598abd992ca705e92f6c
Kirill Cherkashin <kirjs@google.com>
2025-07-21 17:47:01 +0000

    74e4778 docs: fix minor typos in styleguide (#62706) PR Close #62706

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-b388f1e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-74e4778`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
