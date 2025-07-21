07355cea789b4831b9059b8ae0924ebab1891411
Kirill Cherkashin <kirjs@google.com>
2025-07-21 16:13:24 +0000

    f8c8ff2 docs: update Angular CLI help (#62729)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-8b5f814`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-f8c8ff2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
