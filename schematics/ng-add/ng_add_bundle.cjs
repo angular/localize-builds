e29e505639d5a91e9ab27f33dc4d675488c7799d
Kirill Cherkashin <kirjs@google.com>
2025-07-21 20:43:55 +0000

    d0cf8fc test(devtools): bump cypress timeout to 10 seconds (#62726)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-bb7422a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-d0cf8fc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
