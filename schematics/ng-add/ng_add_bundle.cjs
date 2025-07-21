8ae082fcdb425fc70c06d988b94ae020a64f1f6c
Kirill Cherkashin <kirjs@google.com>
2025-07-21 15:02:23 +0000

    bca8a3c build: update dependency node to v22.17.1 (#62715)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-a1a78b2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-bca8a3c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
