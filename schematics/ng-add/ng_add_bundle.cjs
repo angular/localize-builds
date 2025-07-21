8b6b9d10eeb40be114909a4556fc13a86ea98564
Kirill Cherkashin <kirjs@google.com>
2025-07-21 14:16:20 +0000

    a6b2d7d fix(devtools): add event tagging to prevent DDOS (#62645)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-97b1bd1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-a6b2d7d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
