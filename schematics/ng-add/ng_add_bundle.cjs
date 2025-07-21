5ed53829468b295d20702a14e042bbac9fade13a
Kirill Cherkashin <kirjs@google.com>
2025-07-21 14:30:21 +0000

    a1a78b2 build: update all non-major dependencies (#62716)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-cf48b13`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-a1a78b2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
