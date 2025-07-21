d5c2e74d70ca517adf4f92dc83f51f24e11e6358
Kirill Cherkashin <kirjs@google.com>
2025-07-21 17:18:18 +0000

    b388f1e build: remove all usages of the interop_deps attr for ts_project and ng_project (#62733)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-aee5260`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-b388f1e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
