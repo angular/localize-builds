59129bf819cf644528035dfe63fc3688465cf4b1
Kirill Cherkashin <kirjs@google.com>
2025-07-22 16:39:47 +0000

    91bdd17 build: update @angular/ng-dev digest to 75a2aee (#62745)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-db8f7ce`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-91bdd17`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
