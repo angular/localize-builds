fe25c7830c72d63422f960fa6b5cac301c91ea07
Kirill Cherkashin <kirjs@google.com>
2025-07-23 18:08:45 +0000

    9562b63 docs: guard a resource error with `hasValue()` (#62708)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-593cc8a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-9562b63`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
