e1a9890637ecfdad86a7cb4c0545981e0d2233fa
Kirill Cherkashin <kirjs@google.com>
2025-07-23 11:32:53 +0000

    b7c745b docs: create MCP server setup guide (#62746)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-91bdd17`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.2+sha-b7c745b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
