7897a16d2305460eabe90318236514af2c7c3085
Kirill Cherkashin <kirjs@google.com>
2025-07-23 19:49:49 +0000

    7f9d37d build: update bazel rules_browsers dependency (#62772)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-063b5e1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-7f9d37d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
