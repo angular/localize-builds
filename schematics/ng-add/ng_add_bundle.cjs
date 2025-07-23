4c53056a49d621ea9989a055bb6cef4bfc1f4a94
Kirill Cherkashin <kirjs@google.com>
2025-07-23 17:03:26 +0000

    593cc8a fix(core): checks if body exists before continuing (#62768)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-a4a4f7f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.1+sha-593cc8a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
