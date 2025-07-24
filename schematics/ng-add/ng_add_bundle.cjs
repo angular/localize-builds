f840abd5f84c01c846243ef9341455c366e944c0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 14:09:09 +0000

    da0fb33 docs: add initial documentation for MCP server tools (#62777)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-206d153`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-da0fb33`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
