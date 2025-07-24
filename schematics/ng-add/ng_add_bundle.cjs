3526f011c68c7d2843c4b13aabf27f84d52b3fe2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 09:35:55 +0000

    15670d8 fix(http): propagate plain errors when parsing fails (#62765)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-d53649c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-15670d8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
