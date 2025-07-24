48df2712e2c50d0cf8bc723862d34d355dc0d0e3
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 15:59:04 +0000

    61eeb9b refactor(devtools): simular transfer cache data in the devtools demo app. (#62665)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.2+sha-24e6f4d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.2+sha-61eeb9b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
