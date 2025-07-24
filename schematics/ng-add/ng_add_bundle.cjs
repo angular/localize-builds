61b132794eb6e35088a949f0375836fa302003ce
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-24 16:20:57 +0000

    b11e050 docs(docs-infra): correct typos in adev and wording in resource api (#62801) PR Close #62801

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.2+sha-61eeb9b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.2+sha-b11e050`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
