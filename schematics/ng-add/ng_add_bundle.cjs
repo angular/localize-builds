9b19e3fcc4beb26f5722197c94711f539ad74f7e
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-07-29 04:50:28 +0000

    622e108 build: standardize usage of js_binary (#62860)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-88913b2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-622e108`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
