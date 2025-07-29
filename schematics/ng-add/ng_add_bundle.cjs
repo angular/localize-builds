775ba6a596fffba92d0f485a12c8f0e0470b40dd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 10:02:35 +0000

    f0fca61 refactor(common): replace aio links to adev (#62839)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-cd256ef`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-f0fca61`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
