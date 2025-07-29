ece23018444a3a6e0c04221772577ebdbeac39ff
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-29 12:24:16 +0000

    0b9f53a docs: fix ActivateRoute example (#62855)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-f0fca61`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-0b9f53a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
