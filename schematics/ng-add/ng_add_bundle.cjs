b131a35dee363370c11322b279b5c59ee3197a06
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 09:34:01 +0000

    25c6b15 refactor(core): add private resource error helper function for g3 migration (#62840)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-af53c93`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-25c6b15`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
