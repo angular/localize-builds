335930ff19255f1e9358b1bd447cc727b9a6c918
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 09:47:41 +0000

    7f5d7c9 docs: add updated label to animations (#62893)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.2+sha-6ad01e6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.2+sha-7f5d7c9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
