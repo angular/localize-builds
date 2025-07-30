1bd482bbd25150ea9f8ca42f7451d8d73f1fae18
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 16:00:48 +0000

    280f1f5 test(language-service): update language-service legacy tests (#62900)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-82fcc9c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.3+sha-280f1f5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
