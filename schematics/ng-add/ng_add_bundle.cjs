c5229b13df36fc5070e639bca51e60fd74eaaa29
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 14:13:50 +0000

    245494a fix(http): add missing http options allowed in fetch API (#62881)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.2+sha-320de4e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.2+sha-245494a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
