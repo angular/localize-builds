2ae15b541eb0295f4467fbafbebae7e47e1c33b0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-30 10:06:47 +0000

    9d05a8a docs(docs-infra): increase until timeout (#62894)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.2+sha-7f5d7c9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.2+sha-9d05a8a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
