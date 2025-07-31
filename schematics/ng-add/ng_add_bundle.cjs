08d3341112705ac0e2580616db1b02e40cf3a31b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-07-31 09:22:42 +0000

    8255e0c feat(service-worker): add messageerror event handling and logging (#62834)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.3+sha-78a6b68`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.3+sha-8255e0c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
