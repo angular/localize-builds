7be86251a5afb30da86679c599e91f51c49f1331
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-07-31 08:45:29 +0000

    d08f9ce docs: release notes for the v20.1.4 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.2+sha-e7d748e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.2+sha-d08f9ce`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
