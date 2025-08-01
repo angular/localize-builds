0c45599bb798fd069495e6e6731cd917fe19969f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 08:14:08 +0000

    ae443f8 fix(http): Reset headers, progress, and statusCode when using `set()` in `HttpResource` (#62873)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.4+sha-216d883`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.4+sha-ae443f8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
