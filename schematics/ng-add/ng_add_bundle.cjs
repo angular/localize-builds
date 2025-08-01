a8a5e7c4f42d8ef6cb3b905aa53054c8deda3fb6
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 14:28:38 +0000

    3e6e1c1 docs: Add `MAX_ANIMATION_TIMEOUT` to the animate docs (#62949)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.3+sha-857675f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.3+sha-3e6e1c1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
