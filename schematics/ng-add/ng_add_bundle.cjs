0aa299be5117f24fae872020f4ecd99f9561853d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 14:27:07 +0000

    c2b48be build: move to pnpm version 9.15.9 (#62951)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.4+sha-f700bd9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.4+sha-c2b48be`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
