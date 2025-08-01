5e0b296eb89510443606376bb9750a70ec905c81
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-01 12:44:37 +0000

    a2b4988 build: replace `docs` script with `adev` (#62944)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.3+sha-17b6a5b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.3+sha-a2b4988`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
