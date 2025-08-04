b293c4185cd639a61aa85ac5d22ee0a2aea24072
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-04 09:49:22 +0000

    e0d436a docs: Adds docs on handling redirect info in interceptors (#62968)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.4+sha-c2b48be`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.4+sha-e0d436a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
