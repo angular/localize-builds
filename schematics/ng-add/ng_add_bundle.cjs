878dcbd3155205953bebf5e0a8af04bcd3e2201a
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-04 10:29:00 +0000

    6597ac0 fix(core): fix support for space separated strings in leave animations (#62979)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.3+sha-cbd300d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.3+sha-6597ac0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
