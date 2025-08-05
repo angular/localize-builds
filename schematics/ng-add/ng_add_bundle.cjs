1785506d9df329698e55b3570b0f5600f3b80224
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-05 08:21:56 +0000

    5802f80 build: no longer rely on archive packages for integration tests (#62996)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.3+sha-6597ac0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.3+sha-5802f80`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
