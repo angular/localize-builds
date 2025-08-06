9644d4d6c8b69d1c6f04f6f543d5aaf00529e99b
Andrew Scott <atscott01@gmail.com>
2025-08-06 19:09:41 +0000

    9365a7f docs: release notes for the v20.1.6 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.4+sha-ecc86e2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.4+sha-9365a7f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
