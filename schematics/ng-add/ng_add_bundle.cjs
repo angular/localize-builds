138aee8d6bf1e00041804f784972a5803e23beda
Andrew Scott <atscott@google.com>
2025-08-07 16:16:07 +0000

    e524b33 docs(devtools): update `pnpm` install command (#63040)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.5+sha-3a320bd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.5+sha-e524b33`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
