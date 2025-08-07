c08b73ae6813c22831ca845708857776de154956
Andrew Scott <atscott@google.com>
2025-08-07 16:35:50 +0000

    0d12eac build: remove unused old toolchain dependencies (#63039)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.5+sha-e524b33`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.5+sha-0d12eac`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
