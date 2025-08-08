9ccb886953992474ceac099644d9f7f215540018
Andrew Scott <atscott@google.com>
2025-08-08 15:56:23 +0000

    322042c fix(core): destroying the effect on `afterRenderEffect` (#63001)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.6+sha-bba7c6f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.6+sha-322042c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
