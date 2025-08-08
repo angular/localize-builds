80aea90c72d3bf0e48147beaf6ac76e965b38461
Andrew Scott <atscott@google.com>
2025-08-08 18:12:04 +0000

    3b214d2 feat(service-worker): Logs unhandled promise rejections in service worker (#63059)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.6+sha-6a11846`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.6+sha-3b214d2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
