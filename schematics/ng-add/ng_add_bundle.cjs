4f09eea1b4b7749977fe7019948922f8fc5ab0c9
Andrew Scott <atscott@google.com>
2025-08-08 16:02:38 +0000

    c0bfe23 refactor(devtools): save theme preference (#63050)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.6+sha-85d51a3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-next.6+sha-c0bfe23`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
