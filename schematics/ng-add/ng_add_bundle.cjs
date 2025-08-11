0528e2b89d754523792b4f4894c54c9745e00fe4
Andrew Scott <atscott@google.com>
2025-08-11 19:20:55 +0000

    fdce7e2 docs: remove router testing from the testing scenarios guide (#63078)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.6+sha-d9e3790`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.6+sha-fdce7e2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
