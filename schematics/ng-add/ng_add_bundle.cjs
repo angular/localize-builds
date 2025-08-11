6250444c2869ec4580203617befbbdbcd3f79fd0
Andrew Scott <atscott@google.com>
2025-08-11 22:48:14 +0000

    af247ca refactor(core): remove temporary resource setter function. (#63097)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.0+sha-499fa8f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.0+sha-af247ca`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
