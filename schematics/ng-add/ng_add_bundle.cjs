e96633be9aeec3364eb37a0e07460f3189a9254f
Andrew Scott <atscott@google.com>
2025-08-11 19:03:31 +0000

    79b9159 fix(compiler): incorrect spans for AST inside input value with leading space (#63082)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.0+sha-5180161`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.0+sha-79b9159`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
