81472b4baead7bf3bca2876c981b6afe81d6eb74
Andrew Scott <atscott@google.com>
2025-08-12 16:44:42 +0000

    f251c7d docs: add missing supported `!=` operator (#63108)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.6+sha-e6c84a4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.6+sha-f251c7d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
