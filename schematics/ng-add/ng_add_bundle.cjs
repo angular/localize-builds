3f9f66a4e8a36f49ad69d857fd249a43a4c1a27c
Andrew Scott <atscott01@gmail.com>
2025-08-12 20:28:22 +0000

    ea78278 refactor(compiler-cli): ensure JIT transforms with queries work with closure property renaming (#63123)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0-next.0+sha-e64271d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0-next.0+sha-ea78278`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
