fc497056fd614b75de77b8fd4d96e2d723977ff3
Andrew Scott <atscott@google.com>
2025-08-12 18:05:28 +0000

    b13c3fc build: update actions/checkout action to v5 (#63115)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.0+sha-31df4d2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.0+sha-b13c3fc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
