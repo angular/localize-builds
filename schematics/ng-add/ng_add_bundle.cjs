91f4453255e52bca0bdf7ce949277c396327cf72
Andrew Scott <atscott01@gmail.com>
2025-08-13 01:50:52 +0000

    657061e build: patch `@angular/build` to allow fw/cli mismatch (#63098)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0-next.0+sha-4d9385b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.0+sha-657061e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
