780004aa72ab6a80b93e764459390d3697abb5f4
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:25:07 +0000

    254b560 docs: update best-practices (#63132)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.0+sha-0065ebb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.0+sha-254b560`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
