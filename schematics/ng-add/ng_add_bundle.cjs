63774dd09ae3d6f3025b8f3460cb68922a45a818
Andrew Scott <atscott01@gmail.com>
2025-08-13 16:19:25 +0000

    0065ebb docs: add link to ai-config command (#63139)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.0+sha-0211668`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.0+sha-0065ebb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
