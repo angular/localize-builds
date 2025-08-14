5ceb435c6607d6bfab1b2e23e57a55cacfaeae8d
Joey Perrott <josephperrott@gmail.com>
2025-08-14 21:19:42 +0000

    8b02aa0 ci: redirect adev to `index.csr.html` (#63168)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.1+sha-c68a099`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.1+sha-8b02aa0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
