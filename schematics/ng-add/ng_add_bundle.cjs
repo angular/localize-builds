ece48f72f97d049e91da70398f5a1f2b75d6d1b6
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-14 11:16:58 +0000

    4bed062 feat(http): Provide http services in root (#56212)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.0+sha-e7cc89e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.0+sha-4bed062`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
