5a30a2178b62533b20754fbba75d7d80c5b59d6e
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-18 10:57:50 +0000

    bc3798d ci: mark myself as unavailable during OOO (#63214)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.7+sha-5d31e20`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.7+sha-bc3798d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
