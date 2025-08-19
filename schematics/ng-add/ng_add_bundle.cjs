61d705b908c5e345d0a9e599ee825eae28d4a0f7
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-19 08:34:05 +0000

    9e26158 refactor(core): update NG0303 error message (#63222) Ensure consistency in error message wording by aligning the NG0303 error with other Angular error strings. This improves clarity and maintains a uniform developer experience

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.1.7+sha-d81baf6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.1.7+sha-9e26158`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
