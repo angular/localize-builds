fcbefd929580516ef9aa63c627838fa198ecaece
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 15:47:40 +0000

    906bce0 docs: add ai design patterns to adev/ai (#63095)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0+sha-a827f02`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0+sha-906bce0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
