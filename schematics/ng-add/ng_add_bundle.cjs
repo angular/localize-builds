2b307c708ffaf380ee292d6d84337c85fca4b5ea
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 16:12:49 +0000

    009b068 build: reenable firefox tests for devtools (#63288)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.0+sha-1c5fb1e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.0+sha-009b068`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
