79296c8d787f67cb8c220b2c08c39631181ad153
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-20 11:19:52 +0000

    9655ea7 build: update dependency playwright-core to v1.55.0 (#63273)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.1+sha-d76a911`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0-rc.1+sha-9655ea7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
