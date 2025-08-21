1e489de2803bc105a3cb5fb39ec81ad1b30a6a08
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-08-21 10:26:33 +0000

    a28672f fix(compiler): Keep paraenthesis in Nullish + Boolean expression. (#63292)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0+sha-65c5a73`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.0+sha-a28672f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
