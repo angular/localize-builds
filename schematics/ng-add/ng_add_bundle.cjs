139068e33b97fa8232c0314cc9ce9e8acb6c21b0
Andrew Kushnir <akushnir@google.com>
2025-08-22 19:32:09 +0000

    e1481d8 ci: add myself to review fw-general (#63337)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.1+sha-e8aa7d3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.1+sha-e1481d8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
