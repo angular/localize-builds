5093d99f23c8fcc789482b44a2a45f97643d289e
Andrew Kushnir <akushnir@google.com>
2025-08-26 21:58:04 +0000

    23d1ace refactor(core): Create a base effect interface and prototype to be used by both angular and wiz. (#62931)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.1+sha-e220a61`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.1+sha-23d1ace`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
