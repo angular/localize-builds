7e4731ad48e4a9cc494c520aa4b81a61a6b7b483
Andrew Kushnir <akushnir@google.com>
2025-08-26 16:50:37 +0000

    a038840 fix(compiler): fixes animations on elements with structural directives (#63390)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.0+sha-aff8bb2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.0+sha-a038840`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
