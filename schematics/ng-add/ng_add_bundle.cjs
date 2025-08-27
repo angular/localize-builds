f9c8d26ab0c8604d9f190025819bcfd240f4c592
Miles Malerba <mmalerba@google.com>
2025-08-27 23:22:55 +0000

    9de2f07 docs: add customizing route behavior guide for router (#63398)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.2+sha-cd748aa`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.2+sha-9de2f07`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
