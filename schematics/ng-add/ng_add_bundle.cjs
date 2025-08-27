421904c2b3549b0e1026b86c5fe36f164fb0a4cc
Andrew Kushnir <akushnir@google.com>
2025-08-27 14:40:36 +0000

    f4f5a8a ci: pin create-pull-request version to sha (#63409)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.0+sha-a43057c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.0+sha-f4f5a8a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
