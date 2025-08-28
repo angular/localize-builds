9fbf78c9f70f71c7999779be06af805379189cf4
Miles Malerba <mmalerba@google.com>
2025-08-28 16:15:56 +0000

    cac0852 docs: update Angular CLI help (#63445)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-b8314bd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-cac0852`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
