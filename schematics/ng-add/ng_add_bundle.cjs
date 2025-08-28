efc25f619e3856c6a28d7e56eee17c5a1ec86e24
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-28 10:24:50 +0000

    690405b docs: Add docs service worker to new options (#63436)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.2+sha-fe533cd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.2+sha-690405b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
