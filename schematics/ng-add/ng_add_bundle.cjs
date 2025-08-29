805165c490d2c7d54caa8edb0b2e4e2d1165acf0
Miles Malerba <mmalerba@google.com>
2025-08-29 16:00:14 +0000

    8dbeac1 build: update cross-repo angular dependencies (#63478)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-1abf6d5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-8dbeac1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
