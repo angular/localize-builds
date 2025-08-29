22ffa853af3b3a715896b3570e61a006b547404a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 11:57:15 +0000

    0a82138 fix(compiler): fixes regression with event parsing and animate prefix (#63470)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-1a26fd3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-0a82138`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
