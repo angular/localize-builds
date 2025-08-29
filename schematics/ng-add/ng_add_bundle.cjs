c2aad2e87155b2d627b067f8fa19bf752caebb0b
Miles Malerba <mmalerba@google.com>
2025-08-29 22:16:09 +0000

    dddce83 ci: add more team members as approvers (#63495)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-e9096b9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-dddce83`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
