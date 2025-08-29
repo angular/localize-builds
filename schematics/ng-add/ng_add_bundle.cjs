b62f08bd0eae4987a64bf8c0a104af728e965afb
Miles Malerba <mmalerba@google.com>
2025-08-29 21:35:26 +0000

    862495c build: configure signal forms for release (#63458)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-e75b04f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-862495c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
