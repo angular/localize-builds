0149c9bf0885b8b145e343cda5c2ae1633721c20
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-08-29 13:23:43 +0000

    3fb02ed build: update google-github-actions/auth action to v3 (#63461)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-a3938d5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-3fb02ed`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
