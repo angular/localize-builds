58ac66b035d2174e7d7955db7e1e450f212a3a0e
Miles Malerba <mmalerba@google.com>
2025-08-29 21:10:19 +0000

    e75b04f docs(docs-infra): properties should be readonly (#63492)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-bbb46e5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-e75b04f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
