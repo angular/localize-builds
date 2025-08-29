3915de48b6568de5b874209d7655ec90ac150e75
Miles Malerba <mmalerba@google.com>
2025-08-29 19:04:39 +0000

    5bb4b75 docs: fix rxResource example (#63484) PR Close #63484

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-8dbeac1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-5bb4b75`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
