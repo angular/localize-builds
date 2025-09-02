413c46bda1919318b50d84a5fe981f6d8e55c72a
Miles Malerba <mmalerba@google.com>
2025-09-02 16:48:55 +0000

    d6f7f68 docs(docs-infra): signal property should be readonly (#63166)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-3b42314`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-d6f7f68`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
