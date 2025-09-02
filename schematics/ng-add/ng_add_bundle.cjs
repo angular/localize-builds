60da093393d27d0cdca59a287afa9ee4f1e6ae45
Miles Malerba <mmalerba@google.com>
2025-09-02 16:53:11 +0000

    c664af8 docs(docs-infra): highlight & parse api descriptions. (#63536)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-d6f7f68`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-c664af8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
