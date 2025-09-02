dcd4a0124bc88efe180ef2709003236238a9cb24
Miles Malerba <mmalerba@google.com>
2025-09-02 17:14:33 +0000

    957b367 build: update github/codeql-action action to v3.30.0 (#63540)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-c664af8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-957b367`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
