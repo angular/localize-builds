9b7e4093fa7f0fe6d8feefbfe1be28cb2ff0c098
Miles Malerba <mmalerba@google.com>
2025-09-02 18:56:46 +0000

    4c36659 refactor(router): Add handling for empty observables in guards (#63546)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-92e09ad`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-4c36659`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
