43d0a3cacef7cd0e5fc746b0dae97cd35abf1276
Miles Malerba <mmalerba@google.com>
2025-09-02 19:43:04 +0000

    4fb8c46 docs: update Angular CLI help (#63552)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-4c36659`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-4fb8c46`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
