33ac375444286cae5e2bb7cbf25d47ef262b3a1a
Miles Malerba <mmalerba@google.com>
2025-09-02 16:54:37 +0000

    7aa9350 build: use cldr typings (#63519)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-e63608d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-7aa9350`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
