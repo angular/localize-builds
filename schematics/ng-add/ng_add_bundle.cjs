6dd920e1f7f02b321332e1f2ce797b5cd3942e05
Andrew Scott <atscott@google.com>
2025-09-03 16:56:47 +0000

    320f1d6 build: update @angular/ng-dev digest to 435c59b (#63571)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-991785f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-320f1d6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
