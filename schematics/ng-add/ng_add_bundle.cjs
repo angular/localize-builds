50134ab8212f9cac96a3225e61470754fb8d68e6
Miles Malerba <mmalerba@google.com>
2025-09-03 19:40:33 +0000

    08d363e release: cut the v20.2.4 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-320f1d6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.4+sha-08d363e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
