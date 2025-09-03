c3fd47bf596ccd108dffe7c8493370d9b093c812
Miles Malerba <mmalerba@google.com>
2025-09-03 16:20:22 +0000

    0c2255b refactor(platform-server): simplify transfer state serialization tracking (#63525)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-f4a2d0a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.3+sha-0c2255b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
