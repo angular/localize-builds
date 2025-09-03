915d624994602c02d5d4308052256ee7817b9620
Miles Malerba <mmalerba@google.com>
2025-09-03 16:04:57 +0000

    e6949ad docs: fix #63555 (#63560)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-d006721`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.1+sha-e6949ad`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
