cbff8838b96bc46208d2313bc59f92da90281314
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-09-04 11:51:20 +0000

    948b811 release: bump Angular DevTools version to 1.1.44 (#63596)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.2+sha-835aad7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.2+sha-948b811`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
