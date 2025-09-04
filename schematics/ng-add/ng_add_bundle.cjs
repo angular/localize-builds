83d0ab597283369b85a11479921a0d948c3ad11a
Andrew Scott <atscott@google.com>
2025-09-04 22:24:58 +0000

    e1dd3b1 docs: uses signal, implement TODOs, and fix typos (#63603)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.4+sha-a3f808d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.4+sha-e1dd3b1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
