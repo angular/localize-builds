8497e026c17c7beebc6500747053c42803966dbf
Andrew Scott <atscott@google.com>
2025-09-08 19:49:32 +0000

    1078954 build: properly override the repo for rules_angular (#63584)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.2+sha-1f21de8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.2+sha-1078954`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
