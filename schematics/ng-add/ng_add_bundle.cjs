08a90cdd1b12e0a8c12c854b8a9f55e40b7041ac
Andrew Scott <atscott@google.com>
2025-09-08 21:27:01 +0000

    9943230 docs: remove installation (#63508)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.4+sha-35a1d5d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.4+sha-9943230`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
