22da6bd8109abecaa03a6f79261b6e4c38b35a45
Andrew Scott <atscott@google.com>
2025-09-08 23:35:55 +0000

    c7b84f5 docs: Update Language Service IDE list (#63647)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.4+sha-a857f72`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.4+sha-c7b84f5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
