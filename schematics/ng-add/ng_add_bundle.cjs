cf98b79432a7f46802d7ea47196a713a16bace46
Andrew Scott <atscott@google.com>
2025-09-09 19:14:44 +0000

    0c8c855 docs: add missing drag & drop docs (#63619)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.4+sha-e097269`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.4+sha-0c8c855`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
