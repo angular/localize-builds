e816f99d8bdb9353bc2c41ce5be60f806de05d42
Andrew Scott <atscott@google.com>
2025-09-09 21:33:30 +0000

    739b091 docs: add missing component harness docs (#63620)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.2.4+sha-0214e98`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.2.4+sha-739b091`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
