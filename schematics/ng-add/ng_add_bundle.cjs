b473dd072e236da5cc7e4bb02594ed3867eb65cf
Andrew Scott <atscott@google.com>
2025-09-10 17:18:07 +0000

    48fee39 build: update `@angular/ng-dev` to latest SHA (#63695)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-4f69e03`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-48fee39`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
