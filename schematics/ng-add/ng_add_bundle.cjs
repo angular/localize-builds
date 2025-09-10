2d258123669e1d245b79f0210d51ff7f3167d483
Andrew Scott <atscott@google.com>
2025-09-10 17:13:27 +0000

    802dbcc fix(core): prevent animation events from being cleaned up on destroy (#63414)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-9096d45`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-802dbcc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
