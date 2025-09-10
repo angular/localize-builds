8da4119b6e649bcd0d99c36166bc8565bf874c3d
Andrew Scott <atscott@google.com>
2025-09-10 14:33:51 +0000

    08f01ac build: address issues with bazelrc file (#63684)

--- schematics/ng-add/ng_add_bundle.js
+++ schematics/ng-add/ng_add_bundle.js
@@ -145,7 +145,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~18.2.13+sha-32512de`);
+  return (0, import_utility.addDependency)("@angular/localize", `~18.2.13+sha-08f01ac`);
 }
 function ng_add_default(options) {
   var _a;
