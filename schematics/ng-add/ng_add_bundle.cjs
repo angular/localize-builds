a868d2050d6fa02452724632d9254883fe4d3c75
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-10 23:12:55 +0000

    c7fd7f9 docs: update mcp guide (#63664)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-5f5828f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-c7fd7f9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
