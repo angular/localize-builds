470be84612c1ff15ce623d1cec29161ae193bd7a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 15:44:41 +0000

    c36cd73 ci: correctly set branchName for the CDK (#63718)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-32ff9eb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-c36cd73`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
