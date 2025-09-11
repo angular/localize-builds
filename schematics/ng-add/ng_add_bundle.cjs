8aeb2cb424ca5e61f46278a9c544d37cca81a7d9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-11 15:40:13 +0000

    37e1eda build: mark `pnpapi` as external for esbuild (#63723)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-cdf5d67`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-37e1eda`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
