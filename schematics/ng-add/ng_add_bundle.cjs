88db61c4a4a52753aa49143cfbb696b54163b4b1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 21:49:30 +0000

    2ebdd57 build: update branches and versioning information (#63772)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-9def492`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-2ebdd57`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
