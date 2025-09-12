cfc13a808d3bcdc1070e8d89e7baec25a8e08175
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 16:08:50 +0000

    9de50b1 docs(docs-infra): move the navigation related files (#63729)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-895f0fb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-9de50b1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
