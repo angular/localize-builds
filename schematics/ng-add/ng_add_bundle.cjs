17e1dd110eadea1711bfab7d7fdfc1a1703fe13b
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-12 21:45:35 +0000

    62ad4db refactor(core): Reorganize and cleanup animations code (#63775)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-bd2a84b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-62ad4db`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
