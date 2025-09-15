33c02926ecac55b9bf11318c7318f1fc1b782365
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 15:07:28 +0000

    c536035 docs: correct typos in AI design patterns guide (#63788)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-d191926`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-c536035`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
