dde517be53f4dadc887f164f64ef816e6709ba10
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 21:16:49 +0000

    7e0cf41 refactor(core): wrap `checkNoChanges` body in `if (ngDevMode)` for better tree-shaking (#63387)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-321eecc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-7e0cf41`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
