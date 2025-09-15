b3073e29ec83beaa7b28e9c3c82138bddb64d4fa
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-15 18:30:02 +0000

    a5e5dbb refactor(core): remove `moduleId` from Component metadata (#63482)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-2473931`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-a5e5dbb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
