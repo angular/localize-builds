359ba4dfa08f1a18262f4f59607d3671da2998d0
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 15:22:26 +0000

    444fd01 docs(docs-infra): Use port 4201 for adev (#63834)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-27b2a4a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-444fd01`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
