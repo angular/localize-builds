bc1c74cd6f77d29e9d106ad384ffbd0f0312c5c1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 17:41:14 +0000

    f612437 docs(docs-infra): Show package name for API references (#63831)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-83dbf0e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-f612437`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
