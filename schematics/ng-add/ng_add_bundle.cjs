1e80e849c2791cc4829048db3a55ffb1946e7037
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 14:58:06 +0000

    7eb5f9a docs: remove leading `$` from script examples (#63795)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-4a23f6e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-7eb5f9a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
