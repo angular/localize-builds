a69f1a187e3ff7c91d10f8ad66f727052982bbc4
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-16 15:05:15 +0000

    c80d9ba docs: update doc to clear up difference between `toObservable`/ `outputToObservable` (#63836)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-7eb5f9a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.0+sha-c80d9ba`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
