cf47e339f9e88aec37403aa930a65bf79d544367
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 17:45:49 +0000

    dee6ef8 refactor(core): tree-shake `HOST_TAG_NAME` in prod (#63861)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-36b542d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-dee6ef8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
