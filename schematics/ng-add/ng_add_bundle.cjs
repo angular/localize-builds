e537a96e4f4f66e764b4d48f9827ad8be675f146
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 21:19:17 +0000

    ef57d61 docs: add web codegen scorer to docs (#63886)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-2a15d78`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-ef57d61`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
