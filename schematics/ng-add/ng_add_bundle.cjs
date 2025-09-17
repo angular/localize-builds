425284a01f5b0697d68b6af842791d229f86f614
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-17 15:27:43 +0000

    af33647 refactor(core): remove usages of Promise.withResolvers (#63856)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-e523384`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.3+sha-af33647`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
