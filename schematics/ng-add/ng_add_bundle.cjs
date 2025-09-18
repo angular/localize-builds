5c7ca76495eb4633e2b70eb328362dd2c062b113
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-18 20:22:40 +0000

    307e4ea refactor(migrations): remove common module if there are no references (#63892)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.4+sha-e426302`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.4+sha-307e4ea`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
