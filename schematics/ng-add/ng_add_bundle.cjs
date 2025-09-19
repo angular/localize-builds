6251ceeff3b03869a0ec3c9981f7a6574e58f12a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 22:17:43 +0000

    923c81c docs: remove api grouping toggle (#63961)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-dbb8dbc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-923c81c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
