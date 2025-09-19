17d0f2ac909eafb21d0b45263dba47ba66112541
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-19 20:25:28 +0000

    e835c35 docs: Add categories to form apis (#63938)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.4+sha-0a4ad98`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.4+sha-e835c35`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
