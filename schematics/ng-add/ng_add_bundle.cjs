46bbc7a81a7fa459bd669f1c47d392a215d423c9
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-22 14:45:37 +0000

    a2d98cc docs: add missing `signal` function (#63974)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.4+sha-6782f71`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.4+sha-a2d98cc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
