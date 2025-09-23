3e12ad0290b82fe0d55cd74d790bdd26a38f1c45
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-23 21:43:55 +0000

    f36a504 build: update pnpm to v10.17.1 (#64015)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.4+sha-4230442`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.4+sha-f36a504`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
