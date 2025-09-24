1c5f3217b472a42fedc9b9154fd2719e71aa68ec
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 14:34:38 +0000

    301155b docs(docs-infra): tutorial navigation list component (#64027)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-522587d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-301155b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
