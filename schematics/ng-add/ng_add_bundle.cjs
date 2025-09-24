60fc1969e11a3ae5fdb1f044334d47a700c53e8a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 20:28:20 +0000

    f04a78a docs(docs-infra): scroll offset for headings (#64039)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-ce6c7d9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-f04a78a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
