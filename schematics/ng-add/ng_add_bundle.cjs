369994fbc40a087773a999ee050f9f9ba5bb0b92
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 16:06:16 +0000

    3ebaecc fix(migrations): handle reused templates in control flow migration (#63996)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-3c14d05`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-3ebaecc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
