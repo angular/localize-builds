ae1da41aa13600ad3d8b3ac952c95d64b067bb99
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 16:53:14 +0000

    1fd8d5d fix(forms): Emit `FormResetEvent` when resetting control (#64034)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-3ebaecc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-1fd8d5d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
