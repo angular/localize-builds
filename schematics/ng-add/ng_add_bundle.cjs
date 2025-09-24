f90ee37c74bc983bc05ecb609ca5b7c4c5777ed8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 14:29:45 +0000

    522587d docs: not external link (#64025)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-71955dc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-522587d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
