48baf45c9a9df2d54890169e83a599287e7a109e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-09-24 21:27:43 +0000

    c525a5a build: update dependency node to v22.20.0 (#64061)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-f04a78a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.1+sha-c525a5a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
