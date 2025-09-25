e8a33f8d97a4d060b646ea2d7c4c7efd88b30014
kirjs <kirts@google.com>
2025-09-25 18:19:58 +0000

    1dee0d3 build: add `.vscode` files back to the `.gitignore` (#64059)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-fe0e5ab`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-1dee0d3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
