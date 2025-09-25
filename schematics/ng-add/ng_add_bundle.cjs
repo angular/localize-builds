77610b1294a6b61a0348fd8dd73425863ca914dc
kirjs <kirts@google.com>
2025-09-25 20:34:14 +0000

    e2fd79b docs: add warning to `AbstractControl.setErrors` (#64063)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-6fee508`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-e2fd79b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
