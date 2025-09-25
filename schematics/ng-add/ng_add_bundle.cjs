4f3679fc8dea51d6a2482e93572e97ea451fa1bb
kirjs <kirts@google.com>
2025-09-25 14:41:21 +0000

    076ccbf refactor(devtools): remove hard newlines from reviewer note (#64062)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.2+sha-31f2030`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.2+sha-076ccbf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
