bcce180449154d54206beceea545bae33294e093
kirjs <kirts@google.com>
2025-09-25 18:32:41 +0000

    d7dead3 docs: fix homepage width (#64087)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-cd23761`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-d7dead3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
