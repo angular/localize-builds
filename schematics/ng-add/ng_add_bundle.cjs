8059751ddd33b2bd291848b3934857eb0901e74e
kirjs <kirts@google.com>
2025-09-26 13:59:36 +0000

    238ccc7 docs: update Angular CLI help (#64103)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-ee02976`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-238ccc7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
