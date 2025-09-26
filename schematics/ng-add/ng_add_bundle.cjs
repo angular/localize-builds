e23f590de570eab9a63f69da302ab0282e501009
kirjs <kirts@google.com>
2025-09-26 13:37:28 +0000

    8ff536b docs: update text (#64095)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-14d5b0e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-8ff536b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
