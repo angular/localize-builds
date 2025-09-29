994ee4b71b4fa7287dc4676ed8f24e232375643c
kirjs <kirts@google.com>
2025-09-29 13:57:32 +0000

    e6c0d7e build: update github/codeql-action action to v3.30.5 (#64121)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.2+sha-c0bb528`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.2+sha-e6c0d7e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
