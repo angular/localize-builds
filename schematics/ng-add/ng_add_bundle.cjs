e3dbf2db31e9af56d89f852eee5534bde303f690
kirjs <kirts@google.com>
2025-09-29 19:41:13 +0000

    04462ed refactor(compiler): Remove the interpolation config (#64071)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-fc643c9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-04462ed`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
