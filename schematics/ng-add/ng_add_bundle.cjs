af429d335373e337ebe4da9141378ca8e73aad1b
kirjs <kirts@google.com>
2025-09-29 17:36:07 +0000

    aa28aa2 docs: Adds guide for custom HTTP parameter encoding (#64119)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.2+sha-4b3f7a0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.2+sha-aa28aa2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
