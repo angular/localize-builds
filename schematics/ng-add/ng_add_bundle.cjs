1d7211b615e127c224670fba46fc666cb76838d9
kirjs <kirts@google.com>
2025-09-30 17:53:54 +0000

    2aca6da refactor(core): Add custom formatters for Signals (#64000)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-899c25f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-2aca6da`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
