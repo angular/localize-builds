9b9b656c1989e5b0503bbfb69c068fafd30b2807
kirjs <kirts@google.com>
2025-09-30 16:39:03 +0000

    20bb459 build: update dependencies and build config (#64154)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-563bc38`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-20bb459`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
