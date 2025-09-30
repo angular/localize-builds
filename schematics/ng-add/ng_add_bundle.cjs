a075944a547c0f7e32eb06f0b29ab0421d8d6469
kirjs <kirts@google.com>
2025-09-30 15:09:28 +0000

    563bc38 docs(docs-infra): fix app-scroller offset. (#64144)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-a10eb38`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-563bc38`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
