19b12a78267996a8f797706e1169254db5101a8f
kirjs <kirts@google.com>
2025-10-01 13:15:33 +0000

    0e928fb fix(core): Fixes animations in conjunction with content projection (#63776)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.2+sha-24c979d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.2+sha-0e928fb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
