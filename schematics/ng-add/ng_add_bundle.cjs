0f8b6f5a82bfcf9379f2c9077cc5feacb787ddad
kirjs <kirts@google.com>
2025-10-01 15:57:59 +0000

    1b102c2 build: update dependency @types/jsdom to v27 (#64165)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-2f6caef`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.5+sha-1b102c2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
