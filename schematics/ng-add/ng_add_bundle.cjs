8cb9058941298b5c0da7035154641c5f5621c199
Miles Malerba <mmalerba@google.com>
2025-10-02 15:18:30 +0000

    cb56fbf fix(core): handle undefined CSS time values in parseCssTimeUnitsToMs function (#64181)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.6+sha-595c668`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.6+sha-cb56fbf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
