5e253ae8624a4e864f1df27146a7befa14b7b72d
Miles Malerba <mmalerba@google.com>
2025-10-02 20:07:33 +0000

    8146fc2 test: fix vscode language service e2e tests (#64197)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.6+sha-5e61e8d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.6+sha-8146fc2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
