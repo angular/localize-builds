ccfafefa8967df6f6ab54bc18cd7f86f07ff72fe
Miles Malerba <mmalerba@google.com>
2025-10-03 14:55:18 +0000

    e0b9d9b build: update all non-major dependencies (#64218)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.6+sha-d3e76b1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.6+sha-e0b9d9b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
