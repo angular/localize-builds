5520e4cafef0dd3c75372189d793fe144bae3b64
Miles Malerba <mmalerba@google.com>
2025-10-03 20:35:02 +0000

    853ed16 fix(core): ensure missing leave animations don't queue leave animations (#64226)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.3+sha-28668a5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.3+sha-853ed16`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
