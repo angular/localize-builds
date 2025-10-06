a5cfb91bc085665880b784e0c84f69325e5eb461
Miles Malerba <mmalerba@google.com>
2025-10-06 19:25:06 +0000

    c63de70 docs: add documentation for routerOutletData input and its usage (#64242)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.3+sha-8528bb5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.3+sha-c63de70`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
