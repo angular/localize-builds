e83184dbacb33ff367fe70902e1292aaac8c50db
Andrew Kushnir <akushnir@google.com>
2025-10-09 12:25:07 +0000

    e8de1b4 refactor(docs-infra): replace @Input with signal input for ignoredElementsIds in ClickOutside directive (#64302)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.4+sha-28b0d53`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.4+sha-e8de1b4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
