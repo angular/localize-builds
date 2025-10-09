f5cc605f797f6f3bcadd0318da88098239d873e3
Andrew Kushnir <akushnir@google.com>
2025-10-09 03:12:34 +0000

    e873c22 build: clean up language service integration tests and Bazel dependencies (#64271)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.7+sha-808d6ae`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.7+sha-e873c22`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
