d6e5676e7f945d23d44ff40ff29197f96553b077
Andrew Kushnir <akushnir@google.com>
2025-10-13 15:47:06 +0000

    84f6e36 build: update pnpm to v10.18.2 (#64358)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.7+sha-e34776a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.7+sha-84f6e36`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
