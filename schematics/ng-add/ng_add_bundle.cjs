1b36d63d853928ba740ddfd1b19c207df395a903
Andrew Kushnir <akushnir@google.com>
2025-10-13 16:32:46 +0000

    09a927a ci: remove `cache-node-modules` from `checkout-and-setup-node` (#64387)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.7+sha-62cda78`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.7+sha-09a927a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
