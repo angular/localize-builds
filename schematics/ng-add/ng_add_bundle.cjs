d29d1010863b031f6e35275f6e96bce7c5e76331
Andrew Kushnir <akushnir@google.com>
2025-10-14 20:58:38 +0000

    c2e817b perf(compiler-cli): fix performance of "interpolated signal not invoked" check (#64410)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.4+sha-285dad4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.4+sha-c2e817b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
