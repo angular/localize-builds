58d21d29fe785d05a262c8ba77c803b55246ca21
Andrew Kushnir <akushnir@google.com>
2025-10-14 16:42:42 +0000

    11b006e refactor(compiler-cli): add a resource debugName transform (#64172)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.4+sha-a2773b5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.4+sha-11b006e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
