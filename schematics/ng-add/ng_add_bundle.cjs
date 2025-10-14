16305653a787767f8cc75c78b5ed8a18ca3c490b
Andrew Kushnir <akushnir@google.com>
2025-10-14 19:03:05 +0000

    5b0363c refactor(core):  warning when `hydration` trigger is used without hydration being enabled (#64185)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.4+sha-11b006e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.4+sha-5b0363c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
