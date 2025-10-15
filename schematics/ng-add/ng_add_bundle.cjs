a306a4fc01f154bc017ec268babfeef40419c02e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-15 21:17:52 +0000

    29c397f fix(platform-browser): `DomEventsPlugin` should always be the last plugin to be called for `supports()`. (#50394)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-9b9ae42`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-29c397f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
