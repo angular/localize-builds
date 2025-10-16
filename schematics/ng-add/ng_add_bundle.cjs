0b69b02516d5b936b7508d9a5bcefe65a3185edb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-16 18:57:37 +0000

    b99d028 refactor(platform-browser): remove unecessary export. (#64469)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.5+sha-d342601`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.5+sha-b99d028`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
