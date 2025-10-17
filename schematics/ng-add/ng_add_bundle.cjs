ebcce560d503b7520f5b296d7b1b9d8db0391a8e
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 14:50:03 +0000

    7d7b4be docs: add matrix parameters docs and examples (#64459)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-10aa4f9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-7d7b4be`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
