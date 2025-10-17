4e6db27ba7ab14db3cc8304e9449d6053246eb58
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 14:36:39 +0000

    3cfa6e8 build: remove unused commit message scopes (#64492)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-c241038`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-3cfa6e8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
