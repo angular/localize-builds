ef3fcb14f15636bb38ed9e81084412b03bae55dd
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-17 18:29:54 +0000

    9a7529d fix(compiler): correctly compile long numeric HTML entities (#64297)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-062a696`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-9a7529d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
