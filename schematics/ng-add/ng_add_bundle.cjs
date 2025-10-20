25b29e704d03650dca7079a7eb04f6ff4f4d4b4d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 21:29:28 +0000

    cfd8ed3 fix(router): Fix outlet serialization and parsing with no primary children (#64505)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.6+sha-182fe78`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.6+sha-cfd8ed3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
