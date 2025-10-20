39a83e9e20d2755a17e33ff6d7b1909daff4aeb8
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-20 15:44:37 +0000

    e464aac build: bump leftover dependency of typescript (#64543)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-9c7029c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-e464aac`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
