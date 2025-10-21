944d4bc17852ee8e53cfe0990b7befd02c808818
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-21 15:28:56 +0000

    e568ddb docs: Standardizes code example formatting in Angular Tools and fixed highlight (#64554)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.6+sha-cfd8ed3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.6+sha-e568ddb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
