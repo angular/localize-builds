8d95f15e34f12ef3d3466393bbaa3da260314066
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-21 17:44:03 +0000

    fd9af2a fix(forms): only propagate schema defined properties from field to control (#64446)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-2b257b3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-fd9af2a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
