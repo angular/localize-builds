346342cde4b53ce83aaeba7f35bd89f856970f3f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-21 15:30:55 +0000

    196fa50 fix(common): properly type ngComponentOutlet (#64561)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-bc9c814`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-196fa50`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
