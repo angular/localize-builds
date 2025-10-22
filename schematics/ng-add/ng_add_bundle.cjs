9fb52eee39ec1237f96dc60893a94457c271a0ff
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 19:11:37 +0000

    81bd671 fix(core): prevent duplicate nodes from being retained with fast `animate.leave`` calls (#64592)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-a3e91b2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-81bd671`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
