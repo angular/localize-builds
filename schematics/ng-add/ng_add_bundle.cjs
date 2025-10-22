dd935a64b04452d6284a59b3e197e84dd855e122
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-10-22 18:32:25 +0000

    a3e91b2 refactor(forms): Add a test better documenting behavior of the error function in built-in validators (#63993)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-e039c6b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-a3e91b2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
