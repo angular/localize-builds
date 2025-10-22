9d59ab5b9bfab3f1468f9b34f9a39559581fe0c1
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-10-22 21:31:53 +0000

    be1598f docs: release notes for the v21.0.0-next.9 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.8+sha-fcf7eee`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-be1598f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
