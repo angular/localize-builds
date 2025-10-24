d6de98a3375cc31b43f76bb2a2f1669f88ebf2f3
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-24 17:08:04 +0000

    e9f55a8 Revert "build: refactor location of best-practices.md"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.9+sha-5099ed4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.9+sha-e9f55a8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
