c002e0ffe5901ead1b51be1dfaa49b77bbc35c19
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-24 07:43:24 +0000

    c02aa80 docs: update Angular CDK apis (#64642)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.9+sha-d7e85a4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.9+sha-c02aa80`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
