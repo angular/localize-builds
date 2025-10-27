fa2ccd2e0320c9cf5c9d62b6fb1d4763ae358e7c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-27 16:27:02 +0000

    8511759 build: update rules_angular digest to 1f1aa3e

--- fesm2022/init.mjs
+++ fesm2022/init.mjs
@@ -1,12 +1,11 @@
 /**
- * @license Angular v21.0.0-next.9+sha-04dd75b
+ * @license Angular v21.0.0-next.9+sha-8511759
  * (c) 2010-2025 Google LLC. https://angular.dev/
  * License: MIT
  */
 
 import { $localize } from './_localize-chunk.mjs';
 
-// Attach $localize to the global context, as a side-effect of this module.
 globalThis.$localize = $localize;
 
 export { $localize };
