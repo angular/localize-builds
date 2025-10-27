975862b302cbf9c3880039296d0b59c088e1a11f
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-27 08:34:14 +0000

    14b5590 fix(docs-infra): preserve shell class for multifile code blocks

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.7+sha-6daabb0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.7+sha-14b5590`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
