df0d13cb4229c06f4c83c7cd22342c0331b9c33f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-10-28 17:40:07 +0000

    5a93eeb Revert "feat(router): Support wildcard params with segments trailing"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-3032332`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-5a93eeb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
