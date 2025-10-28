35bed2b38d52f2e6faaf359d3f8caf0be0b32766
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 16:00:32 +0000

    d1d5c72 docs(docs-infra): allow cors for json files

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.9+sha-4f21ea7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-next.9+sha-d1d5c72`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
