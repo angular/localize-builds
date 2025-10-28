99d16e677214157d5382c628dec9410a233fe499
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-28 14:12:25 +0000

    92d1897 docs: align adev best practices with llm-eval

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.7+sha-f96df92`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.7+sha-92d1897`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
