ba86a30f2069f5ffc97c05e6e50389a7a1ecbeb2
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-10-29 17:53:50 +0000

    54ad3f6 docs: Rename TodoListItem to UserProfile and fix usage of `isValidUserId`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.9+sha-b517c9c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.9+sha-54ad3f6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
