0cbb1870be520c90fde109ca44eec72bd6922eed
Andrew Scott <atscott01@gmail.com>
2025-10-29 21:07:30 +0000

    b0c9c63 build: update cross-repo angular dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-fb569ef`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-b0c9c63`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
