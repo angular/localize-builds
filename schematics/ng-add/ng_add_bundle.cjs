d0f204fb1f025dd8e509fb07dd53f247bf5c094f
Andrew Scott <atscott01@gmail.com>
2025-10-30 19:26:56 +0000

    fee4baf docs: update dependency injection guides (part 1) (#63652)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-d23bb3c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-fee4baf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
