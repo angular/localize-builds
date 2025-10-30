db22f402164234d90d40fe551e3f87118f3b859f
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:29:27 +0000

    58f921d docs: add essential signal forms guide (#64665)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-90452f5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-58f921d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
