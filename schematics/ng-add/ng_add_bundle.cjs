fd100840846c435eb8276751ccdc904bf872b79a
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:54:46 +0000

    a3b08ea docs(docs-infra): allow cors for json files (#64783)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.9+sha-040585d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.9+sha-a3b08ea`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
