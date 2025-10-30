cfa7bd7329b25f05f5bde0f2dab0241d6531d59c
Andrew Scott <atscott01@gmail.com>
2025-10-30 17:47:27 +0000

    ca0e77b fix(docs-infra): Cannot find module message (#64701)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-58f921d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-ca0e77b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
