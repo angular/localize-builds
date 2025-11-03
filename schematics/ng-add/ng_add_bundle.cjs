060d6a409a163b1edbef27ae862b8ef8d1b14428
Andrew Scott <atscott01@gmail.com>
2025-11-03 23:34:34 +0000

    6b3fdf3 docs: remove feature from changelog.

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-d66384d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-6b3fdf3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
