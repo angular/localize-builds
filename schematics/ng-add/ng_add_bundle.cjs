717c76c3bf4e4298d761ca905fabb207da214c4f
Andrew Scott <atscott@google.com>
2025-11-03 21:40:59 +0000

    92f7052 docs(docs-infra): Updates page title strategy to sync meta tags

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-dc99f4c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-92f7052`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
