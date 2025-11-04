723491cd3ae9802684ac501fcca5e70813607f6b
Andrew Scott <atscott@google.com>
2025-11-04 23:02:40 +0000

    9c59ea8 docs(docs-infra): add missing meta tags for author, keywords, and Twitter

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-9596020`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-9c59ea8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
