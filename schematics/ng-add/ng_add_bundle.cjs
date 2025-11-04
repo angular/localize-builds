a653d833168423ae4741b83ab9a9c7b75d04c10a
Andrew Scott <atscott@google.com>
2025-11-04 00:31:06 +0000

    3d6a206 docs: remove standalone: true since it's now the default

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.9+sha-192eb35`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.9+sha-3d6a206`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
