4aa45318c338d6e0a3e42b3cdea04d41aac47130
Andrew Scott <atscott@google.com>
2025-11-04 17:44:04 +0000

    1b6d0cf docs(router): uniformize lazy loading syntax

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.9+sha-7438b9c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.9+sha-1b6d0cf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
