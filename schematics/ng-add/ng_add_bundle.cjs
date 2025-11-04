5a656da54c688aa62b6abddbf95283f9cdc22060
Andrew Scott <atscott01@gmail.com>
2025-11-04 22:54:11 +0000

    43f7f41 fix(docs-infra): properly join url parts

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.9+sha-ec9b074`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.9+sha-43f7f41`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
