f7eab1f9180a115475b7c36da1b8a72b3437f9d4
Andrew Scott <atscott@google.com>
2025-11-05 17:54:32 +0000

    01c5ba5 docs(docs-infra): fix heading layout on mobile

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-d3d2a34`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-01c5ba5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
