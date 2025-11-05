1c1f8cfb89460f2c89588ed948249e7f6427bffc
Andrew Scott <atscott@google.com>
2025-11-05 00:20:34 +0000

    507b346 perf(forms): implement change detection for field control bindings

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-e1f933f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.0+sha-507b346`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
