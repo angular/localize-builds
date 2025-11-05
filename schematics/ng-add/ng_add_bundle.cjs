62cfd55ae0b0f60c1ec0994cb9fabe39c1d25533
Andrew Scott <atscott@google.com>
2025-11-05 17:11:49 +0000

    744cd5c refactor(http): simplifies destruction tracking using destroyed property

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.9+sha-a45e6b2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.9+sha-744cd5c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
