3ba3184f312cd4460a8c24ffcba00df1b9cb4b49
GitHub <noreply@github.com>
2025-11-06 23:06:02 +0000

    85d9abb refactor(router): Intercept navigate events (unless it's a rollback)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-cf47ce2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-85d9abb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
