bccdcfc526c270df3e33cb9bd7a5336522c7c31b
GitHub <noreply@github.com>
2025-11-06 18:14:40 +0000

    ee578d3 build: format md files

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.10+sha-dcdd1bc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.10+sha-ee578d3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
