e4082b475d82838c50f386a192dbe524d2695b4b
Andrew Kushnir <akushnir@google.com>
2025-11-07 19:41:42 +0000

    1e53f4e refactor: exclude tsconfig.json from VSIX package contents (#64991)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.1+sha-f54cc4f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.1+sha-1e53f4e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
