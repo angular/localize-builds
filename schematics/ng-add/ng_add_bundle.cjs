80060d098a07c0926464d9be62b6d20697a6f8ee
Andrew Kushnir <akushnir@google.com>
2025-11-07 21:02:57 +0000

    647e640 refactor(core): Extract NavigateEvent cancel steps based on recent spec update

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.1+sha-781a329`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.1+sha-647e640`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
