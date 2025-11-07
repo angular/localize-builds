dc849eee07ac7fd134ac9166ec6792cb30f1c40f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-07 15:48:55 +0000

    b8972da refactor(devtools): replace HostListener with host metadata for keydown handling

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.10+sha-41466a7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.10+sha-b8972da`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
