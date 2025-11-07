f0139fb0f00112c3235abd435f24dc391fc0c77d
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-07 15:49:40 +0000

    a3cb1df refactor(forms): nicer type errors

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.1+sha-8e73f54`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.1+sha-a3cb1df`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
