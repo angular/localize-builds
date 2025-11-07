5380ca98ef99aee21466d6dadcfcd30b396c7713
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-07 19:48:58 +0000

    aada582 build: update `rules_angular` to latest

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.1+sha-1e53f4e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.1+sha-aada582`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
