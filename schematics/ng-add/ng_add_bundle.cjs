efca4a9c3140ed27a0b949d9c79ecfb67e85d203
Andrew Kushnir <akushnir@google.com>
2025-11-10 16:07:14 +0000

    5092a89 docs: add shell language to CLI installation examples

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.1+sha-590244a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.1+sha-5092a89`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
