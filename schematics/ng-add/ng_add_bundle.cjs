fa75d08977ea9b7f06f449a9f0b3aef933b30938
Andrew Kushnir <akushnir@google.com>
2025-11-10 22:20:58 +0000

    e0407c0 docs: add links to SDUI example and livestream

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.10+sha-8922cae`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.10+sha-e0407c0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
