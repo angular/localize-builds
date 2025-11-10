58eac442cf136e6e2343d63b2241c6982cd2c9e9
Andrew Kushnir <akushnir@google.com>
2025-11-10 17:58:32 +0000

    2ad6b72 refactor(http): migrate XSRF classes to use inject() function

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.10+sha-b20bf5d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.10+sha-2ad6b72`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
