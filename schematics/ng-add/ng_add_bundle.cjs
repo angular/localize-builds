ff9dbec8a4a004f7134ebf901cbe266e64599605
Andrew Kushnir <akushnir@google.com>
2025-11-10 20:16:19 +0000

    5047849 fix(common): remove placeholder image listeners once view is removed

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.10+sha-2ad6b72`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.10+sha-5047849`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
