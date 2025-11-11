26a2c802bc77ad7586a0033001974a63ea34613b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-11-11 22:16:27 +0000

    c3e82fb refactor(core): render additional WAI-ARIA 1.3 property bindings as attributes

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-4ed8781`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-c3e82fb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
