1077fbfd46cca2e37be86a36c18b979f7b781b97
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 23:08:13 +0000

    31ca56a docs: Fixed mermaid diagram node label contrast in dark mode

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.2+sha-be37129`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.2+sha-31ca56a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
