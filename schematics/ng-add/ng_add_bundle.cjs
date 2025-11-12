866277f85653e55ccdbe9e3263fbf18d6ad4031c
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-12 21:17:47 +0000

    1c4fb90 docs: update tutorials with self-closing tags, standalone router directives, and fix syntax highlighting

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.11+sha-2e7623c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.11+sha-1c4fb90`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
