ee7b15da4dd6a6a717c76d81b49e2370d0585693
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-13 23:50:53 +0000

    f689269 Revert "fix(compiler): support one additional level of nesting in :host()"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.11+sha-fdd267a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.11+sha-f689269`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
