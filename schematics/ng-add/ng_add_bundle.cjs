93cd889ed743ec1083fc5fe2817ddd276997cb30
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-13 22:09:48 +0000

    482e137 docs(docs-infra): improve theme picker accessibility and add animations

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.2+sha-16d51f6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.2+sha-482e137`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
