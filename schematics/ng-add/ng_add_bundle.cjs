d133e9ac630e13563e33bf7db8f476b013a6a463
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 17:44:57 +0000

    1464d02 docs(docs-infra): backport docs changes from `21.0.x`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-fc1ef79`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-1464d02`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
