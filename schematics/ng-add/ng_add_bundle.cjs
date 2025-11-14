2fc1177f341a72138e8a8beffbc554e203622d2f
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 16:48:34 +0000

    f550f4a docs: ExperimentalIsolatedShadowDom mentions

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.2+sha-c2a52fd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.2+sha-f550f4a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
