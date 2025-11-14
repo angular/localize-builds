671fe1001c991799f2055f07c9347f0baffcb7cf
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 16:59:05 +0000

    911999e docs: update cross-repo adev docs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.2+sha-f550f4a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.2+sha-911999e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
