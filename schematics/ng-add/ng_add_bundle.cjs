593feb19e5dc5d38ef0d18d199acf73ac2e58767
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-14 22:08:54 +0000

    722292f refactor(forms): improve typing on min & max (#65212)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-de234de`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-722292f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
