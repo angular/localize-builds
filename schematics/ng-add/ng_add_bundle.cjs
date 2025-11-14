1f80aa1acc18c2576c1dbb1baaaa555278675de4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-14 00:06:57 +0000

    c2a52fd docs: add aria toolbar component guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.2+sha-750da17`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.2+sha-c2a52fd`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
