feefad67a4fe1af3c117a7e59fa523160a3e215c
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 16:05:23 +0000

    63ca0de build: update dependency bazel to v7.7.1

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.2+sha-74b95de`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.2+sha-63ca0de`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
