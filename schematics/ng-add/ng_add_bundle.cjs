557e301f1012b6abed8cff1ad4f4941411da7bef
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-17 17:33:58 +0000

    4a1a703 docs(docs-infra): set max-height to code example

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.12+sha-daae263`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.12+sha-4a1a703`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
