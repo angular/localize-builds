28de37b526d74b6e4dd2422b62dcd039a9635026
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 22:49:26 +0000

    e7a6e31 fix(devtools): refine when signal graph button is shown

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-c757fd6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-e7a6e31`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
