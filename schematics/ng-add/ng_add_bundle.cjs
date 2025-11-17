eed4606b47247031e89081cb8c11a3a94281994a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-17 19:03:43 +0000

    5e15553 ci: remove cache usage from ci.yml

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.12+sha-85777ef`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.12+sha-5e15553`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
