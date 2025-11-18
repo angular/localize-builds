e6f798d8781d6c3c3f7d9acec65b1a8a50d203f1
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-18 23:00:39 +0000

    21e2889 docs: aria combobox examples

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.3+sha-73e9700`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.3+sha-21e2889`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
