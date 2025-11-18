48912978a9e29ac2f742f3aed36f99ae0f9b314a
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-18 16:20:02 +0000

    3686987 docs: simplify docs-code header by removing paths and keeping only file name and info

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.12+sha-72b19d8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.12+sha-3686987`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
