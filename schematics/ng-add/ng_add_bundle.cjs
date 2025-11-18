b71019aecb155c266b5b675c5bd7f4d87da26cdb
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-18 22:16:59 +0000

    5aea52a docs: format examples with replacing <docs-code> typescript examples with fenced ts code blocks

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.12+sha-3686987`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.12+sha-5aea52a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
