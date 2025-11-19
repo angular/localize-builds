b75009aa89a765470d6726887c620af31d4deff3
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2025-11-19 22:57:37 +0000

    38f4ea1 docs: add signal forms validation guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-e9ba63a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-38f4ea1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
