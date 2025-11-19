fec4db5c376a8223a9292818066666d0168371d2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 21:12:41 +0000

    3d9c82c build: provide a specific version of zone.js for the vscode pre standalone test

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-28810e0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-3d9c82c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
