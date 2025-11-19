f5ffc5a5491357a8c3402ffbbbd6db4194dc6ef2
Jessica Janiuk <jessica.janiuk@gmail.com>
2025-11-19 16:24:20 +0000

    e69855a docs: aria grid replace overview example with a playful one

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.3+sha-28c1a2d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0-rc.3+sha-e69855a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
