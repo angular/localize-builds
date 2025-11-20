5929911c60f6d513358c46d5e5cabc2489061716
kirjs <kirts@google.com>
2025-11-20 16:26:55 +0000

    f349a91 docs: fix standlone menu overlay

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-630a3b2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-f349a91`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
