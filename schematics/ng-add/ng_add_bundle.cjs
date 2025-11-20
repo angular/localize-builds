773e1ec7267b121f5d7664c15e4653e02fc94457
kirjs <kirts@google.com>
2025-11-20 15:39:56 +0000

    cf7579f docs: add signal forms custom controls guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-f31fbdd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-cf7579f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
