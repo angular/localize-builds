e632c9b3168599282afcc535cb671da90826358e
kirjs <kirts@google.com>
2025-11-21 18:30:36 +0000

    df1d129 docs: consistent casing for onpush zoneless migration MCP tool name

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-1dfce39`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-df1d129`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
