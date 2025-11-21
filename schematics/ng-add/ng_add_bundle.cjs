9bf05b95569364ac4bc953a5fa7e0fdefef3d6a1
kirjs <kirts@google.com>
2025-11-21 21:28:29 +0000

    5170fd4 docs: format JSON examples and replace <docs-code> with fenced code blocks

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-b8da1d4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-5170fd4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
