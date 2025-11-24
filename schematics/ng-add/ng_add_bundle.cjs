a284092bb494c821c59288f482b3d11a0414127e
kirjs <kirts@google.com>
2025-11-24 14:12:51 +0000

    92f0b66 docs: fix v21 event header image

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-696aaa1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-92f0b66`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
