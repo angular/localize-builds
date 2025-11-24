15fb2dc420a26256316fee52faa2eb6a7897abd4
kirjs <kirts@google.com>
2025-11-24 18:54:42 +0000

    4845a33 refactor(forms): support custom control directives

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-534cc3d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-4845a33`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
