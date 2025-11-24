d9804d60b9f0cfafd6af09ad94986b9ab6c7bd4e
kirjs <kirts@google.com>
2025-11-24 16:16:27 +0000

    102cb87 docs: update devtools guide - splits the injector inspector into its own guide - update the screenshots - add defer block section - add hydration section

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-4303f51`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-102cb87`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
