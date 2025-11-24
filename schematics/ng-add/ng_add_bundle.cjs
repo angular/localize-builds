fbe0d353a3b821fccc21bde3000b05a22d2abf09
kirjs <kirts@google.com>
2025-11-24 20:35:11 +0000

    70507b8 fix(core): debug data causing memory leak for root effects

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-4845a33`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-70507b8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
