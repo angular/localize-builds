adaa6b735850a9b32c55d213c0a8dc200f969569
Kirill Cherkashin <kirill@kcherkashin.org>
2025-11-24 20:34:22 +0000

    ca6ab6c fix(core): debug data causing memory leak for root effects

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-f97a1d4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-ca6ab6c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
