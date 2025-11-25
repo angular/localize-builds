558f7b877ef9b8babdef25ea78c9e73b7a60c97a
kirjs <kirts@google.com>
2025-11-25 16:44:25 +0000

    49ad7c6 fix(core): use injected `DOCUMENT` for `CSP_NONCE`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-4634b46`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.0+sha-49ad7c6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
