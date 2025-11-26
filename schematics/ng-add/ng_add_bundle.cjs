db14a58d0e2e78de9ddd6daf08953927c028e82b
kirjs <kirts@google.com>
2025-11-26 20:53:02 +0000

    99b6341 docs(docs-infra): fix form custom control example uses syntax

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.1+sha-4207d89`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.1+sha-99b6341`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
