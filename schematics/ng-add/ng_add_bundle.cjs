73d9819851996802b29f8ff84bd61efa87dbe4e1
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-12-01 09:38:01 +0000

    78fd159 fix(compiler): prevent XSS via SVG animation `attributeName` and MathML/SVG URLs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.1+sha-fec1f1a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.1+sha-78fd159`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
