f8857fcc621c33feeb78e260ddd3e5565c68d01b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-01 18:29:12 +0000

    25bf417 ci: include dev-app files from pullapprove checks

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-721080a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-25bf417`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
