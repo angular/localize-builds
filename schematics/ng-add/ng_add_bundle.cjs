89b2a78e88d9dbc8bf30f2b0c5a3a3893f49eb17
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 09:42:44 +0000

    7fd5890 docs: update syntax highlighting in component documentation

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-71b6131`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-7fd5890`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
