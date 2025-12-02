56eadd90cf03f2e30bb15099fe5bef151d847acf
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 14:17:08 +0000

    5a80a48 fix(compiler-cli): avoid allocating an object for signals in production mode

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.2+sha-017672f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.2+sha-5a80a48`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
