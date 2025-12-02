08a23a2bd5a61dd764c9b0462ce47f79afd49c49
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 15:50:41 +0000

    a0ad5d4 test(router): Add tests for support of path params before/after a wildcard

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-e30e61b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.0+sha-a0ad5d4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
