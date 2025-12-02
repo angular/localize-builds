5927fee5e066e9a7b5847e2c8f80d43c181789d7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-02 18:17:40 +0000

    c2a8934 Revert "refactor(router): compress synchronous end to router navigation to single operator"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.2+sha-5a80a48`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.2+sha-c2a8934`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
