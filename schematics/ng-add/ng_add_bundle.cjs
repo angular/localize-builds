c82d299bbf5b46f6abaa18bc3dbdd0b10bae2ac5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 14:21:14 +0000

    6b4ab87 feat(forms): Allows transforms on `FormUiControl` signals

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.2+sha-6491df9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.2+sha-6b4ab87`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
