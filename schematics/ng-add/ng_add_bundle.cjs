d032019da472d35a5483403581459614afbcdba9
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 11:31:21 +0000

    20474d3 fix(http): enable XSRF protection for same-origin absolute URLs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.2+sha-0f50ea8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.2+sha-20474d3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
