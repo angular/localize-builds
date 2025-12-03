c34049f4c1807cf6af85f722a67bea258cb66a8e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2025-12-03 09:17:20 +0000

    42fbb64 build: update bazel dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.2+sha-e95d2e3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.2+sha-42fbb64`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
