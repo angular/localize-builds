5184736dcc863f927503ff0310ccdb619e2f5091
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 17:19:45 +0000

    f253f12 build: update bazel dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.3+sha-5b911d6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.3+sha-f253f12`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
