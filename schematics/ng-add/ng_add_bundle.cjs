7b8a9c726078f062a36d025930f6d70572b82574
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-08 17:04:56 +0000

    7e1e396 docs: add documentation for `HTTP_TRANSFER_CACHE_ORIGIN_MAP`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.1+sha-28017c1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.1+sha-7e1e396`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
