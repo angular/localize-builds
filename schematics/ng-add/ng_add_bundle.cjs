7414f6ead01ff2c61cf6cd2e141478faa22f717c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-10 18:14:26 +0000

    2dd971b docs: hide menu icons from screen readers in aria example

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.3+sha-834593b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.3+sha-2dd971b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
