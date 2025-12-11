f10089ca684b395bedcf1ebc59cc77ac60cd7654
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 23:38:16 +0000

    4fd2b72 fix(forms): fix signal forms type error

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.4+sha-69d243a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.4+sha-4fd2b72`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
