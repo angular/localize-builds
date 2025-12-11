96b50a3b0910b946c78205f2a636a2b32ea4cd9f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2025-12-11 00:55:27 +0000

    d343ff3 docs: release notes for the v21.0.4 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.1+sha-a52c834`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.1+sha-d343ff3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
