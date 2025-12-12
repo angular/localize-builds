da1376b38e80149f7a5ad9599b75219766103838
Andrew Kushnir <akushnir@google.com>
2025-12-12 18:15:48 +0000

    8cabd67 docs: update `guidelines.md` to match `best-practices.md`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-27bb376`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-8cabd67`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
