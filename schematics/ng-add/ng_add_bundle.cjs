b4bc6c0b79424ba726fe394f2da25b67c8e92303
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-12 01:23:43 +0000

    87d20fd build: update cross-repo angular dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-25f8007`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-87d20fd`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
