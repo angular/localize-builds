88aaa6d497817a81cee90c218e4dd834b7b4127c
GitHub <noreply@github.com>
2025-12-15 22:07:27 +0000

    193aa33 docs(forms): improve JSDoc for Signal Forms Schema types

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.3+sha-356092a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.3+sha-193aa33`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
