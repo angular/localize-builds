dec4d5b5cd3cf014bd34b1d845fa85722be6f813
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-15 23:25:35 +0000

    b4f584c fix(core): return `StaticProvider` for `providePlatformInitializer`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.3+sha-11e271c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.3+sha-b4f584c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
