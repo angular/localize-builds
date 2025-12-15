af0871f9326af68bfebe9a0a4148a4d6276e6003
Andrew Kushnir <akushnir@google.com>
2025-12-15 19:50:13 +0000

    3151c05 refactor(forms): add signal forms to the type tests

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-0dd5417`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-3151c05`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
