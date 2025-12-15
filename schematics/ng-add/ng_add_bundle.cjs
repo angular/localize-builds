62ba6afc7dd7f4a4135f3745f0a52bd11411402c
Andrew Kushnir <akushnir@google.com>
2025-12-15 19:41:28 +0000

    0dd5417 docs: format CLI builder JSON examples using fenced blocks

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-15fd24d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-0dd5417`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
