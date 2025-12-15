7a5519ed9f20c1b3bc561cd4e7b8946d386b5dcf
Andrew Kushnir <akushnir@google.com>
2025-12-15 18:05:42 +0000

    15fd24d docs: replace `@debugging` tag with proper Markdown syntax

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-fe59d9a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-15fd24d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
