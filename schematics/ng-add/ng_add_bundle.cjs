ba69f5b8ee37678e92954fc45bd1b0fa50eed066
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 18:20:03 +0000

    8d7bcbc build: migrate Bazel ignore rules from .bazelignore to REPO.bazel

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-a27bdd3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-8d7bcbc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
