7df54f08964fe0bcbaa8cde6c1dab887ebba78b1
Andrew Kushnir <akushnir@google.com>
2025-12-17 23:47:30 +0000

    59b3790 refactor(forms): rename server errors to submission errors

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-0edbee4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-59b3790`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
