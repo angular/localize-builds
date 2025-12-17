c52c2f8d45f5ca8d953bbde4b11a7f9279eb4cf0
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2025-12-17 18:06:07 +0000

    a27bdd3 build: update bazel dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-3b3b982`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-a27bdd3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
