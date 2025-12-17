0cb0629b08e2329ff8079f5e23b590441f003884
Andrew Kushnir <akushnir@google.com>
2025-12-17 17:50:53 +0000

    14b529e docs: align lightweight injection tokens with the style guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-a4d2de8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.5+sha-14b529e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
