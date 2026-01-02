1b526803b84315d057829e2d3244c0d46f8dfc96
Kristiyan Kostadinov <crisbeto@abv.bg>
2026-01-02 10:44:03 +0000

    f8b6a3f Revert "refactor(core): Add `ngDevMode` guards and new sanitization error codes"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-d370c4b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-f8b6a3f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
