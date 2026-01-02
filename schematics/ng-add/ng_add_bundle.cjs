fa8159f408dd9bdfc2af080140605122c6dc175a
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-02 13:29:25 +0000

    71a4373 build: update cross-repo angular dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-f8b6a3f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-71a4373`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
