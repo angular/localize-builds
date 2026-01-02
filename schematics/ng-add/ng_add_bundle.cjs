a93e97313bcc62b04274ec6946d4dcbccf7ffb0f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-01-02 07:44:05 +0000

    d370c4b build: update bazel dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-069974a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-d370c4b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
