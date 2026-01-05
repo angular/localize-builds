f832b1a8973e18834743933022dac6b97b4562b7
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-05 20:26:42 +0000

    1530814 docs(docs-infra): Add package filter to the API list

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-5edceff`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-1530814`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
