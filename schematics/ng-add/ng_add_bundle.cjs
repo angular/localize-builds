bc19b93f97d1a3378953df51d2e6acf1c4bd967e
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-06 17:27:04 +0000

    10da2f9 fix(compiler): better types for a few expression AST nodes

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-b93d5ec`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-10da2f9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
