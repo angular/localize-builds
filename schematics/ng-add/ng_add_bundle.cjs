97fe74c051e001018652f5b61d091c08904f768d
kirjs <kirts@google.com>
2026-01-06 00:43:45 +0000

    a119427 docs: rewrite testing docs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-b16c2f5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-a119427`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
