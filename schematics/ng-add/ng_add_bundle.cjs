84f1134dc321a23e60a5b42a8fc7c0bf964ecbdf
kirjs <kirts@google.com>
2026-01-07 19:24:52 +0000

    c3adc5a docs(docs-infra): Fix combobox dialog styles for dark theme

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-61370dc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-c3adc5a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
