8e632f2356f8466272c404e5083842ead734ba7d
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 21:50:24 +0000

    8fd585c release: cut the v21.0.7 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-ff53175`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.7+sha-8fd585c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
