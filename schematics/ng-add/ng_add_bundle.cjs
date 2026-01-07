2a182c83ff362db50af7c74f2df367c1cddc7ab5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-07 23:52:34 +0000

    0ad3adc fix(compiler): Support empty cases

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-bb0dc8c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-0ad3adc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
