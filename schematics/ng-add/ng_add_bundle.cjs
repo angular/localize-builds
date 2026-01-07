02b250bbd6faf8362f432b1d759f6f4e0dcdfdcb
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 17:48:08 +0000

    80b0fbb fix(core): avoid leaking view data in animations

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-85ce5f3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-80b0fbb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
