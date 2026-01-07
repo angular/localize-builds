fd759c725d5a8b55d35ed7d8fe4328707fce6584
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-07 22:40:30 +0000

    f5bad8e refactor(core): Improve tree-shakable `RuntimeError` error for `INVALID_FIELD_DIRECTIVE_HOST`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.7+sha-8fd585c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.7+sha-f5bad8e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
