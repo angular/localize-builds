24f8f7f901331388ad86c80a08be9493883c9557
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 16:51:19 +0000

    97fd1de Revert "refactor(router): Add support for precommitHandler in Navigation integration"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-b17f93b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-97fd1de`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
