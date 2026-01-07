a6bc9c3f2a3d511fbe7115503b74745bff7e8abe
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 19:24:30 +0000

    090484c docs(docs-infra): Fix combobox dialog styles for dark theme

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-8c8c5b2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-090484c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
