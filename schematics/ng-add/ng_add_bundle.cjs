1d2f42737c8a12fcfb2784673f84a443e77872cb
Kirill Cherkashin <kirill@kcherkashin.org>
2026-01-07 17:36:58 +0000

    85ce5f3 docs: update copyright year

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-97fd1de`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-85ce5f3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
