c65248b90297cfc93d683dba125551ec80a93c19
kirjs <kirts@google.com>
2026-01-07 20:15:24 +0000

    ff53175 refactor(forms): remove `customError()`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-c3adc5a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-ff53175`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
