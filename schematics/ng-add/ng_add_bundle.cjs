a8145ad21dd885a5bb1d28dfd857ce193d157898
kirjs <kirts@google.com>
2026-01-07 17:44:46 +0000

    7c74674 fix(core): avoid leaking view data in animations

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-7738eb9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-7c74674`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
