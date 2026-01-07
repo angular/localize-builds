d546f3bfd9171538105b9e325715cb3bee51357b
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-07 00:34:04 +0000

    4161161 Revert "fix(router): Ensure createUrlTree does not reuse segments of input"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-e3fba18`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.6+sha-4161161`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
