9865f889dc9d9c7add442bb213838909d92253ca
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-08 20:55:24 +0000

    6ad2ef6 release: cut the v21.1.0-rc.0 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-3b8c4fc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-rc.0+sha-6ad2ef6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
