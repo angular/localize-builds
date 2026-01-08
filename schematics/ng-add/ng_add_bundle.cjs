746c42edbb4e22f51fe5c78c0539b1d12a37329a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 18:08:20 +0000

    2b83091 docs(forms): make onSubmit method async for form submission

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.7+sha-32ff1a0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.7+sha-2b83091`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
