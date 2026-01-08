0feb8b6c9b9ef45de3473a0fc050630bd56009cd
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 21:33:24 +0000

    74af7d8 refactor(core): Use the provided Document value rather than global in FakeNavigation

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-rc.0+sha-6ad2ef6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-rc.0+sha-74af7d8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
