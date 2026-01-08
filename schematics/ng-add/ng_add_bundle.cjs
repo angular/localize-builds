d4c6989b2358dc53dadb66047f657e228f98750a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-08 20:24:48 +0000

    a2b9429 Revert "feat(router): add trailingSlash config option"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-86dc128`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-a2b9429`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
