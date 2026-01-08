52bb95cc3e1ecb4867f7cd44d78301141d46baf7
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-08 17:12:33 +0000

    32ff1a0 docs(docs-infra): Fix toggle button in dark mode Fix visibility toggle button in dark mode

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.7+sha-8aab227`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.7+sha-32ff1a0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
