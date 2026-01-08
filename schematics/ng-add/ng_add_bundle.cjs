83b9255be095717154bb62d9b0088b91afca93ca
GitHub <noreply@github.com>
2026-01-08 16:31:56 +0000

    12fccc5 feat(router): add trailingSlash config option

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-7003e8d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-12fccc5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
