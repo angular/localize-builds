9392818758b4ef73bd769e257368e5ffbd70cee6
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-08 19:26:26 +0000

    ad49d48 fix(docs-infra): improve dark mode button hover contrast for WCAG compliance

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-bd5997c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-next.4+sha-ad49d48`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
