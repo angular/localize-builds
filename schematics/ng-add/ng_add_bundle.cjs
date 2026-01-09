1f056f3a90b8ebe57b8f0bc5e5a7205ca190601b
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 18:04:33 +0000

    4fb24ca refactor(compiler): switch `Binary.isAssignmentOperation` to type guard function

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-94b7079`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-4fb24ca`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
