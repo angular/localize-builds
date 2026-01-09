b14aa529a842dcf349a46e5f11b09360f4223b1c
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 17:31:28 +0000

    a63f6d0 docs: replace UnambiguousRoleValidator in asyc-valdotors part form-validation.md

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.8+sha-24fa4a2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.8+sha-a63f6d0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
