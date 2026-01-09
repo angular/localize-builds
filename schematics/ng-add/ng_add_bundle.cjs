a1eed572cef74c7e45f645f2f5c473dcdd3ebae7
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-09 18:47:02 +0000

    99db2e9 ci: add pnpm and typescript exclusions to a Renovate package rule.

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-4fb24ca`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-99db2e9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
