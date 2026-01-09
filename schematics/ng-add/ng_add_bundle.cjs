1e7228bab48b6226929d622abbd265e9cf7a8bfa
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-09 17:01:06 +0000

    24fa4a2 docs(forms): Clarify returning errors from submit functions

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.8+sha-491a156`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.8+sha-24fa4a2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
