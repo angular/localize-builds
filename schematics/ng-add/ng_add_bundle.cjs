b83eeed2ff232488a91f4020b4d02fbd3e867715
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 16:36:44 +0000

    66b40d4 docs: update testing guide to include Vitest and improve authGuard test setup

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-7f1b790`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-66b40d4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
