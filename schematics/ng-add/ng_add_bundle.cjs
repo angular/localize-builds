e438717a30a316ba5e1cbc520ecf11bf21de853e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 21:46:43 +0000

    1b4dcc0 docs: replace old https://angular.io/license with https://angular.dev/license

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-4d983e9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-1b4dcc0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
