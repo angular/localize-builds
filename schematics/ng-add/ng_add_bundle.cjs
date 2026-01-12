b63347fc899e6b72f6ee2fec4be9a7f7bcc96b7f
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 17:47:17 +0000

    13a7ee8 docs(docs-infra): Show API decorators docs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-rc.0+sha-addccea`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-rc.0+sha-13a7ee8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
