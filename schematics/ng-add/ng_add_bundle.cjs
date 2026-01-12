ab1615c8fd56855c175f8d5317195429b4235ec3
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 18:12:05 +0000

    d1f6bcd docs: remove Component suffix in animations, HTTP, and SSR guides

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-rc.0+sha-481557e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-rc.0+sha-d1f6bcd`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
