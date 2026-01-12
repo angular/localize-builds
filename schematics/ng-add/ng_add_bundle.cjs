2125e634121cc8a5c5934381cd17be294f42e969
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 21:58:53 +0000

    82d556a fix(forms): Ensure the control instruction comes after the other bindings

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.8+sha-6a9d4d5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.8+sha-82d556a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
