ba9cf65b0461d836069a56896139b03fd6bff57a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-01-12 21:56:48 +0000

    1ba9b7a feat(core): resource composition via snapshots

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-1b4dcc0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-1ba9b7a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
