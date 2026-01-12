7ab82185d7847a9764882684d5cda52256025289
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-12 16:23:39 +0000

    4871131 docs: improve separation between enter and leave example

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-rc.0+sha-c7d6053`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-rc.0+sha-4871131`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
