ac312f02d77479b636d0fe9eeb07c3817fda6864
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 21:13:57 +0000

    de9277b build: Update vscode release script

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.8+sha-492b106`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.8+sha-de9277b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
