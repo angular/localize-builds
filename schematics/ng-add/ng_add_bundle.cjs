7dd7c08cdaac8f82d80e4505c069827417452e05
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-13 16:37:25 +0000

    559797c docs: update hierarchical-dependency-injection.md

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-rc.0+sha-76a6984`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0-rc.0+sha-559797c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
