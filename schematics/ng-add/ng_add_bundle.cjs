1027a0fb8c0dfc338e10079518d02024bfd0fd81
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-01-14 16:30:44 +0000

    d710ebe docs(docs-infra): Exempts animation-related symbols from linking

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.0.8+sha-b0b341f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.0.8+sha-d710ebe`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
