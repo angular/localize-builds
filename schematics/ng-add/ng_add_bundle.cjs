dc32cc1e000daf54d4fd00c8eeefb18e649caac9
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-16 18:32:38 +0000

    ac50dbc build: update cross-repo angular dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0+sha-32ad1e0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0+sha-ac50dbc-with-local-changes`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
