bc8642e24251ab79aec4debb0c968f963fc07623
Alon Mishne <amishne@users.noreply.github.com>
2026-01-20 17:41:59 +0000

    82593de fix(docs-infra): support providing links to multiple symbols within a single line of a code block

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0+sha-6b34e5f-with-local-changes`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0+sha-82593de`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
