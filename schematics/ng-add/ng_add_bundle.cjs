778b83a2ee864b9fa1e34e7d65fdfe9a2a93fc33
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-20 18:29:54 +0000

    75c40e7 ci: add template for vscode dedicated issues

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-fc73252`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-75c40e7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
