964432637f72cdd31b8cedb0bcfd915eef121965
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-01-21 18:42:34 +0000

    b885851 build: don't substitute binary files

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-0fa8099`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-b885851`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
