bcb7f1415404355be3e2f45061b4c07813f8e3c4
Alon Mishne <amishne@users.noreply.github.com>
2026-01-21 00:26:50 +0000

    3c8d697 fix(docs-infra): don't use private symbol

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0+sha-e664249`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.0+sha-3c8d697`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
