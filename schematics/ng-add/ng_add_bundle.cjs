3781f9a2b0dcf151f6cd1e40723357cf7be841bf
Andrew Scott <atscott01@gmail.com>
2026-01-22 21:11:35 +0000

    c9043fb refactor(compiler-cli): remove deprecated signature usage

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.1+sha-61dbdee`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.1+sha-c9043fb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
