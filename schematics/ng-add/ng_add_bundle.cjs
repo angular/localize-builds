c45ea1e5b83daeb0f224acd8c67ee3a2cf9a0f31
Andrew Scott <atscott01@gmail.com>
2026-01-22 21:10:54 +0000

    dbc1452 refactor(compiler-cli): remove deprecated signature usage

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-5d6e534`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-dbc1452`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
