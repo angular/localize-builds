fd80d71767c4ead429da13f09e4d7500126f733e
Andrew Scott <atscott01@gmail.com>
2026-01-23 20:14:43 +0000

    8bbe6dc feat(common): Add Location strategies to manage trailing slash on write

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-6fb39d9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-8bbe6dc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
