27978328d04d23da811e89c9050e8107c6fc2c98
Andrew Scott <atscott01@gmail.com>
2026-01-23 18:48:58 +0000

    b203ab0 build: update bazel dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.1+sha-f7eb996`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.1+sha-b203ab0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
