5b65c74133a8b914e3fac2bcb0b5312790ccd5d0
Andrew Scott <atscott01@gmail.com>
2026-01-26 23:05:42 +0000

    c133a2c docs: normalize spacing in animation example boxes

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.1+sha-3151bc3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.1+sha-c133a2c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
