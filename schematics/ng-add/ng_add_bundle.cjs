4471e7d7cb9494d0674c4b602b60809b2ac796a9
Andrew Scott <atscott01@gmail.com>
2026-01-26 22:50:58 +0000

    c921786 docs: Updates form documentation (#66732)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-8efee1c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-c921786`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
