ead63735e34cb46d3968fd126734ace6b4de0abf
Andrew Scott <atscott01@gmail.com>
2026-01-26 23:25:28 +0000

    899df39 docs(docs-infra): remove unused examples

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-ceefceb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-899df39`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
