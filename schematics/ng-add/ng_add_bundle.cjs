78b9ebccc8a65a6d8b55e61422935055a6ad70a4
Andrew Scott <atscott01@gmail.com>
2026-01-26 23:57:01 +0000

    c57b035 fix(language-service): Detect local project version on creation

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.1+sha-06952b4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.1+sha-c57b035`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
