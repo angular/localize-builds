3355228e2d2f546479ecae1c4b83d08784ea8320
Andrew Scott <atscott01@gmail.com>
2026-01-28 21:02:04 +0000

    cfe7340 refactor(core): remove unused restriction parameter

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.2+sha-4f0d29d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.2+sha-cfe7340`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
