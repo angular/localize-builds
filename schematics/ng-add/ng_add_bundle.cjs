e6756c4bbdbd0e21b9ba14b04daf9ef0db6ccef7
Andrew Scott <atscott01@gmail.com>
2026-01-28 18:46:34 +0000

    3de87e1 build: update pnpm to v10.28.2

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.1+sha-31e6f0d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.1+sha-3de87e1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
