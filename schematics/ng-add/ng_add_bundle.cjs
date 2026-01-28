9f0c68667919f1c72f6d957930c9c2975fb9c524
Alan Agius <alan.agius4@gmail.com>
2026-01-28 09:57:16 +0000

    4123ebf release: bump Angular DevTools version to 1.9.0

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-01bfb83`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.0+sha-4123ebf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
