cbca77713bf181283668664447743ee10fd83b16
Andrew Scott <atscott01@gmail.com>
2026-01-28 20:42:07 +0000

    4f0d29d release: cut the v21.1.2 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.1+sha-290b281`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.2+sha-4f0d29d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
