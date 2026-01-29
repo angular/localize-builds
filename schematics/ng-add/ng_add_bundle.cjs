15fee031813432f34bb09929523f5c6b41ba3f92
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 20:05:41 +0000

    0e04233 docs: Add missing link in HostListener documentation

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.1+sha-d072791`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.1+sha-0e04233`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
