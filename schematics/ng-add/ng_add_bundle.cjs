1f1812058672f21450762717fa3c460a35a69965
Leon Senft <leonsenft@users.noreply.github.com>
2026-01-29 21:26:54 +0000

    efb64bf build(language-service): remove local development build script

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.2+sha-8dfbb8b-with-local-changes`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.2+sha-efb64bf-with-local-changes`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
