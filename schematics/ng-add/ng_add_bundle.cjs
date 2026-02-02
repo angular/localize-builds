f5bf077ccd949dbc57461c6f25118ce113149408
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-02 18:23:45 +0000

    575f8a3 docs(animations): fix small typo in animations migrations docs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.2+sha-eb28032`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.2+sha-575f8a3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
