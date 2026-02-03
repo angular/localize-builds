e15450574d67c049331a6d84c088d7a1938fb4d5
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-03 20:40:31 +0000

    f15d33d docs(docs-infra): extract call signature return type.

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.2+sha-6505a26`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.2+sha-f15d33d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
