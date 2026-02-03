59bfdce36cf27fb6df52d614c8917cba41b3da3e
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-03 20:51:39 +0000

    2cf4da0 fix(core): hold constructors weakly in DepsTracker cache

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.2+sha-f15d33d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.2+sha-2cf4da0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
