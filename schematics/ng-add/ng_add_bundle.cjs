76c2b9df6c29106af0da4b26915e32e3d743b2b8
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-03 00:37:02 +0000

    5a0f272 Revert "feat(router): adds browserUrl input support to router links"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.1+sha-68ba9c4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.1+sha-5a0f272`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
