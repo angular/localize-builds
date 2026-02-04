ad80afb16b236e7aa3465df0e3c7ac456ddd771d
Douglas Parker <dgp1130@users.noreply.github.com>
2026-02-04 22:51:21 +0000

    da1f61c release: bump Angular DevTools version to 1.10.0

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.1+sha-680d99b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.1+sha-da1f61c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
