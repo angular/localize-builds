6da399b37f5e67a7a02bd9ff554173c50ba3743a
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-04 22:30:09 +0000

    680d99b refactor(forms): work around internal issue

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.1+sha-cab5ddd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.1+sha-680d99b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
