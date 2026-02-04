2f8d6c07f79589478ff576fa53edd91bd4d239ce
Leon Senft <leonsenft@users.noreply.github.com>
2026-02-04 23:50:15 +0000

    767c6b4 fix(vscode-extension): support highlighting for class bindings with brackets

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.2+sha-1bf6bfc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.2+sha-767c6b4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
