a57329c38d408266b86da680d9464c769e55eb41
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 23:16:06 +0000

    63d828a docs: Signal forms custom validator with 'fieldTree' field in validation result error Update docs for fieldTree property

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.2+sha-460dbbb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.2+sha-63d828a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
