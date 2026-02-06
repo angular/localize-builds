3904a2b244c2092c7006a4678f7a458f68a82d69
Andrew Kushnir <akushnir@google.com>
2026-02-06 01:03:14 +0000

    0729181 test(compiler): remove zone-based testing utilities

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.3+sha-70668be`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.3+sha-0729181`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
