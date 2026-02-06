454d4b03cae2b2e69204a8be17599c874b01ed7c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 01:22:05 +0000

    be4ffb6 build: update rules_browsers digest to e08ae33

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.3+sha-0729181`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.3+sha-be4ffb6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
