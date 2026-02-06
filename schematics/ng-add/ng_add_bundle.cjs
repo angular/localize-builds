7cdc74550087e7935155370126ec5674c1c6cd72
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 18:25:28 +0000

    460dbbb docs: remove non-existant method

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.2+sha-15c71fb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.2+sha-460dbbb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
