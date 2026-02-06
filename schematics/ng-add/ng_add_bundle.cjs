e3bf6bd16d310f1ae685f41ae76a1f1be45c38f9
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 17:54:22 +0000

    15c71fb build: update all non-major dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.2+sha-24c0c5a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.2+sha-15c71fb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
