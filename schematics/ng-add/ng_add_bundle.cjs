a979bef938feb8c6ce58d08c5b109485953fa4b0
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-06 19:19:29 +0000

    c633697 Revert "fix(compiler-cli): update diagnostic to flag no-op arrow functions in listeners"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.3+sha-be4ffb6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.3+sha-c633697`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
