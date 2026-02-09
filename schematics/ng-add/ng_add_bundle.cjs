88c3fbeb094be4286dd102c7d8554588923ec67b
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-09 22:21:37 +0000

    e53c8ab fix(core): Fix flakey test due to document injection

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.2+sha-cb1163e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.2+sha-e53c8ab`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
