af15da6694ba5c7e3be391bf8c0d0cd2f8f4019e
Andrew Kushnir <akushnir@google.com>
2026-02-10 15:54:38 +0000

    2c3f3cc refactor(forms): use optional chaining for safer method calls in form directives

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.3+sha-c6bbf40`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.3+sha-2c3f3cc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
