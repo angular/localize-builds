b3404ac92fe38a716b02771a069ca6620287d65c
Matthew Beck <me@mattrb.com>
2026-02-11 21:46:59 +0000

    8868130 build: add skills in antigravity

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.4+sha-289ddc9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.4+sha-8868130`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
