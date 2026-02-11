ea27b7d9c0c669570672c112f4b11e3e4c1be47c
Andrew Kushnir <43554145+AndrewKushnir@users.noreply.github.com>
2026-02-11 17:56:23 +0000

    289ddc9 release: cut the v21.1.4 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.3+sha-2b99eaa`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.4+sha-289ddc9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
