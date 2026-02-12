203df02ade462920ca15d23bd98f576694f43113
Matthew Beck (Berry) <me@mattrb.com>
2026-02-12 17:35:51 +0000

    06d94ac Revert "refactor(compiler): Add info about unclosed element."

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.3+sha-b386f95`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.3+sha-06d94ac`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
