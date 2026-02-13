cebb744492bf968356caf6a4429349f37cacf9bb
Matthew Beck (Berry) <me@mattrb.com>
2026-02-13 17:26:49 +0000

    50c5353 build: update pnpm to v10.29.3

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.4+sha-55b501a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.4+sha-50c5353`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
