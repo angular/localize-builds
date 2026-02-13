d72dd4a8c04c8e80f85f187d8851eb8f7669ca7f
Matt Beck <mattrb@google.com>
2026-02-13 20:19:39 +0000

    a6a0347 refactor(forms): extract common parser logic (#66917)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.3+sha-2d99878`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-next.3+sha-a6a0347`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
