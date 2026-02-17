89be4aadab2e3ff1a38a7866d7bde41a46d4ee96
Matthew Beck <me@mattrb.com>
2026-02-17 21:07:55 +0000

    7f5a728 refactor(compiler-cli): update updateImportClause away from deprecated signature

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.4+sha-9c1ec6c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.4+sha-7f5a728`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
