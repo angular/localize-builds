9e1c4342d7237c6f6950270a199f6de9f230a0cd
Matthew Beck <me@mattrb.com>
2026-02-18 19:11:47 +0000

    a4eb347 docs: add signal forms async operations guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.4+sha-3c494b4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.4+sha-a4eb347`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
