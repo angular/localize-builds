a15598bc3d88703ec0fa859407faf21592c1b88a
GitHub <noreply@github.com>
2026-02-18 23:20:11 +0000

    dd630ef build: update version target to prep for 21.2.0 release for vscode extension (#67132)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-13fd2b1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-dd630ef`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
