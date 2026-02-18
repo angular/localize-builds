5095d3e9caf3105acb8087afd541b612a8fd9df1
GitHub <noreply@github.com>
2026-02-18 23:20:14 +0000

    2eb6b0d build: update vscode version on main to target v22 (#67133)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.3.0-next.0+sha-2d776f6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.3.0-next.0+sha-2eb6b0d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
