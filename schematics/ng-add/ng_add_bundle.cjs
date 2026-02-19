07d96347820d67e49d718062e3b05f4497651339
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-19 17:17:11 +0000

    a5a1829 docs: improve lazy loading of homepage examples

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.3.0-next.0+sha-a3731a1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.3.0-next.0+sha-a5a1829`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
