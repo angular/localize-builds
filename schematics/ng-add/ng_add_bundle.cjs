6530466f0ea8eced73f24c69defeaa6951ef1747
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 17:00:04 +0000

    4ea3caf docs(docs-infra): fixed missing gray color palette definitions for light and dark modes

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-f58f9c8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-4ea3caf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
