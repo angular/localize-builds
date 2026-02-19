65406f2ead84cea508b1aa52bf8038137b6a3e51
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 23:56:01 +0000

    cccaed0 docs(docs-infra): preserve content before docs-card-container in adev

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.5+sha-9943cb7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.5+sha-cccaed0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
