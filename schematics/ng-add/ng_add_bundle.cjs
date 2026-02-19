19462857449692205bc616c3704f49fa1d7c5e94
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-19 20:46:50 +0000

    d501506 ci: add `zone.js` to `minimumReleaseAgeExclude`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.3.0-next.0+sha-e8cc910`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.3.0-next.0+sha-d501506`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
