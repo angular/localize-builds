cc1f73c3c0ae569f9a8ea259e955301926814e0a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-19 19:29:29 +0000

    5cea357 docs: update changelog to note no changes in 21.1.5 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-79f1cd4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-5cea357`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
