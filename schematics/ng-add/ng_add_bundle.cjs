823d7fb19bd27697d0ba2534f34b982fe7511a64
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 00:05:26 +0000

    f68078b docs: add section on reactive context and async operations

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.3.0-next.0+sha-bc473cf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.3.0-next.0+sha-f68078b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
