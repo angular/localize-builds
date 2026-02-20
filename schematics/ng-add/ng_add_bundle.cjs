e538658e9e34dab2c3401768ab12fefe28b3438e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 17:22:44 +0000

    3b64e7d refactor(common): log a warning when a JsonPipe receives a signal

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.5+sha-24b578c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.5+sha-3b64e7d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
