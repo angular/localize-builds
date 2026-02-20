7e6dddb7f8b0947203c623c8a09f968e862296a9
GitHub <noreply@github.com>
2026-02-20 21:35:40 +0000

    c89d94b refactor(core): guards stringify calls with ngDevMode

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-d7ddebc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-c89d94b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
