9bc5eb584451f5291fe562a519900fc63baad496
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 18:08:08 +0000

    390efd5 docs: add new debugging and troubleshooting di guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-c2cedd1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-390efd5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
