70511aadaa21e8515e65659dffa0d66b530d5786
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-20 19:06:42 +0000

    8737289 ci: update ng-dev config and pullapprove

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-631f60d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-8737289`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
