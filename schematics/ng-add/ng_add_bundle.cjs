5565e98fb01546035d2f8e3f6b25006f87759620
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 16:49:45 +0000

    cfdbb7c ci: bump target version to target 22.0.0-next.0

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.3.0-next.0+sha-c672f92`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-cfdbb7c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
