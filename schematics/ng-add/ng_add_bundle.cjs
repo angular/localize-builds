bd2c3d800db6554e167d59c77db6a3ae0cc7dfc4
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 17:11:06 +0000

    f90e556 fix(compiler-cli): detect uninvoked functions in defer trigger expressions

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-cfdbb7c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-f90e556`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
