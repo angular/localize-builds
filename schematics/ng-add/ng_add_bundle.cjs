8fce3cb58b19a6387f63a6e6feec2609e3e0cc3e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-20 18:33:46 +0000

    fe25c57 fix(forms): preserve parse errors when parse returns value

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-b9b5c27`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-fe25c57`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
