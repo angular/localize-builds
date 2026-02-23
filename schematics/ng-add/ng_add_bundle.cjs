1bd15bf38609d831b72404a2b025a2e8d6070fb9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 17:19:17 +0000

    0596a5b docs: add Route Loading Strategies section and update navigation links

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.5+sha-31d3d56`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.5+sha-0596a5b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
