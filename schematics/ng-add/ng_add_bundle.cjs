25fb05b07585a2e5ca42b21b8c806d75b06cc9ae
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-23 17:19:28 +0000

    31bf694 build: update pnpm to v10.30.1

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-35f0663`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-31bf694`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
