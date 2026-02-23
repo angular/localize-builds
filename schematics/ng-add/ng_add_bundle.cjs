a879dc5f10f643b79bb518c2868f30682570afcf
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-23 21:34:40 +0000

    b9d3fe3 docs: update and reformat the roadmap

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-c9ecf9e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-b9d3fe3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
