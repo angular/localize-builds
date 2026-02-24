b3b12239f92bea2ba46f3b62b04e9e05ae61d2bb
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 17:31:51 +0000

    5c4d9b2 docs: consolidate validation rules for `Host` and `X-Forwarded-Host` headers into a single point.

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.5+sha-4e6ac9c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.5+sha-5c4d9b2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
