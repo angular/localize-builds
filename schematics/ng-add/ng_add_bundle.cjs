f8c2808eee78b9c905ca6956b0b4a45768679a51
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-24 19:07:24 +0000

    9dc3ca4 docs: add docs for `@switch` Exhaustive type checking

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-8af73eb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-9dc3ca4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
