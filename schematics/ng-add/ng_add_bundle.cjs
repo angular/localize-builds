84bacfeec27557c7161c5bc48e2665879c530e01
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-24 17:04:45 +0000

    8f5214e build: update cross-repo angular dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-c7dc705`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-8f5214e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
