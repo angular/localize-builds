3539146c99090bc839cf7c61631dfa4fbed37355
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-24 19:30:03 +0000

    787cd87 refactor(vscode-extension): Add support for exhaustive type check in the syntax

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-1a151f2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0-rc.0+sha-787cd87`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
