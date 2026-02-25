7676fcd5bbaf7424666800985e4198fcceae154d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 01:25:05 +0000

    a5981b8 feat(core): support customization of @defer's on idle behavior

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-0cd00b9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-a5981b8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
