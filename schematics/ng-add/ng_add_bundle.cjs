15f6cfa7c8ce9f1931ca57f2dae7aabf53760f6a
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-02-25 16:19:09 +0000

    89e8661 refactor(common): log a warning when a KeyValuePipe receives a signal

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.1.5+sha-c235b57`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.1.5+sha-89e8661`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
