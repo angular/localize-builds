2265bee0239d81656b5a6eb24f78583953cc5be8
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 16:26:59 +0000

    7f9de3c fix(core): block creation of sensitive URI attributes from ICU messages

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.16+sha-f76d891`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.16+sha-7f9de3c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
