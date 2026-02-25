34466d8af31523d57df9359e7351a6b1bdb63b46
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-25 23:44:46 +0000

    a57ebc9 docs: release notes for the vscode extension 21.2.1 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-de0cc81`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-a57ebc9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
