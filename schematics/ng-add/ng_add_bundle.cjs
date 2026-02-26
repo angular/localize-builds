3771bf19fe1de342f5c579b1849ff5cd1fd4576a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-26 16:25:45 +0000

    95f12c8 docs: add info around route level services

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-4c4a3db`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-95f12c8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
