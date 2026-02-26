0de11674df7b726713faee0f2c00f02d76b30431
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-26 16:04:23 +0000

    4c4a3db refactor: move several schematics from `migrations` to `ng-generate` directories

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-01eadde`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-4c4a3db`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
