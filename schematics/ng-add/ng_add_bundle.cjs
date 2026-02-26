669b01cd41262c0e94d9e5754eed053c7c47c2bd
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-02-26 18:03:51 +0000

    b4f68ac build: update repository locations for bazel rules

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-d0afeba`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-b4f68ac`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
