b79fd0f96043c7a58d1d33451b91a89261ec5b55
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-02 19:47:51 +0000

    f5e9a86 refactor(core): ignore infinite animations in animate api

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0+sha-0c8a285`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0+sha-f5e9a86`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
