4188f28220fa09efa33ba73aa4f407f6a18d56f2
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-02 16:53:57 +0000

    23cb9a4 refactor(forms): address feedback

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0+sha-fc5f9d0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0+sha-23cb9a4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
