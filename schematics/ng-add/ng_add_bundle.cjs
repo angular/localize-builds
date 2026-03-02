f84a886f821c3b43d14a84e63d05651ae242bbdc
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-02 21:27:32 +0000

    dd77363 build: update shiki monorepo to v4

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-c90c8a1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-dd77363`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
