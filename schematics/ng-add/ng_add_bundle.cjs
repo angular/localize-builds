ed20b978681c56618924dbf5c0aba9c59a0c2ef9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-04 17:28:10 +0000

    93c6dc6 Revert "refactor(http): Improves base64 encoding/decoding with feature detection (#67002)"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0+sha-b4ec3cc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0+sha-93c6dc6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
