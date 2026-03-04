5b1a9b2364ca480060017dec7dced33c327b66e3
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-03-04 16:28:09 +0000

    b4ec3cc fix(core): prevent child animation elements from being orphaned

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0+sha-84e79f5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.0+sha-b4ec3cc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
