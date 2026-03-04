3a646cbacb7ac8f1b7e9ae7f3e3445e6120d5463
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-03-04 18:16:43 +0000

    9208421 docs: add customization options for browser URL in routing guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-5338b59`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.0+sha-9208421`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
