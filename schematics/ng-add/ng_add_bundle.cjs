6e8c57b59f8b6496ca1e83aadc7368a6a2cd16d5
Andrew Scott <atscott01@gmail.com>
2026-03-05 22:24:16 +0000

    dd464e5 ci: remove redundant `actions/checkout` steps from dev-infra workflows

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.1+sha-8217ef9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.1+sha-dd464e5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
