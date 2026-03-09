09813d6b8a382f1029e33b80847c20da2a84643a
GitHub <noreply@github.com>
2026-03-09 20:32:04 +0000

    b918bed feat(core): allow debouncing signals

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.1+sha-82b758e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.1+sha-b918bed`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
