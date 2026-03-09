f7ed968fdb59412f7be901c51d289ce55852fc6a
Andrew Scott <atscott@google.com>
2026-03-09 17:28:47 +0000

    79ac570 refactor(devtools): fix browser-specific styles infra

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.1+sha-276c12a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.1+sha-79ac570`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
