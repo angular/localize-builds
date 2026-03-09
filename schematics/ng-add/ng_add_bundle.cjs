6f7d688a35ad969ec1f71eb114ec72a5bb8f157d
Andrew Scott <atscott01@gmail.com>
2026-03-09 23:34:44 +0000

    bf93ff8 ci: fix exclusion of manual_api_docs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.1+sha-b918bed`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.1+sha-bf93ff8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
