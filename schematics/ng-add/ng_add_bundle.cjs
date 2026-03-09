4b534a13b2ec5b8c24b6d423a8ffa783bf286a3a
Andrew Scott <atscott01@gmail.com>
2026-03-09 17:53:26 +0000

    263b819 refactor(devtools): prevent spamming the message bus

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.1+sha-0346536`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.1+sha-263b819`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
