06e24de3c5c3d697376c0821e5dc057942c0fb68
Andrew Scott <atscott01@gmail.com>
2026-03-11 18:01:20 +0000

    6c73aac refactor(common): Removes unused generic type parameters from `KeyValueDiffers`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.2+sha-d45fced`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.2+sha-6c73aac`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
