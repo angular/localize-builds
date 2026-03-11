3117491630c9712b19ef8dc7a595a4971b50be48
Andrew Scott <atscott01@gmail.com>
2026-03-11 18:46:43 +0000

    3c2d95a refactor(forms): normalize experimental version tags in signals api

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.1+sha-b401c18`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.1+sha-3c2d95a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
