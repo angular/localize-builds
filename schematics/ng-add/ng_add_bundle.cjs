b2826dbaa71acc7653f0cb037012e2d9e83b5bda
Andrew Scott <atscott01@gmail.com>
2026-03-11 16:49:33 +0000

    ed48c41 refactor(compiler-cli): simplifies `IncrementalCompilation.fresh`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.2+sha-7513558`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.2+sha-ed48c41`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
