291c26a104c942e2ec8d113abd6170388749ca6f
Andrew Scott <atscott01@gmail.com>
2026-03-11 17:20:49 +0000

    4c532bd build: lock file maintenance

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.1+sha-66e72ef`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.1+sha-4c532bd`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
