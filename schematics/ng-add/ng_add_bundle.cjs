ad61e3eccd93f244c489a5f86a6b04838ba90bcb
Andrew Scott <atscott01@gmail.com>
2026-03-11 18:40:26 +0000

    21b1c3b fix(core): include signal debug names in their `toString()` representation

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.2+sha-6c73aac`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.2+sha-21b1c3b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
