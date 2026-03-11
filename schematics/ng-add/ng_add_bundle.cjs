a920558e76ba6a9c3013c4b57b8f5f8efd9af861
Andrew Scott <atscott01@gmail.com>
2026-03-11 21:38:05 +0000

    8ba2844 docs: document coverageExclude option for excluding files from coverage

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.3+sha-74698d5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.3+sha-8ba2844`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
