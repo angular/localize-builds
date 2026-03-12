a3e920967dfd354624f1243f05c92497f93aff3c
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 22:29:11 +0000

    8edcf41 refactor(compiler-cli): add warning for matching viewport @defer trigger options

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-a929494`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-8edcf41`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
