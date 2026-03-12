2500c9983af98e8df86263eb40221b9cfbe4dab8
Matthew Beck (Berry) <me@mattrb.com>
2026-03-12 23:42:47 +0000

    17d8a88 refactor(core): Update determineLongestAnimation to account for playback rate

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-89c9a4d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-17d8a88`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
