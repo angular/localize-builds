6b3a6b1d22cabae96d94b692d4122a1f033a34a4
Matthew Beck <me@mattrb.com>
2026-03-12 17:11:49 +0000

    ed2d324 fix(compiler): disallow translations of iframe src

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.3+sha-c06e3a0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.3+sha-ed2d324`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
