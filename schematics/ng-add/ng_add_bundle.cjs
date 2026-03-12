5713a0e77907deb7dacb7caefb8ccaa351e5cbd9
Matthew Beck <me@mattrb.com>
2026-03-12 21:06:28 +0000

    5d755be refactor: ensure modules with 'declare global' are correctly handled by Rollup

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.4+sha-8017617`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.4+sha-5d755be`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
