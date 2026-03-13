52113df202202d94e7a2c96810967fb765465934
Matthew Beck (Berry) <me@mattrb.com>
2026-03-13 22:33:56 +0000

    ada150c build: update cross-repo angular dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-e127927`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-ada150c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
