a6fcce62ff660b8e3a27d6a539a056ea2a75d466
Matthew Beck (Berry) <me@mattrb.com>
2026-03-13 19:21:21 +0000

    e127927 docs: combine multiple documentation typo fixes

--- fesm2022/_localize-chunk.mjs
+++ fesm2022/_localize-chunk.mjs
@@ -1,5 +1,5 @@
 /**
- * @license Angular v22.0.0-next.3+sha-412788f
+ * @license Angular v22.0.0-next.3+sha-e127927
  * (c) 2010-2026 Google LLC. https://angular.dev/
  * License: MIT
  */
@@ -18,7 +18,7 @@ function fingerprint(str) {
   let lo = hash32(view, utf8.length, 102072);
   if (hi == 0 && (lo == 0 || lo == 1)) {
     hi = hi ^ 0x130f9bef;
-    lo = lo ^ -0x6b5f56d8;
+    lo = lo ^ -1801410264;
   }
   return BigInt.asUintN(32, BigInt(hi)) << BigInt(32) | BigInt.asUintN(32, BigInt(lo));
 }
--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-412788f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-e127927`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
