9b6a15529a2e208b0a3ad8b23bcc4ad9cdec7a7e
Matthew Beck (Berry) <me@mattrb.com>
2026-03-16 18:13:56 +0000

    dc174d1 refactor(devtools): introduce prop-actions-menu

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-45641f2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-dc174d1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
