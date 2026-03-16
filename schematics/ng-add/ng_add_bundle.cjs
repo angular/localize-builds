fff7dd79f816997fae11985a6224a433d00c7c63
Matthew Beck <me@mattrb.com>
2026-03-16 16:20:07 +0000

    1890c30 fix(core): clean up dehydrated views during HMR component replacement

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.4+sha-f39896d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.4+sha-1890c30`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
