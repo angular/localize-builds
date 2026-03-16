11a93c0f7a4d0d783c1eb6bb450fe3323a8b1831
Matthew Beck (Berry) <me@mattrb.com>
2026-03-16 21:31:49 +0000

    a94958b refactor(forms): Address more feedback

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-470cf43`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-a94958b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
