313f83bd8b2149482b7d77fb97e4438c555973c6
Matthew Beck <me@mattrb.com>
2026-03-17 20:07:35 +0000

    a8f80c1 refactor(core): declare explicit reactive node prototypes types

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.4+sha-67e0ba7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.4+sha-a8f80c1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
