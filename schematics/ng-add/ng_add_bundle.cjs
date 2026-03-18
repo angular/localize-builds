81c7aeb5c8195c6c322bf730bdbaafb6c97f91b4
Matthew Beck (Berry) <me@mattrb.com>
2026-03-18 23:41:17 +0000

    57ce2b6 release: cut the v21.2.5 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.4+sha-5393d8e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.5+sha-57ce2b6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
