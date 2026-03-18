7be80de10109f9d26a45df5a83553ee13d2478cb
Matthew Beck (Berry) <me@mattrb.com>
2026-03-18 17:41:00 +0000

    890c973 Revert "refactor(core): Ensure determineLongestAnimation is run synchronously after style applies"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-0b697f1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.3+sha-890c973`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
