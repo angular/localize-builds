a2c1137cd9704f7c30492740582f2f50b69cd9ca
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-19 23:14:05 +0000

    9182b62 docs(forms): add section on managing validators dynamically in form validation guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.5+sha-ad0156e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.5+sha-9182b62`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
