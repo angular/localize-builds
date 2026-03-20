22c9fdc8a9e941a717d84caea301d1221ca52180
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-20 22:28:26 +0000

    621c908 build: update cross-repo angular dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.4+sha-50e599e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.4+sha-621c908`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
