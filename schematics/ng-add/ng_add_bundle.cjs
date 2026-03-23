4444de49338f397caeb4c2aae9885fa6af26b315
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-23 21:56:59 +0000

    c7a1892 fix(devtools): fix incorrect logic in destroy function for ChromeMessageBus

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.5+sha-b658acc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.5+sha-c7a1892`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
