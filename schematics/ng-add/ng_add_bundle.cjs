ca752c3af377b331dbfc20306d67fc5dc514f09f
Leon Senft <leonsenft@users.noreply.github.com>
2026-03-25 23:11:28 +0000

    d4c085d release: cut the v22.0.0-next.5 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.4+sha-f187d23`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.5+sha-d4c085d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
