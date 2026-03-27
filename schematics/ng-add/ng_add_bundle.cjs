7fd2e2e1524773a5b566f98979070d5a5f05094b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-27 16:25:26 +0000

    9b48cb2 docs(docs-infra): introduce a custom UrlSerializer

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.6+sha-bba5ed8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.6+sha-9b48cb2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
