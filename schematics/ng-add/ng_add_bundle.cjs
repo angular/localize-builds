69129e01997b7d7e8f15274f78c834e98f11eb72
Andrew Scott <atscott@google.com>
2026-03-27 18:49:09 +0000

    0c9c270 docs: add agent skills documentation and new readme

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.6+sha-9b48cb2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.6+sha-0c9c270`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
