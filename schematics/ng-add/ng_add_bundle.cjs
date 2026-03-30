cf299da84434f26016b051932eba52c16efd93f7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-03-30 09:16:12 +0000

    713eca7 docs: update signal form tutorial

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.5+sha-c6f73f4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.5+sha-713eca7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
