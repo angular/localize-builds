52cc3eb6ffdef1d483917ff6a3f562b33bb1648e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 06:45:08 +0000

    7ba8929 test: fix flakiness in `image-directive` e2e tests

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.5+sha-ef7679b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.5+sha-7ba8929`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
