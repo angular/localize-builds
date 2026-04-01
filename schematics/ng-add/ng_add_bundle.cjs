0d300ca129c8ede5bd4d1a4504425e139156f117
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-04-01 09:53:50 +0000

    daa9b2a fix(router): pass outlet context to split to fix empty path named outlets

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.5+sha-f257f54`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.5+sha-daa9b2a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
