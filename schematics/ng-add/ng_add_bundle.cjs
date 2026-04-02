0d44c8a3ccc6f114c6ebb4322f06ef2a3fe69042
Andrew Scott <atscott01@gmail.com>
2026-04-02 23:10:16 +0000

    9d76ac8 refactor(core): remove ComponentFactoryResolver & ComponentFactory from the api surface

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-164cf98`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-9d76ac8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
