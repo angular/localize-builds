0f40bd69c928ec952532578b0a9174684a5fd44c
Andrew Scott <atscott01@gmail.com>
2026-04-06 18:41:53 +0000

    33b001d fix(devtools): clean up removed directive entries in IdentityTracker to prevent memory leak

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-2cb67c0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-33b001d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
