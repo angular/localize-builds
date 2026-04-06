3badb4dd80d75075053bd62c171526d80c7145ec
Andrew Scott <atscott01@gmail.com>
2026-04-06 18:11:35 +0000

    1ef503e test(http): disable XSRF and mock location in HttpClient tests to avoid Domino failures and state leakage

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-d95e856`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-1ef503e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
