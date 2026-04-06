9ad1800999bc8a2a6c08ae12bb5fb9bc35292ffb
Andrew Scott <atscott01@gmail.com>
2026-04-06 18:02:56 +0000

    d95e856 fix(docs-infra): align search result icon with text on mobile

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-ecae525`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-d95e856`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
