579b8ad50f4d816b20911583c6393112672dd00d
Andrew Scott <atscott01@gmail.com>
2026-04-07 21:34:53 +0000

    de56d74 fix(forms): align FormField CVA selection priority with standard forms

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-a0aa830`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-de56d74`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
