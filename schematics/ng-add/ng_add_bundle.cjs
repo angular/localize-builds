4f28f91f15ab7c1f9049c014db8c3c464a3b840c
Andrew Scott <atscott01@gmail.com>
2026-04-08 18:43:05 +0000

    6fee651 refactor(compiler-cli): decouple SymbolBuilder from BoundTarget and minimize adapter surface

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-c7518a4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-6fee651`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
