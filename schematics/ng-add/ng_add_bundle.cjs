d2be2d564e173ed50c7093d6b8318fbf468cac4b
Andrew Scott <atscott01@gmail.com>
2026-04-08 15:21:19 +0000

    30c950f refactor(compiler-cli): Fix regressions caused by ts.typechecker removal

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-e95d846`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.6+sha-30c950f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
