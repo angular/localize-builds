2701b9ceb6ad1664f37233be14e9c13e55429a0e
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-09 15:19:00 +0000

    b9265cc docs: update typescript version requirement for angular 22

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-1a16a40`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-b9265cc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
