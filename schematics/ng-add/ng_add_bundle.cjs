32a496fc8abfcbacfc87a6b445219000aa05c680
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-09 13:06:43 +0000

    1a16a40 docs: update cross-repo adev docs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-db9f9a8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-1a16a40`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
