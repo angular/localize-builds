b79a8308ef3865ff182d1ebcffe9e0e08f70f07b
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-09 12:46:58 +0000

    db9f9a8 build: update all non-major dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-5516769`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-db9f9a8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
