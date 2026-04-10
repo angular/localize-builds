c30c58879f2477e3b5c5fa6fcc6bc677aa53566a
kirjs <kirts@google.com>
2026-04-10 14:48:19 +0000

    b4a747a docs(docs-infra): fix homepage nav overlay and banner visibility between 701–900px

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-b52a626`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-b4a747a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
