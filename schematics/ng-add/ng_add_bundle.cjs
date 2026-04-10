84bc394630dd1c0b5b16387819dae54815206f6f
kirjs <kirts@google.com>
2026-04-10 17:28:09 +0000

    b1407e1 build: update rules_angular setup in MODULE.bazel

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-b4a747a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-b1407e1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
