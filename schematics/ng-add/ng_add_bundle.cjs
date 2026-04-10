f098af486aec7d6510e17dbd9cf138279dcbf986
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-10 13:54:22 +0000

    337e6e7 refactor: add flaky tests workflow

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-0304228`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-337e6e7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
