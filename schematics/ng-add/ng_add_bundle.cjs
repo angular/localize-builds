d525a8177c5270f44b93f5370178e5f8b2cb7e72
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 18:26:29 +0000

    cb19c69 docs(devtools): create router tree documentation

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-eb2b06f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-cb19c69`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
