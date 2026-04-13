57a0edc5548edb63bcc709c573e3e5cf3ec4eda8
kirjs <kirts@google.com>
2026-04-13 07:57:44 +0000

    b351d49 docs(docs-infra): fix essentials next-step navigation pills

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-b1407e1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-b351d49`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
