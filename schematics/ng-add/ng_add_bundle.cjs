114705cbf636c5a7d9e3ffc6abad5633a250d3ce
kirjs <kirts@google.com>
2026-04-13 13:10:13 +0000

    540536c fix(http): add CSP nonce support to JsonpClientBackend

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-f603d47`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-540536c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
