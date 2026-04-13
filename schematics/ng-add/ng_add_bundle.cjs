8eef9565feb7e593724abbafb401d6e10ad62e54
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 18:04:19 +0000

    eb2b06f docs: draft PR spam policy addendum

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-0454d4c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-eb2b06f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
