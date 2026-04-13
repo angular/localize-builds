a6af9dc5eacf72960b19508e992af1d22afe97f6
kirjs <kirts@google.com>
2026-04-13 08:30:34 +0000

    3ed14c6 refactor: Mobile layout api reference Fix mobile layout shift in API reference

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-245bcdd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-3ed14c6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
