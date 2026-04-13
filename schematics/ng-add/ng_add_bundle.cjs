2eb0360392c77bcb9c5fb9798f66439ddb84e542
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-13 08:24:11 +0000

    59513b7 docs: add dev-app section to contributing docs docs: link directly to dev-app `README.md`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-c3d4be4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-59513b7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
