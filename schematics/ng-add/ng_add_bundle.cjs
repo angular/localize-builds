10ea3987558fa87de34d4e02fed79ba467b58ec1
GitHub <noreply@github.com>
2026-04-13 19:47:32 +0000

    2f5ab54 feat(core): enhance profiling with documentation URLs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-f9b74e9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-2f5ab54`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
