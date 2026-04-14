ba320a52cca62710e8f0f2e98581aaa07a7c76fd
kirjs <kirts@google.com>
2026-04-14 09:45:40 +0000

    684e9fd fix(router): normalize multiple leading slashes in URL parser

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-bb8cdd9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-684e9fd`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
