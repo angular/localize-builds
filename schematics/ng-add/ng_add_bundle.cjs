cd3dc318a5bca3a08ca0a4e866c8e5684590ef4f
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 15:00:07 +0000

    8b4581d build: lock file maintenance

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-e32159b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-8b4581d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
