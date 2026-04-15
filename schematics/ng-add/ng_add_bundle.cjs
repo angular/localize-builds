6ad7dcbb8c81114206370964332eb17cfef8c4e8
Kirill Cherkashin <kirill@kcherkashin.org>
2026-04-15 17:16:09 +0000

    281a2db docs: Add guide for debounced signals

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-8b4581d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.7+sha-281a2db`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
