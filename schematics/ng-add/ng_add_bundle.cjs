48804cd4e3c70a469b36ad6f34e8be3d434d0409
kirjs <kirts@google.com>
2026-04-15 16:54:29 +0000

    32a8302 docs: add new signal forms - form submission guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-4f65bb3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.8+sha-32a8302`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
