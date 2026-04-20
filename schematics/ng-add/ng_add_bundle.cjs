dc747983781b31fd80de813dc3257534373a4a7e
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 16:37:24 +0000

    a718e18 docs: improve clarity in dynamic forms guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.8+sha-a0d4563`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.8+sha-a718e18`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
