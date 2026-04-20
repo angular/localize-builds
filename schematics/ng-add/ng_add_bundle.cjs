2f483a07357ac238ad08d8085b2c0a1c5c778974
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 20:28:45 +0000

    c04c0b9 docs(docs-infra): guard sandbox reset before initialization in playground

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.8+sha-3ae40e6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.8+sha-c04c0b9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
