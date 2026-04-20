a4d5aeb04c77c0ca3ff6c9add1f6af657016ef58
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 16:59:36 +0000

    c610425 docs: fix wording in reactive forms guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.8+sha-a718e18`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.8+sha-c610425`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
