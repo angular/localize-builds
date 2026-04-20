1a702368114d66f0f7c64e95f0799f61e0c80a9b
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-20 20:03:43 +0000

    c9215b3 Revert "refactor(core): complete removal of deprecated `createNgModuleRef` alias"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.9+sha-e5b93ea`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.9+sha-c9215b3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
