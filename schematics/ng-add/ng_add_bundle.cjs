1e6d7478b89282cf4fb8cc799b3e6a22e5a66b10
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-22 17:17:53 +0000

    5533ab4 fix(migrations): fix NgClass leaving trailing comma after removal

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.9+sha-4dc7bf5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.9+sha-5533ab4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
