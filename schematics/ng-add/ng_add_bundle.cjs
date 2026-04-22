81df6f5d64f80becdaa63df6426ea1bbfc43b9a8
Leon Senft <leonsenft@users.noreply.github.com>
2026-04-22 21:49:39 +0000

    7f3f3d7 ci: remove remainings of saucelabs tests

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.8+sha-2c141c0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.8+sha-7f3f3d7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
