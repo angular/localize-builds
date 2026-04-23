a3784eacdced71ab0e1c0c6997b856e113eb0dda
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-23 21:24:01 +0000

    f3308dc build: update cross-repo angular dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.10+sha-f9b70a0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.10+sha-f3308dc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
