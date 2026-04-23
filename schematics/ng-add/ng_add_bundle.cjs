3dbdf406104a1f73353408ea829a2e0796860477
Alon Mishne <amishne@users.noreply.github.com>
2026-04-23 18:48:42 +0000

    a8dd801 docs: link Angular CLI in installation prerequisites

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.10+sha-2ec753c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.10+sha-a8dd801`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
