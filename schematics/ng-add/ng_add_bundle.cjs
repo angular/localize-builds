c24973509c80386222b7a4a0ed645426f703da91
Alon Mishne <amishne@users.noreply.github.com>
2026-04-28 17:39:46 +0000

    fb7e67c docs: use inject() in @Self example in hierarchical DI guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.10+sha-9bcbf37`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.10+sha-fb7e67c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
