83f2dde7cac1716fcea0ec1d4d92cf4d21ae7901
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-28 19:52:12 +0000

    8f8972b feat(migrations): model + output migrations becomes input + linkedSignal

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.9+sha-5dfe37d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.9+sha-8f8972b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
