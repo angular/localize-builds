8a77c7fefa84e5dfb1fe36779113f41d5a66ba25
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-28 23:48:18 +0000

    42d57c3 refactor(common): fix viewport tests

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.10+sha-10ad3c0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.10+sha-42d57c3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
