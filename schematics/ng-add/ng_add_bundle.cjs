fc5c1f8b6cd171a3b4ecf737812aa769ea2c4341
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-29 20:51:05 +0000

    af24a4a fix(vscode-extension): Look for tsdk override in the new js/ts.tsdk.path setting

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.9+sha-37ec63e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.9+sha-af24a4a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
