ccf82a047c50b57c06802a5b1151c8f94bcb495b
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-29 23:20:36 +0000

    4c9afb6 fix(core): respect ngSkipHydration on components with projectable nodes in LContainers

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.9+sha-662560e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.10+sha-4c9afb6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
