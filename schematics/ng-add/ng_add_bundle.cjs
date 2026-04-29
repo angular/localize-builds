673a338d74d7d765541b3e5e2a6945bbb9458ebd
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-04-29 22:46:29 +0000

    ef9669c release: cut the v21.2.11 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.10+sha-6c6db5b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.11+sha-ef9669c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
