3e960801bfa634c8c3bd4056054e67e24ed37fde
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-04-30 23:04:06 +0000

    ebffd80 build: update all github actions

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.10+sha-4c9afb6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.10+sha-ebffd80`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
