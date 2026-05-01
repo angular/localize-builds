d9459a011cfce63d55ae80b4ed5639fdf4de9c64
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-01 23:11:48 +0000

    c84642a feat(router): add unmatchedInputBehavior option to componentInputBinding

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.10+sha-a4145de`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.10+sha-c84642a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
