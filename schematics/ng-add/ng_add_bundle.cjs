fe5d9ae72f827ea3d8f0809c8a473c0f77dbd565
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-05 16:48:20 +0000

    fc52633 build: lock file maintenance

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.10+sha-4f048e7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.10+sha-fc52633`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
