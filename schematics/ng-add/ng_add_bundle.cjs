eecb21fd787ae3d0f6ec9780a3ed6fb5d6025c76
GitHub <noreply@github.com>
2026-05-05 23:04:33 +0000

    f81fa6e docs: add documentation for lazy loading services and update navigation

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.10+sha-8a7f955`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.10+sha-f81fa6e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
