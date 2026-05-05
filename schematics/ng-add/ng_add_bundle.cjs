79e19d38a0639d022b212ffca6e90f6764b00b4a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-05 23:38:08 +0000

    3524de2 fix(forms): Add support for range type with outside of native bounds

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.10+sha-f81fa6e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.10+sha-3524de2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
