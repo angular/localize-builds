72db7b1cc169746e03af18c0108b95652d2bed3c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-06 21:50:41 +0000

    25e4e07 fix(platform-server): ensure origin has a trailing slash when parsing url (#68468)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.19+sha-b9ec542`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.19+sha-25e4e07`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
