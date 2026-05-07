69cbe3c42d4df82588628f8f4f1758699e464cbc
Matthew Beck (Berry) <me@mattrb.com>
2026-05-07 22:03:53 +0000

    2fcfffb docs: Update error display for password field in signal forms playground

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-f1738b5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-2fcfffb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
