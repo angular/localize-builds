4aabc093fb441abc187a2f69fdc3a5b3dec10b5d
Matthew Beck (Berry) <me@mattrb.com>
2026-05-07 22:29:48 +0000

    f584840 fix(platform-server): add `allowedHosts` option to `renderModule` and `renderApplication`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.20+sha-4aee744`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.20+sha-f584840`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
