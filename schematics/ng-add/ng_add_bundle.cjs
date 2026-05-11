992e8fde2b3ebf233f785978e706ed356ca5b7b5
Matthew Beck <me@mattrb.com>
2026-05-11 18:55:27 +0000

    5a6e946 docs: remove experimental warnings from resource/signal forms documentation

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.12+sha-119a19e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.12+sha-5a6e946`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
