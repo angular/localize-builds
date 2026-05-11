0e2aa02964da3eae6a9659d088835f01c67c9f7e
Matthew Beck <me@mattrb.com>
2026-05-11 19:17:18 +0000

    ff21eb9 docs: add new signal forms field metadata guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.12+sha-5a6e946`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.12+sha-ff21eb9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
