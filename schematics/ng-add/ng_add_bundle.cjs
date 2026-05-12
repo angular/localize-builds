94bb6f4e288975082bd036dc9e3b876a8cbcbe89
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 17:19:06 +0000

    591fa53 release: cut the v20.3.21 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.20+sha-f584840`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.21+sha-591fa53`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
