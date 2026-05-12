20d278125c373c30f10c9c38085744f517aefcc3
Matthew Beck <me@mattrb.com>
2026-05-12 21:44:34 +0000

    7dc1017 fix(compiler): simplify handling of colon host with a selector list

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.12+sha-a4ec2b4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.12+sha-7dc1017`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
