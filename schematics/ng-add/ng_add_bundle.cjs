bf51cc50c93e461732e4bb9c500f12bee8d0ef8a
Matthew Beck <me@mattrb.com>
2026-05-12 17:58:34 +0000

    cc0fa6e refactor(http): update HTTP resource options APIs to stable

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.12+sha-e9d1c7e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.12+sha-cc0fa6e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
