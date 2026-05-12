720055b916457f452a2c37bfc1fdf025d2e13882
Matthew Beck <me@mattrb.com>
2026-05-12 20:35:59 +0000

    a4ec2b4 refactor(compiler-cli): drop @ts-ignore around fs.readFileSync

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.12+sha-60eac0e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.12+sha-a4ec2b4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
