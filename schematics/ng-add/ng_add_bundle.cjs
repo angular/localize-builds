77b7e60d862f1023b853a32bd00ba2efb15bdb41
Matthew Beck (Berry) <me@mattrb.com>
2026-05-12 21:45:07 +0000

    6a07fdd build: update @angular/ng-dev

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-e0593ec`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-6a07fdd`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
