6d4a0c5bed2e0f6b660db584a7e483ae5caf0715
Matthew Beck (Berry) <me@mattrb.com>
2026-05-13 23:40:29 +0000

    5ca0c5a release: cut the v22.0.0-rc.0 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.12+sha-639a785`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.0+sha-5ca0c5a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
