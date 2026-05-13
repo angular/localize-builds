788bf83a5f9af24a6ddc8462eb71ab4e065b1c1b
Matthew Beck <me@mattrb.com>
2026-05-13 22:45:20 +0000

    639a785 docs: add version 22 to the browser support reference table

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.12+sha-1f0c1ab`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-next.12+sha-639a785`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
