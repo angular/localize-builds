d1da14ce75962929a74ef8b9341915c030852563
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-15 18:55:37 +0000

    2959d6b docs: update cross-repo adev docs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-358d2e6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-2959d6b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
