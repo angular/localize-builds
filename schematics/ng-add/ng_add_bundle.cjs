c5bb15bef8a5ad5d8cfabce3e3160fcc993e7528
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-15 19:24:30 +0000

    9a7dedc docs: use when in signal forms rule examples

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-2959d6b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-9a7dedc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
