ecf67aa6e684a222252d08c205a9922c750c00ca
leonsenft <leonsenft@users.noreply.github.com>
2026-05-15 17:51:39 +0000

    fc434c1 refactor(compiler-cli): Remove unused properties of IndexedComponent interface

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.13+sha-8282c09`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.13+sha-fc434c1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
