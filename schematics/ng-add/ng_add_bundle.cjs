1b24972ac2dd361ba48b5f2fb1ddd978ea2435e2
leonsenft <leonsenft@users.noreply.github.com>
2026-05-19 20:51:14 +0000

    0e1ec0c docs: update HTTP testing setup guidance

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.0+sha-38aca8f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.0+sha-0e1ec0c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
