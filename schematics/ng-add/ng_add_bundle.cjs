adb944424e8cd4622ad7b44ee6bb519df3435610
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-19 21:11:02 +0000

    048817d fix(core): visit ICU expressions in signal migration schematics

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-7c60a98`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-048817d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
