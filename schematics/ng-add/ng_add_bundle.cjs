1b62fffde89f0f929028a352d8b23e5f55ddf1b2
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-20 18:29:53 +0000

    ca44055 docs: clarify ngDoCheck invocation behavior with OnPush strategy

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-cb95723`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-ca44055`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
