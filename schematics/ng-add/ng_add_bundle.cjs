cd1b2d058f309a31630e0faf34fc954798c92c5c
Leon Senft <leonsenft@users.noreply.github.com>
2026-05-20 17:58:23 +0000

    cbddb5c build: update bazel dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.0+sha-83026cf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.0+sha-cbddb5c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
