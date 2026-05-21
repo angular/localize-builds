c23db21ec07e18b8314ab004072b2e7c994015c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-21 18:10:07 +0000

    3b4ef1e perf(forms): avoid redundant invalidations in parser errors signal

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.1+sha-d8a848b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.1+sha-3b4ef1e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
