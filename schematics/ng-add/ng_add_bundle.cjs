99cea94cc1318531a8600062109d084871d0fcf8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-22 17:15:20 +0000

    6d8b156 fix(vscode-extension): restrict jsdoc markdown trust and harden document opening

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.14+sha-3fe8562`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.14+sha-6d8b156`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
