7c0cbac9efacbc28f71d92a586e8f0fa57faf394
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 23:50:32 +0000

    d88b796 build: update cross-repo angular dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.1+sha-618c850`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.1+sha-d88b796`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
