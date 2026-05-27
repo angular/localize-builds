6160175f2d46a11c20b8675cab1104c6f20672bd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-05-27 18:37:43 +0000

    29ceeff docs: fix typos in source code comments

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.14+sha-2b17b2d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.14+sha-29ceeff`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
