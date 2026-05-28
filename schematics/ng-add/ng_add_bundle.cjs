fda36cdbce1e4429e5b214c40be16d7324a01f09
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 14:52:09 +0000

    e2ccea9 docs: release notes for the v19.2.24 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-25922d1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-e2ccea9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
