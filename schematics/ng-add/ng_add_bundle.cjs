03a90eaad66c2ca4fd9b811bc998d73ead86a362
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 12:14:33 +0000

    eb1cbbf fix(compiler): prevent namespaced SVG <style> elements from being stripped

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.14+sha-8538bdc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.14+sha-eb1cbbf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
