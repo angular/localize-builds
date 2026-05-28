f2b1f52365c0016ca77df3785bb2219dac7fbe33
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 14:35:59 +0000

    25922d1 docs: release notes for the v20.3.23 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-80d6a3b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-25922d1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
