caf8daa5ae0d88df9013fcf8fc0a65763868a131
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 11:54:00 +0000

    8538bdc docs: fix grammar issues in resource guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.14+sha-582a417`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.14+sha-8538bdc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
