359eb7b7f039ce3f0973cc80493b15991ce370c7
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-28 14:15:10 +0000

    6b8202e refactor(docs-infra): extract magic 27 in navigation-list tooltip threshold

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.14+sha-eb1cbbf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.14+sha-6b8202e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
