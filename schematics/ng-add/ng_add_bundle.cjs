d24fa6744124b6e6b9dcbb6eab2f37c7e4a4297a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 09:59:50 +0000

    c2153b1 docs: document i18n object forms for sourceLocale and locales in angular.json

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.2+sha-ebd698b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.2+sha-c2153b1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
