890c1bdb73e4ab7374a6a2731ef856d47f27c2a5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-05-29 11:26:40 +0000

    d109bf9 fix(common): only strip a literal /index.html suffix from URLs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-89c28d3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-d109bf9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
