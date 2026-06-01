104981c65d36ca58914c920134f80bb32100b584
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 16:42:14 +0000

    ec4f08b docs(docs-infra): simplify code block styles

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-5215f73`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-ec4f08b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
