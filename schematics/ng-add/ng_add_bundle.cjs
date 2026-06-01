4a6c9873ede77532a03635a57ec965e963f2b467
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 10:34:25 +0000

    f0b28f6 fix(compiler): move projection attributes into constants

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-f777dd1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-f0b28f6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
