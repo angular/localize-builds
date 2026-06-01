4333fff4e3567559176eff8ae8f6f1964780bcda
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-01 14:40:54 +0000

    61b3483 docs: align `main` and `22.0.x` for aria guides

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.2+sha-2891f7e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.2+sha-61b3483`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
