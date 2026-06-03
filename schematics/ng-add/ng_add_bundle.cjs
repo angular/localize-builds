001804c3cd2585d4ac56b7867fdf773549ce1565
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-06-03 09:29:30 +0000

    3401189 docs: add Signal Forms integration example to Angular Aria Autocomplete guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.3+sha-e10647b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0-rc.3+sha-3401189`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
