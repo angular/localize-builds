5e736756ae78a9ccfd1068ef9d9b9dd28e193e96
Andrew Scott <atscott01@gmail.com>
2026-06-03 19:39:47 +0000

    8e4fd59 build: deprecate @angular/animations and @angular/platform-browser-dynamic

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-3d9ca2f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-8e4fd59`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
