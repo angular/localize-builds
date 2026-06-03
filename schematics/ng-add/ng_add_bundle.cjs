84247648c444d2569c850f469da08986c9d05a00
Andrew Scott <atscott01@gmail.com>
2026-06-03 19:17:36 +0000

    3d9ca2f fix(compiler-cli): bind switch exhaustive check expressions

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-32b348a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-3d9ca2f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
