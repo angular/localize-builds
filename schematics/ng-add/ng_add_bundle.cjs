2308501c2e0d33640c4a639d22d34b53b78c9da8
Andrew Scott <atscott01@gmail.com>
2026-06-03 19:35:45 +0000

    1523061 fix(core): harden TransferState restoration against DOM clobbering

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.16+sha-03188dd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.16+sha-1523061`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
