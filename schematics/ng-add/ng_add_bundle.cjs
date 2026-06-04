7ed891142c2dda1b867636d5e14fdcbd883a9d81
Andrew Scott <atscott01@gmail.com>
2026-06-04 18:51:39 +0000

    669146b fix(core): disable WebMCP during SSR

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-302cb60`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-669146b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
