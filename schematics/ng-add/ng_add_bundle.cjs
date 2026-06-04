e896c1bb43eceb2e65862978c239a6036feb7f9d
Andrew Scott <atscott01@gmail.com>
2026-06-04 18:28:29 +0000

    a68ec70 fix(compiler): sanitize two-way properties

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.24+sha-566ad05`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.24+sha-a68ec70`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
