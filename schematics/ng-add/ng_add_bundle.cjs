5aa44e3dc1be489eac5dc6dce272cc1c92e47a7f
Andrew Scott <atscott01@gmail.com>
2026-06-05 17:49:42 +0000

    1a62130 fix(common): use cryptographically secure SHA-256 for transfer cache key generation

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.24+sha-49368c1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.24+sha-1a62130`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
