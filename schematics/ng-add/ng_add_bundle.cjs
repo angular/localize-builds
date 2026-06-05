a74b62901f5b42c4f2af2e21ed73569b8ff3fbfe
Andrew Scott <atscott01@gmail.com>
2026-06-05 17:53:49 +0000

    ed2420c fix(vscode-extension): inspect tsdk configurations correctly

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-af04e26`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-ed2420c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
