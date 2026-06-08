c89422ce3dc3b8f578cb3b1e6c9de7d808d82a78
Andrew Scott <atscott01@gmail.com>
2026-06-08 21:31:53 +0000

    e7dae12 fix(docs-infra): avoid code copy button overlap

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-167bd4c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-e7dae12`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
