e73dfa2fa555783a7ca952a83b80c36c1bc23685
Andrew Scott <atscott01@gmail.com>
2026-06-08 18:27:05 +0000

    a7d6845 refactor(devtools): use ng-filter for the router tree search

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-a816848`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-a7d6845`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
