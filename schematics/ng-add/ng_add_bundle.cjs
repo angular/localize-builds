12a1896d554f66942c21c62b0749ca5132a3298a
Andrew Scott <atscott01@gmail.com>
2026-06-08 20:21:40 +0000

    d65a5f4 fix(service-worker): Strips sensitive headers on cross-origin redirects

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.24+sha-1a62130`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.24+sha-d65a5f4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
