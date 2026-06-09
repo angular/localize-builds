5abe543f19c45d04c386ee8c114abc1158a8622d
GitHub <noreply@github.com>
2026-06-09 17:13:55 +0000

    13fb0af refactor(platform-server): deprecate ServerXhr (#69255)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.16+sha-86a56dc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.16+sha-13fb0af`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
