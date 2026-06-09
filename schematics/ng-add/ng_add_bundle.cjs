1ca7303b13da3ddd266147247ea62bd73ea59c46
Andrew Scott <atscott01@gmail.com>
2026-06-09 20:22:31 +0000

    4289c4c fix(core): update comment for Default change detection

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-562a566`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-4289c4c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
