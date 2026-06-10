5f33481e84f93cf89e886d10b5b1bcabdf77938c
GitHub <noreply@github.com>
2026-06-10 16:56:20 +0000

    ca48b47 fix(core): validate lowercase SVG animation attribute names (#69270)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.24+sha-d55c94a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.24+sha-ca48b47`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
