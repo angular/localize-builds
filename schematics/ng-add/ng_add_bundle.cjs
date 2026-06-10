6aedc6b48700816ac429a9bdc7280e87fb74bee8
Andrew Scott <atscott01@gmail.com>
2026-06-10 18:33:56 +0000

    b416da6 docs: add caching guidance for resource data with SSR

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-f6695b0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-b416da6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
