91a097c8c09cf96296e7a3ad06de2caaf5d6e994
Andrew Scott <atscott01@gmail.com>
2026-06-10 20:24:58 +0000

    9f4b3dc docs: release notes for the v22.0.1 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-7e94ab9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-9f4b3dc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
