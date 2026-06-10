68c93c5b7da9fe37294367a556b4d285f0f780c1
Andrew Scott <atscott01@gmail.com>
2026-06-10 18:09:07 +0000

    4645850 refactor(compiler): Remove 80 char limit on AbstractEmitterVisitor

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-e51ad37`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-4645850`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
