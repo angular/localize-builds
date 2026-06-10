67a536142ed6f0661e450042f55ab969bf156d18
Andrew Scott <atscott01@gmail.com>
2026-06-10 20:47:01 +0000

    6f3e832 release: cut the v22.1.0-next.0 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-9f4b3dc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-6f3e832`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
