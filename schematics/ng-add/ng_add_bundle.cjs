edaed6ab8ca7a30d33e431e870c281a6750797a1
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-10 22:48:30 +0000

    71bb19d build: lock file maintenance

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-6f3e832`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-71bb19d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
