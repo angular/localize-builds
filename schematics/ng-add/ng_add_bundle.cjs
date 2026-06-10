9059ed8d5bfee3d8e64a4c2b89bad52785bdbdd2
GitHub <noreply@github.com>
2026-06-10 19:28:38 +0000

    a075161 release: cut the v20.3.25 release (#69288)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.24+sha-ca48b47`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.25+sha-a075161`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
