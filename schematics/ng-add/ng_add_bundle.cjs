fe7d4ee0c44f03d63a5215331354d5501f52416c
Andrew Scott <atscott01@gmail.com>
2026-06-10 00:17:51 +0000

    6e3d51d refactor(migrations): Improve `safeNavigationMigration` heuristic

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-01ea640`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-6e3d51d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
