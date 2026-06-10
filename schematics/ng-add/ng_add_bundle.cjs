5688d254979d18b41fd2e4a322bce8733a20b494
Andrew Scott <atscott01@gmail.com>
2026-06-10 02:34:57 +0000

    85d2d10 fix(forms): harden FormGroup control lookups against prototype shadowing

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-6e3d51d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.0+sha-85d2d10`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
