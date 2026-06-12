ec50c31fe916420d551c678b750702c6a21cb274
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-12 17:24:18 +0000

    1d97355 docs: clarify value attribute on radio/checkbox inputs is allowed with formField

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.1+sha-15314c1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.1+sha-1d97355`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
