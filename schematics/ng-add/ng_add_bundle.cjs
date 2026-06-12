7c36f42a81fd2f1d1d4ca592c6644d0017cb7028
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-12 16:35:54 +0000

    15314c1 fix(migrations): migration skip any target are not build or test

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.1+sha-b7aa8dc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.1+sha-15314c1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
