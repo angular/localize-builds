cddd7269002e66d41a49a0bccf8c44b7126d1040
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-16 15:39:29 +0000

    93d0a5f fix(core): prevent unsubscribe during emit from throwing off other listeners

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.1+sha-fef8bcf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.1+sha-93d0a5f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
