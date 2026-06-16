3dce4bf75882e28b8214b74a61fa3bdd818ffbd0
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-06-16 17:35:47 +0000

    86ade07 refactor(compiler): add support for @Input transforms under isolatedDeclarations

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-0152e3c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.0+sha-86ade07`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
