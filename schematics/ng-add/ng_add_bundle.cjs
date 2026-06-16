647fd28d2281df36c78d0e5fb9c5db30d5d0cd56
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-16 16:17:07 +0000

    b32ee7c fix(core): treat iframe credentialless as security-sensitive

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.1+sha-f2c5045`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.1+sha-b32ee7c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
