6a73af29cc4e814cf27d72be0d8e55b3528f3603
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-06-16 15:55:42 +0000

    f2c5045 docs: use the @Service decorator in the learn-angular DI tutorial

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.1+sha-93d0a5f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.1+sha-f2c5045`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
