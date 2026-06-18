4bafece15881cb4de665720bb06e112ded1e51bc
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-18 18:31:01 +0000

    7e1fcd4 build: update all github actions to v6.0.9

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-e77a8a0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-7e1fcd4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
