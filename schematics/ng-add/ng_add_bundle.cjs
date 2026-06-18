c240e1ab3e11db221aae0b324b7a2c3de9b3f35b
kirjs <kirts@google.com>
2026-06-18 16:40:28 +0000

    3e7c83b Revert "fix(core): escape overlapping comment delimiters in escapeCommentText"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.2+sha-73880c5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.2+sha-3e7c83b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
