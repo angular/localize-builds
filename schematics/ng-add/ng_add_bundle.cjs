17620a1794c2d2f36a809a309ea274dc6f52e16c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-06-18 13:34:32 +0000

    c23ddd6 docs(docs-infra): remove deprecated ts flags

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-302dd0f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-c23ddd6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
