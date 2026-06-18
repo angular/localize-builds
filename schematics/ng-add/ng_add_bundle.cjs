a2a69d8a5da99b48ff4bb1be78578788b7e50b4c
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-18 16:40:08 +0000

    a849b6f Revert "fix(core): escape overlapping comment delimiters in escapeCommentText"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-1566442`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-a849b6f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
