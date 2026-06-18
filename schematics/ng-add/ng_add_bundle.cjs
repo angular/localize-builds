9350225d877035e01752f32457d507003f5cd58c
kirjs <kirts@google.com>
2026-06-18 14:58:30 +0000

    73880c5 fix(docs-infra): correct select dropdown scrolling, sizing, and selection

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.2+sha-291d574`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.2+sha-73880c5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
