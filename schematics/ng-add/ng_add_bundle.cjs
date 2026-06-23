101de646c9e1535cfe470524a7b0addd8c920e14
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-23 15:36:42 +0000

    dead64f docs: add error guide for NG05101

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-e5d7b3a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-dead64f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
