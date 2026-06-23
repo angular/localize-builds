317f84b8b17e88c575a53360ee28525ae0b41545
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-23 13:54:00 +0000

    a4e9ff1 docs: add missing space before decorator link in components guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-69ac1d5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-a4e9ff1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
