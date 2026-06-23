1310796ddae605dd7543a75d1ca6428e1792729b
kirjs <kirts@google.com>
2026-06-23 15:37:31 +0000

    f598dcf docs: add error guide for NG05101

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.2+sha-7766dc0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.2+sha-f598dcf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
