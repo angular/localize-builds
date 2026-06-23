a749112077b277b7ea7e8edb09fc7f4c963f7364
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-23 18:22:23 +0000

    06e9da4 docs: document support for standard forwarded proxy header

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-91d168e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-06e9da4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
