1afece3fe1f4e5af9f23ed569bdf487e0fd20194
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-23 17:06:39 +0000

    91d168e fix(core): avoid uncaught promise errors in injectAsync prefetching

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-dead64f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-91d168e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
