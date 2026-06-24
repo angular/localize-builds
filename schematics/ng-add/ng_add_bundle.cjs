f8a25283826acc5282763d5046d1d2fda65dbf22
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-24 14:46:26 +0000

    9d1d1de build: update dependency node to v22.23.1

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.2+sha-b7ab51d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.2+sha-9d1d1de`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
