4e32411ce8e8b95f1a10f7b79e00bc55b6e52a86
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-24 15:44:13 +0000

    12fcec8 docs(forms): clarify debounce('blur') usage with custom FormValueControl

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-edea40b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-12fcec8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
