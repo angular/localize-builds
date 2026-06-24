97527e76efb7aac21baab02102c2df642989fed8
Kirill Cherkashin <kirill@kcherkashin.org>
2026-06-24 15:34:08 +0000

    edea40b docs: add doc to focusBoundControl feature

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-ea17725`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.1+sha-edea40b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
