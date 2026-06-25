e3fbb4a2ca78e7025d7f97b8a7abb1010159bd07
Alan Agius <alan.agius4@gmail.com>
2026-06-25 15:17:44 +0000

    5ba6443 release: cut the v22.0.3 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.2+sha-f4f7f37`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.3+sha-5ba6443`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
