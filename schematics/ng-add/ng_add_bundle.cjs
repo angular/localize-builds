a5e3d8b5e7f762eb7b3cf69b93b299f6a03d6da3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-26 17:44:59 +0000

    32c1f30 docs: update Vitest configuration option from `configFile` to `runnerConfig`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.4+sha-0b1cbbd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.4+sha-32c1f30`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
