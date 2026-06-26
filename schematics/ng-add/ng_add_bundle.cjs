d734fc31eb7004e600c5c172189ac0fe49a666ea
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-26 17:11:14 +0000

    0b1cbbd release: cut the v22.0.4 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.3+sha-fd37f09`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.4+sha-0b1cbbd`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
