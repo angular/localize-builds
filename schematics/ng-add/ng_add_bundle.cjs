83d24db69001946d717f89e01fb488d3ab12ad91
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-26 16:24:43 +0000

    26b0c71 fix(migrations): resolve migration failure when tsconfig specifies rootDir

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.2+sha-28a90e3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.2+sha-26b0c71`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
