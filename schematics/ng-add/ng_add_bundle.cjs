b7721cd47f3860143b47b5afabb9291c943e836e
GitHub <noreply@github.com>
2026-06-29 21:39:31 +0000

    1659952 fix(compiler-cli): include toSignal in debugName transform

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.3+sha-d109cc5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.3+sha-1659952`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
