3d48a37014d96671d260eb93d19e9da8f406674d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-29 21:41:06 +0000

    baf09a9 fix(compiler-cli): include toSignal in debugName transform

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.4+sha-0c18ccb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.4+sha-baf09a9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
