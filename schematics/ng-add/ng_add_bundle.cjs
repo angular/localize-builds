4eead1af54c82eb7aeecc489ca4d0d455882d449
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-29 23:13:13 +0000

    b5ea340 refactor(compiler): remove `visitAttributeComment`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.4+sha-ca76b2c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.4+sha-b5ea340`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
