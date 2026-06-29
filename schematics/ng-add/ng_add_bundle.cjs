d07e724971e23bd59b98f2c1e0f3e21a0e0ae84a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-29 21:56:22 +0000

    ca76b2c docs(forms): correct signal field access in form model example

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.4+sha-baf09a9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.4+sha-ca76b2c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
