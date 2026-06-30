0aada3acf994a971fe21dec116eba857074e532f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-06-30 20:31:02 +0000

    a5f1b20 Revert "fix(compiler-cli): include toSignal in debugName transform"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.3+sha-a720094`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.3+sha-a5f1b20`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
