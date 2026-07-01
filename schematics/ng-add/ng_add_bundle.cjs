98926dc532143546fc62b3fcbf9120164aa24103
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-01 21:00:10 +0000

    c4fa24a docs: release notes for the v22.0.5 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.3+sha-c4cb66e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.3+sha-c4fa24a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
