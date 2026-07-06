0761428d6d30ebf091f1cf986bfa207ad87741d1
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-06 21:08:47 +0000

    8d22cc9 fix(compiler-cli): update babel dependencies to latest v7

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.17+sha-5a693ba`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.17+sha-8d22cc9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
