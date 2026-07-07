71aeef47bbc2ab9a6c7613622eeebe066015abb3
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 20:55:37 +0000

    f4d2069 build: lock file maintenance

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.5+sha-534fe81`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.5+sha-f4d2069`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
