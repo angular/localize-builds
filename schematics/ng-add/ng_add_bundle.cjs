2c85392d6d68492b6710ec7b7b54d9a0c9e665a5
GitHub <noreply@github.com>
2026-07-07 16:33:16 +0000

    d837d62 docs: add signal forms dynamic forms with json guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.4+sha-731d665`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.4+sha-d837d62`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
