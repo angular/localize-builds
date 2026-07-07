686827cb8d715ea8427ff1499e3a13cf030e472f
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 17:21:44 +0000

    91df739 fix(http): prevent caching of responses with Set-Cookie headers

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.17+sha-8d22cc9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.17+sha-91df739`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
