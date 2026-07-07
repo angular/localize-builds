b99167c5fabd2e4df3bbf48c6b140eaa15f81ffe
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 17:00:21 +0000

    5ec0b16 fix(docs-infra): disable nav-list tooltip on mobile

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.4+sha-e91046f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.4+sha-5ec0b16`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
