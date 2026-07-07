b62d7c39f53f3f53e785f9d8b1d599998fdecf7e
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-07 17:26:15 +0000

    56847dc fix(devtools): highlight SVG and directive hosts correctly

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.4+sha-6edabc6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.4+sha-56847dc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
