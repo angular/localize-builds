d81f40ebe959157767aa47348cda3a3daf0722eb
leonsenft <leonsenft@users.noreply.github.com>
2026-07-07 18:13:13 +0000

    534fe81 fix(compiler-cli): apply debugName transform to required signal queries

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.5+sha-c91a951`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.5+sha-534fe81`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
