f9cedcdc9e113aec99bd2b67f5546d18b60adda4
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 17:52:03 +0000

    39ea1c5 docs: fix grammar and punctuation issues in guides

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.4+sha-c1829f6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.4+sha-39ea1c5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
