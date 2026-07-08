ccea97a64353c6d95503169a506484a3e3dab5c2
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 19:27:24 +0000

    eac04cf docs: clarify runtime component comparison in elements guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.4+sha-c0eaaed`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.4+sha-eac04cf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
