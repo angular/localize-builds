3d7eb5ae6ccd162d3680d5f80a552ca1fa0c9afe
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 18:34:19 +0000

    c0eaaed refactor(core): cleanup Meta service

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.4+sha-39ea1c5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.4+sha-c0eaaed`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
