7610fcc53e6a77c02097f877a3b0fa655539834f
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 17:52:31 +0000

    2d52d53 docs: update optional chaining behavior

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.5+sha-748faa4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.5+sha-2d52d53`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
