916a3c525bef98eff14c3c8ce5003f66d718e465
leonsenft <leonsenft@users.noreply.github.com>
2026-07-08 18:34:58 +0000

    ab18f5f refactor(core): cleanup Meta service

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.5+sha-2d52d53`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.5+sha-ab18f5f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
