5feb0e3da897858c7e8bd10131aa7a45d5728e40
Leon Senft <leonsenft@users.noreply.github.com>
2026-07-08 22:10:16 +0000

    5d00aa2 release: cut the v21.2.18 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.17+sha-e86c31b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.18+sha-5d00aa2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
