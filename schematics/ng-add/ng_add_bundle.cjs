4f1a9ed0e36d53ef94b6669b84c927cff6cfea49
Matthew Beck <me@mattrb.com>
2026-07-09 16:43:37 +0000

    5bd00ad fix(compiler): support foreign components inside control flow blocks (#69674)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.5+sha-e3ac727`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.5+sha-5bd00ad`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
