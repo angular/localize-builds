ae63cff47ec76f3c8799468b980f043eabc41010
Matthew Beck <me@mattrb.com>
2026-07-10 18:22:45 +0000

    e6272f2 feat(docs-infra): add Angie to the embedded editor error state

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.6+sha-c82ae67`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.6+sha-e6272f2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
