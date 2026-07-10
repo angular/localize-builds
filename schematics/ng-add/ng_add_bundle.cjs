d7f531b5212f7030bfc8708cfc4440008e1b97a4
Matthew Beck <me@mattrb.com>
2026-07-10 01:05:46 +0000

    c82ae67 Revert "fix(core): allow static attributes for explicit input transforms"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.6+sha-97c830b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.6+sha-c82ae67`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
