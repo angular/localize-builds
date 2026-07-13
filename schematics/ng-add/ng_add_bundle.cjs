d63c76af225912806bc5b394605b6f661f05af6d
Matthew Beck <me@mattrb.com>
2026-07-13 18:57:57 +0000

    f6a00ff docs(docs-infra): remove orphaned IDX config left behind by #68946

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.5+sha-13d9cc3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.5+sha-f6a00ff`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
