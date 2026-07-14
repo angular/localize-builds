ae2577a4a671b9451a1913336eb3eddb74a4815b
Matthew Beck <me@mattrb.com>
2026-07-14 15:06:23 +0000

    ca37790 docs: add native html validation clarification in signal forms docs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.5+sha-d5e0b13`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.5+sha-ca37790`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
