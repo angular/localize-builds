1af296b69d20a257799aea567a8a1bc87b36d071
Matthew Beck <me@mattrb.com>
2026-07-14 16:28:32 +0000

    20b7dc3 fix(http): prevent interceptor signal reads from leaking into calling reactive contexts

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.6+sha-2695290`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.6+sha-20b7dc3`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
