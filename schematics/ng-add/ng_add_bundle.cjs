76708ad3c6eebd54c6ba3d7b4f797a168fa3ff1c
Matthew Beck <me@mattrb.com>
2026-07-15 23:02:41 +0000

    64d6d47 fix(forms): preserve intermediate number values in signal forms

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.6+sha-829d98e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.6+sha-64d6d47`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
