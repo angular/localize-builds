3731a7581e7ffc6cdfc7ba56d8dde2ea86fa9104
Matthew Beck <me@mattrb.com>
2026-07-15 19:11:26 +0000

    0ae6d81 fix(core): preserve explicit input transform write type

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.5+sha-d997a96`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.5+sha-0ae6d81`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
