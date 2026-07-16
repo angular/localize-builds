3b5e421665d83331ab812380ec6a999cc981de2a
Matthew Beck <me@mattrb.com>
2026-07-16 04:34:56 +0000

    f8c9e9d release: cut the v22.1.0-next.6 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.5+sha-265de87`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.6+sha-f8c9e9d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
