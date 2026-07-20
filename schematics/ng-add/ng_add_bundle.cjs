af3cd742ee6b0c1b8f3155e030d3738d4f6f8ba8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 12:46:17 +0000

    9d40f8a fix(http): prevent transfer cache key collisions

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.7+sha-e02a0de`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.7+sha-9d40f8a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
