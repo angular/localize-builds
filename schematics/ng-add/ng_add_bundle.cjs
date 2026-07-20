6620da9041d6628dac4bf2665bd414cd0f7fe879
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 14:10:26 +0000

    3ff6100 fix(docs-infra): only claim a search on the 404 page when results exist

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.7+sha-9d40f8a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.7+sha-3ff6100`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
