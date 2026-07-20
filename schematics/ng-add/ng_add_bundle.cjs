5de31270f9b0d011a026ea439d88445ec8d4c93a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 09:32:31 +0000

    e00e291 build: update all github actions

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.7+sha-e14ead1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.7+sha-e00e291`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
