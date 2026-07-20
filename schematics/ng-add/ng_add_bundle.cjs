77ece55982fbcbd3a9082fb6962cbe46d07bf593
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-20 08:46:55 +0000

    e14ead1 Revert "fix(forms): preserve intermediate number values in signal forms"

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.7+sha-f8df06d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.7+sha-e14ead1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
