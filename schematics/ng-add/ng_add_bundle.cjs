8e1ae84aff096b17476ba15ea37b2df79af72766
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 17:37:08 +0000

    388daea fix(migrations): correctly migrate ngClass with mixed space-separated keys

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.7+sha-fb68711`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.7+sha-388daea`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
