fbe6a8784be557ac50c8638e6b314b6149e2515f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-21 11:54:04 +0000

    d302c7a fix(forms):  ensure `pending` status propagates to the root form in signal forms

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.7+sha-a79eb0d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.7+sha-d302c7a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
