59ec619667737b8eb8e82a371f82cbf625967e01
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 12:31:53 +0000

    ab52df4 fix(docs-infra): fail the guide build when a markdown file starts with a BOM

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.6+sha-9a1e620`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.6+sha-ab52df4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
