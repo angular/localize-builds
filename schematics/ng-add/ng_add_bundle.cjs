5ddc31f350d7544493dc914f44bbce4b716dcc27
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 14:40:19 +0000

    548ef47 release: cut the v22.0.8 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.7+sha-45eef01`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.8+sha-548ef47`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
