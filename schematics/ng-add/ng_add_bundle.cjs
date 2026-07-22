85b54ac4e84c286581c88adf9cf333f05be9f30a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 14:23:56 +0000

    e428df2 build: update pnpm-lock.yaml

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.6+sha-ab52df4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.6+sha-e428df2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
