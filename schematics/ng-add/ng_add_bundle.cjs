af9a5ff5b598cee254ebdb1e1292ab3e10b517de
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 14:40:38 +0000

    509cad6 docs: release notes for the v22.0.8 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.6+sha-e428df2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.6+sha-509cad6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
