3d1da6026e3b94f5f770665e0ef85bec0b3077f8
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-07-22 14:51:18 +0000

    3bf2430 docs: release notes for the v22.1.0-rc.0 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0-next.6+sha-509cad6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.0+sha-3bf2430`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
