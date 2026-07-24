cac67509eb61747fbc7d0d612b4f1941345e413f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 15:33:45 +0000

    28d59e8 docs: use https for external links in adev

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.0+sha-e779309`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.0+sha-28d59e8`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
