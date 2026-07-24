0ed276bb930748ca95c8ff50519561da81ef831f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-24 21:01:07 +0000

    5ad8231 fix(migrations): correctly detect `then`/`else` keywords in control flow migration

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.0+sha-e606a02`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.0+sha-5ad8231`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
