8b1ced7d3f61331caa2b525faae9a8df6791fe98
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 19:57:47 +0000

    72a8512 release: cut the v20.3.27 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.26+sha-db0d4a1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.27+sha-72a8512`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
