496a532bd7047ee3a37aad4be30e5c235f2d8de6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 16:00:52 +0000

    7b884f5 fix(compiler): restrict possible event handler check to property names longer than 2 characters

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.18+sha-9949dcc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.18+sha-7b884f5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
