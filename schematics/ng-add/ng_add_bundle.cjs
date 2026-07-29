9fc255ce66213b01aeb1fefaf3e1bec8bacd500a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-07-29 16:55:42 +0000

    f90690f docs: fix self-referential link in defer block description

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.0.8+sha-45d7de4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.0.8+sha-f90690f`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
