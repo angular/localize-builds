89fa813f027fd6bbbc1c660978256dedd0ea0bf9
Alan Agius <alan.agius4@gmail.com>
2026-07-29 20:55:45 +0000

    07cb840 release: cut the v21.2.19 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.18+sha-7b884f5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.19+sha-07cb840`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
