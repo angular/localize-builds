b7123dc0a2e75cfe52593e0cbb62fe365bcbca8e
GitHub <noreply@github.com>
2026-07-30 15:51:59 +0000

    2f96c80 fix(core): sanitize host bindings on concrete hosts

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.27+sha-72a8512`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.27+sha-2f96c80`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
