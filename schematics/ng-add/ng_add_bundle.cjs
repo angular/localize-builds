41956a082a147d1358426976e59598b78c2965e8
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-30 15:28:26 +0000

    c4b1432 docs: document overlay stacking change in v21 update guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0+sha-d366e4b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0+sha-c4b1432`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
