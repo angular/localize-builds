addc20a6b09839e51ad659b9528ffb5b90df77f5
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-31 15:38:48 +0000

    a13b968 fix(http): run root interceptors in the terminal request chain

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0+sha-8c6f5e7`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0+sha-a13b968`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
