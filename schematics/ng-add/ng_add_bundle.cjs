81a2f8349d240f84ee6ed55fbfffacddce76af74
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-07-31 22:22:46 +0000

    caf6166 fix(http): run root interceptors in the terminal request chain

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.19+sha-6afe6fa`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.19+sha-caf6166`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
