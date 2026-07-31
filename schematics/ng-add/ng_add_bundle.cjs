30f03c87a0bc0ea1c645e490133b0a4e56e344f0
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-07-31 22:41:43 +0000

    3d0067a refactor(animations): remove outdated comment

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0+sha-aa6d318`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0+sha-3d0067a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
