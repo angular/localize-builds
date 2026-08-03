13877065a12fc2f6a552874680f4175bc59bba8c
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-03 16:38:23 +0000

    832ff2a test(platform-server): add SSR integration test for null and normal input values

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0+sha-baff4d0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0+sha-832ff2a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
