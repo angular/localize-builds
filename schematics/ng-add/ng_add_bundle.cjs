9bae339a67d5ca77473fbd50c9a493ed1d1f0cbd
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-04 17:39:35 +0000

    6f86d66 docs: fix supported Node.js version in installation prerequisites

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0+sha-ac3728e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.0+sha-6f86d66`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
