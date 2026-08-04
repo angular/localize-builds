2320a6dc22edada5d3daf36119b3aca26e49367d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-04 16:45:24 +0000

    ef4dfea fix(http): avoid aborting completed requests in FetchBackend

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.0+sha-2f0be5b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.0+sha-ef4dfea`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
