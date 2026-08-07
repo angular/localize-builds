ea112ffa6a43ae1312d49c8d62dc4f42071c6b92
Alon Mishne <amishne@users.noreply.github.com>
2026-08-07 23:42:31 +0000

    4d985a1 fix(core): incorrect loop in defer blocks

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.1+sha-7c15737`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.1+sha-4d985a1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
