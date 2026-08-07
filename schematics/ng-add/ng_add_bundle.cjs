cc8c67154d8ca0f25f9b37ff177a43116891de41
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-07 23:29:33 +0000

    4631a6e refactor(forms): modernize Signal Forms tests

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-adb770b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-4631a6e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
