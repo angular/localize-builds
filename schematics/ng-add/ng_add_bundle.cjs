69f7640c309420837acea57e0c50748ea12bd1c8
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-07 23:10:15 +0000

    adb770b build: update bazel dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-cf1844b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-adb770b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
