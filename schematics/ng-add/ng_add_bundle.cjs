8339bef13d8666dff701ca0d6df09789dd9c202e
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-07 17:37:49 +0000

    cf1844b refactor(devtools): use JavaScriptTransformer for Angular optimization esbuild plugin

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-8a40fbe`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-cf1844b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
