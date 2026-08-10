85e4b2af2127ba3791bb5c0ff1970efec1ae3d06
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-10 16:51:01 +0000

    4fc6e31 build: update dependency @types/jsdom to v30

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-768c8c2`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-4fc6e31`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
