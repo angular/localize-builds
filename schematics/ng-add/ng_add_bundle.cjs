008d85266d3cd94878f7273857d3a4b7f3667d29
Alon Mishne <amishne@users.noreply.github.com>
2026-08-10 22:43:59 +0000

    0df9d5e fix(core): ensure i18n_util hasOwnProperty checks are safe for property renaming

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.1+sha-596356e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.1+sha-0df9d5e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
