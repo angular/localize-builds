895806881f75784aa4c6e88eba9535b136622d90
Alon Mishne <amishne@users.noreply.github.com>
2026-08-11 19:15:19 +0000

    2f82601 fix(router): limit protocol-relative URL handling to serialization

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.1+sha-65e2aed`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.1+sha-2f82601`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
