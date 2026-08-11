9335fc5be6ba03d84786837b0d52760f92db47e1
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-11 17:13:10 +0000

    b65dea4 feat(router): allow throwing RedirectCommand to trigger redirects

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-8925a4e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-b65dea4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
