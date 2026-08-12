2b9ffbb1bbe6e653e46d9e2114ac1804a0374c2a
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-12 23:24:23 +0000

    bf1f634 build: update cross-repo angular dependencies to v22.2.0-next.1

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-3d32db6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-bf1f634`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
