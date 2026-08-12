f7bbd3a71d3317235dfe83742b5e1b1ebd5eca94
Alon Mishne <amishne@users.noreply.github.com>
2026-08-12 23:24:34 +0000

    e9660b1 fix(compiler-cli): correctly resolve symbol for SafePropertyRead in chained optional navigation

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.1+sha-3032e7f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.1+sha-e9660b1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
