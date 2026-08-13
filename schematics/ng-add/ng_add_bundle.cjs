54d462253bdbc0ef0aa8e8e233d9b9c9248a70a6
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 00:15:49 +0000

    c9990c4 docs: release notes for the v21.2.20 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-bf1f634`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-c9990c4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
