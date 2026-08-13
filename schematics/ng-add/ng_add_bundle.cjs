465412c01a1f382f9daed1cbdf84b86eddc7089f
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 17:14:47 +0000

    1d576ee release: cut the v20.3.28 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.27+sha-e4c416c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.28+sha-1d576ee`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
