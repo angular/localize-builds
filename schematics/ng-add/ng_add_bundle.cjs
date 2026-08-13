443e49452b987f692e8dc258c9f2771fcfdb5b78
Alon Mishne <3197814+amishne@users.noreply.github.com>
2026-08-13 18:55:09 +0000

    40d23fa release: cut the v22.2.0-next.2 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-044e69c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.2+sha-40d23fa`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
