09d8023af1df3543a1344676868831419a3b872a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-13 16:04:56 +0000

    647bf8e docs: note SafeResourceUrl audio src change

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-6f848db`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.1+sha-647bf8e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
