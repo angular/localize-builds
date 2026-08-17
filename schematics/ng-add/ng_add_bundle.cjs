12cc847f2826c67b86331b9996a7da39dda79d45
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-17 17:53:36 +0000

    2d87ed9 docs: update Angular MCP skill reference

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.2+sha-c04931c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.2+sha-2d87ed9`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
