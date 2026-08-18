8fa4a0d78bd284669014948a0b575945aa8fc125
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-18 16:10:21 +0000

    85f12a5 fix(core): stop running further effects once one destroys the view mid-flush

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.2+sha-b3c78a5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.2+sha-85f12a5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
