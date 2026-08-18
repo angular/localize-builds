b501ee7c34abcbe2bdd68813e4fe1084dc86a5ae
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-18 22:15:29 +0000

    abe019d fix(router): pass correct component to canDeactivate for named outlets in componentless parent routes

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.2+sha-49f4e15`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.2+sha-abe019d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
