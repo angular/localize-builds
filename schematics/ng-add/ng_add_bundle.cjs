22b3d80361bd72858db9379e361d3eb5bdb230f5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-18 23:04:29 +0000

    53fc371 fix(compiler): preserve &ngsp; between sibling control flow blocks

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.2+sha-f1a4c85`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.2+sha-53fc371`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
