83d15e06ddd143669d2e176abe4b8cef0415f9b4
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-08-19 00:17:54 +0000

    83450d2 fix(forms): report forbidden 2way bindings on when `FormField` is applied

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.2+sha-94f0b9a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.2+sha-83450d2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
