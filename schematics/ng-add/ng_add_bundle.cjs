ecfe6231f6a606365ff44393fec62330b8510017
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-19 20:06:36 +0000

    77f7b60 release: cut the v20.3.29 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -131,7 +131,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~20.3.28+sha-7538744`);
+  return (0, import_utility.addDependency)("@angular/localize", `~20.3.29+sha-77f7b60`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
