427a45e50ab8cbb563b736b46985fce98d6de21e
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-08-19 20:29:07 +0000

    d609cf6 release: bump Angular DevTools version to 1.20.0

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.2+sha-b7079c5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.3+sha-d609cf6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
