f0b55ba5dd3c8d19b40f46ebd824df7b8cf5beda
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-21 19:08:20 +0000

    238d8bf refactor(docs-infra): hold editor diagnostics in a signal

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.3+sha-2e2c426`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.3+sha-238d8bf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
