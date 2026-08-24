5e91632fc5c485253e7185a401aeb0b135535253
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-24 22:43:06 +0000

    82a7675 feat(devtools): add "Watch signal" button to signal-details component

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.3+sha-671d8d3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.3+sha-82a7675`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
