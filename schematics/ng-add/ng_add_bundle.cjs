d5b238508aef59e0bdcdf2b08d6a70a249d55ad6
leonsenft <leonsenft@users.noreply.github.com>
2026-08-25 23:37:28 +0000

    b199bdf fix(core): explicitly reject foreign components in JIT mode

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.3+sha-bf69e85`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.3+sha-b199bdf`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
