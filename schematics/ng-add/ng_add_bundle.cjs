d3da6cdaa9954f3a5de079fee792ab0830209e06
Leon Senft <leonsenft@users.noreply.github.com>
2026-08-25 17:14:45 +0000

    5aa6d97 fix(platform-server): avoid stripping unicode whitespace during url resolution

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~21.2.21+sha-73d8bbd`);
+  return (0, import_utility.addDependency)("@angular/localize", `~21.2.21+sha-5aa6d97`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
