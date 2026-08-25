84eaacc1dd0933e5785d66e0dde5b7cfd21c6f97
leonsenft <leonsenft@users.noreply.github.com>
2026-08-25 22:29:20 +0000

    bf69e85 refactor(docs-infra): remove example configs for tooling that no longer exists

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.3+sha-e3bf968`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.3+sha-bf69e85`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
