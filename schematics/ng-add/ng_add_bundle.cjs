53de37ae0b2201431a4ae92f151a33389a43dcff
leonsenft <leonsenft@users.noreply.github.com>
2026-08-26 20:57:44 +0000

    151a8f2 refactor(core): Decouple ɵɵFactoryDeclaration and ɵɵInjectableDeclaration from type parameter T.

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.3+sha-b199bdf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.3+sha-151a8f2`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
