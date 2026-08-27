8e1d3e870fcf970ef0d623a0b550aa860b604616
Matthew Beck <me@mattrb.com>
2026-08-27 17:29:41 +0000

    bee058e fix(docs-infra): point the tutorial .gitignore at live GitHub docs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.4+sha-6f51be6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.4+sha-bee058e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
