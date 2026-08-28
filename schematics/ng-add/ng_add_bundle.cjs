a4d12a0d45b2916ceb42817271b189d54a74c442
Matthew Beck <me@mattrb.com>
2026-08-28 04:15:28 +0000

    34817da refactor(core): defer foreign component rendering to post-update pass

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.4+sha-ad22e89`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.4+sha-34817da`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
