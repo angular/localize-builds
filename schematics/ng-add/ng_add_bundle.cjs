f78955a0233d832ac150f6e3859dd0d51f5c7a8e
Matthew Beck <me@mattrb.com>
2026-08-31 18:38:14 +0000

    d9afca0 fix(docs-infra): reject unclosed paired docs elements

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -142,7 +142,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.4+sha-563850e`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.4+sha-d9afca0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
