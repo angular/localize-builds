4f81e119d1d3295817bec35d67c1c760f0ccbb80
Matthew Beck <me@mattrb.com>
2026-08-31 22:19:42 +0000

    2ceeb27 fix(core): cancel stale debounce timers to prevent timer leaks

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.4+sha-3b8723c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.4+sha-2ceeb27`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
