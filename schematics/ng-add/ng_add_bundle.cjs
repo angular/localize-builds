7249a1678f4eb7805a721f0fc40097de0c8f22fc
Matthew Beck <me@mattrb.com>
2026-08-31 22:44:38 +0000

    87d38ec build: lock file maintenance

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.4+sha-4c4a705`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.4+sha-87d38ec`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
