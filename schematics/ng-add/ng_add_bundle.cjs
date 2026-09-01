275df390d82ec128bd5df775aa3866a43bd85563
Matthew Beck <me@mattrb.com>
2026-09-01 03:42:25 +0000

    d8e74e7 docs(docs-infra): improve update checkbox accessibility

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.4+sha-87d38ec`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.4+sha-d8e74e7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
