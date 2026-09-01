c1c94a1bd9611f5049019a5f129d7055e0b0fa17
Matthew Beck <me@mattrb.com>
2026-09-01 21:52:46 +0000

    b2903de ci: move `packages/private` under `fw-general`

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.4+sha-d90698d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.4+sha-b2903de`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
