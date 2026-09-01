5ffedcfce20d5a99d4badf302183ef566d770582
Matthew Beck <me@mattrb.com>
2026-09-01 16:50:33 +0000

    d90698d fix(compiler-cli): check uninvoked signal aliases in extended diagnostic

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.4+sha-36fc3f0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.4+sha-d90698d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
