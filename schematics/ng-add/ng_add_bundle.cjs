1e675e8650615f7a694b3e978e3ca4a65e4491cc
Matthew Beck <me@mattrb.com>
2026-09-02 22:59:27 +0000

    a66fd64 build: update cross-repo angular dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.4+sha-de91d10`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.4+sha-a66fd64`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
