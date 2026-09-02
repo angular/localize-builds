fda16d03f7c2ef0cd2282610d90441c82fc8ceb5
Matthew Beck <me@mattrb.com>
2026-09-02 23:17:53 +0000

    731b959 fix(vscode-extension): prevent JSDoc link provider injection

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.4+sha-94b1d3d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.4+sha-731b959`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
