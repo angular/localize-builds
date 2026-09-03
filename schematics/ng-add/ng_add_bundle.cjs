8cd57a1ab8a0bc800b74ce2608a25adb9052f9f2
Matthew Beck <me@mattrb.com>
2026-09-03 02:13:23 +0000

    e03f20a fix(docs-infra): ignore external links when mapping navigation items to routes

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.4+sha-731b959`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.4+sha-e03f20a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
