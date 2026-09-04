665770cb7eb4cad4f890dbc74e8d5e284dc98d0b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:15:25 +0000

    dfe7be4 refactor(compiler): emit any as type argument for ɵɵInjectableDeclaration

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.5+sha-468b65b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.5+sha-dfe7be4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
