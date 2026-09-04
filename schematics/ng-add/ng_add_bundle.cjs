d2dd86c301affdc76d0b38a7cb9a58f93ffa2aa1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:21:44 +0000

    74c1716 refactor(compiler-cli): index pipes in template expressions

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.5+sha-81c3f3a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.5+sha-74c1716`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
