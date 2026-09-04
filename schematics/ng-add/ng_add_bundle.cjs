bb6525793cd3aba621d1b8811ab67fb965d13c7a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:56:36 +0000

    ef48630 test(core): replace act with actAsync

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.5+sha-6f5406c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.5+sha-ef48630-with-local-changes`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
