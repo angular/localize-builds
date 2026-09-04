4ded5a4eb646f92a55a065794d8a92736b500085
Alex Rickabaugh <alx+alxhub@alxandria.net>
2026-09-04 14:33:19 +0000

    bae170d build: update bazel dependencies

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.5+sha-74c1716`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.5+sha-bae170d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
