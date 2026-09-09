6f9b28c107284e5095f3344548acc3a65efdd475
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 16:18:00 +0000

    679c50d refactor(router): Remove ActivatedRouteSnapshot from resourceContext

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.5+sha-25228d0`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.5+sha-679c50d`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
