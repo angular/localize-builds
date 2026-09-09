4fbfeb5bb6f5c99029a09d78ba55a88638d9faa9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 14:46:00 +0000

    2c71e88 docs: update cross-repo adev docs

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.5+sha-5afdd98`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.5+sha-2c71e88-with-local-changes`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
