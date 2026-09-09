89a7ed79a618eb6d6726e354d8f565d1631dd2dc
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2026-09-09 20:19:18 +0000

    356adf7 release: cut the v22.1.6 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.5+sha-05a05f5`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.6+sha-356adf7`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
