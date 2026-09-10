cbade9af87f42cac86f8267adf1cc18d57cc1685
Andrew Scott <atscott01@gmail.com>
2026-09-10 20:36:29 +0000

    5e632b6 fix(compiler): wrap `@for` collection expression before appending non-null assertion

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.6+sha-1905dc6`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.6+sha-5e632b6`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
