6403184b41c1c0cdb3f9a7f0746ec7ee14d1a067
Andrew Scott <atscott01@gmail.com>
2026-09-11 20:20:59 +0000

    c097c01 docs: import bootstrapApplication from platform-browser in the CSP example

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-a823323`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-c097c01`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
