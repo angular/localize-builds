1bf2216de4898208ba0904778c3f8c5fcbc45638
Andrew Scott <atscott01@gmail.com>
2026-09-11 21:46:50 +0000

    2dcdf9a fix(router): mark router_resource module-level symbols as side-effect free

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-c097c01`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-2dcdf9a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
