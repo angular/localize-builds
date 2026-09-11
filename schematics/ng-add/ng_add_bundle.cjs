a748e081e1b9e09de9534cfdef9b99f930a01ebb
Andrew Scott <atscott01@gmail.com>
2026-09-11 22:25:46 +0000

    f2cf65a fix(vscode-extension): handle template literals when detecting supported decorator fields

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-2dcdf9a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-f2cf65a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
