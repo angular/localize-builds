233be1ae5ac598bd3faf46ab0c5fa62b400ed502
Andrew Scott <atscott01@gmail.com>
2026-09-11 18:09:05 +0000

    d0ed76c docs: fix the broken housing location template in the first-app tutorial

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-01d091b`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-d0ed76c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
