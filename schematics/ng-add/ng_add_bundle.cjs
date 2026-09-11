28c6998855b1e7193fdf81d734d9172cdb402c0d
GitHub <noreply@github.com>
2026-09-11 19:52:04 +0000

    a823323 fix(core): update FakeNavigation to match WHATWG HTML spec

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-d0ed76c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-a823323`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
