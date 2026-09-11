39e3485057730c0c1ab6e4b19b3e7f563f54c015
Andrew Scott <atscott01@gmail.com>
2026-09-11 23:08:58 +0000

    fc2f6fd docs: stop the disabled aria toolbar radio group from changing selection

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-f2cf65a`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-fc2f6fd`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
