152bcc7097fa91fa29476f04fe85237db98a1a30
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 14:52:22 +0000

    f73f059 docs: fix API links that point at missing member anchors

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.6+sha-24ca9d4`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.6+sha-f73f059`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
