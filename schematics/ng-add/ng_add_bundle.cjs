1ad9575390e47bae1a03b7c40b78b3aeb1eddea5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2026-09-16 14:10:44 +0000

    5cdae0a docs: remove outdated useFactory and expression lowering guidance

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-430a8eb`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-next.7+sha-5cdae0a`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
