94804808ec47febc8e0c37ca7705da8708a87db9
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-17 17:42:15 +0000

    c0ed0ad docs(core): restore the PipeTransform code examples

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-7263031`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-c0ed0ad`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
