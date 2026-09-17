755a9a89d3fffcd2df09d764cb49ef69abc041ba
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-17 21:25:43 +0000

    0b632de refactor(platform-browser): type shadow DOM renderer fields

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-03754b3`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-0b632de`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
