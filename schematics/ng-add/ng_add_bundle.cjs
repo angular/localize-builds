bb42829b66295a8e0d6c5fd7ad74381dc5cd566a
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-17 18:08:08 +0000

    7e678e0 fix(core): cancel leave animations in nested views during list reordering

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.3.0-next.0+sha-6e3ce79`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.3.0-next.0+sha-7e678e0`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
