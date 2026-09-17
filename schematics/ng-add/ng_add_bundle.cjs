159a4b8c84092ab3e1b6f704fa208d6bd7a5f03d
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-17 17:54:03 +0000

    6e3ce79 docs: use the remote-cache config in the bazel caching guide

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.3.0-next.0+sha-9ba5faa`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.3.0-next.0+sha-6e3ce79`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
