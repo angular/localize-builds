6d571f84b54b11e2ba5853ef43f6dc962b8de139
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-18 16:55:04 +0000

    c08ba52 docs(core): add error guide for NG0600

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-7bf58f9`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-c08ba52`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
