504159eb9e9fce907047b8ec895fa1b52fa26f92
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-18 16:15:25 +0000

    f15be33 fix(docs-infra): render the signature of decorators without members

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-0b632de`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-f15be33`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
