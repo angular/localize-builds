b76fdd3415148aa9b068acc80b453ff203cce34c
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 21:51:07 +0000

    378ecfc docs: sync AI context files with best-practices.md

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-rc.0+sha-602799c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-rc.0+sha-378ecfc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
