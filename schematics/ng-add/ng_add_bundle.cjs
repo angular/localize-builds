2b1376274e4599ec62d1e19df0ea452aa9f21976
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-21 17:17:42 +0000

    aa2cabb refactor(compiler): add method to emitter context

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-f39db77`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-aa2cabb`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
