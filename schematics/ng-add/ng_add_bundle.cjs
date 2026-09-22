d3bc765a22f501b84f146a705148cb2f85828145
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 23:11:39 +0000

    efe48df docs(core): fix three JSDoc rendering bugs on the API pages

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-rc.0+sha-8287b3f`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-rc.0+sha-efe48df`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
