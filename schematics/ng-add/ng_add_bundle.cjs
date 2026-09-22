ada0c8590910ae7ffca0b7d933bc73c29915670e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 16:24:17 +0000

    d0f7229 test(core): cover boundary around projected content

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-rc.0+sha-bc402cf`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-rc.0+sha-d0f7229`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
