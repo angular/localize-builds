d7987a2cc78abe2ddfbb541212184c18c9ce9cdb
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-22 16:16:22 +0000

    8d8859c fix(core): don't use plain objects as maps for jsaction data

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.3.0-next.0+sha-d5f409c`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.3.0-next.0+sha-8d8859c`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
