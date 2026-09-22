e3ff0343e70cef3f7d5e1f561e2db1f9da2f193d
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-22 21:27:06 +0000

    726368b docs: correct the adev writing guide skill

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-71f84a8`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-726368b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
