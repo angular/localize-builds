5b9f428f8fe209cc029f62484519f42640e085c3
Andrew Scott <atscott01@gmail.com>
2026-09-23 19:38:45 +0000

    0737c4e test(router): add test coverage for route resource reloading and input binding

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.3.0-next.0+sha-4d3f502`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.3.0-next.0+sha-0737c4e`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
