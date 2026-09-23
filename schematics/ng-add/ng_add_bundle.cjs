dbce58bb7c695a968ba462d7b9815af40173aa59
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 18:01:49 +0000

    fc187d4 release: cut the v22.2.0 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0-rc.0+sha-52c33bc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0+sha-fc187d4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
