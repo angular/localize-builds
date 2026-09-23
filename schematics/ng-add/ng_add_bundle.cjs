0d834a08d2ca54410beb97e501d02b0effeee00e
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-23 15:50:04 +0000

    a2fedc1 docs(upgrade): add maintenance mode note

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-282f9fe`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-a2fedc1`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
