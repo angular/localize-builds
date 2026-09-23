96b43b5653082ae673939a622a02d495ca54fe9c
Jessica Janiuk <jessica.janiuk@gmail.com>
2026-09-23 16:02:26 +0000

    2cab6f5 fix(router): avoid dense elements allocation for numeric URL keys

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-a2fedc1`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.1.7+sha-2cab6f5`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
