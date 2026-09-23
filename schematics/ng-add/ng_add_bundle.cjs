a52bc570827f732cae6947a97c41ce237bf8cfc5
Jessica Janiuk <1596273+thePunderWoman@users.noreply.github.com>
2026-09-23 17:53:45 +0000

    123729b docs: release notes for the v21.2.24 release

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.3.0-next.0+sha-42ce791`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.3.0-next.0+sha-123729b`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
