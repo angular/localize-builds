ee60a3ae6dc95a0525fa9a6e4eceb2b92c1239b3
Andrew Scott <atscott01@gmail.com>
2026-09-24 19:45:10 +0000

    21851fc fix(core): add debugName on afterRenderEffect

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0+sha-7f9c14d`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0+sha-21851fc`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
