c8400506936ff8407d8d646e6aebf5a5ded9868b
Andrew Scott <atscott01@gmail.com>
2026-09-25 22:50:12 +0000

    756e8c4 docs: update releases.md for v22.2

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0+sha-870f200`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0+sha-756e8c4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
