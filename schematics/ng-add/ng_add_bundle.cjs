15e53b517930c959185f52065910cbcb1894dccf
GitHub <noreply@github.com>
2026-09-25 17:53:21 +0000

    5db6fc4 release: bump VSCode extension version to 22.2.0 (#70950)

--- schematics/ng-add/ng_add_bundle.cjs
+++ schematics/ng-add/ng_add_bundle.cjs
@@ -141,7 +141,7 @@ function moveToDependencies(host) {
     return;
   }
   (0, import_dependencies.removePackageJsonDependency)(host, "@angular/localize");
-  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0+sha-21851fc`);
+  return (0, import_utility.addDependency)("@angular/localize", `~22.2.0+sha-5db6fc4`);
 }
 function ng_add_default(options) {
   const projectName = options.project;
